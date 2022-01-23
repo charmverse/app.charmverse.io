9a7a5e74fe52093730eef24f7bbdb03f6993f878
Safwan Shaheer <devorein00@gmail.com>
2022-01-23 11:44:40 +0600

    Moved dropdown component to separate module

---
+++ components/Dropdown.tsx
@@ -0,0 +1,103 @@
+import { EditorView } from "@bangle.dev/pm";
+import autocomplete, { ActionKind, closeAutocomplete, FromTo, Options } from "prosemirror-autocomplete";
+import dropdownGroups from "./dropdown.json";
+import { DropdownGroup } from "./types";
+
+const picker = {
+  view: null as EditorView | null,
+  open: false,
+  current: 0,
+  range: null as FromTo | null,
+};
+
+const totalDropdownItems = dropdownGroups.reduce((current, dropdownGroup) => current + dropdownGroup.items.length, 0);
+const dropdownItems: HTMLDivElement[] = [];
+
+function placeSuggestion() {
+  const suggestion = document.querySelector('#suggestion') as HTMLDivElement;
+  suggestion.style.display = picker.open ? 'block' : 'none';
+  const rect = document.getElementsByClassName('autocomplete')[0]?.getBoundingClientRect();
+
+  if (!rect) return;
+  suggestion.style.top = `${rect.top + rect.height}px`;
+  suggestion.style.left = `${rect.left}px`;
+
+  dropdownItems.forEach((dropdownItem, dropdownItemIndex) => {
+    dropdownItem.classList[dropdownItemIndex === picker.current ? 'add' : 'remove']('selected')
+  });
+}
+
+const options: Options = {
+  reducer: (action) => {
+    picker.view = action.view;
+    switch (action.kind) {
+      case ActionKind.open:
+        picker.current = 0;
+        picker.open = true;
+        picker.range = action.range;
+        placeSuggestion();
+        return true;
+      case ActionKind.close:
+        picker.open = false;
+        placeSuggestion();
+        return true;
+      case ActionKind.up:
+        picker.current -= 1;
+        picker.current += totalDropdownItems;
+        picker.current %= totalDropdownItems;
+        placeSuggestion();
+        return true;
+      case ActionKind.down:
+        picker.current += 1;
+        picker.current %= totalDropdownItems;
+        placeSuggestion();
+        return true;
+      case ActionKind.enter: {
+        const tr = action.view.state.tr
+          .deleteRange(action.range.from, action.range.to)
+          .insertText(`You can define this ${action.type ? `${action.type?.name} ` : ''}action!`);
+        action.view.dispatch(tr);
+        return true;
+      }
+      default:
+        return false;
+    }
+  },
+  triggers: [
+    { name: 'command', trigger: '/', decorationAttrs: { class: 'command' } },
+  ],
+};
+
+export const autocompletePlugin = autocomplete(options);
+
+function dropdownItemClickHandler() {
+  if (!picker.view) return;
+  closeAutocomplete(picker.view);
+  picker.open = false;
+  placeSuggestion();
+  if (!picker.range) return;
+  const tr = picker.view.state.tr
+    .deleteRange(picker.range.from, picker.range.to)
+    .insertText(`Clicked`);
+  picker.view.dispatch(tr);
+  picker.view.focus();
+}
+
+export const DropdownComponent = <div id="suggestion" style={{ display: "none" }}>
+  {(dropdownGroups as DropdownGroup[]).map(dropdownGroup => <div className="suggestion-group" key={dropdownGroup.group}>
+    <div className="suggestion-group-name">{dropdownGroup.group}</div>
+    <div className="suggestion-group-items">
+      {dropdownGroup.items.map(item => {
+        const dropdownItemComponent = <div ref={(element) => {
+          if (element) {
+            dropdownItems.push(element);
+          }
+        }} key={`${dropdownGroup.group}.${item.label}`} onClick={dropdownItemClickHandler} className="suggestion-group-item">
+          <span className="suggestion-group-item-icon">{item.icon}</span>
+          <span className="suggestion-group-item-label">{item.label}</span>
+        </div>;
+        return dropdownItemComponent;
+      })}
+    </div>
+  </div>)}
+</div>
--- components/editor/BangleEditor.tsx
+++ components/editor/BangleEditor.tsx
@@ -22,115 +22,16 @@ import {
 import '@bangle.dev/core/style.css';
 import { emoji } from '@bangle.dev/emoji';
 import { markdownParser, markdownSerializer } from '@bangle.dev/markdown';
-import { EditorView, NodeSelection } from '@bangle.dev/pm';
+import { NodeSelection } from '@bangle.dev/pm';
 import { BangleEditor as ReactBangleEditor, useEditorState } from '@bangle.dev/react';
 import { EmojiSuggest, emojiSuggest } from '@bangle.dev/react-emoji-suggest';
 import { floatingMenu, FloatingMenu } from '@bangle.dev/react-menu';
 import '@bangle.dev/react-menu/style.css';
 import '@bangle.dev/tooltip/style.css';
 import gemojiData from 'emoji-lookup-data/data/gemoji.json';
-import { ActionKind, autocomplete, closeAutocomplete, FromTo, Options } from "prosemirror-autocomplete";
-import { DropdownGroup } from '../../components/types';
-import dropdownGroups from "../dropdown.json";
+import { autocompletePlugin, DropdownComponent } from '../../components/Dropdown';
 
 const menuKey = new PluginKey('menuKey');
-
-const picker = {
-  view: null as EditorView | null,
-  open: false,
-  current: 0,
-  range: null as FromTo | null,
-};
-
-const totalDropdownItems = dropdownGroups.reduce((current, dropdownGroup) => current + dropdownGroup.items.length, 0);
-const dropdownItems: HTMLDivElement[] = [];
-
-function placeSuggestion() {
-  const suggestion = document.querySelector('#suggestion') as HTMLDivElement;
-  suggestion.style.display = picker.open ? 'block' : 'none';
-  const rect = document.getElementsByClassName('autocomplete')[0]?.getBoundingClientRect();
-
-  if (!rect) return;
-  suggestion.style.top = `${rect.top + rect.height}px`;
-  suggestion.style.left = `${rect.left}px`;
-
-  dropdownItems.forEach((dropdownItem, dropdownItemIndex) => {
-    dropdownItem.classList[dropdownItemIndex === picker.current ? 'add' : 'remove']('selected')
-  });
-}
-
-const options: Options = {
-  reducer: (action) => {
-    picker.view = action.view;
-    switch (action.kind) {
-      case ActionKind.open:
-        picker.current = 0;
-        picker.open = true;
-        picker.range = action.range;
-        placeSuggestion();
-        return true;
-      case ActionKind.close:
-        picker.open = false;
-        placeSuggestion();
-        return true;
-      case ActionKind.up:
-        picker.current -= 1;
-        picker.current += totalDropdownItems; // negative modulus doesn't work
-        picker.current %= totalDropdownItems;
-        placeSuggestion();
-        return true;
-      case ActionKind.down:
-        picker.current += 1;
-        picker.current %= totalDropdownItems;
-        placeSuggestion();
-        return true;
-      case ActionKind.enter: {
-        const tr = action.view.state.tr
-          .deleteRange(action.range.from, action.range.to)
-          .insertText(`You can define this ${action.type ? `${action.type?.name} ` : ''}action!`);
-        action.view.dispatch(tr);
-        return true;
-      }
-      default:
-        return false;
-    }
-  },
-  triggers: [
-    { name: 'command', trigger: '/', decorationAttrs: { class: 'command' } },
-  ],
-};
-
-function suggestionItemClickHandler() {
-  if (!picker.view) return;
-  closeAutocomplete(picker.view);
-  picker.open = false;
-  placeSuggestion();
-  if (!picker.range) return;
-  const tr = picker.view.state.tr
-    .deleteRange(picker.range.from, picker.range.to)
-    .insertText(`Clicked`);
-  picker.view.dispatch(tr);
-  picker.view.focus();
-}
-
-const suggestionComponent = <div id="suggestion" style={{ display: "none" }}>
-  {(dropdownGroups as DropdownGroup[]).map(dropdownGroup => <div className="suggestion-group" key={dropdownGroup.group}>
-    <div className="suggestion-group-name">{dropdownGroup.group}</div>
-    <div className="suggestion-group-items">
-      {dropdownGroup.items.map(item => {
-        const dropdownItemComponent = <div ref={(element) => {
-          if (element) {
-            dropdownItems.push(element);
-          }
-        }} key={`${dropdownGroup.group}.${item.label}`} onClick={suggestionItemClickHandler} className="suggestion-group-item">
-          <span className="suggestion-group-item-icon">{item.icon}</span>
-          <span className="suggestion-group-item-label">{item.label}</span>
-        </div>;
-        return dropdownItemComponent;
-      })}
-    </div>
-  </div>)}
-</div>
 
 const emojiSuggestKey = new PluginKey('emojiSuggestKey');
 
@@ -180,7 +81,6 @@ const specRegistry = new SpecRegistry([
 const parser = markdownParser(specRegistry);
 const serializer = markdownSerializer(specRegistry);
 
-
 export default function Editor() {
   const state = useEditorState({
     specRegistry,
@@ -201,7 +101,7 @@ export default function Editor() {
       paragraph.plugins(),
       strike.plugins(),
       underline.plugins(),
-      ...autocomplete(options),
+      ...autocompletePlugin,
       emoji.plugins(),
       emojiSuggest.plugins({
         key: emojiSuggestKey,
@@ -245,7 +145,7 @@ export default function Editor() {
   return <ReactBangleEditor state={state}>
     <FloatingMenu menuKey={menuKey} />
     <EmojiSuggest emojiSuggestKey={emojiSuggestKey} />
-    {suggestionComponent}
+    {DropdownComponent}
   </ReactBangleEditor>
 }
 
