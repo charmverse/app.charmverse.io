46871935f2b84718aaa6ec76705be35bd321d36a
Safwan Shaheer <devorein00@gmail.com>
2022-01-24 09:13:37 +0600

    Removed prosemirror autocomplete dependency

--- components/editor/BangleEditor.tsx
+++ components/editor/BangleEditor.tsx
@@ -29,8 +29,7 @@ import { floatingMenu, FloatingMenu } from '@bangle.dev/react-menu';
 import '@bangle.dev/react-menu/style.css';
 import '@bangle.dev/tooltip/style.css';
 import gemojiData from 'emoji-lookup-data/data/gemoji.json';
-import { ActionKind, autocomplete, closeAutocomplete, FromTo, Options } from "prosemirror-autocomplete";
-import { useEffect } from 'react';
+import { FromTo } from "prosemirror-autocomplete";
 
 const menuKey = new PluginKey('menuKey');
 
@@ -39,61 +38,6 @@ const picker = {
   open: false,
   current: 0,
   range: null as FromTo | null,
-};
-
-const NUM_SUGGESTIONS = 3;
-
-function placeSuggestion() {
-  const suggestion = document.querySelector('#suggestion') as HTMLDivElement;
-  suggestion.style.display = picker.open ? 'block' : 'none';
-  const rect = document.getElementsByClassName('autocomplete')[0]?.getBoundingClientRect();
-  if (!rect) return;
-  suggestion.style.top = `${rect.top + rect.height}px`;
-  suggestion.style.left = `${rect.left}px`;
-  [].forEach.call(suggestion.children, (item: HTMLDivElement, i) => {
-    item.classList[i === picker.current ? 'add' : 'remove']('selected');
-  });
-}
-
-const options: Options = {
-  reducer: (action) => {
-    picker.view = action.view;
-    switch (action.kind) {
-      case ActionKind.open:
-        picker.current = 0;
-        picker.open = true;
-        picker.range = action.range;
-        placeSuggestion();
-        return true;
-      case ActionKind.close:
-        picker.open = false;
-        placeSuggestion();
-        return true;
-      case ActionKind.up:
-        picker.current -= 1;
-        picker.current += NUM_SUGGESTIONS; // negative modulus doesn't work
-        picker.current %= NUM_SUGGESTIONS;
-        placeSuggestion();
-        return true;
-      case ActionKind.down:
-        picker.current += 1;
-        picker.current %= NUM_SUGGESTIONS;
-        placeSuggestion();
-        return true;
-      case ActionKind.enter: {
-        const tr = action.view.state.tr
-          .deleteRange(action.range.from, action.range.to)
-          .insertText(`You can define this ${action.type ? `${action.type?.name} ` : ''}action!`);
-        action.view.dispatch(tr);
-        return true;
-      }
-      default:
-        return false;
-    }
-  },
-  triggers: [
-    { name: 'command', trigger: '/', decorationAttrs: { class: 'command' } },
-  ],
 };
 
 const emojiSuggestKey = new PluginKey('emojiSuggestKey');
@@ -147,25 +91,6 @@ const serializer = markdownSerializer(specRegistry);
 export default function Editor() {
   const suggestion = document.querySelector('#suggestion') as HTMLDivElement;
 
-  useEffect(() => {
-    if (suggestion) {
-      Array.from(suggestion.children).forEach((item, index) => {
-        item.addEventListener('click', () => {
-          if (!picker.view) return;
-          closeAutocomplete(picker.view);
-          picker.open = false;
-          placeSuggestion();
-          if (!picker.range) return;
-          const tr = picker.view.state.tr
-            .deleteRange(picker.range.from, picker.range.to)
-            .insertText(`Clicked on ${index + 1}`);
-          picker.view.dispatch(tr);
-          picker.view.focus();
-        });
-      })
-    }
-  }, [suggestion])
-
   const state = useEditorState({
     specRegistry,
     plugins: () => [
@@ -185,7 +110,6 @@ export default function Editor() {
       paragraph.plugins(),
       strike.plugins(),
       underline.plugins(),
-      ...autocomplete(options),
       emoji.plugins(),
       emojiSuggest.plugins({
         key: emojiSuggestKey,
@@ -229,11 +153,6 @@ export default function Editor() {
   return <ReactBangleEditor state={state}>
     <FloatingMenu menuKey={menuKey} />
     <EmojiSuggest emojiSuggestKey={emojiSuggestKey} />
-    <div id="suggestion" style={{ display: "none" }}>
-      <div>Suggestion 1</div>
-      <div>Suggestion 2</div>
-      <div>Suggestion 3</div>
-    </div>
   </ReactBangleEditor>
 }
 
