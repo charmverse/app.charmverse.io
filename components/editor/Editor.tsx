3d8cc023007bdb38ebd230d4efa683a99e11e822
Matt Casey <mattwad@gmail.com>
2022-01-25 21:06:06 -0700

    add emoji next to page title (#12)
    * add emoji next to page title
    
    * add box
    
    * fix page title

--- components/editor/Editor.tsx
+++ components/editor/Editor.tsx
@@ -1,9 +1,12 @@
+
+import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import styled from '@emotion/styled';
 import BangleEditor from './BangleEditor';
+import Emoji from 'components/common/Emoji';
 
 const Container = styled.div`
-  width: 900px;
+  width: 800px;
   max-width: 100%;
   margin: 0 auto 5px;
 `;
@@ -11,6 +14,12 @@ const Container = styled.div`
 const Controls = styled.div`
   margin-top: 80px;
   margin-bottom: 4px;
+`;
+
+const PageTitle = styled(Typography)`
+  cursor: text;
+  font-size: 40px;
+  font-weight: 700;
 `;
 
 export function Editor () {
@@ -24,7 +33,10 @@ export function Editor () {
       <Controls>
 
       </Controls>
-      <Typography variant='h1'>{page.title}</Typography>
+      <Box py={3}>
+        <Emoji sx={{ fontSize: 78 }}>📌</Emoji>
+      </Box>
+      <PageTitle>{page.title}</PageTitle>
     </Container>
     <Container>
       <BangleEditor />
