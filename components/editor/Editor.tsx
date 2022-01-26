1b4d07b3b393d113943ada9c265266ee761db516
Matt Casey <mattwad@gmail.com>
2022-01-25 20:59:46 -0700

    fix page title

--- components/editor/Editor.tsx
+++ components/editor/Editor.tsx
@@ -6,7 +6,7 @@ import BangleEditor from './BangleEditor';
 import Emoji from 'components/common/Emoji';
 
 const Container = styled.div`
-  width: 900px;
+  width: 800px;
   max-width: 100%;
   margin: 0 auto 5px;
 `;
@@ -14,6 +14,12 @@ const Container = styled.div`
 const Controls = styled.div`
   margin-top: 80px;
   margin-bottom: 4px;
+`;
+
+const PageTitle = styled(Typography)`
+  cursor: text;
+  font-size: 40px;
+  font-weight: 700;
 `;
 
 export function Editor () {
@@ -30,7 +36,7 @@ export function Editor () {
       <Box py={3}>
         <Emoji sx={{ fontSize: 78 }}>📌</Emoji>
       </Box>
-      <Typography variant='h1'>{page.title}</Typography>
+      <PageTitle>{page.title}</PageTitle>
     </Container>
     <Container>
       <BangleEditor />
