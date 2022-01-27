80f30b732a60e5274bd4909e81dc6a178944155f
Safwan Shaheer <devorein00@gmail.com>
2022-01-27 15:29:04 +0600

    Converted Menu to use styled components

--- components/@bangle.dev/react-menu/Menu.tsx
+++ components/@bangle.dev/react-menu/Menu.tsx
@@ -1,16 +1,25 @@
+import styled from "@emotion/styled";
 import React from 'react';
+import { sidebarBackgroundColor, sidebarBackgroundColorDarkMode } from "theme/colors";
+
+const StyledMenu = styled.div`
+  background-color: ${({ theme }) => theme.palette.mode === "dark" ? sidebarBackgroundColorDarkMode : sidebarBackgroundColor};
+  box-shadow: 0 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1);
+  border-radius: ${({ theme }) => theme.spacing(0.5)};
+  display: flex;
+  padding: ${({ theme }) => theme.spacing(0.75, 0.5)};
+`
 
 export function Menu({
   className = '',
-  children,
-  ...props
+  children
 }: {
   className?: string;
   children: React.ReactNode;
 }) {
   return (
-    <div className={`bangle-menu ${className}`} {...props}>
+    <StyledMenu className={className}>
       {children}
-    </div>
+    </StyledMenu>
   );
 }
