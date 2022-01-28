6af34199645277d207db6ad2c5fb38b6d2fdc8e6
Safwan Shaheer <devorein00@gmail.com>
2022-01-28 17:36:23 +0600

    Added floating menu module to reorganize BangleEditor

--- components/editor/BangleEditor.tsx
+++ components/editor/BangleEditor.tsx
@@ -20,20 +20,18 @@ import { PluginKey, SpecRegistry } from '@bangle.dev/core';
 import '@bangle.dev/core/style.css';
 import { emoji } from '@bangle.dev/emoji';
 import { markdownParser } from '@bangle.dev/markdown';
-import { columnResizing, EditorView, keymap, Node, NodeSelection, ResolvedPos } from '@bangle.dev/pm';
+import { columnResizing, EditorView, keymap } from '@bangle.dev/pm';
 import { BangleEditor as ReactBangleEditor, useEditorState } from '@bangle.dev/react';
 import { EmojiSuggest, emojiSuggest } from '@bangle.dev/react-emoji-suggest';
-import { floatingMenu, FloatingMenu } from '@bangle.dev/react-menu';
 import '@bangle.dev/react-menu/style.css';
 import { table, tableCell, tableHeader, tablePlugins, tableRow } from "@bangle.dev/table";
 import '@bangle.dev/tooltip/style.css';
-import { CustomFloatingMenu } from 'components/editor/CustomFloatingMenu';
+import FloatingMenu, { floatingMenuPlugin } from 'components/editor/FloatingMenu';
 import gemojiData from 'emoji-lookup-data/data/gemoji.json';
 import { paletteMarkName, palettePluginKey } from './@bangle.io/extensions/inline-command-palette/config';
 import { InlineCommandPalette } from "./@bangle.io/extensions/inline-command-palette/InlineCommandPalette";
 import { inlinePalette, queryInlinePaletteActive } from './@bangle.io/js-lib/inline-palette';
 import { keybindings } from './@bangle.io/lib/config';
-const menuKey = new PluginKey('menuKey');
 const emojiSuggestKey = new PluginKey('emojiSuggestKey');
 
 const emojiData = Object.values(
@@ -145,31 +143,7 @@ export default function Editor({ markdown }: { markdown: string }) {
       }),
       tablePlugins(),
       columnResizing,
-      floatingMenu.plugins({
-        key: menuKey,
-        calculateType: (state) => {
-          // if inside a table, first check to see if we are resizing or not
-          const isInsideTable = state.selection.$anchor.parent.type.name.match(/^(table_cell|table_header)$/);
-          if (isInsideTable) {
-            const { path } = (state.selection.$anchor) as ResolvedPos & { path: Node[] };
-            if (path) {
-              for (let index = path.length - 1; index > 0; index--) {
-                const node = path[index];
-                // We are inside a paragraph, then show floating menu
-                if (node.type && node.type.name === "paragraph") {
-                  return "defaultMenu"
-                }
-              }
-              // We are not inside a paragraph, so dont show floating menu
-              return null;
-            }
-          }
-          if (state.selection.empty || (state.selection as NodeSelection)?.node?.type?.name === "image") {
-            return null;
-          }
-          return 'defaultMenu'
-        }
-      }),
+      floatingMenuPlugin(),
       keymap({
         [keybindings.toggleInlineCommandPalette.key]: (
           state,
@@ -201,14 +175,7 @@ export default function Editor({ markdown }: { markdown: string }) {
   });
 
   return <ReactBangleEditor state={state}>
-    <FloatingMenu menuKey={menuKey} renderMenuType={({ type }) => {
-      if (type === 'defaultMenu') {
-        return (
-          <CustomFloatingMenu />
-        );
-      }
-      return null;
-    }} />
+    <FloatingMenu />
     <EmojiSuggest emojiSuggestKey={emojiSuggestKey} />
     <InlineCommandPalette />
   </ReactBangleEditor>
---
+++ components/editor/FloatingMenu.tsx
@@ -0,0 +1,62 @@
+import { PluginKey } from "@bangle.dev/core";
+import { Node, ResolvedPos } from "@bangle.dev/pm";
+import { FloatingMenu as BangleFloatingMenu, floatingMenu } from "@bangle.dev/react-menu";
+import { NodeSelection } from "prosemirror-state";
+import { Menu } from "./@bangle.dev/react-menu/Menu";
+import { BlockquoteButton, BoldButton, CodeButton, HeadingButton, ItalicButton, ParagraphButton } from "./@bangle.dev/react-menu/MenuButtons";
+import { MenuGroup } from "./@bangle.dev/react-menu/MenuGroup";
+const menuKey = new PluginKey('menuKey');
+
+export default function FloatingMenu() {
+  return (
+    <BangleFloatingMenu menuKey={menuKey} renderMenuType={({ type }) => {
+      if (type === 'defaultMenu') {
+        return (
+          <Menu>
+            <MenuGroup>
+              <BoldButton />
+              <ItalicButton />
+              <CodeButton />
+            </MenuGroup>
+            <MenuGroup isLastGroup>
+              <ParagraphButton />
+              <BlockquoteButton />
+              <HeadingButton level={1} />
+              <HeadingButton level={2} />
+              <HeadingButton level={3} />
+            </MenuGroup>
+          </Menu>
+        );
+      }
+      return null;
+    }} />
+  );
+}
+
+export const floatingMenuPlugin = () => {
+  return floatingMenu.plugins({
+    key: menuKey,
+    calculateType: (state) => {
+      // if inside a table, first check to see if we are resizing or not
+      const isInsideTable = state.selection.$anchor.parent.type.name.match(/^(table_cell|table_header)$/);
+      if (isInsideTable) {
+        const { path } = (state.selection.$anchor) as ResolvedPos & { path: Node[] };
+        if (path) {
+          for (let index = path.length - 1; index > 0; index--) {
+            const node = path[index];
+            // We are inside a paragraph, then show floating menu
+            if (node.type && node.type.name === "paragraph") {
+              return "defaultMenu"
+            }
+          }
+          // We are not inside a paragraph, so dont show floating menu
+          return null;
+        }
+      }
+      if (state.selection.empty || (state.selection as NodeSelection)?.node?.type?.name === "image") {
+        return null;
+      }
+      return 'defaultMenu'
+    }
+  })
+}
