5f62cdf84cae5336ed847b0e17d85471de0572dc
Matt Casey <mattwad@gmail.com>
2022-02-01 01:02:02 -0700

    allow editing page titles

--- components/editor/Editor.tsx
+++ components/editor/Editor.tsx
@@ -1,7 +1,8 @@
-
+import { ChangeEvent } from 'react';
 import styled from '@emotion/styled';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
+import Input from '@mui/material/Input';
 import Emoji from 'components/common/Emoji';
 import { Page } from 'models';
 import BangleEditor from './BangleEditor';
@@ -18,20 +19,27 @@ const Controls = styled.div`
   margin-bottom: 4px;
 `;
 
-const PageTitle = styled(Typography)`
+const PageTitle = styled.input`
+  border: 0 none;
   cursor: text;
   font-size: 40px;
   font-weight: 700;
+  outline: none;
 `;
 
-export function Editor ({ page }: { page: Page }) {
+export function Editor ({ page, setPage }: { page: Page, setPage: (p: Page) => void }) {
+
+  function updateTitle (event: ChangeEvent<HTMLInputElement>) {
+    setPage({ ...page, title: event.target.value });
+  }
+
   return (
     <Container>
       <Controls />
       <Box py={3}>
         <Emoji sx={{ fontSize: 78 }}>{page.icon}</Emoji>
       </Box>
-      <PageTitle>{page.title}</PageTitle>
+      <PageTitle placeholder='Untitled' autoFocus value={page.title} onChange={updateTitle} />
       <BangleEditor content={page.content} />
     </Container>
   );
