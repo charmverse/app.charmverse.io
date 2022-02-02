efe2bb22da5a1add7137d11ddf43578e868b578e
Safwan Shaheer <devorein00@gmail.com>
2022-02-02 21:02:25 +0600

    Fixed layout of image align controls

--- components/editor/Image.tsx
+++ components/editor/Image.tsx
@@ -1,4 +1,5 @@
 import { NodeViewProps } from '@bangle.dev/core';
+import { useTheme } from '@emotion/react';
 import styled from '@emotion/styled';
 import AlignHorizontalCenterIcon from '@mui/icons-material/AlignHorizontalCenter';
 import AlignHorizontalLeftIcon from '@mui/icons-material/AlignHorizontalLeft';
@@ -6,55 +7,55 @@ import AlignHorizontalRightIcon from '@mui/icons-material/AlignHorizontalRight';
 import { ListItem } from '@mui/material';
 import { useState } from 'react';
 
-const StyledImage = styled.div<{align?: string}>`
+const StyledImage = styled.div<{ align?: string }>`
   display: flex;
   justify-content: ${props => props.align};
-  &:hover .controls {
-    display: flex;
-  }
 `;
 
 const Controls = styled.div`
-  display: none;
   position: absolute;
+  background: ${({ theme }) => theme.palette.background.light};
+  border-radius: ${({ theme }) => theme.spacing(0.5)};
 `;
 
 export function Image ({ node }: NodeViewProps) {
   const [align, setAlign] = useState('center');
+  const theme = useTheme();
 
   return (
     <StyledImage align={align}>
-      <Controls className='controls'>
-        <ListItem
-          button
-          disableRipple
-          onClick={() => {
-            setAlign('start');
-          }}
-        >
-          <AlignHorizontalLeftIcon fontSize='small' />
-        </ListItem>
-        <ListItem
-          button
-          disableRipple
-          onClick={() => {
-            setAlign('center');
-          }}
-        >
-          <AlignHorizontalCenterIcon fontSize='small' />
-        </ListItem>
-        <ListItem
-          button
-          disableRipple
-          onClick={() => {
-            setAlign('end');
-          }}
-        >
-          <AlignHorizontalRightIcon fontSize='small' />
-        </ListItem>
-      </Controls>
-      { /* eslint-disable-next-line */}
-      <img width={500} contentEditable={false} draggable src={node.attrs.src} alt={node.attrs.alt} />
+      <div className='content'>
+        <Controls>
+          {[
+            [
+              'start', <AlignHorizontalLeftIcon fontSize='small' />
+            ], [
+              'center', <AlignHorizontalCenterIcon fontSize='small' />
+            ], [
+              'end', <AlignHorizontalRightIcon fontSize='small' />
+            ]
+          ].map(([alignLabel, alignIcon]) => (
+            <ListItem
+              key={alignLabel as string}
+              sx={{
+                padding: theme.spacing(1),
+                borderRadius: theme.spacing(0.5),
+                backgroundColor: align === alignLabel ? theme.palette.background.dark : 'inherit'
+              }}
+              button
+              disableRipple
+              onClick={() => {
+                setAlign(alignLabel as string);
+              }}
+            >
+              {alignIcon}
+            </ListItem>
+          ))}
+        </Controls>
+        { /* eslint-disable-next-line */}
+        <img width={500} contentEditable={false} draggable src={node.attrs.src} alt={node.attrs.alt} />
+
+      </div>
     </StyledImage>
   );
 }
