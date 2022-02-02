eb398359dc42547a312cf87937f7bed9593bb27c
Safwan Shaheer <devorein00@gmail.com>
2022-02-02 15:43:09 +0600

    Added button to add icon for page

--- components/editor/Editor.tsx
+++ components/editor/Editor.tsx
@@ -1,10 +1,10 @@
-import { ChangeEvent } from 'react';
 import styled from '@emotion/styled';
+import EmojiEmotionsIcon from '@mui/icons-material/EmojiEmotions';
+import { ListItem, useTheme } from '@mui/material';
 import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import Input from '@mui/material/Input';
 import Emoji from 'components/common/Emoji';
 import { Page } from 'models';
+import { ChangeEvent } from 'react';
 import BangleEditor from './BangleEditor';
 
 const Container = styled.div`
@@ -30,18 +30,42 @@ const PageTitle = styled.input`
 `;
 
 export function Editor ({ page, setPage }: { page: Page, setPage: (p: Page) => void }) {
-
   function updateTitle (event: ChangeEvent<HTMLInputElement>) {
     setPage({ ...page, title: event.target.value });
   }
 
+  const theme = useTheme();
+
   return (
     <Container>
-      <Controls />
+      <Controls>
+        {!page.icon && (
+        <ListItem
+          button
+          disableRipple
+          sx={{
+            borderRadius: theme.spacing(0.5),
+            opacity: 0.5,
+            display: 'flex',
+            gap: 1,
+            padding: `${theme.spacing(0.75)}`,
+            width: 'fit-content'
+          }}
+        >
+          <EmojiEmotionsIcon fontSize='small' />
+          Add Icon
+        </ListItem>
+        )}
+      </Controls>
       <Box py={3}>
         <Emoji sx={{ fontSize: 78 }}>{page.icon}</Emoji>
       </Box>
-      <PageTitle placeholder='Untitled' autoFocus value={page.title} onChange={updateTitle} />
+      <PageTitle
+        placeholder='Untitled'
+        autoFocus
+        value={page.title}
+        onChange={updateTitle}
+      />
       <BangleEditor content={page.content} />
     </Container>
   );
