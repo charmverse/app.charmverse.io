29c9dbfa44106831fcaac28879e209e12fbe8cec
Safwan Shaheer <devorein00@gmail.com>
2022-02-05 19:41:41 +0600

    Button for adding icon and cover for a page (#53)

--- components/common/Emoji.tsx
+++ components/common/Emoji.tsx
@@ -1,12 +1,25 @@
-import { ComponentProps } from 'react';
-import Box from '@mui/material/Box';
 import styled from '@emotion/styled';
+import Box from '@mui/material/Box';
+import { ComponentProps } from 'react';
 
 const Emoji = styled(Box)`
   /* font family taken from Notion */
   font-family: "Apple Color Emoji", "Segoe UI Emoji", NotoColorEmoji, "Noto Color Emoji", "Segoe UI Symbol", "Android Emoji", EmojiSymbols;
-  line-height: 1em;
   white-space: nowrap;
+  user-select: none;
+  cursor: pointer;
+  padding: ${({ theme }) => theme.spacing(1)};
+  border-radius: ${({ theme }) => theme.spacing(0.5)};
+  width: 100px;
+  height: 100px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  &:hover {
+    background-color: ${({ theme }) => theme.palette.background.light}
+  }
 `;
 
 export default function EmojiCon ({ children, ...props }: ComponentProps<typeof Emoji>) {
