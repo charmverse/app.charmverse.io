14983b9d8fd720eec30c980ff0b988856d45be10
Tung Vo <tung.vo@fmgsuite.com>
2022-02-08 11:47:06 +0700

    Add some placeholder for BountyCard and FloatButton

---
+++ components/bounty/BountyCard.tsx
@@ -0,0 +1,14 @@
+import Box from '@mui/material/Box';
+
+export default function BountyCard () {
+  return (
+    <Box
+      sx={{
+        width: 290,
+        height: 228,
+        borderRadius: 10,
+        backgroundColor: 'red'
+      }}
+    />
+  );
+}
--- pages/[domain]/bounty.tsx
+++ pages/[domain]/bounty.tsx
@@ -1,14 +1,16 @@
 import { PageLayout } from 'components/common/page-layout';
-import { usePageTitle } from 'hooks/usePageTitle';
-import { DatabaseEditor } from 'components/databases';
-import { Editor } from 'components/editor';
-import { usePages } from 'hooks/usePages';
-import { Page } from 'models';
-import { useRouter } from 'next/router';
-import { ReactElement, useEffect } from 'react';
+import BountyCard from 'components/bounty/BountyCard';
+import FloatButton from 'components/bounty/FloatButton';
+
+import { ReactElement } from 'react';
 
 export default function BountyPage () {
-  return <div>TODO: put the bounty cards here</div>;
+  return (
+    <div position='relative'>
+      <BountyCard />
+      <FloatButton />
+    </div>
+  );
 }
 
 BountyPage.getLayout = (page: ReactElement) => {
