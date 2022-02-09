9a75cd6a6bed2c38326c4f1f63c8a60c4cd4adf5
Matt Casey <mattwad@gmail.com>
2022-02-09 07:51:57 -0700

    allow not logging in for now

--- components/settings/WalletConnection.tsx
+++ components/settings/WalletConnection.tsx
@@ -4,6 +4,7 @@ import Button from 'components/common/Button';
 import Typography from '@mui/material/Typography';
 import { injected, walletConnect, walletLink } from 'connectors';
 import { Web3Connection } from 'components/_app/Web3ConnectionManager';
+import PrimaryButton from 'components/common/PrimaryButton';
 import { useWeb3React } from '@web3-react/core';
 import CopyableAddress from './CopyableAddress';
 
@@ -11,7 +12,7 @@ export default function WalletConnection () {
 
   const { account, connector } = useWeb3React();
 
-  const { openWalletSelectorModal } = useContext(Web3Connection);
+  const { openWalletSelectorModal, triedEager } = useContext(Web3Connection);
 
   const handleWalletProviderSwitch = () => {
     openWalletSelectorModal();
@@ -31,25 +32,31 @@ export default function WalletConnection () {
   };
 
   return (
-    <Box display='flex' justifyContent='space-between' alignItems='center' my={3}>
-      {account ? (
-        <CopyableAddress
-          address={account}
-          decimals={5}
-          sx={{ fontSize: 20, background: 'transparent !important', color: 'inherit', px: 0 }}
-        />
-      ) : <div></div>}
-      <Box
-        display='flex'
-        alignItems='center'
-      >
-        <Typography color='secondary' variant='body2'>
-          {`Connected with ${connectorName(connector)}`}
-        </Typography>
-        <Button size='small' color='secondary' variant='outlined' onClick={handleWalletProviderSwitch} sx={{ ml: 3 }}>
-          Switch
-        </Button>
+    account ? (
+      <Box display='flex' justifyContent='space-between' alignItems='center' my={3}>
+        {account ? (
+          <CopyableAddress
+            address={account}
+            decimals={5}
+            sx={{ fontSize: 20, background: 'transparent !important', color: 'inherit', px: 0 }}
+          />
+        ) : <div></div>}
+        <Box
+          display='flex'
+          alignItems='center'
+        >
+          <Typography color='secondary' variant='body2'>
+            {`Connected with ${connectorName(connector)}`}
+          </Typography>
+          <Button size='small' color='secondary' variant='outlined' onClick={handleWalletProviderSwitch} sx={{ ml: 3 }}>
+            Switch
+          </Button>
+        </Box>
       </Box>
-    </Box>
+    ) : (
+      <PrimaryButton size='large' loading={!triedEager} onClick={openWalletSelectorModal}>
+        Connect Wallet
+      </PrimaryButton>
+    )
   );
 }
