c2f2d2d7c1068b99c2506fb384cadddf5495b434
Tung Vo <tung.vo@fmgsuite.com>
2022-02-10 16:45:07 +0700

    Improve Styling for suggestion header

--- pages/[domain]/bounty.tsx
+++ pages/[domain]/bounty.tsx
@@ -3,7 +3,10 @@ import BountyCard from 'components/bounty/BountyCard';
 import BountyTable from 'components/bounty/BountyTable';
 import BountyEditorModal from 'components/bounty/BountyEditorModal';
 import { BountyProvider, useBounty } from 'components/bounty/hooks/useBounty';
-import { SuggestionProvider, useBountySuggestion } from 'components/bounty/hooks/useBountySuggestion';
+import {
+  SuggestionProvider,
+  useBountySuggestion
+} from 'components/bounty/hooks/useBountySuggestion';
 
 import Grid from '@mui/material/Grid';
 import Button from '@mui/material/Button';
@@ -31,11 +34,32 @@ function SuggestionContainer (): ReactElement {
   const [bountyDialogOpen, setBountyDialogOpen] = useState(false);
   return (
     <>
-      <Box sx={{ display: 'flex', flexDirection: 'row' }}>
-        <Typography>Suggestions</Typography>
-        <Button onClick={() => {
-          setBountyDialogOpen(true);
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          padding: '8px 16px',
+          borderBottom: '1px solid',
+          borderColor: 'divider',
+          margin: '8px 0',
+          alignItems: 'center'
         }}
+      >
+        {/* // xtungvo TODO: update correct variant style */}
+        <Typography
+          sx={{
+            fontSize: '30px',
+            color: '#6A6A6A',
+            fontWeight: 'bold'
+          }}
+        >
+          Suggestions
+        </Typography>
+        <Button
+          onClick={() => {
+            setBountyDialogOpen(true);
+          }}
         >
           Suggest (+)
         </Button>
@@ -47,7 +71,8 @@ function SuggestionContainer (): ReactElement {
           setBountyDialogOpen(false);
         }}
         onSubmit={(bounty) => {
-          addBounty(bounty); setBountyDialogOpen(false);
+          addBounty(bounty);
+          setBountyDialogOpen(false);
         }}
       />
     </>
@@ -56,6 +81,16 @@ function SuggestionContainer (): ReactElement {
 export default function BountyPage () {
   return (
     <>
+      <Typography
+        sx={{
+          fontSize: '30px',
+          color: '#6A6A6A',
+          fontWeight: 'bold',
+          marginLeft: '8px'
+        }}
+      >
+        Bounty Panel
+      </Typography>
       <BountyProvider>
         <BountyContainer />
       </BountyProvider>
