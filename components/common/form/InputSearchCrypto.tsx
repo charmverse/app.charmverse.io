e3ebaebc112290b8bf4059bd9144f4e6c26893dc
melboudi <mo@anyahq.co>
2022-02-10 16:37:15 +0200

    Setup crypto input search

---
+++ components/common/form/InputSearchCrypto.tsx
@@ -0,0 +1,62 @@
+import { Autocomplete, Box, TextField } from '@mui/material';
+import Image from 'next/image';
+import { CryptoCurrencyList, CryptoCurrency } from '../../../models/Currency';
+
+const currencyOptions = Object.keys(CryptoCurrencyList);
+
+/**
+ * https://www.cryptofonts.com/icons.php
+ */
+const cryptoLogoPaths: Record<CryptoCurrency, string> = {
+  ETH: '/cryptoLogos/ethereum-eth-logo.svg',
+  BTC: '/cryptoLogos/bitcoin-btc-logo.svg',
+  BCH: '/cryptoLogos/bitcoin-cash-bch-logo.svg',
+  ETC: '/cryptoLogos/ethereum-classic-etc-logo.svg',
+  LTC: '/cryptoLogos/litecoin-ltc-logo.svg',
+  XRP: '/cryptoLogos/ripple-xrp-logo.svg'
+};
+
+export function InputSearchCrypto ({ onChange }: {onChange: (value: CryptoCurrency) => any}) {
+
+  function emitValue (value: string) {
+    if (value !== null && currencyOptions.indexOf(value) >= 0) {
+      onChange(value as CryptoCurrency);
+    }
+  }
+
+  return (
+    <Autocomplete
+      onChange={(event, value) => {
+        emitValue(value as any);
+      }}
+      id='currency'
+      sx={{ minWidth: 150 }}
+      options={currencyOptions}
+      autoHighlight
+      renderOption={(props, option) => (
+        <Box component='li' sx={{ '& > img': { mr: 2, flexShrink: 0 } }} {...props}>
+          <Image
+            loading='lazy'
+            width='20px'
+            height='20px'
+            src={cryptoLogoPaths[option as CryptoCurrency]}
+          />
+          {'    '}
+          {option}
+          {' '}
+          {CryptoCurrencyList[option as CryptoCurrency]}
+        </Box>
+      )}
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          label='Choose a crypto'
+          inputProps={{
+            ...params.inputProps
+          }}
+        />
+      )}
+    />
+  );
+}
+
