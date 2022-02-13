efe60ff2f1deca270c9580e600efd5a0ed3ac44f
Safwan Shaheer <devorein00@gmail.com>
2022-02-13 19:27:17 +0600

    Moved ResizeHandle to separate component

--- components/editor/Image.tsx
+++ components/editor/Image.tsx
@@ -10,7 +10,7 @@ import { Box, ListItem, Typography } from '@mui/material';
 import useResize from 'hooks/useResize';
 import React, { HTMLAttributes, useState } from 'react';
 import ImageSelector from './ImageSelector';
-import { StyledResizeHandle } from './ResizeHandle';
+import { ResizeHandle } from './ResizeHandle';
 
 const MAX_IMAGE_WIDTH = 750; const
   MIN_IMAGE_WIDTH = 250;
@@ -79,86 +79,6 @@ const StyledImage = styled.img`
   border-radius: ${({ theme }) => theme.spacing(1)};
 `;
 
-interface ImageResizeHandleProps {
-  width: number
-  setWidth: React.Dispatch<React.SetStateAction<number>>
-  setClientX: React.Dispatch<React.SetStateAction<number>>
-  clientX: number,
-  position: 'right' | 'left'
-  isDragging: boolean
-  setIsDragging: React.Dispatch<React.SetStateAction<boolean>>
-}
-
-function ImageResizeHandle (
-  { isDragging, setIsDragging, position, width, setClientX, setWidth, clientX }: ImageResizeHandleProps
-) {
-  return (
-    <>
-      {/** Adding StyledResizeHandle twice to hide image ghost while dragging  */}
-      <StyledResizeHandle
-        pos={position}
-        /** custom class required to show resize handler when hovering over the image */
-        className='image-resize-handler'
-      />
-      <StyledResizeHandle
-        pos={position}
-        onDragEnd={() => {
-          if (isDragging) {
-            setIsDragging(false);
-          }
-        }}
-        onDrag={(e) => {
-          if (!isDragging) {
-            setIsDragging(true);
-          }
-          // Make sure the image is not below 250px, and above 750px
-          if (width >= MIN_IMAGE_WIDTH && width <= MAX_IMAGE_WIDTH) {
-            let difference = clientX - e.clientX;
-            // Making sure the difference isn't too abrupt clipping the difference to a limit
-            difference = difference < -5 ? -5 : difference > 5 ? 5 : difference;
-            // Avoid updating state when the clientX value are the same
-            // This happens when the user is dragging but staying still
-            if (clientX !== e.clientX) {
-              let newImageWidth = width;
-              if (position === 'right') {
-                // left to right movement. Increasing image size
-                if (difference < 0) {
-                  newImageWidth -= difference;
-                }
-                // Right to left movement. Decreasing image size
-                else {
-                  newImageWidth -= difference;
-                }
-              }
-              else if (position === 'left') {
-                // right to left movement. Increasing image size
-                if (difference > 0) {
-                  newImageWidth += difference;
-                }
-                // Left to right movement. Decreasing image size
-                else {
-                  newImageWidth += difference;
-                }
-              }
-              // Making sure that the image width is within a certain range
-              if (newImageWidth < MIN_IMAGE_WIDTH) {
-                newImageWidth = MIN_IMAGE_WIDTH;
-              }
-              else if (newImageWidth > MAX_IMAGE_WIDTH) {
-                newImageWidth = MAX_IMAGE_WIDTH;
-              }
-              setWidth(newImageWidth);
-
-              setClientX(e.clientX);
-            }
-          }
-        }}
-        draggable
-      />
-    </>
-  );
-}
-
 export function Image ({ node, updateAttrs }: NodeViewProps) {
   const [align, setAlign] = useState('center');
   const theme = useTheme();
@@ -193,19 +113,19 @@ export function Image ({ node, updateAttrs }: NodeViewProps) {
             position: 'relative',
             cursor: 'col-resize',
             width: resizeState.width,
-            '&:hover .image-resize-handler': {
+            '&:hover .resize-handler': {
               opacity: resizeState.isDragging ? 0 : 1,
               transition: 'opacity 250ms ease-in-out'
             }
           }}
         >
-          <ImageResizeHandle {...resizeState} position='left' />
+          <ResizeHandle maxWidth={MAX_IMAGE_WIDTH} minWidth={MIN_IMAGE_WIDTH} {...resizeState} position='left' />
           <StyledImage
             draggable={false}
             src={node.attrs.src}
             alt={node.attrs.alt}
           />
-          <ImageResizeHandle {...resizeState} position='right' />
+          <ResizeHandle maxWidth={MAX_IMAGE_WIDTH} minWidth={MIN_IMAGE_WIDTH} {...resizeState} position='right' />
         </Box>
         <Controls className='controls'>
           {[
--- components/editor/ResizeHandle.tsx
+++ components/editor/ResizeHandle.tsx
@@ -1,5 +1,6 @@
 import styled from '@emotion/styled';
 import { Box } from '@mui/system';
+import React from 'react';
 
 export const StyledResizeHandle = styled(Box)<{pos: 'right' | 'left'}>`
   width: 7.5px;
@@ -15,3 +16,85 @@ export const StyledResizeHandle = styled(Box)<{pos: 'right' | 'left'}>`
   ${({ pos }) => pos === 'left' ? 'left: 15px' : 'right: 15px'};
   cursor: col-resize;
 `;
+
+interface ResizeHandleProps {
+  width: number
+  setWidth: React.Dispatch<React.SetStateAction<number>>
+  setClientX: React.Dispatch<React.SetStateAction<number>>
+  clientX: number,
+  position: 'right' | 'left'
+  isDragging: boolean
+  setIsDragging: React.Dispatch<React.SetStateAction<boolean>>
+  minWidth: number
+  maxWidth: number
+}
+
+export function ResizeHandle (
+  { minWidth, maxWidth, isDragging, setIsDragging, position, width, setClientX, setWidth, clientX }: ResizeHandleProps
+) {
+  return (
+    <>
+      {/** Adding StyledResizeHandle twice to hide image ghost while dragging  */}
+      <StyledResizeHandle
+        pos={position}
+        /** custom class required to show resize handler when hovering over the image */
+        className='resize-handler'
+      />
+      <StyledResizeHandle
+        pos={position}
+        onDragEnd={() => {
+          if (isDragging) {
+            setIsDragging(false);
+          }
+        }}
+        onDrag={(e) => {
+          if (!isDragging) {
+            setIsDragging(true);
+          }
+          // Make sure the width is within certain limits
+          if (width >= minWidth && width <= maxWidth) {
+            let difference = clientX - e.clientX;
+            // Making sure the difference isn't too abrupt clipping the difference to a limit
+            difference = difference < -5 ? -5 : difference > 5 ? 5 : difference;
+            // Avoid updating state when the clientX value are the same
+            // This happens when the user is dragging but staying still
+            if (clientX !== e.clientX) {
+              let newWidth = width;
+              if (position === 'right') {
+                // left to right movement. Increasing width
+                if (difference < 0) {
+                  newWidth -= difference;
+                }
+                // Right to left movement. Decreasing width
+                else {
+                  newWidth -= difference;
+                }
+              }
+              else if (position === 'left') {
+                // right to left movement. Increasing width
+                if (difference > 0) {
+                  newWidth += difference;
+                }
+                // Left to right movement. Decreasing width
+                else {
+                  newWidth += difference;
+                }
+              }
+              // Making sure that the width is within a certain range
+              if (newWidth < minWidth) {
+                newWidth = minWidth;
+              }
+              else if (newWidth > maxWidth) {
+                newWidth = maxWidth;
+              }
+              setWidth(newWidth);
+
+              setClientX(e.clientX);
+            }
+          }
+        }}
+        draggable
+      />
+    </>
+  );
+}
