d625add1e7cc503d7555d8221739b19ff8b5e01d
Safwan Shaheer <devorein00@gmail.com>
2022-02-14 18:53:53 +0600

    Integrated BlockAligner with Image

--- components/editor/Image.tsx
+++ components/editor/Image.tsx
@@ -1,40 +1,16 @@
 import { NodeViewProps } from '@bangle.dev/core';
 import { useTheme } from '@emotion/react';
 import styled from '@emotion/styled';
-import AlignHorizontalCenterIcon from '@mui/icons-material/AlignHorizontalCenter';
-import AlignHorizontalLeftIcon from '@mui/icons-material/AlignHorizontalLeft';
-import AlignHorizontalRightIcon from '@mui/icons-material/AlignHorizontalRight';
-import DeleteIcon from '@mui/icons-material/Delete';
 import ImageIcon from '@mui/icons-material/Image';
 import { Box, ListItem, Typography } from '@mui/material';
 import useResize from 'hooks/useResize';
-import React, { HTMLAttributes, useState } from 'react';
+import React, { HTMLAttributes } from 'react';
+import BlockAligner from './BlockAligner';
 import ImageSelector from './ImageSelector';
 import { ResizeHandle } from './ResizeHandle';
 
 const MAX_IMAGE_WIDTH = 750; const
   MIN_IMAGE_WIDTH = 250;
-
-const StyledImageContainer = styled.div<{ align?: string }>`
-  display: flex;
-  justify-content: ${props => props.align};
-  &:hover .controls {
-    opacity: 1;
-    transition: opacity 250ms ease-in-out;
-  }
-`;
-
-const Controls = styled.div`
-  position: absolute;
-  background: ${({ theme }) => theme.palette.background.light};
-  border-radius: ${({ theme }) => theme.spacing(0.5)};
-  padding: ${({ theme }) => theme.spacing(0.5)};
-  display: flex;
-  right: 0;
-  top: 0;
-  opacity: 0;
-  transition: opacity 250ms ease-in-out;
-`;
 
 const StyledEmptyImageContainer = styled(Box)`
   display: flex;
@@ -81,8 +57,6 @@ const StyledImage = styled.img`
 
 export function Image ({ node, updateAttrs }: NodeViewProps) {
   const theme = useTheme();
-  const [align, setAlign] = useState('center');
-
   const resizeState = useResize({ initialWidth: 500 });
 
   // If there are no source for the node, return the image select component
@@ -100,85 +74,31 @@ export function Image ({ node, updateAttrs }: NodeViewProps) {
   }
 
   return (
-    <StyledImageContainer
-      align={align}
+    <BlockAligner onDelete={() => {
+      updateAttrs({
+        src: null
+      });
+    }}
     >
-      <div className='content' style={{ position: 'relative' }}>
-        { /* eslint-disable-next-line */}
-        <Box
-          sx={{
-            position: 'relative',
-            cursor: 'col-resize',
-            width: resizeState.width,
-            '&:hover .resize-handler': {
-              opacity: resizeState.isDragging ? 0 : 1,
-              transition: 'opacity 250ms ease-in-out'
-            }
-          }}
-        >
-          <ResizeHandle maxWidth={MAX_IMAGE_WIDTH} minWidth={MIN_IMAGE_WIDTH} {...resizeState} position='left' />
-          <StyledImage
-            draggable={false}
-            src={node.attrs.src}
-            alt={node.attrs.alt}
-          />
-          <ResizeHandle maxWidth={MAX_IMAGE_WIDTH} minWidth={MIN_IMAGE_WIDTH} {...resizeState} position='right' />
-        </Box>
-        <Controls className='controls'>
-          {[
-            [
-              'start', <AlignHorizontalLeftIcon sx={{
-                fontSize: 14
-              }}
-              />
-            ], [
-              'center', <AlignHorizontalCenterIcon sx={{
-                fontSize: 14
-              }}
-              />
-            ], [
-              'end', <AlignHorizontalRightIcon
-                sx={{
-                  fontSize: 14
-                }}
-              />
-            ]
-          ].map(([alignLabel, alignIcon]) => (
-            <ListItem
-              key={alignLabel as string}
-              sx={{
-                padding: theme.spacing(1),
-                backgroundColor: align === alignLabel ? theme.palette.background.dark : 'inherit'
-              }}
-              button
-              disableRipple
-              onClick={() => {
-                setAlign(alignLabel as string);
-              }}
-            >
-              {alignIcon}
-            </ListItem>
-          ))}
-          <ListItem
-            button
-            disableRipple
-            onClick={() => {
-              updateAttrs({
-                src: null
-              });
-            }}
-            sx={{
-              padding: theme.spacing(1),
-              backgroundColor: 'inherit'
-            }}
-          >
-            <DeleteIcon sx={{
-              fontSize: 14
-            }}
-            />
-          </ListItem>
-        </Controls>
-      </div>
-    </StyledImageContainer>
+      <Box
+        sx={{
+          position: 'relative',
+          cursor: 'col-resize',
+          width: resizeState.width,
+          '&:hover .resize-handler': {
+            opacity: resizeState.isDragging ? 0 : 1,
+            transition: 'opacity 250ms ease-in-out'
+          }
+        }}
+      >
+        <ResizeHandle maxWidth={MAX_IMAGE_WIDTH} minWidth={MIN_IMAGE_WIDTH} {...resizeState} position='left' />
+        <StyledImage
+          draggable={false}
+          src={node.attrs.src}
+          alt={node.attrs.alt}
+        />
+        <ResizeHandle maxWidth={MAX_IMAGE_WIDTH} minWidth={MIN_IMAGE_WIDTH} {...resizeState} position='right' />
+      </Box>
+    </BlockAligner>
   );
 }
