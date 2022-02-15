225e8252ff34520b24171819a6d38dfbb798b2db
melboudi <mo@anyahq.co>
2022-02-15 15:20:09 +0200

    Update bounty statuses

--- models/Bounty.ts
+++ models/Bounty.ts
@@ -1,7 +1,15 @@
 import { CryptoCurrency } from './Currency';
 
-export const BOUNTY_STATUSES = ['pending', 'in-progress', 'done'] as const;
-export type BountyStatus = typeof BOUNTY_STATUSES[number];
+const BOUNTY_LABELS = {
+  open: 'Open',
+  assigned: 'Assigned',
+  review: 'Review',
+  complete: 'Complete'
+};
+
+export type BountyStatus = keyof typeof BOUNTY_LABELS;
+
+export const BOUNTY_STATUSES = Object.keys(BOUNTY_LABELS) as BountyStatus [];
 
 export interface Bounty {
   id: string;
