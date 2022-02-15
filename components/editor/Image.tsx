9b73550f936641bbb5002ef31b49315fc9eb1995
Safwan Shaheer <devorein00@gmail.com>
2022-02-15 20:22:25 +0600

    Updated Resizer component to use react-resizable

--- components/editor/Image.tsx
+++ components/editor/Image.tsx
@@ -3,13 +3,13 @@ import { useTheme } from '@emotion/react';
 import styled from '@emotion/styled';
 import ImageIcon from '@mui/icons-material/Image';
 import { Box, ListItem, Typography } from '@mui/material';
-import React, { HTMLAttributes, useState } from 'react';
-import { ResizableBox } from 'react-resizable';
+import React, { HTMLAttributes } from 'react';
 import BlockAligner from './BlockAligner';
 import ImageSelector from './ImageSelector';
+import Resizer from './Resizer';
 
-const MAX_IMAGE_WIDTH = 750; const
-  MIN_IMAGE_WIDTH = 250;
+const MAX_IMAGE_SIZE = 750; const
+  MIN_IMAGE_SIZE = 250;
 
 const StyledEmptyImageContainer = styled(Box)`
   display: flex;
@@ -56,31 +56,7 @@ const StyledImage = styled.img`
   box-shadow: ${({ theme }) => theme.shadows[3]}
 `;
 
-export const StyledResizeHandle = styled(Box)`
-  width: 7.5px;
-  height: calc(100% - 15px);
-  max-height: 75px;
-  border-radius: ${({ theme }) => theme.spacing(2)};
-  background-color: ${({ theme }) => theme.palette.background.dark};
-  opacity: 0;
-  position: absolute;
-  top: 50%;
-  transform: translateY(-50%);
-  transition: opacity 250ms ease-in-out;
-  cursor: col-resize;
-
-  &.react-resizable-handle-w {
-    left: 15px;
-  }
-
-  &.react-resizable-handle-e {
-    right: 15px;
-  }
-`;
-
 export function Image ({ node, updateAttrs }: NodeViewProps) {
-  const [dimensions, setDimensions] = useState({ width: 200, height: 200 });
-
   // If there are no source for the node, return the image select component
   if (!node.attrs.src) {
     return (
@@ -102,32 +78,13 @@ export function Image ({ node, updateAttrs }: NodeViewProps) {
       });
     }}
     >
-      <Box sx={{
-        '&:hover .react-resizable-handle': {
-          opacity: 1,
-          transition: 'opacity 250ms ease-in-out'
-        }
-      }}
-      >
-        <ResizableBox
-          resizeHandles={['w', 'e']}
-          lockAspectRatio
-          {...dimensions}
-          onResize={(_, { size }) => {
-            setDimensions(size);
-          }}
-          minConstraints={[MIN_IMAGE_WIDTH, MIN_IMAGE_WIDTH]}
-          maxConstraints={[MAX_IMAGE_WIDTH, MAX_IMAGE_WIDTH]}
-          /* eslint-disable-next-line */
-          handle={(handleAxis: string, ref: React.Ref<unknown>) => <StyledResizeHandle ref={ref} className={`react-resizable-handle react-resizable-handle-${handleAxis}`} />}
-        >
-          <StyledImage
-            draggable={false}
-            src={node.attrs.src}
-            alt={node.attrs.alt}
-          />
-        </ResizableBox>
-      </Box>
+      <Resizer initialSize={200} maxSize={MAX_IMAGE_SIZE} minSize={MIN_IMAGE_SIZE}>
+        <StyledImage
+          draggable={false}
+          src={node.attrs.src}
+          alt={node.attrs.alt}
+        />
+      </Resizer>
     </BlockAligner>
   );
 }
--- components/editor/Resizer.tsx
+++ components/editor/Resizer.tsx
@@ -1,33 +1,61 @@
+import styled from '@emotion/styled';
 import { Box } from '@mui/material';
-import useResize from 'hooks/useResize';
 import { ReactNode } from 'react';
-import { ResizeHandle } from './ResizeHandle';
+import { ResizableBox } from 'react-resizable';
 
 interface ResizerProps {
-  maxWidth: number
-  minWidth: number
+  maxSize: number
+  initialSize: number
+  minSize: number
   children: ReactNode
 }
 
+export const StyledResizeHandle = styled(Box)`
+  width: 7.5px;
+  height: calc(100% - 15px);
+  max-height: 75px;
+  border-radius: ${({ theme }) => theme.spacing(2)};
+  background-color: ${({ theme }) => theme.palette.background.dark};
+  opacity: 0;
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  transition: opacity 250ms ease-in-out;
+  cursor: col-resize;
+
+  &.react-resizable-handle-w {
+    left: 15px;
+  }
+
+  &.react-resizable-handle-e {
+    right: 15px;
+  }
+`;
+
 export default function Resizer (props: ResizerProps) {
-  const resizeState = useResize({ initialWidth: 500 });
-  const { maxWidth, minWidth, children } = props;
+  const { initialSize, minSize, maxSize, children } = props;
 
   return (
     <Box
       sx={{
-        position: 'relative',
-        cursor: 'col-resize',
-        width: resizeState.width,
-        '&:hover .resize-handler': {
-          opacity: resizeState.isDragging ? 0 : 1,
+        '&:hover .react-resizable-handle': {
+          opacity: 1,
           transition: 'opacity 250ms ease-in-out'
         }
       }}
     >
-      <ResizeHandle maxWidth={maxWidth} minWidth={minWidth} {...resizeState} position='left' />
-      {children}
-      <ResizeHandle maxWidth={maxWidth} minWidth={minWidth} {...resizeState} position='right' />
+      <ResizableBox
+        width={initialSize}
+        height={initialSize}
+        resizeHandles={['w', 'e']}
+        lockAspectRatio
+        minConstraints={[minSize, minSize]}
+        maxConstraints={[maxSize, maxSize]}
+        /* eslint-disable-next-line */
+        handle={(handleAxis: string, ref: React.Ref<unknown>) => <StyledResizeHandle ref={ref} className={`react-resizable-handle react-resizable-handle-${handleAxis}`} />}
+      >
+        {children}
+      </ResizableBox>
     </Box>
   );
 }
