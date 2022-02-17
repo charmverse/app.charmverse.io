94e82538cc93aa709c5f1c0f210c5429c87e2a3e
Matt Casey <mattwad@gmail.com>
2022-02-17 15:57:04 -0700

    clean up signup page

--- components/signup/SignupForm.tsx
+++ components/signup/SignupForm.tsx
@@ -2,6 +2,7 @@ import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Image from 'components/common/Image';
 import Typography from '@mui/material/Typography';
+import Card from '@mui/material/Card';
 import styled from '@emotion/styled';
 import PrimaryButton from 'components/common/PrimaryButton';
 import { useWeb3React } from '@web3-react/core';
@@ -17,6 +18,19 @@ export const Content = styled(Box)`
   max-width: 100%;
   width: 1170px;
   margin: 0 auto;
+`;
+
+const AdventureCard = styled(Card)`
+  cursor: pointer;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-end;
+  height: 100%;
+  padding: ${({ theme }) => theme.spacing(6)};
+  &:hover {
+    box-shadow: ${({ theme }) => theme.shadows[15]};
+  }
 `;
 
 export default function SignupPageContent () {
@@ -41,18 +55,22 @@ export default function SignupPageContent () {
       <Typography gutterBottom variant='h2' align='center'>
         Choose your adventure:
       </Typography>
-      <Grid container sx={{ mt: 3 }}>
-        <Grid item xs container alignItems='center' justifyContent='flex-end' flexDirection='column'>
-          <Image mb={3} src={rocketImage} />
-          <PrimaryButton size='large' onClick={createAccount}>
-            Create a new workspace
-          </PrimaryButton>
+      <Grid container sx={{ mt: 10 }} columnSpacing={6}>
+        <Grid item xs>
+          <AdventureCard onClick={createAccount}>
+            <Image mb={3} src={rocketImage} />
+            <PrimaryButton size='large'>
+              Create a new workspace
+            </PrimaryButton>
+          </AdventureCard>
         </Grid>
-        <Grid item xs container alignItems='center' justifyContent='flex-end' flexDirection='column'>
-          <Image mb={3} src={gatesImage} />
-          <PrimaryButton size='large'>
-            Join an existing workspace
-          </PrimaryButton>
+        <Grid item xs>
+          <AdventureCard>
+            <Image mb={3} src={gatesImage} />
+            <PrimaryButton size='large'>
+              Join an existing workspace
+            </PrimaryButton>
+          </AdventureCard>
         </Grid>
       </Grid>
     </Content>
