f23d4d91aa25f8ea3995b26e9c1258d80dda2fc7
melboudi <mo@anyahq.co>
2022-02-21 23:39:24 +0200

    Delete unused imports

--- pages/api/bounties/index.ts
+++ pages/api/bounties/index.ts
@@ -1,11 +1,10 @@
 
-import { NextApiRequest, NextApiResponse } from 'next';
-import nc from 'next-connect';
-import { Bounty, Prisma, Space } from '@prisma/client';
+import { Bounty, Prisma } from '@prisma/client';
 import { prisma } from 'db';
 import { onError, onNoMatch, requireUser } from 'lib/middleware';
 import { withSessionRoute } from 'lib/session/withSession';
-import { gettingStartedPageContent } from 'seedData';
+import { NextApiRequest, NextApiResponse } from 'next';
+import nc from 'next-connect';
 
 const handler = nc<NextApiRequest, NextApiResponse>({ onError, onNoMatch });
 
