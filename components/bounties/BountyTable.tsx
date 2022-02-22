c4132c4ae8ed79ccb68d24df89d4e5b9f91f00df
melboudi <mo@anyahq.co>
2022-02-22 13:41:44 +0200

    Fix build-breaking import in table

--- components/bounties/BountyTable.tsx
+++ components/bounties/BountyTable.tsx
@@ -5,7 +5,7 @@ import TableCell from '@mui/material/TableCell';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 
-import type { Bounty } from 'models/Bounty';
+import { Bounty } from '@prisma/client';
 
 interface Props {
   items: Bounty[];
