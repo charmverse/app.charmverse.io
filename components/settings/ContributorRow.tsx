d8b097f32503231591c68d53694e8e22417ab87a
Matt Casey <mattwad@gmail.com>
2022-02-22 01:37:47 -0700

    update web3 identity

--- components/settings/ContributorRow.tsx
+++ components/settings/ContributorRow.tsx
@@ -5,6 +5,7 @@ import Box from '@mui/material/Box';
 import Avatar from 'components/common/Avatar';
 import { Contributor } from 'models';
 import getDisplayName from 'lib/users/getDisplayName';
+import useENSName from 'hooks/useENSName';
 
 const StyledRow = styled(Box)`
   border-bottom: 1px solid #ddd;
@@ -13,15 +14,17 @@ const StyledRow = styled(Box)`
   justify-content: space-between;
 `;
 
-type Props = { contributor: Contributor, spaceId: string };
+type Props = { contributor: Contributor };
 
-export default function ContributorRow ({ contributor, spaceId }: Props) {
+export default function ContributorRow ({ contributor }: Props) {
+  const ensName = useENSName(contributor.addresses[0]);
   return (
     <StyledRow pb={2} mb={2}>
       <Box display='flex' alignItems='center'>
-        <Avatar name={getDisplayName(contributor)} />
+        <Avatar name={ensName || getDisplayName(contributor)} />
         <Box pl={2}>
-          <Typography variant='body1'><strong>{getDisplayName(contributor)}</strong></Typography>
+          <Typography variant='body1'><strong>{ensName || getDisplayName(contributor)}</strong></Typography>
+          {ensName && <Typography variant='body2'>{getDisplayName(contributor)}</Typography>}
         </Box>
       </Box>
       <Typography color='secondary' variant='body2' sx={{ px: 3 }}>
