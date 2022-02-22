bb3b920dc95cc8710c2f54ec7da00d44f716717f
Matt Casey <mattwad@gmail.com>
2022-02-21 20:14:47 -0700

    fix migration

--- prisma/migrations/20220222031344_invite_links/migration.sql
+++ prisma/migrations/20220222031344_invite_links/migration.sql
@@ -1,6 +1,3 @@
--- CreateEnum
-CREATE TYPE "BountyStatus" AS ENUM ('open', 'assigned', 'review', 'complete', 'paid');
-
 -- CreateTable
 CREATE TABLE "InviteLink" (
     "id" UUID NOT NULL,
@@ -13,26 +10,6 @@ CREATE TABLE "InviteLink" (
     "useCount" INTEGER NOT NULL DEFAULT 0,
 
     CONSTRAINT "InviteLink_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "Bounty" (
-    "id" UUID NOT NULL,
-    "author" UUID NOT NULL,
-    "workspaceId" UUID NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "description" TEXT NOT NULL,
-    "descriptionNodes" JSONB NOT NULL,
-    "reviewer" UUID NOT NULL,
-    "assignee" UUID NOT NULL,
-    "rewardAmount" DOUBLE PRECISION NOT NULL,
-    "rewardToken" TEXT NOT NULL,
-    "status" "BountyStatus" NOT NULL,
-    "title" TEXT NOT NULL,
-    "linkedTaskId" UUID NOT NULL,
-
-    CONSTRAINT "Bounty_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
