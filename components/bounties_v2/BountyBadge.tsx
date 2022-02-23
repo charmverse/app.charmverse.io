0d6d2bad63710bc0b41f796716a8967d878c254c
Safwan Shaheer <devorein00@gmail.com>
2022-02-23 18:07:29 +0600

    Updated BountyBadge styles

--- components/bounties_v2/BountyBadge.tsx
+++ components/bounties_v2/BountyBadge.tsx
@@ -1,5 +1,8 @@
+import { useTheme } from '@emotion/react';
 import LaunchIcon from '@mui/icons-material/Launch';
+import { IconButton, Typography } from '@mui/material';
 import Box from '@mui/material/Box';
+import { grey } from '@mui/material/colors';
 import Container from '@mui/material/Container';
 import { Bounty } from '@prisma/client';
 import { useCurrentSpace } from 'hooks/useCurrentSpace';
@@ -19,40 +22,80 @@ export interface IBountyBadgeProps {
 }
 
 export function BountyBadge ({ bounty, hideLink = false } : IBountyBadgeProps) {
-
+  const theme = useTheme();
   const [space] = useCurrentSpace();
-  const radius = '10px';
 
   const bountyLink = `/${space!.domain}/bounty/${bounty.id}`;
 
   return (
-    <Box sx={{ maxWidth: '400px', background: 'background', borderRadius: radius }}>
-      <Container sx={{ display: 'flex', alignItems: 'center', verticalAlign: 'middle' }}>
-        <Image
-          loading='lazy'
-          width='30px'
-          height='30px'
-          src={CryptoLogoPaths[bounty.rewardToken as CryptoCurrency]}
-        />
-        <Box component='span' px={1}>
-          {bounty.rewardAmount}
+    <Box sx={{ maxWidth: '400px', background: 'background', borderRadius: theme.spacing(1) }}>
+      <Container sx={{ display: 'flex', alignItems: 'center', padding: theme.spacing(1) }}>
+        <Box
+          mr={0.75}
+          component='span'
+          sx={{
+            width: 25,
+            display: 'flex',
+            alignItems: 'center'
+          }}
+        >
+          <Image
+            loading='lazy'
+            width={25}
+            height={25}
+            src={CryptoLogoPaths[bounty.rewardToken as CryptoCurrency]}
+          />
         </Box>
-        <Box component='span' px={1}>
+        <Typography
+          component='span'
+          sx={{
+            fontWeight: 600,
+            color: grey[50]
+          }}
+          mr={0.5}
+          variant='h6'
+        >
+          {bounty.rewardAmount}
+        </Typography>
+        <Box
+          component='span'
+          mr={2}
+          sx={{
+            position: 'relative',
+            top: 2,
+            fontSize: 12,
+            color: grey[50]
+          }}
+        >
           {bounty.rewardToken}
         </Box>
         {
           hideLink === false && (
             <Link href={bountyLink} passHref={true}>
-              <LaunchIcon sx={{ float: 'right' }} />
+              <IconButton>
+                <LaunchIcon sx={{
+                  fill: grey[50]
+                }}
+                />
+              </IconButton>
             </Link>
           )
         }
 
       </Container>
-      <Container sx={{ background: BountyStatusColours[bounty.status], textAlign: 'center', fontWeight: 'bold' }}>
-        {BOUNTY_LABELS[bounty.status]}
-        {' '}
-        bounty
+      <Container sx={{ padding: theme.spacing(1), background: BountyStatusColours[bounty.status], textAlign: 'center', fontWeight: 'bold' }}>
+        <Typography
+          component='span'
+          sx={{
+            textTransform: 'uppercase',
+            fontWeight: 600,
+            color: grey[50]
+          }}
+          variant='h6'
+          px={1}
+        >
+          {BOUNTY_LABELS[bounty.status]}
+        </Typography>
       </Container>
     </Box>
   );
