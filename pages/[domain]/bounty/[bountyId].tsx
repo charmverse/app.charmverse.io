ddabf7123ac9c84a40b8d79b19172ccc2b98692a
melboudi <mo@anyahq.co>
2022-02-23 14:42:02 +0200

    Add edit bounty modal inside bounty details page

--- pages/[domain]/bounty/[bountyId].tsx
+++ pages/[domain]/bounty/[bountyId].tsx
@@ -1,12 +1,15 @@
 import Avatar from '@mui/material/Avatar';
 import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
+import EditIcon from '@mui/icons-material/Edit';
 import Table from '@mui/material/Table';
 import Typography from '@mui/material/Typography';
 import { Bounty } from '@prisma/client';
 import charmClient from 'charmClient';
 import { BountyBadge } from 'components/bounties_v2/BountyBadge';
 import { PageLayout } from 'components/common/page-layout';
+import BountyModal from 'components/bounties_v2/BountyModal';
 import CharmEditor from 'components/editor/CharmEditor';
 import { useCurrentSpace } from 'hooks/useCurrentSpace';
 import { useRouter } from 'next/router';
@@ -17,6 +20,7 @@ export default function BountyDetails () {
 
   const [space] = useCurrentSpace();
   const [bounty, setBounty] = useState(null as any as Bounty);
+  const [showBountyEditDialog, setShowBountyEditDialog] = useState(false);
   const router = useRouter();
 
   async function loadBounty () {
@@ -25,9 +29,15 @@ export default function BountyDetails () {
     setBounty(foundBounty);
   }
 
+  function toggleDialog () {
+    setShowBountyEditDialog(!showBountyEditDialog);
+  }
+
   useEffect(() => {
     loadBounty();
   }, []);
+
+  const viewerCanModifyBounty: boolean = true;
 
   //  charmClient.getBounty();
 
@@ -41,14 +51,27 @@ export default function BountyDetails () {
 
   return (
     <>
+
+      <BountyModal onSubmit={loadBounty} mode='update' bounty={bounty} open={showBountyEditDialog} onClose={toggleDialog} />
+
       <Grid container direction='column' justifyContent='space-between'>
         <Grid item xs={8}>
-          <Typography variant='h1'>{bounty.title}</Typography>
+          <Typography variant='h1'>
+            <Box component='span' px={1}>
+              {bounty.title}
+            </Box>
+            {
+              viewerCanModifyBounty === true && (
+                <EditIcon fontSize='small' onClick={toggleDialog} />
+
+              )
+            }
+          </Typography>
         </Grid>
         <Grid item xs={4}>
-          <BountyBadge bounty={bounty} hideLink={true} />
-        </Grid>
-        <Grid item xs={12}>
+          <Box sx={{ float: 'right' }}>
+            <BountyBadge bounty={bounty} hideLink={true} />
+          </Box>
         </Grid>
       </Grid>
 
@@ -57,7 +80,7 @@ export default function BountyDetails () {
         <CharmEditor content={bounty.descriptionNodes as any}></CharmEditor>
       </Box>
 
-      <Grid container direction='column' display='block' justifyContent='space-between'>
+      <Grid container direction='column'>
         <Grid item xs={6}>
           <Typography variant='h5'>Reviewer</Typography>
 
@@ -70,17 +93,21 @@ export default function BountyDetails () {
 
         <Grid item xs={6}>
           <Typography variant='h5'>Assignee</Typography>
+
           {
-            /*
-            <Typography variant='h5'>Assignee</Typography>
-          Content */
+            // eslint-disable-next-line eqeqeq
+            bounty.assignee == undefined && (
+              <Box>
+                <p>Open to applications</p>
+                <Button>Apply now</Button>
+              </Box>
+            )
           }
-          No assignee yet
 
         </Grid>
       </Grid>
 
-      <BountyApplicantList bountyId='' />
+      <BountyApplicantList bountyId={bounty.id} />
     </>
   );
 }
