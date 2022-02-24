d5fc1d571d08261e35f5ab35824e92a478ed037b
Matt Casey <mattwad@gmail.com>
2022-02-23 22:21:32 -0700

    layout for icon

--- components/common/Emoji.tsx
+++ components/common/Emoji.tsx
@@ -18,9 +18,23 @@ const Emoji = styled(Box)`
   display: flex;
   align-items: center;
   justify-content: center;
+  position: relative;
+  z-index: 1;
 
-  &:hover {
+  &:before {
+    border-radius: 4px;
+    content: " ";
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    opacity: 0;
+    z-index: -1;
     background-color: ${({ theme }) => theme.palette.background.light}
+  }
+  &:hover:before {
+    opacity: 0.5;
   }
 `;
 
--- components/editor/Editor.tsx
+++ components/editor/Editor.tsx
@@ -25,7 +25,7 @@ const PageControlItem = styled(ListItemButton)`
   border-radius: ${({ theme }) => theme.spacing(0.5)};
   opacity: 0.5;
   display: flex;
-  padding: ${({ theme }) => theme.spacing(0.75)};
+  padding: 0 ${({ theme }) => theme.spacing(0.75)};
   width: fit-content;
 `;
 
@@ -50,10 +50,9 @@ const EditorHeader = styled.div`
   overflow: visible;
 
   .page-controls {
-    height: 36px;
+    min-height: 32px;
     opacity: 0;
     display: flex;
-    gap: 1;
   }
 
   &:hover .page-controls {
@@ -64,9 +63,15 @@ const EditorHeader = styled.div`
 export function Editor ({ page, setPage }: { page: Page, setPage: (p: Partial<Page>) => void }) {
   const { isEditing, setIsEditing } = usePages();
 
-  let pageTop = 100;
+  let pageTop = 200;
   if (page.icon) {
-    pageTop = 130;
+    pageTop = 280;
+  }
+  if (page.headerImage) {
+    pageTop = 100;
+    if (page.icon) {
+      pageTop = 120;
+    }
   }
 
   function updateTitle (event: ChangeEvent<HTMLInputElement>) {
@@ -75,6 +80,10 @@ export function Editor ({ page, setPage }: { page: Page, setPage: (p: Partial<Pa
 
   function addPageHeader () {
     const headerImage = PageCoverGalleryImageGroups['Color & Gradient'][randomIntFromInterval(0, PageCoverGalleryImageGroups['Color & Gradient'].length - 1)];
+    setPage({ headerImage });
+  }
+
+  function updatePageHeader (headerImage: string | null) {
     setPage({ headerImage });
   }
 
@@ -99,7 +108,7 @@ export function Editor ({ page, setPage }: { page: Page, setPage: (p: Partial<Pa
 
   return (
     <Box>
-      <PageBanner page={page} setPage={setPage} />
+      {page.headerImage && <PageBanner image={page.headerImage} setImage={updatePageHeader} />}
       <Container top={pageTop}>
         <CharmEditor
           key={page.id}
@@ -112,11 +121,8 @@ export function Editor ({ page, setPage }: { page: Page, setPage: (p: Partial<Pa
               <Emoji sx={{ fontSize: 78 }}>{page.icon}</Emoji>
             </EmojiContainer>
             )}
-            <Controls>
-              <Box
-                className='page-controls'
-              >
-                {!page.icon && (
+            <Controls className='page-controls'>
+              {!page.icon && (
                 <PageControlItem onClick={addPageIcon}>
                   <EmojiEmotionsIcon
                     fontSize='small'
@@ -124,8 +130,8 @@ export function Editor ({ page, setPage }: { page: Page, setPage: (p: Partial<Pa
                   />
                   Add icon
                 </PageControlItem>
-                )}
-                {!page.headerImage && (
+              )}
+              {!page.headerImage && (
                 <PageControlItem onClick={addPageHeader}>
                   <ImageIcon
                     fontSize='small'
@@ -133,8 +139,7 @@ export function Editor ({ page, setPage }: { page: Page, setPage: (p: Partial<Pa
                   />
                   Add cover
                 </PageControlItem>
-                )}
-              </Box>
+              )}
             </Controls>
             <PageTitle
               value={page.title}
