912d25aae002881b41625a8bb1d7fe652c24630a
Safwan Shaheer <devorein00@gmail.com>
2022-02-25 18:26:08 +0600

    Multiline page title support (#131)

--- components/editor/Page/PageTitle.tsx
+++ components/editor/Page/PageTitle.tsx
@@ -1,16 +1,36 @@
 import { EditorViewContext } from '@bangle.dev/react';
 import styled from '@emotion/styled';
+import { TextField } from '@mui/material';
 import { TextSelection } from 'prosemirror-state';
 import { ChangeEvent, useContext } from 'react';
 
-const StyledPageTitle = styled.input`
-  background: transparent;
-  border: 0 none;
-  color: ${({ theme }) => theme.palette.text.primary};
-  cursor: text;
-  font-size: 40px;
-  font-weight: 700;
-  outline: none;
+const StyledPageTitle = styled(TextField)`
+  &.MuiFormControl-root {
+    width: 100%
+  }
+
+  & .MuiInput-input {
+    background: transparent;
+    border: 0 none;
+    color: ${({ theme }) => theme.palette.text.primary};
+    cursor: text;
+    font-size: 40px;
+    font-weight: 700;
+    outline: none;
+    line-height: 1.25em;
+  }
+
+  & .MuiInput-root:after {
+    border: none;
+  }
+
+  & .MuiInput-root:before {
+    border: none;
+  }
+
+  & .MuiInput-root:hover:not(.Mui-disabled):before {
+    border: none
+  }
 `;
 
 interface PageTitleProps {
@@ -26,6 +46,8 @@ export default function PageTitle ({ value, onChange }: PageTitleProps) {
       onChange={onChange}
       placeholder='Untitled'
       autoFocus
+      multiline
+      variant='standard'
       onKeyDown={(e) => {
         if (e.code === 'Enter') {
           // prevent inserting a new line into the editor
