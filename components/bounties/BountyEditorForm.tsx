5185b9d35753f2920080dc94d9fb3fef92a2484c
Matt Casey <mattwad@gmail.com>
2022-02-25 09:31:51 -0700

    fix currency inputg

--- components/bounties/BountyEditorForm.tsx
+++ components/bounties/BountyEditorForm.tsx
@@ -31,7 +31,7 @@ export const schema = yup.object({
   rewardToken: yup.string().required(),
   descriptionNodes: yup.mixed(),
   description: yup.string(),
-  reviewer: yup.string()
+  reviewer: yup.string().nullable(true)
 });
 
 type FormValues = yup.InferType<typeof schema>
@@ -67,7 +67,10 @@ export function BountyEditorForm ({ onSubmit, bounty, mode = 'create' }: IBounty
     formState: { errors, isValid, isSubmitting }
   } = useForm<FormValues>({
     mode: 'onBlur',
-    defaultValues: bounty as any,
+    defaultValues: {
+      rewardToken: 'ETH' as CryptoCurrency,
+      ...(bounty || {})
+    },
     resolver: yupResolver(schema)
   });
 
@@ -168,7 +171,7 @@ export function BountyEditorForm ({ onSubmit, bounty, mode = 'create' }: IBounty
               <InputLabel>
                 Reward token
               </InputLabel>
-              <InputSearchCrypto defaultValue={bounty?.rewardToken as CryptoCurrency || 'ETH'} label='' register={register} modelKey='rewardToken' />
+              <InputSearchCrypto label='' register={register} modelKey='rewardToken' />
             </Grid>
           </Grid>
           <Grid item>
