09b199d9f6817c8aea4d6bf63cabfa6884fc6bee
Safwan Shaheer <devorein00@gmail.com>
2022-02-25 09:07:04 +0600

    Moved from sx props to styled

--- components/editor/Page/PageTitle.tsx
+++ components/editor/Page/PageTitle.tsx
@@ -6,7 +6,32 @@ import { TextSelection } from 'prosemirror-state';
 import { ChangeEvent, useContext } from 'react';
 
 const StyledPageTitle = styled(TextField)`
-  
+  &.MuiFormControl-root {
+    width: 100%
+  }
+
+  & .MuiInput-input {
+    background: transparent;
+    border: 0 none;
+    color: ${({ theme }) => theme.palette.text.primary};
+    cursor: text;
+    font-size: 40px;
+    font-weight: 700;
+    outline: none;
+    line-height: 1.5em;
+  }
+
+  & .MuiInput-root:after {
+    border: none;
+  }
+
+  & .MuiInput-root:before {
+    border: none;
+  }
+
+  & .MuiInput-root:hover:not(.Mui-disabled):before {
+    border: none
+  }
 `;
 
 interface PageTitleProps {
@@ -26,30 +51,6 @@ export default function PageTitle ({ value, onChange }: PageTitleProps) {
       autoFocus
       multiline
       variant='standard'
-      sx={{
-        '&.MuiFormControl-root': {
-          width: '100%'
-        },
-        '& .MuiInput-input': {
-          background: 'transparent',
-          border: '0 none',
-          color: theme.palette.text.primary,
-          cursor: 'text',
-          fontSize: 40,
-          fontWeight: 700,
-          outline: 'none',
-          lineHeight: '1.5em'
-        },
-        '& .MuiInput-root:after': {
-          border: 'none'
-        },
-        '& .MuiInput-root:before': {
-          border: 'none'
-        },
-        '& .MuiInput-root:hover:not(.Mui-disabled):before': {
-          border: 'none'
-        }
-      }}
       onKeyDown={(e) => {
         if (e.code === 'Enter') {
           // prevent inserting a new line into the editor
