b8df8118736a41a9bb15c89b507503151a64584e
Safwan Shaheer <devorein00@gmail.com>
2022-02-28 20:00:54 +0600

    Use page icon for nested page

--- components/editor/NestedPage.tsx
+++ components/editor/NestedPage.tsx
@@ -66,11 +66,13 @@ export function NestedPage ({ node }: NodeViewProps) {
       }}
     >
       <Box>
+        {nestedPage?.icon ?? (
         <svg viewBox='0 0 32 32' height='1em' width='1em' className='page' style={{ transform: 'scale(1.25)', fill: 'currentColor', flexShrink: 0, backfaceVisibility: 'hidden' }}>
           <g>
             <path d='M16,1H4v28h22V11L16,1z M16,3.828L23.172,11H16V3.828z M24,27H6V3h8v10h10V27z M8,17h14v-2H8V17z M8,21h14v-2H8V21z M8,25h14v-2H8V25z' />
           </g>
         </svg>
+        )}
       </Box>
       <Box fontWeight={600} component='span'>
         {nestedPage?.title ? nestedPage.title : 'Untitled'}
