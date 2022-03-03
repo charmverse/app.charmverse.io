be0530c3aa5c2a714795c80b19967b39b40723e9
motechFR <mo@anyahq.co>
2022-03-03 20:30:38 +0200

    Handle empty workspace (#163)
    * Handle empty workspace
    
    * Provide untitled seed data page
    
    * Provide a unique id for untitled pages
    
    * Refresh page list, redirect user to workspace home if current page is deleted
    
    * Redirect workspace user from inexistent page to workspace home
    
    * Redirect a user from workspace they cannot access to their home workspace

--- pages/[domain]/index.tsx
+++ pages/[domain]/index.tsx
@@ -5,24 +5,37 @@ import { useCurrentSpace } from 'hooks/useCurrentSpace';
 import { usePages } from 'hooks/usePages';
 import { useRouter } from 'next/router';
 import { prisma } from 'db';
+import { untitledPage } from 'seedData';
+import { withSessionSsr } from 'lib/session/withSession';
+import { checkUserCanVisitWorkspace, IUserCannotAccessSpace } from 'lib/middleware/serverSidePropsRoleCheck';
 
 import { GetServerSideProps } from 'next';
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const domain = context.query.domain as string;
-  const space = await prisma.space.findUnique({ where: { domain } });
-  if (!space) {
-    console.error(`No space found by domain: ${domain}`);
-    return {
-      notFound: true
-    };
+export const getServerSideProps: GetServerSideProps = withSessionSsr(async (context) => {
+
+  const {
+    redirect,
+    notFound
+  } = await checkUserCanVisitWorkspace(context);
+
+  if (redirect) {
+    return { redirect };
   }
-  const firstPage = await prisma.page.findFirst({ where: { spaceId: space.id } });
+
+  if (notFound) {
+    return { notFound };
+  }
+
+  const domain = context.query.domain as string;
+  const space = (await prisma.space.findUnique({ where: { domain } }))!;
+  const userId = context.req.session.user?.id;
+
+  let firstPage = await prisma.page.findFirst({ where: { spaceId: space.id } });
   if (!firstPage) {
-    console.error(`Space has no pages: ${space.id}`);
-    return {
-      notFound: true
-    };
+
+    firstPage = await prisma.page.create({
+      data: untitledPage({ userId, spaceId: space.id })
+    });
   }
   return {
     redirect: {
@@ -30,7 +43,7 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
       permanent: false
     }
   };
-};
+});
 
 export default function IndexPage () {
   const [space] = useCurrentSpace();
