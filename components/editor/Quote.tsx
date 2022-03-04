2087b5a58d609a1ce26ccd491dea9b56bfb49c93
melboudi <mo@anyahq.co>
2022-03-04 13:48:36 +0200

    Improve the spacing

--- components/editor/Quote.tsx
+++ components/editor/Quote.tsx
@@ -8,7 +8,9 @@ import { emojiSuggestKey } from './EmojiSuggest';
 const StyledBlockQuote = styled.div`
   border-left: 4px solid ${({ theme }) => theme.palette.text.primary};
   font-size: 20px;
-  padding: ${({ theme }) => theme.spacing(1)};
+  padding-left: ${({ theme }) => theme.spacing(2)};
+  padding-top: ${({ theme }) => theme.spacing(1.2)};
+  padding-bottom: ${({ theme }) => theme.spacing(1.2)};
   margin-top: ${({ theme }) => theme.spacing(1)};
   display: flex;
   gap: ${({ theme }) => theme.spacing(1)};
