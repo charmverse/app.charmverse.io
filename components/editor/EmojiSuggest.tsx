e030afa0d2978acca3cdd50fd09116ad9f21e3c4
Safwan Shaheer <devorein00@gmail.com>
2022-03-10 16:44:47 +0600

    Removed gemoji.json as its no longer required

--- components/editor/EmojiSuggest.tsx
+++ components/editor/EmojiSuggest.tsx
@@ -2,30 +2,9 @@ import { PluginKey } from '@bangle.dev/core';
 import { emojiSpec } from 'components/editor/@bangle.dev/emoji/emoji';
 import * as emojiSuggest from 'components/editor/@bangle.dev/react-emoji-suggest/emoji-suggest';
 import { EmojiSuggest } from 'components/editor/@bangle.dev/react-emoji-suggest/EmojiSuggest';
-import gemojiData from 'emoji-lookup-data/data/gemoji.json';
 
 export const emojiSuggestKey = new PluginKey('emojiSuggestKey');
 export const emojiSuggestMarkName = 'emojiSuggest';
-
-const emojiData = Object.values(
-  gemojiData.reduce((prev, obj) => {
-    if (!prev[obj.category]) {
-      prev[obj.category] = { name: obj.category, emojis: [] };
-    }
-    prev[obj.category].emojis.push([obj.aliases[0], obj.emoji]);
-
-    return prev;
-  }, {} as Record<string, { name: string, emojis: [string, string][] }>)
-);
-
-export const getEmojiByAlias = (emojiAlias: string) => {
-  for (const { emojis } of emojiData) {
-    const match = emojis.find(e => e[0] === emojiAlias);
-    if (match) {
-      return match;
-    }
-  }
-};
 
 export const emojiSpecs = () => {
   return [
