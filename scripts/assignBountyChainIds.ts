3d2059f801c6f7c28b786c0e1cbbd12ce8fe0dda
melboudi <mo@anyahq.co>
2022-03-11 19:36:47 +0200

    Update the script to ignore testnets

--- scripts/assignBountyChainIds.ts
+++ scripts/assignBountyChainIds.ts
@@ -4,27 +4,33 @@ import { prisma } from 'db';
 function assignChainIds (): Promise<any> {
 
   return Promise.all(
-    RPCList.map(chain => {
-      const nativeCurrency = chain.nativeCurrency.symbol;
-      const chainId = chain.chainId;
-      console.log(chainId, nativeCurrency);
-      if (chainId && nativeCurrency) {
-        console.log('Moving inside');
-        return prisma.bounty.updateMany({
-          where: {
-            rewardToken: nativeCurrency
-          },
-          data: {
-            chainId
-          }
-        });
-      }
-      return Promise.resolve(false);
-    })
+    RPCList
+      .filter(chain => {
+        // Ignore Goerli, Rinkeby and Mumbai chains
+        return [5, 4, 80001].indexOf(chain.chainId) === -1;
+      })
+      .map(chain => {
+        const nativeCurrency = chain.nativeCurrency.symbol;
+        const chainId = chain.chainId;
+        if (chainId && nativeCurrency) {
+          return prisma.bounty.updateMany({
+            where: {
+              rewardToken: nativeCurrency,
+              chainId: null
+            },
+            data: {
+              chainId
+            }
+          });
+        }
+        return Promise.resolve(false);
+      })
   );
 }
 
+/*
 assignChainIds()
   .then(() => {
     console.log('Success!');
   });
+*/
