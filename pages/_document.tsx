0b99c126a2af43ebdde3ae5df2795ac9cf585c6b
Matt Casey <mattwad@gmail.com>
2022-03-12 08:31:31 -0700

    more viewport meta tag to _app.tsx (#216)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -13,7 +13,6 @@ class MyDocument extends Document {
           <meta name='theme-color' content={blueColor} />
           <link rel='icon' href='/favicon.png' />
           <meta name='description' content='The First Web 3 Native All-in-one Workspace' />
-          <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width' />
           <meta property='og:title' content='CharmVerse' />
           <meta property='og:image' content='https://app.charmverse.io/images/charmverse_logo_sm_black.png' />
           <meta property='twitter:title' content='CharmVerse' />
