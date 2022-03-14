91d9aea0174ce518380cd109c62b1fc28e09fe60
Matt Casey <mattwad@gmail.com>
2022-03-14 02:25:35 -0600

    ..

--- components/common/Modal/ErrorModal.tsx
+++ components/common/Modal/ErrorModal.tsx
@@ -1,10 +1,11 @@
 import { ComponentProps } from 'react';
 import Alert from '@mui/material/Alert';
-import { Modal } from './Modal';
+import { Modal, DialogTitle } from './Modal';
 
 export function ErrorModal ({ message, ...props }: { message: string } & Omit<ComponentProps<typeof Modal>, 'children'>) {
   return (
     <Modal size='fluid' {...props}>
+      <DialogTitle onClose={props.onClose}>An error occurred</DialogTitle>
       <Alert severity='error'>{message}</Alert>
     </Modal>
   );
