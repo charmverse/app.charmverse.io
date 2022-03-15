c9bb5b64036e4f39fb151e3ca3fa7621647a5fcf
melboudi <mo@anyahq.co>
2022-03-15 14:44:44 +0200

    Display the available payment methods

--- components/settings/PaymentMethods.tsx
+++ components/settings/PaymentMethods.tsx
@@ -1,7 +1,11 @@
 import { useState } from 'react';
 import { PaymentMethod } from '@prisma/client';
 import { usePopupState, bindTrigger } from 'material-ui-popup-state/hooks';
+import DeleteIcon from '@mui/icons-material/Delete';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 import { usePaymentMethods } from 'hooks/usePaymentMethods';
+import { getChainById } from 'connectors';
 import { Modal } from 'components/common/Modal';
 import { CustomErcTokenForm } from 'components/common/form/CustomErcTokenForm';
 import charmClient from 'charmClient';
@@ -39,6 +43,50 @@ export default function PaymentMethodList ({ isAdmin = true }) {
         </Button>
         )}
       </Legend>
+
+      <Box>
+        {
+          (Object.entries(paymentMethods).map(chain => {
+
+            const chainDetails = getChainById(chain[0]);
+
+            const chainPaymentMethods = chain[1];
+
+            return (
+              <Box key={chain[0]} sx={{ mb: 2 }}>
+                <Typography variant='h2'>{chainDetails?.chainName}</Typography>
+
+                {
+                  chainPaymentMethods.map(paymentMethod => {
+                    return (
+                      <Box width='100%' sx={{ display: 'flex' }}>
+
+                        <Box component='span' sx={{ pr: 1 }}>
+                          {paymentMethod.tokenSymbol}
+                        </Box>
+
+                        {
+                          paymentMethod.tokenLogo && (
+                            <Box component='span'>
+
+                              <img width='25x' height='25px' alt='Crypto logo' src={paymentMethod.tokenLogo} />
+                            </Box>
+                          )
+                        }
+
+                        {paymentMethod.contractAddress}
+                        <DeleteIcon sx={{ fill: 'red' }}></DeleteIcon>
+                        ;
+                      </Box>
+                    );
+                  })
+                }
+
+              </Box>
+            );
+          }))
+        }
+      </Box>
     </>
   );
 }
