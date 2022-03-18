fbe8b64978bae6fbd284c4f9780ed2789f103182
AndreiMitrea <andrei.mitrea@imbuelabs.com>
2022-03-18 22:06:52 +0200

    Added tasks table

--- components/profile/tasks/TasksList.tsx
+++ components/profile/tasks/TasksList.tsx
@@ -1,5 +1,68 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
+import styled from '@emotion/styled';
+import Chip from '@mui/material/Chip';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import OpenInNewIcon from '@mui/icons-material/OpenInNew';
 
-export function TasksList () {
+const StyledTableHead = styled(TableHead)`
+  font-weight: 700;
+`;
 
-  return <div></div>;
+export type Link = {id: string; name: string; url:string;};
+
+export type Task = {
+  id: string;
+  date: Date;
+  description: string;
+  links: Link[];
+  type: string;
+  workspace: string;
+};
+
+type TasksListProps = {
+  tasks: Task[];
+};
+
+export function TasksList ({ tasks }: TasksListProps) {
+
+  const handleSign = () => {
+  };
+
+  return (
+    <Table size='small' aria-label='simple table'>
+      <StyledTableHead>
+        <TableRow>
+          <TableCell sx={{ px: 0 }}>Date</TableCell>
+          <TableCell>My Tasks</TableCell>
+          <TableCell>Type</TableCell>
+          <TableCell>Workspace</TableCell>
+          <TableCell></TableCell>
+        </TableRow>
+      </StyledTableHead>
+      <TableBody>
+        {
+          tasks.map((task: Task, _taskIndex: number) => (
+            <TableRow key={`task-row-${task.id}`}>
+              <TableCell sx={{ px: 0 }}>{task.date.toLocaleDateString()}</TableCell>
+              <TableCell>
+                {task.description} <br />
+                {task.links.map((link: Link, _linkIndex: number) => (
+                  <span key={`link-${link.id}`}>
+                    [{link.name}<a href={link.url}><OpenInNewIcon /></a>]
+                  </span>
+                ))}
+              </TableCell>
+              <TableCell><Chip label={task.type} variant='outlined' /></TableCell>
+              <TableCell>{task.workspace}</TableCell>
+              <TableCell><Chip label='Sign' variant='outlined' onClick={handleSign} /></TableCell>
+            </TableRow>
+          ))
+        }
+      </TableBody>
+    </Table>
+  );
 }
--- pages/profile/tasks.tsx
+++ pages/profile/tasks.tsx
@@ -2,8 +2,24 @@ import { ReactElement } from 'react';
 import Box from '@mui/material/Box';
 import { PageLayout } from 'components/common/page-layout';
 import { setTitle } from 'hooks/usePageTitle';
-import { ProfileHeader, TasksList } from 'components/profile';
+import { ProfileHeader, Task, TasksList } from 'components/profile';
 
+const tasks: Task[] = [{
+  id: '2a2b16ba-2a0e-458f-a6cd-24af8745dcf7',
+  date: new Date(),
+  description: 'Send 23.2 CHARM',
+  links: [{
+    id: 'd89de30b-0531-4321-a38c-73c9176d6bfa',
+    name: 'Gnosis',
+    url: 'https://gnosis-safe.io/app/'
+  }, {
+    id: '4ebbc49e-9703-4415-91f7-0e2022799c47',
+    name: 'Bounty',
+    url: 'https://app.charmverse.io/'
+  }],
+  type: 'Multisig',
+  workspace: 'Bankless'
+}];
 export default function TasksPage () {
 
   setTitle('Tasks');
@@ -11,7 +27,7 @@ export default function TasksPage () {
   return (
     <Box py={3} px='80px'>
       <ProfileHeader />
-      <TasksList />
+      <TasksList tasks={tasks} />
     </Box>
   );
 
