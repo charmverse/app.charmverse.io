7eb3908dec220bad477343ee1e2c463e85f04e5f
Safwan Shaheer <devorein00@gmail.com>
2022-03-22 19:02:31 +0600

    Better error handling for token issues

--- pages/api/notion/import.ts
+++ pages/api/notion/import.ts
@@ -37,24 +37,35 @@ async function importNotion (req: NextApiRequest, res: NextApiResponse) {
 
   const encodedToken = Buffer.from(`${process.env.NOTION_OAUTH_CLIENT_ID}:${process.env.NOTION_OAUTH_SECRET}`).toString('base64');
 
-  const token = await http.POST<NotionApiResponse>('https://api.notion.com/v1/oauth/token', {
-    grant_type: 'authorization_code',
-    redirect_uri: req.headers.host!.startsWith('localhost') ? `http://${req.headers.host}/api/notion/callback` : 'https://app.charmverse.io/api/notion/callback',
-    code: tempAuthCode
-  }, {
-    headers: {
-      Authorization: `Basic ${encodedToken}`,
-      'Content-Type': 'application/json'
+  try {
+    const token = await http.POST<NotionApiResponse>('https://api.notion.com/v1/oauth/token', {
+      grant_type: 'authorization_code',
+      redirect_uri: req.headers.host!.startsWith('localhost') ? `http://${req.headers.host}/api/notion/callback` : 'https://app.charmverse.io/api/notion/callback',
+      code: tempAuthCode
+    }, {
+      headers: {
+        Authorization: `Basic ${encodedToken}`,
+        'Content-Type': 'application/json'
+      }
+    });
+    await importFromWorkspace({
+      spaceId,
+      userId: req.session.user.id,
+      accessToken: token.access_token,
+      workspaceName: token.workspace_name,
+      workspaceIcon: token.workspace_icon
+    });
+    res.status(200).end();
+  }
+  catch (err: any) {
+    console.log('Error', err);
+    if (err.error === 'invalid_grant' || err.error === 'invalid_request') {
+      res.status(400).json({ error: 'Invalid code. Please try importing again' });
     }
-  });
-  await importFromWorkspace({
-    spaceId,
-    userId: req.session.user.id,
-    accessToken: token.access_token,
-    workspaceName: token.workspace_name,
-    workspaceIcon: token.workspace_icon
-  });
-  res.status(200).end();
+    else {
+      res.status(400).json({ error: 'Something went wrong!' });
+    }
+  }
 }
 
 export default withSessionRoute(handler);
