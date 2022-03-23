8a1021bc0d6b646ee0fbee104f604eeb56827cd9
Matt Casey <mattwad@gmail.com>
2022-03-23 01:53:53 -0600

    make it easier to add content to end of documents

--- components/editor/Editor.tsx
+++ components/editor/Editor.tsx
@@ -10,7 +10,7 @@ import PageHeader from './Page/PageHeader';
 export const Container = styled(Box)<{ top: number }>`
   width: 860px;
   max-width: 100%;
-  margin: 0 auto 5px;
+  margin: 0 auto ${({ top }) => top + 100}px;
   padding: 0 80px;
   position: relative;
   top: ${({ top }) => top}px;
