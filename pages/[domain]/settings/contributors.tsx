2dad6826dad073ca5af62ff2e4d622d2f9b7f8b2
melboudi <mo@anyahq.co>
2022-03-24 19:47:17 +0200

    Hide roles form

--- pages/[domain]/settings/contributors.tsx
+++ pages/[domain]/settings/contributors.tsx
@@ -22,7 +22,12 @@ export default function ContributorSettings () {
   return (
     <>
       <InviteLinkList isAdmin={isAdmin} spaceId={space.id} />
+      {
+        /*
       <RoleAssignment />
+        */
+      }
+
       <ContributorList isAdmin={isAdmin} spaceId={space.id} spaceOwner={space.createdBy} />
     </>
   );
