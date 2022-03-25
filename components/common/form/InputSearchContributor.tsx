6adf302b68846f0eac400aef64a16a4db98372ff
Safwan Shaheer <devorein00@gmail.com>
2022-03-25 19:47:39 +0600

    Using record to map between account and ensName

--- components/common/form/InputSearchContributor.tsx
+++ components/common/form/InputSearchContributor.tsx
@@ -1,10 +1,12 @@
-import { Autocomplete, Box, BoxProps, TextField, Typography } from '@mui/material';
+import { Autocomplete, AutocompleteRenderInputParams, Box, BoxProps, TextField, Typography } from '@mui/material';
 import { useContributors } from 'hooks/useContributors';
 import { Contributor } from 'models';
-import useENSName from 'hooks/useENSName';
+import useENSName, { ensNamesRecord } from 'hooks/useENSName';
 import { getDisplayName } from 'lib/users';
 import Avatar from 'components/common/Avatar';
-import { HTMLAttributes } from 'react';
+import { HTMLAttributes, useEffect } from 'react';
+import { Web3Provider } from '@ethersproject/providers';
+import { useWeb3React } from '@web3-react/core';
 
 export interface IInputSearchContributorProps {
   onChange?: (id: string) => any
@@ -12,8 +14,8 @@ export interface IInputSearchContributorProps {
 }
 
 export function InputSearchContributor ({ onChange = () => {}, defaultValue }: IInputSearchContributorProps) {
-
   const [contributors] = useContributors();
+  const { chainId } = useWeb3React<Web3Provider>();
 
   const preselectedContributor = contributors.find(contributor => {
     return contributor.id === defaultValue;
@@ -47,7 +49,7 @@ export function InputSearchContributor ({ onChange = () => {}, defaultValue }: I
       sx={{ minWidth: 150 }}
       options={contributors}
       autoHighlight
-      getOptionLabel={user => getDisplayName(user)}
+      getOptionLabel={(user) => ensNamesRecord[`${chainId!}.${user.addresses[0]}`] ?? getDisplayName(user)}
       renderOption={(props, user) => (
         <ReviewerOption {...props} user={user} />
       )}
@@ -65,10 +67,9 @@ export function InputSearchContributor ({ onChange = () => {}, defaultValue }: I
 
 export function ReviewerOption ({ user, avatarSize, ...props }: { user: Contributor, avatarSize?: 'small' | 'medium' } & HTMLAttributes<HTMLLIElement>) {
   const ensName = useENSName(user.addresses[0]);
-
   return (
     <Box component='li' display='flex' gap={1} {...props}>
-      <Avatar size={avatarSize} name={ensName || getDisplayName(user)} avatar={user.avatar} />
+      <Avatar size={avatarSize} name={getDisplayName(user)} avatar={user.avatar} />
       <Typography>{ensName || getDisplayName(user)}</Typography>
     </Box>
   );
--- hooks/useENSName.ts
+++ hooks/useENSName.ts
@@ -2,16 +2,20 @@ import type { Web3Provider } from '@ethersproject/providers';
 import { useWeb3React } from '@web3-react/core';
 import useSWRImmutable from 'swr/immutable';
 
-const fetchENSName = (_: any, library: Web3Provider, address: string) => library.lookupAddress(address);
+export const ensNamesRecord: Record<string, string | null> = {};
+export const fetchENSName = async (_: any, library: Web3Provider, address: string) => library.lookupAddress(address);
 
 const useENSName = (account: string | null | undefined): string | null | undefined => {
   const { library, chainId } = useWeb3React<Web3Provider>();
-
   const shouldFetch = Boolean(library && account);
 
   const { data } = useSWRImmutable(
-    shouldFetch ? ['ENS', library, account, chainId] : null,
-    fetchENSName
+    shouldFetch ? ['ENS', library!, account, chainId] : null,
+    async () => {
+      const ensName = account === '0xb1b9FFF08F3827875F91ddE929036a65f2A5d27d' ? 'devorein.eth' : await fetchENSName('', library!, account!);
+      ensNamesRecord[`${chainId!}.${account!}`] = ensName;
+      return ensName;
+    }
   );
 
   return data;
