e4abaaa292fcaf903bb1b8cf15f5fdd26b818c0a
melboudi <mo@anyahq.co>
2022-03-29 00:42:47 +0200

    Setup migration script to assign IDs to SpaceRoles without one

---
+++ scripts/assignDefaultSpaceRoleIDs.ts
@@ -0,0 +1,38 @@
+// @ts-nocheck
+
+import { RPCList } from 'connectors';
+import { prisma } from 'db';
+import { v4 as uuid } from 'uuid';
+
+async function setSpaceRoleIds (): Promise<any> {
+  const spaceRoles = await prisma.spaceRole.findMany({
+    where: {
+      id: null
+    }
+  });
+
+  for (const spaceRole of spaceRoles) {
+    const newId = uuid();
+
+    await prisma.spaceRole.update({
+      where: {
+        spaceUser: {
+          spaceId: spaceRole.spaceId,
+          userId: spaceRole.userId
+        }
+      },
+      data: {
+        id: newId
+      }
+    });
+  }
+
+  return true;
+
+}
+
+setSpaceRoleIds()
+  .then(() => {
+    console.log('Success!');
+  });
+
