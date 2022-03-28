ed5003c07ad3daed0e28fd9c6e672bd0b0d62ff2
Safwan Shaheer <devorein00@gmail.com>
2022-03-28 20:25:56 +0600

    Added ImportNotionWorkspace component

--- pages/[domain]/settings/workspace.tsx
+++ pages/[domain]/settings/workspace.tsx
@@ -17,11 +17,8 @@ import { useRouter } from 'next/router';
 import { yupResolver } from '@hookform/resolvers/yup';
 import charmClient from 'charmClient';
 import { Box, Typography } from '@mui/material';
-import NotionIcon from 'public/images/notion_logo.svg';
-import SvgIcon from '@mui/material/SvgIcon';
-import CircularProgress from '@mui/material/CircularProgress';
-import useNotionImport from 'hooks/useNotionImport';
 import ImportDiscordRoles from 'components/settings/Import/ImportDiscordRoles';
+import ImportNotionWorkspace from 'components/settings/Import/ImportNotionWorkspace';
 
 export interface FailedImportsError {
   pageId: string,
@@ -34,11 +31,6 @@ export default function WorkspaceSettings () {
   const router = useRouter();
   const [space, setSpace] = useCurrentSpace();
   const [spaces] = useSpaces();
-  const {
-    isImportingFromNotion,
-    notionFailedImports,
-    notionImportError
-  } = useNotionImport();
 
   const {
     register,
@@ -113,61 +105,7 @@ export default function WorkspaceSettings () {
       </form>
       <Legend>Import</Legend>
       <Box sx={{ ml: 1 }} display='flex' flexDirection='column' gap={1}>
-        <div>
-          <Button
-            disabled={isImportingFromNotion}
-            href={`/api/notion/login?redirect=${encodeURIComponent(window.location.href.split('?')[0])}`}
-            variant='outlined'
-            startIcon={(
-              <SvgIcon sx={{ color: 'text.primary' }}>
-                <NotionIcon />
-              </SvgIcon>
-            )}
-            endIcon={(
-              isImportingFromNotion && <CircularProgress size={20} />
-            )}
-          >
-            {isImportingFromNotion ? 'Importing pages from Notion' : 'Import pages from Notion'}
-          </Button>
-          {notionFailedImports.length !== 0 && (
-            <Alert severity='warning' sx={{ mt: 2 }}>
-              <Box sx={{
-                display: 'flex', gap: 2, flexDirection: 'column'
-              }}
-              >
-                Pages where we encountered issues
-                {notionFailedImports.map(failedImport => (
-                  <div>
-                    <Box sx={{
-                      display: 'flex',
-                      gap: 1
-                    }}
-                    >
-                      <span>Type: {failedImport.type}</span>
-                      <span>Title: {failedImport.title}</span>
-                      <span>Id: {failedImport.pageId}</span>
-                    </Box>
-                    {failedImport.blocks.length !== 0 ? (
-                      <div>
-                        Blocks that failed to import for the page
-                        {failedImport.blocks.map((blockTrails, blockTrailsIndex) => (
-                          <div>
-                            {blockTrailsIndex + 1}. {blockTrails.map(([blockType, blockIndex]) => `${blockType}(${blockIndex + 1})`).join(' -> ')}
-                          </div>
-                        ))}
-                      </div>
-                    ) : null}
-                  </div>
-                ))}
-              </Box>
-            </Alert>
-          )}
-          {notionImportError && (
-            <Alert severity='error' sx={{ mt: 2 }}>
-              {notionImportError}
-            </Alert>
-          )}
-        </div>
+        <ImportNotionWorkspace />
         <ImportDiscordRoles />
       </Box>
     </>
