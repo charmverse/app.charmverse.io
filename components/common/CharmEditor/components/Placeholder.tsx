35da2b36e9402f4443af7329e8cdb45c3e4e4a72
Matt Casey <mattwad@gmail.com>
2022-03-28 22:30:12 -0600

    ..

--- components/common/CharmEditor/components/Placeholder.tsx
+++ components/common/CharmEditor/components/Placeholder.tsx
@@ -1,5 +1,5 @@
 import { useEditorViewContext } from '@bangle.dev/react';
-import { alpha } from '@mui/material';
+import { alpha } from '@mui/system';
 import styled from '@emotion/styled';
 
 const StyledPlaceholder = styled.div`
