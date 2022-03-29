7364ca69b7546f149c05704c4a45d0ad49f07364
Matt Casey <mattwad@gmail.com>
2022-03-29 02:15:41 -0600

    hide roles

--- components/settings/pages.tsx
+++ components/settings/pages.tsx
@@ -8,7 +8,7 @@ import PaymentsOutlinedIcon from '@mui/icons-material/PaymentsOutlined';
 export const SETTINGS_TABS = [
   { icon: <SettingsIcon fontSize='small' />, path: 'workspace', label: 'Workspace' },
   { icon: <PersonIcon fontSize='small' />, path: 'contributors', label: 'Contributors' },
-  { icon: <UserRoleIcon fontSize='small' />, path: 'roles', label: 'Roles' },
+  // { icon: <UserRoleIcon fontSize='small' />, path: 'roles', label: 'Roles' },
   { icon: <LockIcon fontSize='small' />, path: 'token-gates', label: 'Token Gates' },
   { icon: <PaymentsOutlinedIcon fontSize='small' />, path: 'payment-methods', label: 'Payment Methods' }
 ];
