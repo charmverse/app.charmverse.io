2138fa044467a0b5f18fc14d8882c2976e6334cb
melboudi <mo@anyahq.co>
2022-03-31 20:57:59 +0200

    Return only pages a user has access to

--- pages/api/spaces/[id]/pages.ts
+++ pages/api/spaces/[id]/pages.ts
@@ -11,14 +11,68 @@ const handler = nc<NextApiRequest, NextApiResponse>({ onError, onNoMatch });
 handler.use(requireUser).get(getPages);
 
 async function getPages (req: NextApiRequest, res: NextApiResponse<Page[]>) {
+
+  const spaceId = req.query.id as string;
+
+  const userId = req.session.user.id;
+
   const pages = await prisma.page.findMany({
     where: {
-      spaceId: req.query.id as string
+      OR: [
+        {
+          spaceId,
+          permissions: {
+            some: {
+              OR: [
+                {
+                  role: {
+                    spaceRolesToRole: {
+                      some: {
+                        spaceRole: {
+                          userId
+                        }
+                      }
+                    }
+                  }
+                },
+                {
+                  userId
+                },
+                {
+                  spaceId
+                }
+              ]
+            }
+          }
+        },
+        {
+          space: {
+            id: spaceId,
+            spaceRoles: {
+              some: {
+                OR: [{
+                  userId,
+                  isAdmin: true
+                }, {
+                  userId,
+                  role: 'admin'
+                }]
+              }
+            }
+          }
+        }
+      ]
+
     },
     include: {
       permissions: true
     }
   });
+
+  if (spaceId === 'eed82490-f0bd-4cf8-8651-775b666947e5') {
+    console.log('Pages found', pages.length);
+  }
+
   return res.status(200).json(pages);
 }
 
