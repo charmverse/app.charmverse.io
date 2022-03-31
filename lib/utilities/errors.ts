0af3d39fc248a135f53d6989fe74d1ad642cd730
melboudi <mo@anyahq.co>
2022-03-31 13:38:34 +0200

    Allow empty interface exception

--- lib/utilities/errors.ts
+++ lib/utilities/errors.ts
@@ -4,6 +4,8 @@ export interface IError {
   message: string,
 }
 
+// Keep empty for now until we decide how to do error handling in future
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface IApiError extends IError {}
 
 export interface IUserError extends IError {
