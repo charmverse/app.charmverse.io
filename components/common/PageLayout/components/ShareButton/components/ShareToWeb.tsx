e1799b982e1cb2a3a8fddabf9f0dca9bf134e61b
Matt Casey <mattwad@gmail.com>
2022-03-31 12:00:07 -0600

    fix ts errors

--- components/common/PageLayout/components/ShareButton/components/ShareToWeb.tsx
+++ components/common/PageLayout/components/ShareButton/components/ShareToWeb.tsx
@@ -37,7 +37,7 @@ export default function ShareToWeb ({ pagePermissions }: { pagePermissions: IPag
     setPages(_pages => ({
       ..._pages,
       [currentPageId]: {
-        ..._pages[currentPageId],
+        ..._pages[currentPageId]!,
         ...updates
       }
     }));
