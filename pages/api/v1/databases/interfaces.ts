7fe1f7d896574256d4dd8d297529b3f620789478
melboudi <mo@anyahq.co>
2022-04-04 23:56:19 +0200

    Add example values to the API docs

--- pages/api/v1/databases/interfaces.ts
+++ pages/api/v1/databases/interfaces.ts
@@ -16,28 +16,34 @@ export interface ApiPage extends Pick<Page, 'id' | 'createdAt' | 'type' | 'title
  *        id:
  *          type: string
  *          format: uuid
+ *          example: 3fa85f64-5717-4562-b3fc-2c963f66afa6
  *        createdAt:
  *          type: string
  *          format: date-time
+ *          example: 2022-04-04T21:32:38.317Z
  *        type:
  *          type: string
+ *          example: board
  *        title:
  *          type: string
+ *          example: Todo tracker
  *        content:
  *          type: string
+ *          example: content
  *        url:
  *          type: string
+ *          example: https://app.charmverse.io/my-workspace/page-5985679461310778
 
  */
 
 /*
- *     example:
- *        id: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
-*         createdAt: '2022-04-04T21:32:38.317Z',
-*         type: 'board',
-*         title: 'Todo tracker',
-*         content: 'string',
-*         url: 'https://app.charmverse.io/page-5985679461310778'
+ *      example:
+ *        id:
+*         createdAt:
+*         type: board
+*         title: Todo tracker
+*         content: string
+*         url: https://app.charmverse.io/page-5985679461310778
 */
 
 export interface ApiBoard extends ApiPage {
