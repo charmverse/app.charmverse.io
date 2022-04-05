095ecc7d7acf89e7b1723fe07baac806e7abf845
Matt Casey <mattwad@gmail.com>
2022-04-05 11:48:20 -0600

    move code into its own component

--- components/common/PageLayout/components/Header/Header.tsx
+++ components/common/PageLayout/components/Header/Header.tsx
@@ -12,7 +12,6 @@ import FavoritedIcon from '@mui/icons-material/Star';
 import NotFavoritedIcon from '@mui/icons-material/StarBorder';
 import { Box, CircularProgress } from '@mui/material';
 import IconButton from '@mui/material/IconButton';
-import Link from 'components/common/Link';
 import List from '@mui/material/List';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
@@ -25,15 +24,14 @@ import { Page } from '@prisma/client';
 import charmClient from 'charmClient';
 import { charmEditorPlugins, specRegistry } from 'components/common/CharmEditor/CharmEditor';
 import { useColorMode } from 'context/color-mode';
-import { LinkedPage, usePages } from 'hooks/usePages';
+import { usePages } from 'hooks/usePages';
 import { useUser } from 'hooks/useUser';
 import { PageContent } from 'models';
 import { useRouter } from 'next/router';
-import { Fragment, useMemo, useRef, useState } from 'react';
-import { useBounties } from 'hooks/useBounties';
-import Account from './Account';
-import ShareButton from './ShareButton';
-import { StyledPageIcon } from './PageNavigation';
+import { useRef, useState } from 'react';
+import Account from '../Account';
+import ShareButton from '../ShareButton';
+import PageTitleWithBreadcrumbs from './PageTitleWithBreadcrumbs';
 
 export const headerHeight = 56;
 
@@ -42,96 +40,6 @@ const StyledToolbar = styled(Toolbar)`
   height: ${headerHeight}px;
   min-height: ${headerHeight}px;
 `;
-
-const BreadCrumb = styled.span`
-  & .breadcrumb-slash {
-    opacity: .5;
-    margin: 0em 0.5em;
-  }
-  display: flex;
-  padding-right: 0em;
-  a {
-    color: inherit;
-  }
-`;
-
-function BreadCrumbs ({ items }: {items: {
-  link: null | string,
-  icon: string | null,
-  title: string,
-  id: string
-}[]}) {
-  return (
-    <BreadCrumb>
-      {items.map((item, itemIndex) => {
-        const itemTitle = item.title || 'Untitled';
-        const currentPageCrumb = (
-          <Box display='flex'>
-            {item.icon && <StyledPageIcon icon={item.icon} />}
-            {itemTitle}
-          </Box>
-        );
-
-        // Last item doesn't have any /
-        if (itemIndex !== items.length - 1) {
-          return (
-            <Fragment key={item.id}>
-              {item.link ? (
-                <Link href={item.link}>
-                  {currentPageCrumb}
-                </Link>
-              ) : <div>{currentPageCrumb}</div>}
-              <span className='breadcrumb-slash'>/</span>
-            </Fragment>
-          );
-        }
-
-        return <div key={item.id}>{currentPageCrumb}</div>;
-      })}
-    </BreadCrumb>
-  );
-}
-
-function BountyBreadcrumbs () {
-  const router = useRouter();
-  const bountyId = router.query.bountyId as string;
-  const { bounties } = useBounties();
-  const bounty = bounties.find(_bounty => _bounty.id === bountyId);
-
-  return (
-    <BreadCrumbs items={[{
-      icon: null,
-      id: 'bounties',
-      link: `/${router.query.domain}/bounties`,
-      title: 'Bounties'
-    }, {
-      icon: null,
-      id: router.query.bountyId as string,
-      link: null,
-      title: bounty?.title ?? `Bounty (${bountyId})`
-    }]}
-    />
-  );
-}
-
-function PageBreadcrumbs ({ pages }: {pages: Page[]}) {
-  const router = useRouter();
-  const trimTrails = pages.length > 3;
-  const collapsedCrumb = {
-    title: '...',
-    path: null,
-    icon: null,
-    id: '123'
-  };
-  const displayedCrumbs = (trimTrails ? [pages[0], collapsedCrumb, pages[pages.length - 2], pages[pages.length - 1]] : pages).map(page => ({
-    title: page.title,
-    link: page.path ? `/${router.query.domain}/${page.path}` : null,
-    icon: page.icon,
-    id: page.id
-  }));
-
-  return <BreadCrumbs items={displayedCrumbs} />;
-}
 
 export default function Header ({ open, openSidebar }: { open: boolean, openSidebar: () => void }) {
   const router = useRouter();
@@ -144,21 +52,6 @@ export default function Header ({ open, openSidebar }: { open: boolean, openSide
   const pageMenuAnchor = useRef();
 
   const currentPage = currentPageId ? pages[currentPageId] : undefined;
-
-  const currentPageChain = useMemo(() => {
-    const currentPageParentChain: Page[] = [];
-    let currentPageInChain: Page | null | undefined = pages[currentPageId];
-    while (currentPageInChain) {
-      currentPageParentChain.push(currentPageInChain);
-      if (currentPageInChain.parentId) {
-        currentPageInChain = pages[currentPageInChain.parentId];
-      }
-      else {
-        currentPageInChain = null;
-      }
-    }
-    return currentPageParentChain.reverse();
-  }, [pages, currentPage]);
 
   const isFavorite = currentPage && user?.favorites.some(({ pageId }) => pageId === currentPage.id);
 
@@ -240,9 +133,7 @@ export default function Header ({ open, openSidebar }: { open: boolean, openSide
           gap: 2
         }}
         >
-          <Typography noWrap component='div' sx={{ fontWeight: 500, maxWidth: 500, textOverflow: 'ellipsis' }}>
-            {router.route === '/[domain]/bounties/[bountyId]' ? <BountyBreadcrumbs /> : router.route === '/[domain]/[pageId]' ? <PageBreadcrumbs pages={currentPageChain} /> : null}
-          </Typography>
+          <PageTitleWithBreadcrumbs />
           {isEditing && (
             <Box sx={{
               display: 'flex',
@@ -306,10 +197,6 @@ export default function Header ({ open, openSidebar }: { open: boolean, openSide
             </Box>
           )}
 
-          {/** context menu */}
-          {/* <IconButton size='small' sx={{ mx: 1 }} color='inherit'>
-            <MoreHorizIcon />
-          </IconButton> */}
           {/** dark mode toggle */}
           <Tooltip title={theme.palette.mode === 'dark' ? 'Light mode' : 'Dark mode'} arrow placement='bottom'>
             <IconButton sx={{ mx: 1 }} onClick={colorMode.toggleColorMode} color='inherit'>
---
+++ components/common/PageLayout/components/Header/PageTitleWithBreadcrumbs.tsx
@@ -0,0 +1,117 @@
+import styled from '@emotion/styled';
+import { Box } from '@mui/material';
+import Link from 'components/common/Link';
+import Typography from '@mui/material/Typography';
+import { usePageTitle } from 'hooks/usePageTitle';
+import { usePages } from 'hooks/usePages';
+import { useRouter } from 'next/router';
+import { ReactNode } from 'react';
+import { StyledPageIcon } from '../PageNavigation';
+
+const BreadCrumb = styled.span`
+  :after {
+    content: ' / ';
+    opacity: .5;
+    margin-left: .5em;
+    margin-right: .5em;
+  }
+  a {
+    color: inherit;
+  }
+`;
+
+function PageTitleWrapper ({ children }: { children: ReactNode }) {
+  return (
+    <Typography noWrap component='div' sx={{ fontWeight: 500, maxWidth: 500, textOverflow: 'ellipsis' }}>
+      {children}
+    </Typography>
+  );
+}
+
+interface PageBreadCrumb {
+  path: null | string;
+  icon: string | null;
+  title: string;
+}
+
+function WorkspacePageTitle () {
+  const router = useRouter();
+  const { currentPageId, pages } = usePages();
+
+  const currentPage = pages[currentPageId];
+
+  // find parent pages
+  let activePage = currentPage;
+  const breadcrumbs: PageBreadCrumb[] = [];
+  while (activePage?.parentId) {
+    activePage = pages[activePage.parentId];
+    if (activePage) {
+      breadcrumbs.unshift(activePage);
+    }
+  }
+
+  const displayedCrumbs = breadcrumbs.slice(0, 2);
+  const trimmedCrumbs = (displayedCrumbs.length < breadcrumbs.length);
+  return (
+    <>
+      {displayedCrumbs.map(crumb => (
+        <BreadCrumb key={crumb.path}>
+          <Link href={`/${router.query.domain}/${crumb.path}`}>
+            <Box display='inline-flex'>
+              {crumb.icon && <StyledPageIcon icon={crumb.icon} />}
+              {crumb.title || 'Untitled'}
+            </Box>
+          </Link>
+        </BreadCrumb>
+      ))}
+      {trimmedCrumbs && (
+        <BreadCrumb>
+          <span className='breadcrumb-slash'>...</span>
+        </BreadCrumb>
+      )}
+      {currentPage && (
+        <Box display='inline-flex'>
+          {currentPage.icon && <StyledPageIcon icon={currentPage.icon} />}
+          {currentPage.title || 'Untitled'}
+        </Box>
+      )}
+    </>
+  );
+}
+
+function BountyPageTitle () {
+  const router = useRouter();
+  const [pageTitle] = usePageTitle();
+  return (
+    <PageTitleWrapper>
+      <BreadCrumb>
+        <Link href={`/${router.query.domain}/bounties`}>
+          Bounties
+        </Link>
+      </BreadCrumb>
+      {pageTitle}
+    </PageTitleWrapper>
+  );
+}
+
+function StandardPageTitle () {
+  const [pageTitle] = usePageTitle();
+  return (
+    <PageTitleWrapper>
+      {pageTitle}
+    </PageTitleWrapper>
+  );
+}
+
+export default function PageTitleWithBreadcrumbs () {
+  const router = useRouter();
+  if (router.route === '/[domain]/bounties/[bountyId]') {
+    return <BountyPageTitle />;
+  }
+  else if (router.route === '/[domain]/[pageId]') {
+    return <WorkspacePageTitle />;
+  }
+  else {
+    return <StandardPageTitle />;
+  }
+}
