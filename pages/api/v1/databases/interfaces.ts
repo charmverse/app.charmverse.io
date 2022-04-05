fec101864b7542100a505d9a0f73d90df078dde9
melboudi <mo@anyahq.co>
2022-04-05 16:21:00 +0200

    Add schema for Card

--- pages/api/v1/databases/interfaces.ts
+++ pages/api/v1/databases/interfaces.ts
@@ -39,8 +39,48 @@ export interface BoardPage extends ApiPage {
   type: 'board'
 }
 
+/**
+ * @example https://github.com/jellydn/next-swagger-doc/blob/main/example/models/organization.ts
+ *
+ * @swagger
+ * components:
+ *  schemas:
+ *    Card:
+ *      type: object
+ *      properties:
+ *        id:
+ *          type: string
+ *          format: uuid
+ *          example: 3fa85f64-5717-4562-b3fc-2c963f66afa6
+ *        databaseId:
+ *          type: string
+ *          format: uuid
+ *          example: 4e22cad9-8025-4e47-91dc-e37501c0ec71
+ *        createdAt:
+ *          type: string
+ *          format: date-time
+ *          example: 2022-03-04T21:32:38.317Z
+ *        updatedAt:
+ *          type: string
+ *          format: date-time
+ *          example: 2022-04-02T07:22:31.097Z
+ *        title:
+ *          type: string
+ *          example: Finalise community guidelines
+ *        content:
+ *          type: string
+ *          example: Markdown content appears here
+ *        isTemplate:
+ *          type: string
+ *          example: false
+ *        properties:
+ *          type: object
+ *          example: {}
+ */
 export interface Card {
   id: string
+  createdAt: string
+  updatedAt: string
   databaseId: string
   content: string
   title: string
