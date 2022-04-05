e51786a13e722cd0f52df66663222ca39f4dc2eb
melboudi <mo@anyahq.co>
2022-04-05 16:21:44 +0200

    Implement endpoint for retrieving a card

--- pages/api/v1/databases/[id]/cards/[cardId].ts
+++ pages/api/v1/databases/[id]/cards/[cardId].ts
@@ -4,7 +4,7 @@ import { onError, onNoMatch, requireApiKey } from 'lib/middleware';
 import { NextApiRequest, NextApiResponse } from 'next';
 import nc from 'next-connect';
 import { Card } from 'pages/api/v1/databases/interfaces';
-import { convertPageContentToMarkdown } from 'lib/utilities/strings';
+import { convertPageContentToMarkdown } from 'components/common/CharmEditor/CharmEditor';
 import { PageContent } from 'models';
 
 const handler = nc<NextApiRequest, NextApiResponse>({ onError, onNoMatch });
@@ -15,16 +15,16 @@ handler
 
 /**
  * @swagger
- * /databases/{databaseId}:
+ * /databases/{databaseId}/card/{cardId}:
  *   get:
- *     summary: Find database by ID
+ *     summary: Find card by ID
  *     responses:
  *       200:
  *         description: Summary of the database
  *         content:
  *            application/json:
  *              schema:
- *                $ref: '#/components/schemas/BoardPage'
+ *                $ref: '#/components/schemas/Card'
  */
 async function getCard (req: NextApiRequest, res: NextApiResponse) {
 
@@ -42,19 +42,29 @@ async function getCard (req: NextApiRequest, res: NextApiResponse) {
     return res.status(400).send({ error: 'Task not found' });
   }
 
-  const cardContent = (await prisma.block.findFirst({
-    where: {
-      type: 'charm_text',
-      parentId: card.id
-    }
-  })) ?? {};
+  const [board, cardPageContent] = await Promise.all([
+    prisma.block.findFirst({
+      where: {
+        // Parameter only added for documentation purposes. All cards linked to a root board
+        type: 'board',
+        id: card.rootId
+      }
+    }),
+    prisma.block.findFirst({
+      where: {
+        type: 'charm_text',
+        parentId: card.id
+      }
+    }) as any
+  ]);
 
   const markdown = '';
-
-  // convertPageContentToMarkdown(cardContent as PageContent, card.title);
+  // convertPageContentToMarkdown(cardPageContent, card.title);
 
   const cardToReturn: Card = {
     id: card.id,
+    createdAt: new Date(card.createdAt).toISOString(),
+    updatedAt: new Date(card.updatedAt).toISOString(),
     databaseId: card.rootId,
     title: card.title,
     content: markdown,
