0ae9fea0c22eaa77571d12fafdc90f29a321588f
melboudi <mo@anyahq.co>
2022-04-07 12:26:33 +0200

    Hard-code banned api-docs domain name as it messes up dependency tree

--- components/common/RouteGuard.tsx
+++ components/common/RouteGuard.tsx
@@ -13,7 +13,7 @@ import log from 'lib/log';
 // TODO: Discord login for /invite route
 
 // Pages shared to the public that don't require user login
-export const publicPages = ['/', 'invite', 'share', 'api-docs'];
+const publicPages = ['/', 'invite', 'share', 'api-docs'];
 
 export default function RouteGuard ({ children }: { children: ReactNode }) {
   const router = useRouter();
--- lib/spaces/index.ts
+++ lib/spaces/index.ts
@@ -1,7 +1,5 @@
 
-import { publicPages } from 'components/common/RouteGuard';
-
-export const DOMAIN_BLACKLIST = ['api', 'invite', 'login', 'signup', 'createWorkspace', 'share', 'images', 'join', ...publicPages];
+export const DOMAIN_BLACKLIST = ['api', 'invite', 'login', 'signup', 'createWorkspace', 'share', 'images', 'join', 'api-docs'];
 
 export function isSpaceDomain (domain: string) {
   return domain && !DOMAIN_BLACKLIST.includes(domain);
