ed4844fe03e045d2b4f9af5c82548a478e2c7a01
Matt Casey <mattwad@gmail.com>
2022-04-07 09:22:30 -0600

    fix: focalboard loses scroll position when opening a card (#330)
    * fix: focalboard loses scroll position when opening a card
    
    * ..
    
    * revert changes
    
    * .
    
    * .

--- lib/browser.ts
+++ lib/browser.ts
@@ -104,6 +104,12 @@ function scrollIntoViewIfNeededPolyfill (
   }
 }
 
+// source: https://github.com/vercel/next.js/discussions/18072
+// update URL without Next.js re-rendering the page
+export function silentlyUpdateURL (newUrl: string) {
+  window.history.replaceState({ ...window.history.state, as: newUrl, url: newUrl }, '', newUrl);
+}
+
 export function getCookie (name: string): string {
   const cookieMap = document.cookie.split(';').reduce<{ [key: string]: string }>((cookies, cookie) => {
     const _name = cookie.trim().split('=')[0];
