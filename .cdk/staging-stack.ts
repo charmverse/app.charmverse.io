fcb4685cfd8be9529dbc95eff075ec7f1360c2c2
Matt Casey <mattwad@gmail.com>
2022-04-13 17:26:30 -0600

    use route53 zone id

--- .cdk/staging-stack.ts
+++ .cdk/staging-stack.ts
@@ -82,7 +82,6 @@ export class CdkDeployStack extends Stack {
     // Create an Elastic Beanstalk environment to run the application
     const ebEnv = new elasticbeanstalk.CfnEnvironment(this, 'Environment', {
       environmentName: appName,
-      cnamePrefix: process.env.STACK,
       applicationName: ebApp.applicationName || appName,
       solutionStackName: '64bit Amazon Linux 2 v3.4.13 running Docker',
       optionSettings: optionSettingProperties,
@@ -97,13 +96,13 @@ export class CdkDeployStack extends Stack {
 
     new route53.ARecord(this, 'ARecord', {
       zone,
-      recordName: deploymentDomain,
+      recordName: deploymentDomain + '.',
       //target: route53.RecordTarget.fromAlias(new targets.ElasticBeanstalkEnvironmentEndpointTarget(ebEnv.attrEndpointUrl)),
       target: route53.RecordTarget.fromAlias({
         bind: (): route53.AliasRecordTargetConfig => ({
           dnsName: ebEnv.attrEndpointUrl,//`${process.env.STACK}.us-east-1.elasticbeanstalk.com`,
           // https://docs.aws.amazon.com/general/latest/gr/elasticbeanstalk.html
-          hostedZoneId: 'Z117KPS5GTRQ2G' // for us-east-1
+          hostedZoneId: 'Z09220931NGAZLIXPM7SF' // from Route 52 in AWS console
         })
       }),
     });
