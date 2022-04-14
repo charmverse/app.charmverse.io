5efcd8e8f034c9c69cbbca53cb6e32237ef70c32
melboudi <mo@anyahq.co>
2022-04-14 15:01:21 +0200

    Add tests for mapping custom properties

--- lib/public-api/errors.ts
+++ lib/public-api/errors.ts
@@ -1,4 +1,5 @@
 import { ISystemErrorInput, SystemError } from 'lib/utilities/errors';
+import { PageProperty } from './interfaces';
 
 export class PageNotFoundError extends SystemError {
 
@@ -58,6 +59,65 @@ export class SpaceNotFoundError extends SystemError {
       message: `Space with id '${id}' was not found`,
       errorType: 'Data not found',
       severity: 'warning'
+    });
+  }
+}
+
+/**
+ * Specify one or multiple invalid keys that were attempted when assigning custom properties
+ */
+export class InvalidCustomPropertyKeyError extends SystemError<UnsupportedKeyDetails> {
+
+  constructor (errorInfo: {key: string | string [], boardSchema: PageProperty []}) {
+
+    const allowedKeys = errorInfo.boardSchema.reduce((keys: string [], schema) => {
+      keys.push(schema.name);
+      return keys;
+    }, []);
+
+    const example = errorInfo.boardSchema.reduce((exampleObj: Record<string, any>, schema) => {
+      exampleObj[schema.name] = 'value';
+
+      if (schema.type === 'multiSelect' || schema.type === 'select') {
+        exampleObj[schema.name] = schema.options[0]?.value;
+      }
+
+      return exampleObj;
+    }, {});
+
+    const errorDetails: UnsupportedKeyDetails = {
+      allowedKeys,
+      unsupportedKeys: errorInfo.key instanceof Array ? errorInfo.key : [errorInfo.key],
+      example
+    };
+
+    const invalidKeyMessage = errorInfo.key instanceof Array ? `Key(s) '${errorInfo.key.join(', ')}' are invalid custom properties.` : `Key '${errorInfo.key} is an invalid custom property'`;
+
+    super({
+      message: invalidKeyMessage,
+      errorType: 'Invalid input',
+      severity: 'warning',
+      error: errorDetails
+    });
+  }
+}
+
+/**
+ * Used when an option does not exist in a list
+ */
+export class InvalidCustomPropertyValueError extends SystemError {
+
+  constructor (errorInfo: {key: string, value: any, boardSchema: PageProperty []}) {
+
+    const allowedValues = errorInfo.boardSchema.find(schema => schema.name === 'key')?.options.map(opt => opt.value) ?? [];
+
+    super({
+      message: `Value '${errorInfo.value}' is an invalid option for property ${errorInfo.key}`,
+      errorType: 'Invalid input',
+      severity: 'warning',
+      error: [
+        allowedValues
+      ]
     });
   }
 }
