e6e38b6eee2bb1f79cb50d8fd191b6b1e0c47450
Matt Casey <mattwad@gmail.com>
2022-04-13 18:12:58 -0600

    update alb

--- .cdk/staging-stack.ts
+++ .cdk/staging-stack.ts
@@ -53,6 +53,16 @@ export class CdkDeployStack extends Stack {
       },
       {
         namespace: 'aws:elbv2:listener:443',
+        optionName: 'Protocol',
+        value: 'HTTPS',
+      },
+      {
+        namespace: 'aws:elbv2:listener:443',
+        optionName: 'ListenerEnabled',
+        value: 'true',
+      },
+      {
+        namespace: 'aws:elbv2:listener:443',
         optionName: 'SSLCertificateArns',
         value: 'arn:aws:acm:us-east-1:310849459438:certificate/bfea3120-a440-4667-80fd-d285146f2339',
       },
