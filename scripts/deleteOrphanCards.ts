410e053bebcdd35249af3443f6ed7d621434197c
Matt Casey <mattwad@gmail.com>
2022-04-15 11:03:51 -0600

    simplify script

--- scripts/deleteOrphanCards.ts
+++ scripts/deleteOrphanCards.ts
@@ -1,78 +1,62 @@
 import { prisma } from 'db';
 import log from 'lib/log';
+import uniq from 'lodash/uniq';
 
-async function deleteOrphanCards () {
+async function init () {
   const cards = await prisma.block.findMany({
     where: {
       type: 'card'
     },
     select: {
-      parentId: true,
+      rootId: true,
       id: true
     }
   });
 
-  let totalOrphanCards = 0;
-  let index = 0;
-  let page = 0;
-  const cardsPerPage = 50;
+  const boardIds = uniq(cards.map(c => c.rootId));
+  const boardsInDb = await prisma.block.findMany({
+    where: {
+      id: {
+        in: boardIds
+      }
+    }
+  });
+  const deletedBoardIds = boardIds.filter(id => !boardsInDb.some(p => p.id === id));
+  const blocksToDelete = await prisma.block.findMany({
+    where: {
+      rootId: {
+        in: deletedBoardIds
+      }
+    },
+    select: {
+      id: true
+    }
+  });
 
-  for (;index < cards.length;) {
-    // eslint-disable-next-line
-    const orphanCards = await Promise.all<null | string>(new Array(cardsPerPage).fill(0).map((_, index) => {
-      return new Promise((resolve) => {
-        const card = cards[(page * cardsPerPage) + index];
-        if (card) {
-          prisma.block.findUnique({
-            where: {
-              id: cards[(page * cardsPerPage) + index].parentId
-            },
-            select: {
-              id: true
-            }
-          }).then((board) => {
-            if (!board) {
-              resolve(card.id);
-            }
-            else {
-              resolve(null);
-            }
-          });
-        }
-        else {
-          resolve(null);
-        }
-      });
-    }));
-    page += 1;
-    index += cardsPerPage;
-
-    const orphanCardDeletionPromises: Promise<any>[] = [];
-    orphanCards.forEach(orphanCard => {
-      // Delete the orphan charm_text and card block
-      if (orphanCard) {
-        orphanCardDeletionPromises.push(prisma.block.delete({
-          where: {
-            id: orphanCard
-          }
-        }));
-        orphanCardDeletionPromises.push(prisma.block.deleteMany({
-          where: {
-            type: 'charm_text',
-            parentId: orphanCard
-          }
-        }));
+  // sanity check!
+  if (deletedBoardIds.length > 0) {
+    const board = await prisma.block.findUnique({
+      where: {
+        id: deletedBoardIds[0]
       }
     });
-    await Promise.all(orphanCardDeletionPromises);
-
-    // eslint-disable-next-line
-    const orphans = orphanCards.filter(orphanCard => orphanCard)
-    totalOrphanCards += orphans.length;
-    log.debug(`Page: ${page}. Orphans: ${totalOrphanCards}/${page * cardsPerPage}`);
+    if (board) {
+      throw new Error(`Check the script! Found a board that is supposed to be deleted: ${deletedBoardIds[0]}`);
+    }
   }
 
-  log.debug('Total orphan cards ', totalOrphanCards);
+  console.log('cards', cards.length);
+  console.log('total boards', boardIds.length);
+  console.log('deleted boards', deletedBoardIds.length);
+  console.log('child blocks to delete', blocksToDelete.length);
+
+  // await prisma.block.deleteMany({
+  //   where: {
+  //     id: {
+  //       in: blocksToDelete.map(b => b.id)
+  //     }
+  //   }
+  // });
 }
 
-deleteOrphanCards();
+init();
