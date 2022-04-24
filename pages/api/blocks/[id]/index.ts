399042b00ab475b53720a95c46003ebc59615528
Safwan Shaheer <devorein00@gmail.com>
2022-04-24 20:13:49 +0600

    Recursively make all child pages/blocks of board/card not clearalive

--- pages/api/blocks/[id]/index.ts
+++ pages/api/blocks/[id]/index.ts
@@ -5,72 +5,104 @@ import { Block } from '@prisma/client';
 import { prisma } from 'db';
 import { onError, onNoMatch, requireUser } from 'lib/middleware';
 import { withSessionRoute } from 'lib/session/withSession';
-import { computeUserPagePermissions } from 'lib/permissions/pages/page-permission-compute';
 
 const handler = nc<NextApiRequest, NextApiResponse>({ onError, onNoMatch });
 
 handler.use(requireUser).delete(deleteBlock);
 
 async function deleteBlock (req: NextApiRequest, res: NextApiResponse<Block | {error: string}>) {
-  const blockId = req.query.id as string;
-  const blockType = await prisma.block.findUnique({
-    where: {
-      id: blockId
-    },
-    select: {
-      type: true
-    }
-  });
+  const parentId = req.query.id as string;
+  let childPageIds = [parentId];
+  const deletedChildPageIds: string[] = [];
 
-  async function _deleteBlock () {
-    const deleted = await prisma.block.update({
-      where: {
-        id: blockId
-      },
-      data: {
-        isAlive: false,
-        deletedAt: new Date()
-      }
-    });
-    await prisma.block.updateMany({
+  while (childPageIds.length !== 0) {
+    deletedChildPageIds.push(...childPageIds);
+    childPageIds = (await prisma.block.findMany({
       where: {
         OR: [
           {
-            rootId: blockId
+            parentId: {
+              in: childPageIds
+            }
           },
           {
-            parentId: blockId
+            rootId: {
+              in: childPageIds
+            }
           }
         ]
       },
-      data: {
-        isAlive: false,
-        deletedAt: new Date()
+      select: {
+        id: true
       }
-    });
+    })).map(childPage => childPage.id);
 
-    return deleted;
+    childPageIds.push(...(await prisma.page.findMany({
+      where: {
+        parentId: {
+          in: childPageIds
+        }
+      },
+      select: {
+        id: true
+      }
+    })).map(childPage => childPage.id));
+
+    childPageIds = Array.from(new Set(childPageIds));
   }
 
-  if (blockType?.type === 'card') {
-    // Check if the user has the permission to delete the card page
-    const permissionSet = await computeUserPagePermissions({
-      pageId: blockId,
-      userId: req.session.user.id as string
-    });
-
-    if (!permissionSet.delete) {
-      return res.status(401).json({
-        error: 'You are not allowed to perform this action'
-      });
+  const deleted = await prisma.block.update({
+    where: {
+      id: parentId
+    },
+    data: {
+      isAlive: false,
+      deletedAt: new Date()
     }
-    else {
-      await _deleteBlock();
+  });
+
+  await prisma.block.updateMany({
+    where: {
+      id: {
+        in: deletedChildPageIds
+      }
+    },
+    data: {
+      isAlive: false,
+      deletedAt: new Date()
     }
-  }
-  else {
-    return res.status(200).json(await _deleteBlock());
-  }
+  });
+
+  await prisma.page.updateMany({
+    where: {
+      id: {
+        in: deletedChildPageIds
+      }
+    },
+    data: {
+      isAlive: false,
+      deletedAt: new Date()
+    }
+  });
+
+  // if (blockType?.type === 'card') {
+  //   // Check if the user has the permission to delete the card page
+  //   const permissionSet = await computeUserPagePermissions({
+  //     pageId: blockId,
+  //     userId: req.session.user.id as string
+  //   });
+
+  //   if (!permissionSet.delete) {
+  //     return res.status(401).json({
+  //       error: 'You are not allowed to perform this action'
+  //     });
+  //   }
+  //   else {
+  //   }
+  // }
+  // else {
+  // }
+  return res.status(200).json(deleted);
 }
 
 export default withSessionRoute(handler);
