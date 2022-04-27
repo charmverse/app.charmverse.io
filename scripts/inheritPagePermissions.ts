0dc7fa581a98103092f9357963e303747cff2dc1
melboudi <mo@anyahq.co>
2022-04-27 13:07:39 +0200

    Update page import

--- scripts/inheritPagePermissions.ts
+++ scripts/inheritPagePermissions.ts
@@ -1,6 +1,6 @@
 import { prisma } from 'db';
 import { setupPermissionsAfterPageRepositioned } from 'lib/permissions/pages/triggers';
-import { IPageWithPermissions, resolveChildPages } from '../lib/pages/server';
+import { IPageWithPermissions, resolveChildPages } from 'lib/pages/server';
 
 async function recursiveRebuild (pageId: string | IPageWithPermissions, level = 0, sourcePageNumber = 0): Promise<true> {
   await setupPermissionsAfterPageRepositioned(pageId);
