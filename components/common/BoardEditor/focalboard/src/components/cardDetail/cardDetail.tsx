8d43425043492514ca523735bcadb299291dcb33
Matt Casey <mattwad@gmail.com>
2022-04-27 16:08:34 -0600

    remove title stuff

--- components/common/BoardEditor/focalboard/src/components/cardDetail/cardDetail.tsx
+++ components/common/BoardEditor/focalboard/src/components/cardDetail/cardDetail.tsx
@@ -2,11 +2,8 @@
 // See LICENSE.txt for license information.
 
 import DocumentPage from 'components/[pageId]/DocumentPage'
-import { useCallback, useEffect, useRef, useState, useMemo } from 'react'
+import { useCallback } from 'react'
 import { Card } from '../../blocks/card'
-import mutator from '../../mutator'
-import { Focusable } from '../../widgets/editable'
-import useImagePaste from './imagePaste'
 import { usePages } from 'hooks/usePages'
 import log from 'lib/log'
 import { Prisma, Page } from '@prisma/client';
@@ -20,39 +17,6 @@ type Props = {
 
 const CardDetail = (props: Props): JSX.Element|null => {
     const {card, readonly} = props
-
-    const [title, setTitle] = useState(card.title)
-    const [serverTitle, setServerTitle] = useState(card.title)
-    const titleRef = useRef<Focusable>(null)
-    const saveTitle = useCallback(() => {
-        if (title !== card.title) {
-            mutator.changeTitle(card.id, card.title, title)
-        }
-    }, [card.title, title])
-
-    const saveTitleRef = useRef<() => void>(saveTitle)
-    saveTitleRef.current = saveTitle
-
-    useImagePaste(card.id, card.fields.contentOrder, card.rootId)
-
-    useEffect(() => {
-        if (!title) {
-            titleRef.current?.focus()
-        }
-    }, [])
-
-    useEffect(() => {
-        if (serverTitle === title) {
-            setTitle(card.title)
-        }
-        setServerTitle(card.title)
-    }, [card.title, title])
-
-    useEffect(() => {
-        return () => {
-            saveTitleRef.current && saveTitleRef.current()
-        }
-    }, [])
 
     const { pages, setPages } = usePages();
 
