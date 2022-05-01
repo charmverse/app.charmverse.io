17549ad4207f4afaaa8905ab53eff452daed94ab
Matt Casey <mattwad@gmail.com>
2022-05-01 10:00:57 -0600

    Token gate fix (#425)
    * fix token gate to use ethereum for connection
    
    * ..
    
    * disable alert for lit protocol
    
    * add show message
    
    * better ui for showing test button
    
    * ..

--- components/common/Modal/Modal.tsx
+++ components/common/Modal/Modal.tsx
@@ -55,9 +55,9 @@ export function Modal ({ children, size = defaultSize, title, ...props }: ModalP
   );
 }
 
-export function DialogTitle ({ children, onClose }: { children: ReactNode, onClose?: () => void }) {
+export function DialogTitle ({ children, onClose, sx }: { children: ReactNode, onClose?: () => void, sx?: any }) {
   return (
-    <StyledDialogTitle>
+    <StyledDialogTitle sx={sx}>
       {children}
       {onClose && (
         <CloseButton onClick={onClose}>
