f2acfe45afdd1aad78811a9432197ccf17c40aa3
Safwan Shaheer <devorein00@gmail.com>
2022-05-02 21:51:07 +0600

    Removed custom filter logic and added necessary npm scripts

--- scripts/deleteArchivedPages.ts
+++ scripts/deleteArchivedPages.ts
@@ -5,60 +5,31 @@ import { DateTime } from 'luxon';
 
 const MAX_ARCHIVE_PAGE_DAYS = 30;
 
-// Filter all pages that are at least a month old
-function filterByDays (date: Date, geq: number) {
-  return DateTime.now().diff(DateTime.fromMillis(new Date(date).getTime()), 'days').toObject().days as number >= geq;
-}
-
 export async function main () {
   log.debug('[cron]: Starting delete-archived-pages cron job');
   // Cron job that runs every hour
-  cron.schedule('0 * * * *', async () => {
-    const pages = await prisma.page.findMany({
+  cron.schedule('*/30 * * * * *', async () => {
+    const { count: deletedPagesCount } = await prisma.page.deleteMany({
       where: {
         deletedAt: {
-          not: null
+          lte: DateTime.now().minus({
+            days: MAX_ARCHIVE_PAGE_DAYS
+          }).toISO()
         }
-      },
-      select: {
-        id: true,
-        deletedAt: true
       }
     });
 
-    const pagesToBeDeleted = pages.filter(({ deletedAt }) => deletedAt && filterByDays(deletedAt, MAX_ARCHIVE_PAGE_DAYS));
-
-    const blocks = await prisma.page.findMany({
+    const { count: deletedBlocksCount } = await prisma.block.deleteMany({
       where: {
         deletedAt: {
-          not: null
-        }
-      },
-      select: {
-        id: true,
-        deletedAt: true
-      }
-    });
-
-    const blocksToBeDeleted = blocks.filter(({ deletedAt }) => deletedAt && filterByDays(deletedAt, MAX_ARCHIVE_PAGE_DAYS));
-
-    await prisma.page.deleteMany({
-      where: {
-        id: {
-          in: pagesToBeDeleted.map(({ id }) => id)
+          lte: DateTime.now().minus({
+            days: MAX_ARCHIVE_PAGE_DAYS
+          }).toISO()
         }
       }
     });
 
-    await prisma.block.deleteMany({
-      where: {
-        id: {
-          in: blocksToBeDeleted.map(({ id }) => id)
-        }
-      }
-    });
-
-    // log.debug(`[cron]: Deleted ${pagesToBeDeleted.length} pages, ${blocksToBeDeleted.length} blocks`);
+    log.debug(`[cron]: Deleted ${deletedPagesCount} pages, ${deletedBlocksCount} blocks`);
   });
 }
 
