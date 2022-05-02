558c5e311848649314f2c8f2a09e5c0bfe087c38
melboudi <mo@anyahq.co>
2022-05-02 12:48:23 +0200

    Fix unit tests

--- lib/public-api/createDatabaseCardPage.ts
+++ lib/public-api/createDatabaseCardPage.ts
@@ -13,7 +13,7 @@ export async function createDatabase (boardInfo: Record<keyof Pick<Page, 'title'
     data: {
       id: boardId,
       title: 'Example title',
-      path: v4(),
+      path: `path-${v4()}`,
       type: 'board',
       contentText: '',
       boardId,
