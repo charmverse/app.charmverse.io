863d606f758094ad664c858cb64e926fcc4fd577
Matt Casey <mattwad@gmail.com>
2022-05-02 12:04:08 -0600

    ..

--- background/deleteArchived.ts
+++ background/deleteArchived.ts
@@ -5,9 +5,14 @@ import { deleteArchivedPages } from '../lib/pages/server/deleteArchivedPages';
 const MAX_ARCHIVE_DAYS = process.env.MAX_ARCHIVE_DAYS ? parseInt(process.env.MAX_ARCHIVE_DAYS) : 30;
 
 export async function main () {
-  log.debug('[cron]: Starting delete-archived-pages cron job');
+
+  log.debug('[cron]: Starting delete-archived cron job');
+
   // Cron job that runs every hour
   cron.schedule('0 * * * *', async () => {
+
+    log.debug('[cron]: Running delete-archived cron job');
+
     try {
       const { deletedPagesCount, deletedBlocksCount } = await deleteArchivedPages(MAX_ARCHIVE_DAYS);
       log.info(`[cron]: Deleted ${deletedPagesCount} pages, ${deletedBlocksCount} blocks`);
