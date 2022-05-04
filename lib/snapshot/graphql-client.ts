bb9b860e1040b70402b17690761adbc23901539c
melboudi <mo@anyahq.co>
2022-05-04 12:31:37 +0200

    Fix caching behaviour

--- lib/snapshot/graphql-client.ts
+++ lib/snapshot/graphql-client.ts
@@ -3,8 +3,14 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const client = new ApolloClient({
   uri: 'https://hub.snapshot.org/graphql',
-  cache: new InMemoryCache()
+  cache: new InMemoryCache({
+  })
 });
+
+// Every 15 seconds, clear the cached values in the client
+setInterval(() => {
+  client.clearStore();
+}, 15000);
 
 export default client;
 
