7a7554dbbe57f788787d89dad40275ac177a6795
Safwan Shaheer <devorein00@gmail.com>
2022-05-05 23:53:45 +0600

    Basic search text for archived pages

--- components/common/PageLayout/components/TrashModal.tsx
+++ components/common/PageLayout/components/TrashModal.tsx
@@ -1,4 +1,4 @@
-import { IconButton, List, MenuItem, ListItemText, ListItemIcon, Tooltip, Typography } from '@mui/material';
+import { IconButton, List, MenuItem, ListItemText, ListItemIcon, Tooltip, Typography, TextField } from '@mui/material';
 import { usePages } from 'hooks/usePages';
 import { ScrollableModal as Modal } from 'components/common/Modal';
 import { checkForEmpty } from 'components/common/CharmEditor/utils';
@@ -6,23 +6,97 @@ import { Page, PageContent } from 'models';
 import DeleteIcon from '@mui/icons-material/Delete';
 import RestoreIcon from '@mui/icons-material/Restore';
 import { useCurrentSpace } from 'hooks/useCurrentSpace';
-import { useMemo, useState } from 'react';
+import { MouseEvent, memo, useMemo, useState, useCallback } from 'react';
 import { DateTime } from 'luxon';
 import Link from 'next/link';
 import { fancyTrim } from 'lib/utilities/strings';
 import PageIcon from './PageIcon';
 
+const PageArchivedDate = memo<{date: Date, title: string}>(({ date, title }) => {
+  return (
+    <ListItemText secondary={DateTime.fromJSDate(new Date(date)).toRelative({ base: (DateTime.now()) })}>
+      {fancyTrim(title, 34) || 'Untitled'}
+    </ListItemText>
+  );
+});
+
+const ArchivedPageItem = memo<
+{
+  archivedPage: Page,
+  disabled: boolean,
+  onRestore:(e: MouseEvent<HTMLButtonElement, MouseEvent>, pageId: string) => void,
+  onDelete: (e: MouseEvent<HTMLButtonElement, MouseEvent>, pageId: string) => void
+    }>(({ onRestore, onDelete, disabled, archivedPage }) => {
+      const [space] = useCurrentSpace();
+      const isEditorEmpty = checkForEmpty(archivedPage.content as PageContent);
+      return (
+        <Link href={`/${space?.domain}/${archivedPage.path}`} passHref key={archivedPage.id}>
+          <MenuItem component='a' dense disabled={disabled} sx={{ pl: 4 }}>
+            <ListItemIcon sx={{ minWidth: 0, mr: 1 }}>
+              <PageIcon pageType={archivedPage.type} icon={archivedPage.icon} isEditorEmpty={isEditorEmpty} />
+            </ListItemIcon>
+            <PageArchivedDate date={archivedPage.deletedAt as Date} title={archivedPage.title} />
+            <div onClick={e => e.stopPropagation()}>
+              <IconButton
+                disabled={disabled}
+                size='small'
+                onClick={(e) => onRestore(e as any, archivedPage.id)}
+              >
+                <Tooltip arrow placement='top' title='Restore page'>
+                  <RestoreIcon color='info' fontSize='small' />
+                </Tooltip>
+              </IconButton>
+              <IconButton
+                disabled={disabled}
+                size='small'
+                onClick={(e) => onDelete(e as any, archivedPage.id)}
+              >
+                <Tooltip arrow placement='top' title='Delete page permanently'>
+                  <DeleteIcon color='error' fontSize='small' />
+                </Tooltip>
+              </IconButton>
+            </div>
+          </MenuItem>
+        </Link>
+      );
+    });
+
 export default function TrashModal ({ onClose, isOpen }: {onClose: () => void, isOpen: boolean}) {
   const { pages, deletePage, restorePage, getPagePermissions } = usePages();
-  const [space] = useCurrentSpace();
   const [isMutating, setIsMutating] = useState(false);
+  const [searchText, setSearchText] = useState('');
+  const archivedPages = useMemo(() => {
+    const _archivedPages: Page[] = [];
+    Object
+      .values(pages).forEach(page => {
+        if (page && page.deletedAt !== null && getPagePermissions(page.id).delete) {
+          const pageTitle = page.title || 'Untitled';
+          _archivedPages.push({ ...page, title: pageTitle });
+        }
+      });
 
-  const deletedPages = useMemo(() => (Object
-    .values(pages)
-    .filter(page => page && page.deletedAt !== null
-        && getPagePermissions(page.id).delete) as Page[]).sort((deletedPageA, deletedPageB) => deletedPageA.deletedAt && deletedPageB.deletedAt
-    ? new Date(deletedPageB.deletedAt).getTime() - new Date(deletedPageA.deletedAt).getTime()
-    : 0), [pages]);
+    return _archivedPages.sort((deletedPageA, deletedPageB) => deletedPageA.deletedAt && deletedPageB.deletedAt
+      ? new Date(deletedPageB.deletedAt).getTime() - new Date(deletedPageA.deletedAt).getTime()
+      : 0);
+  }, [pages]);
+
+  const searchTextMatchedPages = useMemo(() => {
+    return archivedPages.filter(archivedPage => archivedPage.title.toLowerCase().startsWith(searchText.toLowerCase()));
+  }, [archivedPages, searchText]);
+
+  const onRestorePage = useCallback(async (e: MouseEvent<HTMLButtonElement, MouseEvent>, pageId: string) => {
+    e.preventDefault();
+    setIsMutating(true);
+    await restorePage(pageId);
+    setIsMutating(false);
+  }, [isMutating]);
+
+  const onDeletePage = useCallback(async (e: MouseEvent<HTMLButtonElement, MouseEvent>, pageId: string) => {
+    e.preventDefault();
+    setIsMutating(true);
+    await deletePage(pageId);
+    setIsMutating(false);
+  }, [isMutating]);
 
   // Remove the pages you dont have delete access of
   return (
@@ -32,60 +106,43 @@ export default function TrashModal ({ onClose, isOpen }: {onClose: () => void, i
       title={(
         <>
           Trash
-          <Typography variant='body2' color='secondary'>{deletedPages.length} pages</Typography>
+          <Typography variant='body2' color='secondary'>{archivedPages.length} pages</Typography>
         </>
       )}
     >
-      {deletedPages.length === 0
+      {archivedPages.length === 0
         ? <Typography sx={{ pl: 4 }} variant='subtitle1' color='secondary'>No archived pages</Typography>
         : (
-          <List>
-            {deletedPages.map(deletedPage => {
-              const isEditorEmpty = checkForEmpty(deletedPage.content as PageContent);
-              return (
-                <Link href={`/${space?.domain}/${deletedPage.path}`} passHref key={deletedPage.id}>
-                  <MenuItem component='a' dense disabled={isMutating} sx={{ pl: 4 }}>
-                    <ListItemIcon sx={{ minWidth: 0, mr: 1 }}>
-                      <PageIcon pageType={deletedPage.type} icon={deletedPage.icon} isEditorEmpty={isEditorEmpty} />
-                    </ListItemIcon>
-                    <ListItemText secondary={DateTime.fromJSDate(new Date(deletedPage.deletedAt as Date)).toRelative({ base: (DateTime.now()) })}>
-                      {fancyTrim(deletedPage.title, 34) || 'Untitled'}
-                    </ListItemText>
-                    <div onClick={e => e.stopPropagation()}>
-                      <IconButton
-                        disabled={isMutating}
-                        size='small'
-                        onClick={async (e) => {
-                          e.preventDefault();
-                          setIsMutating(true);
-                          await restorePage(deletedPage.id);
-                          setIsMutating(false);
-                        }}
-                      >
-                        <Tooltip arrow placement='top' title='Restore page'>
-                          <RestoreIcon color='info' fontSize='small' />
-                        </Tooltip>
-                      </IconButton>
-                      <IconButton
-                        disabled={isMutating}
-                        size='small'
-                        onClick={async (e) => {
-                          e.preventDefault();
-                          setIsMutating(true);
-                          await deletePage(deletedPage.id);
-                          setIsMutating(false);
-                        }}
-                      >
-                        <Tooltip arrow placement='top' title='Delete page permanently'>
-                          <DeleteIcon color='error' fontSize='small' />
-                        </Tooltip>
-                      </IconButton>
-                    </div>
-                  </MenuItem>
-                </Link>
-              );
-            })}
-          </List>
+          <>
+            <TextField
+              fullWidth
+              sx={{
+                px: 2,
+                '& .MuiFormHelperText-root': {
+                  margin: 0,
+                  marginTop: 0.5,
+                  opacity: 0.5,
+                  fontWeight: 500
+                }
+              }}
+              value={searchText}
+              onChange={(e) => setSearchText(e.target.value)}
+              helperText='Type to filter archived pages by their title'
+            />
+            <List>
+              {searchTextMatchedPages.map((archivedPage) => {
+                return (
+                  <ArchivedPageItem
+                    key={archivedPage.id}
+                    archivedPage={archivedPage}
+                    disabled={isMutating}
+                    onDelete={onDeletePage}
+                    onRestore={onRestorePage}
+                  />
+                );
+              })}
+            </List>
+          </>
         )}
     </Modal>
   );
