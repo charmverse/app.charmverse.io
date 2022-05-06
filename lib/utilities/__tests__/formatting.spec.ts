749e7fcc78ec74cf337244d2669871806a38a38a
AndreiMitrea <andrei.mitrea.hq@gmail.com>
2022-05-06 14:46:03 +0300

    Updated crypto price format (#447)
    * Updated crypto price format
    
    * Updated jest config test environment
    
    * Added jest docblock
    
    * Mocked window object
    
    * Removed test file using window object, as it is run with server config
    
    * Moved window object reference outside utility function
    
    * Added formatting test file
    
    * Fixed failing test
    
    * Code review change

---
+++ lib/utilities/__tests__/formatting.spec.ts
@@ -0,0 +1,17 @@
+import { formatMoney } from '../formatting';
+
+describe('amount formatting', () => {
+
+  it('should be correct for values larger than 1', () => {
+    const amount = formatMoney(2932.12345, 'USD', 'en-US');
+
+    expect(amount).toBe('$2,932.12');
+  });
+
+  it('should be correct for values less than 1', () => {
+    const amount = formatMoney(0.12345, 'USD', 'en-US');
+
+    expect(amount).toBe('$0.1235');
+  });
+
+});
