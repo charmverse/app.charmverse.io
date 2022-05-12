fa12bdd5c17e00a8779db8c4e266bdfe750e3435
Safwan Shaheer <devorein00@gmail.com>
2022-05-12 11:56:03 +0600

    Using Promise.all for resolving all guild roles from all addresses of user

--- lib/guild-xyz/assignGuildRolesForSpace.ts
+++ lib/guild-xyz/assignGuildRolesForSpace.ts
@@ -39,33 +39,34 @@ export async function assignGuildRolesForSpace (spaceId: string) {
     }
   });
 
-  const guildImportedRoleIdsInWorkspace = new Set(rolesImportedFromGuild.map(roleImportedWithGuild => roleImportedWithGuild.sourceId as string));
-
   for (const spaceRole of spaceRoles) {
     try {
       const addresses = spaceRole.user.addresses;
       // Only proceed further if the user has at least a single address
       if (addresses.length > 0) {
-        const guildRoleIdsSet: Set<string> = new Set();
+        const guildRoleIds: string[] = [];
         // Get all the guild roles associated with all of the addresses of the user
-        for (const address of addresses) {
-          const guildMemberships = await user.getMemberships(address);
-          guildMemberships?.forEach(guildMembership => {
-            guildMembership.roleids.forEach(roleid => guildRoleIdsSet.add(String(roleid)));
+        const guildMembershipsResponses = await Promise.all(addresses.map(address => user.getMemberships(address)));
+        guildMembershipsResponses.forEach(guildMembershipsResponse => {
+          guildMembershipsResponse?.forEach(guildMemberships => {
+            guildRoleIds.push(...guildMemberships.roleids.map(String));
           });
-        }
+        });
 
+        const charmverseRoleIds: string[] = [];
         // Filter the roles that are part of the workspace only
-        const roleIds = Array.from(guildRoleIdsSet).filter(
-          guildRoleId => guildImportedRoleIdsInWorkspace.has(guildRoleId)
-        ).map(
-          guildRoleId => guildRoleIdCharmverseRoleIdRecord[guildRoleId]
+        guildRoleIds.forEach(
+          guildRoleId => {
+            if (guildRoleIdCharmverseRoleIdRecord[guildRoleId]) {
+              charmverseRoleIds.push(guildRoleIdCharmverseRoleIdRecord[guildRoleId]);
+            }
+          }
         );
 
-        await prisma.$transaction(roleIds.map(roleId => prisma.spaceRoleToRole.upsert({
+        await prisma.$transaction(charmverseRoleIds.map(charmverseRoleId => prisma.spaceRoleToRole.upsert({
           where: {
             spaceRoleId_roleId: {
-              roleId,
+              roleId: charmverseRoleId,
               spaceRoleId: spaceRole.id
             }
           },
@@ -73,7 +74,7 @@ export async function assignGuildRolesForSpace (spaceId: string) {
           create: {
             role: {
               connect: {
-                id: roleId
+                id: charmverseRoleId
               }
             },
             spaceRole: {
