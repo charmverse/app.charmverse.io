c38bd82a754dfa1441f0749f4418ab9ee1743c4e
Safwan Shaheer <devorein00@gmail.com>
2022-05-12 22:52:36 +0600

    Used basic autocomplete

--- components/settings/roles/components/ImportGuildRolesMenuItem.tsx
+++ components/settings/roles/components/ImportGuildRolesMenuItem.tsx
@@ -1,47 +1,50 @@
 import { useEffect, useMemo, useState } from 'react';
 import { ScrollableModal } from 'components/common/Modal';
 import { guild, user } from '@guildxyz/sdk';
-import { Avatar, Box, Checkbox, ListItem, ListItemIcon, ListItemText, MenuItem, Typography } from '@mui/material';
-import Link from 'components/common/Link';
+import { Autocomplete, Box, MenuItem, TextField, Typography } from '@mui/material';
 import charmClient from 'charmClient';
 import { useCurrentSpace } from 'hooks/useCurrentSpace';
 import { mutate } from 'swr';
 import { useUser } from 'hooks/useUser';
-import { FixedSizeList } from 'react-window';
 import GuildXYZIcon from 'public/images/guild_logo.svg';
 import { useSnackbar } from 'hooks/useSnackbar';
 import { PimpedButton, StyledSpinner } from '../../../common/Button';
 
+interface GuildInfo {
+  id: number
+  name: string
+  urlName: string
+  roles: any[]
+  imageUrl: string
+}
+
 export default function ImportGuildRolesMenuItem ({ onClose }: {onClose: () => void}) {
   const [showImportedRolesModal, setShowImportedRolesModal] = useState(false);
-  const [guilds, setGuilds] = useState<{id: number, name: string, urlName: string, roles: any[], imageUrl: string}[]>([]);
+  const [guilds, setGuilds] = useState<GuildInfo[]>([]);
   const [fetchingGuilds, setFetchingGuilds] = useState(false);
   const [importingRoles, setImportingRoles] = useState(false);
   const [selectedGuildIds, setSelectedGuildIds] = useState<number[]>([]);
   const [space] = useCurrentSpace();
-  const selectedGuildIdsSet = useMemo(() => new Set(selectedGuildIds), [selectedGuildIds]);
   const [currentUser] = useUser();
   const addresses = currentUser?.addresses ?? [];
   const { showMessage } = useSnackbar();
+  const guildIdRecord: Record<string, GuildInfo> = useMemo(() => guilds.reduce((cur, _guild) => ({ ...cur, [_guild.id]: _guild }), {}), [guilds]);
 
   useEffect(() => {
     async function main () {
       if (showImportedRolesModal) {
         setFetchingGuilds(true);
         const guildMembershipsResponses = await Promise.all(addresses.map(address => user.getMemberships(address)));
-        const guildIds: number[] = [];
+        const userGuildIds: number[] = [];
 
         guildMembershipsResponses.forEach(guildMembershipsResponse => {
           if (guildMembershipsResponse) {
-            guildIds.push(...guildMembershipsResponse.map(guildMembership => guildMembership.guildId));
+            userGuildIds.push(...guildMembershipsResponse.map(guildMembership => guildMembership.guildId));
           }
         });
-        const fetchedGuilds = await Promise.all(guildIds?.map(guildId => guild.get(guildId)));
-
-        setGuilds(fetchedGuilds);
-        setSelectedGuildIds(fetchedGuilds.map(fetchedGuild => fetchedGuild.id));
-        // const allGuilds = await guild.getAll();
-        // setGuilds(allGuilds);
+        const allGuilds = await guild.getAll();
+        setSelectedGuildIds(userGuildIds);
+        setGuilds(allGuilds);
         setFetchingGuilds(false);
       }
     }
@@ -97,78 +100,28 @@ export default function ImportGuildRolesMenuItem ({ onClose }: {onClose: () => v
                   paddingRight: 1
                 }}
                 >
-                  <FixedSizeList
-                    height={325}
-                    width='100%'
-                    itemSize={65}
-                    itemCount={guilds.length}
-                    overscanCount={5}
-                  >
-                    {({ index, style }) => {
-                      const _guild = guilds[index];
-                      return (
-                        <ListItem
-                          style={style}
-                          key={_guild.id}
-                          sx={{
-                            display: 'flex',
-                            pl: 0
-                          }}
-                        >
-                          <Checkbox
-                            disabled={importingRoles}
-                            checked={selectedGuildIdsSet.has(_guild.id)}
-                            onClick={(event) => {
-                              if ((event.target as any).checked) {
-                                setSelectedGuildIds([...selectedGuildIds, _guild.id]);
-                              }
-                              else {
-                                setSelectedGuildIds(selectedGuildIds.filter(selectedGuildId => selectedGuildId !== _guild.id));
-                              }
-                            }}
-                          />
-                          <Box sx={{
-                            width: '100%'
-                          }}
-                          >
-                            <Link external target='_blank' href={`https://guild.xyz/${_guild.urlName}`}>
-                              <MenuItem
-                                component='div'
-                                disabled={importingRoles}
-                              >
-                                <ListItemIcon sx={{ mr: 1 }}>
-                                  <Avatar sx={{ width: 32, height: 32 }} src={_guild.imageUrl?.startsWith('/') ? `https://guild.xyz${_guild.imageUrl}` : _guild.imageUrl} />
-                                </ListItemIcon>
-                                <ListItemText
-                                  secondary={_guild.urlName}
-                                  sx={{
-                                    '& .MuiTypography-root': {
-                                      display: 'flex',
-                                      justifyContent: 'space-between',
-                                      flexDirection: 'row'
-                                    }
-                                  }}
-                                >
-                                  {_guild.name}
-                                  <Box display='flex'>
-                                    <Typography variant='subtitle2' color='secondary'>
-                                      {_guild.roles.length} Role(s)
-                                    </Typography>
-                                  </Box>
-                                </ListItemText>
-                              </MenuItem>
-                            </Link>
-                          </Box>
-                        </ListItem>
-                      );
+                  <Autocomplete<GuildInfo, true>
+                    multiple
+                    options={guilds}
+                    getOptionLabel={(option) => option.name}
+                    value={selectedGuildIds.map(selectedGuildId => guildIdRecord[selectedGuildId])}
+                    filterSelectedOptions
+                    onChange={(_, guildInfos) => {
+                      setSelectedGuildIds(guildInfos.map(guildInfo => guildInfo.id));
                     }}
-                  </FixedSizeList>
+                    renderInput={(params) => (
+                      <TextField
+                        {...params}
+                        placeholder='Type Guild name...'
+                      />
+                    )}
+                  />
                   <PimpedButton
                     loading={importingRoles}
                     sx={{
                       mt: 2
                     }}
-                    disabled={importingRoles}
+                    disabled={importingRoles || selectedGuildIds.length === 0}
                     onClick={importRoles}
                   >Import Roles
                   </PimpedButton>
