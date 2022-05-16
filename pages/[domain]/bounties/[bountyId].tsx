1f2dbcf399d8a24171f2ada6864169a93e5bb756
melboudi <mo@anyahq.co>
2022-05-16 14:22:46 +0200

    Add conditional to switch between old and new bounty

--- pages/[domain]/bounties/[bountyId].tsx
+++ pages/[domain]/bounties/[bountyId].tsx
@@ -1,15 +1,39 @@
 import Box from '@mui/material/Box';
+import Switch from '@mui/material/Switch';
 import BountyDetails from 'components/bounties/[bountyId]/BountyDetails';
+import BountyDetailsNew from 'components/bounties/[bountyId]/BountyDetails_v3';
 import PageLayout from 'components/common/PageLayout';
 import { setTitle } from 'hooks/usePageTitle';
-import { ReactElement } from 'react';
+import { ReactElement, useState, useEffect } from 'react';
+import ScrollableWindow from 'components/common/PageLayout/components/ScrollableWindow';
+import { useRouter } from 'next/router';
+import { useBounties } from 'hooks/useBounties';
 
 export default function BountyPage () {
 
+  const { updateCurrentBountyId } = useBounties();
+
   setTitle('Bounties');
+  const router = useRouter();
+
+  // TEMPORARY
+  const [showNew, setShowNew] = useState(false);
+
+  useEffect(() => {
+    updateCurrentBountyId((router.query.bountyId as string) ?? null);
+
+    console.log('Updating id', router.query.bountyId);
+  }, [router.query.bountyId]);
 
   return (
-    <BountyDetails />
+    <>
+      <Switch onChange={(ev) => setShowNew(ev.target.checked)} />
+      {
+        showNew ? <BountyDetailsNew /> : <BountyDetails />
+      }
+
+    </>
+
   );
 
 }
@@ -17,7 +41,9 @@ export default function BountyPage () {
 BountyPage.getLayout = (page: ReactElement) => {
   return (
     <PageLayout>
-      {page}
+      <ScrollableWindow>
+        {page}
+      </ScrollableWindow>
     </PageLayout>
   );
 };
