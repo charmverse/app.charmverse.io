84078678947af84c95445dce4413fccadd50d127
melboudi <mo@anyahq.co>
2022-05-20 14:42:58 +0200

    Add approve application tests

--- lib/applications/actions/__tests__/approveApplication.spec.ts
+++ lib/applications/actions/__tests__/approveApplication.spec.ts
@@ -6,7 +6,7 @@ import { v4 } from 'uuid';
 import { ExpectedAnError } from 'testing/errors';
 import { UserIsNotSpaceMemberError } from 'lib/users/errors';
 import { DataNotFoundError, InvalidInputError, UnauthorisedActionError, LimitReachedError, PositiveNumbersOnlyError, DuplicateDataError, StringTooShortError } from 'lib/utilities/errors';
-import { createBounty } from 'lib/bounties/createBounty';
+import { createBounty, updateBountySettings } from 'lib/bounties';
 import { createApplication } from '../createApplication';
 import { MINIMUM_APPLICATION_MESSAGE_CHARACTERS } from '../../shared';
 import { approveApplication } from '../approveApplication';
@@ -64,6 +64,75 @@ describe('approveApplication', () => {
       userId: user.id,
       applicationOrApplicationId: application
     });
+
+  });
+
+  it('should fail to approve the application if the user is not a reviewer or space admin', async () => {
+
+    const { user: nonAdminUser, space: nonAdminSpace } = await generateUserAndSpaceWithApiToken(undefined, false);
+
+    const bounty = await createBounty({
+      title: 'My bounty',
+      createdBy: nonAdminUser.id,
+      spaceId: nonAdminSpace.id,
+      // Random person
+      reviewer: null
+    });
+
+    const application = await createApplication({
+      bountyId: bounty.id,
+      message: 'My application message',
+      userId: user.id
+    });
+
+    try {
+      await approveApplication({
+        userId: nonAdminUser.id,
+        applicationOrApplicationId: application
+      });
+      throw new ExpectedAnError();
+    }
+    catch (error) {
+      expect(error).toBeInstanceOf(UnauthorisedActionError);
+    }
+
+  });
+
+  it('should fail to approve the application if the limit of active submissions has been reached', async () => {
+
+    const { user: adminUser, space: adminSpace } = await generateUserAndSpaceWithApiToken(undefined, true);
+
+    const bounty = await createBounty({
+      title: 'My bounty',
+      createdBy: adminUser.id,
+      spaceId: adminSpace.id,
+      // Random person
+      reviewer: null
+    });
+
+    const application = await createApplication({
+      bountyId: bounty.id,
+      message: 'My application message',
+      userId: adminUser.id
+    });
+
+    await updateBountySettings({
+      bountyId: bounty.id,
+      updateContent: {
+        maxSubmissions: 0
+      }
+    });
+
+    try {
+      await approveApplication({
+        userId: adminUser.id,
+        applicationOrApplicationId: application
+      });
+      throw new ExpectedAnError();
+    }
+    catch (error) {
+      expect(error).toBeInstanceOf(LimitReachedError);
+    }
 
   });
 
--- lib/applications/actions/approveApplication.ts
+++ lib/applications/actions/approveApplication.ts
@@ -1,9 +1,12 @@
-import { Application } from '@prisma/client';
-import { DataNotFoundError, UnauthorisedActionError } from 'lib/utilities/errors';
+import { Application, Bounty } from '@prisma/client';
+import { DataNotFoundError, LimitReachedError, UnauthorisedActionError } from 'lib/utilities/errors';
 import { hasAccessToSpace } from 'lib/users/hasAccessToSpace';
 import { prisma } from 'db';
+import { getBounty } from 'lib/bounties';
 import { ApplicationActionRequest } from '../interfaces';
 import { getApplication } from '../getApplication';
+import { submissionsCapReached } from '../shared';
+import { BountyWithDetails } from '../../../models';
 
 export async function approveApplication ({ applicationOrApplicationId, userId }: ApplicationActionRequest): Promise<Application> {
   const application = await getApplication(typeof applicationOrApplicationId === 'string' ? applicationOrApplicationId : applicationOrApplicationId.id);
@@ -22,6 +25,14 @@ export async function approveApplication ({ applicationOrApplicationId, userId }
   // Admin or reviewer can assign the application
   if (isAdmin === false && application.bounty.reviewer !== userId) {
     throw new UnauthorisedActionError('You do not have permissions to approve this application');
+  }
+
+  const bounty = await getBounty(application.bountyId) as BountyWithDetails;
+
+  const capReached = submissionsCapReached({ bounty, submissions: bounty.applications });
+
+  if (capReached) {
+    throw new LimitReachedError(`This application cannot be approved as the limit of active submissions of ${bounty.maxSubmissions} has been reached.`);
   }
 
   const updated = await prisma.application.update({
