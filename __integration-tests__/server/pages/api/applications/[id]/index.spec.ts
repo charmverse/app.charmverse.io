8c60294d7e393ec1c7d0bad0393c80a9f9e5e9a5
melboudi <mo@anyahq.co>
2022-05-20 02:47:04 +0200

    Update API to use lib method, implement tests

---
+++ __integration-tests__/server/pages/api/applications/[id]/index.spec.ts
@@ -0,0 +1,102 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Application, Bounty, Space, User } from '@prisma/client';
+import request from 'supertest';
+import { baseUrl } from 'testing/mockApiCall';
+import { generateUserAndSpaceWithApiToken } from 'testing/setupDatabase';
+import { ApplicationCreationData, ApplicationUpdateData } from 'lib/applications/interfaces';
+import { createBounty } from 'lib/bounties';
+import { createApplication } from 'lib/applications/actions';
+
+let nonAdminUser: User;
+let nonAdminUserSpace: Space;
+let nonAdminCookie: string;
+
+beforeAll(async () => {
+  const generated = await generateUserAndSpaceWithApiToken(undefined, false);
+
+  nonAdminUser = generated.user;
+  nonAdminUserSpace = generated.space;
+  nonAdminCookie = (await request(baseUrl)
+    .post('/api/session/login')
+    .send({
+      address: nonAdminUser.addresses[0]
+    })).headers['set-cookie'][0];
+});
+
+describe('PUT /api/applications/{applicationId} - update an application', () => {
+
+  it('should update the application and respond with 200', async () => {
+
+    const bounty = await createBounty({
+      createdBy: nonAdminUser.id,
+      spaceId: nonAdminUserSpace.id,
+      title: 'Example title',
+      status: 'open',
+      rewardAmount: 1
+    });
+
+    const creationContent: Partial<ApplicationCreationData> = {
+      bountyId: bounty.id,
+      message: "I'm volunteering for this as it's in my field of expertise"
+    };
+
+    const createdApplication = (await request(baseUrl)
+      .post('/api/applications')
+      .set('Cookie', nonAdminCookie)
+      .send(creationContent)
+      .expect(201)).body;
+
+    const update: Partial<ApplicationUpdateData> = {
+      message: 'This is my new application message'
+    };
+
+    const updatedApplication = (await request(baseUrl)
+      .put(`/api/applications/${createdApplication.id}`)
+      .set('Cookie', nonAdminCookie)
+      .send(update)
+      .expect(200)).body;
+
+    expect(updatedApplication).toEqual(
+      expect.objectContaining<Partial<Application>>({
+        ...creationContent,
+        message: update.message
+      })
+    );
+
+  });
+
+  it('should reject attempts to update an application that does not belong to the requester, and respond with 401', async () => {
+
+    const { user: otherUser, space: otherSpace } = await generateUserAndSpaceWithApiToken();
+
+    const bounty = await createBounty({
+      createdBy: otherUser.id,
+      spaceId: otherSpace.id,
+      title: 'Example title',
+      status: 'open',
+      rewardAmount: 1
+    });
+
+    const creationContent: Partial<ApplicationCreationData> = {
+      bountyId: bounty.id,
+      message: "I'm volunteering for this as it's in my field of expertise"
+    };
+
+    // Use method here so we don't have to generate a second cookie
+    const createdApplication = await createApplication({
+      ...creationContent,
+      userId: otherUser.id
+    } as ApplicationCreationData);
+
+    const update: Partial<ApplicationUpdateData> = {
+      message: 'This is my new application message'
+    };
+
+    await request(baseUrl)
+      .put(`/api/applications/${createdApplication.id}`)
+      .set('Cookie', nonAdminCookie)
+      .send(update)
+      .expect(401);
+  });
+
+});
--- pages/api/applications/[id]/index.ts
+++ pages/api/applications/[id]/index.ts
@@ -1,68 +1,44 @@
 
-import { Application, Prisma } from '@prisma/client';
+import { Application } from '@prisma/client';
 import { prisma } from 'db';
-import { onError, onNoMatch, requireUser } from 'lib/middleware';
+import { updateApplication } from 'lib/applications/actions/updateApplication';
+import { onError, onNoMatch, requireKeys, requireUser } from 'lib/middleware';
 import { withSessionRoute } from 'lib/session/withSession';
+import { DataNotFoundError, UnauthorisedActionError } from 'lib/utilities/errors';
 import { NextApiRequest, NextApiResponse } from 'next';
 import nc from 'next-connect';
-import { getApplication } from 'lib/applications/getApplication';
-import { DataNotFoundError, UnauthorisedActionError } from 'lib/utilities/errors';
 
 const handler = nc<NextApiRequest, NextApiResponse>({ onError, onNoMatch });
 
-handler.use(requireUser).put(updateApplication);
+handler.use(requireUser)
+  .use(requireKeys<Application>(['message'], 'body'))
+  .put(updateApplicationController);
 
-async function updateApplication (req: NextApiRequest, res: NextApiResponse<Application>) {
+async function updateApplicationController (req: NextApiRequest, res: NextApiResponse<Application>) {
+
   const { id } = req.query;
 
-  if (id === undefined) {
-    return res.status(400).send({ error: 'Please provide a valid application ID' } as any);
-  }
-
-  const body = req.body as Partial<Application>;
-
-  const applicationWithBounty = await getApplication(id as string);
-
-  if (!applicationWithBounty) {
-    throw new DataNotFoundError(`Application with ID ${id} not found`);
-  }
-
-  const bounty = applicationWithBounty.bounty;
-
-  if (bounty.approveSubmitters === true && applicationWithBounty.status === 'applied') {
-    throw new UnauthorisedActionError('Your application must have been accepted in order to make a submission');
-  }
-
-  const pageContent = typeof body.submissionNodes === 'string' ? body.submissionNodes
-    : typeof body.submissionNodes === 'object' ? JSON.stringify(body.submissionNodes) : undefined;
-
-  // Prevent accidental nulling of these fields
-  const updateContent: Prisma.ApplicationUpdateInput = {
-    message: body.message ?? undefined,
-    walletAddress: body.walletAddress ?? undefined,
-    submission: body.submission ?? undefined,
-    submissionNodes: pageContent,
-    // Auto progress an in progress application to review
-    status: applicationWithBounty.status === 'inProgress' ? 'review' : undefined
-  };
-
-  let updatedApplication = await prisma.application.update({
+  const existingApplicationWithIdsOnly = await prisma.application.findFirst({
     where: {
       id: id as string
     },
-    data: updateContent
+    select: {
+      createdBy: true
+    }
   });
 
-  if (updatedApplication.status === 'inProgress') {
-    updatedApplication = await prisma.application.update({
-      where: {
-        id: id as string
-      },
-      data: {
-        status: 'review'
-      }
-    });
+  if (!existingApplicationWithIdsOnly) {
+    throw new DataNotFoundError(`Application with id ${id} was not found`);
   }
+
+  if (existingApplicationWithIdsOnly.createdBy !== req.session.user.id) {
+    throw new UnauthorisedActionError('You cannot edit another user\'s application');
+  }
+
+  const updatedApplication = await updateApplication({
+    applicationId: id as string,
+    message: req.body.message
+  });
 
   return res.status(200).json(updatedApplication);
 }
