05febe139f1e8465ce790674ab7e37fa0379ec81
melboudi <mo@anyahq.co>
2022-05-20 21:58:49 +0200

    Implement submission update API endpoint

--- lib/applications/actions/index.ts
+++ lib/applications/actions/index.ts
@@ -1,4 +1,5 @@
 export * from './createSubmission';
+export * from './updateSubmission';
 export * from './reviewSubmission';
 export * from './listSubmissions';
 export * from './createApplication';
