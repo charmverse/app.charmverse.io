6fa06a8ccb8615b6991ac9d947b57e21f24435e6
melboudi <mo@anyahq.co>
2022-05-27 00:27:58 +0200

    Fix tests for list submissions

--- lib/applications/actions/__tests__/listSubmissions.spec.ts
+++ lib/applications/actions/__tests__/listSubmissions.spec.ts
@@ -1,6 +1,7 @@
 import { Space, User } from '@prisma/client';
-import { generateBountyWithSingleApplication, generateTransaction, generateUserAndSpaceWithApiToken } from 'testing/setupDatabase';
+import { generateBountyWithSingleApplication, generateSpaceUser, generateTransaction, generateUserAndSpaceWithApiToken } from 'testing/setupDatabase';
 import { v4 } from 'uuid';
+import { prisma } from 'db';
 import { listSubmissions } from '../listSubmissions';
 
 let user: User;
@@ -13,9 +14,68 @@ beforeAll(async () => {
 });
 
 describe('listSubmissions', () => {
-  it('Should retrieve transaction for a submission', async () => {
+
+  it('Should only return inProgress, review, complete or paid status submissions (not applied or rejected)', async () => {
     const bounty = await generateBountyWithSingleApplication({
       applicationStatus: 'applied',
+      bountyCap: 1,
+      userId: user.id,
+      bountyStatus: 'open',
+      spaceId: space.id
+    });
+
+    const [user0, user1, user2, user3, user4, user5] = await Promise.all([0, 1, 2, 3, 4, 5].map(() => generateSpaceUser({
+      spaceId: space.id,
+      isAdmin: false
+    })));
+
+    await prisma.application.createMany({
+      data: [
+        {
+          bountyId: bounty.id,
+          createdBy: user0.id,
+          status: 'applied'
+        },
+        {
+          bountyId: bounty.id,
+          createdBy: user1.id,
+          status: 'inProgress'
+        },
+        {
+          bountyId: bounty.id,
+          createdBy: user2.id,
+          status: 'review'
+        },
+        {
+          bountyId: bounty.id,
+          createdBy: user3.id,
+          status: 'complete'
+        },
+        {
+          bountyId: bounty.id,
+          createdBy: user4.id,
+          status: 'paid'
+        },
+        {
+          bountyId: bounty.id,
+          createdBy: user5.id,
+          status: 'rejected'
+        }
+      ]
+    });
+
+    const submissionsWithTransaction = await listSubmissions(bounty.id);
+    expect(submissionsWithTransaction.length).toBe(4);
+
+    const subStatusApplied = submissionsWithTransaction.find(sub => sub.status === 'applied');
+    const subStatusRejected = submissionsWithTransaction.find(sub => sub.status === 'rejected');
+
+    expect(subStatusApplied).toBeUndefined();
+    expect(subStatusRejected).toBeUndefined();
+  });
+  it('Should retrieve transaction for a submission', async () => {
+    const bounty = await generateBountyWithSingleApplication({
+      applicationStatus: 'complete',
       bountyCap: 1,
       userId: user.id,
       bountyStatus: 'open',
