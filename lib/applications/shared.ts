7815b975e8a838a419153ab2a92c7fd7358f311e
Safwan Shaheer <devorein00@gmail.com>
2022-05-27 02:05:07 +0600

    Removed extra from moveUserApplicationToFirstRow

--- lib/applications/shared.ts
+++ lib/applications/shared.ts
@@ -1,5 +1,4 @@
 import { Application, Bounty } from '@prisma/client';
-import { ApplicationWithTransactions } from './actions';
 
 export const MINIMUM_APPLICATION_MESSAGE_CHARACTERS = 10;
 
@@ -24,7 +23,7 @@ export function submissionIsEditable ({ submission, bounty }: {submission: Appli
   return true;
 }
 
-export function moveUserApplicationToFirstRow<Data extends ApplicationWithTransactions | Application> (submissions: Data[], userId: string): Data[] {
+export function moveUserApplicationToFirstRow<Data extends Application> (submissions: Data[], userId: string): Data[] {
 
   const copiedSubmissions = submissions.slice();
 
