95b70f2748e6c7967c3cc5fef788ba954d41136f
Matt Casey <mattwad@gmail.com>
2022-05-26 10:24:11 -0600

    remove script

--- scripts/getENSName.ts
+++
@@ -1,8 +0,0 @@
-import { ethers } from 'ethers';
-
-const providerUrl = `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`;
-const provider = new ethers.providers.JsonRpcProvider(providerUrl);
-(async () => {
-  const result = await provider.lookupAddress('0x66525057AC951a0DB5C9fa7fAC6E056D6b8997E2');
-  console.log(result);
-})();
