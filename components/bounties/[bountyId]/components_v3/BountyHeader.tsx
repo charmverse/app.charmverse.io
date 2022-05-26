fb67258f061bfe29155ca0021de6c277d8bf3232
melboudi <mo@anyahq.co>
2022-05-26 13:32:36 +0200

    Setup popup inside header

--- components/bounties/[bountyId]/components_v3/BountyHeader.tsx
+++ components/bounties/[bountyId]/components_v3/BountyHeader.tsx
@@ -3,6 +3,14 @@ import EditIcon from '@mui/icons-material/Edit';
 import Box from '@mui/material/Box';
 import IconButton from '@mui/material/IconButton';
 import Tooltip from '@mui/material/Tooltip';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import ListItemIcon from '@mui/material/ListItemIcon';
+// import Menu from '@mui/icons-material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import Menu, { MenuProps } from '@mui/material/Menu';
+import ListItemText from '@mui/material/ListItem';
+import DuplicateIcon from '@mui/icons-material/ContentCopy';
+
 import Typography from '@mui/material/Typography';
 import BountyDelete from 'components/bounties/components/BountyDelete';
 import BountyModal from 'components/bounties/components/BountyModal';
@@ -10,7 +18,18 @@ import BountyStatusBadge from 'components/bounties/components/BountyStatusBadge'
 import Modal from 'components/common/Modal';
 import { useBounties } from 'hooks/useBounties';
 import useIsAdmin from 'hooks/useIsAdmin';
-import { usePopupState } from 'material-ui-popup-state/hooks';
+import { usePopupState, bindTrigger, bindMenu } from 'material-ui-popup-state/hooks';
+
+const menuPosition: Partial<MenuProps> = {
+  anchorOrigin: {
+    horizontal: 'right',
+    vertical: 'bottom'
+  },
+  transformOrigin: {
+    vertical: 'top',
+    horizontal: 'left'
+  }
+};
 
 export default function BountyHeader () {
   const { currentBounty } = useBounties();
@@ -19,6 +38,8 @@ export default function BountyHeader () {
 
   const bountyEditModal = usePopupState({ variant: 'popover', popupId: 'edit-bounty' });
   const bountyDeleteModal = usePopupState({ variant: 'popover', popupId: 'delete-bounty' });
+
+  const popupState = usePopupState({ variant: 'popover', popupId: 'bounty-actions' });
 
   const viewerCanModifyBounty = isAdmin === true;
 
@@ -65,6 +86,22 @@ export default function BountyHeader () {
           )
         }
           </Typography>
+
+          <MoreHorizIcon color='secondary' {...bindTrigger(popupState)} />
+
+          <Menu
+            {...bindMenu(popupState)}
+            {...menuPosition}
+          >
+            <MenuItem onClick={() => {}}>
+              <ListItemIcon><DeleteIcon color='secondary' /></ListItemIcon>
+              <ListItemText>Delete</ListItemText>
+            </MenuItem>
+            <MenuItem onClick={() => {}}>
+              <ListItemIcon><DuplicateIcon color='secondary' fontSize='small' /></ListItemIcon>
+              <ListItemText>Duplicate</ListItemText>
+            </MenuItem>
+          </Menu>
         </Box>
         <Box sx={{
           display: 'flex',
