28ad2df8c17fefb01f239a0c3d55cc5d48aae8eb
Matt Casey <mattwad@gmail.com>
2022-05-27 20:16:16 -0600

    fix widths of modals

--- components/common/Modal/SuccessModal.tsx
+++ components/common/Modal/SuccessModal.tsx
@@ -9,7 +9,7 @@ export default function SuccessModal ({ title, ...props }: { title?: string } & 
       <DialogTitle onClose={props.onClose} sx={{ padding: 0 }}>
         <Box display='flex' gap={1} width={300} alignItems='center'>
           <CheckCircleOutlineIcon color='success' fontSize='large' />
-          {typeof title === 'undefined' ? 'Success' : title}
+          {typeof title === 'undefined' ? 'Success' : 'title'}
         </Box>
       </DialogTitle>
     </Modal>
