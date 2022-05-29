209b74cd061dff88c40f329003032720451ed071
Safwan Shaheer <devorein00@gmail.com>
2022-05-29 22:52:51 +0600

    Replaced confirm with modal when removing contributor

--- components/settings/contributors/ContributorList.tsx
+++ components/settings/contributors/ContributorList.tsx
@@ -1,9 +1,14 @@
-import { Table, TableHead, TableRow, TableCell, TableBody } from '@mui/material';
+import { Table, TableHead, TableRow, TableCell, TableBody, Typography, Box } from '@mui/material';
 import { Contributor } from 'models';
 import { useContributors } from 'hooks/useContributors';
 import charmClient from 'charmClient';
-import Legend from '../Legend';
+import { bindMenu, usePopupState } from 'material-ui-popup-state/hooks';
+import Modal, { DialogTitle } from 'components/common/Modal';
+import { useState } from 'react';
+import { getDisplayName } from 'lib/users';
+import Button from 'components/common/Button';
 import ContributorListItem, { RoleAction } from './ContributorListItem';
+import Legend from '../Legend';
 
 interface Props {
   isAdmin: boolean;
@@ -12,8 +17,18 @@ interface Props {
 }
 
 export default function ContributorList ({ isAdmin, spaceId, spaceOwner }: Props) {
-
+  const popupState = usePopupState({ variant: 'popover', popupId: 'contributor-list' });
   const [contributors, setContributors] = useContributors();
+  const [removedContributorId, setRemovedContributorId] = useState<string | null>(null);
+
+  const removedContributor = removedContributorId ? contributors.find(contributor => contributor.id === removedContributorId) : null;
+
+  const closed = popupState.close;
+
+  popupState.close = () => {
+    setRemovedContributorId(null);
+    closed();
+  };
 
   async function updateContributor (action: RoleAction, contributor: Contributor) {
     switch (action) {
@@ -29,11 +44,8 @@ export default function ContributorList ({ isAdmin, spaceId, spaceOwner }: Props
         break;
 
       case 'removeFromSpace':
-        if (!window.confirm('Please confirm you want to remove this person')) {
-          return;
-        }
-        await charmClient.removeContributor({ spaceId, userId: contributor.id });
-        setContributors(contributors.filter(c => c.id !== contributor.id));
+        setRemovedContributorId(contributor.id);
+        popupState.open();
         break;
 
       default:
@@ -65,6 +77,29 @@ export default function ContributorList ({ isAdmin, spaceId, spaceOwner }: Props
           ))}
         </TableBody>
       </Table>
+      {removedContributor && (
+      <Modal {...bindMenu(popupState)}>
+        <DialogTitle onClose={popupState.close}>Remove from space</DialogTitle>
+        <Typography>
+          {`Are you sure you want to remove ${getDisplayName(removedContributor)} from space?`}
+        </Typography>
+        <Box component='div' sx={{ columnSpacing: 2, mt: 3 }}>
+          <Button
+            color='error'
+            sx={{ mr: 2, fontWeight: 'bold' }}
+            onClick={async () => {
+              await charmClient.removeContributor({ spaceId, userId: removedContributorId as string });
+              setContributors(contributors.filter(c => c.id !== removedContributorId));
+              setRemovedContributorId(null);
+            }}
+          >
+            {`Remove ${getDisplayName(removedContributor)}`}
+          </Button>
+
+          <Button color='secondary' onClick={popupState.close}>Cancel</Button>
+        </Box>
+      </Modal>
+      )}
     </>
   );
 }
