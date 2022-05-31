df24968c94e22b826dfc781f7347c4643f4c75d7
Matt Casey <mattwad@gmail.com>
2022-05-30 22:58:47 -0600

    Clean up nexus header (#536)
    * ..
    
    * .
    
    * bring account back
    
    * add standard header
    
    * standard header
    
    * fix header
    
    * use var
    
    * ..
    
    * fix links to root page
    
    * hide tabs for task types for now
    
    * show icons at md
    
    * remove color of divider
    
    * Fixed react key error
    
    Co-authored-by: Safwan Shaheer <devorein00@gmail.com>

--- components/profile/tasks/GnosisTasksList.tsx
+++ components/profile/tasks/GnosisTasksList.tsx
@@ -32,7 +32,6 @@ function TransactionRow (
   const [expanded, setExpanded] = useState(false);
   const isReadyToExecute = transaction.confirmations?.length === transaction.threshold;
   const isFirstTask = transaction.nonce === firstNonce;
-
   return (
     <>
       <Grid justifyContent='space-between' alignItems='center' container key={transaction.id} sx={{ width: '100%', height: rowHeight }} onClick={() => setExpanded(!expanded)}>
@@ -105,7 +104,7 @@ function TransactionRow (
           <Grid container spacing={1}>
             <Grid item xs={5}>
               {transaction.actions.map(action => (
-                <Box py={1}>
+                <Box py={1} key={action.to.address}>
                   <Typography
                     gutterBottom
                   >
@@ -147,7 +146,7 @@ function TransactionRow (
             <Grid item xs={5} pr={1}>
               <Typography color='secondary' gutterBottom variant='body2'>Confirmations</Typography>
               {transaction.confirmations.map(confirmation => (
-                <Box py={1}>
+                <Box py={1} key={confirmation.address}>
                   {confirmation.user ? (
                     <UserDisplay
                       sx={{
@@ -179,7 +178,7 @@ function TransactionRow (
               ))}
               {transaction.snoozedUsers.length !== 0 ? <Typography sx={{ mt: 2 }} color='secondary' gutterBottom variant='body2'>Snoozed</Typography> : null}
               {transaction.snoozedUsers.map(snoozedUser => (
-                <Box py={1} display='flex' justifyContent='space-between'>
+                <Box key={snoozedUser.id} py={1} display='flex' justifyContent='space-between'>
                   <UserDisplay avatarSize='small' user={snoozedUser} />
                   <Box display='flex' gap={1} alignItems='center'>
                     {snoozedUser.gnosisSafeState?.transactionsSnoozeMessage && (
