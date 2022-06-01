94e9051c7892cc6403d675f99c798e36a015048f
Matt Casey <mattwad@gmail.com>
2022-06-01 10:32:00 -0600

    install dev tools from zip file

--- lib/users/getDisplayName.ts
+++ lib/users/getDisplayName.ts
@@ -4,7 +4,7 @@ import { PublicUser } from 'pages/api/public/profile/[userPath]/index';
 
 export function getDisplayName (user?: User | LoggedInUser | PublicUser) {
   if (!user) return '';
-  console.log('user', user);
+
   return (user as LoggedInUser).ensName
     || user.username
     || ((user as User).addresses && shortenHex((user as User).addresses[0]));
