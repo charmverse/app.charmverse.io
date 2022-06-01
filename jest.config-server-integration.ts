89b76cd453c0ae18ca094d3cabbe71f70bf407ed
Safwan Shaheer <devorein00@gmail.com>
2022-06-01 22:37:22 +0600

    Reverted back server integration testMatch

--- jest.config-server-integration.ts
+++ jest.config-server-integration.ts
@@ -5,7 +5,7 @@ import { jestConfig } from './jest.config-server';
 const integrationConfig = {
   ...jestConfig,
   testMatch: [
-    '**/__integration-tests__/server/pages/api/spaces/[id]/set-default-page-permissions.spec.ts'
+    '**/__integration-tests__/server/**/?(*.)+(spec|test).[tj]s?(x)'
   ]
 };
 
