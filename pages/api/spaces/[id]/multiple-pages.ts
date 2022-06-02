9230ecd88747d50bf882360c5c7df54fa4f3a3c6
Safwan Shaheer <devorein00@gmail.com>
2022-06-03 00:51:23 +0600

    Fetch pages first before computing their permissions

--- pages/api/spaces/[id]/multiple-pages.ts
+++ pages/api/spaces/[id]/multiple-pages.ts
@@ -5,38 +5,48 @@ import { onError, onNoMatch, requireUser } from 'lib/middleware';
 import { withSessionRoute } from 'lib/session/withSession';
 import { Page } from '@prisma/client';
 import { computeUserPagePermissions } from 'lib/permissions/pages';
-import { getPage, IPageWithPermissions } from 'lib/pages/server';
+import { IPageWithPermissions } from 'lib/pages/server';
+import { prisma } from 'db';
 
 const handler = nc<NextApiRequest, NextApiResponse>({ onError, onNoMatch });
 
 handler.use(requireUser).post(getPagesByIds);
 
 async function getPagesByIds (req: NextApiRequest, res: NextApiResponse<Page[]>) {
-  const spaceId = req.query.id as string;
   const userId = req.session?.user?.id;
   const { pageIds } = req.body as {pageIds: string[]};
+  const pages = await prisma.page.findMany({
+    where: {
+      id: {
+        in: pageIds
+      }
+    },
+    include: {
+      permissions: {
+        include: {
+          sourcePermission: true
+        }
+      }
+    }
+  });
 
-  const pages = (await Promise.all(pageIds.map((pageId) => new Promise<IPageWithPermissions | null>((resolve) => {
-    getPage(pageId, spaceId).then((page) => {
-      computeUserPagePermissions({
-        pageId,
-        userId
-      }).then((permissions) => {
-        if (permissions.read) {
-          resolve(page);
-        }
-        else {
-          resolve(null);
-        }
-      }).catch(() => {
+  const accessiblePages = (await Promise.all(pages.map((page) => new Promise<IPageWithPermissions | null>((resolve) => {
+    computeUserPagePermissions({
+      pageId: page.id,
+      userId
+    }).then((permissions) => {
+      if (permissions.read) {
+        resolve(page);
+      }
+      else {
         resolve(null);
-      });
+      }
     }).catch(() => {
       resolve(null);
     });
   })))).filter(page => page) as IPageWithPermissions[];
 
-  return res.status(200).json(pages);
+  return res.status(200).json(accessiblePages);
 }
 
 export default withSessionRoute(handler);
