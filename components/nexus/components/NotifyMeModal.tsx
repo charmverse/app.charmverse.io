3a7f493c71226179991999a1d2f95a747fee67ab
Matt Casey <mattwad@gmail.com>
2022-06-02 09:44:46 -0600

    Request email (#545)
    * refactor snooze button
    
    * save name
    
    * add email to schema
    
    * add buttons
    
    * ..
    
    * hide Remove
    
    * change path to email
    
    * remove type field

---
+++ components/nexus/components/NotifyMeModal.tsx
@@ -0,0 +1,83 @@
+import { useEffect } from 'react';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+import { useForm } from 'react-hook-form';
+import { Box, FormHelperText, Stack } from '@mui/material';
+import Button from 'components/common/Button';
+import TextField from '@mui/material/TextField';
+import { Modal } from 'components/common/Modal';
+
+export const schema = yup.object({
+  email: yup.string().ensure().trim()
+    .email()
+    .max(50)
+});
+
+export type FormValues = yup.InferType<typeof schema>;
+
+type Props = {
+  currentValue: string | null | undefined,
+  save: (path: string) => void,
+  close: () => void,
+  isOpen: boolean,
+};
+
+export default function NotifyMeModal (props: Props) {
+  const { currentValue, close, isOpen, save } = props;
+
+  const {
+    register,
+    reset,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<FormValues>({
+    defaultValues: {
+      email: currentValue || ''
+    },
+    resolver: yupResolver(schema)
+  });
+
+  useEffect(() => {
+    reset({
+      email: currentValue || ''
+    });
+  }, [currentValue]);
+
+  function onSubmit (values: FormValues) {
+    save(values.email);
+  }
+
+  function removeNotifications () {
+    save('');
+  }
+
+  return (
+    <Modal open={isOpen} onClose={close} title='Receive daily notifications (beta)'>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <FormHelperText sx={{ mb: 1 }}>
+          Input your email to receive daily notifications.
+          <br />Note: Your email is private and not used for anything else
+        </FormHelperText>
+
+        <TextField
+          {...register('email')}
+          fullWidth
+          error={!!errors.email}
+          helperText={errors.email?.message}
+          placeholder='me@gmail.com'
+          sx={{ mb: 2 }}
+        />
+        <Box display='flex' gap={1}>
+          <Button type='submit'>
+            Save
+          </Button>
+          {currentValue && (
+            <Button onClick={removeNotifications} color='secondary' variant='outlined'>
+              Remove
+            </Button>
+          )}
+        </Box>
+      </form>
+    </Modal>
+  );
+}
