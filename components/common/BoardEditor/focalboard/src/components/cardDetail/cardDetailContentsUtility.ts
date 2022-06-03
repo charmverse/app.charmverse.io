ff103fbdcf066585284198ad2e4214c5445d18c2
Matt Casey <mattwad@gmail.com>
2022-06-03 00:20:48 -0600

    fix it

--- components/common/BoardEditor/focalboard/src/components/cardDetail/cardDetailContentsUtility.ts
+++ components/common/BoardEditor/focalboard/src/components/cardDetail/cardDetailContentsUtility.ts
@@ -1,6 +1,6 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
-type Position = 'left' | 'right' | 'above' | 'below' | 'aboveRow' | 'belowRow'
+export type Position = 'left' | 'right' | 'above' | 'below' | 'aboveRow' | 'belowRow'
 
 type DragAndDropRearrangeData = {
     contentOrder: Array<string|string[]>,
