779686446a8db7904a026f578e88853d10e57698
Matt Casey <mattwad@gmail.com>
2022-06-03 16:19:44 -0600

    update cron

--- background/cron.ts
+++ background/cron.ts
@@ -5,9 +5,8 @@ import { task as archiveTask } from './tasks/deleteArchivedPages';
 
 log.debug('Starting cron jobs');
 
-// Cron job that runs every hour
+// Delete archived pages once an hour
 cron.schedule('0 * * * *', archiveTask);
 
-// Cron job that runs at 12pm EST every day
-// cron.schedule('0 16 * * *', notificationTask);
-cron.schedule('*/10 * * * *', notificationTask); // every 5 minutes for testing
+// Send user task notifications by email
+cron.schedule('0 * * * *', notificationTask);
