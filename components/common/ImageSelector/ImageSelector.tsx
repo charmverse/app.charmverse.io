b5f02e7af7e7944ca63d040769386908265e90c6
Matt Casey <mattwad@gmail.com>
2022-06-05 12:30:05 -0600

    automatically open image inserts

--- components/common/ImageSelector/ImageSelector.tsx
+++ components/common/ImageSelector/ImageSelector.tsx
@@ -27,65 +27,67 @@ export default function ImageSelector ({ children, galleryImages, onImageSelect 
   }
 
   return (
-    <PopperPopup popupContent={(
-      <Box sx={{
-        width: 750
-      }}
-      >
-        <MultiTabs tabs={[
-          ...tabs,
-          [
-            'Upload',
-            <Box sx={{
-              display: 'flex',
-              justifyContent: 'center',
-              width: '100%'
-            }}
-            >
-              <Button component='label' variant='contained'>
-                Choose an image
-                <input
-                  type='file'
-                  hidden
-                  accept='image/*'
-                  onChange={async (e) => {
-                    const firstFile = e.target.files?.[0];
-                    if (firstFile) {
-                      const { url } = await uploadToS3(firstFile);
-                      onImageSelect(url);
-                    }
-                  }}
-                />
-              </Button>
-            </Box>
-          ],
-          [
-            'Link',
-            <Box sx={{
-              display: 'flex',
-              flexDirection: 'column',
-              gap: 2,
-              alignItems: 'center'
-            }}
-            >
-              <TextField autoFocus placeholder='Paste the image link...' value={embedLink} onChange={(e) => setEmbedLink(e.target.value)} />
-              <Button
-                disabled={!embedLink}
-                sx={{
-                  width: 250
-                }}
-                onClick={() => {
-                  onImageSelect(embedLink);
-                  setEmbedLink('');
-                }}
+    <PopperPopup
+      autoOpen
+      popupContent={(
+        <Box sx={{
+          width: 750
+        }}
+        >
+          <MultiTabs tabs={[
+            ...tabs,
+            [
+              'Upload',
+              <Box sx={{
+                display: 'flex',
+                justifyContent: 'center',
+                width: '100%'
+              }}
               >
-                Embed Image
-              </Button>
-            </Box>
-          ]
-        ]}
-        />
-      </Box>
+                <Button component='label' variant='contained'>
+                  Choose an image
+                  <input
+                    type='file'
+                    hidden
+                    accept='image/*'
+                    onChange={async (e) => {
+                      const firstFile = e.target.files?.[0];
+                      if (firstFile) {
+                        const { url } = await uploadToS3(firstFile);
+                        onImageSelect(url);
+                      }
+                    }}
+                  />
+                </Button>
+              </Box>
+            ],
+            [
+              'Link',
+              <Box sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                gap: 2,
+                alignItems: 'center'
+              }}
+              >
+                <TextField autoFocus placeholder='Paste the image link...' value={embedLink} onChange={(e) => setEmbedLink(e.target.value)} />
+                <Button
+                  disabled={!embedLink}
+                  sx={{
+                    width: 250
+                  }}
+                  onClick={() => {
+                    onImageSelect(embedLink);
+                    setEmbedLink('');
+                  }}
+                >
+                  Embed Image
+                </Button>
+              </Box>
+            ]
+          ]}
+          />
+        </Box>
   )}
     >
       {children}
