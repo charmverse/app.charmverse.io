21affe069c49a8b79939d0f72ec63ccb56b87983
melboudi <mo@anyahq.co>
2022-06-08 15:16:11 +0200

    Implement createThread method

---
+++ lib/threads/createThread.ts
@@ -0,0 +1,75 @@
+import { prisma } from 'db';
+import { DataNotFoundError, InvalidInputError } from 'lib/utilities/errors';
+import { ThreadCreate, ThreadWithComments } from './interfaces';
+
+export async function createThread ({ comment, pageId, userId, context }: ThreadCreate): Promise<ThreadWithComments> {
+
+  if (!comment) {
+    throw new InvalidInputError('Please provide a valid comment');
+  }
+
+  if (!context) {
+    throw new InvalidInputError('Please provide a valid context');
+  }
+
+  const existingPage = await prisma.page.findUnique({
+    where: {
+      id: pageId
+    },
+    select: {
+      id: true,
+      spaceId: true
+    }
+  });
+
+  if (!existingPage) {
+    throw new DataNotFoundError(`Cannot create thread as linked page with id ${pageId} was not found.`);
+  }
+
+  const thread = await prisma.thread.create({
+    data: {
+      context,
+      resolved: false,
+      page: {
+        connect: {
+          id: existingPage.id
+        }
+      },
+      space: {
+        connect: {
+          id: existingPage.spaceId as string
+        }
+      },
+      user: {
+        connect: {
+          id: userId
+        }
+      },
+      comments: {
+        create: {
+          content: comment,
+          page: {
+            connect: {
+              id: existingPage.id
+            }
+          },
+          space: {
+            connect: {
+              id: existingPage.spaceId as string
+            }
+          },
+          user: {
+            connect: {
+              id: userId
+            }
+          }
+        }
+      }
+    },
+    include: {
+      comments: true
+    }
+  });
+
+  return thread;
+}
--- lib/threads/interfaces.ts
+++ lib/threads/interfaces.ts
@@ -1,3 +1,4 @@
+import { Thread, Comment } from '@prisma/client';
 
 export enum ThreadStatus {
   open,
@@ -9,4 +10,18 @@ export type ThreadStatusType = keyof typeof ThreadStatus
 export interface ThreadStatusUpdate {
   id: string;
   status: ThreadStatusType
+}
+
+/**
+ * @context Prosemirror content for knowing where to display this thread inside the CharmEditor
+ */
+export interface ThreadCreate {
+  comment: string;
+  pageId: string;
+  userId: string;
+  context: string;
+}
+
+export interface ThreadWithComments extends Thread {
+  comments: Comment[]
 }
