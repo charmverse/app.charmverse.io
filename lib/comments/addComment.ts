de600003da1ad59ef9ce0eb66388ed6a582cebad
melboudi <mo@anyahq.co>
2022-06-08 14:04:45 +0200

    Implement addComment lib method

---
+++ lib/comments/addComment.ts
@@ -0,0 +1,62 @@
+import { prisma } from 'db';
+import { DataNotFoundError, InvalidInputError } from 'lib/utilities/errors';
+import { CommentCreate, CommentWithUser } from './interfaces';
+
+export async function addComment ({ content, threadId, userId }: CommentCreate): Promise<CommentWithUser> {
+
+  if (!content) {
+    throw new InvalidInputError('Please provide non-empty content to create a comment');
+  }
+
+  const thread = await prisma.thread.findUnique({
+    where: {
+      id: threadId
+    },
+    include: {
+      user: true,
+      page: true
+    }
+  });
+
+  if (!thread) {
+    throw new DataNotFoundError(`Thread with id ${threadId} not found`);
+  }
+
+  if (!thread.page) {
+    throw new DataNotFoundError(`Linked page for ${threadId} not found`);
+  }
+
+  const commentMessage = typeof content === 'object' ? JSON.stringify(content) : content;
+
+  const createdComment = await prisma.comment.create({
+    data: {
+      content: commentMessage,
+      thread: {
+        connect: {
+          id: threadId
+        }
+      },
+      user: {
+        connect: {
+          id: userId
+        }
+      },
+      space: {
+        connect: {
+          id: thread.spaceId
+        }
+      },
+      page: {
+        connect: {
+          id: thread.pageId
+        }
+      }
+
+    },
+    include: {
+      user: true
+    }
+  });
+
+  return createdComment;
+}
--- lib/comments/interfaces.ts
+++ lib/comments/interfaces.ts
@@ -1,5 +1,7 @@
 import { Comment, User } from '@prisma/client';
 
+export type CommentCreate = Pick<Comment, 'content' | 'threadId' | 'userId'>
+
 export type CommentUpdate = Pick<Comment, 'content' | 'id'>
 
 export interface CommentWithUser extends Comment {
