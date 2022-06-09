19e7ea0abe1df8181a48c97009af56120745037d
AndreiMitrea <andrei.mitrea@imbuelabs.com>
2022-06-09 14:21:54 +0300

    Replaced code

--- components/profile/components/PoapSection.tsx
+++ components/profile/components/PoapSection.tsx
@@ -35,7 +35,7 @@ function PoapSection (props: PoapSectionProps) {
   const { openWalletSelectorModal } = useContext(Web3Connection);
   const isPublic = isPublicUser(user);
   const { data: poapData, mutate: mutatePoaps } = useSWRImmutable(`/poaps/${user.id}`, () => {
-    return isPublicUser(user) ? Promise.resolve({ visiblePoaps: user.visiblePoaps || [], hiddenPoaps: [] }) : charmClient.getUserPoaps();
+    return isPublic ? Promise.resolve({ visiblePoaps: user.visiblePoaps || [], hiddenPoaps: [] }) : charmClient.getUserPoaps();
   });
 
   const hasConnectedWallet: boolean = !isPublic && user.addresses.length !== 0;
