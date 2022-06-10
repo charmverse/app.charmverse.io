e6007c02272178cd8e6cf80c5e1614cf4e6a248a
Matt Casey <mattwad@gmail.com>
2022-06-10 01:52:47 -0600

    remove log

--- components/common/PageLayout/components/CurrentPageFavicon.tsx
+++ components/common/PageLayout/components/CurrentPageFavicon.tsx
@@ -5,6 +5,5 @@ import Favicon from './Favicon';
 export default function CurrentPageFavicon () {
   const { currentPageId, pages } = usePages();
   const currentPage = pages[currentPageId];
-  console.log('current page', currentPage);
   return <Favicon pageIcon={currentPage?.icon} />;
 }
