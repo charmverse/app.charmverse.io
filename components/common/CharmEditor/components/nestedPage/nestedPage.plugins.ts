4dbc3b76c260d5d4e99703f0017d40620f6208ff
Safwan Shaheer <devorein00@gmail.com>
2022-06-24 16:52:06 +0600

    Drag and drop support for linked pages (#638)
    * d&d working for linked pages draging downwards
    
    * d&d working for linked pages draging upwards
    
    * Insert node rather than replacing it to not consume spaces
    
    * Moved drag & drop plugin into its own component
    
    * Refactored code to have same logic when moving upwards or downwards
    
    * Disable draggable for image nodes
    
    * Fixed creating extra space when dragging downwards

--- components/common/CharmEditor/components/nestedPage/nestedPage.plugins.ts
+++ components/common/CharmEditor/components/nestedPage/nestedPage.plugins.ts
@@ -1,8 +1,8 @@
 import { createTooltipDOM, SuggestTooltipRenderOpts } from '@bangle.dev/tooltip';
-import { Plugin, PluginKey } from '@bangle.dev/pm';
+import { NodeSelection, Plugin, PluginKey, TextSelection } from '@bangle.dev/pm';
 import { insertNestedPage } from 'lib/prosemirror/insertNestedPage';
 import { NodeView } from '@bangle.dev/core';
-import { nestedPageSuggestMarkName } from './nestedPage.constants';
+import { nestedPageNodeName, nestedPageSuggestMarkName } from './nestedPage.constants';
 import { NestedPagePluginState } from './nestedPage.interfaces';
 import * as suggestTooltip from '../@bangle.dev/tooltip/suggest-tooltip';
 
@@ -37,6 +37,36 @@ export function nestedPagePlugins ({
           },
           apply (_, pluginState) {
             return pluginState;
+          }
+        }
+      }), new Plugin({
+        key: new PluginKey('nestedPage-drop'),
+        props: {
+          handleDOMEvents: {
+            dragend (view, event) {
+              const nodeSelection = (view.state.selection as NodeSelection);
+              const draggedNode = nodeSelection.node;
+              if (event && draggedNode?.type.name === nestedPageNodeName) {
+                const draggedNodeStartPos = nodeSelection.ranges[0].$from.pos;
+                const draggedNodeEndPos = nodeSelection.ranges[0].$to.pos;
+                const containerXOffset = (event?.target as Element)?.getBoundingClientRect().left;
+                const clientX = event.clientX as number;
+                const left = (clientX - containerXOffset) < 50 ? clientX + 50 : clientX;
+                const ob = view.posAtCoords({ left, top: event.clientY });
+                if (ob) {
+                  const destinationPos = ob.inside > 0 ? ob.inside : ob.pos;
+                  const node = view.state.doc.nodeAt(destinationPos);
+                  if (node) {
+                    view.dispatch(view.state.tr.deleteRange(draggedNodeStartPos, draggedNodeEndPos)
+                      .insert(draggedNodeStartPos < destinationPos ? destinationPos - 1 : destinationPos, draggedNode));
+                    view.dispatch(view.state.tr.setSelection(
+                      TextSelection.near(view.state.tr.doc.resolve(draggedNodeStartPos < destinationPos ? destinationPos - 1 : destinationPos))
+                    ));
+                  }
+                }
+              }
+              return true;
+            }
           }
         }
       }),
