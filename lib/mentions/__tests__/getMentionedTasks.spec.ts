6c7025b420b6b39a85677a6c8a7534c82bc73be9
Safwan Shaheer <devorein00@gmail.com>
2022-06-27 18:03:57 +0600

    Fixed tests for getMentionedTasks lib module

--- lib/mentions/__tests__/getMentionedTasks.spec.ts
+++ lib/mentions/__tests__/getMentionedTasks.spec.ts
@@ -1,20 +1,18 @@
-import { Page, Space, User } from '@prisma/client';
+import { Space, User } from '@prisma/client';
 import { createPage, generateUserAndSpaceWithApiToken } from 'testing/setupDatabase';
 import { v4 } from 'uuid';
 import { prisma } from 'db';
 import { getMentionedTasks } from '../getMentionedTasks';
-import { MentionedTask } from '../interfaces';
 
 let user1: User;
 let user2: User;
 let space1: Space;
 let space2: Space;
-let page1: Page;
-let page2: Page;
 
 const mention1Id = v4();
 const mention2Id = v4();
 const mention3Id = v4();
+const mention4Id = v4();
 
 beforeAll(async () => {
   const generated1 = await generateUserAndSpaceWithApiToken();
@@ -33,22 +31,30 @@ beforeAll(async () => {
     }
   });
 
-  page1 = await createPage({
+  await createPage({
     spaceId: space1.id,
-    createdBy: user1.id,
-    content: { type: 'doc', content: [{ type: 'paragraph', content: [{ type: 'mention', attrs: { id: mention1Id, type: 'user', value: user1.id, createdAt: '2022-06-14T16:24', createdBy: user2.id } }] }] }
+    createdBy: user2.id,
+    content: { type: 'doc', content: [{ type: 'paragraph', content: [{ type: 'mention', attrs: { id: mention1Id, type: 'user', value: user1.id, createdAt: new Date().toISOString(), createdBy: user2.id } }] }] }
   });
 
-  page2 = await createPage({
+  // This will be skipped as the mention was created by the same user
+  await createPage({
     spaceId: space2.id,
     createdBy: user2.id,
-    content: { type: 'doc', content: [{ type: 'paragraph', content: [{ type: 'mention', attrs: { id: mention2Id, type: 'user', value: user1.id, createdAt: '2022-06-14T16:24', createdBy: user1.id } }] }] }
+    content: { type: 'doc', content: [{ type: 'paragraph', content: [{ type: 'mention', attrs: { id: mention2Id, type: 'user', value: user1.id, createdAt: new Date().toISOString(), createdBy: user1.id } }] }] }
+  });
+
+  // This will be skipped as the mention was not for the user
+  await createPage({
+    spaceId: space2.id,
+    createdBy: user2.id,
+    content: { type: 'doc', content: [{ type: 'paragraph', content: [{ type: 'mention', attrs: { id: mention3Id, type: 'user', value: user2.id, createdAt: new Date().toISOString(), createdBy: user1.id } }] }] }
   });
 
   await createPage({
     spaceId: space2.id,
     createdBy: user2.id,
-    content: { type: 'doc', content: [{ type: 'paragraph', content: [{ type: 'mention', attrs: { id: mention3Id, type: 'user', value: user2.id, createdAt: '2022-06-14T16:24', createdBy: user1.id } }] }] }
+    content: { type: 'doc', content: [{ type: 'paragraph', content: [{ type: 'mention', attrs: { id: mention4Id, type: 'user', value: user1.id, createdAt: new Date().toISOString(), createdBy: user2.id } }] }] }
   });
 
   await prisma.userNotification.create({
@@ -67,21 +73,7 @@ describe('getMentionedTasks', () => {
 
     expect(marked.length).toBe(1);
     expect(unmarked.length).toBe(1);
-    expect(marked[0]).toMatchObject(expect.objectContaining<Pick<MentionedTask, 'mentionId' | 'pageId' | 'spaceId'> & {createdBy: Pick<User, 'id'>}>({
-      mentionId: mention1Id,
-      pageId: page1.id,
-      spaceId: space1.id,
-      createdBy: expect.objectContaining({
-        id: user2.id
-      })
-    }));
-    expect(unmarked[0]).toMatchObject(expect.objectContaining<Pick<MentionedTask, 'mentionId' | 'pageId' | 'spaceId'> & {createdBy: Pick<User, 'id'>}>({
-      mentionId: mention2Id,
-      pageId: page2.id,
-      spaceId: space2.id,
-      createdBy: expect.objectContaining({
-        id: user1.id
-      })
-    }));
+    expect(marked[0].mentionId).toBe(mention1Id);
+    expect(unmarked[0].mentionId).toBe(mention4Id);
   });
 });
