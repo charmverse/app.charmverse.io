76ec5278dbb754aab4346a69f0fb57de88a9370d
melboudi <mo@anyahq.co>
2022-06-27 18:35:21 +0200

    Export Bounty permissions lib for consumption

---
+++ lib/permissions/bounties/index.ts
@@ -0,0 +1,6 @@
+export * from './interfaces';
+export * from './addBountyPermissionGroup';
+export * from './removeBountyPermissionGroup';
+export * from './computeBountyPermissions';
+export * from './queryBountyPermissions';
+export * from './availableBountyPermissions';
