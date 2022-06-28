65fa080d4b6039dbecbeb2b9fcdf9d7d4224bb6d
AndreiMitrea <andrei.mitrea@imbuelabs.com>
2022-06-28 16:31:52 +0300

    Removed external link PDF option due to CORS

--- components/common/PdfSelector/PdfSelector.tsx
+++ components/common/PdfSelector/PdfSelector.tsx
@@ -1,9 +1,9 @@
-import { Button, TextField } from '@mui/material';
+import { Button } from '@mui/material';
 import { Box } from '@mui/system';
 import MultiTabs from 'components/common/MultiTabs';
 import PopperPopup from 'components/common/PopperPopup';
 import { uploadToS3 } from 'lib/aws/uploadToS3Browser';
-import { ReactNode, useState } from 'react';
+import { ReactNode } from 'react';
 
 interface PdfSelectorProps {
   autoOpen?: boolean;
@@ -12,7 +12,6 @@ interface PdfSelectorProps {
 }
 
 export default function PdfSelector ({ autoOpen = false, children, onPdfSelect }: PdfSelectorProps) {
-  const [embedLink, setEmbedLink] = useState('');
   const tabs: [string, ReactNode][] = [];
 
   return (
@@ -47,30 +46,6 @@ export default function PdfSelector ({ autoOpen = false, children, onPdfSelect }
                       }
                     }}
                   />
-                </Button>
-              </Box>
-            ],
-            [
-              'Link',
-              <Box sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                gap: 2,
-                alignItems: 'center'
-              }}
-              >
-                <TextField autoFocus placeholder='Paste the PDF link...' value={embedLink} onChange={(e) => setEmbedLink(e.target.value)} />
-                <Button
-                  disabled={!embedLink}
-                  sx={{
-                    width: 250
-                  }}
-                  onClick={() => {
-                    onPdfSelect(embedLink);
-                    setEmbedLink('');
-                  }}
-                >
-                  Embed PDF
                 </Button>
               </Box>
             ]
