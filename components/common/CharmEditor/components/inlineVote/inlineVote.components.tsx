dad9b93ba3ceffa8d7c60182032472c7958ce14c
Safwan Shaheer <devorein00@gmail.com>
2022-06-29 15:52:52 +0600

    Removed Grow animation and fixed background for InlineCharmeditor

--- components/common/CharmEditor/components/inlineVote/inlineVote.components.tsx
+++ components/common/CharmEditor/components/inlineVote/inlineVote.components.tsx
@@ -57,161 +57,150 @@ export function InlineVoteSubMenu ({ pluginKey }: {pluginKey: PluginKey}) {
       hideSelectionTooltip(pluginKey)(view.state, view.dispatch, view);
     }}
     >
-      <Grow
-        in={true}
-        easing={{
-          enter: 'ease-in',
-          exit: 'ease-out'
+      <Box
+        flexDirection='column'
+        gap={1.5}
+        m={1}
+        display='flex'
+        width='400px'
+        sx={{
+          height: 400,
+          overflowY: 'scroll'
         }}
-        timeout={150}
       >
-        <Box
-          flexDirection='column'
-          gap={1.5}
-          m={1}
-          display='flex'
-          width='400px'
-          sx={{
-            height: 400,
-            overflowY: 'scroll'
+        <Box flexDirection='column' display='flex'>
+          <FieldLabel>Title</FieldLabel>
+          <TextField
+            placeholder="What's the vote?"
+            value={voteTitle}
+            onChange={(e) => {
+              setVoteTitle(e.target.value);
+            }}
+          />
+        </Box>
+        <Box flexGrow={1}>
+          <InlineCharmEditor
+            content={voteDescription}
+            style={{
+              fontSize: '14px',
+              backgroundColor: 'var(--input-bg)'
+            }}
+            placeholderText='Details (Optional)'
+            onContentChange={({ doc }) => {
+              setVoteDescription(doc);
+            }}
+          />
+        </Box>
+        <Box flexDirection='column' display='flex'>
+          <FieldLabel>Deadline</FieldLabel>
+          <DateTimePicker
+            minDate={DateTime.fromMillis(Date.now())}
+            value={deadline}
+            onAccept={async (value) => {
+              if (value) {
+                setDeadline(value);
+              }
+            }}
+            onChange={(value) => {
+              if (value) {
+                setDeadline(value);
+              }
+            }}
+            renderInput={(props) => (
+              <TextField
+                {...props}
+                inputProps={{
+                  ...props.inputProps,
+                  readOnly: true
+                }}
+                disabled
+                fullWidth
+              />
+            )}
+          />
+        </Box>
+        <RadioGroup
+          row
+          defaultValue='boolean'
+          value={voteType}
+          onChange={(e) => {
+            setVoteType(e.target.value as VoteType);
           }}
         >
-          <Box flexDirection='column' display='flex'>
-            <FieldLabel>Title</FieldLabel>
-            <TextField
-              placeholder="What's the vote?"
-              value={voteTitle}
-              onChange={(e) => {
-                setVoteTitle(e.target.value);
-              }}
-            />
-          </Box>
-          <Box flexGrow={1}>
-            <InlineCharmEditor
-              content={voteDescription}
-              style={{
-                fontSize: '14px',
-                borderColor: theme.palette.secondary.main,
-                borderWidth: '2px',
-                borderStyle: 'solid'
-              }}
-              placeholderText='Details (Optional)'
-              onContentChange={({ doc }) => {
-                setVoteDescription(doc);
-              }}
-            />
-          </Box>
-          <Box flexDirection='column' display='flex'>
-            <FieldLabel>Deadline</FieldLabel>
-            <DateTimePicker
-              minDate={DateTime.fromMillis(Date.now())}
-              value={deadline}
-              onAccept={async (value) => {
-                if (value) {
-                  setDeadline(value);
-                }
-              }}
-              onChange={(value) => {
-                if (value) {
-                  setDeadline(value);
-                }
-              }}
-              renderInput={(props) => (
+          <FormControlLabel
+            value='boolean'
+            control={<Radio />}
+            label='Yes / No'
+          />
+          <FormControlLabel value='options' control={<Radio />} label='# Options' />
+        </RadioGroup>
+        {voteType === 'boolean' ? (
+          <List>
+            <ListItem sx={{ p: 0 }}>
+              <CheckCircleIcon fontSize='small' sx={{ mr: 1 }} />
+              <ListItemText>Yes</ListItemText>
+            </ListItem>
+            <ListItem sx={{ p: 0 }}>
+              <CancelIcon fontSize='small' sx={{ mr: 1 }} />
+              <ListItemText>No</ListItemText>
+            </ListItem>
+            <ListItem sx={{ p: 0 }}>
+              <RemoveCircleIcon fontSize='small' sx={{ mr: 1 }} />
+              <ListItemText>Abstain</ListItemText>
+            </ListItem>
+          </List>
+        ) : (
+          <List>
+            {options.map((option, index) => (
+              <ListItem sx={{ px: 0, display: 'flex', gap: 1 }}>
                 <TextField
-                  {...props}
-                  inputProps={{
-                    ...props.inputProps,
-                    readOnly: true
-                  }}
-                  disabled
                   fullWidth
+                  placeholder={`Option ${index + 1}`}
+                  value={option}
+                  onChange={(e) => {
+                    options[index] = e.target.value;
+                    setOptions([...options]);
+                  }}
                 />
-              )}
-            />
-          </Box>
-          <RadioGroup
-            row
-            defaultValue='boolean'
-            value={voteType}
-            onChange={(e) => {
-              setVoteType(e.target.value as VoteType);
-            }}
-          >
-            <FormControlLabel
-              value='boolean'
-              control={<Radio />}
-              label='Yes / No'
-            />
-            <FormControlLabel value='options' control={<Radio />} label='# Options' />
-          </RadioGroup>
-          {voteType === 'boolean' ? (
-            <List>
-              <ListItem sx={{ p: 0 }}>
-                <CheckCircleIcon fontSize='small' sx={{ mr: 1 }} />
-                <ListItemText>Yes</ListItemText>
-              </ListItem>
-              <ListItem sx={{ p: 0 }}>
-                <CancelIcon fontSize='small' sx={{ mr: 1 }} />
-                <ListItemText>No</ListItemText>
-              </ListItem>
-              <ListItem sx={{ p: 0 }}>
-                <RemoveCircleIcon fontSize='small' sx={{ mr: 1 }} />
-                <ListItemText>Abstain</ListItemText>
-              </ListItem>
-            </List>
-          ) : (
-            <List>
-              {options.map((option, index) => (
-                <ListItem sx={{ px: 0, display: 'flex', gap: 1 }}>
-                  <TextField
-                    fullWidth
-                    placeholder={`Option ${index + 1}`}
-                    value={option}
-                    onChange={(e) => {
-                      options[index] = e.target.value;
-                      setOptions([...options]);
-                    }}
-                  />
-                  <IconButton
-                    disabled={options.length === 2}
-                    size='small'
-                    onClick={() => {
-                      setOptions([...options.slice(0, index), ...options.slice(index + 1)]);
-                    }}
-                  >
-                    <DeleteIcon fontSize='small' />
-                  </IconButton>
-                </ListItem>
-              ))}
-              <Box display='flex' gap={0.5} alignItems='center'>
                 <IconButton
+                  disabled={options.length === 2}
                   size='small'
                   onClick={() => {
-                    setOptions([...options, '']);
+                    setOptions([...options.slice(0, index), ...options.slice(index + 1)]);
                   }}
                 >
-                  <AddCircle fontSize='small' />
+                  <DeleteIcon fontSize='small' />
                 </IconButton>
-                <Typography variant='subtitle1'>
-                  Add Option
-                </Typography>
-              </Box>
-            </List>
-          ) }
-          <Button
-            size='small'
-            onClick={handleSubmit}
-            sx={{
-              alignSelf: 'flex-start',
-              marginBottom: '4px',
-              marginRight: '8px'
-            }}
-            disabled={isEmpty}
-          >
-            Start
-          </Button>
-        </Box>
-      </Grow>
+              </ListItem>
+            ))}
+            <Box display='flex' gap={0.5} alignItems='center'>
+              <IconButton
+                size='small'
+                onClick={() => {
+                  setOptions([...options, '']);
+                }}
+              >
+                <AddCircle fontSize='small' />
+              </IconButton>
+              <Typography variant='subtitle1'>
+                Add Option
+              </Typography>
+            </Box>
+          </List>
+        ) }
+        <Button
+          size='small'
+          onClick={handleSubmit}
+          sx={{
+            alignSelf: 'flex-start',
+            marginBottom: '4px',
+            marginRight: '8px'
+          }}
+          disabled={isEmpty || (voteType === 'options' && options.findIndex(option => option.length === 0) !== -1)}
+        >
+          Start
+        </Button>
+      </Box>
     </ClickAwayListener>
   );
 }
