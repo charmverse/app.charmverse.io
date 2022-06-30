fb5363cb0fc1e56b1e102a5fc42e06000a983c4f
AndreiMitrea <andrei.mitrea@imbuelabs.com>
2022-06-30 16:46:03 +0300

    Order criteria fix

--- lib/votes/getPageVotes.ts
+++ lib/votes/getPageVotes.ts
@@ -11,7 +11,7 @@ export async function getPageVotes (pageId: string): Promise<Vote[]> {
       voteOptions: true
     },
     orderBy: {
-      updatedAt: 'desc'
+      createdAt: 'desc'
     }
   });
 }
