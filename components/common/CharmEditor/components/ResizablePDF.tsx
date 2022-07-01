481655963be9ec46f2fb65b293b2ad75e1da0b80
AndreiMitrea <andrei.mitrea.hq@gmail.com>
2022-07-01 23:26:25 +0300

    Fix pdf refresh (#657)
    * Memoize PDF component
    
    * Changed PDF default width

--- components/common/CharmEditor/components/ResizablePDF.tsx
+++ components/common/CharmEditor/components/ResizablePDF.tsx
@@ -8,7 +8,7 @@ import IconButton from '@mui/material/IconButton';
 import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
 import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 import charmClient from 'charmClient';
-import { HTMLAttributes, memo, useCallback, useMemo, useState } from 'react';
+import { HTMLAttributes, memo, useMemo, useState } from 'react';
 import PdfSelector from 'components/common/PdfSelector';
 import { MIN_PDF_WIDTH, MAX_PDF_WIDTH } from 'lib/image/constants';
 import { Document, Page, pdfjs } from 'react-pdf';
@@ -93,7 +93,7 @@ export function pdfSpec () {
           default: null
         },
         size: {
-          default: (MIN_PDF_WIDTH + MAX_PDF_WIDTH) / 2
+          default: MAX_PDF_WIDTH
         }
       },
       group: 'block',
@@ -115,7 +115,7 @@ type PDFViewerProps = {
   width: number
 };
 
-function PDF (props: PDFViewerProps) {
+const PDF = memo((props: PDFViewerProps) => {
   const { url, width } = props;
   const [pageCount, setPageCount] = useState<number | null>(null);
   const [pageNumber, setPageNumber] = useState(1);
@@ -158,7 +158,7 @@ function PDF (props: PDFViewerProps) {
       </div>
     </Box>
   );
-}
+});
 
 function ResizablePDF ({ readOnly, onResizeStop, node, updateAttrs, selected }:
   NodeViewProps & {readOnly?: boolean, onResizeStop?: (view: EditorView) => void }) {
