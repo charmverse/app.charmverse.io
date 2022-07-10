ac6bd0b326886db131dd85c9bf920de7e32278c0
Safwan Shaheer <devorein00@gmail.com>
2022-07-10 22:00:29 +0600

    Added tests for /api/spaces/[id]/votes

---
+++ __integration-tests__/server/pages/api/spaces/[id]/votes.spec.ts
@@ -0,0 +1,27 @@
+import { Space, User } from '@prisma/client';
+import request from 'supertest';
+import { baseUrl, loginUser } from 'testing/mockApiCall';
+import { generateUserAndSpaceWithApiToken } from 'testing/setupDatabase';
+import { v4 } from 'uuid';
+
+let space: Space;
+let user: User;
+
+let userCookie: string;
+
+beforeAll(async () => {
+  const { space: generatedSpace, user: generatedUser } = await generateUserAndSpaceWithApiToken(undefined, true);
+  user = generatedUser;
+  space = generatedSpace;
+  userCookie = await loginUser(user);
+});
+
+describe('GET /api/spaces/[id]/votes', () => {
+  it('Should get all votes for space and respond 200', async () => {
+    await request(baseUrl).get(`/api/spaces/${space.id}/votes`).set('Cookie', userCookie).expect(200);
+  });
+
+  it('Should fail if user is not a part of a space and respond 404', async () => {
+    await request(baseUrl).get(`/api/spaces/${v4()}/votes`).set('Cookie', userCookie).expect(401);
+  });
+});
