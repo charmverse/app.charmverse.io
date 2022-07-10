1c7a9cdd97877a404252ccbadd921ff7b05419f1
Safwan Shaheer <devorein00@gmail.com>
2022-07-10 23:10:41 +0600

    Added tests for /lib/votes/createVote

---
+++ lib/votes/__tests__/createVote.spec.ts
@@ -0,0 +1,80 @@
+import { VoteStatus } from '@prisma/client';
+import { addSpaceOperations } from 'lib/permissions/spaces';
+import { DataNotFoundError, UnauthorisedActionError, UndesirableOperationError } from 'lib/utilities/errors';
+import { ExpectedAnError } from 'testing/errors';
+import { createPage, createVote, generateUserAndSpaceWithApiToken } from 'testing/setupDatabase';
+import { v4 } from 'uuid';
+import { createVote as createVoteService } from '../createVote';
+
+describe('createVote', () => {
+  it('should throw error if page doesn\'t exist', async () => {
+    try {
+      await createVoteService({
+        pageId: v4()
+      } as any);
+      throw new ExpectedAnError();
+    }
+    catch (err) {
+      expect(err).toBeInstanceOf(DataNotFoundError);
+    }
+  });
+
+  it('should throw error if user don\'t have permission to update vote', async () => {
+    const { space, user } = await generateUserAndSpaceWithApiToken(undefined, false);
+
+    const page = await createPage({
+      createdBy: user.id,
+      spaceId: space.id
+    });
+
+    try {
+      await createVoteService({
+        pageId: page.id
+      } as any);
+      throw new ExpectedAnError();
+    }
+    catch (err) {
+      expect(err).toBeInstanceOf(UnauthorisedActionError);
+    }
+  });
+
+  it('should create and return vote', async () => {
+    const { space, user } = await generateUserAndSpaceWithApiToken(undefined, false);
+
+    const page = await createPage({
+      createdBy: user.id,
+      spaceId: space.id
+    });
+
+    await addSpaceOperations({
+      forSpaceId: space.id,
+      operations: ['createVote'],
+      spaceId: space.id
+    });
+
+    const updatedVote = await createVoteService({
+      createdBy: user.id,
+      deadline: new Date(),
+      description: null,
+      pageId: page.id,
+      spaceId: space.id,
+      threshold: 50,
+      title: 'First vote',
+      type: 'Approval',
+      voteOptions: ['1', '2', '3']
+    });
+    expect(updatedVote).toMatchObject(expect.objectContaining({
+      totalVotes: 0,
+      status: VoteStatus.InProgress,
+      aggregatedResult: {
+        1: 0,
+        2: 0,
+        3: 0
+      },
+      userChoice: null,
+      pageId: page.id,
+      spaceId: space.id,
+      createdBy: user.id
+    }));
+  });
+});
