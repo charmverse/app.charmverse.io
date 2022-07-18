280c6df45f387f6f523ffbc2bdb313ced12719dc
Safwan Shaheer <devorein00@gmail.com>
2022-07-18 15:37:11 +0600

    Added source url for util functions used inside findChildrenByMark module

--- lib/prosemirror/findChildrenByMark.ts
+++ lib/prosemirror/findChildrenByMark.ts
@@ -1,5 +1,6 @@
 import { MarkType, Node } from '@bangle.dev/pm';
 
+// source: https://github.com/atlassian/prosemirror-utils/blob/master/src/node.js#L7
 function flatten (node: Node) {
   const result: {node: Node, pos: number}[] = [];
   node.descendants((child, pos) => {
@@ -9,12 +10,14 @@ function flatten (node: Node) {
   return result;
 }
 
+// source: https://github.com/atlassian/prosemirror-utils/blob/master/src/node.js#L27
 function findChildren (node: Node, predicate: (node: Node) => boolean) {
   return flatten(node).filter((child) => {
     return predicate(child.node);
   });
 }
 
+// source: https://github.com/atlassian/prosemirror-utils/blob/master/src/node.js#L92
 export function findChildrenByMark (node: Node, markType: MarkType) {
   return findChildren(node, (child) => Boolean(markType.isInSet(child.marks)));
 }
