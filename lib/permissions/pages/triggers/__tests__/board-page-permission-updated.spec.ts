5c3a6175a2ba5ca891aafe6cecef7d895eecb2e1
MEB <mo@anyahq.co>
2022-07-20 16:42:47 +0200

    Delete unused test file

--- lib/permissions/pages/triggers/__tests__/board-page-permission-updated.spec.ts
+++
@@ -1,7 +0,0 @@
-import { boardPagePermissionUpdated } from '../board-page-permission-updated';
-
-describe('boardPagePermissionUpdated', () => {
-  it('should', async () => {
-    boardPagePermissionUpdated.exec();
-  });
-});
