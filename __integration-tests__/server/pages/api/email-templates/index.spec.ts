02d04bc956f339b4b08f70ea6e45457c34813ba9
Matt Casey <mattwad@gmail.com>
2022-07-25 13:06:58 -0600

    add test on mjml

---
+++ __integration-tests__/server/pages/api/email-templates/index.spec.ts
@@ -0,0 +1,14 @@
+
+import request from 'supertest';
+import { baseUrl } from 'testing/mockApiCall';
+
+describe('GET /api/email-templates - renders email templates', () => {
+
+  it('should return 200', async () => {
+    await request(baseUrl)
+      .get('/api/email-templates')
+      .expect(200);
+
+  });
+
+});
