98ceb869076642f48c60a5391861e3a3006f7ebc
Matt Casey <mattwad@gmail.com>
2022-07-29 01:12:27 -0600

    Bounty UI board (#747)
    Co-authored-by: Safwan Shaheer <devorein00@gmail.com>

--- components/bounties/BountyList.tsx
+++ components/bounties/BountyList.tsx
@@ -1,21 +1,19 @@
 import { Box, Grid, Typography } from '@mui/material';
-import { BountyStatus } from '@prisma/client';
+import { BountyStatus, Page } from '@prisma/client';
 import Button from 'components/common/Button';
-import { FullWidthPageContent } from 'components/common/PageLayout/components/PageContent';
+import PageDialog from 'components/common/Page/PageDialog';
 import { sortArrayByObjectProperty } from 'lib/utilities/array';
 import { BountyWithDetails } from 'models';
-import { useMemo } from 'react';
+import { useMemo, useState } from 'react';
 import { CSVLink } from 'react-csv';
-import { BountyCard } from './components/BountyCard';
+import { usePages } from 'hooks/usePages';
+import BountyCard from './components/BountyCard';
 import { BountyStatusChip } from './components/BountyStatusBadge';
 import MultiPaymentModal from './components/MultiPaymentModal';
 import NewBountyButton from './components/NewBountyButton';
 
 const bountyStatuses: BountyStatus[] = ['open', 'inProgress', 'complete', 'paid', 'suggestion'];
 
-/**
- *
- */
 interface Props {
   publicMode?: boolean
   bountyCardClicked?: (bounty: BountyWithDetails) => void,
@@ -23,27 +21,25 @@ interface Props {
 }
 
 export default function BountyList ({ publicMode, bountyCardClicked = () => null, bounties }: Props) {
-  // User can only suggest a bounty instead of creating it
 
-  const sortedBounties = bounties ? sortArrayByObjectProperty(bounties, 'status', bountyStatuses) : [];
+  const [activeBountyPage, setBountyPage] = useState<Page | null>(null);
+  const { pages } = usePages();
 
-  const bountiesStatusRecord = useMemo(() => {
-    const _bountiesStatusRecord: Record<BountyStatus, BountyWithDetails[]> = {
-      complete: [],
-      inProgress: [],
-      open: [],
-      paid: [],
-      suggestion: []
-    };
+  const bountiesSorted = bounties ? sortArrayByObjectProperty(bounties, 'status', bountyStatuses) : [];
 
-    sortedBounties.forEach(sortedBounty => {
-      _bountiesStatusRecord[sortedBounty.status].push(sortedBounty);
-    });
-    return _bountiesStatusRecord;
-  }, [sortedBounties]);
+  const bountiesGroupedByStatus = bountiesSorted.reduce<Record<BountyStatus, BountyWithDetails[]>>((record, sortedBounty) => {
+    record[sortedBounty.status].push(sortedBounty);
+    return record;
+  }, {
+    complete: [],
+    inProgress: [],
+    open: [],
+    paid: [],
+    suggestion: []
+  });
 
   const csvData = useMemo(() => {
-    const completedBounties = sortedBounties.filter(bounty => bounty.status === BountyStatus.complete);
+    const completedBounties = bountiesSorted.filter(bounty => bounty.status === BountyStatus.complete);
     if (!completedBounties.length) {
       return [];
     }
@@ -62,95 +58,96 @@ export default function BountyList ({ publicMode, bountyCardClicked = () => null
         bounty.chainId
       ])
     ];
-  }, [sortedBounties]);
+  }, [bountiesSorted]);
 
   return (
-    <FullWidthPageContent>
+    <div className='focalboard-body'>
+      <div className='BoardComponent'>
 
-      <Grid container display='flex' justifyContent='space-between' alignContent='center' mb={3}>
+        <div className='top-head'>
+          <Grid container display='flex' justifyContent='space-between' alignContent='center' mb={3} mt={10}>
 
-        <Grid display='flex' justifyContent='space-between' item xs={12} mb={2}>
-          <Box width='fit-content'>
-            <Typography variant='h1' display='flex' alignItems='center' sx={{ height: '100%' }}>
-              Bounties
-            </Typography>
-          </Box>
+            <Grid display='flex' justifyContent='space-between' item xs={12} mb={2}>
+              <Typography variant='h1' display='flex' alignItems='center' sx={{ height: '100%' }}>
+                Bounties
+              </Typography>
 
-          {
-          !publicMode && (
-            <Box width='fit-content' display='flex' gap={1}>
-              { !!csvData.length
-              && (
-                <CSVLink data={csvData} filename='Gnosis Safe Airdrop.csv' style={{ textDecoration: 'none' }}>
-                  <Button color='secondary' variant='outlined'>
-                    Export to CSV
-                  </Button>
-                </CSVLink>
+              {!publicMode && (
+                <Box width='fit-content' display='flex' gap={1}>
+                  { !!csvData.length
+                      && (
+                        <CSVLink data={csvData} filename='Gnosis Safe Airdrop.csv' style={{ textDecoration: 'none' }}>
+                          <Button color='secondary' variant='outlined'>
+                            Export to CSV
+                          </Button>
+                        </CSVLink>
+                      )}
+                  <MultiPaymentModal bounties={bounties} />
+                  <NewBountyButton />
+                </Box>
               )}
-              <MultiPaymentModal bounties={bounties} />
-              <NewBountyButton />
-            </Box>
+
+            </Grid>
+
+          </Grid>
+        </div>
+
+        <div className='container-container'>
+
+          {/* Onboarding video when no bounties exist */}
+          {
+          bounties.length === 0 && (
+            <div style={{ marginTop: '25px' }}>
+
+              <Typography variant='h6'>
+                Getting started with bounties
+              </Typography>
+
+              <iframe
+                src='https://tiny.charmverse.io/bounties'
+                style={{ maxWidth: '100%', border: '0 none' }}
+                height='367px'
+                width='650px'
+                title='Bounties | Getting started with Charmverse'
+              >
+              </iframe>
+
+            </div>
           )
         }
 
-        </Grid>
-
-      </Grid>
-
-      {/* Onboarding video when no bounties exist */}
-      {
-            bounties.length === 0 && (
-              <div style={{ marginTop: '25px' }}>
-
-                <Typography variant='h6'>
-                  Getting started with bounties
-                </Typography>
-
-                <iframe
-                  src='https://tiny.charmverse.io/bounties'
-                  style={{ maxWidth: '100%', border: '0 none' }}
-                  height='367px'
-                  width='650px'
-                  title='Bounties | Getting started with Charmverse'
-                >
-                </iframe>
-
-              </div>
-            )
-          }
-
-      {/* List of bounties based on current filter */}
-
-      <Grid container>
-        {bountyStatuses.map(bountyStatus => (
-          <Grid container className='octo-board-header' gap={1} item md={12 / 5} alignContent='flex-start' key={bountyStatus}>
-            <Grid item height='fit-content'>
-              <BountyStatusChip status={bountyStatus} />
-            </Grid>
-          </Grid>
-        ))}
-        {
-            bountyStatuses.map(bountyStatus => {
-              return (
-                <Grid container gap={1} item md={12 / 5} alignContent='flex-start' key={bountyStatus}>
-                  {bountiesStatusRecord[bountyStatus].map(bounty => (
-                    <Grid
+          {/* List of bounties based on current filter */}
+          <div className='Kanban'>
+            <div className='octo-board-header'>
+              {bountyStatuses.map(bountyStatus => (
+                <div className='octo-board-header-cell' key={bountyStatus}>
+                  <BountyStatusChip status={bountyStatus} />
+                </div>
+              ))}
+            </div>
+            <div className='octo-board-body'>
+              {bountyStatuses.map(bountyStatus => (
+                <div className='octo-board-column' key={bountyStatus}>
+                  {bountiesGroupedByStatus[bountyStatus].filter(bounty => Boolean(pages[bounty.page?.id])).map(bounty => (
+                    <BountyCard
                       key={bounty.id}
-                      item
+                      bounty={bounty}
+                      page={pages[bounty.page.id] as Page}
                       onClick={() => {
                         bountyCardClicked(bounty);
+                        const bountyPage = pages[bounty.page?.id] as Page;
+                        setBountyPage(bountyPage);
                       }}
-                      width='100%'
-                      marginRight={1}
-                    >
-                      <BountyCard truncate={false} key={bounty.id} bounty={bounty} />
-                    </Grid>
+                    />
                   ))}
-                </Grid>
-              );
-            })
-          }
-      </Grid>
-    </FullWidthPageContent>
+                </div>
+              ))}
+            </div>
+
+            {activeBountyPage && <PageDialog page={activeBountyPage} onClose={() => setBountyPage(null)} />}
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
