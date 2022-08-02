8e26b2239c98c6199ddcc34059a63e450e9934f0
MEB <mo@anyahq.co>
2022-08-02 21:31:55 +0200

    Fix the automated test cleanup

--- testing/wipeTestData.ts
+++ testing/wipeTestData.ts
@@ -2,14 +2,9 @@ import { prisma } from 'db';
 
 export default async function wipeTestData (): Promise<true> {
 
-  // console.log('\r\n-----\r\n-----Test cleanup fired-----\r\n-----\r\n')
-
-  await prisma.bounty.deleteMany({});
-
-  await Promise.all([
-    prisma.space.deleteMany({}),
-    prisma.user.deleteMany({})
-  ]);
+  // We have to delete the space first, as otherwise user-created content (such as bounties) will throw an error on delete
+  await prisma.space.deleteMany({});
+  await prisma.user.deleteMany({});
 
   return true;
 }
