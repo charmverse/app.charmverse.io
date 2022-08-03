995a9b4b82f0ac06247542bdc82777845ec0aef8
MEB <mo@anyahq.co>
2022-08-03 18:26:16 +0200

    Mandatory vote context

--- prisma/migrations/20220803162559_add_vote_context/migration.sql
+++ prisma/migrations/20220803162559_add_vote_context/migration.sql
@@ -2,4 +2,4 @@
 CREATE TYPE "VoteContext" AS ENUM ('inline', 'proposal');
 
 -- AlterTable
-ALTER TABLE "Vote" ADD COLUMN     "context" "VoteContext" DEFAULT 'inline';
+ALTER TABLE "Vote" ADD COLUMN     "context" "VoteContext" NOT NULL DEFAULT 'inline';
