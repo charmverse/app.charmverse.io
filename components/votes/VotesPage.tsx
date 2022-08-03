8ad4842641ad897d2c6438cc8fa38e89e5f42115
MEB <mo@anyahq.co>
2022-08-03 18:20:35 +0200

    Handle initial loading state in votes page

--- components/votes/VotesPage.tsx
+++ components/votes/VotesPage.tsx
@@ -6,6 +6,7 @@ import { filterVotes, sortVotes, ViewOptions, VoteFilter, VoteSort } from 'compo
 import { useCurrentSpace } from 'hooks/useCurrentSpace';
 import { usePages } from 'hooks/usePages';
 import { Reducer, useReducer } from 'react';
+import LoadingComponent from 'components/common/LoadingComponent';
 import useSWR from 'swr';
 import NewProposalButton from './components/NewProposalButton';
 import VotesTable, { VoteRow } from './components/VotesTable';
@@ -29,10 +30,16 @@ export default function VotesPage () {
   const [viewState, setViewState] = useReducer<ViewStateReducer>((state, updates) => ({ ...state, ...updates }), defaultViewState);
 
   const [currentSpace] = useCurrentSpace();
-  const { data, mutate: mutateVotes } = useSWR(() => `votesBySpace/${currentSpace?.id}`, () => currentSpace ? charmClient.getVotesBySpace(currentSpace.id) : []);
+  const { data, mutate: mutateVotes } = useSWR(() => `votesBySpace/${currentSpace?.id}`, () => currentSpace ? charmClient.getVotesBySpace(currentSpace.id) : [], {
+    fallbackData: undefined
+  });
 
   // votes dont exist right away for proposals, so treat them like draft votes
   const { pages } = usePages();
+
+  // For now, consider that empty pages list means we are loading pages
+  const loadingData = !pages || Object.keys(pages).length === 0 || !data;
+
   const proposalsWithoutVotes = Object.values(pages).filter(page => page?.type === 'proposal' && !page.deletedAt && !data?.some(vote => vote.pageId === page.id && vote.context === 'proposal'));
   const draftVotes: VoteRow[] = proposalsWithoutVotes.map(page => ({
     id: page!.id,
@@ -66,14 +73,33 @@ export default function VotesPage () {
             Votes
           </Typography>
         </Grid>
-        <Grid item xs={12} lg={8} display='flex'>
-          <Box gap={3} sx={{ display: 'flex', alignItems: { xs: 'flex-start', lg: 'center' }, width: '100%', justifyContent: { xs: 'flex-start', lg: 'flex-end' }, flexDirection: { xs: 'column-reverse', lg: 'row' } }}>
-            <ViewOptions voteSort={viewState.sortBy} voteFilter={viewState.filterBy} setVoteSort={setVoteSort} setVoteFilter={setVoteFilter} />
-            <NewProposalButton />
-          </Box>
-        </Grid>
+
+        {
+            loadingData ? (
+              <Grid item xs={12} sx={{ mt: 12 }}>
+                <LoadingComponent isLoading size={50} />
+              </Grid>
+            ) : (
+              <>
+                <Grid item xs={12} lg={8} display='flex'>
+                  <Box gap={3} sx={{ display: 'flex', alignItems: { xs: 'flex-start', lg: 'center' }, width: '100%', justifyContent: { xs: 'flex-start', lg: 'flex-end' }, flexDirection: { xs: 'column-reverse', lg: 'row' } }}>
+                    <ViewOptions
+                      voteSort={viewState.sortBy}
+                      voteFilter={viewState.filterBy}
+                      setVoteSort={setVoteSort}
+                      setVoteFilter={setVoteFilter}
+                    />
+                    <NewProposalButton />
+                  </Box>
+                </Grid>
+                <Grid item xs={12} sx={{ mt: 5 }}>
+                  <VotesTable votes={sortedVotes} mutateVotes={mutateVotes} />
+                </Grid>
+              </>
+
+            )
+          }
       </Grid>
-      <VotesTable votes={sortedVotes} mutateVotes={mutateVotes} />
     </CenteredPageContent>
   );
 }
