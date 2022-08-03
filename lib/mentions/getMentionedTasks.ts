e35b03ea1a9c606c75fd8082d45feec7e1c0fe50
Matt Casey <mattwad@gmail.com>
2022-08-03 15:27:28 -0600

    include comments from fb blocks in mention request

--- lib/mentions/getMentionedTasks.ts
+++ lib/mentions/getMentionedTasks.ts
@@ -3,11 +3,26 @@ import { prisma } from 'db';
 import { extractMentions } from 'lib/prosemirror/extractMentions';
 import { shortenHex } from 'lib/utilities/strings';
 import { PageContent, User } from 'models';
+import { uniq } from 'lodash';
 import { MentionedTask } from './interfaces';
 
 export type MentionedTasksGroup = {
   marked: MentionedTask[],
   unmarked: MentionedTask[]
+}
+
+type Mention = Omit<MentionedTask, 'createdBy'> & { userId: string };
+
+interface GetMentionsInput {
+  userId: string;
+  username: string;
+  spaceIds: string[];
+  spaceRecord: Record<string, Pick<Space, 'name' | 'domain' | 'id'>>;
+}
+
+interface GetMentionsResponse {
+  mentions: Record<string, Mention>;
+  mentionedUserIds: string[];
 }
 
 export async function getMentionedTasks (userId: string): Promise<MentionedTasksGroup> {
@@ -68,22 +83,55 @@ export async function getMentionedTasks (userId: string): Promise<MentionedTasks
   // Get the marked mention task ids (all the discussion type tasks that exist in the db)
   const markedMentionTaskIds = new Set(markedMentionTasks.map(markedMentionTask => markedMentionTask.taskId));
 
-  // Get all the pages of all the spaces this user is part of
-  const pages = await prisma.page.findMany({
+  const context: GetMentionsInput = { userId, username, spaceRecord, spaceIds };
+
+  const { mentions, mentionedUserIds } = await Promise.all([
+    getMentionsFromBounties(context),
+    getMentionsFromComments(context),
+    getMentionsFromPages(context),
+    getMentionsFromCommentBlocks(context)
+  ]).then(results => {
+    // aggregate the results
+    return results.reduce((acc, result) => {
+      Object.assign(acc.mentions, result.mentions);
+      acc.mentionedUserIds = uniq(acc.mentionedUserIds.concat(result.mentionedUserIds));
+      return acc;
+    }, { mentions: {}, mentionedUserIds: [] });
+  });
+
+  // Only fetch the users that created the mentions
+  const users = await prisma.user.findMany({
     where: {
-      spaceId: {
-        in: spaceIds
+      id: {
+        in: mentionedUserIds
       }
-    },
-    select: {
-      content: true,
-      id: true,
-      path: true,
-      title: true,
-      createdBy: true,
-      spaceId: true
     }
   });
+
+  // Create a record for the user
+  const usersRecord: Record<string, User> = users.reduce((acc, cur) => ({ ...acc, [cur.id]: cur }), {});
+
+  const mentionedTasks: MentionedTasksGroup = { marked: [], unmarked: [] };
+
+  // Loop through each mentioned task and attach the user data using usersRecord
+  Object.values(mentions).forEach(mentionedTaskWithoutUser => {
+    // joining the mentioned task with the user
+    const mentionedTask = { ...mentionedTaskWithoutUser, createdBy: usersRecord[mentionedTaskWithoutUser.userId] } as MentionedTask;
+    if (markedMentionTaskIds.has(mentionedTask.mentionId)) {
+      mentionedTasks.marked.push(mentionedTask);
+    }
+    else {
+      mentionedTasks.unmarked.push(mentionedTask);
+    }
+  });
+
+  return {
+    marked: mentionedTasks.marked.sort((mentionTaskA, mentionTaskB) => mentionTaskA.createdAt > mentionTaskB.createdAt ? -1 : 1),
+    unmarked: mentionedTasks.unmarked.sort((mentionTaskA, mentionTaskB) => mentionTaskA.createdAt > mentionTaskB.createdAt ? -1 : 1)
+  };
+}
+
+async function getMentionsFromBounties ({ userId, username, spaceRecord, spaceIds }: GetMentionsInput): Promise<GetMentionsResponse> {
 
   const bounties = await prisma.bounty.findMany({
     where: {
@@ -103,6 +151,104 @@ export async function getMentionedTasks (userId: string): Promise<MentionedTasks
       }
     }
   });
+
+  const mentionsMap: GetMentionsResponse['mentions'] = {};
+  const mentionedUserIds: string[] = [];
+
+  for (const bounty of bounties) {
+    const content = bounty.page?.content as PageContent;
+    if (content) {
+      const mentions = extractMentions(content, username);
+      mentions.forEach(mention => {
+        if (bounty.spaceId && mention.value === userId && mention.createdBy !== userId) {
+          mentionedUserIds.push(mention.createdBy);
+          mentionsMap[mention.id] = {
+            mentionId: mention.id,
+            createdAt: mention.createdAt,
+            pageId: null,
+            spaceId: bounty.spaceId,
+            spaceDomain: spaceRecord[bounty.spaceId].domain,
+            pagePath: null,
+            spaceName: spaceRecord[bounty.spaceId].name,
+            userId: mention.createdBy,
+            pageTitle: null,
+            text: mention.text,
+            bountyId: bounty.id,
+            bountyTitle: bounty.page?.title || 'Untitled',
+            commentId: null,
+            type: 'bounty'
+          };
+        }
+      });
+    }
+  }
+  return {
+    mentions: mentionsMap,
+    mentionedUserIds
+  };
+}
+
+async function getMentionsFromCommentBlocks ({ userId, username, spaceRecord, spaceIds }: GetMentionsInput): Promise<GetMentionsResponse> {
+
+  const blockComments = await prisma.block.findMany({
+    where: {
+      type: 'comment',
+      spaceId: {
+        in: spaceIds
+      }
+    },
+    select: {
+      id: true,
+      createdBy: true,
+      spaceId: true,
+      fields: true,
+      page: {
+        select: {
+          id: true,
+          title: true,
+          path: true
+        }
+      }
+    }
+  });
+
+  const mentionsMap: GetMentionsResponse['mentions'] = {};
+  const mentionedUserIds: string[] = [];
+
+  for (const comment of blockComments) {
+    const content = comment.fields?.content as PageContent;
+    if (content) {
+      const mentions = extractMentions(content, username);
+      mentions.forEach(mention => {
+        if (comment.spaceId && mention.value === userId && mention.createdBy !== userId && comment.createdBy !== userId) {
+          mentionedUserIds.push(mention.createdBy);
+          mentionsMap[mention.id] = {
+            mentionId: mention.id,
+            createdAt: mention.createdAt,
+            pageId: comment.page?.id || '',
+            spaceId: comment.spaceId,
+            spaceDomain: spaceRecord[comment.spaceId].domain,
+            pagePath: comment.page?.path || '',
+            spaceName: spaceRecord[comment.spaceId].name,
+            userId: mention.createdBy,
+            pageTitle: comment.page?.title || '',
+            text: mention.text,
+            bountyId: null,
+            bountyTitle: null,
+            commentId: comment.id,
+            type: 'page'
+          };
+        }
+      });
+    }
+  }
+  return {
+    mentions: mentionsMap,
+    mentionedUserIds
+  };
+}
+
+async function getMentionsFromComments ({ userId, username, spaceRecord, spaceIds }: GetMentionsInput): Promise<GetMentionsResponse> {
 
   const comments = await prisma.comment.findMany({
     where: {
@@ -125,9 +271,63 @@ export async function getMentionedTasks (userId: string): Promise<MentionedTasks
     }
   });
 
-  // A mapping between mention id and the mention data (without user)
-  const mentionedTasksWithoutUserRecord: Record<string, Omit<MentionedTask, 'createdBy'> & {userId: string}> = {};
-  const mentionUserIds: Set<string> = new Set();
+  const mentionsMap: GetMentionsResponse['mentions'] = {};
+  const mentionedUserIds: string[] = [];
+
+  for (const comment of comments) {
+    const content = comment.content as PageContent;
+    if (content) {
+      const mentions = extractMentions(content, username);
+      mentions.forEach(mention => {
+        if (comment.spaceId && mention.value === userId && mention.createdBy !== userId && comment.userId !== userId) {
+          mentionedUserIds.push(mention.createdBy);
+          mentionsMap[mention.id] = {
+            mentionId: mention.id,
+            createdAt: mention.createdAt,
+            pageId: comment.page.id,
+            spaceId: comment.spaceId,
+            spaceDomain: spaceRecord[comment.spaceId].domain,
+            pagePath: comment.page.path,
+            spaceName: spaceRecord[comment.spaceId].name,
+            userId: mention.createdBy,
+            pageTitle: comment.page.title,
+            text: mention.text,
+            bountyId: null,
+            bountyTitle: null,
+            commentId: comment.id,
+            type: 'page'
+          };
+        }
+      });
+    }
+  }
+  return {
+    mentions: mentionsMap,
+    mentionedUserIds
+  };
+}
+
+async function getMentionsFromPages ({ userId, username, spaceRecord, spaceIds }: GetMentionsInput): Promise<GetMentionsResponse> {
+
+  // Get all the pages of all the spaces this user is part of
+  const pages = await prisma.page.findMany({
+    where: {
+      spaceId: {
+        in: spaceIds
+      }
+    },
+    select: {
+      content: true,
+      id: true,
+      path: true,
+      title: true,
+      createdBy: true,
+      spaceId: true
+    }
+  });
+
+  const mentionsMap: GetMentionsResponse['mentions'] = {};
+  const mentionedUserIds: string[] = [];
 
   for (const page of pages) {
     const content = page.content as PageContent;
@@ -136,8 +336,8 @@ export async function getMentionedTasks (userId: string): Promise<MentionedTasks
       mentions.forEach(mention => {
         // Skip mentions not for the user, self mentions and inside user created pages
         if (page.spaceId && mention.value === userId && mention.createdBy !== userId) {
-          mentionUserIds.add(mention.createdBy);
-          mentionedTasksWithoutUserRecord[mention.id] = {
+          mentionedUserIds.push(mention.createdBy);
+          mentionsMap[mention.id] = {
             mentionId: mention.id,
             createdAt: mention.createdAt,
             pageId: page.id,
@@ -158,92 +358,8 @@ export async function getMentionedTasks (userId: string): Promise<MentionedTasks
     }
   }
 
-  for (const bounty of bounties) {
-    const content = bounty.page?.content as PageContent;
-    if (content) {
-      const mentions = extractMentions(content, username);
-      mentions.forEach(mention => {
-        if (bounty.spaceId && mention.value === userId && mention.createdBy !== userId) {
-          mentionUserIds.add(mention.createdBy);
-          mentionedTasksWithoutUserRecord[mention.id] = {
-            mentionId: mention.id,
-            createdAt: mention.createdAt,
-            pageId: null,
-            spaceId: bounty.spaceId,
-            spaceDomain: spaceRecord[bounty.spaceId].domain,
-            pagePath: null,
-            spaceName: spaceRecord[bounty.spaceId].name,
-            userId: mention.createdBy,
-            pageTitle: null,
-            text: mention.text,
-            bountyId: bounty.id,
-            bountyTitle: bounty.page?.title || 'Untitled',
-            commentId: null,
-            type: 'bounty'
-          };
-        }
-      });
-    }
-  }
-
-  for (const comment of comments) {
-    const content = comment.content as PageContent;
-    if (content) {
-      const mentions = extractMentions(content, username);
-      mentions.forEach(mention => {
-        if (comment.spaceId && mention.value === userId && mention.createdBy !== userId && comment.userId !== userId) {
-          mentionUserIds.add(mention.createdBy);
-          mentionedTasksWithoutUserRecord[mention.id] = {
-            mentionId: mention.id,
-            createdAt: mention.createdAt,
-            pageId: comment.page.id,
-            spaceId: comment.spaceId,
-            spaceDomain: spaceRecord[comment.spaceId].domain,
-            pagePath: comment.page.path,
-            spaceName: spaceRecord[comment.spaceId].name,
-            userId: mention.createdBy,
-            pageTitle: comment.page.title,
-            text: mention.text,
-            bountyId: null,
-            bountyTitle: null,
-            commentId: comment.id,
-            type: 'page'
-          };
-        }
-      });
-    }
-  }
-
-  // Only fetch the users that created the mentions
-  const users = await prisma.user.findMany({
-    where: {
-      id: {
-        in: Array.from(mentionUserIds)
-      }
-    }
-  });
-
-  // Create a record for the user
-  const usersRecord: Record<string, User> = users.reduce((acc, cur) => ({ ...acc, [cur.id]: cur }), {});
-
-  const mentionedTasks: MentionedTasksGroup = { marked: [], unmarked: [] };
-
-  // Loop through each mentioned task and attach the user data using usersRecord
-  Object.values(
-    mentionedTasksWithoutUserRecord
-  ).forEach(mentionedTaskWithoutUser => {
-    // joining the mentioned task with the user
-    const mentionedTask = { ...mentionedTaskWithoutUser, createdBy: usersRecord[mentionedTaskWithoutUser.userId] } as MentionedTask;
-    if (markedMentionTaskIds.has(mentionedTask.mentionId)) {
-      mentionedTasks.marked.push(mentionedTask);
-    }
-    else {
-      mentionedTasks.unmarked.push(mentionedTask);
-    }
-  });
-
   return {
-    marked: mentionedTasks.marked.sort((mentionTaskA, mentionTaskB) => mentionTaskA.createdAt > mentionTaskB.createdAt ? -1 : 1),
-    unmarked: mentionedTasks.unmarked.sort((mentionTaskA, mentionTaskB) => mentionTaskA.createdAt > mentionTaskB.createdAt ? -1 : 1)
+    mentions: mentionsMap,
+    mentionedUserIds
   };
 }
