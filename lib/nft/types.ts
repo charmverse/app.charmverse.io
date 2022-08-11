e569ac8e1242a863ed646455fe0877f8199f5103
Marek <m.sadura@outlook.com>
2022-08-11 09:39:29 +0200

    refactor

--- lib/nft/types.ts
+++ lib/nft/types.ts
@@ -2,10 +2,10 @@ export type NftData = {
   tokenId: string
   tokenIdInt: number | null
   contract: string;
-  image: string;
-  imageRaw: string;
-  imageThumbnail: string
   title: string;
   description: string
   chainId: number
+  image: string;
+  imageRaw: string;
+  imageThumb?: string
 }
