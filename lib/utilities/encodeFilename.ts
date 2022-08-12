d924514211f7cc11b3442f223aaf2d6d0c02a68a
Matt Casey <mattwad@gmail.com>
2022-08-12 12:53:04 -0600

    remove logs

--- lib/utilities/encodeFilename.ts
+++ lib/utilities/encodeFilename.ts
@@ -1,6 +1,6 @@
 export const encodeFilename = (rawName: string): string => {
   const extension = rawName.split('.').pop() || ''; // lowercase the extension to simplify possible values
-  const filename = encodeURIComponent(rawName.replace(extension, extension.toLowerCase()));
+  const filename = rawName.replace(extension, extension.toLowerCase());
 
   return encodeURIComponent(filename);
 };
