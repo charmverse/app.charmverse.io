b5fc905d0e55009173550d83b89104b57134d466
Safwan Shaheer <devorein00@gmail.com>
2022-08-18 20:29:25 +0600

    Fetch deepdao profile data

--- lib/deepdao/client.ts
+++ lib/deepdao/client.ts
@@ -13,16 +13,66 @@ interface DeepDaoParticipationScore {
   proposals: number
   votes: number
 }
-type GetParticipationScoreResponse = DeepDaoApiResponse<DeepDaoParticipationScore>
 
-export type DeepDaoAggregateData = Pick<DeepDaoParticipationScore, 'daos' | 'proposals' | 'votes'> & {bounties: number}
+export type DeepDaoProfile = {
+  totalVotes: number
+  totalProposals: number
+  organizations: {
+    name: string
+    organizationId: string
+    description: string
+  }[]
+  proposals: {
+    title: string
+    status: string
+    choices: string[]
+    outcome: number
+    createdAt: string
+    description: string
+    voteChoice: number
+    organizationId: string
+    successfulVote: boolean
+  }[]
+  votes: {
+    title: string
+    daoId: string
+    organizationId: string
+    choices: string[]
+    voteId: string
+    createdAt: string
+    successful: null | boolean
+    description: string
+  }[]
+}
+
+type GetParticipationScoreResponse = DeepDaoApiResponse<DeepDaoParticipationScore>
+type GetProfileResponse = DeepDaoApiResponse<DeepDaoProfile>
+
+export type DeepDaoAggregateData = Pick<DeepDaoParticipationScore, 'daos'> & Pick<DeepDaoProfile, 'organizations' | 'totalProposals' | 'totalVotes' | 'proposals' | 'votes'> & {bounties: number}
 
 export async function getParticipationScore (address: string, apiToken = DEEPDAO_API_KEY): Promise<GetParticipationScoreResponse | null> {
+  address = '0xef8305e140ac520225daf050e2f71d5fbcc543e7';
+
   if (!apiToken) {
     log.debug('Skip request: No API Key for DeepDAO');
     return null;
   }
+
   return fetch<GetParticipationScoreResponse>(`${DEEP_DAO_BASE_URL}/v0.1/people/participation_score/${address}`, {
+    method: 'GET',
+    headers: {
+      'x-api-key': apiToken
+    }
+  });
+}
+
+export async function getProfile (address: string, apiToken = DEEPDAO_API_KEY): Promise<GetProfileResponse | null> {
+  if (!apiToken) {
+    log.debug('Skip request: No API Key for DeepDAO');
+    return null;
+  }
+
+  return fetch<GetProfileResponse>(`${DEEP_DAO_BASE_URL}/v0.1/people/profile/${address}`, {
     method: 'GET',
     headers: {
       'x-api-key': apiToken
