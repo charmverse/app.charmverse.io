f7329827861ac1e0af286a9f4349632102d4d43c
Safwan Shaheer <devorein00@gmail.com>
2022-08-18 21:26:19 +0600

    Show individual deepdao organization data

--- components/profile/components/AggregatedData.tsx
+++ components/profile/components/AggregatedData.tsx
@@ -1,9 +1,13 @@
-import { Box, CircularProgress, Grid, Paper, Typography } from '@mui/material';
+import { Box, CircularProgress, Grid, IconButton, Paper, Stack, Typography } from '@mui/material';
 import charmClient from 'charmClient';
+import { DeepDaoOrganization } from 'lib/deepdao/client';
 import useSWRImmutable from 'swr/immutable';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ExpandLessIcon from '@mui/icons-material/ExpandLess';
+import { useState } from 'react';
 import { UserDetailsProps } from './UserDetails';
 
-export function AggregatedDataItem ({ value, label }: {value: number, label: string}) {
+export function AggregatedDataItem ({ value, label }: { value: number, label: string }) {
 
   return (
     <Grid item xs={6}>
@@ -39,6 +43,34 @@ export function AggregatedDataItem ({ value, label }: {value: number, label: str
   );
 }
 
+function showDateWithMonthAndYear (dateInput: Date | string) {
+  const date = typeof dateInput === 'string' ? new Date(dateInput) : dateInput;
+  return `${date.toLocaleString('default', {
+    month: 'long'
+  })} ${date.getFullYear()}`;
+}
+
+function DeepDaoOrganization ({ organization, earliestEventDate, latestEventDate }:
+  { organization: DeepDaoOrganization, earliestEventDate: string, latestEventDate: string }) {
+  const [isCollapsed, setIsCollapsed] = useState(true);
+  return (
+    <Stack key={organization.organizationId}>
+      <Stack flexDirection='row' justifyContent='space-between'>
+        <Typography variant='h5'>{organization.name}</Typography>
+        <IconButton
+          size='small'
+          onClick={() => setIsCollapsed(!isCollapsed)}
+        >
+          {isCollapsed ? <ExpandMoreIcon fontSize='small' /> : <ExpandLessIcon fontSize='small' />}
+        </IconButton>
+      </Stack>
+      <Typography variant='subtitle1'>{showDateWithMonthAndYear(earliestEventDate) ?? '?'} - {showDateWithMonthAndYear(latestEventDate) ?? '?'}</Typography>
+
+    </Stack>
+  );
+
+}
+
 export function AggregatedData ({ user }: Pick<UserDetailsProps, 'user'>) {
   const { data, isValidating } = useSWRImmutable(user ? `userAggregatedData/${user.id}` : null, () => {
     return charmClient.getAggregatedData(user.id);
@@ -56,6 +88,43 @@ export function AggregatedData ({ user }: Pick<UserDetailsProps, 'user'>) {
     return null;
   }
 
+  const organizationsRecord: Record<string, DeepDaoOrganization> = data.organizations
+    .reduce((acc, org) => ({ ...acc, [org.organizationId]: org }), {});
+
+  // Sort the proposals and votes based on their created at date and attach organization data with it
+  const events = [...data.proposals, ...data.votes]
+    .sort((eventA, eventB) => eventA.createdAt > eventB.createdAt ? -1 : 1)
+    .map(event => ({ ...event, organization: organizationsRecord[event.organizationId] }));
+
+  const organizationIdSet: Set<string> = new Set();
+  const sortedOrganizationIds: string[] = [];
+  const organizationEventDates: Record<string, {
+    oldest: string
+    latest: string
+  }> = {};
+
+  events.forEach(event => {
+    if (!organizationIdSet.has(event.organizationId)) {
+      sortedOrganizationIds.push(event.organizationId);
+      organizationIdSet.add(event.organizationId);
+    }
+
+    if (!organizationEventDates[event.organizationId]) {
+      organizationEventDates[event.organizationId] = {
+        oldest: event.createdAt,
+        latest: event.createdAt
+      };
+    }
+    else if (organizationEventDates[event.organizationId].latest < event.createdAt) {
+      organizationEventDates[event.organizationId].latest = event.createdAt;
+    }
+    else if (organizationEventDates[event.organizationId].oldest > event.createdAt) {
+      organizationEventDates[event.organizationId].oldest = event.createdAt;
+    }
+  });
+
+  const sortedOrganizations = sortedOrganizationIds.map(organizationId => organizationsRecord[organizationId]);
+
   return (
     <Grid container display='flex' gap={2} flexDirection='column'>
       <Box display='flex' gap={2} mr={2}>
@@ -66,6 +135,17 @@ export function AggregatedData ({ user }: Pick<UserDetailsProps, 'user'>) {
         <AggregatedDataItem label='proposals' value={data.totalProposals} />
         <AggregatedDataItem label={data.bounties > 1 ? 'bounties' : 'bounty'} value={data.bounties} />
       </Box>
+
+      <Stack gap={2}>
+        {sortedOrganizations.map(organization => (
+          <DeepDaoOrganization
+            organization={organization}
+            key={organization.organizationId}
+            latestEventDate={organizationEventDates[organization.organizationId]?.latest}
+            earliestEventDate={organizationEventDates[organization.organizationId]?.oldest}
+          />
+        ))}
+      </Stack>
     </Grid>
   );
 }
