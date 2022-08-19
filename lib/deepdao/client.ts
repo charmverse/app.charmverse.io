38559737add176e31cbc30082e1fea3a5d8c7d38
MEB <mo@anyahq.co>
2022-08-19 13:25:12 +0200

    Comment out testing addresses

--- lib/deepdao/client.ts
+++ lib/deepdao/client.ts
@@ -5,7 +5,8 @@ import { GetParticipationScoreResponse, GetProfileResponse } from './interfaces'
 export const { DEEPDAO_API_KEY, DEEP_DAO_BASE_URL } = process.env;
 
 export async function getParticipationScore (address: string, apiToken = DEEPDAO_API_KEY): Promise<GetParticipationScoreResponse | null> {
-  address = '0xef8305e140ac520225daf050e2f71d5fbcc543e7';
+  // Use this address for testing as it has alot of on-chain data
+  // address = '0x155b6485305ccab44ef7da58ac886c62ce105cf9';
 
   if (!apiToken) {
     log.debug('Skip request: No API Key for DeepDAO');
@@ -21,6 +22,9 @@ export async function getParticipationScore (address: string, apiToken = DEEPDAO
 }
 
 export async function getProfile (address: string, apiToken = DEEPDAO_API_KEY): Promise<GetProfileResponse | null> {
+  // Use this address for testing as it has alot of on-chain data
+  // address = '0x155b6485305ccab44ef7da58ac886c62ce105cf9';
+
   if (!apiToken) {
     log.debug('Skip request: No API Key for DeepDAO');
     return null;
