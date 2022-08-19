dd8362262f126a3970c3e0407d6735057fa54ef8
Safwan Shaheer <devorein00@gmail.com>
2022-08-19 17:41:34 +0600

    Deepdao orgs/votes/proposals api integration (#812)
    * Fetch deepdao profile data
    
    * Show individual deepdao organization data
    
    * Display organization votes
    
    * Show vote verdict icon
    
    * Display organization proposals
    
    * Sort organization votes and proposals
    
    * Rename types file to interfaces
    
    * Fixed broken tests
    
    * Fix import structure
    
    * Comment out testing addresses
    
    Co-authored-by: MEB <mo@anyahq.co>

--- components/profile/components/AggregatedData.tsx
+++ components/profile/components/AggregatedData.tsx
@@ -1,9 +1,18 @@
-import { Box, CircularProgress, Grid, Paper, Typography } from '@mui/material';
+import { Box, CircularProgress, Collapse, Divider, Grid, IconButton, Paper, Stack, Tab, Tabs, Typography } from '@mui/material';
 import charmClient from 'charmClient';
+import { useTheme } from '@emotion/react';
+import { DeepDaoOrganization, DeepDaoProposal, DeepDaoVote } from 'lib/deepdao/interfaces';
 import useSWRImmutable from 'swr/immutable';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ExpandLessIcon from '@mui/icons-material/ExpandLess';
+import HowToVoteIcon from '@mui/icons-material/HowToVote';
+import ForumIcon from '@mui/icons-material/Forum';
+import { useState } from 'react';
+import ThumbUpIcon from '@mui/icons-material/ThumbUp';
+import ThumbDownIcon from '@mui/icons-material/ThumbDown';
 import { UserDetailsProps } from './UserDetails';
 
-export function AggregatedDataItem ({ value, label }: {value: number, label: string}) {
+export function AggregatedDataItem ({ value, label }: { value: number, label: string }) {
 
   return (
     <Grid item xs={6}>
@@ -39,6 +48,112 @@ export function AggregatedDataItem ({ value, label }: {value: number, label: str
   );
 }
 
+function showDateWithMonthAndYear (dateInput: Date | string, showDate?: boolean) {
+  const date = typeof dateInput === 'string' ? new Date(dateInput) : dateInput;
+  return `${date.toLocaleString('default', {
+    month: 'long'
+  })}${showDate ? ` ${date.getDate()},` : ''} ${date.getFullYear()}`;
+}
+
+interface DeepDaoOrganizationRowProps {
+  organization: DeepDaoOrganization
+  earliestEventDate: string
+  latestEventDate: string
+  proposals: DeepDaoProposal[]
+  votes: DeepDaoVote[]
+}
+
+const TASK_TABS = [
+  { icon: <HowToVoteIcon />, label: 'Votes', type: 'vote' },
+  { icon: <ForumIcon />, label: 'Proposals', type: 'proposal' }
+] as const;
+
+function DeepDaoOrganizationRow ({ votes, proposals, organization, earliestEventDate, latestEventDate }: DeepDaoOrganizationRowProps) {
+  const [isCollapsed, setIsCollapsed] = useState(true);
+  const [currentTask, setCurrentTask] = useState<'vote' | 'proposal'>('vote');
+  const theme = useTheme();
+
+  proposals = proposals.sort((proposalA, proposalB) => proposalA.createdAt > proposalB.createdAt ? -1 : 1);
+  votes = votes.sort((voteA, voteB) => voteA.createdAt > voteB.createdAt ? -1 : 1);
+
+  return (
+    <Stack key={organization.organizationId} gap={1}>
+      <Stack flexDirection='row' justifyContent='space-between'>
+        <Typography fontWeight={500} variant='h5'>{organization.name}</Typography>
+        <IconButton
+          size='small'
+          onClick={() => setIsCollapsed(!isCollapsed)}
+        >
+          {isCollapsed ? <ExpandMoreIcon fontSize='small' /> : <ExpandLessIcon fontSize='small' />}
+        </IconButton>
+      </Stack>
+      <Typography>{showDateWithMonthAndYear(earliestEventDate) ?? '?'} - {showDateWithMonthAndYear(latestEventDate) ?? '?'}</Typography>
+
+      <Collapse in={!isCollapsed}>
+        <Box>
+          <Tabs
+            sx={{
+              mb: 2
+            }}
+            indicatorColor='primary'
+            value={TASK_TABS.findIndex(taskTab => taskTab.type === currentTask)}
+          >
+            {TASK_TABS.map(task => (
+              <Tab
+                component='div'
+                disableRipple
+                iconPosition='start'
+                icon={task.icon}
+                key={task.label}
+                sx={{
+                  px: 1.5,
+                  fontSize: 14,
+                  minHeight: 0,
+                  '&.MuiTab-root': {
+                    color: theme.palette.secondary.main
+                  }
+                }}
+                label={task.label}
+                onClick={() => {
+                  setCurrentTask(task.type);
+                }}
+              />
+            ))}
+          </Tabs>
+          {(currentTask === 'vote' ? (
+            <Stack gap={2}>
+              {votes.map((vote, voteNumber) => (
+                <Stack key={vote.voteId} flexDirection='row' justifyContent='space-between'>
+                  <Stack flexDirection='row' gap={1} alignItems='center'>
+                    {vote.successful ? <ThumbUpIcon color='success' fontSize='small' /> : <ThumbDownIcon color='error' fontSize='small' />}
+                    <Typography fontWeight={500}>{voteNumber + 1}.</Typography>
+                    <Typography>{vote.title}</Typography>
+                  </Stack>
+                  <Typography variant='subtitle1'>{showDateWithMonthAndYear(vote.createdAt, true)}</Typography>
+                </Stack>
+              ))}
+            </Stack>
+          ) : (
+            <Stack gap={2}>
+              {proposals.map((proposal, proposalNumber) => (
+                <Stack key={proposal.proposalId} flexDirection='row' justifyContent='space-between'>
+                  <Stack flexDirection='row' gap={1} alignItems='center'>
+                    {proposal.outcome === proposal.voteChoice ? <ThumbUpIcon color='success' fontSize='small' /> : <ThumbDownIcon color='error' fontSize='small' />}
+                    <Typography fontWeight={500}>{proposalNumber + 1}.</Typography>
+                    <Typography>{proposal.title}</Typography>
+                  </Stack>
+                  <Typography variant='subtitle1'>{showDateWithMonthAndYear(proposal.createdAt, true)}</Typography>
+                </Stack>
+              ))}
+            </Stack>
+          ))}
+        </Box>
+      </Collapse>
+    </Stack>
+  );
+
+}
+
 export function AggregatedData ({ user }: Pick<UserDetailsProps, 'user'>) {
   const { data, isValidating } = useSWRImmutable(user ? `userAggregatedData/${user.id}` : null, () => {
     return charmClient.getAggregatedData(user.id);
@@ -48,7 +163,6 @@ export function AggregatedData ({ user }: Pick<UserDetailsProps, 'user'>) {
     return (
       <Box display='flex' alignItems='center' gap={1}>
         <CircularProgress size={24} />
-        <Typography variant='subtitle1' color='secondary'>Fetching data</Typography>
       </Box>
     );
   }
@@ -57,16 +171,86 @@ export function AggregatedData ({ user }: Pick<UserDetailsProps, 'user'>) {
     return null;
   }
 
+  const organizationsRecord: Record<string, DeepDaoOrganization & {proposals: DeepDaoProposal[], votes: DeepDaoVote[]}> = data.organizations
+    .reduce((acc, org) => ({ ...acc,
+      [org.organizationId]: {
+        ...org,
+        proposals: [],
+        votes: []
+      } }), {});
+
+  data.proposals.forEach(proposal => {
+    organizationsRecord[proposal.organizationId].proposals.push(proposal);
+  });
+
+  data.votes.forEach(vote => {
+    organizationsRecord[vote.organizationId].votes.push(vote);
+  });
+
+  // Sort the proposals and votes based on their created at date and attach organization data with it
+  const events = [...data.proposals, ...data.votes]
+    .sort((eventA, eventB) => eventA.createdAt > eventB.createdAt ? 1 : -1)
+    .map(event => ({ ...event, organization: organizationsRecord[event.organizationId] }));
+
+  const organizationIdSet: Set<string> = new Set();
+  const sortedOrganizationIds: string[] = [];
+  const organizationEventDates: Record<string, {
+    oldest: string
+    latest: string
+  }> = {};
+
+  events.forEach(event => {
+    if (!organizationIdSet.has(event.organizationId)) {
+      sortedOrganizationIds.push(event.organizationId);
+      organizationIdSet.add(event.organizationId);
+    }
+
+    if (!organizationEventDates[event.organizationId]) {
+      organizationEventDates[event.organizationId] = {
+        oldest: event.createdAt,
+        latest: event.createdAt
+      };
+    }
+    else if (organizationEventDates[event.organizationId].latest < event.createdAt) {
+      organizationEventDates[event.organizationId].latest = event.createdAt;
+    }
+    else if (organizationEventDates[event.organizationId].oldest > event.createdAt) {
+      organizationEventDates[event.organizationId].oldest = event.createdAt;
+    }
+  });
+
+  const sortedOrganizations = sortedOrganizationIds.reverse().map(organizationId => organizationsRecord[organizationId]);
+
   return (
     <Grid container display='flex' gap={2} flexDirection='column'>
       <Box display='flex' gap={2} mr={2}>
         <AggregatedDataItem label='communities' value={data.daos} />
-        <AggregatedDataItem label='votes' value={data.votes} />
+        <AggregatedDataItem label='votes' value={data.totalVotes} />
       </Box>
       <Box display='flex' gap={2} mr={2}>
-        <AggregatedDataItem label='proposals' value={data.proposals} />
+        <AggregatedDataItem label='proposals' value={data.totalProposals} />
         <AggregatedDataItem label={data.bounties > 1 ? 'bounties' : 'bounty'} value={data.bounties} />
       </Box>
+
+      <Stack gap={2}>
+        {sortedOrganizations.map(organization => (
+          <Box
+            key={organization.organizationId}
+          >
+            <DeepDaoOrganizationRow
+              votes={organizationsRecord[organization.organizationId].votes}
+              proposals={organizationsRecord[organization.organizationId].proposals}
+              organization={organization}
+              latestEventDate={organizationEventDates[organization.organizationId]?.latest}
+              earliestEventDate={organizationEventDates[organization.organizationId]?.oldest}
+            />
+            <Divider sx={{
+              mt: 2
+            }}
+            />
+          </Box>
+        ))}
+      </Stack>
     </Grid>
   );
 }
