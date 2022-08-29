1822fbf4d7aae129de8a589a5dcdbd3d2339a3e9
Matt Casey <mattwad@gmail.com>
2022-08-28 21:19:51 -0600

    add post method

--- lib/collabland/collabland.ts
+++ lib/collabland/collabland.ts
@@ -1,38 +1,5 @@
-import fetch from 'adapters/http/fetch.server';
-import log from 'lib/log';
 
-const DOMAIN = 'https://api-qa.collab.land';
-const API_KEY = process.env.COLLAB_API_KEY as string;
-
-interface CollablandCredentialResponse {
-  hash: string;
-  verifiableCredential: {
-    credentialSubject: {
-      discordUserId: string; // '936350836110536735',
-      discordUserName: string; // 'ghostpepper#7801',
-      discordUserAvatar: string; // 'https://cdn.discordapp.com/avatars/936350836110536735/2ccdf2f75c168e3eb54f96c0981a5e86.webp',
-      discordGuildId: string; // '943256209488748614',
-      discordGuildName: string; // 'CollabLand Token Gated Server',
-      discordGuildAvatar: string; // 'https://cdn.discordapp.com/icons/943256209488748614/4226079b4690b9a8bf1294f44402366a.webp',
-      discordRoleId: string; // '-IflGlzZF95dnzRy8Ljsm:994305887286083705',
-      discordRoleName: string; // 'SUPER-0',
-      description: string; // 'ghostpepper#7801 is granted a guest pass SUPER-0 in Discord community CollabLand Token Gated Server',
-      exp?: number; // 1662395622,
-      id: string; // '936350836110536735'
-    },
-    issuer: {
-      id: string; // 'did:ethr:rinkeby:0x038e829e042560e41de5a1f7c12aded55f95ea903465f7f9c8a805ed83f8cdc936'
-    },
-    id: string; // 'Z6s-qJzLBpNj9oAPKpcCN',
-    type: 'VerifiableCredential' | 'GuestPass'[],
-    '@context': 'https://www.w3.org/2018/credentials/v1'[],
-    issuanceDate: string;
-    proof: {
-      'type': 'JwtProof2020';
-      'jwt': string;
-    }
-  }
-}
+import * as client from './collablandClient';
 
 export interface CollablandCredential {
   id: string;
@@ -56,20 +23,7 @@ export interface CollablandCredential {
 
 export async function getCredentials ({ aeToken }: { aeToken: string }): Promise<CollablandCredential[]> {
 
-  if (!API_KEY) {
-    log.warn('No API Key provided for collab.land');
-    return [];
-  }
-
-  const res = await fetch<CollablandCredentialResponse[]>(`${DOMAIN}/veramo/vcs`, {
-    method: 'GET',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-      'X-API-KEY': API_KEY,
-      Authorization: `AE ${aeToken}`
-    }
-  });
+  const res = await client.getCredentials({ aeToken });
 
   const credentials = res.map(({ verifiableCredential }): CollablandCredential => {
     return {
@@ -108,4 +62,8 @@ export async function getCredentials ({ aeToken }: { aeToken: string }): Promise
     }, []);
 
   return credentials;
+}
+
+export async function createCredential ({ aeToken }: { aeToken: string }) {
+  await client.createCredential({ aeToken });
 }
