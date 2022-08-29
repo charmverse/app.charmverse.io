0eb4e869f97ef8717ab023e48d8f7390f51fb448
MEB <mo@anyahq.co>
2022-08-29 13:15:26 +0200

    Comment out non headless test

--- __e2e__/login.spec.ts
+++ __e2e__/login.spec.ts
@@ -1,30 +1,6 @@
 import { test, expect } from '@playwright/test';
 import { v4 } from 'uuid';
-import { baseUrl, registerUser } from 'testing/mockApiCall';
-
-// test('public page should open', async ({ page }) => {
-
-//   const rawDomain = baseUrl.replace(/^https?:\/\//, '').replace(/:\d{4}/, '');
-
-//   console.log(rawDomain);
-
-//   // Arrange
-//   const walletAddress = v4();
-
-//   await page.request.post(`${baseUrl}/api/profile`, {
-//     data: {
-//       address: walletAddress
-//     }
-//   });
-
-//   await page.goto(baseUrl as string);
-//   // Act
-
-//   // Assert
-
-//   expect(true).toBe(true);
-
-// });
+import { baseUrl } from 'testing/mockApiCall';
 
 test('login - sets a cookie inside the user browser', async ({ page }) => {
 
--- __e2e__/record-custom.spec.ts
+++ __e2e__/record-custom.spec.ts
@@ -1,59 +1,59 @@
 import { chromium } from '@playwright/test';
 import { LoggedInUser } from 'models';
-import { baseUrl, registerUser } from 'testing/mockApiCall';
+import { baseUrl } from 'testing/mockApiCall';
 import { v4 } from 'uuid';
 
-(async () => {
-  // ARRANGE
-  // Make sure to run headed.
-  const browser = await chromium.launch({ headless: false });
+// (async () => {
+//   // ARRANGE
+//   // Make sure to run headed.
+//   const browser = await chromium.launch({ headless: false });
 
-  // Setup context however you like.
-  const context = await browser.newContext({ /* pass any options */ });
-  await context.route('**/*', route => route.continue());
+//   // Setup context however you like.
+//   const context = await browser.newContext({ /* pass any options */ });
+//   await context.route('**/*', route => route.continue());
 
-  // Pause the page, and start recording manually.
-  const page = await context.newPage();
+//   // Pause the page, and start recording manually.
+//   const page = await context.newPage();
 
-  const walletAddress = v4();
+//   const walletAddress = v4();
 
-  const profile: LoggedInUser = await (page.request.post(`${baseUrl}/api/profile`, {
-    data: {
-      address: walletAddress
-    }
-  }).then(res => res.json()));
+//   const profile: LoggedInUser = await (page.request.post(`${baseUrl}/api/profile`, {
+//     data: {
+//       address: walletAddress
+//     }
+//   }).then(res => res.json()));
 
-  const domain = `domain-${v4()}`;
+//   const domain = `domain-${v4()}`;
 
-  await page.request.post(`${baseUrl}/api/spaces`, {
-    data: {
-      author: {
-        connect: {
-          id: profile.id
-        }
-      },
-      createdAt: new Date(),
-      updatedAt: new Date(),
-      updatedBy: profile.id,
-      spaceRoles: {
-        create: [{
-          isAdmin: true,
-          user: {
-            connect: {
-              id: profile!.id
-            }
-          }
-        }]
-      },
-      domain,
-      name: 'Testing space'
-    }
-  });
+//   await page.request.post(`${baseUrl}/api/spaces`, {
+//     data: {
+//       author: {
+//         connect: {
+//           id: profile.id
+//         }
+//       },
+//       createdAt: new Date(),
+//       updatedAt: new Date(),
+//       updatedBy: profile.id,
+//       spaceRoles: {
+//         create: [{
+//           isAdmin: true,
+//           user: {
+//             connect: {
+//               id: profile!.id
+//             }
+//           }
+//         }]
+//       },
+//       domain,
+//       name: 'Testing space'
+//     }
+//   });
 
-  // ACT
+//   // ACT
 
-  // ASSERT
-  page.goto(baseUrl as string);
+//   // ASSERT
+//   page.goto(baseUrl as string);
 
-  await page.pause();
-})();
+//   await page.pause();
+// })();
