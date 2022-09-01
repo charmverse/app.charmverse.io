e8c3d3f27e0a8f9b1decfc5ab0aa27f3d568fa25
Safwan Shaheer <devorein00@gmail.com>
2022-09-02 00:39:13 +0600

    Updated migration script

--- scripts/migrateProposal.ts
+++ scripts/migrateProposal.ts
@@ -1,3 +1,4 @@
+import { ProposalStatus } from '@prisma/client';
 import { prisma } from 'db';
 
 async function migrateProposal () {
@@ -7,11 +8,7 @@ async function migrateProposal () {
     },
     select: {
       id: true,
-      author: {
-        select: {
-          id: true
-        }
-      },
+      createdBy: true,
       spaceId: true
     }
   });
@@ -19,23 +16,36 @@ async function migrateProposal () {
   await prisma.$transaction([
     prisma.proposal.createMany({
       data: proposalPages.map(proposalPage => ({
-        createdBy: proposalPage.author.id,
+        id: proposalPage.id,
+        createdBy: proposalPage.createdBy,
         spaceId: proposalPage.spaceId,
-        status: 'private_draft'
+        status: 'private_draft' as ProposalStatus
       }))
     }),
     prisma.proposalAuthor.createMany({
       data: proposalPages.map(proposalPage => ({
         proposalId: proposalPage.id,
-        userId: proposalPage.author.id
+        userId: proposalPage.createdBy
       }))
     }),
     prisma.proposalReviewer.createMany({
       data: proposalPages.map(proposalPage => ({
         proposalId: proposalPage.id,
-        userId: proposalPage.author.id
+        userId: proposalPage.createdBy
       }))
-    })
+    }),
+    ...proposalPages.map(proposalPage => prisma.page.update({
+      where: {
+        id: proposalPage.id
+      },
+      data: {
+        proposal: {
+          connect: {
+            id: proposalPage.id
+          }
+        }
+      }
+    }))
   ]);
 }
 
