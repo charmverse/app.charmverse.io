a2a8587606029323273be2f731f01831984ccef2
Matt Casey <mattwad@gmail.com>
2022-09-08 02:03:34 -0600

    fix loading of new workspace

--- components/[pageId]/EditorPage/EditorPage.tsx
+++ components/[pageId]/EditorPage/EditorPage.tsx
@@ -12,7 +12,7 @@ import BoardPage from '../BoardPage';
 import DocumentPage from '../DocumentPage';
 
 export default function EditorPage ({ pageId }: { pageId: string }) {
-  const { setIsEditing, pages, setCurrentPageId, setPages, getPagePermissions } = usePages();
+  const { setIsEditing, pages, currentPageId, setCurrentPageId, setPages, getPagePermissions } = usePages();
   const [, setTitleState] = usePageTitle();
   const [pageNotFound, setPageNotFound] = useState(false);
   const [space] = useCurrentSpace();
@@ -25,7 +25,7 @@ export default function EditorPage ({ pageId }: { pageId: string }) {
   useEffect(() => {
     async function main () {
       setIsAccessDenied(false);
-      if (pageId && pagesLoaded && space) {
+      if (pageId && pagesLoaded && space && pageId !== currentPageId) {
         try {
           const page = await charmClient.getPage(pageId, space.id);
           if (page) {
