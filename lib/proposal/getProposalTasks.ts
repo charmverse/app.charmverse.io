1d1a76874c2458b0c7ada3bfa46dc353053ea24a
Safwan Shaheer <devorein00@gmail.com>
2022-09-09 19:06:51 +0600

    Get proposal tasks based on spaces

--- lib/proposal/getProposalTasks.ts
+++ lib/proposal/getProposalTasks.ts
@@ -1,15 +1,50 @@
 import { prisma } from 'db';
+import { accessiblePagesByPermissionsQuery } from 'lib/pages/server';
 import { ExtendedProposal } from './interface';
 
 export async function getProposalTasks (userId: string): Promise<ExtendedProposal[]> {
-  const proposals = await prisma.proposal.findMany({
-    include: {
-      authors: true,
-      reviewers: true,
-      page: true,
-      space: true
+  // Get all the space the user is part of
+  const spaceRoles = await prisma.spaceRole.findMany({
+    where: {
+      userId
+    },
+    select: {
+      spaceId: true
     }
   });
+
+  const spaceIds = spaceRoles.map(spaceRole => spaceRole.spaceId);
+
+  const proposals = (await Promise.all(spaceIds.map(spaceId => prisma.proposal.findMany({
+    where: {
+      spaceId,
+      page: {
+        deletedAt: null,
+        OR: [{
+          permissions: accessiblePagesByPermissionsQuery({
+            spaceId,
+            userId
+          })
+        }, {
+          space: {
+            spaceRoles: {
+              some: {
+                spaceId,
+                userId,
+                isAdmin: true
+              }
+            }
+          }
+        }]
+      }
+    },
+    include: {
+      page: true,
+      space: true,
+      authors: true,
+      reviewers: true
+    }
+  })))).flat();
 
   return proposals as ExtendedProposal[];
 }
