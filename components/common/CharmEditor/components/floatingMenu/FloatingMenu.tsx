00866201a1fd7e724fa643c7834f2c4d51017876
MEB <mo@anyahq.co>
2022-09-10 14:26:01 +0200

    Rely on external page id in Floating menu

--- components/common/CharmEditor/components/floatingMenu/FloatingMenu.tsx
+++ components/common/CharmEditor/components/floatingMenu/FloatingMenu.tsx
@@ -1,14 +1,13 @@
 import { PluginKey } from '@bangle.dev/core';
 import { FloatingMenu } from '@bangle.dev/react-menu';
+import { PageType } from '@prisma/client';
+import { useCurrentSpacePermissions } from 'hooks/useCurrentSpacePermissions';
 import { usePages } from 'hooks/usePages';
 import { useSnackbar } from 'hooks/useSnackbar';
-import { AllowedPagePermissions } from 'lib/permissions/pages/available-page-permissions.class';
-import { useCurrentSpacePermissions } from 'hooks/useCurrentSpacePermissions';
-import { PageType } from '@prisma/client';
 import { SubMenu } from '../@bangle.dev/react-menu/floating-menu';
 import { LinkSubMenu } from '../@bangle.dev/react-menu/LinkSubMenu';
 import { Menu } from '../@bangle.dev/react-menu/Menu';
-import { BoldButton, CalloutButton, CodeButton, InlineCommentButton, FloatingLinkButton, HeadingButton, ItalicButton, ParagraphButton, StrikeButton, UnderlineButton, InlineVoteButton } from '../@bangle.dev/react-menu/MenuButtons';
+import { BoldButton, CalloutButton, CodeButton, FloatingLinkButton, HeadingButton, InlineCommentButton, InlineVoteButton, ItalicButton, ParagraphButton, StrikeButton, UnderlineButton } from '../@bangle.dev/react-menu/MenuButtons';
 import { MenuGroup } from '../@bangle.dev/react-menu/MenuGroup';
 import { InlineCommentSubMenu } from '../inlineComment/InlineComment.components';
 import InlineVoteSubMenu from '../inlineVote/components/InlineVoteSubmenu';
@@ -16,21 +15,22 @@ import InlineVoteSubMenu from '../inlineVote/components/InlineVoteSubmenu';
 type FloatingMenuVariant = 'defaultMenu' | 'linkSubMenu' | 'inlineCommentSubMenu' | 'commentOnlyMenu';
 
 interface Props {
-  enableComments?: boolean,
-  enableVoting?: boolean,
-  pluginKey: PluginKey,
-  inline?: boolean,
-  pageType?: PageType
+  enableComments?: boolean;
+  enableVoting?: boolean;
+  pluginKey: PluginKey;
+  inline?: boolean;
+  pageType?: PageType;
+  pageId: string;
 }
 
 export default function FloatingMenuComponent (
   {
-    pluginKey, enableComments = true, enableVoting = false, inline = false, pageType }: Props
+    pageId, pluginKey, enableComments = true, enableVoting = false, inline = false, pageType }: Props
 
 ) {
   const { showMessage } = useSnackbar();
-  const { getPagePermissions, currentPageId } = usePages();
-  const permissions = currentPageId ? getPagePermissions(currentPageId) : new AllowedPagePermissions();
+  const { getPagePermissions } = usePages();
+  const permissions = getPagePermissions(pageId);
   const [currentUserPermissions] = useCurrentSpacePermissions();
   const displayInlineCommentButton = !inline && permissions.comment && enableComments && pageType !== 'card_template';
 
