58935496f4ecd4defed9cde148baf22313797c21
Safwan Shaheer <devorein00@gmail.com>
2022-09-13 18:11:25 +0600

    Added workspace event model

---
+++ prisma/migrations/20220913121013_workspace_event/migration.sql
@@ -0,0 +1,20 @@
+-- CreateTable
+CREATE TABLE "WorkspaceEvent" (
+    "id" UUID NOT NULL,
+    "actorId" UUID,
+    "type" TEXT NOT NULL,
+    "meta" JSONB,
+    "spaceId" UUID NOT NULL,
+    "pageId" UUID NOT NULL,
+
+    CONSTRAINT "WorkspaceEvent_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "WorkspaceEvent" ADD CONSTRAINT "WorkspaceEvent_actorId_fkey" FOREIGN KEY ("actorId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "WorkspaceEvent" ADD CONSTRAINT "WorkspaceEvent_spaceId_fkey" FOREIGN KEY ("spaceId") REFERENCES "Space"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "WorkspaceEvent" ADD CONSTRAINT "WorkspaceEvent_pageId_fkey" FOREIGN KEY ("pageId") REFERENCES "Page"("id") ON DELETE CASCADE ON UPDATE CASCADE;
