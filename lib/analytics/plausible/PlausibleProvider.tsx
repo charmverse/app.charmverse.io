1ba377af82b06d6ef925988ea1d8bda60ca36973
Marek <m.sadura@outlook.com>
2022-09-16 12:51:07 +0200

    Plausible provider

---
+++ lib/analytics/plausible/PlausibleProvider.tsx
@@ -0,0 +1,25 @@
+import { ReactNode } from 'react';
+import Plausible from 'next-plausible';
+import { PLAUSIBLE_DOMAIN } from 'lib/analytics/constatnts';
+
+type Props = { children: ReactNode; };
+
+const domain = process.env.NODE_ENV === 'production'
+  ? PLAUSIBLE_DOMAIN
+  : 'localhost:3000';
+const isLocalhost = domain.indexOf('localhost') !== -1;
+
+export default function PlausibleProvider ({ children }: Props) {
+  return (
+    <Plausible
+      domain={domain}
+      trackOutboundLinks
+      trackFileDownloads
+      // For testing purposes - set tracking localhost to true
+      enabled={isLocalhost || undefined}
+      trackLocalhost={isLocalhost}
+    >
+      {children}
+    </Plausible>
+  );
+}
