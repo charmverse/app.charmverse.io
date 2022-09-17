12736bf128a8f34692e7c47cec4db83050542192
Safwan Shaheer <devorein00@gmail.com>
2022-09-17 21:07:10 +0600

    Fix nft url

--- components/profile/PublicProfile.tsx
+++ components/profile/PublicProfile.tsx
@@ -23,13 +23,14 @@ function transformPoap (poap: ExtendedPoap): Collectable {
 }
 
 function transformNft (nft: NftData): Collectable {
+  const tokenId = nft.tokenId.startsWith('0x') ? parseInt(nft.tokenId, 16) : nft.tokenId;
   return {
     type: 'nft',
     date: nft.timeLastUpdated,
     id: nft.id,
     image: nft.image ?? nft.imageThumb,
     title: nft.title,
-    link: `https://opensea.io/assets/ethereum/${nft.contract}/${nft.tokenId}`,
+    link: nft.chainId === 42161 ? `https://stratosnft.io/assets/${nft.contract}/${tokenId}` : `https://opensea.io/assets/${nft.chainId === 1 ? 'ethereum' : 'matic'}/${nft.contract}/${tokenId}`,
     isHidden: nft.isHidden
   };
 }
