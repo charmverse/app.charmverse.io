d76c0a70f09b77e3c7c3854604987e74bf048947
Marek <m.sadura@outlook.com>
2022-09-20 11:17:42 +0200

    Refactor

--- lib/analytics/mixpanel/server.ts
+++ lib/analytics/mixpanel/server.ts
@@ -1,10 +1,14 @@
 import Mixpanel from 'mixpanel';
 import { MixpanelEvent, MixpanelEventName } from './interfaces/index';
 
-const mixpanelInstance = Mixpanel.init(process.env.MIXPANEL_API_KEY as string);
+let mixpanelInstance: Mixpanel.Mixpanel | null = null;
+
+if (process.env.MIXPANEL_API_KEY) {
+  mixpanelInstance = Mixpanel.init(process.env.MIXPANEL_API_KEY as string);
+}
 
 function track<T extends MixpanelEventName> (eventName: T, params: MixpanelEvent[T]) {
-  mixpanelInstance.track(eventName, params);
+  mixpanelInstance?.track(eventName, params);
 }
 
 export const mixpanel = {
