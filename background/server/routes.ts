9225f6adaaf6c791adaf54a8fac631535202ad5a
yiwork Ying-Chi Connie I <cloudme@halfpast9.com>
2022-09-21 15:53:58 -0400

    adding a count for healthcheck endpoint, hopefully will give us something

--- background/server/routes.ts
+++ background/server/routes.ts
@@ -1,8 +1,10 @@
 import Koa from 'koa';
 import log from 'lib/log';
+import { gauge, count } from 'lib/metrics';
 
 export async function healthCheck (ctx: Koa.Context) {
   ctx.body = 'hello world';
+  count('healthcheck_ct', 1);
 }
 
 export async function errorHandler (ctx: Koa.Context, next: () => Promise<any>) {
