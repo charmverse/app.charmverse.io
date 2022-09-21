0379fc751d87006e37ffaa207cca655ccd63c392
melboudi <mo@anyahq.co>
2022-09-21 16:24:32 +0200

    Add logic for proposal template instantiation

--- lib/proposal/createProposal.ts
+++ lib/proposal/createProposal.ts
@@ -1,53 +1,134 @@
 import type { Prisma } from '@prisma/client';
 import { prisma } from 'db';
 import { v4 } from 'uuid';
+import { DataNotFoundError, InsecureOperationError } from 'lib/utilities/errors';
+import { getProposal } from './getProposal';
+import type { IPageWithPermissions, PageWithProposal } from '../pages';
+import { getPagePath } from '../pages';
 
 export interface CreateProposalInput {
-  pageCreateInput: Prisma.PageCreateInput
+  pageCreateInput?: Prisma.PageCreateInput
   spaceId: string
   userId: string
+  templateId?: string
 }
 
 export async function createProposal ({
   pageCreateInput,
   userId,
   spaceId
-}: CreateProposalInput) {
+}: Omit<Required<CreateProposalInput>, 'templateId'>): Promise<IPageWithPermissions & PageWithProposal>
+export async function createProposal ({
+  userId,
+  spaceId,
+  templateId
+}: Omit<Required<CreateProposalInput>, 'pageCreateInput'>): Promise<IPageWithPermissions & PageWithProposal>
+export async function createProposal ({
+  pageCreateInput,
+  userId,
+  spaceId,
+  templateId
+}: CreateProposalInput): Promise<IPageWithPermissions & PageWithProposal> {
   const proposalId = v4();
-  // Making the page id same as proposalId
-  const pageData: Prisma.PageCreateInput = {
-    ...pageCreateInput,
-    id: proposalId
-  };
-  // Using a transaction to ensure both the proposal and page gets created together
-  const createdPage = await prisma.page.create({
-    data: {
-      ...pageData,
+
+  const proposalTemplate = templateId ? await getProposal({ proposalId: templateId }) : null;
+
+  if (templateId && !proposalTemplate) {
+    throw new DataNotFoundError(`Proposal template with id ${templateId} not found`);
+    // Making the page id same as proposalId
+  }
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  else if (templateId && spaceId !== proposalTemplate!.id) {
+    throw new InsecureOperationError('You cannot copy proposals from a different space');
+  }
+
+  let pageData: Prisma.PageCreateInput;
+
+  if (proposalTemplate) {
+    pageData = {
+      author: {
+        connect: {
+          id: userId
+        }
+      },
+      updatedBy: userId,
+      space: {
+        connect: {
+          id: spaceId
+        }
+      },
+      contentText: proposalTemplate.contentText,
+      content: proposalTemplate.content as Prisma.InputJsonValue,
+      id: proposalId,
       type: 'proposal',
+      path: getPagePath(),
+      title: `Copy of ${proposalTemplate.title}`,
       proposal: {
         create: {
-          createdBy: userId,
           id: proposalId,
-          spaceId,
+          space: {
+            connect: {
+              id: spaceId
+            }
+          },
+          createdBy: userId,
           status: 'private_draft',
-          // Add page creator as the proposal's first author
           authors: {
             create: {
-              author: {
-                connect: {
-                  id: userId
-                }
-              }
+              userId
+            }
+          },
+          reviewers: {
+            createMany: {
+              data: (proposalTemplate.proposal?.reviewers ?? []).map(r => {
+                return {
+                  roleId: r.roleId ? r.roleId : undefined,
+                  userId: r.userId ? r.userId : undefined
+                };
+              })
             }
           }
         }
       }
-    },
+    };
+  }
+  else {
+    pageData = {
+      ...pageCreateInput as Prisma.PageCreateInput,
+      id: proposalId,
+      proposal: {
+        create: {
+          id: proposalId,
+          createdBy: userId,
+          status: 'private_draft',
+          space: {
+            connect: {
+              id: spaceId
+            }
+          },
+          authors: {
+            create: {
+              userId
+            }
+          }
+        }
+      }
+    };
+  }
+
+  // Using a transaction to ensure both the proposal and page gets created together
+  const createdPage = await prisma.page.create({
+    data: pageData,
     include: {
       proposal: {
         include: {
           authors: true,
           reviewers: true
+        }
+      },
+      permissions: {
+        include: {
+          sourcePermission: true
         }
       }
     }
