88b72e6e343998d27131ab14f68249ef6fd514a2
yiwork Ying-Chi Connie I <cloudme@halfpast9.com>
2022-09-20 22:50:25 -0400

    adding possible platform files modify instances before deployment. added function to install yq, and modified env file to include format that references secrets from secrets manager

--- .ebextensions/pull_secrets.sh
+++ .ebextensions/pull_secrets.sh
@@ -1,26 +1,16 @@
 #!/bin/bash -e 
 
 # arguments to this script is the environment namespace that the secret belongs to. 
-#  This would be basically NODE_ENV. Defaults to "staging"
-SECRETS_ENV=${1:-staging}
-APP_STAGING_DIR="/var/app/staging/"
-SECRETS_FILE="${APP_STAGING_DIR}/.env-secrets"
+SECRETS_ENV=${1:-stg}
+APP_STAGING_DIR="/var/app/staging/"              # this is where amazon puts app to be deployed
+SECRETS_FILE="${APP_STAGING_DIR}/.env-secrets"   # What we gonna generate
 SECRETS_APP_NAMESPACE="/io.cv.app"
 SECRETS_NAMESPACE="$SECRETS_APP_NAMESPACE/$SECRETS_ENV"
 
-# remove previously generated secrets and fetch again
+# remove previously generated secrets and generate again
 [ -f "$SECRETS_FILE" ] && rm "$SECRETS_FILE"
 
-declare -A serets_to_lookup
-serets_to_lookup=( \
-    [DB_USER]="$SECRETS_NAMESPACE/db:username"   \
-    [DB_PASSWD]="$SECRETS_NAMESPACE/db:password" \
-    [DB_HOSTNAME]="$SECRETS_NAMESPACE/db:host"   \
-    [DB_NAME]="$SECRETS_NAMESPACE/db:dbname"     \
-    [AUTH_SECRET]="$SECRETS_NAMESPACE/auth_secret:auth_secret" \
-    [DD_API_KEY]="$SECRETS_APP_NAMESPACE/shared/datadog/api_key:dd_api_key"   # in shared namespace
-)
-
+grep -v 
 for env_var_name in "${!serets_to_lookup[@]}"; do 
     secret_name=${serets_to_lookup[$env_var_name]%:*}
     secret_json_keyname=${serets_to_lookup[$env_var_name]#*:}
@@ -33,3 +23,32 @@ for env_var_name in "${!serets_to_lookup[@]}"; do
 
     echo "$env_var_name=$secret_value" >> $SECRETS_FILE
 done
+
+
+function install_yq {
+    yq_version="v4.27.5"
+    yq_binary_name="yq_linux_amd64"
+    # if i can't find yq in my path...i'll just install
+    if ! which yq; then
+        echo "No prior yq cmd installed, download and installing now..."
+        tmp_dir=$(mktemp -d -t yqXXX)
+        wget -q -P $tmp_dir -B "https://github.com/mikefarah/yq/releases/download/$yq_version/$yq_binary_name" \
+             -i - <<FILES
+checksums_hashes_order
+extract-checksum.sh
+checksums
+yq_linux_amd64
+FILES
+        
+        cd $tmp_dir
+        chmod u+x ./extract-checksum.sh
+        checksum_status=$(./extract-checksum.sh MD5 $yq_binary_name | awk '{ print $2 " " $1}' | md5sum -c -)
+        [[ $checksum_status =~ OK ]] && {
+            mv $tmp_dir/$yq_binary_name /usr/local/bin/yq    # should I move it to /root/bin ?
+            chmod 755 /usr/local/bin/yq
+            chown root:root /usr/local/bin/yq
+        }
+        rm -fr $tmp_dir
+    fi
+    which yq
+}
