1c361e5a63dccaba36f70c75fb8b795b8fa3bbc9
yiwork Ying-Chi Connie I <cloudme@halfpast9.com>
2022-09-22 19:33:21 -0400

    removed all troubleshooting metric submission from background/server healthcheck. Modified updateVoteStatus to use a counter instead of a gauge. added server health endpoint which replies just an ok. Also added endpoint to force triggering of datadog metric submission, might help in the future.. added setting to datadog-agent that turns on dogstatsd to collect stats. Put deploy_staging back to orig state so we do not turn on datadog agents in staging

--- background/server/routes.ts
+++ background/server/routes.ts
@@ -1,10 +1,8 @@
 import type Koa from 'koa';
 import log from 'lib/log';
-import { gauge, count } from 'lib/metrics';
 
 export async function healthCheck (ctx: Koa.Context) {
   ctx.body = 'hello world';
-  count('healthcheck_ct', 1);
 }
 
 export async function errorHandler (ctx: Koa.Context, next: () => Promise<any>) {
