42f3e0b04a5d9e93bc5ab4ca54976b526506269a
Safwan Shaheer <devorein00@gmail.com>
2022-09-22 22:29:27 +0600

    Show proposal icon in nexus task (#963)

--- components/nexus/ProposalTasksList.tsx
+++ components/nexus/ProposalTasksList.tsx
@@ -57,6 +57,7 @@ export function ProposalTasksListRow (
             }}
             fontSize={{ sm: 16, xs: 18 }}
           >
+            <TaskOutlinedIcon color='secondary' />
             {pageTitle || 'Untitled'}
           </Grid>
           <Grid
