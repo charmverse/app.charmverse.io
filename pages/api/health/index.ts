2127768048f09ad3005d9ab4010f534ee30099c4
yiwork Ying-Chi Connie I <cloudme@halfpast9.com>
2022-09-22 22:52:09 -0400

    removed preceding period in test endpoint metric name

--- pages/api/health/index.ts
+++ pages/api/health/index.ts
@@ -8,11 +8,11 @@ export default function handler (req: NextApiRequest, res: NextApiResponse) {
 
 // need to add a router to this.
 export function testDDGauge (req: NextApiRequest, res: NextApiResponse) {
-  gauge('.health.dd.gauge', 99);
+  gauge('health.dd.gauge', 99);
   res.status(200).json({ gauge: 'sent 99 as gauge' });
 }
 
 export function testDDCount (req: NextApiRequest, res: NextApiResponse) {
-  count('.health.dd.count', 1);
+  count('health.dd.count', 1);
   res.status(200).json({ count: 'adding 1 to count' });
 }
