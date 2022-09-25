f32202ede4c62af2b4ea0ba3be7a4665aeeb170a
melboudi <mo@anyahq.co>
2022-09-25 21:44:37 +0200

    Create onlyChanged config

---
+++ jest.config-only-changed.ts
@@ -0,0 +1,13 @@
+import createJestConfig from 'testing/createJestConfig';
+
+import { jestConfig } from './jest.config';
+
+const integrationConfig = {
+  ...jestConfig,
+  testMatch: [
+    '**/__integration-tests__/**/*.spec.ts',
+    '**/lib/**/*.spec.ts'
+  ]
+};
+
+export default createJestConfig(integrationConfig);
