c58ae7dc3b7bffe63f4732979cb92530878631e3
Matt Casey <mattwad@gmail.com>
2022-09-26 15:10:12 -0600

    fix components

--- components/common/BoardEditor/focalboard/src/blocks/contentBlock.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.createContentBlock = void 0;
-var block_1 = require("./block");
-var createContentBlock = block_1.createBlock;
-exports.createContentBlock = createContentBlock;
