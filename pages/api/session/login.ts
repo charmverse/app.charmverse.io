d2a5766982461a86dbbfd6890c13fe79abb33dd9
melboudi <mo@anyahq.co>
2022-09-30 03:29:08 +0200

    Implement router middleware

--- pages/api/session/login.ts
+++ pages/api/session/login.ts
@@ -10,50 +10,18 @@ import type { AuthSig } from 'lib/blockchain/interfaces';
 import { UnauthorisedActionError } from 'lib/utilities/errors';
 import { getAddress, toUtf8Bytes, verifyMessage } from 'ethers/lib/utils';
 import { SiweMessage } from 'lit-siwe';
-import { lowerCaseEqual } from '../../../lib/utilities/strings';
-
-type LoginRequest = {
-  address: string;
-  walletSignature: AuthSig;
-};
+import { lowerCaseEqual } from 'lib/utilities/strings';
+import type { Web3LoginRequest } from 'lib/middleware/requireWalletSignature';
+import { requireWalletSignature } from 'lib/middleware/requireWalletSignature';
 
 const handler = nc<NextApiRequest, NextApiResponse>({ onError, onNoMatch });
 
 handler
-  .use(requireKeys<LoginRequest>(['address', 'walletSignature'], 'body'))
+  .use(requireWalletSignature)
   .post(login);
 
 async function login (req: NextApiRequest, res: NextApiResponse<LoggedInUser | { error: any }>) {
-  const { address, walletSignature } = req.body as LoginRequest;
-
-  const origin = req.headers.origin as string;
-
-  const domain = origin.match('https') ? origin.split('https://')[1] : origin.split('http://')[1];
-
-  // Extract chain id from the signature
-  const chainId = walletSignature.signedMessage.split('Chain ID:')[1]?.split('\n')[0]?.trim();
-  const nonce = walletSignature.signedMessage.split('Nonce:')[1]?.split('\n')[0]?.trim();
-  const issuedAt = walletSignature.signedMessage.split('Issued At:')[1]?.split('\n')[0]?.trim();
-
-  const preparedMessage = {
-    domain,
-    address: getAddress(address as string), // convert to EIP-55 format or else SIWE complains
-    uri: origin,
-    version: '1',
-    chainId: parseInt(chainId),
-    nonce,
-    issuedAt
-  };
-
-  const message = new SiweMessage(preparedMessage);
-
-  const body = message.prepareMessage();
-
-  const signatureAddress = verifyMessage(body, walletSignature.sig);
-
-  if (!lowerCaseEqual(signatureAddress, address)) {
-    throw new UnauthorisedActionError('Invalid signature');
-  }
+  const { address } = req.body as Web3LoginRequest;
 
   const user = await prisma.user.findFirst({
     where: {
