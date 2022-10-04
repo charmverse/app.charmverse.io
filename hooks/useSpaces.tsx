2c58c3c67ae85984c9d720e82a20acf02ab10b8c
msadura <m.sadura@outlook.com>
2022-10-04 21:09:31 +0200

    Move add workspace function to useSpaces

--- hooks/useSpaces.tsx
+++ hooks/useSpaces.tsx
@@ -1,7 +1,7 @@
-import type { Space } from '@prisma/client';
+import type { Prisma, Space } from '@prisma/client';
 import { useRouter } from 'next/router';
 import type { ReactNode } from 'react';
-import { createContext, useContext, useEffect, useMemo, useState } from 'react';
+import { useCallback, createContext, useContext, useEffect, useMemo, useState } from 'react';
 
 import charmClient from 'charmClient';
 
@@ -11,13 +11,16 @@ type IContext = {
   spaces: Space[];
   setSpaces: (spaces: Space[]) => void;
   isLoaded: boolean;
+  createNewSpace: (data: Prisma.SpaceCreateInput) => Promise<void>;
 };
 
-export const SpacesContext = createContext<Readonly<IContext>>({ spaces: [], setSpaces: () => undefined, isLoaded: false });
+export const SpacesContext = createContext<Readonly<IContext>>({
+  spaces: [], setSpaces: () => undefined, isLoaded: false, createNewSpace: () => Promise.resolve()
+});
 
 export function SpacesProvider ({ children }: { children: ReactNode }) {
 
-  const { user, isLoaded: isUserLoaded } = useUser();
+  const { user, isLoaded: isUserLoaded, setUser } = useUser();
   const [spaces, setSpaces] = useState<Space[]>([]);
   const [isLoaded, setIsLoaded] = useState(false);
   const router = useRouter();
@@ -37,7 +40,19 @@ export function SpacesProvider ({ children }: { children: ReactNode }) {
     }
   }, [user?.id, isUserLoaded]);
 
-  const value = useMemo(() => ({ spaces, setSpaces, isLoaded }) as IContext, [spaces, isLoaded]);
+  const createNewSpace = useCallback(async (newSpace: Prisma.SpaceCreateInput) => {
+    const space = await charmClient.createSpace(newSpace);
+    setSpaces((s) => [...s, space]);
+    // refresh user permissions
+    const _user = await charmClient.getUser();
+    setUser(_user);
+    // give some time for spaces state to update or user will be redirected to /join in RouteGuard
+    setTimeout(() => {
+      router.push(`/${space.domain}`);
+    }, 50);
+  }, []);
+
+  const value = useMemo(() => ({ spaces, setSpaces, isLoaded, createNewSpace }) as IContext, [spaces, isLoaded]);
 
   return (
     <SpacesContext.Provider value={value}>
