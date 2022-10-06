fa3cbc1ca14d5ec003e1e75f0f8ca72fdf8c1990
Matt Casey <mattwad@gmail.com>
2022-10-06 00:28:25 -0600

    simplifty envs

--- config/constants.ts
+++ config/constants.ts
@@ -1,4 +1,5 @@
-export const isDevEnv = process.env.NODE_ENV === 'development';
-export const isProdEnv = process.env.APP_ENV === 'production';
 export const isTestEnv = process.env.APP_ENV === 'test';
+// NODE_ENV can only be 'development' or 'production' according to Next.js, but we don't want to mix them with test env
+export const isDevEnv = process.env.NODE_ENV === 'development' && !isTestEnv;
+export const isProdEnv = process.env.NODE_ENV === 'production' && !isTestEnv;
 export const baseUrl = process.env.DOMAIN as string;
