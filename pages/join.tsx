577453539db3d3147bf51243ebae0620bced5f52
Safwan Shaheer <devorein00@gmail.com>
2022-10-06 20:34:22 +0600

    Full autocomplete for workspace textinput

--- pages/join.tsx
+++ pages/join.tsx
@@ -1,6 +1,6 @@
 import NavigateNextIcon from '@mui/icons-material/ArrowRightAlt';
-import CheckIcon from '@mui/icons-material/Check';
-import { Typography } from '@mui/material';
+import type { PopperProps } from '@mui/material';
+import { Autocomplete, Popper, Stack, Typography } from '@mui/material';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import Divider from '@mui/material/Divider';
@@ -9,9 +9,10 @@ import type { Space } from '@prisma/client';
 import debounce from 'lodash/debounce';
 import { useRouter } from 'next/router';
 import type { ChangeEvent, ReactNode } from 'react';
-import { useMemo, useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 
 import charmClient from 'charmClient';
+import AvatarWithIcons from 'components/common/AvatarWithIcons';
 import getBaseLayout from 'components/common/BaseLayout/BaseLayout';
 import Button from 'components/common/Button';
 import FieldLabel from 'components/common/form/FieldLabel';
@@ -48,6 +49,7 @@ export default function CreateSpace () {
   const [spaceDomain, setSpaceDomain] = useState<string>('');
   const [spacesInfo, setSpacesInfo] = useState<Space[]>([]);
   const [userInputStatus, setStatus] = useState('');
+  const [selectedSpace, setSelectedSpace] = useState<null | Space>(null);
 
   const domain = router.query.domain;
 
@@ -98,45 +100,80 @@ export default function CreateSpace () {
     setSpaceDomain(stripUrlParts(event.target.value));
   }
 
+  const popperFactoryMemoized = useMemo(() => {
+    return function factory ({ style, ...props }: PopperProps) {
+      return (
+        <Popper
+          {...props}
+          style={{ ...style, height: 0 }}
+        />
+      );
+    };
+  }, []);
+
   return (
     <Box sx={{ width: 600, maxWidth: '100%', mx: 'auto', mb: 6, px: 2 }}>
       <Card sx={{ p: 4, mb: 3 }} variant='outlined'>
         <DialogTitle>Join a workspace</DialogTitle>
         <Divider />
-        {spaceInfo && (
-          <Card sx={{ p: 3, display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
-            <Box mb={3}>
-              <WorkspaceAvatar image={spaceInfo.spaceImage} name={spaceInfo.name} variant='rounded' />
-            </Box>
-            <Box display='flex' flexDirection='column' alignItems='center'>
-              <Typography variant='h5'>{spaceInfo.name}</Typography>
-            </Box>
-          </Card>
+        {spaceInfo && domain && (
+          <>
+            <Card sx={{ p: 3, display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
+              <Box mb={3}>
+                <WorkspaceAvatar image={spaceInfo.spaceImage} name={spaceInfo.name} variant='rounded' />
+              </Box>
+              <Box display='flex' flexDirection='column' alignItems='center'>
+                <Typography variant='h5'>{spaceInfo.name}</Typography>
+              </Box>
+            </Card>
+            {
+              spaceInfo && (
+                <TokenGateForm onSuccess={onJoinSpace} spaceDomain={spaceDomain} />
+              )
+            }
+          </>
         )}
         {/* If there is no domain param in the url, show the text input to search for workspaces */}
         {!domain && (
           <>
             <br />
             <FieldLabel>Enter a CharmVerse workspace name</FieldLabel>
-            <TextField
-              onChange={onChangeDomainName}
-              autoFocus
+            <Autocomplete<Space>
+              disablePortal
+              options={spacesInfo}
               placeholder='my-space'
-              fullWidth
-              value={spaceDomain}
-              helperText={userInputStatus}
-              InputProps={{
-                endAdornment: spaceInfo && <CheckIcon color='success' />
+              value={selectedSpace}
+              onChange={(e, _space) => {
+                setSelectedSpace(_space);
               }}
+              getOptionLabel={(space) => space.name}
+              fullWidth
+              PopperComponent={popperFactoryMemoized}
+              renderOption={(props, space) => (
+                <Box component='li' sx={{ display: 'flex', gap: 1 }} {...props}>
+                  <AvatarWithIcons
+                    avatar={space.spaceImage}
+                    icons={[]}
+                    name={space.name}
+                    variant='rounded'
+                    size='small'
+                  />
+                  <Stack>
+                    <Typography component='span'>
+                      {space.name}
+                    </Typography>
+                    <Typography variant='subtitle2'>
+                      {space.domain}
+                    </Typography>
+                  </Stack>
+                </Box>
+              )}
+              noOptionsText='No spaces found'
+              renderInput={(params) => <TextField placeholder='my-space' {...params} value={spaceDomain} onChange={onChangeDomainName} />}
             />
+            {selectedSpace && <TokenGateForm onSuccess={onJoinSpace} spaceDomain={selectedSpace.domain} />}
           </>
         )}
-        {
-          spaceInfo && (
-            <TokenGateForm onSuccess={onJoinSpace} spaceDomain={spaceDomain} />
-          )
-        }
-
       </Card>
       <AlternateRouteButton href='/createWorkspace'>
         Create a workspace
