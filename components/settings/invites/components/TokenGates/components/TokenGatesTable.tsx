d649429f855ddf52614095120168eaabf8ac989e
Matt Casey <mattwad@gmail.com>
2022-10-09 09:33:47 -0600

    check that users wallet is connected before testing token gate (#1066)
    Co-authored-by: Safwan Shaheer <devorein00@gmail.com>

--- components/settings/invites/components/TokenGates/components/TokenGatesTable.tsx
+++ components/settings/invites/components/TokenGates/components/TokenGatesTable.tsx
@@ -13,12 +13,13 @@ import { useWeb3React } from '@web3-react/core';
 import { checkAndSignAuthMessage, humanizeAccessControlConditions } from 'lit-js-sdk';
 import { useRouter } from 'next/router';
 import type { MouseEvent } from 'react';
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import CopyToClipboard from 'react-copy-to-clipboard';
 import { mutate } from 'swr';
 
 import useLitProtocol from 'adapters/litProtocol/hooks/useLitProtocol';
 import charmClient from 'charmClient';
+import { Web3Connection } from 'components/_app/Web3ConnectionManager';
 import ButtonChip from 'components/common/ButtonChip';
 import TableRow from 'components/common/Table/TableRow';
 import { useCurrentSpace } from 'hooks/useCurrentSpace';
@@ -56,7 +57,7 @@ export default function TokenGatesTable ({ isAdmin, onDelete, tokenGates }: Prop
   const router = useRouter();
   const { showMessage } = useSnackbar();
   const shareLink = `${window.location.origin}/join?domain=${router.query.domain}`;
-
+  const { openWalletSelectorModal } = useContext(Web3Connection);
   function onCopy () {
     showMessage('Link copied to clipboard');
   }
@@ -107,11 +108,13 @@ export default function TokenGatesTable ({ isAdmin, onDelete, tokenGates }: Prop
         chain: (tokenGate.conditions as any).chain || 'ethereum',
         ...tokenGate.conditions as any
       });
+
       await charmClient.verifyTokenGate({ commit: false, spaceId: space?.id as string, tokens: [{ signedToken: jwt, tokenGateId: tokenGate.id }] });
 
       setTestResult({ status: 'success' });
     }
     catch (error) {
+      log.warn('Error when verifying wallet', error);
       let message = '';
       switch ((error as any).errorCode) {
         case 'not_authorized':
@@ -178,9 +181,26 @@ export default function TokenGatesTable ({ isAdmin, onDelete, tokenGates }: Prop
                   />
                 </TableCell>
                 <TableCell align='center'>
-                  <Tooltip arrow placement='top' title={litClient ? 'Test this gate using your own wallet' : 'Lit Protocol client has not initialized'}>
+                  <Tooltip arrow placement='top' title={litClient ? (!account ? 'Connect your wallet to test' : 'Test this gate using your own wallet') : 'Lit Protocol client has not initialized'}>
                     <Box component='span'>
-                      <Chip onClick={() => litClient && testConnect(tokenGate)} sx={{ width: 90 }} clickable={Boolean(litClient)} color='secondary' size='small' variant='outlined' label='Test' />
+                      <Chip
+                        onClick={() => {
+                          if (litClient) {
+                            if (account) {
+                              testConnect(tokenGate);
+                            }
+                            else {
+                              openWalletSelectorModal();
+                            }
+                          }
+                        }}
+                        sx={{ width: 90 }}
+                        clickable={Boolean(account && litClient)}
+                        color='secondary'
+                        size='small'
+                        variant='outlined'
+                        label='Test'
+                      />
                     </Box>
                   </Tooltip>
                 </TableCell>
