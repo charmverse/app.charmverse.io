93388e899128e528b90f24219f0fd4d49b23d963
Safwan Shaheer <devorein00@gmail.com>
2022-10-12 21:47:55 +0600

    Throw InvalidStateError if not wallet exist

--- pages/api/discord/disconnect.ts
+++ pages/api/discord/disconnect.ts
@@ -3,7 +3,7 @@ import nc from 'next-connect';
 
 import { prisma } from 'db';
 import getENSName from 'lib/blockchain/getENSName';
-import { onError, onNoMatch, requireUser } from 'lib/middleware';
+import { InvalidStateError, onError, onNoMatch, requireUser } from 'lib/middleware';
 import { withSessionRoute } from 'lib/session/withSession';
 import { shortenHex } from 'lib/utilities/strings';
 import type { IdentityType } from 'models';
@@ -68,9 +68,12 @@ async function disconnectDiscord (req: NextApiRequest, res: NextApiResponse) {
     newUserName = telegramAccount.username || `${telegramAccount.first_name} ${telegramAccount.last_name}`;
     newIdentityProvider = IDENTITY_TYPES[2];
   }
-  else {
-    newUserName = shortenHex(user.wallets[0]?.address);
+  else if (user.wallets.length) {
+    newUserName = shortenHex(user.wallets[0].address);
     newIdentityProvider = IDENTITY_TYPES[0];
+  }
+  else {
+    throw new InvalidStateError();
   }
 
   const updatedUser = await prisma.user.update({
