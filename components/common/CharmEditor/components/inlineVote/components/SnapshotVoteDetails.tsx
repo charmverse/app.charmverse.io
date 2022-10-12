8e357a253ff4f557ef6b18b5b62ea9751babf631
DESKTOP-GJ3FLA3 <mo@anyahq.co>
2022-10-13 01:27:56 +0200

    Auto-update proposal status in the UI

--- components/common/CharmEditor/components/inlineVote/components/SnapshotVoteDetails.tsx
+++ components/common/CharmEditor/components/inlineVote/components/SnapshotVoteDetails.tsx
@@ -1,8 +1,10 @@
 import PublishIcon from '@mui/icons-material/ElectricBolt';
 import { Box, FormControlLabel, Radio, RadioGroup, Typography } from '@mui/material';
 import Alert from '@mui/material/Alert';
+import type { Proposal } from '@prisma/client';
 import { useEffect, useState } from 'react';
 
+import charmClient from 'charmClient';
 import Button from 'components/common/Button';
 import Loader from 'components/common/LoadingComponent';
 import VoteStatusChip from 'components/votes/components/VoteStatusChip';
@@ -15,10 +17,12 @@ import { StyledFormControl, VotesWrapper } from './VotesWrapper';
 
 type Props = {
   snapshotProposalId: string;
+  proposalId: string;
 }
 
-export function SnapshotVoteDetails ({ snapshotProposalId }: Props) {
+export function SnapshotVoteDetails ({ snapshotProposalId, proposalId }: Props) {
 
+  const [proposal, setProposal] = useState<Proposal | null>(null);
   const [snapshotProposal, setSnapshotProposal] = useState<SnapshotProposal | null>(null);
   const [userVotes, setUserVotes] = useState<SnapshotVote[]>([]);
   const [loading, setLoading] = useState(true);
@@ -32,7 +36,6 @@ export function SnapshotVoteDetails ({ snapshotProposalId }: Props) {
   //  const account = '0x344d5A4da2329A885b24aeC4C8937e20Cd67b112';
 
   // Either the number of votes or tokens
-  const totalVotes = 0;
 
   const voteChoices = snapshotProposal?.choices ?? [];
   const voteScores = snapshotProposal?.scores ?? [];
@@ -40,25 +43,29 @@ export function SnapshotVoteDetails ({ snapshotProposalId }: Props) {
   useEffect(() => {
 
     getSnapshotProposal(snapshotProposalId)
-      .then((proposal) => {
-        setSnapshotProposal(proposal);
+      .then((_snapshotProposal) => {
+        setSnapshotProposal(_snapshotProposal);
         setLoading(false);
 
         getUserProposalVotes({ proposalId: snapshotProposalId, walletAddress: account })
           .then(foundVotes => setUserVotes(foundVotes));
       });
 
+    charmClient.proposals.getProposal(proposalId)
+      .then(_proposal => setProposal(_proposal));
+
   }, []);
 
-  if (loading) {
-    return <Loader isLoading={true} />;
-  }
-
-  if (!snapshotProposal) {
-    return <Alert severity='warning'>Proposal not found on Snapshot</Alert>;
-  }
-
   const hasPassedDeadline = proposalEndDate < Date.now();
+
+  useEffect(() => {
+    if (snapshotProposal && hasPassedDeadline && proposal) {
+      charmClient.proposals.updateStatus(proposalId, 'vote_closed')
+        .then(updatedProposal => {
+          setProposal(updatedProposal);
+        });
+    }
+  }, [snapshotProposal, proposal]);
   const remainingTime = relativeTime(proposalEndDate);
 
   const currentUserChoices = userVotes.map(v => voteChoices[v.choice - 1]).join(',');
@@ -72,12 +79,13 @@ export function SnapshotVoteDetails ({ snapshotProposalId }: Props) {
 
         <Button
           startIcon={<PublishIcon />}
-          href={`https://snapshot.org/#/${snapshotProposal.space.id}/proposal/${snapshotProposal.id}`}
+          href={`https://snapshot.org/#/${snapshotProposal?.space.id}/proposal/${snapshotProposal?.id}`}
           size='small'
           color='secondary'
           variant='outlined'
           external
           target='_blank'
+          disabled={!snapshotProposal}
         >
           View on snapshot
         </Button>
@@ -88,30 +96,56 @@ export function SnapshotVoteDetails ({ snapshotProposalId }: Props) {
           color='secondary'
           variant='subtitle1'
         >
-          {hasPassedDeadline ? `Finished on ${humanFriendlyDate(proposalEndDate, { withYear: true })}` : `Finishes ${remainingTime}`}
+          {
+            !snapshotProposal && loading && 'Loading...'
+          }
+          {snapshotProposal
+            && (hasPassedDeadline ? `Finished on ${humanFriendlyDate(proposalEndDate, { withYear: true })}` : `Finishes ${remainingTime}`)}
         </Typography>
-        <VoteStatusChip size='small' status={hasPassedDeadline ? 'Complete' : 'InProgress'} />
+        {
+          snapshotProposal && (
+            <VoteStatusChip size='small' status={hasPassedDeadline ? 'Complete' : 'InProgress'} />
+          )
+        }
+
       </Box>
 
-      <StyledFormControl>
-        <RadioGroup name={snapshotProposal.id} value={currentUserChoices as any}>
-          {voteChoices.map((voteOption, index) => (
-            <FormControlLabel
-              key={voteOption}
-              control={<Radio size='small' />}
-              disabled
-              value={voteOption}
-              label={(
-                <Box display='flex' justifyContent='space-between' flexGrow={1}>
-                  <span>{voteOption}</span>
-                  <Typography variant='subtitle1' color='secondary'>{percent({ value: voteScores[index], total: snapshotProposal.scores_total, significantDigits: 2 })}</Typography>
-                </Box>
-              )}
-              disableTypography
-            />
-          ))}
-        </RadioGroup>
-      </StyledFormControl>
+      {
+        !snapshotProposal && loading && (
+          <Loader isLoading={true} />
+        )
+      }
+
+      {
+        !snapshotProposal && !loading && (
+          <Alert severity='warning'>Proposal not found on Snapshot</Alert>
+        )
+      }
+
+      {
+        snapshotProposal && (
+          <StyledFormControl>
+            <RadioGroup name={snapshotProposal.id} value={currentUserChoices as any}>
+              {voteChoices.map((voteOption, index) => (
+                <FormControlLabel
+                  key={voteOption}
+                  control={<Radio size='small' />}
+                  disabled
+                  value={voteOption}
+                  label={(
+                    <Box display='flex' justifyContent='space-between' flexGrow={1}>
+                      <span>{voteOption}</span>
+                      <Typography variant='subtitle1' color='secondary'>{percent({ value: voteScores[index], total: snapshotProposal.scores_total, significantDigits: 2 })}</Typography>
+                    </Box>
+                  )}
+                  disableTypography
+                />
+              ))}
+            </RadioGroup>
+          </StyledFormControl>
+        )
+      }
+
     </VotesWrapper>
   );
 }
