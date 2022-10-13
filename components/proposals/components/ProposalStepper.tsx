ca6dfbfaa21d8e0a081e4d878c57c43925069c8b
Matt Casey <mattwad@gmail.com>
2022-10-13 16:31:02 -0500

    add hover effect on proposal status

--- components/proposals/components/ProposalStepper.tsx
+++ components/proposals/components/ProposalStepper.tsx
@@ -1,8 +1,8 @@
-import { useTheme } from '@emotion/react';
+import styled from '@emotion/styled';
 import CheckIcon from '@mui/icons-material/Check';
 import { Divider, Grid, MenuItem, Select, Stack, Tooltip, Typography } from '@mui/material';
 import { Box } from '@mui/system';
-import type { ProposalStatus } from '@prisma/client';
+import type { ProposalStatus, ProposalReviewer } from '@prisma/client';
 import { Fragment, useState } from 'react';
 import type { KeyedMutator } from 'swr';
 
@@ -14,7 +14,13 @@ import type { ProposalWithUsers } from 'lib/proposal/interface';
 import type { ProposalUserGroup } from 'lib/proposal/proposalStatusTransition';
 import { proposalStatusTransitionPermission, proposalStatusTransitionRecord, PROPOSAL_STATUS_LABELS } from 'lib/proposal/proposalStatusTransition';
 
-const proposalStatuses = Object.keys(proposalStatusTransitionRecord) as ProposalStatus[];
+type StepperContainerProps = {
+  refreshProposal: KeyedMutator<ProposalWithUsers>;
+  proposalUserGroups: ProposalUserGroup[];
+  proposal?: ProposalWithUsers;
+};
+
+const PROPOSAL_STATUSES = Object.keys(proposalStatusTransitionRecord) as ProposalStatus[];
 
 const proposalStatusTooltips: Record<ProposalStatus, string> = {
   private_draft: 'Only authors can view and edit this proposal',
@@ -26,26 +32,48 @@ const proposalStatusTooltips: Record<ProposalStatus, string> = {
   vote_closed: 'The vote is complete'
 };
 
-export default function ProposalStepper (
-  { refreshProposal, proposal, proposalUserGroups }:
-  { refreshProposal: KeyedMutator<ProposalWithUsers>, proposalUserGroups: ProposalUserGroup[], proposal?: ProposalWithUsers }
-) {
+const stepperSize = 25;
 
-  const { status: currentStatus, id: proposalId, reviewers } = proposal ?? {
-    status: null,
-    id: null,
-    reviewers: []
-  };
+const StepperIcon = styled.div<{ isCurrent: boolean, isEnabled: boolean }>(({ theme, isCurrent, isEnabled }) => `
+  width: ${stepperSize}px;
+  height: ${stepperSize}px;
+  background-color: ${isCurrent
+    ? theme.palette.purple.main
+    : isEnabled
+      ? theme.palette.teal.main : theme.palette.gray.main};
+  justify-content: center;
+  align-items: center;
+  display: flex;
+  border-radius: 100%;
+  cursor: ${isEnabled ? 'pointer' : 'default'};
+  position: relative;
 
-  const theme = useTheme();
+  // disable hover UX on ios which converts first click to a hover event
+  @media (pointer: fine) {
+
+    &:hover::before  {
+      border-radius: 100%;
+      content: '';
+      display: block;
+      position: absolute;
+      height: 100%;
+      width: 100%;
+      ${!isCurrent && isEnabled && `
+        box-shadow: 0 0 0 2px ${theme.palette.background.default}, 0 0 0 5px ${theme.palette.teal.main};
+      `}
+    }
+  }
+
+`);
+
+export default function ProposalStepper ({ refreshProposal, proposal, proposalUserGroups }: StepperContainerProps) {
+
   const { mutate: mutateTasks } = useTasks();
   const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const currentStatusIndex = currentStatus ? proposalStatuses.indexOf(currentStatus) : -1;
-
   async function updateProposalStatus (newStatus: ProposalStatus) {
-    if (newStatus !== currentStatus && proposalId) {
-      await charmClient.proposals.updateStatus(proposalId, newStatus);
+    if (proposal && newStatus !== proposal.status) {
+      await charmClient.proposals.updateStatus(proposal.id, newStatus);
       await refreshProposal();
       mutateTasks();
     }
@@ -55,181 +83,197 @@ export default function ProposalStepper (
     setIsModalOpen(true);
   }
 
-  const stepperDimension = 25;
-
   return (
     <>
-      <Grid
-        container
-        sx={{
-          display: {
-            xs: 'none',
-            md: 'flex'
-          }
+      <DesktopStepper
+        currentStatus={proposal?.status}
+        reviewers={proposal?.reviewers || []}
+        openVoteModal={openVoteModal}
+        proposalUserGroups={proposalUserGroups}
+        updateProposalStatus={updateProposalStatus}
+      />
+      <MobileStepper
+        currentStatus={proposal?.status}
+        reviewers={proposal?.reviewers || []}
+        openVoteModal={openVoteModal}
+        proposalUserGroups={proposalUserGroups}
+        updateProposalStatus={updateProposalStatus}
+      />
+      <CreateVoteModal
+        proposal={proposal}
+        open={isModalOpen}
+        onCreateVote={() => {
+          setIsModalOpen(false);
+          updateProposalStatus('vote_active');
         }}
-      >
-        {proposalStatuses.map((status, statusIndex) => {
-          const canChangeStatus = currentStatus ? (currentStatus === 'discussion' && status === 'review' ? reviewers.length !== 0 : true) && (proposalUserGroups.some(
-            proposalUserGroup => proposalStatusTransitionPermission[currentStatus]?.[proposalUserGroup]?.includes(status)
-          )) : false;
+        onPublishToSnapshot={() => {
+          setIsModalOpen(false);
+          updateProposalStatus('vote_active');
+        }}
+        onClose={() => {
+          setIsModalOpen(false);
+        }}
+      />
+    </>
+  );
+}
 
-          return (
-            <Fragment key={status}>
-              <Grid item md={12 / 13} display='flex' position='relative' alignItems='center' justifyContent='center'>
-                <Stack
-                  alignItems='center'
-                  height='100%'
-                  gap={1}
-                >
-                  <Tooltip title={proposalStatusTooltips[status]}>
-                    <Box
-                      sx={{
-                        width: stepperDimension,
-                        height: stepperDimension,
-                        borderRadius: '50%',
-                        display: 'flex',
-                        alignItems: 'center',
-                        justifyContent: 'center',
-                        cursor: canChangeStatus ? 'pointer' : 'initial',
-                        background: status === currentStatus
-                          ? theme.palette.purple.main : canChangeStatus
-                            ? theme.palette.teal.main : theme.palette.gray.main
-                      }}
-                      onClick={() => {
-                        if (canChangeStatus) {
-                          if (status === 'vote_active') {
-                            openVoteModal();
-                          }
-                          else {
-                            updateProposalStatus(status);
-                          }
+type StepperProps = {
+  openVoteModal: () => void;
+  currentStatus?: ProposalStatus;
+  reviewers: ProposalReviewer[];
+  proposalUserGroups: ProposalUserGroup[];
+  updateProposalStatus: (newStatus: ProposalStatus) => Promise<void>;
+}
+
+function DesktopStepper ({ openVoteModal, currentStatus, proposalUserGroups, updateProposalStatus, reviewers }: StepperProps) {
+
+  const currentStatusIndex = currentStatus ? PROPOSAL_STATUSES.indexOf(currentStatus) : -1;
+
+  return (
+
+    <Grid
+      container
+      display={{
+        xs: 'none',
+        md: 'flex'
+      }}
+    >
+      {PROPOSAL_STATUSES.map((status, statusIndex) => {
+        const canChangeStatus = currentStatus ? (currentStatus === 'discussion' && status === 'review' ? reviewers.length !== 0 : true) && (proposalUserGroups.some(
+          proposalUserGroup => proposalStatusTransitionPermission[currentStatus]?.[proposalUserGroup]?.includes(status)
+        )) : false;
+
+        return (
+          <Fragment key={status}>
+            <Grid item md={12 / 13} display='flex' position='relative' alignItems='center' justifyContent='center'>
+              <Stack
+                alignItems='center'
+                height='100%'
+                gap={1}
+              >
+                <Tooltip title={proposalStatusTooltips[status]}>
+                  <StepperIcon
+                    isCurrent={currentStatusIndex === statusIndex}
+                    isEnabled={canChangeStatus}
+                    onClick={() => {
+                      if (canChangeStatus) {
+                        if (status === 'vote_active') {
+                          openVoteModal();
                         }
-                      }}
-                    >
-                      {currentStatusIndex >= statusIndex ? <CheckIcon fontSize='small' />
-                        : (
-                          <Typography sx={{
-                            fontWeight: 500
-                          }}
-                          >
-                            {statusIndex + 1}
-                          </Typography>
-                        )}
-                    </Box>
-                  </Tooltip>
-                  <Typography
-                    textAlign='center'
-                    sx={{
-                      fontWeight: currentStatusIndex === statusIndex ? 600 : 400,
-                      fontSize: 14,
-                      whiteSpace: 'nowrap'
+                        else {
+                          updateProposalStatus(status);
+                        }
+                      }
                     }}
                   >
-                    {PROPOSAL_STATUS_LABELS[status as ProposalStatus]}
-                  </Typography>
-                </Stack>
+                    {currentStatusIndex >= statusIndex
+                      ? <CheckIcon fontSize='small' />
+                      : (
+                        <Typography fontWeight={500}>
+                          {statusIndex + 1}
+                        </Typography>
+                      )}
+                  </StepperIcon>
+                </Tooltip>
+                <Typography
+                  textAlign='center'
+                  fontWeight={currentStatusIndex === statusIndex ? 600 : 400}
+                  fontSize={14}
+                  whiteSpace='nowrap'
+                >
+                  {PROPOSAL_STATUS_LABELS[status as ProposalStatus]}
+                </Typography>
+              </Stack>
+            </Grid>
+            {statusIndex !== PROPOSAL_STATUSES.length - 1 && (
+              <Grid item md={12 / 13}>
+                <Divider
+                  sx={{
+                    position: 'relative',
+                    top: stepperSize / 2
+                  }}
+                />
               </Grid>
-              {statusIndex !== proposalStatuses.length - 1 && (
-                <Grid item md={12 / 13}>
-                  <Divider
-                    sx={{
-                      position: 'relative',
-                      top: stepperDimension / 2
-                    }}
-                  />
-                </Grid>
-              )}
-            </Fragment>
-          );
-        })}
-        <CreateVoteModal
-          proposal={proposal}
-          open={isModalOpen}
-          onCreateVote={async () => {
-            setIsModalOpen(false);
-            await updateProposalStatus('vote_active');
-          }}
-          onPublishToSnapshot={async () => {
-            setIsModalOpen(false);
-            await updateProposalStatus('vote_active');
-          }}
-          onClose={() => {
-            setIsModalOpen(false);
-          }}
-        />
-      </Grid>
-      <Grid
-        container
-        sx={{
-          width: '100%',
-          display: {
-            xs: 'flex',
-            md: 'none'
-          }
-        }}
-      >
-        <Box width='100%' justifyContent='space-between' gap={2} alignItems='center' my='6px'>
-          <Box display='flex' height='fit-content' flex={1} className='octo-propertyrow'>
-            <div className='octo-propertyname octo-propertyname--readonly'>
-              <Button>Status</Button>
-            </div>
-            <Box display='flex' flex={1}>
-              <Select
-                fullWidth
-                value={currentStatus ?? ''}
-                onChange={(e) => {
-                  const status = e.target.value as ProposalStatus;
-                  const canChangeStatus = currentStatus ? (currentStatus === 'discussion' && status === 'review' ? reviewers.length !== 0 : true) && (proposalUserGroups.some(
-                    proposalUserGroup => proposalStatusTransitionPermission[currentStatus]?.[proposalUserGroup]?.includes(status as ProposalStatus)
-                  )) : false;
+            )}
+          </Fragment>
+        );
+      })}
 
-                  if (canChangeStatus) {
-                    if (status === 'vote_active') {
-                      openVoteModal();
-                    }
-                    else {
-                      updateProposalStatus(status);
-                    }
+    </Grid>
+  );
+}
+
+function MobileStepper ({ openVoteModal, currentStatus, proposalUserGroups, updateProposalStatus, reviewers }: StepperProps) {
+
+  return (
+    <Box
+      width='100%'
+      display={{
+        xs: 'flex',
+        md: 'none'
+      }}
+    >
+      <Box width='100%' justifyContent='space-between' gap={2} alignItems='center' my='6px'>
+        <Box display='flex' height='fit-content' flex={1} className='octo-propertyrow'>
+          <div className='octo-propertyname octo-propertyname--readonly'>
+            <Button>Status</Button>
+          </div>
+          <Box display='flex' flex={1}>
+            <Select
+              fullWidth
+              value={currentStatus ?? ''}
+              onChange={(e) => {
+                const status = e.target.value as ProposalStatus;
+                const canChangeStatus = currentStatus ? (currentStatus === 'discussion' && status === 'review' ? reviewers.length !== 0 : true) && (proposalUserGroups.some(
+                  proposalUserGroup => proposalStatusTransitionPermission[currentStatus]?.[proposalUserGroup]?.includes(status as ProposalStatus)
+                )) : false;
+
+                if (canChangeStatus) {
+                  if (status === 'vote_active') {
+                    openVoteModal();
                   }
-                }}
-                renderValue={(status) => {
-                  return <Typography>{PROPOSAL_STATUS_LABELS[status as ProposalStatus]}</Typography>;
-                }}
-              >
-                {
-                  proposalStatuses.map((status) => {
-                    const canChangeStatus = currentStatus ? (currentStatus === 'discussion' && status === 'review' ? reviewers.length !== 0 : true) && (proposalUserGroups.some(
-                      proposalUserGroup => proposalStatusTransitionPermission[currentStatus]?.[proposalUserGroup]?.includes(status as ProposalStatus)
-                    )) : false;
-
-                    return (
-                      <MenuItem
-                        sx={{
-                          p: 1
-                        }}
-                        value={status}
-                        disabled={!canChangeStatus}
-                      >
-                        <Stack>
-                          <Typography>{PROPOSAL_STATUS_LABELS[status as ProposalStatus]}</Typography>
-                          <Typography
-                            variant='subtitle2'
-                            sx={{
-                              whiteSpace: 'break-spaces'
-                            }}
-                          >
-                            {proposalStatusTooltips[status]}
-                          </Typography>
-                        </Stack>
-                      </MenuItem>
-                    );
-                  })
+                  else {
+                    updateProposalStatus(status);
+                  }
                 }
-              </Select>
-            </Box>
+              }}
+              renderValue={(status) => {
+                return <Typography>{PROPOSAL_STATUS_LABELS[status as ProposalStatus]}</Typography>;
+              }}
+            >
+              {PROPOSAL_STATUSES.map((status) => {
+                const canChangeStatus = currentStatus ? (currentStatus === 'discussion' && status === 'review' ? reviewers.length !== 0 : true) && (proposalUserGroups.some(
+                  proposalUserGroup => proposalStatusTransitionPermission[currentStatus]?.[proposalUserGroup]?.includes(status as ProposalStatus)
+                )) : false;
+
+                return (
+                  <MenuItem
+                    sx={{
+                      p: 1
+                    }}
+                    value={status}
+                    disabled={!canChangeStatus}
+                  >
+                    <Stack>
+                      <Typography>{PROPOSAL_STATUS_LABELS[status as ProposalStatus]}</Typography>
+                      <Typography
+                        variant='subtitle2'
+                        sx={{
+                          whiteSpace: 'break-spaces'
+                        }}
+                      >
+                        {proposalStatusTooltips[status]}
+                      </Typography>
+                    </Stack>
+                  </MenuItem>
+                );
+              })}
+            </Select>
           </Box>
         </Box>
-      </Grid>
-    </>
+      </Box>
+
+    </Box>
   );
 }
