23a8ca7a1a13e80bc02947a4971bf0677a5dc40d
msadura <m.sadura@outlook.com>
2022-10-13 21:35:47 +0200

    Storybook init

---
+++ stories/Button.stories.tsx
@@ -0,0 +1,42 @@
+import type { ComponentStory, ComponentMeta } from '@storybook/react';
+import React from 'react';
+
+import { Button } from './Button';
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'Example/Button',
+  component: Button,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    backgroundColor: { control: 'color' }
+  }
+} as ComponentMeta<typeof Button>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+// eslint-disable-next-line react/function-component-definition
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+
+export const Primary = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Primary.args = {
+  primary: true,
+  label: 'Button'
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  label: 'Button'
+};
+
+export const Large = Template.bind({});
+Large.args = {
+  size: 'large',
+  label: 'Button'
+};
+
+export const Small = Template.bind({});
+Small.args = {
+  size: 'small',
+  label: 'Button'
+};
---
+++ stories/Header.stories.tsx
@@ -0,0 +1,26 @@
+import type { ComponentStory, ComponentMeta } from '@storybook/react';
+import React from 'react';
+
+import { Header } from './Header';
+
+export default {
+  title: 'Example/Header',
+  component: Header,
+  parameters: {
+    // More on Story layout: https://storybook.js.org/docs/react/configure/story-layout
+    layout: 'fullscreen'
+  }
+} as ComponentMeta<typeof Header>;
+
+// eslint-disable-next-line react/function-component-definition
+const Template: ComponentStory<typeof Header> = (args) => <Header {...args} />;
+
+export const LoggedIn = Template.bind({});
+LoggedIn.args = {
+  user: {
+    name: 'Jane Doe'
+  }
+};
+
+export const LoggedOut = Template.bind({});
+LoggedOut.args = {};
