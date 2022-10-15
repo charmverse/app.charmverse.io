09ead0849a3f82c15cfc2161088121aa6631ab8f
Safwan Shaheer <devorein00@gmail.com>
2022-10-15 20:48:18 +0600

    Moved search bar to separate component

---
+++ components/members/MemberDirectorySearchBar.tsx
@@ -0,0 +1,46 @@
+import { TextField } from '@mui/material';
+import debounce from 'lodash/debounce';
+import type { Dispatch, SetStateAction } from 'react';
+import { useEffect, useMemo } from 'react';
+
+import charmClient from 'charmClient';
+import { useCurrentSpace } from 'hooks/useCurrentSpace';
+import { useMembers } from 'hooks/useMembers';
+import type { Member } from 'lib/members/interfaces';
+
+export function MemberDirectorySearchBar ({ onChange }: {
+  onChange: Dispatch<SetStateAction<Member[]>>;
+}) {
+  const { members } = useMembers();
+  const [space] = useCurrentSpace();
+
+  useEffect(() => {
+    onChange(members);
+  }, [members]);
+
+  const debouncedSearchMembers = useMemo(() => debounce(async (searchedContent: string) => {
+    if (space) {
+      const searchResult = await charmClient.members.getMembers(space.id, searchedContent);
+      onChange(searchResult);
+    }
+  }, 1000), []);
+
+  return (
+    <TextField
+      placeholder='Search for members, roles, skills, interests, etc'
+      fullWidth
+      sx={{
+        my: 2
+      }}
+      onChange={(e) => {
+        const search = e.target.value;
+        if (search.length !== 0) {
+          debouncedSearchMembers(search);
+        }
+        else {
+          onChange(members);
+        }
+      }}
+    />
+  );
+}
