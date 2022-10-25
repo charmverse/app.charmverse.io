f54ef7fe7655b7bef5377c8311ed7bef8ce4adce
msadura <m.sadura@outlook.com>
2022-10-25 13:45:59 +0200

    Tests

--- lib/members/getAccessibleMemberPropertiesBySpace.tsx
+++ lib/members/getAccessibleMemberPropertiesBySpace.tsx
@@ -16,31 +16,46 @@ export function getAccessibleMemberPropertiesBySpace ({ userId, spaceId }: GetVi
 
   return prisma.memberProperty.findMany({
     where: {
-      OR: [
-        // User has permission to view property
-        {
-          spaceId: { in: spaceIdQuery },
-          permissions: accessiblePropertiesByPermissionsQuery({
-            spaceIds: spaceIdQuery,
-            userId
-          })
-        },
-        // No permissions are set, permission visible for everyone
-        {
-          spaceId: { in: spaceIdQuery },
-          permissions: { none: {} }
-        },
-        // Admin override to always return all pages
+      AND: [
+        // User must be a member of space
         {
           space: {
             id: { in: spaceIdQuery },
             spaceRoles: {
               some: {
-                userId,
-                isAdmin: true
+                userId
               }
             }
           }
+        },
+        {
+          OR: [
+            // User has permission to view property
+            {
+              spaceId: { in: spaceIdQuery },
+              permissions: accessiblePropertiesByPermissionsQuery({
+                spaceIds: spaceIdQuery,
+                userId
+              })
+            },
+            // No permissions are set, permission visible for everyone
+            {
+              spaceId: { in: spaceIdQuery },
+              permissions: { none: {} }
+            },
+            // Admin override to always return all pages
+            {
+              space: {
+                id: { in: spaceIdQuery },
+                spaceRoles: {
+                  some: {
+                    userId,
+                    isAdmin: true
+                  }
+                }
+              }
+            }
+          ]
         }
       ]
     },
