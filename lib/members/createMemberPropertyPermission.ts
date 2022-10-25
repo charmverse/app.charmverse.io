2e6749357865aa17801e731ed22289a638d5b400
msadura <m.sadura@outlook.com>
2022-10-25 16:20:39 +0200

    createProperty business logic and test

--- lib/members/createMemberPropertyPermission.ts
+++ lib/members/createMemberPropertyPermission.ts
@@ -3,8 +3,28 @@ import { MemberPropertyPermissionLevel } from '@prisma/client';
 
 import { prisma } from 'db';
 import type { CreateMemberPropertyPermissionInput } from 'lib/members/interfaces';
+import { InvalidInputError } from 'lib/utilities/errors';
 
-export function createMemberPropertyPermission (data: CreateMemberPropertyPermissionInput) {
+export async function createMemberPropertyPermission (data: CreateMemberPropertyPermissionInput) {
+  const space = await prisma.space.findFirst({
+    where: {
+      memberProperty: {
+        some: {
+          id: data.memberPropertyId
+        }
+      },
+      roles: {
+        some: {
+          id: data.roleId
+        }
+      }
+    }
+  });
+
+  if (!space) {
+    throw new InvalidInputError('Member property and role must be in the same workspace');
+  }
+
   return prisma.memberPropertyPermission.create({
     data: {
       ...data,
--- lib/members/interfaces.ts
+++ lib/members/interfaces.ts
@@ -46,7 +46,7 @@ export type CommonSpacesInput = {
   spaceId?: string | undefined;
 }
 
-export type CreateMemberPropertyPermissionInput = { memberPropertyId: string } & Partial<Pick<MemberPropertyPermission, 'roleId' >>
+export type CreateMemberPropertyPermissionInput = { memberPropertyId: string, roleId: string }
 
 export type MemberPropertyPermissionWithRole = MemberPropertyPermission & { role:
   { name: string } | null;
