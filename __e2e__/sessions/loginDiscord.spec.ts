0b112b44056fc6a984e9e4c51610715252b0d5cb
Matt Casey <mattwad@gmail.com>
2022-10-26 14:41:04 -0600

    add fix for logging into discord while metamask is connected

--- __e2e__/sessions/loginDiscord.spec.ts
+++ __e2e__/sessions/loginDiscord.spec.ts
@@ -1,11 +1,12 @@
 import type { Page } from '@playwright/test';
-import { test as base } from '@playwright/test';
+import { expect, test as base } from '@playwright/test';
 import { v4 as uuid } from 'uuid';
 
 import type { WorkerFixture } from '../fixtures/discordServer';
 import { discordServer as discordServerFixture, getServerHost } from '../fixtures/discordServer';
 import { LoginPage } from '../po/login.po';
 import { generateUserAndSpace, createDiscordUser } from '../utils/mocks';
+import { mockWeb3 } from '../utils/web3';
 
 type Fixtures = {
   sandboxPage: Page;
@@ -30,6 +31,39 @@ test('login - allows user to login and see their workspace', async ({ discordSer
   await createDiscordUser({ userId: user.id, discordUserId });
 
   await loginPage.goto();
+
+  const discordApiUrl = getServerHost(discordServer);
+  const discordWebsiteUrl = await loginPage.getDiscordUrl();
+  await loginPage.gotoDiscordCallback({ discordApiUrl, discordWebsiteUrl });
+
+  // should auto redirect to workspace
+  await loginPage.waitForWorkspaceLoaded({ domain: space.domain, page });
+
+});
+
+test('login - allows user to login and see their workspace even when a wallet is connected (regression check)', async ({ discordServer, loginPage }) => {
+
+  const { address, user, space, page } = await generateUserAndSpace();
+  await createDiscordUser({ userId: user.id, discordUserId });
+
+  await mockWeb3({
+    page: loginPage.page,
+    context: { address, privateKey: false },
+    init: ({ Web3Mock, context }) => {
+
+      Web3Mock.mock({
+        blockchain: 'ethereum',
+        accounts: {
+          return: [context.address]
+        }
+      });
+
+    }
+  });
+
+  await loginPage.goto();
+
+  await expect(loginPage.verifyWalletButton).toBeVisible();
 
   const discordApiUrl = getServerHost(discordServer);
   const discordWebsiteUrl = await loginPage.getDiscordUrl();
