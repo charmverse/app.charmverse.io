9b17a3fc647affa18359b7b71aa2873ed94e040c
msadura <m.sadura@outlook.com>
2022-10-26 10:23:49 +0200

    Fix integration tests

--- __integration-tests__/server/pages/api/spaces/[id]/members/properties/permissions.spec.ts
+++ __integration-tests__/server/pages/api/spaces/[id]/members/properties/permissions.spec.ts
@@ -13,6 +13,7 @@ let adminUser: LoggedInUser;
 let nonAdminUser: LoggedInUser;
 let adminCookie: string;
 let nonAdminCookie: string;
+let role1: Role;
 
 beforeAll(async () => {
   const { user, space: generatedSpace } = await generateUserAndSpaceWithApiToken(undefined, true);
@@ -21,19 +22,14 @@ beforeAll(async () => {
   nonAdminUser = await generateSpaceUser({ spaceId: generatedSpace.id, isAdmin: false });
   nonAdminCookie = await loginUser(nonAdminUser.id);
   adminCookie = await loginUser(adminUser.id);
+  role1 = await generateRole({ spaceId: space.id, roleName: 'test role 1', createdBy: adminUser.id });
 });
 
 describe('GET /api/space/[id]/members/properties/permissions - Create and delete property permissions', () => {
   let property1: MemberProperty;
-  let property2: MemberProperty;
-  let role1: Role;
-  let role2: Role;
 
-  beforeAll(async () => {
+  beforeEach(async () => {
     property1 = await generateMemberProperty({ type: 'text', userId: adminUser.id, spaceId: space.id, name: 'test text' });
-    property2 = await generateMemberProperty({ type: 'number', userId: adminUser.id, spaceId: space.id, name: 'test number' });
-    role1 = await generateRole({ spaceId: space.id, roleName: 'test role 1', createdBy: adminUser.id });
-    role2 = await generateRole({ spaceId: space.id, roleName: 'test role 2', createdBy: adminUser.id });
   });
 
   it('should create member property permission for admin user', async () => {
