1024f39cbe55c80ffef62ca6b1c72bfc97e499db
Mohamad El Boudi <mo@anyahq.co>
2022-11-01 23:15:33 +0200

    Drop duplicate events

--- charmClient/apis/trackApi.ts
+++ charmClient/apis/trackApi.ts
@@ -3,8 +3,30 @@ import * as http from 'adapters/http';
 import type { MixpanelEventMap, MixpanelEventName } from 'lib/metrics/mixpanel/interfaces';
 
 export class TrackApi {
+
+  private lastEvent: { payload: string, timestamp: number } | null = null;
+
   trackAction<T extends MixpanelEventName> (event: T, payload: Omit<MixpanelEventMap[T], 'userId'>) {
-    return http.POST<{ success: 'ok' }>(`/api/events?event=${event}`, payload);
+
+    const payloadAsString = JSON.stringify(payload);
+    const now = Date.now();
+
+    // Ignore identical events within 1 second of each other
+    // This also stops sending duplicate events when the user clicks multiple times on a page in the sidebar
+    if (this.lastEvent && this.lastEvent.payload === payloadAsString && now - this.lastEvent.timestamp < 1000) {
+      this.lastEvent = {
+        payload: payloadAsString,
+        timestamp: now
+      };
+      return Promise.resolve({ success: 'ignored' });
+    }
+
+    this.lastEvent = {
+      payload: payloadAsString,
+      timestamp: now
+    };
+
+    return http.POST<{ success: 'ok' | 'ignored' }>(`/api/events?event=${event}`, payload);
   }
 }
 
