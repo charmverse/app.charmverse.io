25e32a7e3d42321813678edfd932c39b14d077d8
msadura <m.sadura@outlook.com>
2022-11-14 12:50:12 +0100

    Fix schema + add count log

--- background/tasks/verifyTokenGateMemberships/task.ts
+++ background/tasks/verifyTokenGateMemberships/task.ts
@@ -1,5 +1,6 @@
 
 import log from 'lib/log';
+import { count } from 'lib/metrics';
 import { verifyTokenGateMemberships } from 'lib/token-gates/verifyTokenGateMemberships';
 
 export async function task () {
@@ -9,6 +10,8 @@ export async function task () {
   try {
     const results = await verifyTokenGateMemberships();
     log.debug('Number of members removed due to invalid token gate conditions', results.deletedUsers);
+
+    count('cron.token-gate-verification.removed-members', results.deletedUsers);
   }
   catch (error: any) {
     log.error(`Error expiring proposals: ${error.stack || error.message || error}`, { error });
