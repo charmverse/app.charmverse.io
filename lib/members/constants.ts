e0ab8db91fdbffc5aaa9aa44c5f63a945adc5ae3
Safwan Shaheer <devorein00@gmail.com>
2022-11-14 22:06:38 +0600

    Hide join date for all views

--- lib/members/constants.ts
+++ lib/members/constants.ts
@@ -1,6 +1,6 @@
 import type { MemberPropertyType } from '@prisma/client';
 
-type MemberPropertyConfig = { label: string, readonly?: boolean, default?: boolean, unhideable?: boolean };
+type MemberPropertyConfig = { label: string, hidden?: boolean, readonly?: boolean, default?: boolean, unhideable?: boolean };
 
 // the following properties are listed in the order they should appear in the UI. Chronological sort is guaranteed since es2015
 export const MEMBER_PROPERTY_CONFIG: Record<MemberPropertyType, MemberPropertyConfig> = {
@@ -42,7 +42,9 @@ export const MEMBER_PROPERTY_CONFIG: Record<MemberPropertyType, MemberPropertyCo
   },
   join_date: {
     label: 'Join date',
-    readonly: true
+    default: true,
+    readonly: true,
+    hidden: true
   },
   email: {
     label: 'Email'
@@ -74,14 +76,6 @@ const propertyTypes = Object.keys(MEMBER_PROPERTY_CONFIG) as MemberPropertyType[
 
 export const READONLY_MEMBER_PROPERTIES = propertyTypes.filter(prop => MEMBER_PROPERTY_CONFIG[prop].readonly);
 
-export const DEFAULT_MEMBER_PROPERTIES = propertyTypes.filter(prop => MEMBER_PROPERTY_CONFIG[prop].default);
+export const HIDDEN_MEMBER_PROPERTIES = propertyTypes.filter(prop => MEMBER_PROPERTY_CONFIG[prop].hidden);
 
-export const DEFAULT_MEMBER_PROPERTIES_ORDER = [
-  'name',
-  'role',
-  'profile_pic',
-  'bio',
-  'discord',
-  'twitter',
-  'timezone'
-] as const;
+export const DEFAULT_MEMBER_PROPERTIES = propertyTypes.filter(prop => MEMBER_PROPERTY_CONFIG[prop].default);
