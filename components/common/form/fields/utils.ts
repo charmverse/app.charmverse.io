3130a584be8f92d563f3f0c508688de92e8eddee
msadura <m.sadura@outlook.com>
2022-11-16 16:52:03 +0100

    Hide empty values from kanban cards

--- components/common/form/fields/utils.ts
+++ components/common/form/fields/utils.ts
@@ -3,3 +3,7 @@ import type { MemberPropertyType } from '@prisma/client';
 export function isSelectType (type: MemberPropertyType | null) {
   return type === 'select' || type === 'multiselect';
 }
+
+export function isEmptyValue (value: string | string[] | null | number | undefined) {
+  return value === null || value === undefined || value === '' || (Array.isArray(value) && value.length === 0);
+}
