1bedb7ded39c6e1025e115ffb65bfd18849281e9
Mohamad El Boudi <mo@anyahq.co>
2022-11-17 12:17:54 +0200

    Re-enable signup from invites page

--- components/invite/InviteLinkPage.tsx
+++ components/invite/InviteLinkPage.tsx
@@ -6,6 +6,7 @@ import { useContext } from 'react';
 import charmClient from 'charmClient';
 import { Web3Connection } from 'components/_app/Web3ConnectionManager';
 import PrimaryButton from 'components/common/PrimaryButton';
+import { WalletSign } from 'components/login/WalletSign';
 import WorkspaceAvatar from 'components/settings/workspace/LargeAvatar';
 import { useOnboarding } from 'hooks/useOnboarding';
 import { useUser } from 'hooks/useUser';
@@ -17,8 +18,8 @@ import { CenteredBox } from './components/CenteredBox';
 export default function InvitationPage ({ invite }: { invite: InviteLinkPopulated }) {
 
   const { user } = useUser();
-  const { openWalletSelectorModal, triedEager } = useContext(Web3Connection);
-  const { account, walletAuthSignature, verifiableWalletDetected } = useWeb3AuthSig();
+  const { connectWallet, walletAuthSignature, verifiableWalletDetected } = useWeb3AuthSig();
+  const { loginFromWeb3Account } = useUser();
   const { showOnboarding } = useOnboarding();
 
   async function joinSpace () {
@@ -40,15 +41,13 @@ export default function InvitationPage ({ invite }: { invite: InviteLinkPopulate
           <Typography gutterBottom>You've been invited to join</Typography>
           <Typography variant='h5'>{invite.space.name}</Typography>
         </Box>
-        {(account || user) ? (
+        {user ? (
           <PrimaryButton data-test='accept-invite-button' fullWidth size='large' onClick={joinSpace}>
             Accept Invite
           </PrimaryButton>
         ) : (
           <Box display='flex' gap={2}>
-            <PrimaryButton size='large' loading={!triedEager} onClick={openWalletSelectorModal}>
-              Connect Wallet
-            </PrimaryButton>
+            <WalletSign signSuccess={loginFromWeb3Account} />
             <PrimaryButton size='large' href={`/api/discord/oauth?redirect=${encodeURIComponent(window.location.href.split('?')[0])}&type=login`}>
               Connect Discord
             </PrimaryButton>
