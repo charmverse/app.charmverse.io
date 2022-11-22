c0270e12440ea70334b61d650a0af11f3024112a
Matt Casey <mattwad@gmail.com>
2022-11-21 22:59:17 -0700

    fix env var for app env

--- config/constants.ts
+++ config/constants.ts
@@ -1,6 +1,6 @@
 // Note: NODE_ENV can only be 'development' or 'production' according to Next.js, but we don't want to mix them with test env
-export const isTestEnv = process.env.APP_ENV === 'test';
-export const isStagingEnv = process.env.APP_ENV === 'staging';
+export const isTestEnv = process.env.NEXT_PUBLIC_APP_ENV === 'test';
+export const isStagingEnv = process.env.NEXT_PUBLIC_APP_ENV === 'staging';
 export const isDevEnv = process.env.NODE_ENV === 'development' && !isTestEnv && !isStagingEnv;
 export const isProdEnv = process.env.NODE_ENV === 'production' && !isTestEnv && !isStagingEnv;
 export const isNodeEnv = typeof window === 'undefined';
