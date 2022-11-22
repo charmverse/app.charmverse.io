659c45a7cd3a7b6f9a5e90500d296daff8b0cebb
Matt Casey <mattwad@gmail.com>
2022-11-21 22:55:35 -0700

    fix env vars

--- config/constants.ts
+++ config/constants.ts
@@ -1,8 +1,8 @@
+// Note: NODE_ENV can only be 'development' or 'production' according to Next.js, but we don't want to mix them with test env
 export const isTestEnv = process.env.APP_ENV === 'test';
 export const isStagingEnv = process.env.APP_ENV === 'staging';
-// NODE_ENV can only be 'development' or 'production' according to Next.js, but we don't want to mix them with test env
-export const isDevEnv = process.env.NODE_ENV === 'development' && !isTestEnv;
-export const isProdEnv = process.env.NODE_ENV === 'production' && !isTestEnv;
+export const isDevEnv = process.env.NODE_ENV === 'development' && !isTestEnv && !isStagingEnv;
+export const isProdEnv = process.env.NODE_ENV === 'production' && !isTestEnv && !isStagingEnv;
 export const isNodeEnv = typeof window === 'undefined';
 export const baseUrl = process.env.DOMAIN as string | undefined;
 // for cookies
@@ -10,8 +10,4 @@ export const authSecret = process.env.AUTH_SECRET as string | undefined;
 export const cookieDomain = process.env.COOKIE_DOMAIN as string | undefined;
 export const cookieName = 'charm.sessionId';
 // web sockets
-export const websocketsHost = isStagingEnv
-  ? 'https://sockets.charmverse.co'
-  : isProdEnv
-    ? 'https://sockets.charmverse.io'
-    : process.env.WEBSOCKETS_HOST || '' as string;
+export const websocketsHost = process.env.NEXT_PUBLIC_WEBSOCKETS_HOST;
