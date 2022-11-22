9f0a6f77386bc91f09edbd5e1855979247d0dcbc
Matt Casey <mattwad@gmail.com>
2022-11-21 22:15:04 -0700

    fix env vars

--- config/constants.ts
+++ config/constants.ts
@@ -1,4 +1,5 @@
 export const isTestEnv = process.env.APP_ENV === 'test';
+export const isStagingEnv = process.env.APP_ENV === 'staging';
 // NODE_ENV can only be 'development' or 'production' according to Next.js, but we don't want to mix them with test env
 export const isDevEnv = process.env.NODE_ENV === 'development' && !isTestEnv;
 export const isProdEnv = process.env.NODE_ENV === 'production' && !isTestEnv;
@@ -9,4 +10,8 @@ export const authSecret = process.env.AUTH_SECRET as string | undefined;
 export const cookieDomain = process.env.COOKIE_DOMAIN as string | undefined;
 export const cookieName = 'charm.sessionId';
 // web sockets
-export const websocketsHost = process.env.NEXT_PUBLIC_WEBSOCKETS_HOST || '' as string;
+export const websocketsHost = isProdEnv
+  ? 'https://sockets.charmverse.io'
+  : isStagingEnv
+    ? 'https://sockets.charmverse.co'
+    : process.env.WEBSOCKETS_HOST || '' as string;
