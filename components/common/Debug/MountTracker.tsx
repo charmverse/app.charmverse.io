f90b4e2afa594bedc07d0a0a5158b436ae985ae1
Mohamad El Boudi <mo@anyahq.co>
2022-11-23 16:28:01 +0200

    Improve the mount tracker

--- components/common/Debug/MountTracker.tsx
+++ components/common/Debug/MountTracker.tsx
@@ -1,12 +1,20 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 
 import log from 'lib/log';
 
-export function MountTracker ({ name, children }: { name: string, children: JSX.Element }) {
+export function MountTracker ({ name, children, countRenders }: { name: string, children: JSX.Element, countRenders?: boolean }) {
+
+  const renders = useRef(0);
+
+  if (countRenders) {
+    renders.current += 1;
+    log.debug(`mount debug: ${name} renders: ${renders.current}`);
+  }
+
   useEffect(() => {
     log.debug(`✅ ${name} mounted - mount debug: ${Date.now()}`);
 
-    return () => log.debug(`🗑️ ${name} component deleted - mount debug`);
+    return () => log.debug(`🔴 ${name} component deleted - mount debug`);
   }, []);
 
   return children;
