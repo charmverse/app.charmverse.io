d3c0a9bc4c9d1dedbbb38fcda7e603c3a83c181e
Matt Casey <mattwad@gmail.com>
2022-11-25 16:20:31 -0700

    allow editing tags (#1277)

--- components/common/form/fields/Select/SelectOptionItem.tsx
+++ components/common/form/fields/Select/SelectOptionItem.tsx
@@ -1,4 +1,4 @@
-import { Chip, MenuItem, Stack } from '@mui/material';
+import { Box, Chip, MenuItem, Stack } from '@mui/material';
 import type { HTMLAttributes } from 'react';
 
 import type { SelectOptionType } from 'components/common/form/fields/Select/interfaces';
@@ -19,9 +19,13 @@ export function SelectOptionItem ({ option, onChange, onDelete, onToggleOptionEd
   return (
     <MenuItem {...menuItemProps} sx={{ display: 'flex' }}>
       <Stack flexDirection='row' justifyContent='space-between' alignItems='center' flex={1}>
-        <Chip label={option.name} color={option.color} size='small' sx={{ px: 0.5 }} />
+        <Chip label={option.name} color={option.color} size='small' sx={{ px: 0.5, zIndex: 0, position: 'relative' }} />
 
-        {!readOnly && <SelectOptionEdit option={option} onChange={onChange} onDelete={onDelete} onToggleOptionEdit={onToggleOptionEdit} />}
+        {!readOnly && (
+          <Box position='absolute' right='5px'>
+            <SelectOptionEdit option={option} onChange={onChange} onDelete={onDelete} onToggleOptionEdit={onToggleOptionEdit} />
+          </Box>
+        )}
       </Stack>
     </MenuItem>
   );
