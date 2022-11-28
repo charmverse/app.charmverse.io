844a89273d0cdf8e7aeb264c07dac3d71dd519d6
Valentin L <ludu.valentin@gmail.com>
2022-11-28 22:21:02 +0200

    Highlight selection on sidebar

--- components/forum/components/FilterList/FilterList.tsx
+++ components/forum/components/FilterList/FilterList.tsx
@@ -2,6 +2,7 @@ import ButtonGroup from '@mui/material/ButtonGroup';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Divider from '@mui/material/Divider';
+import { useRouter } from 'next/router';
 
 import Button from 'components/common/Button';
 
@@ -14,19 +15,20 @@ interface FilterListProps {
 
 export default function FilterList (props: FilterListProps) {
   const { disabled = false, handleClick, categories, sortList } = props;
+  const { query } = useRouter();
 
   return (
     <Card variant='outlined'>
       <CardContent>
         <ButtonGroup orientation='vertical' sx={{ alignItems: 'flex-start' }} color='inherit' disabled={disabled}>
-          {sortList.map(btn => (
-            <Button key={btn} variant='text' onClick={() => handleClick(btn)}>{btn}</Button>
+          {sortList.map(sort => (
+            <Button key={sort} variant='text' onClick={() => handleClick(sort)} sx={{ fontWeight: sort === query.sort ? 'bold' : 'initial' }}>{sort}</Button>
           ))}
         </ButtonGroup>
         <Divider sx={{ pt: '10px', mb: '10px' }} />
         <ButtonGroup orientation='vertical' sx={{ alignItems: 'flex-start' }} color='inherit' disabled={disabled}>
           {categories.map(category => (
-            <Button key={category} variant='text' onClick={() => handleClick(category)} disabled={disabled}>{category}</Button>
+            <Button key={category} variant='text' onClick={() => handleClick(category)} disabled={disabled} sx={{ fontWeight: category === query.category ? 'bold' : 'initial' }}>{category}</Button>
           ))}
         </ButtonGroup>
       </CardContent>
