2fc1d2a8d95255d201132ebf6127e6fe6521cc0b
Valentin L <ludu.valentin@gmail.com>
2022-11-29 10:45:26 +0200

    Fix lint errors

--- lib/forum/getForumCategories.ts
+++ lib/forum/getForumCategories.ts
@@ -1,6 +1,15 @@
 import type { AvailableResourcesRequest } from 'lib/permissions/interfaces';
 
-export async function getForumCategories ({ spaceId }: AvailableResourcesRequest): Promise<string[]> {
-  const categories = ['Announcements', 'Governance', 'Randoms', 'NFTS', 'Introductions', 'Question & Support', 'New York', 'Devcon VI'];
+export async function getForumCategories({ spaceId }: AvailableResourcesRequest): Promise<string[]> {
+  const categories = [
+    'Announcements',
+    'Governance',
+    'Randoms',
+    'NFTS',
+    'Introductions',
+    'Question & Support',
+    'New York',
+    'Devcon VI'
+  ];
   return categories;
 }
