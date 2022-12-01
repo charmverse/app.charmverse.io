f535d5cbb38f78119817fb936c5a90514151d21a
Mohamad El Boudi <mo@anyahq.co>
2022-12-01 02:24:58 +0200

    Implement paginated search in the client

--- components/forum/components/ForumPosts/ForumPosts.tsx
+++ components/forum/components/ForumPosts/ForumPosts.tsx
@@ -1,13 +1,14 @@
 import Alert from '@mui/material/Alert';
 import { Box } from '@mui/system';
 import { useRouter } from 'next/router';
-import { useEffect, useMemo, useRef } from 'react';
-import useSWRInfinite from 'swr/infinite';
+import { useEffect, useRef, useState } from 'react';
 
 import charmClient from 'charmClient';
 import { useCurrentSpace } from 'hooks/useCurrentSpace';
 import { useMembers } from 'hooks/useMembers';
 import useOnScreen from 'hooks/useOnScreen';
+import type { PaginatedPostList } from 'lib/forums/posts/listForumPosts';
+import type { Member } from 'lib/members/interfaces';
 
 import CreateForumPost from '../CreateForumPost';
 import ForumPost from '../ForumPost/ForumPost';
@@ -17,82 +18,106 @@ interface ForumPostsProps {
   search: string;
 }
 
-const count = 15;
+const resultsPerQuery = 1;
+
+// Add a manual delay so the user sees the post loading skeleton
+const postRefreshTimeout = 1000;
 
 export default function ForumPosts({ search }: ForumPostsProps) {
   const ref = useRef();
   const currentSpace = useCurrentSpace();
-  const isVisible = useOnScreen(ref);
+  const bottomPostReached = useOnScreen(ref);
   const { query } = useRouter();
-  const querySort = query.sort;
+
   const queryCategory = query.category;
 
-  const sortValue = useMemo(() => {
-    const defaultValue = 'Most popular';
-    if (querySort) {
-      if (Array.isArray(querySort)) {
-        return querySort[0] || defaultValue;
-      } else {
-        return querySort;
-      }
-    }
-    return 'Most popular';
-  }, [querySort]);
+  // Re-enable sorting later on
 
-  const categoryValue = useMemo(() => {
-    if (queryCategory) {
-      if (Array.isArray(queryCategory)) {
-        return queryCategory[0];
-      } else {
-        return queryCategory;
-      }
-    }
-    return undefined;
-  }, [queryCategory]);
+  // const querySort = query.sort;
+  // const sortValue = useMemo(() => {
+  //   const defaultValue = 'Most popular';
+  //   if (querySort) {
+  //     if (Array.isArray(querySort)) {
+  //       return querySort[0] || defaultValue;
+  //     } else {
+  //       return querySort;
+  //     }
+  //   }
+  //   return 'Most popular';
+  // }, [querySort]);
 
-  const { data, error, size, setSize, isValidating } = useSWRInfinite(
-    (index) => (currentSpace ? { url: 'forum/posts', arguments: { page: index + 1 } } : null),
-    (args) =>
-      charmClient.forum.listForumPosts(
-        currentSpace!.id,
-        sortValue || 'Most Popular',
-        categoryValue,
-        count,
-        args.arguments.page
-      )
-  );
   const { members } = useMembers();
 
-  const posts = data
-    ? [...data].flat().map((post) => {
-        const { userId, ...restPost } = post;
-        return {
-          ...restPost,
-          user: members.find((item) => item.id === post.userId)
-        };
+  const [posts, setPosts] = useState<PaginatedPostList<{ user?: Member }> | null>(null);
+  const [error, setError] = useState<Error | null>(null);
+
+  const [isLoadingMore, setIsLoadingMore] = useState(false);
+
+  function loadMorePosts() {
+    setIsLoadingMore(true);
+    charmClient.forum
+      .listForumPosts({
+        spaceId: currentSpace!.id,
+        categoryIds: queryCategory,
+        count: resultsPerQuery,
+        page: posts?.cursor
       })
-    : [];
-  const isLoadingInitialData = !data && !error;
-  const isLoadingMore = isLoadingInitialData || (size > 0 && data && typeof data[size - 1] === 'undefined');
-  const isEmpty = data?.[0]?.length === 0;
-  const isReachingEnd = isEmpty || (data && data[data.length - 1]?.length < count);
-  const isRefreshing = isValidating && data && data.length === size;
+      .then((foundPosts) => {
+        setError(null);
+
+        // UX improvement: add a delay so the user sees the post loading skeleton
+        setTimeout(() => {
+          setPosts((_prevList) => {
+            const filteredPosts = foundPosts.data
+              .filter((post) => !posts?.data.find((i) => i.id === post.id))
+              .map((post) => {
+                const user = members.find((member) => member.id === post.createdBy);
+                return { ...post, user };
+              });
+
+            if (!_prevList) {
+              return foundPosts;
+            }
+
+            _prevList.cursor = foundPosts.cursor;
+            _prevList.data = [..._prevList.data, ...filteredPosts];
+            _prevList.hasNext = foundPosts.hasNext;
+
+            return {
+              ..._prevList
+            };
+          });
+          setIsLoadingMore(false);
+        }, postRefreshTimeout);
+      })
+      .catch((err) => {
+        setError(err);
+        setIsLoadingMore(false);
+      });
+  }
 
   useEffect(() => {
-    if (isVisible && !isReachingEnd && data && !isRefreshing && !isLoadingMore) {
-      setSize(size + 1);
+    if (
+      currentSpace &&
+      members &&
+      members?.length > 0 &&
+      !isLoadingMore &&
+      (!posts || (posts.hasNext && bottomPostReached))
+    ) {
+      loadMorePosts();
     }
-  }, [isVisible, isRefreshing, isReachingEnd, data, isLoadingMore]);
+  }, [bottomPostReached, members, currentSpace, posts, isLoadingMore]);
 
   return (
     <>
       <CreateForumPost />
       {error && <Alert severity='error'>There was an unexpected error while loading the posts</Alert>}
-      {isLoadingMore && <ForumPostSkeleton />}
-      {posts.map((post) => (
+
+      {posts?.data.map((post) => (
         <ForumPost key={post.id} {...post} />
       ))}
-      <Box ref={ref}>{isReachingEnd && <Alert severity='info'>End of the forum</Alert>}</Box>
+      {isLoadingMore && <ForumPostSkeleton />}
+      <Box ref={ref}>{posts?.hasNext === false && <Alert severity='info'>End of the forum</Alert>}</Box>
     </>
   );
 }
