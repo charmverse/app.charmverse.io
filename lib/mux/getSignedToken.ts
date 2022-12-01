2f4fd787bae95ea5ee07c4d38ddf7f494d0d7e7b
Matt Casey <mattwad@gmail.com>
2022-12-01 11:46:20 -0700

    add playback restriction

--- lib/mux/getSignedToken.ts
+++ lib/mux/getSignedToken.ts
@@ -4,11 +4,19 @@ import { mux } from './muxClient';
 
 const signingKeyId = process.env.MUX_SIGNING_KEY_ID as string | undefined;
 const signingKeySecret = process.env.MUX_SIGNING_KEY_SECRET as string | undefined;
+// Playback Restriction restricts video to just charmverse domains. To generate a new restriction, use:
+// curl 'https://api.mux.com/video/v1/playback-restrictions' \
+//   -X POST \
+//   -d '{ "referrer": { "allowed_domains": ["*.charmverse.io", "*.charmverse.co"] } }' \
+//   -H "Content-Type: application/json" \
+//   -u $MUX_TOKEN_ID:$MUX_TOKEN_SECRET
+const playbackRestrictionId = process.env.MUX_PLAYBACK_RESTRICTION_ID as string | undefined;
 
 const baseOptions = {
   keyId: signingKeyId, // Enter your signing key id here
   keySecret: signingKeySecret, // Enter your base64 encoded private key here
-  expiration: '7d' // E.g 60, "2 days", "10h", "7d", numeric value interpreted as seconds
+  playback_restriction_id: playbackRestrictionId,
+  expiration: '1d' // E.g 60, "2 days", "10h", "7d", numeric value interpreted as seconds
 };
 
 export async function getSignedToken(playbackId: string) {
