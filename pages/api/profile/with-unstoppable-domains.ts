ea1080f0860e03bb118e652ec44186912a02947e
Mohamad El Boudi <mo@anyahq.co>
2022-12-02 14:00:32 +0200

    Add unstoppable domains integration

--- pages/api/profile/with-unstoppable-domains.ts
+++ pages/api/profile/with-unstoppable-domains.ts
@@ -4,6 +4,8 @@ import nc from 'next-connect';
 import { v4 } from 'uuid';
 
 import { prisma } from 'db';
+import type { UnstoppableDomainsAuthSig } from 'lib/blockchain/verifyUnstoppableDomainsSignature';
+import { verifyUnstoppableDomainsSignature } from 'lib/blockchain/verifyUnstoppableDomainsSignature';
 import { updateGuildRolesForUser } from 'lib/guild-xyz/server/updateGuildRolesForUser';
 import { updateTrackUserProfile } from 'lib/metrics/mixpanel/updateTrackUserProfile';
 import { extractSignupAnalytics } from 'lib/metrics/mixpanel/utilsSignup';
@@ -20,12 +22,17 @@ import type { LoggedInUser } from 'models';
 
 const handler = nc<NextApiRequest, NextApiResponse>({ onError, onNoMatch });
 
-handler.post(createUserFromUnstoppableDomains);
+handler.post(loginViaUnstoppableDomains);
 
-async function createUserFromUnstoppableDomains(
-  req: NextApiRequest,
-  res: NextApiResponse<LoggedInUser | { error: any }>
-) {
+async function loginViaUnstoppableDomains(req: NextApiRequest, res: NextApiResponse<LoggedInUser>) {
+  const authSig = req.body as UnstoppableDomainsAuthSig;
+
+  const isValid = verifyUnstoppableDomainsSignature(authSig);
+
+  if (isValid) {
+    const address = authSig.idToken.wallet_address.toLowerCase();
+  }
+
   // TODO Verify inbound request is legitimately from Unstoppable Domains
   res.status(200).send({} as any);
 }
