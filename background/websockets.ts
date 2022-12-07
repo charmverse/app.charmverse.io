f763645ce9cd6651349db93577e7eac30ddd0e42
Matt Casey <mattwad@gmail.com>
2022-12-07 13:20:59 -0700

    remove comments

--- background/websockets.ts
+++ background/websockets.ts
@@ -2,7 +2,7 @@ import { createServer } from 'http';
 
 import { Server } from 'socket.io';
 
-import { baseUrl, appEnv } from 'config/constants';
+import { appEnv } from 'config/constants';
 import log from 'lib/log';
 import { relay } from 'lib/websockets/relay';
 
@@ -17,15 +17,8 @@ const io = new Server(server, {
     allowedHeaders: ['authorization'],
     credentials: true,
     origin: (requestOrigin, callback) => {
-      // eslint-disable-next-line no-console
-      console.log('CORS ORIGIN REQUEST', requestOrigin);
-      if (baseUrl) {
-        callback(null, baseUrl);
-      }
       // support any subdomain for staging
-      else if (requestOrigin?.endsWith('.charmverse.co') || requestOrigin?.endsWith('.charmverse.io')) {
-        // eslint-disable-next-line no-console
-        console.log('return production origin');
+      if (requestOrigin?.endsWith('.charmverse.co') || requestOrigin?.endsWith('.charmverse.io')) {
         callback(null, requestOrigin);
       } else {
         callback(new Error('Not allowed by CORS'));
