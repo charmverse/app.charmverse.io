dd40b380acd898e8d29e9d1f0ab92e6b69b5d002
Rieranthony <rieraanthony13@gmail.com>
2022-12-14 13:24:10 +0000

    feat: basic serverless setup

---
+++ serverless/webhookWorker.ts
@@ -0,0 +1,17 @@
+import type { Context, SQSEvent, SQSHandler } from 'aws-lambda';
+
+/**
+ * SQS worker logic goes there
+ */
+export const handler: SQSHandler = async (event: SQSEvent, context: Context) => {
+  // SQS may be invoked with multiple messages
+  for (const message of event.Records) {
+    const bodyData = JSON.parse(message.body); // Message body
+
+    // Gets message information
+    // Connect to DB (with prisma) and get space's webhook config
+    // Sign message and add it to header
+    // Trigger HTTP call using space's setting
+    // Expects 200 back
+  }
+};
