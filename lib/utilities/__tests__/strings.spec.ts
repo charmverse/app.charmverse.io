a318b84217faeb6b40ba63174ac97e1611b2edc8
Matt Casey <mattwad@gmail.com>
2022-12-15 22:59:59 -0700

    sanitize inputs for mentions

---
+++ lib/utilities/__tests__/strings.spec.ts
@@ -0,0 +1,9 @@
+import { sanitizeForRegex } from '../strings';
+
+describe('strings', () => {
+  it('should sanitize parenthesis in a regex', () => {
+    const text = sanitizeForRegex('test (test)');
+
+    expect(text).toBe('test \\(test\\)');
+  });
+});
