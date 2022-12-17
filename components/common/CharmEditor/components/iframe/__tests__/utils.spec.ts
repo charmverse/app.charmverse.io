7552cf7b1dcf25ce3b47d304639c8cce6b7fe7d9
Matt Casey <mattwad@gmail.com>
2022-12-17 01:24:05 -0700

    fix test

--- components/common/CharmEditor/components/iframe/__tests__/utils.spec.ts
+++ components/common/CharmEditor/components/iframe/__tests__/utils.spec.ts
@@ -4,6 +4,6 @@ describe('CharmEditor iframe utils', () => {
   test('extractIframeProps() extracts data from Google forms embed code', () => {
     const src = 'https://docs.google.com/forms/d/e/1FAI...ltEQ/viewform?embedded=true';
     const sample = `<iframe src="${src}" width="640" height="2831" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>`;
-    expect(extractIframeProps(sample)).toBe({ src, height: 2831 });
+    expect(extractIframeProps(sample)).toEqual({ src, height: 2831 });
   });
 });
