6ff2459c4eab398fc5443aeb663ecbfbb756e8dd
Matt Casey <mattwad@gmail.com>
2022-12-20 16:00:36 -0700

    ..

--- scripts/deletePosts.ts
+++ scripts/deletePosts.ts
@@ -1,18 +1,25 @@
 import { prisma } from 'db'
 import { generateDefaultPostCategories } from 'lib/forums/categories/generateDefaultPostCategories'
 async function deletePosts () {
-  await prisma.post.deleteMany({});
+
+  const r = await prisma.post.deleteMany({});
+  console.log('deleted ', r.count, 'posts');
+
   const spaces = await prisma.space.findMany({
     include: {
       postCategories: true,
     }
   });
 
+  let updated = 0;
   for (const space of spaces) {
     if (space.postCategories.length === 0) {
       await prisma.postCategory.createMany({ data: generateDefaultPostCategories(space.id) });
+      updated++;
     }
   }
+
+  console.log('Generated categories for' + updated + 'spaces')
 
 }
 
