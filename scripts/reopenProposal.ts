34d12cd38727f7a5f3e1864a070e6bcee0e1e710
Matt Casey <mattwad@gmail.com>
2022-12-20 11:03:22 -0700

    update proposal as well

--- scripts/reopenProposal.ts
+++ scripts/reopenProposal.ts
@@ -5,36 +5,48 @@ import { strict as assert } from 'node:assert';
 async function reopenProposal (path: string, newDate: Date) {
   const spaceDomain = path.split('/')[0];
   const spacePagePath = path.split('/')[1];
-  const space = await prisma.space.findFirstOrThrow({
-    where: {
-      domain: spaceDomain
-    }
-  });
 
   const page = await prisma.page.findFirstOrThrow({
     where: {
-      spaceId: space.id,
-      path: spacePagePath
+      path: spacePagePath,
+      space: {
+        domain: spaceDomain
+      }
     },
     include: {
+      proposal: true,
       votes: true
     }
   });
 
   const vote = page.votes[0];
   assert(vote, 'Vote not found');
+  const proposal = page.proposal;
+  assert(proposal, 'Proposal not found');
 
-  console.log('Vote found', vote.id, vote.deadline, vote.status);
+  console.log('Proposal + Vote found', { voteDeadline: vote.deadline, voteStatus: vote.status, proposalStatus: proposal.status });
 
-  await prisma.vote.update({
-    where: {
-      id: page.votes[0].id
-    },
-    data: {
-      deadline: newDate,
-      status: 'InProgress'
-    }
-  });
+  const [voteRes, proposalRes] = await prisma.$transaction([
+    prisma.vote.update({
+      where: {
+        id: vote.id
+      },
+      data: {
+        deadline: newDate,
+        status: 'InProgress'
+      }
+    }),
+    prisma.proposal.update({
+      where: {
+        id: proposal.id
+      },
+      data: {
+        status: 'vote_active'
+      }
+    })
+  ]);
+  console.log('Vote updated', voteRes.id, voteRes.deadline, voteRes.status);
+  console.log('Proposal updated', proposalRes.id, proposalRes.status);
 }
 
 reopenProposal('story-dao/page-04742242151671694', new Date(2022, 11, 21, 21 ));
