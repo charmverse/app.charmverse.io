accf7852e7969f89c64e508a4488bb733b3249e1
Matt Casey <mattwad@gmail.com>
2022-12-20 10:52:51 -0700

    fix proposal status

---
+++ scripts/reopenVote.ts
@@ -0,0 +1,39 @@
+
+import { prisma } from 'db'
+import { strict as assert } from 'node:assert';
+
+async function reopenProposal (path: string, newDate: Date) {
+  const spaceDomain = path.split('/')[0];
+  const spacePagePath = path.split('/')[1];
+  const space = await prisma.space.findFirstOrThrow({
+    where: {
+      domain: spaceDomain
+    }
+  });
+
+  const page = await prisma.page.findFirstOrThrow({
+    where: {
+      spaceId: space.id,
+      path: spacePagePath
+    },
+    include: {
+      votes: true
+    }
+  });
+  console.log(page.votes, newDate);
+  const vote = page.votes[0];
+  assert(vote, 'Vote not found');
+
+  await prisma.vote.update({
+    where: {
+      id: page.votes[0].id
+    },
+    data: {
+      deadline: newDate,
+      status: 'InProgress'
+    }
+  });
+}
+
+reopenProposal('story-dao/page-04742242151671694', new Date(2022, 11, 21, 21 ));
+reopenProposal('story-dao/page-08009722798423935', new Date(2022, 11, 21, 23 ));
