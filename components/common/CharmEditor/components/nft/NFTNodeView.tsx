777d757ff30afed2ebaff8d713a4039c9b058ee5
Matt Casey <mattwad@gmail.com>
2022-12-19 17:26:25 -0700

    make opensea embed look decent inside of column

--- components/common/CharmEditor/components/nft/NFTNodeView.tsx
+++ components/common/CharmEditor/components/nft/NFTNodeView.tsx
@@ -1,7 +1,7 @@
 import styled from '@emotion/styled';
 import { Box } from '@mui/material';
 import Script from 'next/script';
-import { useEffect, useRef } from 'react';
+import { useEffect, useLayoutEffect, useRef, useState } from 'react';
 
 import BlockAligner from '../BlockAligner';
 import { MediaSelectionPopup } from '../common/MediaSelectionPopup';
@@ -22,11 +22,13 @@ const StyledContainer = styled.div`
   nft-card-front > div {
     background: transparent !important;
   }
+  width: 100%;
 `;
 
 export function NFTNodeView({ deleteNode, readOnly, node, updateAttrs }: CharmNodeViewProps) {
   const ref = useRef<HTMLDivElement>(null);
   const attrs = node.attrs as Partial<NodeAttrs>;
+  const [width, setWidth] = useState(0);
 
   useEffect(() => {
     if (ref.current) {
@@ -34,6 +36,14 @@ export function NFTNodeView({ deleteNode, readOnly, node, updateAttrs }: CharmNo
     }
   }, [ref.current]);
 
+  useLayoutEffect(() => {
+    const elm = ref.current;
+    if (elm) {
+      setTimeout(() => {
+        setWidth(elm.clientWidth);
+      }, 0);
+    }
+  }, [ref.current]);
   // If there are no source for the node, return the image select component
   if (!attrs.contract) {
     if (readOnly) {
@@ -65,15 +75,23 @@ export function NFTNodeView({ deleteNode, readOnly, node, updateAttrs }: CharmNo
     }
   }
 
+  const forceMobile = width < 400; // default width of document is 700
+
   return (
-    <>
+    <div>
       <Script id='opensea-script' src={widgetJS} />
       <BlockAligner onDelete={deleteNode}>
         <StyledContainer ref={ref}>
           {/* @ts-ignore nft-card element is from OpenSea */}
-          <nft-card contractAddress={attrs.contract} tokenId={attrs.token} width='100%'></nft-card>
+          <nft-card
+            contractAddress={attrs.contract}
+            orientationMode='manual' // it has to be manual since we set width
+            vertical={forceMobile === true ? true : undefined}
+            tokenId={attrs.token}
+            width='100%'
+          />
         </StyledContainer>
       </BlockAligner>
-    </>
+    </div>
   );
 }
