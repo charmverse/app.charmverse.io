1fa3216ca29d0a4f94e271ac23b7ba4008a7d7c1
Matt Casey <mattwad@gmail.com>
2022-12-21 20:33:15 -0700

    use charmeditor

--- components/forum/components/PostList/components/PostSummary.tsx
+++ components/forum/components/PostList/components/PostSummary.tsx
@@ -1,21 +1,6 @@
-import { DOMSerializer, Node } from 'prosemirror-model';
-import { useEffect, useRef } from 'react';
-
-import { specRegistry } from 'components/common/CharmEditor/specRegistry';
+import CharmEditor from 'components/common/CharmEditor/CharmEditor';
 import type { PageContent } from 'lib/prosemirror/interfaces';
 
 export function PostSummary({ content }: { content: PageContent | null }) {
-  const ref = useRef<HTMLDivElement>(null);
-  useEffect(() => {
-    if (content && ref.current) {
-      const contentNode = Node.fromJSON(specRegistry.schema, content);
-      DOMSerializer.fromSchema(specRegistry.schema).serializeFragment(contentNode.content, {}, ref.current);
-    }
-    return () => {
-      if (ref.current) {
-        ref.current.innerHTML = '';
-      }
-    };
-  }, []);
-  return <div ref={ref} />;
+  return <CharmEditor content={content ?? undefined} readOnly={true} />;
 }
