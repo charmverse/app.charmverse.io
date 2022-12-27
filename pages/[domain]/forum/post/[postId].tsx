b83abbec0e4f29c427ac30d3b9e360034ca7763c
Safwan Shaheer <devorein00@gmail.com>
2022-12-27 21:28:01 +0600

    Forum post standalone page

---
+++ pages/[domain]/forum/post/[postId].tsx
@@ -0,0 +1,22 @@
+import { useRouter } from 'next/router';
+import useSWR from 'swr';
+
+import charmClient from 'charmClient';
+import ErrorPage from 'components/common/errors/ErrorPage';
+import getPageLayout from 'components/common/PageLayout/getLayout';
+import { PostPage } from 'components/forum/components/PostPage/PostPage';
+
+export default function BlocksEditorPage() {
+  const router = useRouter();
+  const pagePath = router.query.postId as string;
+
+  const { data, isValidating } = useSWR(`post-page-${pagePath}`, () => charmClient.forum.getForumPost(pagePath));
+
+  if (!data && !isValidating) {
+    return <ErrorPage message={"Sorry, that page doesn't exist"} />;
+  }
+
+  return data ? <PostPage page={data} spaceId={data.spaceId} /> : null;
+}
+
+BlocksEditorPage.getLayout = getPageLayout;
