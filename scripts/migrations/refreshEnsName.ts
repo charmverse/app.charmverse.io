654b34e5c91aee16fc8525cb8e003c8615c798a6
Mohamad El Boudi <mo@anyahq.co>
2022-12-30 03:39:33 +0200

    Add script logging

--- scripts/migrations/refreshEnsName.ts
+++ scripts/migrations/refreshEnsName.ts
@@ -7,12 +7,22 @@ async function bulkRefreshEnsNames({skip = 0, concurrent = 10}: {skip?: number, 
   const totalUserWallets =  await prisma.userWallet.count();
 
   for (let i = skip; i < totalUserWallets; i += concurrent) {
+    console.log('Processing users', i, 'to', i + concurrent, 'of', totalUserWallets, '...')
     const userWallets = await prisma.userWallet.findMany({
       skip: i,
       take: concurrent,
     });
 
-    await Promise.all(userWallets.map(async (userWallet) => refreshENSName({userId: userWallet.userId, address: userWallet.address})));
+    let count = 0;
+
+    await Promise.all(userWallets.map(async (userWallet) => {
+      if (!userWallet.ensname) {
+        count += 1;
+        refreshENSName({userId: userWallet.userId, address: userWallet.address})
+      }
+    }));
+
+    console.log('Refreshed', count, 'users')
   }
 
 }
