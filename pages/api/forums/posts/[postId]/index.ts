f1db7580b440a69dd33b599d9e90a87fc1bf71fa
Mohamad El Boudi <mo@anyahq.co>
2023-01-03 17:39:29 +0200

    Relocate API endpoints using postId instead of pageId

--- pages/api/forums/posts/[postId]/index.ts
+++ pages/api/forums/posts/[postId]/index.ts
@@ -1,10 +1,10 @@
+import type { Post } from '@prisma/client';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import nc from 'next-connect';
 
-import { checkPostAccess } from 'lib/forums/posts/checkPostAccess';
+import { canEditPost } from 'lib/forums/posts/canEditPost';
 import { deleteForumPost } from 'lib/forums/posts/deleteForumPost';
 import { getForumPost } from 'lib/forums/posts/getForumPost';
-import type { ForumPostPage } from 'lib/forums/posts/interfaces';
 import { updateForumPost } from 'lib/forums/posts/updateForumPost';
 import { onError, onNoMatch, requireUser } from 'lib/middleware';
 import { PageNotFoundError } from 'lib/pages/server';
@@ -15,35 +15,35 @@ const handler = nc<NextApiRequest, NextApiResponse>({ onError, onNoMatch });
 handler.use(requireUser).get(getForumPostController).put(updateForumPostController).delete(deleteForumPostController);
 
 // TODO - Update posts
-async function updateForumPostController(req: NextApiRequest, res: NextApiResponse<ForumPostPage>) {
-  const { pageId } = req.query as any as { pageId: string };
+async function updateForumPostController(req: NextApiRequest, res: NextApiResponse<Post>) {
+  const { postId } = req.query as any as { postId: string };
   const userId = req.session.user.id;
 
-  await checkPostAccess({
-    pageId,
+  await canEditPost({
+    postId,
     userId
   });
 
-  await updateForumPost({ userId, postId: pageId, ...req.body });
+  await updateForumPost(postId, req.body);
 
   res.status(200).end();
 }
 
 async function deleteForumPostController(req: NextApiRequest, res: NextApiResponse) {
-  const { pageId } = req.query as any as { pageId: string };
+  const { postId } = req.query as any as { postId: string };
   const userId = req.session.user.id;
 
-  await checkPostAccess({
-    pageId,
+  await canEditPost({
+    postId,
     userId
   });
 
-  await deleteForumPost(pageId);
+  await deleteForumPost(postId);
 
   res.status(200).end();
 }
 
-async function getForumPostController(req: NextApiRequest, res: NextApiResponse<ForumPostPage>) {
+async function getForumPostController(req: NextApiRequest, res: NextApiResponse<Post>) {
   const { pageId } = req.query as any as { pageId: string };
   const userId = req.session.user.id;
 
