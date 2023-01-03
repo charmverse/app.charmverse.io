e1f11be9dbbf6eb02ca9923e2907cfd4edd22157
Mohamad El Boudi <mo@anyahq.co>
2023-01-03 14:12:03 +0200

    Update create forum post methods and API

--- lib/forums/posts/createForumPost.ts
+++ lib/forums/posts/createForumPost.ts
@@ -5,11 +5,10 @@ import { prisma } from 'db';
 import { getPagePath } from 'lib/pages/utils';
 import { InsecureOperationError } from 'lib/utilities/errors';
 
-import { selectPageValues } from './getForumPost';
-import type { ForumPostPage } from './interfaces';
-
-export type CreateForumPostInput = Pick<Page, 'createdBy' | 'spaceId' | 'content' | 'contentText' | 'title'> &
-  Pick<Post, 'categoryId'>;
+export type CreateForumPostInput = Pick<
+  Post,
+  'createdBy' | 'spaceId' | 'content' | 'contentText' | 'title' | 'categoryId'
+>;
 
 export async function createForumPost({
   content,
@@ -18,7 +17,7 @@ export async function createForumPost({
   spaceId,
   title,
   categoryId
-}: CreateForumPostInput): Promise<ForumPostPage> {
+}: CreateForumPostInput): Promise<Post> {
   if (categoryId) {
     const category = await prisma.postCategory.findUnique({
       where: {
@@ -36,13 +35,17 @@ export async function createForumPost({
 
   const postId = v4();
 
-  const createdPost = await prisma.page.create({
+  const createdPost = await prisma.post.create({
     data: {
       id: postId,
       title,
       content: (content ?? undefined) as Prisma.InputJsonObject,
       contentText,
-      updatedBy: createdBy,
+      category: {
+        connect: {
+          id: categoryId
+        }
+      },
       author: {
         connect: {
           id: createdBy
@@ -53,23 +56,9 @@ export async function createForumPost({
           id: spaceId
         }
       },
-      type: 'post',
-      path: getPagePath(),
-      post: {
-        create: {
-          id: postId,
-          spaceId,
-          categoryId
-        }
-      }
-    },
-    include: {
-      post: true
+      path: getPagePath()
     }
   });
 
-  return {
-    ...selectPageValues(createdPost),
-    post: createdPost.post!
-  };
+  return createdPost;
 }
