6bd42c1c908804ea532717c3aea022a251acdf5b
Mohamad El Boudi <mo@anyahq.co>
2023-01-06 00:19:38 +0200

    Fix for loading all tweets

--- components/common/CharmEditor/components/tweet/TweetNodeView.tsx
+++ components/common/CharmEditor/components/tweet/TweetNodeView.tsx
@@ -3,7 +3,7 @@ import styled from '@emotion/styled';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import { Box } from '@mui/material';
 import Script from 'next/script';
-import { useRef } from 'react';
+import { useEffect, useRef } from 'react';
 
 import log from 'lib/log';
 
@@ -65,11 +65,11 @@ export function TweetNodeView({ deleteNode, readOnly, node, selected, updateAttr
   const theme = useTheme();
   const attrs = node.attrs as Partial<TweetNodeAttrs>;
 
-  function onLoadScript() {
-    if (ref.current && attrs.id) {
+  useEffect(() => {
+    if (window.twttr && ref.current && attrs.id) {
       render(attrs.id, ref.current, { theme: theme.palette.mode });
     }
-  }
+  }, [!!window.twttr]);
 
   // If there are no source for the node, return the image select component
   if (!attrs.id) {
@@ -105,7 +105,7 @@ export function TweetNodeView({ deleteNode, readOnly, node, selected, updateAttr
 
   return (
     <>
-      <Script src={twitterWidgetJs} onReady={onLoadScript} />
+      <Script src={twitterWidgetJs} />
       <BlockAligner onDelete={deleteNode}>
         <StyledTweet ref={ref} />
       </BlockAligner>
