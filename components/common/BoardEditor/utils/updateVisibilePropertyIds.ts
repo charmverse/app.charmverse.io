04bd458d1a9fcf2de8a158887b2621ff84a10e15
Safwan Shaheer <devorein00@gmail.com>
2023-01-05 22:22:54 +0600

    Rename table column header name (#1418)

--- components/common/BoardEditor/utils/updateVisibilePropertyIds.ts
+++ components/common/BoardEditor/utils/updateVisibilePropertyIds.ts
@@ -5,10 +5,13 @@ import { Constants } from '../focalboard/src/constants';
 
 export function filterPropertyTemplates(visiblePropertyIds: string[], cardProperties: IPropertyTemplate[]) {
   const titleProperty: IPropertyTemplate = { id: Constants.titleColumnId, name: 'Title', type: 'text', options: [] };
+  const titleCardProperty = cardProperties.find((cardProperty) => cardProperty.id === Constants.titleColumnId);
   if (!visiblePropertyIds.includes(Constants.titleColumnId)) {
     visiblePropertyIds = [Constants.titleColumnId, ...visiblePropertyIds];
   }
   return visiblePropertyIds
-    .map((id) => (id === Constants.titleColumnId ? titleProperty : cardProperties.find((t) => t.id === id)))
+    .map((id) =>
+      id === Constants.titleColumnId ? titleCardProperty || titleProperty : cardProperties.find((t) => t.id === id)
+    )
     .filter(isTruthy);
 }
