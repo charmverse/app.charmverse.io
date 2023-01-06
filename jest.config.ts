8a5a71d420b452bd9082e5f25223b51a0cedd48d
Safwan Shaheer <devorein00@gmail.com>
2023-01-06 17:07:26 +0600

    Fixed broken test

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,3 @@
-import path from 'path';
-
 import createJestConfig from 'testing/createJestConfig';
 /*
  * For a detailed explanation regarding each configuration property and type check, visit:
@@ -29,9 +27,7 @@ export const jestConfig = {
   testEnvironment: 'jest-environment-node',
 
   // The glob patterns Jest uses to detect test files
-  testMatch: [
-    path.join(__dirname, '__integration-tests__/server/pages/api/forums/posts/[postId]/comments/[commentId].spec.ts')
-  ],
+  testMatch: ['**/lib/**/*.spec.ts', '**/testing/**/*.spec.ts', '**/background/**/*.spec.ts'],
 
   testTimeout: 30000,
   transform: {
