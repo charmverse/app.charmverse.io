d756d1ff0aad3181459c489b50bd70ec0e3b8e21
Matt Casey <mattwad@gmail.com>
2023-01-08 23:01:11 -0700

    enable forums for some DAOs (#1439)
    * enable forums for some DAOs
    
    * ..
    
    * simplify

--- hooks/useIsCharmverseSpace.tsx
+++ hooks/useIsCharmverseSpace.tsx
@@ -2,13 +2,15 @@ import { isProdEnv } from 'config/constants';
 
 import { useCurrentSpace } from './useCurrentSpace';
 
+const defaultDomains = ['charmverse'];
+
 /**
  * Returns true if the current space is charmverse, OR the current environment is not prod
  *
  * Use this hook for reserving some features in prod for charmverse users only
  */
-export function useIsCharmverseSpace() {
+export function useIsCharmverseSpace(allowedDomains = defaultDomains) {
   const currentSpace = useCurrentSpace();
 
-  return !isProdEnv || currentSpace?.domain === 'charmverse' || currentSpace?.domain.startsWith('cvt-');
+  return !isProdEnv || allowedDomains.includes(currentSpace?.domain ?? '') || currentSpace?.domain.startsWith('cvt-');
 }
