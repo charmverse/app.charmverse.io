3412a56ce9588a5648646e5f3d209cfa10ca27d3
Matt Casey <mattwad@gmail.com>
2023-01-11 11:28:23 -0700

    Fix google refresh (#1465)
    * fix refreshing google form data
    
    * ..
    
    * remove type

--- lib/google/forms/getFormAndResponses.ts
+++ lib/google/forms/getFormAndResponses.ts
@@ -17,19 +17,19 @@ export async function getFormAndResponses(sourceData: GoogleFormSourceData, last
     formId
   });
 
-  // retrieve only the latest responses since we last updated
-  let pageToken: string | null | undefined = 'default';
+  const reqBody: googlForms.forms_v1.Params$Resource$Forms$Responses$List = {
+    formId,
+    filter: lastUpdated ? `timestamp >= ${lastUpdated.toISOString()}` : undefined
+  };
   const responses: GoogleFormResponse[] = [];
+
   let maxCalls = 20; // avoid endless loop
-  while (pageToken && maxCalls > 0) {
-    const res = await forms.forms.responses.list({
-      filter: lastUpdated ? `timestamp >= ${lastUpdated.toISOString()}` : undefined,
-      formId
-    });
+  while ((!reqBody.hasOwnProperty('pageToken') || reqBody.pageToken) && maxCalls > 0) {
+    const res = await forms.forms.responses.list(reqBody);
     if (res.data.responses) {
       responses.push(...res.data.responses);
     }
-    pageToken = res.data.nextPageToken;
+    reqBody.pageToken = res.data.nextPageToken ?? undefined;
     maxCalls -= 1;
   }
 
