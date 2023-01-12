2e33fdf5868325d6aea6b81dd11de290bfe17a79
Matt Casey <mattwad@gmail.com>
2023-01-12 11:44:56 -0700

    export types still

--- db.ts
+++ db.ts
@@ -2,7 +2,7 @@ import type { Prisma } from '@prisma/client';
 import { PrismaClient } from '@prisma/client';
 
 // @ts-expect-error - dont mutate global for Node.js
-export const prisma = global.prisma || new PrismaClient({});
+export const prisma: PrismaClient = global.prisma || new PrismaClient({});
 
 // remember this instance of prisma in development to avoid too many clients
 if (process.env.NODE_ENV === 'development') {
