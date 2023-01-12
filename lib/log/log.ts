fd1af98f14e201f0b9714b00ba06c1459e1c19ac
Matt Casey <mattwad@gmail.com>
2023-01-12 08:57:56 -0700

    remove global typescript defs for prisma and log

--- lib/log/log.ts
+++ lib/log/log.ts
@@ -2,15 +2,11 @@ import logBase from 'loglevel';
 
 import { apply } from './logLevel';
 
-// Export the singleton instance
-declare global {
-  // eslint-disable-next-line no-var, vars-on-top
-  var log: logBase.Logger;
-}
-
+// @ts-expect-error - dont mutate global for Node.js
 export const log = global.log ?? apply(logBase.getLogger('default'));
 
 // remember this instance of prisma in development to avoid too many clients
 if (process.env.NODE_ENV === 'development') {
+  // @ts-expect-error
   global.log = log;
 }
