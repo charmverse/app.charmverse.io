0d15abe59e4b373365cd178e4943c14049dfa348
Valentin <ludu.valentin@gmail.com>
2023-01-17 10:31:56 +0200

    Update poll deadline (#1500)
    * Update poll deadline
    
    * fix tests
    
    * Added test
    
    * Do not allow times in the past
    
    * Prevent changing time in the past

--- components/votes/components/VoteActionsMenu.tsx
+++ components/votes/components/VoteActionsMenu.tsx
@@ -1,10 +1,16 @@
+import DateRangeIcon from '@mui/icons-material/DateRange';
 import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
 import DoNotDisturbIcon from '@mui/icons-material/DoNotDisturb';
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
-import { IconButton, ListItemText, Menu, MenuItem } from '@mui/material';
-import type { VoteContext } from '@prisma/client';
-import { bindMenu, usePopupState } from 'material-ui-popup-state/hooks';
+import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
+import ListItemText from '@mui/material/ListItemText';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import type { VoteContext, VoteStatus } from '@prisma/client';
+import { bindMenu, bindPopover, anchorRef, usePopupState } from 'material-ui-popup-state/hooks';
 
+import ConfirmDeadlinePopover from 'components/common/CharmEditor/components/inlineVote/components/ConfirmDeadlinePopover';
 import ConfirmDeleteModal from 'components/common/Modal/ConfirmDeleteModal';
 import { useUser } from 'hooks/useUser';
 
@@ -12,13 +18,21 @@ interface VoteActionsProps {
   deleteVote?: (voteId: string) => Promise<void>;
   cancelVote: (voteId: string) => Promise<void>;
   removeFromPage?: (voteId: string) => void;
-  vote: { createdBy: string; id: string; deadline?: Date; status: string; title: string; context: VoteContext };
+  updateDeadline?: (voteId: string, deadline: Date) => Promise<void>;
+  vote: { createdBy: string; id: string; deadline?: Date; status: VoteStatus; title: string; context: VoteContext };
 }
 
-export default function VoteActionsMenu({ cancelVote, deleteVote, removeFromPage, vote }: VoteActionsProps) {
+export default function VoteActionsMenu({
+  cancelVote,
+  deleteVote,
+  removeFromPage,
+  updateDeadline,
+  vote
+}: VoteActionsProps) {
   const { user } = useUser();
   const actionsPopup = usePopupState({ variant: 'popover', popupId: 'inline-votes-action' });
-  const popupState = usePopupState({ variant: 'popover', popupId: 'delete-inline-vote' });
+  const deletePopup = usePopupState({ variant: 'popover', popupId: 'delete-inline-vote' });
+  const deadlinePopup = usePopupState({ variant: 'popover', popupId: 'deadline-inline-vote' });
   const hasPassedDeadline = Boolean(vote.deadline && new Date(vote.deadline) <= new Date());
 
   const deleteQuestion = vote.title ? (
@@ -31,8 +45,20 @@ export default function VoteActionsMenu({ cancelVote, deleteVote, removeFromPage
     <p>Are you sure you want to remove the vote from this proposal?</p>
   );
 
-  return vote.context === 'inline' ? (
+  return vote.context === 'inline' || vote.status === 'InProgress' ? (
     <>
+      <Box position='relative' ref={(c: any) => anchorRef(deadlinePopup)(c)}>
+        {vote.deadline && updateDeadline && (
+          <ConfirmDeadlinePopover
+            initialDeadline={vote.deadline}
+            updateDeadline={async (deadline: Date) => {
+              deadlinePopup.setOpen(false);
+              await updateDeadline(vote.id, deadline);
+            }}
+            {...bindPopover(deadlinePopup)}
+          />
+        )}
+      </Box>
       {vote.createdBy === user?.id && (
         <IconButton size='small' onClick={actionsPopup.open}>
           <MoreHorizIcon fontSize='small' />
@@ -40,8 +66,8 @@ export default function VoteActionsMenu({ cancelVote, deleteVote, removeFromPage
       )}
       <ConfirmDeleteModal
         title='Delete vote'
-        onClose={popupState.close}
-        open={popupState.isOpen}
+        onClose={deletePopup.close}
+        open={deletePopup.isOpen}
         buttonText='Delete'
         onConfirm={() => {
           removeFromPage?.(vote.id);
@@ -58,7 +84,7 @@ export default function VoteActionsMenu({ cancelVote, deleteVote, removeFromPage
           actionsPopup.close();
         }}
       >
-        {vote.status === 'InProgress' && !hasPassedDeadline && (
+        {vote.status === 'InProgress' && !hasPassedDeadline && vote.context === 'inline' && (
           <MenuItem
             dense
             onClick={() => {
@@ -70,10 +96,16 @@ export default function VoteActionsMenu({ cancelVote, deleteVote, removeFromPage
             <ListItemText>Cancel vote</ListItemText>
           </MenuItem>
         )}
-        {deleteVote && (
-          <MenuItem dense onClick={popupState.open}>
+        {deleteVote && vote.context === 'inline' && (
+          <MenuItem dense onClick={deletePopup.open}>
             <DeleteOutlineIcon fontSize='small' sx={{ mr: 1 }} />
             <ListItemText>Delete vote</ListItemText>
+          </MenuItem>
+        )}
+        {vote.status === 'InProgress' && vote.deadline && !hasPassedDeadline && updateDeadline && (
+          <MenuItem dense onClick={(event) => deadlinePopup.open(event)}>
+            <DateRangeIcon fontSize='small' sx={{ mr: 1 }} />
+            <ListItemText>Change deadline</ListItemText>
           </MenuItem>
         )}
       </Menu>
