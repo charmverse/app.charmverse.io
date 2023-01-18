164851fd79efdc67eee00473d8a08d340832faf2
Marek <m.sadura@outlook.com>
2023-01-18 11:53:02 +0100

    Code refactor and cleanup

--- lib/aws/config.ts
+++ lib/aws/config.ts
@@ -1,3 +1,2 @@
 export const AWS_REGION = 'us-east-1';
-export const SQS_NAME = 'stg-webhook-collabland.fifo';
 export const SQS_URL = 'https://sqs.us-east-1.amazonaws.com/310849459438/stg-webhook-collabland.fifo';
