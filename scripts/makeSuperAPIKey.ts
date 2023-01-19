3b0a2c6cae5caf7ccc6057604e262df33e20c338
Matt Casey <mattwad@gmail.com>
2023-01-19 16:32:36 -0700

    improve DX for super tokens

--- scripts/makeSuperAPIKey.ts
+++ scripts/makeSuperAPIKey.ts
@@ -5,13 +5,14 @@ import { provisionSuperApiKey } from 'lib/middleware/requireSuperApiKey';
 // use this file and run against production to generate super api keys
 // Super API keys allow you to make requests to the API to do things like generate a new space
 const name = '';
+const token = ''; // set this to create a specific token
 
 (async () => {
     if (!name) {
       throw 'Please provide a name for the key';
     }
 
-    const key = await provisionSuperApiKey(name);
+    const key = await provisionSuperApiKey(name, token);
     console.log('Super api key provisioned:', key);
 
 })();
