a265e2618ad19c7c91760b24b9b353c5a09782fa
Matt Casey <mattwad@gmail.com>
2023-01-20 13:08:15 -0700

    clean up code

--- components/common/CharmEditor/components/nft/nft.ts
+++ components/common/CharmEditor/components/nft/nft.ts
@@ -1,7 +1,6 @@
 import { Plugin, NodeView } from '@bangle.dev/core';
-import type { EditorView, Slice } from '@bangle.dev/pm';
+import type { EditorView } from '@bangle.dev/pm';
 
-import log from 'lib/log';
 import { insertNode } from 'lib/prosemirror/insertNode';
 
 import { name } from './config';
@@ -17,12 +16,9 @@ export function plugins() {
     }),
     new Plugin({
       props: {
-        handlePaste: (view: EditorView, rawEvent: ClipboardEvent, slice: Slice) => {
-          // @ts-ignore
-          const contentRow = slice.content.content?.[0]?.content.content?.[0] ?? slice.content.content?.[0];
+        handlePaste: (view: EditorView, rawEvent: ClipboardEvent) => {
           const text = rawEvent.clipboardData?.getData('text/plain');
           const attrs = extractAttrsFromEmbedCode(text ?? '');
-          log.debug('pasted nft content', { contentRow, attrs, text });
           if (attrs) {
             insertNode(name, view.state, view.dispatch, view, attrs);
             return true;
