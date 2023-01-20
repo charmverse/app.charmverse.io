908fd31a0961d6ac6db5de8b0e41239884c69737
Valentin L <ludu.valentin@gmail.com>
2023-01-20 10:52:03 +0200

    Improve code

--- pages/[domain]/forum/[categoryPath]/index.tsx
+++ pages/[domain]/forum/[categoryPath]/index.tsx
@@ -2,7 +2,6 @@ import getPageLayout from 'components/common/PageLayout/getLayout';
 import { PostDialogProvider } from 'components/forum/components/PostDialog/hooks/usePostDialog';
 import PostDialogGlobal from 'components/forum/components/PostDialog/PostDialogGlobal';
 import { ForumPage } from 'components/forum/ForumPage';
-import { setTitle } from 'hooks/usePageTitle';
 
 export default function ForumPageComponent() {
   return (
