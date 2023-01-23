692e6cc28e18b4aebe9973dc8fa7276af8807437
Matt Casey <mattwad@gmail.com>
2023-01-23 15:57:39 -0700

    add back suffix to posts

--- lib/forums/posts/getPostPath.ts
+++ lib/forums/posts/getPostPath.ts
@@ -1,7 +1,7 @@
-import { stringToValidPath } from 'lib/utilities/strings';
+import { stringToValidPath, uid } from 'lib/utilities/strings';
 
 const maxTitleCharacters = 50;
 
 export function getPostPath(postTitle: string): string {
-  return stringToValidPath(postTitle, maxTitleCharacters);
+  return `${stringToValidPath(postTitle, maxTitleCharacters)}_${uid()}`;
 }
