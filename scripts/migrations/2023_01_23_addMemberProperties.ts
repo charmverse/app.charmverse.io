21dad04f264b3d80193d2693248fe112f9673735
Matt Casey <mattwad@gmail.com>
2023-01-23 21:08:40 -0700

    inject icons

--- scripts/migrations/2023_01_23_addMemberProperties.ts
+++ scripts/migrations/2023_01_23_addMemberProperties.ts
@@ -25,26 +25,43 @@ async function init () {
         }
       });
       if (admin) {
-        const lastIndex = Math.max(...space.memberProperties.map(prop => prop.index));
-        console.log('lastIndex', lastIndex)
+        // we want to inject these after the last social property
+        const lastSocialIndex = Math.max(...space.memberProperties.filter(prop => prop.type === 'twitter' || prop.type === 'discord').map(prop => prop.index));
+
         const createdBy = admin.id;
-        await prisma.memberProperty.createMany({
-          data: [{
-            createdBy,
-            updatedBy: createdBy,
-            index: lastIndex + 1,
-            name: 'LinkedIn',
-            type: 'linked_in',
-            spaceId: space.id
-          },{
-            createdBy,
-            updatedBy: createdBy,
-            index: lastIndex + 2,
-            name: 'GitHub',
-            type: 'github',
-            spaceId: space.id
-          }],
-        })
+        await prisma.$transaction([
+          // update all indexes after the last social index
+          prisma.memberProperty.updateMany({
+            where: {
+              spaceId: space.id,
+              index: {
+                gt: lastSocialIndex
+              }
+            },
+            data: {
+              index:{
+                increment: 2
+              }
+            }
+          }),
+          prisma.memberProperty.createMany({
+              data: [{
+                createdBy,
+                updatedBy: createdBy,
+                index: lastSocialIndex + 1,
+                name: 'LinkedIn',
+                type: 'linked_in',
+                spaceId: space.id
+              },{
+                createdBy,
+                updatedBy: createdBy,
+                index: lastSocialIndex + 2,
+                name: 'GitHub',
+                type: 'github',
+                spaceId: space.id
+              }],
+          })
+        ])
       }
       else {
         console.warn('No admin for space', { spaceId: space.id, spaceName: space.name })
