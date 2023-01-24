44662fac2c0cceebfc9464893782ebb6b68126da
Matt Casey <mattwad@gmail.com>
2023-01-24 12:18:21 -0700

    update gnosis

--- hooks/useGnosisPayment.ts
+++ hooks/useGnosisPayment.ts
@@ -1,6 +1,6 @@
-import type { MetaTransactionData } from '@gnosis.pm/safe-core-sdk-types';
-import EthersAdapter from '@gnosis.pm/safe-ethers-lib';
-import SafeServiceClient from '@gnosis.pm/safe-service-client';
+import type { MetaTransactionData } from '@safe-global/safe-core-sdk-types';
+import EthersAdapter from '@safe-global/safe-ethers-lib';
+import SafeServiceClient from '@safe-global/safe-service-client';
 import { getChainById } from 'connectors';
 import { ethers, utils } from 'ethers';
 
@@ -34,27 +34,27 @@ export function useGnosisPayment({ chainId, safeAddress, transactions, onSuccess
     if (!safe || !account || !network?.gnosisUrl) {
       return;
     }
-    const safeTransaction = await safe.createTransaction(
-      transactions.map((transaction) => ({
+    const safeTransaction = await safe.createTransaction({
+      safeTransactionData: transactions.map((transaction) => ({
         data: transaction.data,
         to: transaction.to,
         value: transaction.value,
         operation: transaction.operation
       }))
-    );
-    await safe.signTransaction(safeTransaction);
+    });
     const txHash = await safe.getTransactionHash(safeTransaction);
     const signer = await library.getSigner(account);
     const ethAdapter = new EthersAdapter({
       ethers,
-      signer
+      signerOrProvider: signer
     });
     const safeService = new SafeServiceClient({ txServiceUrl: network.gnosisUrl, ethAdapter });
     await safeService.proposeTransaction({
       safeAddress,
-      safeTransaction,
+      safeTransactionData: safeTransaction.data,
       safeTxHash: txHash,
       senderAddress: utils.getAddress(account),
+      senderSignature: [...safeTransaction.signatures][0][0],
       origin
     });
     onSuccess({ safeAddress, transactions, txHash });
