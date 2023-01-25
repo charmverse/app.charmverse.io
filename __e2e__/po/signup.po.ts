581fdcbbb3feb01860187bc650cf1da55a6468c3
Mohamad El Boudi <mo@anyahq.co>
2023-01-25 23:16:03 +0200

    Fix e2e testsagain

--- __e2e__/po/signup.po.ts
+++ __e2e__/po/signup.po.ts
@@ -1,5 +1,6 @@
 // playwright-dev-page.ts
 import type { Locator, Page } from '@playwright/test';
+import type { Space } from '@prisma/client';
 
 // capture actions on the pages in signup flow
 export class SignUpPage {
@@ -7,14 +8,11 @@ export class SignUpPage {
 
   readonly selectNewWorkspaceButton: Locator;
 
-  readonly workspaceFormDomainInput: Locator;
-
   readonly workspaceFormSubmit: Locator;
 
   constructor(page: Page) {
     this.page = page;
     this.selectNewWorkspaceButton = page.locator('data-test=goto-create-workspace');
-    this.workspaceFormDomainInput = page.locator('data-test=workspace-domain-input');
     this.workspaceFormSubmit = page.locator('data-test=create-workspace');
   }
 
@@ -35,8 +33,17 @@ export class SignUpPage {
     await this.selectNewWorkspaceButton.click();
   }
 
-  async submitWorkspaceForm({ domain }: { domain: string }) {
-    await this.workspaceFormDomainInput.fill(domain);
-    await this.workspaceFormSubmit.click();
+  async submitWorkspaceForm(): Promise<Space> {
+    this.workspaceFormSubmit.click();
+
+    const response = await this.page.waitForResponse('**/api/spaces');
+
+    const parsedResponse = await response.json();
+
+    if (response.status() >= 400) {
+      throw parsedResponse;
+    }
+
+    return parsedResponse as Space;
   }
 }
