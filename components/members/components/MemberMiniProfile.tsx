a448566724ce6ab23c0d4158dc9352ce771f1a95
Safwan Shaheer <devorein00@gmail.com>
2023-01-31 21:04:03 +0600

    Add view full profile button

--- components/members/components/MemberMiniProfile.tsx
+++ components/members/components/MemberMiniProfile.tsx
@@ -1,11 +1,14 @@
 import { Dialog, DialogContent, Typography, useMediaQuery } from '@mui/material';
-import { useTheme } from '@mui/system';
+import { Stack, useTheme } from '@mui/system';
+import type { MemberProperty, MemberPropertyType } from '@prisma/client';
 import useSWR from 'swr';
 
 import charmClient from 'charmClient';
+import Button from 'components/common/Button';
 import LoadingComponent from 'components/common/LoadingComponent';
 import { DialogTitle } from 'components/common/Modal';
 import { UserDetails } from 'components/profile/components';
+import { useMemberProperties } from 'hooks/useMemberProperties';
 import type { Member } from 'lib/members/interfaces';
 
 import { SpacesMemberDetails } from '../../profile/components/SpacesMemberDetails/SpacesMemberDetails';
@@ -13,6 +16,15 @@ import { SpacesMemberDetails } from '../../profile/components/SpacesMemberDetail
 export function MemberMiniProfile({ member, onClose }: { member: Member; onClose: VoidFunction }) {
   const theme = useTheme();
   const fullScreen = useMediaQuery(theme.breakpoints.down('md'));
+  const { properties = [] } = useMemberProperties();
+  const propertiesRecord = properties.reduce((record, prop) => {
+    record[prop.type] = prop;
+    return record;
+  }, {} as Record<MemberPropertyType, MemberProperty>);
+
+  const username =
+    (member.properties.find((memberProperty) => memberProperty.memberPropertyId === propertiesRecord.name?.id)
+      ?.value as string) ?? member.username;
 
   const { data: user, isLoading } = useSWR(`users/${member.path}`, () =>
     charmClient.getUserByPath(member.path as string)
@@ -26,8 +38,23 @@ export function MemberMiniProfile({ member, onClose }: { member: Member; onClose
         </DialogContent>
       ) : (
         <>
-          <DialogTitle sx={{ '&&': { px: 2, py: 2 } }} onClose={onClose}>
-            <Typography>{user.username} public profile</Typography>
+          <DialogTitle
+            sx={{ '&&': { px: 2, py: 2 }, display: 'flex', justifyContent: 'space-between' }}
+            onClose={onClose}
+          >
+            <Stack display='flex' flexDirection='row' width='100%' alignItems='center' justifyContent='space-between'>
+              <Typography>{username} public profile</Typography>
+              <Button
+                href={`/u/${user.path}`}
+                color='secondary'
+                variant='outlined'
+                sx={{
+                  mx: 1
+                }}
+              >
+                View full profile
+              </Button>
+            </Stack>
           </DialogTitle>
           <DialogContent dividers>
             <UserDetails user={user} readOnly />
