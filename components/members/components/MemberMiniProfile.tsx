67fa20100f85e1abc60d39925518dd6fc13c14a0
Safwan Shaheer <devorein00@gmail.com>
2023-02-01 16:37:33 +0600

    Removed accordion and added space properties to mini profile

--- components/members/components/MemberMiniProfile.tsx
+++ components/members/components/MemberMiniProfile.tsx
@@ -1,5 +1,5 @@
 import { Dialog, DialogContent, Typography, useMediaQuery } from '@mui/material';
-import { Stack, useTheme } from '@mui/system';
+import { Box, Stack, useTheme } from '@mui/system';
 import type { MemberProperty, MemberPropertyType } from '@prisma/client';
 import useSWR from 'swr';
 
@@ -8,19 +8,25 @@ import Button from 'components/common/Button';
 import LoadingComponent from 'components/common/LoadingComponent';
 import { DialogTitle } from 'components/common/Modal';
 import { UserDetails } from 'components/profile/components';
+import { MemberPropertiesRenderer } from 'components/profile/components/SpacesMemberDetails/components/MemberPropertiesRenderer';
+import { useCurrentSpace } from 'hooks/useCurrentSpace';
 import { useMemberProperties } from 'hooks/useMemberProperties';
+import { useMemberPropertyValues } from 'hooks/useMemberPropertyValues';
 import type { Member } from 'lib/members/interfaces';
-
-import { SpacesMemberDetails } from '../../profile/components/SpacesMemberDetails/SpacesMemberDetails';
 
 export function MemberMiniProfile({ member, onClose }: { member: Member; onClose: VoidFunction }) {
   const theme = useTheme();
+  const currentSpace = useCurrentSpace();
   const fullScreen = useMediaQuery(theme.breakpoints.down('md'));
   const { properties = [] } = useMemberProperties();
   const propertiesRecord = properties.reduce((record, prop) => {
     record[prop.type] = prop;
     return record;
   }, {} as Record<MemberPropertyType, MemberProperty>);
+  const { memberPropertyValues = [] } = useMemberPropertyValues(member.id);
+  const currentSpacePropertyValues = memberPropertyValues.find(
+    (memberPropertyValue) => memberPropertyValue.spaceId === currentSpace?.id
+  );
 
   const username =
     (member.properties.find((memberProperty) => memberProperty.memberPropertyId === propertiesRecord.name?.id)
@@ -58,7 +64,12 @@ export function MemberMiniProfile({ member, onClose }: { member: Member; onClose
           </DialogTitle>
           <DialogContent dividers>
             <UserDetails user={user} readOnly />
-            <SpacesMemberDetails memberId={user.id} expandCurrentSpace showOnlyCurrentSpace />
+
+            {currentSpacePropertyValues && (
+              <Box my={3}>
+                <MemberPropertiesRenderer properties={currentSpacePropertyValues.properties} />
+              </Box>
+            )}
           </DialogContent>
         </>
       )}
