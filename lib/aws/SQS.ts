fd04f2827fb2b51a8467a087099929dd5f766ec3
Rieranthony <rieraanthony13@gmail.com>
2023-02-02 13:07:37 +0000

    feat: fix aws region config

--- lib/aws/SQS.ts
+++ lib/aws/SQS.ts
@@ -2,11 +2,12 @@ import { SQS } from '@aws-sdk/client-sqs';
 
 const AWS_SECRET = process.env.AWS_SECRET_ACCESS_KEY as string;
 const AWS_KEY_ID = process.env.AWS_ACCESS_KEY_ID as string;
+const AWS_REGION = process.env.AWS_REGION;
 
 // Connect to SQS
 const sqs = new SQS({
   apiVersion: 'latest',
-  region: process.env.AWS_REGION,
+  region: AWS_REGION || 'us-east-1',
   credentials: {
     secretAccessKey: AWS_SECRET,
     accessKeyId: AWS_KEY_ID
