7e6006ba96cf628943b34898d487c604263317fb
Rieranthony <rieraanthony13@gmail.com>
2023-02-06 14:24:50 +0000

    feat: fix sqs credentials

--- lib/aws/SQS.ts
+++ lib/aws/SQS.ts
@@ -1,18 +1,20 @@
+import type { SQSClientConfig } from '@aws-sdk/client-sqs';
 import { SQS } from '@aws-sdk/client-sqs';
 
-const AWS_SECRET = process.env.AWS_SECRET_ACCESS_KEY as string;
-const AWS_KEY_ID = process.env.AWS_ACCESS_KEY_ID as string;
-const AWS_REGION = process.env.AWS_REGION;
+import { AWS_REGION } from './config';
+
+const AWS_API_KEY = process.env.AWS_ACCESS_KEY_ID as string;
+const AWS_API_SECRET = process.env.AWS_SECRET_ACCESS_KEY as string;
+const SQS_REGION = (process.env.AWS_REGION as string) || AWS_REGION;
+
+const config: SQSClientConfig = { region: SQS_REGION, apiVersion: 'latest' };
+
+if (AWS_API_KEY && AWS_API_SECRET) {
+  config.credentials = { accessKeyId: AWS_API_KEY, secretAccessKey: AWS_API_SECRET };
+}
 
 // Connect to SQS
-const sqs = new SQS({
-  apiVersion: 'latest',
-  region: AWS_REGION || 'us-east-1',
-  credentials: {
-    secretAccessKey: AWS_SECRET,
-    accessKeyId: AWS_KEY_ID
-  }
-});
+const sqs = new SQS(config);
 
 export async function addMessageToSQS(queueUrl: string, stringifiedBody: string) {
   await sqs.sendMessage({
