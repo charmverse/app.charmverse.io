4c9084b34aad117744dafe6930c1ba30dceec143
Marek <m.sadura@outlook.com>
2023-02-08 12:45:07 +0100

    update test

--- hooks/__tests__/useDateFormatter.spec.ts
+++ hooks/__tests__/useDateFormatter.spec.ts
@@ -18,14 +18,14 @@ describe('useDateFormatter', () => {
     const testDate = '2023-02-08T07:59:48.698Z';
     const testDate2 = '2023-02-28T17:59:48.698Z';
 
-    expect(result.current.formatDateTime(testDate)).toBe('2/8/23, 7:59 AM');
-    expect(result.current.formatDateTime(testDate2)).toBe('2/28/23, 5:59 PM');
+    expect(result.current.formatDateTime(testDate)).toEqual('2/8/23, 7:59 AM');
+    expect(result.current.formatDateTime(testDate2)).toEqual('2/28/23, 5:59 PM');
 
     (useUserPreferences as jest.Mock<any, any>).mockReturnValue({ userPreferences: { locale: 'en-GB' } });
     rerender();
 
-    expect(result.current.formatDateTime(testDate)).toBe('08/02/2023, 07:59');
-    expect(result.current.formatDateTime(testDate2)).toBe('28/02/2023, 17:59');
+    expect(result.current.formatDateTime(testDate)).toEqual('08/02/2023, 07:59');
+    expect(result.current.formatDateTime(testDate2)).toEqual('28/02/2023, 17:59');
   });
 
   test('should format date in user friendly format using locale from user preferences', () => {
@@ -34,16 +34,16 @@ describe('useDateFormatter', () => {
     const testDate = '2023-02-08T07:59:48.698Z';
     const testDate2 = '2023-11-28T17:59:48.698Z';
 
-    expect(result.current.formatDate(testDate)).toBe('Feb 8');
-    expect(result.current.formatDate(testDate, { withYear: true })).toBe('Feb 8, 2023');
-    expect(result.current.formatDate(testDate2)).toBe('Nov 28');
+    expect(result.current.formatDate(testDate)).toEqual('Feb 8');
+    expect(result.current.formatDate(testDate, { withYear: true })).toEqual('Feb 8, 2023');
+    expect(result.current.formatDate(testDate2)).toEqual('Nov 28');
 
     (useUserPreferences as jest.Mock<any, any>).mockReturnValue({ userPreferences: { locale: 'en-GB' } });
     rerender();
 
-    expect(result.current.formatDate(testDate)).toBe('8 Feb');
-    expect(result.current.formatDate(testDate, { withYear: true })).toBe('8 Feb 2023');
-    expect(result.current.formatDate(testDate2)).toBe('28 Nov');
+    expect(result.current.formatDate(testDate)).toEqual('8 Feb');
+    expect(result.current.formatDate(testDate, { withYear: true })).toEqual('8 Feb 2023');
+    expect(result.current.formatDate(testDate2)).toEqual('28 Nov');
   });
 
   test('should format time using locale from user preferences', () => {
@@ -52,13 +52,13 @@ describe('useDateFormatter', () => {
     const testDate = '2023-02-08T07:59:48.698Z';
     const testDate2 = '2023-02-28T17:59:48.698Z';
 
-    expect(result.current.formatTime(testDate)).toBe('7:59 AM');
-    expect(result.current.formatTime(testDate2)).toBe('5:59 PM');
+    expect(result.current.formatTime(testDate)).toEqual('7:59 AM');
+    expect(result.current.formatTime(testDate2)).toEqual('5:59 PM');
 
     (useUserPreferences as jest.Mock<any, any>).mockReturnValue({ userPreferences: { locale: 'en-GB' } });
     rerender();
 
-    expect(result.current.formatTime(testDate)).toBe('07:59');
-    expect(result.current.formatTime(testDate2)).toBe('17:59');
+    expect(result.current.formatTime(testDate)).toEqual('07:59');
+    expect(result.current.formatTime(testDate2)).toEqual('17:59');
   });
 });
