ec852260292304f33ac9715f07c168a425ef2bf4
Matt Casey <mattwad@gmail.com>
2023-02-09 13:21:32 -0700

    ..

--- hooks/useSpaceFromPath.ts
+++ hooks/useSpaceFromPath.ts
@@ -5,8 +5,8 @@ import { useSpaces } from 'hooks/useSpaces';
 
 export function useSpaceFromPath(): Space | null {
   const router = useRouter();
-  const baseSpaceId = router.query.domain as string | undefined;
   const { spaces } = useSpaces();
-  const baseSpace = Object.values(spaces).find((space) => space?.id === baseSpaceId || space?.domain === baseSpaceId);
-  return baseSpace || null;
+  const spaceDomain = router.query.domain as string | undefined;
+  const spaceFromPath = Object.values(spaces).find((space) => space.domain === spaceDomain);
+  return spaceFromPath || null;
 }
