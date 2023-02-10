e40f49804b6b9d2a0ebcaa8244637b768e44a863
Safwan Shaheer <devorein00@gmail.com>
2023-02-10 16:42:58 +0600

    Return user profile from gssp

--- pages/u/[userPath].tsx
+++ pages/u/[userPath].tsx
@@ -1,84 +1,118 @@
 import { Box } from '@mui/material';
+import log from 'loglevel';
 import type { GetServerSideProps, InferGetServerSidePropsType } from 'next';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 import type { ReactElement } from 'react';
 import { useEffect } from 'react';
+import useSWR from 'swr';
 
-import fetch from 'adapters/http/fetch.server';
+import charmClient from 'charmClient';
 import ErrorPage from 'components/common/errors/ErrorPage';
-import PageLayout from 'components/nexus/components/NexusLayout';
+import { CenteredPageContent } from 'components/common/PageLayout/components/PageContent';
 import PublicProfile from 'components/profile/PublicProfile';
+import { prisma } from 'db';
 import { usePageTitle } from 'hooks/usePageTitle';
+import { isUUID } from 'lib/utilities/strings';
 import type { PublicUser } from 'pages/api/public/profile/[userId]';
 
-export const getServerSideProps: GetServerSideProps<{ user: PublicUser | null }, { userPath: string }> = async (
+export const getServerSideProps: GetServerSideProps<{ publicUser: PublicUser | null }, { userPath: string }> = async (
   context
 ) => {
   const params = context.params;
-  const origin = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://app.charmverse.io';
+  const userPath = params?.userPath;
   try {
-    if (params?.userPath) {
-      const user = await fetch<PublicUser>(`${origin}/api/public/profile/${params.userPath}`);
+    if (userPath) {
+      const condition = isUUID(userPath)
+        ? {
+            OR: [{ id: userPath }, { path: userPath }]
+          }
+        : { path: userPath };
+
+      const user = await prisma.user.findFirst({
+        where: condition,
+        include: {
+          profile: true
+        }
+      });
+
+      if (!user) {
+        return {
+          props: {
+            publicUser: null
+          }
+        };
+      }
+
       return {
         props: {
-          user
+          publicUser: {
+            ...user,
+            visibleNfts: [],
+            visiblePoaps: []
+          }
         }
       };
     }
     return {
       props: {
-        user: null
+        publicUser: null
       }
     };
   } catch (err) {
+    log.error('Error fetching public user profile', err);
     return {
       props: {
-        user: null
+        publicUser: null
       }
     };
   }
 };
 
-export default function UserProfilePage({ user }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+export default function UserProfilePage({ publicUser }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   const [, setPageTitle] = usePageTitle();
+  const router = useRouter();
+  const userPath = router.query.userPath as string;
+
+  const { data: user, error } = useSWR(`public-profile-${userPath}`, () => charmClient.getUserByPath(userPath));
 
   useEffect(() => {
     let pageTitle = '';
-    if (user) {
-      pageTitle = user.username || '';
+    if (publicUser) {
+      pageTitle = publicUser.username || '';
     }
     setPageTitle(pageTitle);
-  }, [user]);
+  }, [publicUser]);
 
-  if (!user) {
+  if (error?.status === 404 || !publicUser) {
     return (
       <Box my={12}>
         <ErrorPage message={"Sorry, that user doesn't exist"} />
       </Box>
     );
-  } else {
-    return (
-      <>
-        <Head>
-          <title>{user.username} | Profile</title>
-          <meta
-            name='description'
-            content={user.profile?.description ?? `Check out ${user.username}'s public CharmVerse profile`}
-          />
-          <meta
-            property='og:description'
-            content={user.profile?.description ?? `Check out ${user.username}'s public CharmVerse profile`}
-          />
-          <meta property='og:title' content={`${user.username} | Profile`} />
-          {user.avatar && <meta property='og:image' content={user.avatar} />}
-          {user.avatar && <meta property='twitter:image' content={user.avatar} />}
-        </Head>
-        <PublicProfile readOnly={true} user={user} />
-      </>
-    );
   }
+
+  return (
+    <>
+      <Head>
+        <title>{publicUser.username} | Profile</title>
+        <meta
+          name='description'
+          content={publicUser.profile?.description ?? `Check out ${publicUser.username}'s public CharmVerse profile`}
+        />
+        <meta
+          property='og:description'
+          content={publicUser.profile?.description ?? `Check out ${publicUser.username}'s public CharmVerse profile`}
+        />
+        <meta property='og:title' content={`${publicUser.username} | Profile`} />
+        {publicUser.avatar && <meta property='og:image' content={publicUser.avatar} />}
+        {publicUser.avatar && <meta property='twitter:image' content={publicUser.avatar} />}
+      </Head>
+      <PublicProfile readOnly={true} user={user ?? publicUser} />
+    </>
+  );
 }
 
 UserProfilePage.getLayout = (page: ReactElement) => {
-  return <PageLayout>{page}</PageLayout>;
+  return <CenteredPageContent>{page}</CenteredPageContent>;
 };
