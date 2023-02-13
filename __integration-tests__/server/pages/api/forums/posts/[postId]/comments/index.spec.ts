85d1866f373afabb6f64363f40edffa7046a2b61
Safwan Shaheer <devorein00@gmail.com>
2023-02-13 23:13:20 +0600

    Fixed broken forum post comment integration tests

--- __integration-tests__/server/pages/api/forums/posts/[postId]/comments/index.spec.ts
+++ __integration-tests__/server/pages/api/forums/posts/[postId]/comments/index.spec.ts
@@ -3,6 +3,7 @@ import type { PostCategory, Space, User } from '@prisma/client';
 import request from 'supertest';
 import { v4 } from 'uuid';
 
+import { prisma } from 'db';
 import { upsertPostCategoryPermission } from 'lib/permissions/forum/upsertPostCategoryPermission';
 import { baseUrl, loginUser } from 'testing/mockApiCall';
 import { generateUserAndSpace } from 'testing/setupDatabase';
@@ -10,18 +11,30 @@ import { generateForumPost, generatePostCategory } from 'testing/utils/forums';
 
 let space: Space;
 let user: User;
+let otherUser: User;
 let userCookie: string;
+let otherUserCookie: string;
 
 let accessiblePostCategory: PostCategory;
 let disallowedPostCategory: PostCategory;
 beforeAll(async () => {
   const { space: _space, user: _user } = await generateUserAndSpace({ isAdmin: false });
-  const { space: _space2, user: _user2 } = await generateUserAndSpace({ isAdmin: false });
+  const { user: _user2 } = await generateUserAndSpace({ isAdmin: false });
+
+  await prisma.spaceRole.create({
+    data: {
+      spaceId: _space.id,
+      userId: _user2.id
+    }
+  });
 
   space = _space;
   user = _user;
 
+  otherUser = _user2;
+
   userCookie = await loginUser(user.id);
+  otherUserCookie = await loginUser(otherUser.id);
 
   accessiblePostCategory = await generatePostCategory({
     spaceId: space.id
@@ -60,7 +73,7 @@ describe('GET /api/forums/posts/[postId]/comments - Get comments of a post', () 
       categoryId: disallowedPostCategory.id
     });
 
-    await request(baseUrl).get(`/api/forums/posts/${post.id}/comments`).set('Cookie', userCookie).expect(401);
+    await request(baseUrl).get(`/api/forums/posts/${post.id}/comments`).set('Cookie', otherUserCookie).expect(401);
   });
 });
 describe('POST /api/forums/posts/[postId]/comments - Create a comment', () => {
