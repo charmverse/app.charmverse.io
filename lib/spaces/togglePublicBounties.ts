4a7f30434dc7bb289f312dd5175ace66ce46ae39
Matt Casey <mattwad@gmail.com>
2023-02-15 09:53:34 -0700

    fix toggle for public bounties

--- lib/spaces/togglePublicBounties.ts
+++ lib/spaces/togglePublicBounties.ts
@@ -4,22 +4,12 @@ import { v4, validate } from 'uuid';
 import { prisma } from 'db';
 import type { PageNodeWithChildren, PageNodeWithPermissions } from 'lib/pages';
 import { multiResolvePageTree } from 'lib/pages/server/resolvePageTree';
-import { hasSameOrMorePermissions, IPagePermissionWithSource } from 'lib/permissions/pages';
+import { hasSameOrMorePermissions } from 'lib/permissions/pages';
 
-import { DataNotFoundError, InvalidInputError } from '../utilities/errors';
+import { InvalidInputError } from '../utilities/errors';
 
 import type { PublicBountyToggle } from './interfaces';
 
-async function generatePublicBountyPermissionArgs({
-  publicBountyBoard,
-  spaceId
-}: PublicBountyToggle<false>): Promise<[Prisma.PagePermissionDeleteManyArgs | null]>;
-async function generatePublicBountyPermissionArgs({
-  publicBountyBoard,
-  spaceId
-}: PublicBountyToggle<true>): Promise<
-  [Prisma.PagePermissionDeleteManyArgs | null, Prisma.PagePermissionCreateManyArgs, Prisma.PagePermissionCreateManyArgs]
->;
 async function generatePublicBountyPermissionArgs({
   publicBountyBoard,
   spaceId
@@ -150,41 +140,33 @@ export async function togglePublicBounties({ spaceId, publicBountyBoard }: Publi
     throw new InvalidInputError('Please provide a valid space ID.');
   }
 
-  try {
-    const spaceAfterUpdate = await prisma.$transaction(async (tx) => {
-      const updatedSpace = await tx.space.update({
-        where: { id: spaceId },
-        data: {
-          publicBountyBoard
-        }
-      });
+  const [deleteArgs, createArgs, childCreateArgs] = await generatePublicBountyPermissionArgs({
+    publicBountyBoard,
+    spaceId
+  });
 
-      if (publicBountyBoard === true) {
-        const [deleteArgs, createArgs, childCreateArgs] = await generatePublicBountyPermissionArgs({
-          publicBountyBoard,
-          spaceId
-        });
-
-        if (deleteArgs) {
-          await tx.pagePermission.deleteMany(deleteArgs);
-        }
-
-        await tx.pagePermission.createMany(createArgs);
-
-        await tx.pagePermission.createMany(childCreateArgs);
-      } else {
-        const [deleteArgs] = await generatePublicBountyPermissionArgs({ publicBountyBoard, spaceId });
-
-        if (deleteArgs) {
-          await tx.pagePermission.deleteMany(deleteArgs);
-        }
+  const spaceAfterUpdate = await prisma.$transaction(async (tx) => {
+    const updatedSpace = await tx.space.update({
+      where: { id: spaceId },
+      data: {
+        publicBountyBoard
       }
-
-      return updatedSpace;
     });
 
-    return spaceAfterUpdate;
-  } catch (err) {
-    throw new DataNotFoundError(`Space ${spaceId} not found.`);
-  }
+    if (publicBountyBoard === true) {
+      if (deleteArgs) {
+        await tx.pagePermission.deleteMany(deleteArgs);
+      }
+
+      await tx.pagePermission.createMany(createArgs);
+
+      await tx.pagePermission.createMany(childCreateArgs);
+    } else if (deleteArgs) {
+      await tx.pagePermission.deleteMany(deleteArgs);
+    }
+
+    return updatedSpace;
+  });
+
+  return spaceAfterUpdate;
 }
