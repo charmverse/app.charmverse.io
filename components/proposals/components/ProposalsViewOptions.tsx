6547b6ca9ef38a15a92d6a2b4c5ddff76dcaece0
Mohamad El Boudi <mo@anyahq.co>
2023-02-23 22:49:52 +0200

    Add UI for new categories

--- components/proposals/components/ProposalsViewOptions.tsx
+++ components/proposals/components/ProposalsViewOptions.tsx
@@ -1,11 +1,22 @@
-import { Box, Chip, InputLabel, MenuItem, Select } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
+import { Box, Chip, InputLabel, MenuItem, Select, TextField } from '@mui/material';
 import type { ProposalStatus } from '@prisma/client';
+import { usePopupState } from 'material-ui-popup-state/hooks';
+import { useState } from 'react';
 
+import Button from 'components/common/Button';
+import Modal from 'components/common/Modal';
 import { ViewOptions } from 'components/common/ViewOptions';
+import { useCurrentSpace } from 'hooks/useCurrentSpace';
+import { useIsAdmin } from 'hooks/useIsAdmin';
+import { useSnackbar } from 'hooks/useSnackbar';
 import type { ProposalCategoryWithPermissions } from 'lib/permissions/proposals/interfaces';
 import type { ProposalCategory } from 'lib/proposal/interface';
 import { PROPOSAL_STATUS_LABELS } from 'lib/proposal/proposalStatusTransition';
 import type { BrandColor } from 'theme/colors';
+import { getRandomThemeColor } from 'theme/utils/getRandomThemeColor';
+
+import { useProposalCategories } from '../hooks/useProposalCategories';
 
 import { CategoryContextMenu } from './ProposalCategoryContextMenu';
 
@@ -31,6 +42,25 @@ export function ProposalsViewOptions({
   categoryIdFilter,
   setCategoryIdFilter
 }: Props) {
+  const addCategoryPopupState = usePopupState({ variant: 'popover', popupId: 'add-category' });
+
+  const isAdmin = useIsAdmin();
+
+  const { showMessage } = useSnackbar();
+  const [newCategoryName, setNewCategoryName] = useState('');
+  const { addCategory } = useProposalCategories();
+
+  async function createCategory() {
+    addCategory({ title: newCategoryName, color: getRandomThemeColor() })
+      .then(() => {
+        setNewCategoryName('');
+        addCategoryPopupState.close();
+      })
+      .catch((err) => {
+        showMessage(err.message ?? 'Something went wrong', 'error');
+      });
+  }
+
   return (
     <>
       <ViewOptions label='Sort'>
@@ -71,8 +101,64 @@ export function ProposalsViewOptions({
                 <CategoryContextMenu category={category} key={category.id} />
               </MenuItem>
             ))}
+            <MenuItem
+              onClick={(e) => {
+                e.stopPropagation();
+                e.preventDefault();
+              }}
+              value=''
+            >
+              <Button
+                data-test='add-category-button'
+                sx={{
+                  ml: 2
+                }}
+                disabled={!isAdmin}
+                startIcon={<AddIcon />}
+                onClick={addCategoryPopupState.open}
+                variant='outlined'
+                color='secondary'
+                size='small'
+                disabledTooltip="You don't have the permissions to add new forum categories"
+              >
+                Add category
+              </Button>
+            </MenuItem>
           </Select>
         </Box>
+
+        <Modal
+          open={addCategoryPopupState.isOpen}
+          onClose={() => {
+            addCategoryPopupState.close();
+          }}
+          title='Add proposal category'
+        >
+          <TextField
+            data-test='add-category-input'
+            sx={{
+              mb: 1
+            }}
+            autoFocus
+            fullWidth
+            value={newCategoryName}
+            onChange={(e) => {
+              setNewCategoryName(e.target.value);
+            }}
+            onKeyPress={(e) => {
+              if (e.key === 'Enter') {
+                createCategory();
+              }
+            }}
+          />
+          <Button
+            data-test='confirm-new-category-button'
+            disabled={categories.find((category) => category.title === newCategoryName)}
+            onClick={createCategory}
+          >
+            Add
+          </Button>
+        </Modal>
       </ViewOptions>
     </>
   );
