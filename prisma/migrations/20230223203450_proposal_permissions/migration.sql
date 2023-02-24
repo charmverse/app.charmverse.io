e86cddd707e56ed1fc6c3ef7e1119e68d5d36b40
Mohamad El Boudi <mo@anyahq.co>
2023-02-24 14:10:18 +0200

    Restore prisma migration file

--- prisma/migrations/20230223203450_proposal_permissions/migration.sql
+++ prisma/migrations/20230223203450_proposal_permissions/migration.sql
@@ -5,24 +5,19 @@
 
 */
 -- CreateEnum
--- UNDO
--- CREATE TYPE "ProposalCategoryOperation" AS ENUM ('manage_permissions', 'create_proposal', 'edit', 'delete');
+CREATE TYPE "ProposalCategoryOperation" AS ENUM ('manage_permissions', 'create_proposal', 'edit', 'delete');
 
 -- CreateEnum
--- UNDO
--- CREATE TYPE "ProposalOperation" AS ENUM ('edit', 'view', 'delete', 'create_vote', 'vote', 'comment', 'review');
+CREATE TYPE "ProposalOperation" AS ENUM ('edit', 'view', 'delete', 'create_vote', 'vote', 'comment', 'review');
 
 -- CreateEnum
--- UNDO
--- CREATE TYPE  "ProposalCategoryPermissionLevel" AS ENUM ('view', 'view_comment', 'view_comment_vote', 'full_access');
+CREATE TYPE  "ProposalCategoryPermissionLevel" AS ENUM ('view', 'view_comment', 'view_comment_vote', 'full_access');
 
 -- AlterEnum
--- UNDO
--- ALTER TYPE "SpaceOperation" ADD VALUE 'reviewProposals';
+ALTER TYPE "SpaceOperation" ADD VALUE 'reviewProposals';
 
 -- CreateTable
--- UNDO // If not exists
-CREATE TABLE IF NOT EXISTS "ProposalCategoryPermission" (
+CREATE TABLE "ProposalCategoryPermission" (
     "id" UUID NOT NULL,
     "permissionLevel" "ProposalCategoryPermissionLevel" NOT NULL,
     "proposalCategoryId" UUID NOT NULL,
@@ -36,34 +31,34 @@ CREATE TABLE IF NOT EXISTS "ProposalCategoryPermission" (
 );
 
 -- CreateIndex
--- CREATE INDEX "ProposalCategoryPermission_proposalCategoryId_idx" ON "ProposalCategoryPermission"("proposalCategoryId");
+CREATE INDEX "ProposalCategoryPermission_proposalCategoryId_idx" ON "ProposalCategoryPermission"("proposalCategoryId");
 
 -- CreateIndex
--- CREATE INDEX "ProposalCategoryPermission_proposalCategoryId_roleId_idx" ON "ProposalCategoryPermission"("proposalCategoryId", "roleId");
+CREATE INDEX "ProposalCategoryPermission_proposalCategoryId_roleId_idx" ON "ProposalCategoryPermission"("proposalCategoryId", "roleId");
 
 -- CreateIndex
--- CREATE INDEX "ProposalCategoryPermission_proposalCategoryId_spaceId_idx" ON "ProposalCategoryPermission"("proposalCategoryId", "spaceId");
+CREATE INDEX "ProposalCategoryPermission_proposalCategoryId_spaceId_idx" ON "ProposalCategoryPermission"("proposalCategoryId", "spaceId");
 
 -- CreateIndex
--- CREATE INDEX "ProposalCategoryPermission_proposalCategoryId_public_idx" ON "ProposalCategoryPermission"("proposalCategoryId", "public");
+CREATE INDEX "ProposalCategoryPermission_proposalCategoryId_public_idx" ON "ProposalCategoryPermission"("proposalCategoryId", "public");
 
 -- CreateIndex
--- CREATE UNIQUE INDEX "ProposalCategoryPermission_roleId_proposalCategoryId_key" ON "ProposalCategoryPermission"("roleId", "proposalCategoryId");
+CREATE UNIQUE INDEX "ProposalCategoryPermission_roleId_proposalCategoryId_key" ON "ProposalCategoryPermission"("roleId", "proposalCategoryId");
 
 -- CreateIndex
--- CREATE UNIQUE INDEX "ProposalCategoryPermission_spaceId_proposalCategoryId_key" ON "ProposalCategoryPermission"("spaceId", "proposalCategoryId");
+CREATE UNIQUE INDEX "ProposalCategoryPermission_spaceId_proposalCategoryId_key" ON "ProposalCategoryPermission"("spaceId", "proposalCategoryId");
 
 -- CreateIndex
--- CREATE UNIQUE INDEX "ProposalCategoryPermission_public_proposalCategoryId_key" ON "ProposalCategoryPermission"("public", "proposalCategoryId");
+CREATE UNIQUE INDEX "ProposalCategoryPermission_public_proposalCategoryId_key" ON "ProposalCategoryPermission"("public", "proposalCategoryId");
 
 -- CreateIndex
--- CREATE UNIQUE INDEX "ProposalCategory_spaceId_title_key" ON "ProposalCategory"("spaceId", "title");
+CREATE UNIQUE INDEX "ProposalCategory_spaceId_title_key" ON "ProposalCategory"("spaceId", "title");
 
 -- AddForeignKey
--- ALTER TABLE "ProposalCategoryPermission" ADD CONSTRAINT "ProposalCategoryPermission_proposalCategoryId_fkey" FOREIGN KEY ("proposalCategoryId") REFERENCES "ProposalCategory"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "ProposalCategoryPermission" ADD CONSTRAINT "ProposalCategoryPermission_proposalCategoryId_fkey" FOREIGN KEY ("proposalCategoryId") REFERENCES "ProposalCategory"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
--- ALTER TABLE "ProposalCategoryPermission" ADD CONSTRAINT "ProposalCategoryPermission_roleId_fkey" FOREIGN KEY ("roleId") REFERENCES "Role"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "ProposalCategoryPermission" ADD CONSTRAINT "ProposalCategoryPermission_roleId_fkey" FOREIGN KEY ("roleId") REFERENCES "Role"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
--- ALTER TABLE "ProposalCategoryPermission" ADD CONSTRAINT "ProposalCategoryPermission_spaceId_fkey" FOREIGN KEY ("spaceId") REFERENCES "Space"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "ProposalCategoryPermission" ADD CONSTRAINT "ProposalCategoryPermission_spaceId_fkey" FOREIGN KEY ("spaceId") REFERENCES "Space"("id") ON DELETE CASCADE ON UPDATE CASCADE;
