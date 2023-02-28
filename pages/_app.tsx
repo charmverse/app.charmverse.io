315d6f051b79b8398da95f01dad12626c0948fef
Valentin <ludu.valentin@gmail.com>
2023-02-28 19:24:45 +0200

    Fix page title not updating (#1782)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -317,7 +317,7 @@ function PageHead() {
 
   return (
     <Head>
-      <title>{title ? `${prefix}${title} | CharmVerse` : `${prefix}CharmVerse - the all-in-one web3 space'}`}</title>
+      <title>{title ? `${prefix}${title} | CharmVerse` : `${prefix}CharmVerse - the all-in-one web3 space`}</title>
       {/* viewport meta tag goes in _app.tsx - https://nextjs.org/docs/messages/no-document-viewport-meta */}
       <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width' />
       {/* Verification required by google */}
