2bf4eb27d42ca24840ed4f171e95e308916241b7
Safwan Shaheer <devorein00@gmail.com>
2023-02-28 17:13:16 +0600

    Refactored notion import

--- hooks/useNotionImport.tsx
+++ hooks/useNotionImport.tsx
@@ -7,6 +7,7 @@ import CircularProgress from '@mui/material/CircularProgress';
 import Typography from '@mui/material/Typography';
 import { createContext, useContext, useEffect, useMemo, useState } from 'react';
 import { useSWRConfig } from 'swr';
+import useSWRMutation from 'swr/mutation';
 
 import charmClient from 'charmClient';
 import { useAppDispatch } from 'components/common/BoardEditor/focalboard/src/store/hooks';
@@ -23,22 +24,23 @@ interface Props {
   children: JSX.Element;
 }
 
-type INotionImportContext = {
+type NotionImportState = {
   error?: string;
   loading: boolean;
   warning?: string;
   failedImports?: FailedImportsError[];
 };
 
+type INotionImportContext = {
+  loading: boolean;
+};
+
 export const NotionImportContext = createContext<Readonly<INotionImportContext>>({
-  error: undefined,
-  loading: false,
-  warning: undefined,
-  failedImports: []
+  loading: false
 });
 
 export function NotionProvider({ children }: Props) {
-  const [notionState, setNotionState] = useState<INotionImportContext>({ loading: false });
+  const [notionState, setNotionState] = useState<NotionImportState>({ loading: false });
   const { showMessage } = useSnackbar();
   const [modalOpen, setModalOpen] = useState(false);
   const { mutate } = useSWRConfig();
@@ -47,60 +49,53 @@ export function NotionProvider({ children }: Props) {
   const notionCode = getCookie(AUTH_CODE_COOKIE);
   const notionError = getCookie(AUTH_ERROR_COOKIE);
 
+  const { trigger } = useSWRMutation(
+    '/notion/import',
+    (_, { arg }: Readonly<{ arg: { code: string; spaceId: string } }>) => charmClient.importFromNotion(arg),
+    {
+      onSuccess(data) {
+        if (space?.id) {
+          mutate(`pages/${space.id}`);
+          dispatch(initialLoad({ spaceId: space.id }));
+        }
+        setNotionState({ failedImports: data.failedImports, loading: false });
+        if (data.failedImports.length === 0) {
+          showMessage('Notion workspace successfully imported');
+          closeModal();
+        }
+        return data?.failedImports ?? [];
+      },
+      onError(err) {
+        if (err.status === 504) {
+          setNotionState({
+            loading: false,
+            warning:
+              'It can take up to an hour to import large Notion spaces. Your data will appear on the left navigation when the import is completed.'
+          });
+        } else {
+          setNotionState({
+            loading: false,
+            error: notionError || err.message || err.error || 'Something went wrong. Please try again'
+          });
+        }
+      }
+    }
+  );
+
   function closeModal() {
     setModalOpen(false);
   }
 
   useEffect(() => {
-    if (space && notionCode && !notionState.loading) {
-      setNotionState({ failedImports: [], loading: true });
-      setModalOpen(true);
+    if (space?.id && notionCode) {
       deleteCookie(AUTH_CODE_COOKIE);
-      charmClient
-        .importFromNotion({
-          code: notionCode,
-          spaceId: space.id
-        })
-        .then(({ failedImports }) => {
-          setNotionState({ failedImports, loading: false });
-          mutate(`pages/${space.id}`);
-          // Fetch all the focalboard blocks,
-          // TODO: Refactor to only return the imported blocks
-          dispatch(initialLoad({ spaceId: space.id }));
-          if (failedImports.length === 0) {
-            showMessage('Notion workspace successfully imported');
-            closeModal();
-          }
-        })
-        .catch((err) => {
-          if (err.status === 504) {
-            setNotionState({
-              loading: false,
-              warning:
-                'It can take up to an hour to import large Notion spaces. Your data will appear on the left navigation when the import is completed.'
-            });
-          } else {
-            setNotionState({
-              loading: false,
-              error: err.message || err.error || 'Something went wrong. Please try again'
-            });
-          }
-        });
-    }
-  }, [space]);
-
-  useEffect(() => {
-    if (notionError) {
-      deleteCookie(AUTH_ERROR_COOKIE);
       setModalOpen(true);
-      setNotionState({
-        loading: false,
-        error: notionError
-      });
+      setNotionState({ loading: true });
+      trigger({ code: notionCode, spaceId: space.id });
     }
-  }, []);
+  }, [space?.id, notionCode]);
 
-  const value = useMemo<INotionImportContext>(() => notionState, [notionState]);
+  const value = useMemo<INotionImportContext>(() => ({ loading: notionState.loading }), [notionState.loading]);
 
   return (
     <NotionImportContext.Provider value={value}>
