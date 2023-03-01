751587ef3b5bc2aa146ee089a5b3a3f3410804ac
motechFR <mo@anyahq.co>
2023-03-01 15:57:36 -0700

    Hide features (#1787)
    * Hide features in the sidebar
    
    * Add e2e test for hiding features in the sidebar
    
    * Rename to 'hidden features'
    
    * Cleanup

---
+++ lib/spaces/__tests__/setHiddenFeatures.spec.ts
@@ -0,0 +1,38 @@
+import { prisma } from 'db';
+import { generateUserAndSpace } from 'testing/setupDatabase';
+
+import { setHiddenFeatures } from '../setHiddenFeatures';
+
+describe('setHiddenFeatures', () => {
+  it('should update the list of blocked features in the space', async () => {
+    const { space } = await generateUserAndSpace();
+
+    await setHiddenFeatures({
+      spaceId: space.id,
+      hiddenFeatures: ['bounties']
+    });
+
+    const afterUpdate = await prisma.space.findUnique({
+      where: {
+        id: space.id
+      }
+    });
+
+    expect(afterUpdate?.hiddenFeatures.length).toBe(1);
+    expect(afterUpdate?.hiddenFeatures[0]).toBe('bounties');
+    // Perform second setter to make sure this is deterministic
+    await setHiddenFeatures({
+      spaceId: space.id,
+      hiddenFeatures: ['forum']
+    });
+
+    const afterSecondUpdate = await prisma.space.findUnique({
+      where: {
+        id: space.id
+      }
+    });
+
+    expect(afterSecondUpdate?.hiddenFeatures.length).toBe(1);
+    expect(afterSecondUpdate?.hiddenFeatures[0]).toBe('forum');
+  });
+});
