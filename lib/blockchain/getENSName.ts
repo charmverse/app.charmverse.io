0df7344b63577b81fbe6d3ecff1e0c07b81120a0
Valentin <ludu.valentin@gmail.com>
2023-03-01 16:25:50 +0200

    Get ENS details (#1768)
    * Get ENS details
    
    * Fix test
    
    * Fix failed uploading because of image format and cleanup
    
    * Improve getting the ens details

--- lib/blockchain/getENSName.ts
+++ lib/blockchain/getENSName.ts
@@ -19,3 +19,34 @@ export function getENSName(address: string) {
     return null;
   });
 }
+
+export async function getENSDetails(ensName?: string | null) {
+  if (!ensName) {
+    return null;
+  }
+
+  if (!provider) {
+    if (!isTestEnv) {
+      log.warn('No api key provided for Alchemy');
+    }
+    return null;
+  }
+
+  if (!ethers.utils.isValidName(ensName)) {
+    log.warn(`The ens name ${ensName} you provided is not valid`);
+    return null;
+  }
+
+  try {
+    const resolver = await provider.getResolver(ensName);
+    const avatar = await provider.getAvatar(ensName);
+    const [description, discord, github, twitter] = await Promise.all(
+      ['description', 'com.discord', 'com.github', 'com.twitter'].map((text) => resolver?.getText(text))
+    );
+
+    return { avatar, description, discord, github, twitter };
+  } catch (error) {
+    log.warn(`Error looking up ENS details for ens name ${ensName}`, { error });
+    return null;
+  }
+}
