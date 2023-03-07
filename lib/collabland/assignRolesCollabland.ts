57479a85cef92099ea2feff0e369c28f039477cc
Marek <m.sadura@outlook.com>
2023-03-07 11:53:34 +0100

    Updated flow of adding collabland roles

--- lib/collabland/assignRolesCollabland.ts
+++ lib/collabland/assignRolesCollabland.ts
@@ -1,7 +1,6 @@
-import { getGuildRoles } from 'lib/collabland/collablandClient';
+import { prisma } from 'db';
+import { findOrCreateCollablandRoles } from 'lib/collabland/findOrCreateCollablandRoles';
 import { getSpacesAndUserFromDiscord } from 'lib/discord/getSpaceAndUserFromDiscord';
-import type { ExternalRole } from 'lib/roles';
-import { createAndAssignRoles } from 'lib/roles/createAndAssignRoles';
 
 export async function assignRolesCollabland({
   discordUserId,
@@ -13,20 +12,85 @@ export async function assignRolesCollabland({
   roles: string[] | string;
 }) {
   const roleIdsToAdd = Array.isArray(roles) ? roles : [roles];
+
   try {
-    const discordRoles = await getGuildRoles(discordServerId);
-    const rolesToAdd = roleIdsToAdd
-      .map((roleId) => discordRoles.find((role) => role.id === roleId))
-      .filter(Boolean) as ExternalRole[];
-
     const spacesData = await getSpacesAndUserFromDiscord({ discordUserId, discordServerId });
-
     return Promise.allSettled(
       spacesData.map(({ space, user }) =>
-        createAndAssignRoles({ userId: user.id, spaceId: space.id, roles: rolesToAdd })
+        createAndAssignCollablanRoles({ userId: user.id, spaceId: space.id, roles: roleIdsToAdd, discordServerId })
       )
     );
   } catch (e) {
     return null;
   }
+}
+
+async function createAndAssignCollablanRoles({
+  userId,
+  spaceId,
+  roles,
+  discordServerId
+}: {
+  userId: string;
+  spaceId: string;
+  roles: string[];
+  discordServerId: string;
+}) {
+  if (!roles.length) {
+    return;
+  }
+
+  const spaceMembership = await prisma.spaceRole.findFirst({
+    where: {
+      spaceId,
+      userId
+    }
+  });
+
+  if (!spaceMembership) {
+    return;
+  }
+
+  const rolesRecord = await findOrCreateCollablandRoles({
+    externalRoleIds: roles,
+    spaceId,
+    userId,
+    discordServerId
+  });
+
+  const roleIdsToAssign: string[] = [];
+  roles.forEach((roleId) => {
+    const role = rolesRecord[roleId];
+    if (role) {
+      roleIdsToAssign.push(role.id);
+    }
+  });
+
+  await prisma.$transaction(
+    roleIdsToAssign.map((roleId) => {
+      // assign roles to user
+      return prisma.spaceRoleToRole.upsert({
+        where: {
+          spaceRoleId_roleId: {
+            spaceRoleId: spaceMembership.id,
+            roleId
+          }
+        },
+        create: {
+          role: {
+            connect: {
+              id: roleId
+            }
+          },
+          spaceRole: {
+            connect: {
+              id: spaceMembership.id
+            }
+          }
+        },
+        // Perform an empty update if user already has the role
+        update: {}
+      });
+    })
+  );
 }
---
+++ lib/collabland/findOrCreateCollablandRoles.ts
@@ -0,0 +1,75 @@
+import type { Role } from '@prisma/client';
+import { RoleSource } from '@prisma/client';
+
+import { prisma } from 'db';
+import { getGuildRoles } from 'lib/collabland/collablandClient';
+import type { ExternalRole } from 'lib/roles/interfaces';
+
+type RolesRecord = Record<string, Role | null>;
+
+// Create charmverse roles or find them from prisma to generate a final record
+export async function findOrCreateCollablandRoles({
+  externalRoleIds,
+  spaceId,
+  userId,
+  discordServerId
+}: {
+  externalRoleIds: string[];
+  spaceId: string;
+  userId: string;
+  discordServerId: string;
+}): Promise<RolesRecord> {
+  const rolesRecord: RolesRecord = {};
+  let discordRoles: ExternalRole[] | null = await getGuildRoles(discordServerId);
+
+  for (const externalRoleId of externalRoleIds) {
+    // First check if discord role already exists in db
+    const existingRole = await prisma.role.findFirst({
+      where: {
+        OR: [
+          {
+            externalId: String(externalRoleId),
+            spaceId
+          },
+          {
+            sourceId: String(externalRoleId),
+            spaceId
+          }
+        ]
+      }
+    });
+
+    if (existingRole) {
+      rolesRecord[externalRoleId] = existingRole;
+    } else {
+      // fetch discord roles only once and only when needed
+      if (!discordRoles) {
+        discordRoles = await getGuildRoles(discordServerId);
+      }
+
+      const externalRole = discordRoles?.find((role) => role.id === externalRoleId) ?? null;
+      if (externalRole) {
+        const newRoleData = {
+          name: externalRole.name,
+          space: {
+            connect: {
+              id: spaceId
+            }
+          },
+          createdBy: userId,
+          source: RoleSource.collabland,
+          sourceId: String(externalRole.id),
+          externalId: String(externalRole.id)
+        };
+
+        const createdRole = await prisma.role.create({
+          data: newRoleData
+        });
+
+        rolesRecord[externalRole.id] = createdRole;
+      }
+    }
+  }
+
+  return rolesRecord;
+}
