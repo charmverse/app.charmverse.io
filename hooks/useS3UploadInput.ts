9ca9eb14706cafe7ec4a3186382913b97adacaf7
Marek <m.sadura@outlook.com>
2023-03-09 22:03:31 +0100

    max file size 20mb

--- hooks/useS3UploadInput.ts
+++ hooks/useS3UploadInput.ts
@@ -4,6 +4,8 @@ import { useFilePicker } from 'hooks/useFilePicker';
 import { useSnackbar } from 'hooks/useSnackbar';
 import { uploadToS3 } from 'lib/aws/uploadToS3Browser';
 
+const DEFAULT_MAX_FILE_SIZE_MB = 20;
+
 export const useS3UploadInput = (onFileUpload: (url: string) => void) => {
   const { showMessage } = useSnackbar();
   const [isUploading, setIsUploading] = useState(false);
@@ -11,6 +13,11 @@ export const useS3UploadInput = (onFileUpload: (url: string) => void) => {
   const [fileName, setFileName] = useState('');
 
   async function uploadFile(file: File) {
+    if (file.size > DEFAULT_MAX_FILE_SIZE_MB * 1024 ** 2) {
+      showMessage(`File size must be less than ${DEFAULT_MAX_FILE_SIZE_MB}MB`, 'error');
+      return;
+    }
+
     setIsUploading(true);
     setProgress(0);
     setFileName(file.name || '');
