4ea27199b87975777e91422bffed4c5eda10b5f5
Valentin L <ludu.valentin@gmail.com>
2023-03-16 11:51:13 +0200

    E2e for bounty comments

--- __e2e__/bounties/createEditBounty.spec.ts
+++ __e2e__/bounties/createEditBounty.spec.ts
@@ -7,7 +7,9 @@ import { createUser, createUserAndSpace, generateSpaceRole } from '__e2e__/utils
 import { login } from '__e2e__/utils/session';
 
 import { prisma } from 'db';
+import { createApplication } from 'lib/applications/actions';
 import { randomETHWalletAddress } from 'testing/generateStubs';
+import { generateSpaceUser } from 'testing/setupDatabase';
 
 type Fixtures = {
   bountyBoardPage: BountyBoardPage;
@@ -16,7 +18,7 @@ type Fixtures = {
 };
 let space: Space;
 let adminUser: User;
-let bounty: Bounty & { page: { path: string } };
+let bounty: Bounty & { page: { path?: string; comments?: { id?: string }[] } | null };
 
 // This will be used in the test to update the bounty and check the displayed value
 const newBountyAmount = '123';
@@ -46,7 +48,7 @@ test.describe.serial('Create and Edit Bounty', () => {
     await page.waitForTimeout(1000);
 
     // There should be only 1 bounty in the space
-    bounty = (await prisma.bounty.findFirstOrThrow({
+    bounty = await prisma.bounty.findFirstOrThrow({
       where: {
         spaceId: space.id
       },
@@ -57,7 +59,7 @@ test.describe.serial('Create and Edit Bounty', () => {
           }
         }
       }
-    })) as Bounty & { page: { path: string } };
+    });
 
     await expect(bountyPage.bountyPropertiesConfiguration).toBeVisible();
     await expect(bountyPage.bountyPropertyAmount).toBeVisible();
@@ -74,7 +76,7 @@ test.describe.serial('Create and Edit Bounty', () => {
 
     await bountyPage.waitForDocumentPage({
       domain: space.domain,
-      path: `${bounty.page.path}`
+      path: `${bounty.page?.path}`
     });
 
     await expect(bountyPage.bountyHeaderAmount).toBeVisible();
@@ -115,5 +117,109 @@ test.describe.serial('Create and Edit Bounty', () => {
     await expect(bountyPage.bountyHeaderAmount).toBeVisible();
     await expect(bountyPage.bountyHeaderAmount).toHaveText(newBountyAmount);
     await expect(bountyPage.bountyApplicantForm).toBeVisible();
+  });
+
+  test('send a message to the bounty applicant', async ({ bountyBoardPage, page, bountyPage }) => {
+    const generated = await createUserAndSpace({
+      browserPage: page
+    });
+
+    space = generated.space;
+    adminUser = generated.user;
+    const extraUser = await generateSpaceUser({ spaceId: space.id, isAdmin: false });
+
+    await login({ page, userId: adminUser.id });
+
+    await bountyBoardPage.goToBountyBoard(space.domain);
+
+    await expect(bountyBoardPage.createBountyButton).toBeVisible();
+
+    await bountyBoardPage.createBountyButton.click();
+
+    // Give time for bounty to create
+    await page.waitForTimeout(1000);
+
+    // There should be only 1 bounty in the space
+    bounty = await prisma.bounty.findFirstOrThrow({
+      where: {
+        spaceId: space.id
+      },
+      include: {
+        page: {
+          select: {
+            path: true
+          }
+        }
+      }
+    });
+
+    // Another user is applying to the bounty
+    await createApplication({
+      bountyId: bounty.id,
+      message: 'My application message',
+      userId: extraUser.id
+    });
+
+    // Give time for application to be created
+    await page.waitForTimeout(1000);
+
+    // Review button appears and the reviewer is clicking it
+    await expect(bountyPage.bountyReviewButton).toBeVisible();
+    await bountyPage.bountyReviewButton.click();
+
+    // The comment area is empty and the comment button is disabled
+    expect((await bountyPage.bountyCommentArea.innerText()).valueOf().trim()).toBe('');
+    await expect(bountyPage.bountyCommentButton).toBeDisabled();
+
+    // If the user types and removes what he typed, the comment button should be disabled
+    await bountyPage.bountyCommentArea.click();
+    await page.keyboard.type('New', { delay: 100 });
+    await page.keyboard.press('Backspace');
+    await page.keyboard.press('Backspace');
+    await page.keyboard.press('Backspace');
+    await expect(bountyPage.bountyCommentButton).toBeDisabled();
+
+    // Comment is saved and is visible
+    await expect(bountyPage.bountyCommentArea).toBeVisible();
+    await bountyPage.bountyCommentArea.click();
+    await page.keyboard.type('My new comment', { delay: 100 });
+    await bountyPage.bountyCommentButton.click();
+    await expect(page.getByText('My new comment')).toBeVisible();
+
+    // Give time for coment to be created
+    await page.waitForTimeout(1000);
+
+    bounty = await prisma.bounty.findFirstOrThrow({
+      where: {
+        id: bounty.id
+      },
+      include: {
+        page: {
+          select: {
+            path: true,
+            comments: {
+              select: {
+                id: true
+              }
+            }
+          }
+        }
+      }
+    });
+
+    const firstCommentId = bounty.page?.comments?.[0]?.id || '';
+
+    // Edit a comment
+    await bountyPage.getCommentMenuIcon(firstCommentId).hover();
+    expect(bountyPage.getCommentMenuIcon(firstCommentId)).toBeVisible();
+    await bountyPage.getCommentMenuIcon(firstCommentId).click();
+    await bountyPage.getEditCommentButton(firstCommentId).click();
+    await bountyPage.getEditCommentArea(firstCommentId).click();
+    await page.keyboard.press('Meta+A');
+    await page.keyboard.press('Backspace');
+    await expect(bountyPage.getSaveCommentButton(firstCommentId)).toBeDisabled();
+    await page.keyboard.type('Second comment', { delay: 100 });
+    await bountyPage.getSaveCommentButton(firstCommentId).click();
+    await expect(page.getByText('Second comment')).toBeVisible();
   });
 });
