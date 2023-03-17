4e76f207ab7d4d36f56814cf51cb5a5ad9eb29fe
Safwan Shaheer <devorein00@gmail.com>
2023-03-17 18:53:17 +0600

    Member email form with email settings

--- components/members/MemberEmailForm.tsx
+++ components/members/MemberEmailForm.tsx
@@ -1,23 +1,41 @@
 import { yupResolver } from '@hookform/resolvers/yup';
-import { TextField, Typography } from '@mui/material';
+import { Checkbox, FormControlLabel, FormGroup, TextField, Typography } from '@mui/material';
 import { Stack } from '@mui/system';
+import type { User } from '@prisma/client';
+import type { ChangeEvent } from 'react';
 import { useCallback } from 'react';
 import { useForm } from 'react-hook-form';
+import useSWRMutation from 'swr/mutation';
 import * as yup from 'yup';
 
 import charmClient from 'charmClient';
 import Button from 'components/common/Button';
 import { useUser } from 'hooks/useUser';
-import debounce from 'lib/utilities/debounce';
 
 export const schema = yup.object({
-  email: yup.string().ensure().trim().email()
+  email: yup.string().ensure().trim().email(),
+  emailNotifications: yup.boolean(),
+  emailNewsletter: yup.boolean()
 });
 
 export type FormValues = yup.InferType<typeof schema>;
 
-export function MemberEmailForm({ onClick, readOnly = false }: { onClick: VoidFunction; readOnly?: boolean }) {
-  const { user, setUser } = useUser();
+export function MemberEmailForm({ onClick }: { onClick: VoidFunction }) {
+  const { updateUser, user } = useUser();
+
+  const { isMutating } = useSWRMutation(
+    '/api/profile',
+    (_url, { arg }: Readonly<{ arg: Partial<User> }>) => charmClient.updateUser(arg),
+    {
+      onSuccess: (data) => {
+        updateUser({
+          emailNewsletter: data.emailNewsletter,
+          emailNotifications: data.emailNotifications,
+          email: data.email
+        });
+      }
+    }
+  );
 
   const {
     register,
@@ -27,27 +45,36 @@ export function MemberEmailForm({ onClick, readOnly = false }: { onClick: VoidFu
     formState: { errors }
   } = useForm<FormValues>({
     defaultValues: {
-      email: user?.email || ''
+      email: user?.email || '',
+      emailNewsletter: user?.emailNewsletter ?? false,
+      emailNotifications: user?.emailNotifications ?? false
     },
     mode: 'onChange',
     resolver: yupResolver(schema)
   });
 
-  const onSave = useCallback(
-    debounce(async (emailValue: string) => {
-      if (!readOnly) {
-        const validate = await trigger();
+  const email = getValues('email');
+  const emailNewsletter = getValues('emailNewsletter');
+  const emailNotifications = getValues('emailNotifications');
 
-        if (validate) {
-          await charmClient.updateUser({
-            email: emailValue
-          });
-          setUser({ ...user, email: emailValue });
-        }
-      }
-    }, 300),
-    [readOnly]
-  );
+  const onChange = useCallback(async (event: ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.type === 'checkbox' ? event.target.checked : event.target.value;
+    setValue(event.target.name as keyof FormValues, value);
+    await trigger();
+  }, []);
+
+  const onSave = async () => {
+    const validate = await trigger();
+
+    if (validate) {
+      await charmClient.updateUser({
+        email,
+        emailNewsletter,
+        emailNotifications
+      });
+      updateUser({ ...user, email, emailNewsletter, emailNotifications });
+    }
+  };
 
   return (
     <Stack gap={1}>
@@ -57,25 +84,34 @@ export function MemberEmailForm({ onClick, readOnly = false }: { onClick: VoidFu
       </Typography>
       <TextField
         {...register('email')}
-        disabled={readOnly}
         data-test='member-email-input'
         fullWidth
         error={!!errors.email}
         helperText={errors.email?.message}
         placeholder='me@gmail.com'
-        onChange={async (event) => {
-          const val = event.target.value;
-          setValue('email', val);
-        }}
+        onChange={onChange}
       />
+      <FormGroup>
+        <FormControlLabel
+          disabled={!!errors.email || email.length === 0 || isMutating}
+          control={<Checkbox {...register('emailNotifications')} checked={emailNotifications} onChange={onChange} />}
+          label='Receive email updates on mentions, comments, post and other things in CharmVerse.'
+        />
+        <FormControlLabel
+          disabled={!!errors.email || email.length === 0 || isMutating}
+          control={<Checkbox {...register('emailNewsletter')} checked={emailNewsletter} onChange={onChange} />}
+          label='Keep me up to date on whats new with CharmVerse'
+        />
+      </FormGroup>
       <Stack flexDirection='row' gap={1} justifyContent='flex-end'>
         <Button onClick={onClick} variant='outlined' color='secondary'>
           Skip
         </Button>
         <Button
           data-test='member-email-next'
+          disabled={Object.keys(errors).length !== 0}
           onClick={() => {
-            onSave(getValues('email'));
+            onSave();
             onClick();
           }}
         >
