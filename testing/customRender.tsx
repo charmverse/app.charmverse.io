1de8ca58865937fecf631d56dd8af506238a5478
Valentin L <ludu.valentin@gmail.com>
2023-03-23 12:14:48 +0200

    Code review improvements

--- testing/customRender.tsx
+++ testing/customRender.tsx
@@ -6,16 +6,16 @@ import type { IContext } from 'hooks/useUser';
 import { UserContext } from 'hooks/useUser';
 import { createThemeLightSensitive } from 'theme';
 
-import { mockedUser } from './mocks/user';
-
-const theme = createThemeLightSensitive('light');
+import { createMockUser } from './mocks/user';
 
 export const customRenderWithContext = (ui: ReactNode, { value, ...renderOptions }: { value?: Partial<IContext> }) => {
+  const theme = createThemeLightSensitive('light');
+
   return render(
     <ThemeProvider theme={theme}>
       <UserContext.Provider
         value={{
-          user: mockedUser,
+          user: createMockUser(),
           setUser: () => {},
           updateUser: () => {},
           setIsLoaded: () => {},
--- testing/mocks/user.ts
+++ testing/mocks/user.ts
@@ -2,33 +2,36 @@ import { v4 as uuid } from 'uuid';
 
 import type { LoggedInUser } from 'models';
 
-export const mockedUser: LoggedInUser = {
-  id: uuid(),
-  avatar: null,
-  avatarChain: null,
-  avatarContract: null,
-  avatarTokenId: null,
-  createdAt: new Date(),
-  updatedAt: new Date(),
-  deletedAt: null,
-  isBot: false,
-  email: 'user@charmverse.io',
-  emailNewsletter: false,
-  emailNotifications: false,
-  identityType: 'Wallet',
-  username: uuid(),
-  path: uuid(),
-  spacesOrder: [],
-  xpsEngineId: null,
-  wallets: [
-    {
-      ensname: null,
-      address: '0x0000000000000000000000000000000000000000'
-    }
-  ],
-  spaceRoles: [],
-  unstoppableDomains: [],
-  favorites: [],
-  googleAccounts: [],
-  verifiedEmails: []
-};
+export function createMockUser(user?: Partial<LoggedInUser>): LoggedInUser {
+  return {
+    id: uuid(),
+    avatar: null,
+    avatarChain: null,
+    avatarContract: null,
+    avatarTokenId: null,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    deletedAt: null,
+    isBot: false,
+    email: 'user@charmverse.io',
+    emailNewsletter: false,
+    emailNotifications: false,
+    identityType: 'Wallet',
+    username: uuid(),
+    path: uuid(),
+    spacesOrder: [],
+    xpsEngineId: null,
+    wallets: [
+      {
+        ensname: null,
+        address: '0x0000000000000000000000000000000000000000'
+      }
+    ],
+    spaceRoles: [],
+    unstoppableDomains: [],
+    favorites: [],
+    googleAccounts: [],
+    verifiedEmails: [],
+    ...user
+  };
+}
