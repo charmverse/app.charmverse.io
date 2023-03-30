c9c9ba178673fdfd11316b7daf4650118a2151cf
Safwan Shaheer <devorein00@gmail.com>
2023-03-30 22:43:20 +0600

    Updated migration script

--- scripts/migrations/2023_03_30_add_default_path.ts
+++ scripts/migrations/2023_03_30_add_default_path.ts
@@ -2,25 +2,24 @@ import { prisma } from 'db';
 import { uid } from 'lib/utilities/strings';
 
 async function init() {
-
   const users = await prisma.user.findMany({
     select: {
       id: true,
-      path: true
+    },
+    where: {
+      path: null
     }
   })
 
   for (const user of users) {
-    if (!user.path) {
-      await prisma.user.update({
-        where: {
-          id: user.id
-        },
-        data: {
-          path: uid()
-        }
-      })
-    }
+    await prisma.user.update({
+      where: {
+        id: user.id
+      },
+      data: {
+        path: uid()
+      }
+    })
   }
 }
 
