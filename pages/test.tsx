66482026ce6c9975a825308a443bcb15f0a56bcd
Mohamad El Boudi <mo@anyahq.co>
2023-04-04 17:09:48 +0200

    Setup test workflow

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,13 +1,21 @@
 // import { uid } from 'node_modules/common.charmverse.io/packages/utilities-string/src';
 // eslint-disable-next-line import/no-relative-packages, import/no-extraneous-dependencies
-import { randomETHWalletAddress, matchWalletAddress } from 'charmverse-utilities';
+import { randomiser } from 'common.charmverse.io';
+import { useEffect, useState } from 'react';
 
 export default function TestPage() {
+  const [displayValue, setDisplayValue] = useState('0');
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDisplayValue(randomiser());
+    }, 2000);
+
+    return () => clearInterval(interval);
+  }, []);
   return (
     <div>
-      <h1>
-        Content {randomETHWalletAddress()} -- {String(matchWalletAddress(randomETHWalletAddress(), '0xb'))}
-      </h1>
+      <h1>Content {displayValue}</h1>
     </div>
   );
 }
