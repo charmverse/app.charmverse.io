7f3356221eb9af48195162fe9f414450bdde2092
Mohamad El Boudi <mo@anyahq.co>
2023-04-05 11:54:54 +0200

    Delete prisma from webapp

--- prisma/migrations/20220510125012_add_suggestion_status/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterEnum
-ALTER TYPE "BountyStatus" ADD VALUE 'suggestion';
--- prisma/migrations/20221221152601_page_comment_user/migration.sql
+++
@@ -1,2 +0,0 @@
--- AddForeignKey
-ALTER TABLE "PageComment" ADD CONSTRAINT "PageComment_createdBy_fkey" FOREIGN KEY ("createdBy") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
