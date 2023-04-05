9ae239074f3fedc380381b2d403ff5bea524f64c
Mohamad El Boudi <mo@anyahq.co>
2023-04-05 15:13:00 +0200

    Source prisma schema from node_modules

--- scripts/configure-db.sh
+++ scripts/configure-db.sh
@@ -2,5 +2,5 @@
 # This script is used to configure the database for the tests and in our CI
 PGPASSWORD=postgres psql -h localhost -U postgres -c "DROP DATABASE charmversetest;"
 PGPASSWORD=postgres psql -h localhost -U postgres -c "CREATE DATABASE charmversetest;"
-DATABASE_URL=postgres://postgres:postgres@127.0.0.1:5432/charmversetest npx prisma migrate dev
+DATABASE_URL=postgres://postgres:postgres@127.0.0.1:5432/charmversetest npx prisma migrate dev --schema=node_modules/@charmverse/core/src/prisma/schema.prisma
 echo -e "Database created"
