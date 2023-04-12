a8ca4aaa5cac203da1a69d69145747e528cf9c3f
Safwan Shaheer <devorein00@gmail.com>
2023-04-12 20:03:42 +0600

    Added computePostPermissions test for drafted post

--- lib/permissions/forum/computePostPermissions.ts
+++ lib/permissions/forum/computePostPermissions.ts
@@ -119,10 +119,13 @@ async function onlyEditableByAuthor({
 
 async function draftPostPolicy({ resource, flags, userId }: PostPolicyInput): Promise<AvailablePostPermissionFlags> {
   const newPermissions = {
-    ...flags,
-    add_comment: resource.isDraft ? !resource.isDraft : flags.add_comment,
-    upvote: resource.isDraft ? !resource.isDraft : flags.upvote,
-    downvote: resource.isDraft ? !resource.isDraft : flags.downvote,
+    edit_post: userId && resource.isDraft ? resource.createdBy === userId : flags.edit_post,
+    pin_post: userId && resource.isDraft ? false : flags.pin_post,
+    lock_post: userId && resource.isDraft ? false : flags.lock_post,
+    delete_comments: resource.isDraft ? false : flags.delete_comments,
+    add_comment: resource.isDraft ? false : flags.add_comment,
+    upvote: resource.isDraft ? false : flags.upvote,
+    downvote: resource.isDraft ? false : flags.downvote,
     delete_post: userId && resource.isDraft ? resource.createdBy === userId : flags.delete_post,
     view_post: userId && resource.isDraft ? resource.createdBy === userId : flags.view_post
   };
