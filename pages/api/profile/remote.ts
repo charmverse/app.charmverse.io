db7eb6294121a9c950c1d4cd71f7d6de488ead2e
Matt Casey <mattwad@gmail.com>
2023-04-13 13:13:01 -0600

    add endpoint to log in as another user

---
+++ pages/api/profile/remote.ts
@@ -0,0 +1,25 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import nc from 'next-connect';
+
+import { isDevEnv } from 'config/constants';
+import { onError, onNoMatch } from 'lib/middleware';
+import { withSessionRoute } from 'lib/session/withSession';
+
+const handler = nc<NextApiRequest, NextApiResponse>({ onError, onNoMatch });
+
+if (isDevEnv) {
+  handler.get(register);
+}
+
+async function register(req: NextApiRequest, res: NextApiResponse) {
+  const userId = req.query.id || (req.query.userId as string);
+  if (!userId) {
+    throw new Error('Please provider ?userid=...');
+  }
+  req.session.user = { id: userId };
+  await req.session.save();
+
+  res.redirect('/');
+}
+
+export default withSessionRoute(handler);
