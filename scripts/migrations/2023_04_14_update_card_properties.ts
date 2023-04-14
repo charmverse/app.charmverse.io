05f6205ffcd7e033e6a1a22930930a4cbb8b3e61
Safwan Shaheer <devorein00@gmail.com>
2023-04-15 00:09:39 +0600

    Add transformed Condition to migration script

--- scripts/migrations/2023_04_14_update_card_properties.ts
+++ scripts/migrations/2023_04_14_update_card_properties.ts
@@ -3,6 +3,7 @@
 import { Block } from '@prisma/client';
 import { prisma } from 'db';
 import { BoardView } from 'lib/focalboard/boardView';
+import { FilterClause, FilterCondition } from 'lib/focalboard/filterClause';
 import { v4 } from 'uuid';
 
 // use this file and run against production to generate api keys
@@ -19,8 +20,22 @@ import { v4 } from 'uuid';
   }) as Block[])
 
   const viewsWithFilter = viewBlocks.filter(block => (block.fields as any)?.filter?.filters.length > 0) as unknown as BoardView[]
+  
   for (const viewWithFilter of viewsWithFilter) {
-    const updatedFilters = viewWithFilter.fields.filter.filters.map(filter => ({...filter, filterId: v4()}))
+    const updatedFilters = viewWithFilter.fields.filter.filters.map(filter => {
+      const condition = (filter as FilterClause).condition as string;
+      let transformedCondition: FilterCondition = 'contains';
+      if (condition === "includes") {
+        transformedCondition = "contains"
+      } else if (condition === "notIncludes") {
+        transformedCondition = "does_not_contain"
+      } else if (condition === "isNotEmpty") {
+        transformedCondition = "is_not_empty"
+      } else if (condition === "isEmpty") {
+        transformedCondition = "is_empty"
+      }
+      return {...filter, condition: transformedCondition, filterId: v4()}
+    })
     await prisma.block.update({
       where: {
         id: viewWithFilter.id
@@ -29,12 +44,11 @@ import { v4 } from 'uuid';
         fields: {
           ...viewWithFilter.fields,
           filter: {
-            operation: "and",
+            operation: viewWithFilter.fields.filter.operation,
             filters: updatedFilters
           }
         }
       }
     })
   }
-
 })();
