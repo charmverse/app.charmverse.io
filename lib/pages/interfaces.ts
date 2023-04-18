6214303214815b20098d424b1eb13399224a451c
Matt Casey <mattwad@gmail.com>
2023-04-18 14:16:46 -0600

    remove block import

--- lib/pages/interfaces.ts
+++ lib/pages/interfaces.ts
@@ -1,4 +1,4 @@
-import type { Block, Page, PagePermission, Space } from '@prisma/client';
+import type { Page, PagePermission, Space } from '@prisma/client';
 
 import type { BountyWithDetails } from 'lib/bounties';
 import type { Board, IPropertyTemplate } from 'lib/focalboard/board';
