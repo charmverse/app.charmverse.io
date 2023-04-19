f1f66eb146ca3b1a14b7cd40752367f51cbf4a33
Safwan Shaheer <devorein00@gmail.com>
2023-04-19 15:41:26 +0600

    Removed unused scss links

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -87,8 +87,6 @@ import 'components/common/BoardEditor/focalboard/src/components/table/calculatio
 import 'components/common/BoardEditor/focalboard/src/components/table/horizontalGrip.scss';
 import 'components/common/BoardEditor/focalboard/src/components/table/table.scss';
 import 'components/common/BoardEditor/focalboard/src/components/table/tableRow.scss';
-import 'components/common/BoardEditor/focalboard/src/components/topBar.scss';
-import 'components/common/BoardEditor/focalboard/src/components/viewHeader/filterEntry.scss';
 import 'components/common/BoardEditor/focalboard/src/components/viewHeader/viewHeader.scss';
 import 'components/common/BoardEditor/focalboard/src/components/viewTitle.scss';
 import 'components/common/BoardEditor/focalboard/src/styles/labels.scss';
