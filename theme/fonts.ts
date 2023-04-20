67dae7522d01eb64be08ffca452d696c68d0d802
Matt Casey <mattwad@gmail.com>
2023-04-20 13:22:42 -0600

    split next config

--- theme/fonts.ts
+++ theme/fonts.ts
@@ -2,21 +2,19 @@
 import { Source_Serif_4 } from 'next/font/google';
 import localFont from 'next/font/local';
 
-// Fonts from Notion:
-// Serif: Lyon-Text, Georgia, ui-serif, serif;
-// Mono: iawriter-mono, Nitti, Menlo, Courier, monospace;
-
 export const defaultFont =
   'ui-sans-serif, -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Segoe UI Emoji", "Segoe UI Symbol"';
 
 export const serifFont = Source_Serif_4({
   subsets: ['latin'],
   variable: '--font-family-serif',
+  fallback: ['Georgia', 'ui-serif', 'serif'],
   weight: ['400', '600', '700']
 });
 
 export const monoFont = localFont({
   variable: '--font-family-mono',
+  fallback: ['Nitti', 'Menlo', 'Courier', 'monospace'],
   src: [
     {
       path: '../node_modules/@fontsource/ia-writer-mono/files/ia-writer-mono-all-400-normal.woff2',
