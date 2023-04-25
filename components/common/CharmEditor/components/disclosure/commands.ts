d19985660aa0bc3510d4261d73893a3f96c87675
Safwan Shaheer <devorein00@gmail.com>
2023-04-25 20:30:20 +0600

    Open toggle when pressing enter in disclosure summary

--- components/common/CharmEditor/components/disclosure/commands.ts
+++ components/common/CharmEditor/components/disclosure/commands.ts
@@ -48,16 +48,16 @@ export const backspaceCmd: Command = (state, dispatch) => {
   return false;
 };
 
-// TODO: we should move cursor to the content section when hitting Enter
-
-export const moveDownCmd: Command = (state, dispatch) => {
+export const moveDownCmd: Command = (state, dispatch, view) => {
   let { tr } = state;
   // @ts-ignore types package is missing $cursor property as of 1.2.8
   const { $cursor } = state.selection;
   const summaryNode = findParentNodeOfTypeClosestToPos($cursor, state.schema.nodes.disclosureSummary);
   const detailsNode = findParentNodeOfTypeClosestToPos($cursor, state.schema.nodes.disclosureDetails);
   // Must be in summary node for enter to take effect
-  if (detailsNode && summaryNode) {
+  if (detailsNode && summaryNode && view) {
+    const summaryDomNode = view.domAtPos(summaryNode.pos).node as HTMLDivElement;
+    summaryDomNode.setAttribute('open', '');
     const emptyParagraphNode = state.schema.nodes.paragraph.create();
     const summaryNodePosition = tr.doc.resolve(detailsNode.pos + summaryNode.node.nodeSize);
     tr = tr.setSelection(new TextSelection(summaryNodePosition)).replaceSelectionWith(emptyParagraphNode, false);
