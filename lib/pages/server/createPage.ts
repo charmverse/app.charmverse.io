7942a1a176ab696c5ebb26445340313628042cc8
motechFR <mo@anyahq.co>
2023-05-02 18:16:43 +0300

    Setup page restore

--- lib/pages/server/createPage.ts
+++ lib/pages/server/createPage.ts
@@ -2,9 +2,10 @@ import { prisma } from '@charmverse/core';
 import type { Page, Prisma, PrismaPromise } from '@charmverse/core/dist/prisma';
 
 import { checkIsContentEmpty } from 'lib/prosemirror/checkIsContentEmpty';
-import { emptyDocument } from 'lib/prosemirror/constants';
 import { extractPreviewImage } from 'lib/prosemirror/extractPreviewImage';
 import type { PageContent } from 'lib/prosemirror/interfaces';
+
+import { generateFirstDiff } from './generateFirstDiff';
 
 export function createPage<T>({ data, include }: Prisma.PageCreateArgs): PrismaPromise<Page & T> {
   const createArgs: Prisma.PageCreateArgs = {
@@ -12,13 +13,15 @@ export function createPage<T>({ data, include }: Prisma.PageCreateArgs): PrismaP
       ...data,
       hasContent: data.content ? !checkIsContentEmpty(data.content as PageContent) : false,
       galleryImage: extractPreviewImage(data.content as PageContent),
-      diffs: {
-        create: {
-          createdBy: (data.createdBy ?? data.author.connect?.id) as string,
-          data: (data.content ?? emptyDocument) as Prisma.InputJsonValue,
-          version: 0
-        }
-      }
+      diffs: data.content
+        ? {
+            create: generateFirstDiff({
+              // We should be receiving the data from one of these
+              createdBy: data.createdBy ?? (data.author?.connect?.id as string),
+              content: data.content
+            })
+          }
+        : undefined
     }
   };
 
