6c6773c883a0aafe3a2de70092cc075a85922470
Marek <m.sadura@outlook.com>
2023-05-08 23:20:33 +0200

    Remove temp file

--- scripts/testTx.ts
+++
@@ -1,9 +0,0 @@
-import { getSafeTxStatus } from "lib/gnosis/getSafeTxStatus";
-
-async function test(){
-  const res = await getSafeTxStatus({safeTxHash: '0xfe0df7ccebdfa7bcd1eae5ec80382a7c5d77a0d299e4ef9ba804ff44495e27b1', chainId: 137});
-  console.log('🔥', res);
-}
-
-test();
-
