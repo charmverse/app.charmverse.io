787d0ff019b9a013790cdb122d69bd746317b139
Safwan Shaheer <devorein00@gmail.com>
2023-05-10 17:41:49 +0600

    Create stripe products based on usage and period

---
+++ scripts/createStripeProducts.ts
@@ -0,0 +1,19 @@
+import { UsageRecord } from 'lib/payment/utils';
+import Stripe from 'stripe';
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+  apiVersion: '2022-11-15'
+});
+
+async function createStripeProducts() {
+  for (const usage of Object.keys(UsageRecord)) {
+    for (const period of ["monthly", 'annual']) {
+      await stripe.products.create({
+        name: `Pro - ${usage} - ${period} Subscription`,
+        id: `pro-${usage}-${period}`,
+      })
+    }
+  }
+}
+
+createStripeProducts();
