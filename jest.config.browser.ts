10c6006872e210203843f338ffbfdf7c67c6f6b3
Matt Casey <mattwad@gmail.com>
2023-05-10 09:31:21 -0600

    add setup for browser tests (#2129)
    * add setup for browser tests
    
    * udpate config names
    
    * add browser setup

--- jest.config.browser.ts
+++ jest.config.browser.ts
@@ -18,13 +18,15 @@ const jestConfig = {
   // An array of directory names to be searched recursively up from the requiring module's location
   moduleDirectories: ['node_modules', '<rootDir>'],
 
+  // A list of paths to modules that run some code to configure or set up the testing framework before each test
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.browser.ts'],
+
   // The test environment that will be used for testing
   testEnvironment: 'jsdom',
 
   // The glob patterns Jest uses to detect test files
   testMatch: ['**/components/**/?(*.)+(spec).[tj]s?(x)', '**/hooks/**/*.spec.ts'],
   modulePathIgnorePatterns: ['focalboard/src'],
-  testTimeout: 30000,
   moduleNameMapper: {
     // map SVG to something that Jest can read - could be used for other extensions as well?
     // source: https://github.com/vercel/next.js/discussions/42535#discussioncomment-4828013
