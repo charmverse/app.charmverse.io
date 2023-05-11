8e5f79cfc740911d0c39d58c8739be6eccc710eb
Valentin L <ludu.valentin@gmail.com>
2023-05-11 18:40:32 +0300

    Sync safes with our db and ui

--- components/bounties/components/MultiPaymentModal.tsx
+++ components/bounties/components/MultiPaymentModal.tsx
@@ -20,6 +20,7 @@ import { useEffect, useState } from 'react';
 import Button from 'components/common/Button';
 import { DialogTitle, Modal } from 'components/common/Modal';
 import UserDisplay from 'components/common/UserDisplay';
+import useImportSafes from 'hooks/useImportSafes';
 import { useMembers } from 'hooks/useMembers';
 import { useMultiBountyPayment } from 'hooks/useMultiBountyPayment';
 import useMultiWalletSigs from 'hooks/useMultiWalletSigs';
@@ -35,6 +36,7 @@ export function MultiPaymentModal({ bounties }: { bounties: BountyWithDetails[] 
   const popupState = usePopupState({ variant: 'popover', popupId: 'multi-payment-modal' });
   const modalProps = bindPopover(popupState);
   const { data: userGnosisSafes } = useMultiWalletSigs();
+  const { importSafes } = useImportSafes();
   const { onClick } = useSettingsDialog();
 
   const { isDisabled, onPaymentSuccess, getTransactions, gnosisSafes, gnosisSafeData, isLoading, setGnosisSafeData } =
@@ -59,6 +61,12 @@ export function MultiPaymentModal({ bounties }: { bounties: BountyWithDetails[] 
       }, {}) ?? {};
 
   const { getMemberById } = useMembers();
+
+  useEffect(() => {
+    if (transactions.length) {
+      importSafes();
+    }
+  }, [transactions.length]);
 
   useEffect(() => {
     const applicationIds = transactions.map((trans) => trans.applicationId);
---
+++ hooks/useImportSafes.tsx
@@ -0,0 +1,47 @@
+import log from 'loglevel';
+import { useCallback, useState } from 'react';
+
+import useGnosisSigner from 'hooks/useWeb3Signer';
+import { importSafesFromWallet } from 'lib/gnosis/gnosis.importSafes';
+
+import useMultiWalletSigs from './useMultiWalletSigs';
+import { useSnackbar } from './useSnackbar';
+import { useUser } from './useUser';
+
+export default function useImportSafes() {
+  const { data, mutate } = useMultiWalletSigs();
+  const gnosisSigner = useGnosisSigner();
+  const { user } = useUser();
+  const { showMessage } = useSnackbar();
+  const [isLoadingSafes, setIsLoadingSafes] = useState(false);
+
+  const getWalletName = (address: string) => {
+    return data?.find((wallet) => wallet.address === address)?.name;
+  };
+
+  const importSafes = useCallback(async () => {
+    if (gnosisSigner && user) {
+      setIsLoadingSafes(true);
+      try {
+        const safesCount = await importSafesFromWallet({
+          signer: gnosisSigner,
+          addresses: user.wallets.map((w) => w.address),
+          getWalletName
+        });
+
+        if (!safesCount) {
+          showMessage('You do not have any gnosis wallets', 'warning');
+        }
+        await mutate();
+      } catch (e) {
+        log.error('Error importing safes', e);
+
+        showMessage('We could not import your safes', 'error');
+      } finally {
+        setIsLoadingSafes(false);
+      }
+    }
+  }, [gnosisSigner, user]);
+
+  return { isLoadingSafes, importSafes };
+}
