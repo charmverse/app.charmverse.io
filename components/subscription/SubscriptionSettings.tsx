ef7b9c17047471af6199468e7488375861f4f43e
Safwan Shaheer <devorein00@gmail.com>
2023-05-16 20:28:54 +0600

    Better subscription skeleton UI

--- components/subscription/SubscriptionSettings.tsx
+++ components/subscription/SubscriptionSettings.tsx
@@ -1,5 +1,5 @@
 import type { Space } from '@charmverse/core/prisma';
-import { Divider, InputLabel, Typography } from '@mui/material';
+import { Divider, InputLabel, Skeleton, Typography } from '@mui/material';
 import { Stack } from '@mui/system';
 import { Elements } from '@stripe/react-stripe-js';
 import type { Stripe } from '@stripe/stripe-js';
@@ -11,7 +11,6 @@ import useSWR from 'swr';
 import charmClient from 'charmClient';
 import Button from 'components/common/Button';
 import Link from 'components/common/Link';
-import LoadingComponent from 'components/common/LoadingComponent';
 import Legend from 'components/settings/Legend';
 import { useMembers } from 'hooks/useMembers';
 import { SUBSCRIPTION_USAGE_RECORD } from 'lib/subscription/constants';
@@ -47,10 +46,6 @@ export function SubscriptionSettings({ space }: { space: Space }) {
     });
   }
 
-  if (isLoading) {
-    return <LoadingComponent label='Fetching your space subscription' />;
-  }
-
   return (
     <Stack>
       <Stack gap={1}>
@@ -63,58 +58,69 @@ export function SubscriptionSettings({ space }: { space: Space }) {
             Read about all the benefits
           </Link>
         </Typography>
-        <Stack>
-          <InputLabel>Current tier</InputLabel>
-          <Typography>{capitalize(spaceSubscription?.tier ?? 'free')}</Typography>
-        </Stack>
-        {spaceSubscription?.period && (
-          <Stack>
-            <InputLabel>Period</InputLabel>
-            <Typography>{capitalize(spaceSubscription.period)}</Typography>
+        {isLoading ? (
+          <Stack gap={1} mt={2}>
+            <Skeleton variant='rectangular' width={150} height={16} />
+            <Skeleton variant='rectangular' width='100%' height={55} />
+            <Skeleton variant='rectangular' width={150} height={16} sx={{ mt: 1 }} />
+            <Skeleton variant='rectangular' width='100%' height={35} />
           </Stack>
-        )}
-        {spaceSubscription?.usage && (
-          <Stack>
-            <InputLabel>Usage</InputLabel>
-            <Stack>
-              <Typography>Blocks: 0/{SUBSCRIPTION_USAGE_RECORD[spaceSubscription.usage].totalBlocks}</Typography>
-              <Typography>
-                Members: {members.length}/{SUBSCRIPTION_USAGE_RECORD[spaceSubscription.usage].totalActiveUsers}
-              </Typography>
-            </Stack>
-          </Stack>
-        )}
-        <Divider sx={{ mb: 1 }} />
-        {showCheckoutForm && stripePromise.current ? (
-          <Elements stripe={stripePromise.current}>
-            <CheckoutForm
-              spaceSubscription={spaceSubscription}
-              refetch={refetchSpaceSubscription}
-              onCancel={() => setShowCheckoutForm(false)}
-            />
-          </Elements>
         ) : (
-          <Stack flexDirection='row' gap={1}>
-            <Button
-              sx={{
-                width: 'fit-content'
-              }}
-              onClick={handleShowCheckoutForm}
-            >
-              Upgrade
-            </Button>
-            {spaceSubscription !== null && (
-              <Button
-                onClick={() => {
-                  setShowCheckoutForm(false);
-                }}
-                color='error'
-                variant='outlined'
-              >
-                Cancel Plan
-              </Button>
+          <>
+            <Stack>
+              <InputLabel>Current tier</InputLabel>
+              <Typography>{capitalize(spaceSubscription?.tier ?? 'free')}</Typography>
+            </Stack>
+            {spaceSubscription?.period && (
+              <Stack>
+                <InputLabel>Period</InputLabel>
+                <Typography>{capitalize(spaceSubscription.period)}</Typography>
+              </Stack>
             )}
-          </Stack>
+            {spaceSubscription?.usage && (
+              <Stack>
+                <InputLabel>Usage</InputLabel>
+                <Stack>
+                  <Typography>Blocks: 0/{SUBSCRIPTION_USAGE_RECORD[spaceSubscription.usage].totalBlocks}</Typography>
+                  <Typography>
+                    Members: {members.length}/{SUBSCRIPTION_USAGE_RECORD[spaceSubscription.usage].totalActiveUsers}
+                  </Typography>
+                </Stack>
+              </Stack>
+            )}
+            <Divider sx={{ mb: 1 }} />
+            {showCheckoutForm && stripePromise.current ? (
+              <Elements stripe={stripePromise.current}>
+                <CheckoutForm
+                  spaceSubscription={spaceSubscription}
+                  refetch={refetchSpaceSubscription}
+                  onCancel={() => setShowCheckoutForm(false)}
+                />
+              </Elements>
+            ) : (
+              <Stack flexDirection='row' gap={1}>
+                <Button
+                  sx={{
+                    width: 'fit-content'
+                  }}
+                  onClick={handleShowCheckoutForm}
+                >
+                  Upgrade
+                </Button>
+                {spaceSubscription !== null && (
+                  <Button
+                    onClick={() => {
+                      setShowCheckoutForm(false);
+                    }}
+                    color='error'
+                    variant='outlined'
+                  >
+                    Cancel Plan
+                  </Button>
+                )}
+              </Stack>
+            )}
+          </>
         )}
       </Stack>
     </Stack>
