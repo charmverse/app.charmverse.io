dc068c21f1325acdc476495194b2d012d6dd61f1
Marek <m.sadura@outlook.com>
2023-05-20 21:44:18 +0200

    Add secure cookie

--- pages/api/session/logout.ts
+++ pages/api/session/logout.ts
@@ -17,11 +17,12 @@ async function logout(req: NextApiRequest, res: NextApiResponse<{ ok: boolean }>
   if (cookieDomain) {
     const cookiesHeader = res.getHeader('Set-Cookie') || [];
     const setCookiesArray = Array.isArray(cookiesHeader) ? cookiesHeader : [];
+    const secureCookie = req.headers.host?.startsWith('https://') ? 'Secure;' : '';
 
     res.setHeader('Set-Cookie', [
       ...setCookiesArray,
       // remove old domain cookies
-      `${cookieName}=; Max-Age=0; Domain=app.${getCookieDomain()}; Path=/; HttpOnly; SameSite=Strict`
+      `${cookieName}=; Max-Age=0; Domain=app.${getCookieDomain()}; Path=/; HttpOnly; SameSite=Strict ${secureCookie}`
     ]);
   }
 
