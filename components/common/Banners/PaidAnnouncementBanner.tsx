acb674cc5a147f4093a4241a912e1a6fa680a8a6
Safwan Shaheer <devorein00@gmail.com>
2023-05-31 17:15:52 +0600

    Removed extra state and refactored code

--- components/common/Banners/PaidAnnouncementBanner.tsx
+++ components/common/Banners/PaidAnnouncementBanner.tsx
@@ -1,7 +1,6 @@
 import CloseIcon from '@mui/icons-material/Close';
 import EastIcon from '@mui/icons-material/East';
-import { Typography, Stack, IconButton } from '@mui/material';
-import { useEffect, useState } from 'react';
+import { IconButton, Stack, Typography } from '@mui/material';
 
 import Link from 'components/common/Link';
 import { useLocalStorage } from 'hooks/useLocalStorage';
@@ -10,35 +9,27 @@ import { StyledBanner } from './Banner';
 
 export function PaidAnnouncementBanner() {
   const [showPaidAnnouncementBar, setShowPaidAnnouncementBar] = useLocalStorage('show-paid-banner', true);
-  const [component, setComponent] = useState<JSX.Element | null>(null);
-  // Using use-effect to render the component only client side, otherwise next.js throws rehydration mismatch error
-  useEffect(() => {
-    setComponent(
-      showPaidAnnouncementBar ? (
-        <StyledBanner top={50} data-test='paid-announcement-banner'>
-          <Typography>
-            Community Edition coming on June 30th. Include complex role-based access control, API, & custom domain.{' '}
-            <Stack gap={0.5} flexDirection='row' alignItems='center' display='inline-flex'>
-              <Link color='inherit' target='_blank' href='https://charmverse.io/pricing' sx={{ fontWeight: 600 }}>
-                More Details
-              </Link>
-              <EastIcon sx={{ position: 'relative', top: 1.5, fontSize: 16 }} />
-            </Stack>
-          </Typography>
-          <IconButton
-            onClick={() => setShowPaidAnnouncementBar(false)}
-            size='small'
-            sx={{
-              position: 'absolute',
-              right: 5
-            }}
-          >
-            <CloseIcon fontSize='small' />
-          </IconButton>
-        </StyledBanner>
-      ) : null
-    );
-  }, [showPaidAnnouncementBar, setShowPaidAnnouncementBar]);
-
-  return component;
+  return showPaidAnnouncementBar ? (
+    <StyledBanner top={50} data-test='paid-announcement-banner'>
+      <Typography component='div'>
+        Community Edition coming on June 30th. Includes complex role-based access control, API, & custom domain.{' '}
+        <Stack gap={0.5} flexDirection='row' alignItems='center' display='inline-flex'>
+          <Link color='inherit' target='_blank' href='https://charmverse.io/pricing' sx={{ fontWeight: 600 }}>
+            More Details
+          </Link>
+          <EastIcon sx={{ position: 'relative', top: 1.5, fontSize: 16 }} />
+        </Stack>
+      </Typography>
+      <IconButton
+        onClick={() => setShowPaidAnnouncementBar(false)}
+        size='small'
+        sx={{
+          position: 'absolute',
+          right: 5
+        }}
+      >
+        <CloseIcon fontSize='small' />
+      </IconButton>
+    </StyledBanner>
+  ) : null;
 }
