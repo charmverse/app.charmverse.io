34c2aa03e61f919ac18a1f5ad23b952349a2cbb0
Safwan Shaheer <devorein00@gmail.com>
2023-06-04 15:19:31 +0600

    Show proposal template field

---
+++ components/proposals/components/ProposalTemplateInput.tsx
@@ -0,0 +1,56 @@
+import DescriptionOutlinedIcon from '@mui/icons-material/DescriptionOutlined';
+import { ListItemIcon, ListItemText, MenuItem, TextField } from '@mui/material';
+import Autocomplete from '@mui/material/Autocomplete';
+
+import type { PageMeta } from 'lib/pages';
+import { fancyTrim } from 'lib/utilities/strings';
+
+const maxTitleLength = 35;
+
+type Props = {
+  disabled?: boolean;
+  options: PageMeta[];
+  value: PageMeta | null;
+  onChange: (value: PageMeta | null) => void;
+};
+
+export default function ProposalTemplateInput({ disabled, options, value, onChange }: Props) {
+  return (
+    <Autocomplete
+      disabled={disabled}
+      value={value}
+      selectOnFocus
+      clearOnBlur
+      handleHomeEndKeys
+      filterSelectedOptions
+      sx={{ minWidth: 150, width: '100%' }}
+      options={options}
+      autoHighlight
+      clearIcon={null}
+      renderOption={(_props, page) => (
+        <MenuItem>
+          <ListItemIcon>
+            <DescriptionOutlinedIcon />
+          </ListItemIcon>
+          <ListItemText>{fancyTrim(page.title || 'Untitled', maxTitleLength)}</ListItemText>
+        </MenuItem>
+      )}
+      ChipProps={{
+        // Hack for preventing delete from showing
+        onDelete: null as any
+      }}
+      noOptionsText='No templates available'
+      renderInput={(params) => <TextField {...params} placeholder='Select template' size='small' />}
+      getOptionLabel={(templatePage: PageMeta) => {
+        // Regular option
+        return templatePage?.title;
+      }}
+      isOptionEqualToValue={(templatePage, checkValue) => {
+        return templatePage.id === checkValue.id;
+      }}
+      onChange={(_, values) => {
+        onChange(values);
+      }}
+    />
+  );
+}
