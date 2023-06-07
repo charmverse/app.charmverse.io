bd9d2a9b760c2d07095f184d717370ff1162c513
Matt Casey <mattwad@gmail.com>
2023-06-06 22:05:06 -0600

    remove trailing slash

--- lib/websockets/config.ts
+++ lib/websockets/config.ts
@@ -1,4 +1,5 @@
 export const config = {
+  addTrailingSlash: false, // fix for next.js https://github.com/vercel/next.js/issues/49334
   // increase the amount of data that can be sent to the server
   maxHttpBufferSize: 1e7 // set from 1e6 (1MB) to 1e7 (10Mb)
 };
