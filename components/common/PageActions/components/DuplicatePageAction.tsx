602a5949fc02691f57470d8573f1cc4a4b6d6f9c
Matt Casey <mattwad@gmail.com>
2023-06-06 21:51:34 -0600

    restrict duplication rules to users with create page

--- components/common/PageActions/components/DuplicatePageAction.tsx
+++ components/common/PageActions/components/DuplicatePageAction.tsx
@@ -7,6 +7,7 @@ import { useRouter } from 'next/router';
 import charmClient from 'charmClient';
 import { useBounties } from 'hooks/useBounties';
 import { useCurrentSpace } from 'hooks/useCurrentSpace';
+import { useCurrentSpacePermissions } from 'hooks/useCurrentSpacePermissions';
 
 const excludedPageTypes: (PageType | undefined)[] = ['bounty_template', 'proposal_template'];
 
@@ -24,10 +25,11 @@ export function DuplicatePageAction({
   pagePermissions: PagePermissionFlags | undefined;
 }) {
   const currentSpace = useCurrentSpace();
+  const [userSpacePermissions] = useCurrentSpacePermissions();
   const router = useRouter();
   const { refreshBounty } = useBounties();
 
-  const disabled = !pagePermissions?.read;
+  const disabled = !pagePermissions?.read || !userSpacePermissions?.createPage;
 
   async function duplicatePage() {
     if (currentSpace) {
