0b3ee46fe20f7af1d4e25c13b9a19686ef21b3c8
Matt Casey <mattwad@gmail.com>
2023-06-07 10:19:40 -0600

    Revert "Revert "Show paid announcement banner"" (#2238)
    * Revert "Revert "Show paid announcement banner (#2228)" (#2237)"
    
    This reverts commit c9ff3bc6226b79f310aa3ea4e8b6cf1f2eaa0ee5.
    
    * Update PaidAnnouncementBanner.tsx

--- components/common/Banners/Banner.tsx
+++ components/common/Banners/Banner.tsx
@@ -1,12 +1,14 @@
 import styled from '@emotion/styled';
 import Box from '@mui/material/Box';
 
-export const StyledBanner = styled(Box)`
+export const StyledBanner = styled(Box)<{ errorBackground?: boolean }>`
   width: 100%;
   z-index: var(--z-index-appBar);
   display: flex;
   justify-content: center;
   color: ${({ theme }) => theme.palette.text.primary};
-  background-color: var(--bg-blue);
+  background-color: ${({ errorBackground, theme }) => (!errorBackground ? `var(--bg-blue)` : theme.palette.error.main)};
   padding: ${({ theme }) => theme.spacing(1.4)};
+  position: sticky;
+  align-items: center;
 `;
---
+++ components/common/Banners/PaidAnnouncementBanner.tsx
@@ -0,0 +1,35 @@
+import CloseIcon from '@mui/icons-material/Close';
+import EastIcon from '@mui/icons-material/East';
+import { IconButton, Stack, Typography } from '@mui/material';
+
+import Link from 'components/common/Link';
+import { useLocalStorage } from 'hooks/useLocalStorage';
+
+import { StyledBanner } from './Banner';
+
+export function PaidAnnouncementBanner() {
+  const [showPaidAnnouncementBar, setShowPaidAnnouncementBar] = useLocalStorage('show-paid-banner', true);
+  return showPaidAnnouncementBar ? (
+    <StyledBanner top={50} data-test='paid-announcement-banner'>
+      <Typography component='div'>
+        Community Edition coming on June 30th. Includes complex role-based access control, API, & custom domain.{' '}
+        <Stack gap={0.5} flexDirection='row' alignItems='center' display='inline-flex'>
+          <Link color='inherit' target='_blank' href='https://app.charmverse.io/charmverse/page-5371612014886058' sx={{ fontWeight: 600 }}>
+            More Details
+          </Link>
+          <EastIcon sx={{ position: 'relative', top: 1.5, fontSize: 16 }} />
+        </Stack>
+      </Typography>
+      <IconButton
+        onClick={() => setShowPaidAnnouncementBar(false)}
+        size='small'
+        sx={{
+          position: 'absolute',
+          right: 5
+        }}
+      >
+        <CloseIcon fontSize='small' />
+      </IconButton>
+    </StyledBanner>
+  ) : null;
+}
