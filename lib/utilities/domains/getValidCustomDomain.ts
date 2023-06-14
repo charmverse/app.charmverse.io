c8b0b27c3edcd948d9830d4e53967a31bba3ae91
Marek <m.sadura@outlook.com>
2023-06-14 13:25:25 +0200

    More robust cusotm domain check

--- lib/utilities/domains/getValidCustomDomain.ts
+++ lib/utilities/domains/getValidCustomDomain.ts
@@ -1,6 +1,8 @@
 import { getValidDefaultHost } from 'lib/utilities/domains/getValidDefaultHost';
 import { isLocalhostAlias } from 'lib/utilities/domains/isLocalhostAlias';
 
+const BLACKLISTED_DOMAIN_NAMES = ['amazonaws.com'];
+
 export function getValidCustomDomain(host?: string | null) {
   if (!host && typeof window !== 'undefined') {
     // On client side, get the host from window
@@ -11,14 +13,23 @@ export function getValidCustomDomain(host?: string | null) {
     return null;
   }
 
-  const defaultHost = getValidDefaultHost(host);
+  const hostname = host?.split(':')[0];
+
+  if (BLACKLISTED_DOMAIN_NAMES.some((domain) => hostname?.includes(domain))) {
+    return null;
+  }
+
+  if (hostname && !/[a-z]/i.test(hostname)) {
+    // hostname is an IP address - case not supported
+    return null;
+  }
+
+  const defaultHost = getValidDefaultHost(hostname);
 
   if (defaultHost) {
     // app runs on default domain so space does not use custom domain
     return null;
   }
 
-  const customDomain = host?.split(':')[0];
-
-  return customDomain || null;
+  return hostname || null;
 }
