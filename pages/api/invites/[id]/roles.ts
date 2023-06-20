36b4d83c0188ba17be34001765c688e96fec2162
motechFR <mo@anyahq.co>
2023-06-20 21:16:03 +0300

    Block invite link updates and add missing test coverage

--- pages/api/invites/[id]/roles.ts
+++ pages/api/invites/[id]/roles.ts
@@ -1,10 +1,10 @@
-import type { InviteLinkToRole } from '@charmverse/core/prisma';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import nc from 'next-connect';
 
 import type { InviteLinkWithRoles } from 'lib/invites/getSpaceInviteLinks';
 import { updateInviteLinkRoles } from 'lib/invites/updateInviteLinkRoles';
 import { onError, onNoMatch, requireKeys, requireUser } from 'lib/middleware';
+import { requirePaidPermissionsSubscription } from 'lib/middleware/requirePaidPermissionsSubscription';
 import { requireSpaceMembership } from 'lib/middleware/requireSpaceMembership';
 import { withSessionRoute } from 'lib/session/withSession';
 
@@ -13,8 +13,9 @@ const handler = nc<NextApiRequest, NextApiResponse>({ onError, onNoMatch });
 handler
   .use(requireUser)
   .use(requireKeys(['roleIds', 'spaceId'], 'body'))
+  .use(requirePaidPermissionsSubscription({ key: 'spaceId', resourceIdType: 'space', location: 'body' }))
   .use(requireSpaceMembership({ adminOnly: true }))
-  .post(updateInviteLinkRolesHandler);
+  .put(updateInviteLinkRolesHandler);
 
 async function updateInviteLinkRolesHandler(req: NextApiRequest, res: NextApiResponse<InviteLinkWithRoles>) {
   const { roleIds } = req.body as { roleIds: string[] };
