65ddcd3bc80390b129ecd3ad5bb6218f5ecf122d
Matt Casey <mattwad@gmail.com>
2023-06-20 09:57:44 -0400

    dont show an error message right away when client is out of sync with… (#2339)
    * dont show an error message right away when client is out of sync with server
    
    * .

--- lib/websockets/documentEvents/documentEvents.ts
+++ lib/websockets/documentEvents/documentEvents.ts
@@ -159,7 +159,10 @@ export class DocumentEventHandler {
         message
       });
       // Dont know how it gets out of sync, but sometimes these are not in fact duplicate messages. And the user ends up losing all their changes as each new one is ignored.
-      this.sendError('Your version of this document is out of sync with the server. Please refresh the page.');
+      // check if there are at least 10 updates and ask the user to refresh
+      if (message.type === 'diff' && message.ds.length > 10) {
+        this.sendError('Your version of this document is out of sync with the server. Please refresh the page.');
+      }
       return;
     } else if (message.c > this.messages.client + 1) {
       log.warn('Request resent of lost messages from client', {
