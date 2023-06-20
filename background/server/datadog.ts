2d48c35daefd21f727a6ea711a5b98fff56ae8f3
Matt Casey <mattwad@gmail.com>
2023-06-20 11:07:10 -0600

    set service name

--- background/server/datadog.ts
+++ background/server/datadog.ts
@@ -3,7 +3,8 @@ import tracer from 'dd-trace';
 if (process.env.NODE_ENV === 'production') {
   tracer.init({
     logInjection: true,
-    runtimeMetrics: true
+    runtimeMetrics: true,
+    service: process.env.SERVICE_NAME
   });
   tracer.use('koa', {
     blocklist: ['/api/health_check']
