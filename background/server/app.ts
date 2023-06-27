9e9b62045559fa358e911a25db24480884069bd0
Matt Casey <mattwad@gmail.com>
2023-06-27 09:28:53 -0600

    remove kill endpoint

--- background/server/app.ts
+++ background/server/app.ts
@@ -9,14 +9,6 @@ const router = new Router();
 router.get('/', routes.healthCheck);
 router.get('/health_check', routes.healthCheck);
 
-// TODO: Remove this route before merging to master
-router.get('/kill-me', (ctx) => {
-  setTimeout(() => {
-    throw new Error('Killed by /kill-me');
-  }, 1000);
-  ctx.body = 'I shot the sheriff, but I did not shoot the deputy.';
-});
-
 app.use(routes.errorHandler).use(router.routes()).use(router.allowedMethods());
 
 export default app;
