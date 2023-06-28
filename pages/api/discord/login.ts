b46513b125c1e9cc71677bde914c923d36812d53
Marek <m.sadura@outlook.com>
2023-06-28 09:17:27 +0200

    Remove redundant logic

--- pages/api/discord/login.ts
+++ pages/api/discord/login.ts
@@ -1,16 +1,12 @@
 import { log } from '@charmverse/core/log';
-import Cookies from 'cookies';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import nc from 'next-connect';
 
-import { AUTH_CODE_COOKIE } from 'lib/discord/constants';
 import { loginByDiscord } from 'lib/discord/loginByDiscord';
 import { updateGuildRolesForUser } from 'lib/guild-xyz/server/updateGuildRolesForUser';
 import { extractSignupAnalytics } from 'lib/metrics/mixpanel/utilsSignup';
 import { InvalidStateError, onError, onNoMatch } from 'lib/middleware';
 import { withSessionRoute } from 'lib/session/withSession';
-import { getAppApexDomain } from 'lib/utilities/domains/getAppApexDomain';
-import { isLocalhostAlias } from 'lib/utilities/domains/isLocalhostAlias';
 import type { LoggedInUser } from 'models';
 
 const handler = nc<NextApiRequest, NextApiResponse>({ onError, onNoMatch });
@@ -18,11 +14,7 @@ const handler = nc<NextApiRequest, NextApiResponse>({ onError, onNoMatch });
 handler.post(loginDiscordCodeHandler);
 
 async function loginDiscordCodeHandler(req: NextApiRequest, res: NextApiResponse<LoggedInUser>) {
-  const cookies = new Cookies(req, res);
   const tempAuthCode = req.body.code as string;
-  const domain = isLocalhostAlias(req.headers.host) ? undefined : getAppApexDomain();
-
-  cookies.set(AUTH_CODE_COOKIE, tempAuthCode, { httpOnly: false, sameSite: 'strict', domain });
 
   try {
     const signupAnalytics = extractSignupAnalytics(req.cookies as any);
