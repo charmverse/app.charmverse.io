8fe3bc1fb92fe4d7ebdab537f195d484195929c5
motechFR <mo@anyahq.co>
2023-06-29 17:06:15 +0300

    Cleanup unused code

--- lib/subscription/getActiveSpaceSubscription.ts
+++ lib/subscription/getActiveSpaceSubscription.ts
@@ -1,6 +1,3 @@
-import fs from 'node:fs/promises';
-import path from 'node:path';
-
 import type { StripeSubscription } from '@charmverse/core/prisma-client';
 import { prisma } from '@charmverse/core/prisma-client';
 import type Stripe from 'stripe';
