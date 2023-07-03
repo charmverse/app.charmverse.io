21cdc11851f673c69280c68a9fa17e868b32fc98
Matt Casey <mattwad@gmail.com>
2023-07-03 14:52:48 -0600

    ..

--- components/common/PageLayout/PageLayout.tsx
+++ components/common/PageLayout/PageLayout.tsx
@@ -218,7 +218,7 @@ function PageLayout({ children }: PageLayoutProps) {
                 <>
                   <AppBar open={open} sidebarWidth={displaySidebarWidth} position='fixed'>
                     <Header open={open} openSidebar={handleDrawerOpen} />
-                    <PaidAnnouncementBanner />
+                    {space && <PaidAnnouncementBanner spaceId={space.id} />}
                   </AppBar>
                   {isMobile ? (
                     <MuiDrawer
