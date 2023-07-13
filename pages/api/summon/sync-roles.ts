6af7ae48cd94f9634f7d0f1a7a2372c63878e25d
Safwan Shaheer <devorein00@gmail.com>
2023-07-13 18:14:40 +0600

    Removed extra condition around summon role sync

--- pages/api/summon/sync-roles.ts
+++ pages/api/summon/sync-roles.ts
@@ -6,8 +6,6 @@ import { onError, onNoMatch, requireUser } from 'lib/middleware';
 import { requireKeys } from 'lib/middleware/requireKeys';
 import { withSessionRoute } from 'lib/session/withSession';
 import { syncSummonSpaceRoles } from 'lib/summon/syncSummonSpaceRoles';
-import { verifyMembership } from 'lib/summon/verifyMembership';
-import { InvalidInputError } from 'lib/utilities/errors';
 
 const handler = nc<NextApiRequest, NextApiResponse>({ onError, onNoMatch });
 
@@ -20,13 +18,7 @@ async function syncSummonSpaceRolesController(req: NextApiRequest, res: NextApiR
   const spaceId = req.query.spaceId as string;
   const userId = req.session?.user?.id;
 
-  const result = await verifyMembership({ spaceId, userId });
-
-  if (!result.isVerified) {
-    throw new InvalidInputError('You are not a member of this space');
-  }
-
-  const { totalSpaceRolesAdded, totalSpaceRolesUpdated } = await syncSummonSpaceRoles({ spaceId });
+  const { totalSpaceRolesAdded, totalSpaceRolesUpdated } = await syncSummonSpaceRoles({ spaceId, userId });
   if (totalSpaceRolesUpdated !== 0 || totalSpaceRolesAdded !== 0) {
     log.debug(`Space roles sync result`, {
       spaceId,
