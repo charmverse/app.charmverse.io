1cfacf89cb194cc78613936a79576554b387836f
Safwan Shaheer <devorein00@gmail.com>
2023-07-19 19:43:30 +0600

    Always show the user's role for member properties card

--- components/common/UserProfile/components/ProfileWidgets/MemberPropertiesWidget.tsx
+++ components/common/UserProfile/components/ProfileWidgets/MemberPropertiesWidget.tsx
@@ -2,6 +2,7 @@ import type { MemberPropertyType } from '@charmverse/core/prisma-client';
 
 import { useCurrentSpace } from 'hooks/useCurrentSpace';
 import { useMemberProperties } from 'hooks/useMemberProperties';
+import { useMembers } from 'hooks/useMembers';
 
 import { useMemberPropertyValues } from '../../hooks/useMemberPropertyValues';
 import { MemberProperties } from '../MemberProperties';
@@ -11,8 +12,9 @@ import { ProfileWidget } from './ProfileWidget';
 export function MemberPropertiesWidget({ userId }: { userId: string }) {
   const { memberPropertyValues, isLoading } = useMemberPropertyValues(userId);
   const { space } = useCurrentSpace();
-
+  const { members } = useMembers();
   const { getDisplayProperties } = useMemberProperties();
+  const member = members.find((_member) => _member.id === userId);
 
   const visibleProperties = getDisplayProperties('profile');
 
@@ -24,14 +26,28 @@ export function MemberPropertiesWidget({ userId }: { userId: string }) {
     currentSpacePropertyValues?.properties.filter((propertyValue) =>
       visibleProperties.some((prop) => prop.id === propertyValue.memberPropertyId)
     ) ?? []
-  ).filter(
-    (property) =>
-      (Array.isArray(property.value) ? property.value.length !== 0 : !!property.value) &&
-      // These are not shown in member properties, so even if their value exist we don't want to show them
-      !(['bio', 'discord', 'twitter', 'linked_in', 'github', 'timezone'] as MemberPropertyType[]).includes(
-        property.type
-      )
-  );
+  )
+    .map((currentSpaceProperty) => {
+      if (currentSpaceProperty.type === 'role' && member) {
+        return {
+          ...currentSpaceProperty,
+          value: [
+            ...(currentSpaceProperty.value as string[]),
+            member.isAdmin ? 'admin' : member.isGuest ? 'guest' : 'member'
+          ]
+        };
+      }
+
+      return currentSpaceProperty;
+    })
+    .filter(
+      (property) =>
+        (Array.isArray(property.value) ? property.value.length !== 0 : !!property.value) &&
+        // These are not shown in member properties, so even if their value exist we don't want to show them
+        !(['bio', 'discord', 'twitter', 'linked_in', 'github', 'timezone'] as MemberPropertyType[]).includes(
+          property.type
+        )
+    );
 
   return (
     <ProfileWidget
