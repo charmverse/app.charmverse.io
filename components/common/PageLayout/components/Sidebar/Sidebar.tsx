7d70c78cfd6f4e36888079be2139c2bd4d3cbe64
Matt Casey <mattwad@gmail.com>
2023-07-19 23:10:14 -0600

    script to migrate pro to community (#2497)
    * script to migrate pro to community
    
    * fix script
    
    * remove check for pro tier

--- components/common/PageLayout/components/Sidebar/Sidebar.tsx
+++ components/common/PageLayout/components/Sidebar/Sidebar.tsx
@@ -224,7 +224,7 @@ export function Sidebar({ closeSidebar, navAction }: SidebarProps) {
 
           {
             // Don't show block counts for free or entreprise spaces
-            (space?.paidTier === 'community' || space?.paidTier === 'pro') && (
+            space?.paidTier === 'community' && (
               <Box ml={2}>
                 <BlockCounts />
               </Box>
