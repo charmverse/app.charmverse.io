3ce86bdfcdb6e383df27c55119e67cf5b8a17aa8
Safwan Shaheer <devorein00@gmail.com>
2023-07-25 18:31:20 +0600

    Added custom page icon selector for boards

--- components/common/ImageSelector/ImageUploadButton.tsx
+++ components/common/ImageSelector/ImageUploadButton.tsx
@@ -38,7 +38,10 @@ export function ImageUploadButton({
             opacity: 0,
             position: 'absolute',
             top: 0,
-            left: 0
+            left: 0,
+            width: '100%',
+            height: '100%',
+            cursor: 'pointer'
           }}
           accept='image/*'
           onClick={(e) => {
