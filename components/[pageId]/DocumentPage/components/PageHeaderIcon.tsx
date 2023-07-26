ae7385c756d02828d8d89bf87bfb63107e507541
Safwan Shaheer <devorein00@gmail.com>
2023-07-26 17:38:53 +0600

    Custom image upload for page and board icon (#2513)

---
+++ components/[pageId]/DocumentPage/components/PageHeaderIcon.tsx
@@ -0,0 +1,56 @@
+import { Stack, TextField } from '@mui/material';
+import { useState } from 'react';
+
+import EmojiPicker from 'components/common/BoardEditor/focalboard/src/widgets/emojiPicker';
+import Button from 'components/common/Button';
+import { ImageUploadButton } from 'components/common/ImageSelector/ImageUploadButton';
+import MultiTabs from 'components/common/MultiTabs';
+
+export function PageHeaderIcon({ updatePageIcon }: { updatePageIcon: (icon: string) => void }) {
+  const [imageLink, setImageLink] = useState('');
+  return (
+    <MultiTabs
+      tabs={[
+        [
+          'Emojis',
+          <EmojiPicker
+            key='upload'
+            onSelect={(emoji) => {
+              updatePageIcon(emoji);
+            }}
+          />
+        ],
+        [
+          'Custom',
+          <Stack key='custom' spacing={1}>
+            <Stack flexDirection='row' gap={1}>
+              <TextField
+                value={imageLink}
+                onChange={(e) => {
+                  setImageLink(e.target.value);
+                }}
+                placeholder='Paste link to an image ...'
+              />
+              <Button
+                onClick={() => {
+                  updatePageIcon(imageLink);
+                }}
+                disabled={imageLink.length === 0}
+              >
+                Submit
+              </Button>
+            </Stack>
+            <ImageUploadButton
+              variant='outlined'
+              sx={{
+                width: '100%'
+              }}
+              uploadDisclaimer='Recommended size is 280 × 280 pixels'
+              setImage={updatePageIcon}
+            />
+          </Stack>
+        ]
+      ]}
+    />
+  );
+}
--- hooks/useS3UploadInput.ts
+++ hooks/useS3UploadInput.ts
@@ -1,3 +1,4 @@
+import { log } from '@charmverse/core/log';
 import { useState } from 'react';
 
 import { useFilePicker } from 'hooks/useFilePicker';
@@ -9,15 +10,18 @@ export const DEFAULT_MAX_FILE_SIZE_MB = 20;
 export type UploadedFileInfo = { url: string; fileName: string; size?: number };
 export type UploadedFileCallback = (info: UploadedFileInfo) => void;
 
-export const useS3UploadInput = (onFileUpload: UploadedFileCallback) => {
+export const useS3UploadInput = (
+  onFileUpload: UploadedFileCallback,
+  fileSizeLimitMB: number = DEFAULT_MAX_FILE_SIZE_MB
+) => {
   const { showMessage } = useSnackbar();
   const [isUploading, setIsUploading] = useState(false);
   const [progress, setProgress] = useState(0);
   const [fileName, setFileName] = useState('');
 
   async function uploadFile(file: File) {
-    if (file.size > DEFAULT_MAX_FILE_SIZE_MB * 1024 ** 2) {
-      showMessage(`File size must be less than ${DEFAULT_MAX_FILE_SIZE_MB}MB`, 'error');
+    if (file.size > fileSizeLimitMB * 1024 ** 2) {
+      showMessage(`File size must be less than ${fileSizeLimitMB}MB`, 'error');
       return;
     }
 
@@ -28,12 +32,13 @@ export const useS3UploadInput = (onFileUpload: UploadedFileCallback) => {
     try {
       const { url } = await uploadToS3(file, { onUploadPercentageProgress: setProgress });
       onFileUpload({ url, fileName: file.name || '', size: file.size });
-    } catch (e) {
+    } catch (error) {
+      log.error('Error uploading image to s3', { error });
       showMessage('Failed to upload file. Please try again.', 'error');
     } finally {
       setIsUploading(false);
     }
   }
 
-  return { ...useFilePicker(uploadFile), isUploading, progress, fileName, sizeLimit: DEFAULT_MAX_FILE_SIZE_MB };
+  return { ...useFilePicker(uploadFile), isUploading, progress, fileName, sizeLimit: fileSizeLimitMB };
 };
