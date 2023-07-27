116384d8f436063f090caab5487b22da1f05c773
mattcasey <mattwad@gmail.com>
2023-07-27 09:36:16 -0600

    remove unused code

--- components/common/CharmEditor/components/rowActions/rowActions.ts
+++ components/common/CharmEditor/components/rowActions/rowActions.ts
@@ -168,28 +168,7 @@ export function plugins({ key }: { key: PluginKey }) {
           }
         };
       }
-    }),
-    (() => {
-      let focusing = false;
-      return new Plugin({
-        props: {
-          handleDOMEvents: {
-            focus: (view) => {
-              if (focusing) {
-                focusing = false;
-              } else {
-                focusing = true;
-                setTimeout(() => {
-                  view.dom.blur();
-                  view.dom.focus();
-                }, 0);
-              }
-              return false;
-            }
-          }
-        }
-      });
-    })()
+    })
   ];
 }
 
