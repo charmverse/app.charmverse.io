78d3ad19cfb461b00335b3332c047e73f4589795
motechFR <mo@anyahq.co>
2023-08-01 20:35:07 +0300

    Add board node visualiser

--- scripts/visualisePageTree.ts
+++ scripts/visualisePageTree.ts
@@ -2,10 +2,21 @@ import { promises as fs } from 'fs';
 import { createSVGWindow } from 'svgdom';
 import { SVG, registerWindow } from '@svgdotjs/svg.js';
 import { Block, Page, prisma } from '@charmverse/core/prisma-client';
+import { Svg } from '@svgdotjs/svg.js';
 
 const window = createSVGWindow();
 const document = window.document;
 registerWindow(window, document);
+
+const childXOffset = 150;
+const childYOffset = 80;
+
+const viewColor = "#619b8a";
+const boardColor = "#233D4D";
+const cardColor = "#FE7F2D";
+
+const svgWidth = 2000;
+const svgHeight = 3000;
 
 
 type BlockWithChildren = Block & {children?: BlockWithChildren[], page?: Pick<Page, 'id' | 'title' | 'type'>};
@@ -34,7 +45,10 @@ async function loadData(): Promise<BlockWithChildren[]> {
     }
   }).then(pages => pages.reduce((acc, page) => ({ ...acc, [page.id]: page }), {} as Record<string, Pick<Page, 'id' | 'title' | 'type'>>));
 
-  return blocks.map(block => ({ ...block, page: pages[block.id] }));
+  return blocks.map(block => ({ ...block, page: pages[block.id] }))
+  // Order blocks by type
+  .sort((blockA, blockB) => String(blockA.type).charCodeAt(0) < String(blockB.type).charCodeAt(0) ? -1 : 1)
+  .sort((blockA, blockB) => blockA.page?.type.match('linked')  ? -1 : 0);
 }
 
 // Recursive function to create tree
@@ -48,15 +62,35 @@ function createTree(blocks: Block[], parentId: string = ''): Block[] {
 }
 
 // Recursive function to draw SVG tree
-function drawTree(node: BlockWithChildren, draw: any, x: number = 0, y: number = 0): void {
+function drawTree(node: BlockWithChildren, draw: Svg, x: number = 0, y: number = 0): void {
   const group = draw.group();
   group.circle(10).attr({ cx: x, cy: y }); // Draw the node itself
-  group.text((node.page ? `Page: ${node.page.type} // ${node.page.title}\r\n` : '') + `Block: ${node.type}`).move(x, y); // Add the id of the node as text
 
-  const childY = y + 50;
+  let textLabel = 'Block: ' + node.type;
+  if (node.page) {
+    textLabel += '\r\nType: ' + node.page.type + '\r\nTitle: ' + node.page.title;
+  } else if (node.type === 'view') {
+    textLabel += `\r\n${node.title}`;
+    textLabel += `\r\nData:${(node.fields as any).sourceType }`;
+  }
+
+  // Add the id of the node as text with hover interaction
+  const text = group.text(textLabel).move(x - childXOffset / 2, y);
+  // text.attr({
+  //   'onmouseover': 'evt.target.setAttribute("style", "fill: green; background: green; z-index: 1; color: red");',
+  //   'onmouseout': 'evt.target.setAttribute("style", "");',
+  // });
+
+  group.attr({
+    'onmouseover': 'evt.target.setAttribute("style", "fill: blue; background: blue; z-index: 1; color: red; font-size: 20px"); console.log(evt);',
+    'onmouseout': 'evt.target.setAttribute("style", "");',
+  })
+
+
+  const childY = y + childYOffset;
   node.children?.forEach((child, i) => {
-    const childX = x - ((node.children?.length || 0) - 1) * 30 / 2 + i * 30;
-    draw.line(x, y, childX, childY).stroke({ color: '#f06', width: 2 }); // Draw a line to the child node
+    const childX = x - ((node.children?.length || 0) - 1) * childXOffset / 2 + i * childXOffset;
+    draw.line(x, y, childX, childY).stroke({ color: child.type === 'board' ? boardColor : child.type === 'view' ? viewColor : cardColor, width: 4 }); // Draw a line to the child node
     drawTree(child, draw, childX, childY); // Recursive call to draw the child nodes
   });
 }
@@ -71,14 +105,60 @@ async function main() {
 
   // Use SVG.js to draw the tree
   const draw = SVG(document.documentElement);
-  draw.viewbox(0, 0, 500, 500);
+  draw.viewbox(0, 0, svgWidth, svgHeight);
+  draw.attr('preserveAspectRatio', 'xMidYMid meet')
 
   for(let i = 0; i < tree.length; i++) {
-    drawTree(tree[i], draw, i * 100, 50);
+    drawTree(tree[i], draw, childXOffset, i * (childYOffset + 250));
   }
 
-  // Write the SVG to a file
-  await fs.writeFile('tree.svg', draw.svg());
+  let svgContent = draw.svg();
+  let htmlContent = `
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <title>Interactive SVG</title>
+      <style>
+        #container {
+          position: relative;
+          min-width: 500px;
+          min-height: 500px;
+          width: 100%;
+          height: 100%;
+        }
+        svg {
+          top: 0;
+          left: 0;
+          width: ${svgWidth}px;
+          height: ${svgHeight}px;
+        }
+      </style>
+    </head>
+    <body>
+        <div id="container">
+          ${svgContent}
+        </div>
+
+        <script>
+        const container = document.querySelector('#container');
+        const svg = container.querySelector('svg');
+    
+        // Get the aspect ratio of the container and the SVG
+        const containerAspectRatio = container.offsetWidth / container.offsetHeight;
+        const svgAspectRatio = svg.viewBox.baseVal.width / svg.viewBox.baseVal.height;
+    
+        if (containerAspectRatio > svgAspectRatio) {
+          // If the container is wider than the SVG (in terms of aspect ratio), adjust the width of the viewBox
+          svg.viewBox.baseVal.width = svg.viewBox.baseVal.height * containerAspectRatio;
+        } else {
+          // If the container is taller than the SVG (in terms of aspect ratio), adjust the height of the viewBox
+          svg.viewBox.baseVal.height = svg.viewBox.baseVal.width / containerAspectRatio;
+        }
+      </script>
+    </body>
+    </html>`;
+
+  await fs.writeFile('tree.html', htmlContent);
 }
 
 main().catch(err => console.error(err));
