5f83f881b7ec0e654d3ab4e146d481a250ec127d
motechFR <mo@anyahq.co>
2023-08-02 01:19:53 +0300

    Fix app throwing an error on parse

--- lib/prosemirror/checkIsContentEmpty.ts
+++ lib/prosemirror/checkIsContentEmpty.ts
@@ -1,12 +1,18 @@
 import type { PageContent } from 'lib/prosemirror/interfaces';
 
 export function checkIsContentEmpty(content: PageContent | null | undefined) {
+  if (!content) {
+    return true;
+  }
+
   return (
     !content?.content ||
-    content.content.length === 0 ||
-    (content.content.length === 1 &&
+    content?.content?.length === 0 ||
+    (content.content?.length === 1 &&
       // These nodes dont contain any content so there is no content field
-      !content.content[0]?.type.match(/(cryptoPrice|columnLayout|image|iframe|mention|page|poll|tableOfContents)/) &&
-      !content.content[0].content?.length)
+      !String(content.content?.[0]?.type).match(
+        /(cryptoPrice|columnLayout|image|iframe|mention|page|poll|tableOfContents)/
+      ) &&
+      !content?.content?.[0]?.content?.length)
   );
 }
