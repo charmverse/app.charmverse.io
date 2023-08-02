8b446f820ed089e495646371b7de67c66709f216
Safwan Shaheer <devorein00@gmail.com>
2023-08-02 19:57:34 +0600

    Made formidable work without writing to disk

--- pages/api/image/resize.ts
+++ pages/api/image/resize.ts
@@ -1,15 +1,12 @@
-import fs from 'fs/promises';
-import { join } from 'path';
+import { Writable } from 'stream';
 
 import formidable from 'formidable';
-import { DateTime } from 'luxon';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import nc from 'next-connect';
 import sharp from 'sharp';
 
 import { getUserS3FilePath, uploadFileToS3 } from 'lib/aws/uploadToS3Server';
 import { DEFAULT_IMAGE_SIZE, DEFAULT_MAX_FILE_SIZE_MB, FORM_DATA_FILE_PART_NAME } from 'lib/file/constants';
-import { makeDirectory } from 'lib/file/makeDirectory';
 import { onError, onNoMatch, requireUser } from 'lib/middleware';
 import { withSessionRoute } from 'lib/session/withSession';
 
@@ -18,12 +15,18 @@ const handler = nc<NextApiRequest, NextApiResponse>({ onError, onNoMatch });
 handler.use(requireUser).post(resizeImage);
 
 async function resizeImage(req: NextApiRequest, res: NextApiResponse) {
-  const uploadDir = join(process.env.ROOT_DIR || process.cwd(), `/uploads/${DateTime.now().toFormat('dd-MM-yyyy')}`);
   const userId = req.session.user.id;
-  await makeDirectory(uploadDir);
+  const chunks: Buffer[] = [];
+
+  const writable = new Writable({
+    write: (chunk, _enc, next) => {
+      chunks.push(chunk);
+      next();
+    }
+  });
 
   const form = formidable({
-    uploadDir,
+    fileWriteStreamHandler: () => writable,
     maxFileSize: DEFAULT_MAX_FILE_SIZE_MB * 1024 * 1024,
     filter: (part) => {
       return part.name === FORM_DATA_FILE_PART_NAME && (part.mimetype?.includes('image') || false);
@@ -34,16 +37,13 @@ async function resizeImage(req: NextApiRequest, res: NextApiResponse) {
     const image = Array.isArray(files[FORM_DATA_FILE_PART_NAME])
       ? files[FORM_DATA_FILE_PART_NAME][0]
       : files.uploadedFile;
-    const optimizedBuffer = await sharp(image.filepath)
+    const optimizedBuffer = await sharp(Buffer.concat(chunks))
       .resize({
         withoutEnlargement: true,
         width: DEFAULT_IMAGE_SIZE
       })
       .webp({ quality: 80 })
       .toBuffer();
-
-    // Delete the temporary file
-    await fs.unlink(image.filepath);
 
     const { fileUrl: url } = await uploadFileToS3({
       pathInS3: getUserS3FilePath({ userId, url: image.originalFilename ?? image.newFilename }),
