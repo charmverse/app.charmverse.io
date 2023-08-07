6b48dabb5278bd94fc217a6340311ddd6d2cb8fe
Marek <m.sadura@outlook.com>
2023-08-07 11:51:07 +0200

    Move test files

---
+++ __e2e__/specs/memberOnboarding.spec.ts
@@ -0,0 +1,31 @@
+import { expect, test } from '@playwright/test';
+
+import { baseUrl } from 'config/constants';
+
+import { generateUserAndSpace } from '../utils/mocks';
+import { login } from '../utils/session';
+
+test.describe.serial('Add a new space from sidebar and load it', async () => {
+  test('Fill the form and create a new space', async ({ page }) => {
+    const { space, user } = await generateUserAndSpace({
+      skipOnboarding: false
+    });
+
+    await login({ page, userId: user.id });
+
+    const domain = space.domain;
+    const targetPage = `${baseUrl}/${domain}`;
+
+    await page.goto(targetPage);
+    await page.waitForURL(targetPage);
+
+    const memberProfileNftList = page.locator('data-test=member-profile-nft-list');
+    const memberProfilePoapList = page.locator('data-test=member-profile-poap-list');
+    await expect(memberProfileNftList).toBeVisible();
+    await expect(memberProfilePoapList).toBeVisible();
+
+    const closePropertiesModalBtn = page.locator('data-test=close-modal');
+    await expect(closePropertiesModalBtn).toBeVisible();
+    await closePropertiesModalBtn.click();
+  });
+});
