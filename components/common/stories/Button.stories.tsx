dcb9bac610d60245e565f5ff0793e667243ddf10
yiwork Ying-Chi Connie I <cloudme@halfpast9.com>
2023-08-09 11:09:15 -0400

    adding component changes to test chromatic

--- components/common/stories/Button.stories.tsx
+++ components/common/stories/Button.stories.tsx
@@ -19,7 +19,7 @@ export function Primary() {
           <Button color='primary' size='large' variant='contained'>
             Contained large
           </Button>
-          <Button color='secondary' variant='contained'>
+          <Button color='primary' variant='contained'>
             Contained
           </Button>
           <Button color='success' variant='contained'>
