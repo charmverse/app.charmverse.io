9b0e812f61828a9c72453ce6ac02c53b698a59f9
Safwan Shaheer <devorein00@gmail.com>
2023-08-10 18:14:13 +0600

    Add test case for Delete linked pages from page content when parent document is not being viewed

--- lib/websockets/__tests__/spaceEvent.spec.ts
+++ lib/websockets/__tests__/spaceEvent.spec.ts
@@ -59,6 +59,7 @@ describe('page_delete event handler', () => {
 
     const documentEvent = new DocumentEventHandler(
       {
+        emit() {},
         data: {
           documentId: parentPage.id,
           user,
@@ -72,6 +73,71 @@ describe('page_delete event handler', () => {
 
     const parentPageDocRoom = docRooms.get(parentPage.id) as DocumentRoom;
     parentPageDocRoom.participants.set(userId, documentEvent);
+
+    const spaceEventHandler = new SpaceEventHandler({} as any, docRooms);
+    spaceEventHandler.userId = userId;
+
+    await spaceEventHandler.onMessage(message);
+
+    const parentPageWithContent = await prisma.page.findUniqueOrThrow({
+      where: {
+        id: parentPage.id
+      },
+      select: {
+        content: true
+      }
+    });
+
+    expect(parentPageWithContent.content).toStrictEqual({
+      type: 'doc',
+      content: [
+        {
+          type: 'paragraph',
+          content: [{ type: 'text', text: 'Text content 1' }],
+          attrs: {
+            track: []
+          }
+        },
+        {
+          type: 'paragraph',
+          content: [{ type: 'text', text: 'Text content 2' }],
+          attrs: {
+            track: []
+          }
+        }
+      ]
+    });
+  });
+
+  it(`Delete linked pages from page content when parent document is not being viewed`, async () => {
+    const { space, user } = await testUtilsUser.generateUserAndSpace({ isAdmin: true });
+    const spaceId = space.id;
+    const userId = user.id;
+    const childPageId = v4();
+    const parentPageDoc: PageContent = {
+      type: 'doc',
+      content: [
+        { type: 'paragraph', content: [{ type: 'text', text: 'Text content 1' }] },
+        { type: 'page', attrs: { id: childPageId } },
+        { type: 'paragraph', content: [{ type: 'text', text: 'Text content 2' }] }
+      ]
+    };
+    const parentPage = await testUtilsPages.generatePage({ spaceId, createdBy: userId, content: parentPageDoc });
+    const message: ClientMessage = {
+      type: 'page_deleted',
+      payload: {
+        id: childPageId
+      }
+    };
+
+    await testUtilsPages.generatePage({
+      id: childPageId,
+      spaceId,
+      createdBy: userId,
+      parentId: parentPage.id
+    });
+
+    const docRooms: Map<string | undefined, DocumentRoom> = new Map();
 
     const spaceEventHandler = new SpaceEventHandler({} as any, docRooms);
     spaceEventHandler.userId = userId;
