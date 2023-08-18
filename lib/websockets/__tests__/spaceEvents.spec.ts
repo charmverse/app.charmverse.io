51cf059728d6aefaca70135065c4a6cfcb952f2c
Safwan Shaheer <devorein00@gmail.com>
2023-08-18 17:54:27 +0600

    Added tests for page_reordered space event

--- lib/websockets/__tests__/spaceEvents.spec.ts
+++ lib/websockets/__tests__/spaceEvents.spec.ts
@@ -1,5 +1,6 @@
+import type { Page, User } from '@charmverse/core/prisma-client';
 import { prisma } from '@charmverse/core/prisma-client';
-import { testUtilsUser, testUtilsPages } from '@charmverse/core/test';
+import { testUtilsPages, testUtilsUser } from '@charmverse/core/test';
 import { v4 } from 'uuid';
 
 import { getPagePath } from 'lib/pages';
@@ -20,28 +21,36 @@ jest.mock('../relay', () => {
   };
 });
 
-async function socketSetup({
+async function createPageAndSetupDocRooms({
   participants = false,
-  content
+  content,
+  docRooms,
+  spaceId,
+  user,
+  createChildPage = true
 }: {
+  createChildPage?: boolean;
+  spaceId: string;
+  user: User;
   participants?: boolean;
   content: (childPageId: string) => PageContent;
+  docRooms: Map<string | undefined, DocumentRoom>;
 }) {
-  const { space, user } = await testUtilsUser.generateUserAndSpace({ isAdmin: true });
-  const spaceId = space.id;
   const userId = user.id;
   const childPageId = v4();
   const parentContent = content(childPageId);
   const parentPage = await testUtilsPages.generatePage({ spaceId, createdBy: userId, content: parentContent });
   const socketEmitMockFn = jest.fn();
-  await testUtilsPages.generatePage({
-    id: childPageId,
-    spaceId,
-    createdBy: userId,
-    parentId: parentPage.id
-  });
+  let childPage: Page | null = null;
 
-  const docRooms: Map<string | undefined, DocumentRoom> = new Map();
+  if (createChildPage) {
+    childPage = await testUtilsPages.generatePage({
+      id: childPageId,
+      spaceId,
+      createdBy: userId,
+      parentId: parentPage.id
+    });
+  }
 
   if (participants) {
     docRooms.set(parentPage.id, {
@@ -78,21 +87,57 @@ async function socketSetup({
     parentPageDocRoom.participants.set(userId, documentEvent);
   }
 
+  return {
+    docRooms,
+    socketEmitMockFn,
+    childPageId,
+    parentPage,
+    childPage
+  };
+}
+
+async function socketSetup({
+  participants = false,
+  createChildPage = true,
+  content
+}: {
+  createChildPage?: boolean;
+  participants?: boolean;
+  content: (childPageId: string) => PageContent;
+}) {
+  const { space, user } = await testUtilsUser.generateUserAndSpace({ isAdmin: true });
+
+  const spaceId = space.id;
+  const userId = user.id;
+
+  const { docRooms, childPage, childPageId, parentPage, socketEmitMockFn } = await createPageAndSetupDocRooms({
+    createChildPage,
+    participants,
+    content,
+    docRooms: new Map(),
+    spaceId,
+    user
+  });
+
   const spaceEventHandler = new SpaceEventHandler({} as any, docRooms);
   spaceEventHandler.userId = userId;
 
   return {
+    docRooms,
     socketEmitMockFn,
     spaceId,
     userId,
+    space,
+    user,
     childPageId,
     parentPage,
-    spaceEventHandler
+    spaceEventHandler,
+    childPage
   };
 }
 
-describe.only('page_delete event handler', () => {
-  it.only(`Archive nested pages and remove it from parent document's content when it have the nested page in its content and it is being viewed`, async () => {
+describe('page_delete event handler', () => {
+  it(`Archive nested pages and remove it from parent document's content when it have the nested page in its content and it is being viewed`, async () => {
     const { socketEmitMockFn, childPageId, spaceEventHandler, parentPage } = await socketSetup({
       participants: true,
       content: (_childPageId) => ({
@@ -632,5 +677,392 @@ describe('page_created event handler', () => {
     expect(childPage).toBeTruthy();
     expect(socketEmitMockFn).toHaveBeenCalled();
     expect(relayBroadCastMock).toHaveBeenCalled();
+  });
+});
+
+describe.only('page_reordered event handler', () => {
+  it(`Move page from one parent page to another parent page when both documents are being viewed`, async () => {
+    const { spaceEventHandler, socketEmitMockFn, parentPage, childPageId, docRooms, space, user } = await socketSetup({
+      participants: true,
+      content: (_childPageId) => ({
+        type: 'doc',
+        content: [
+          { type: 'paragraph', content: [{ type: 'text', text: 'Text content 1' }] },
+          {
+            type: 'page',
+            attrs: {
+              track: [],
+              id: _childPageId
+            }
+          },
+          { type: 'paragraph', content: [{ type: 'text', text: 'Text content 2' }] }
+        ]
+      })
+    });
+
+    const { parentPage: parentPage2, socketEmitMockFn: socketEmitMockFn2 } = await createPageAndSetupDocRooms({
+      docRooms,
+      spaceId: space.id,
+      user,
+      participants: true,
+      content: () => ({
+        type: 'doc',
+        content: [
+          { type: 'paragraph', content: [{ type: 'text', text: 'Text content 1' }] },
+          { type: 'paragraph', content: [{ type: 'text', text: 'Text content 2' }] }
+        ]
+      })
+    });
+
+    await spaceEventHandler.onMessage({
+      type: 'page_reordered',
+      payload: {
+        currentParentId: parentPage.id,
+        newIndex: 1,
+        newParentId: parentPage2.id,
+        pageId: childPageId
+      }
+    });
+
+    const previousParentPage = await prisma.page.findUniqueOrThrow({
+      where: {
+        id: parentPage.id
+      },
+      select: {
+        content: true
+      }
+    });
+
+    const newParentPage = await prisma.page.findUniqueOrThrow({
+      where: {
+        id: parentPage2.id
+      },
+      select: {
+        content: true
+      }
+    });
+
+    expect(previousParentPage.content).toStrictEqual({
+      type: 'doc',
+      content: [
+        {
+          type: 'paragraph',
+          content: [{ type: 'text', text: 'Text content 1' }],
+          attrs: {
+            track: []
+          }
+        },
+        {
+          type: 'paragraph',
+          content: [{ type: 'text', text: 'Text content 2' }],
+          attrs: {
+            track: []
+          }
+        }
+      ]
+    });
+
+    expect(newParentPage.content).toStrictEqual({
+      type: 'doc',
+      content: [
+        {
+          type: 'paragraph',
+          content: [{ type: 'text', text: 'Text content 1' }],
+          attrs: {
+            track: []
+          }
+        },
+        {
+          type: 'paragraph',
+          content: [{ type: 'text', text: 'Text content 2' }],
+          attrs: {
+            track: []
+          }
+        },
+        {
+          type: 'page',
+          attrs: {
+            track: [],
+            id: childPageId,
+            path: null,
+            type: null
+          }
+        },
+        {
+          type: 'paragraph',
+          attrs: {
+            track: []
+          }
+        }
+      ]
+    });
+    expect(socketEmitMockFn).toHaveBeenCalled();
+    expect(socketEmitMockFn2).toHaveBeenCalled();
+  });
+
+  it(`Move page from one parent page to another parent page (with children) when none of the documents are being viewed`, async () => {
+    const { spaceEventHandler, parentPage, childPageId, docRooms, space, user, socketEmitMockFn } = await socketSetup({
+      content: (_childPageId) => ({
+        type: 'doc',
+        content: [
+          { type: 'paragraph', content: [{ type: 'text', text: 'Text content 1' }] },
+          {
+            type: 'page',
+            attrs: {
+              track: [],
+              id: _childPageId
+            }
+          },
+          { type: 'paragraph', content: [{ type: 'text', text: 'Text content 2' }] }
+        ]
+      })
+    });
+
+    const {
+      parentPage: parentPage2,
+      childPageId: childPageId2,
+      socketEmitMockFn: socketEmitMockFn2
+    } = await createPageAndSetupDocRooms({
+      docRooms,
+      spaceId: space.id,
+      user,
+      content: (_childPageId) => ({
+        type: 'doc',
+        content: [
+          { type: 'paragraph', content: [{ type: 'text', text: 'Text content 1' }] },
+          {
+            type: 'page',
+            attrs: {
+              track: [],
+              id: _childPageId
+            }
+          },
+          { type: 'paragraph', content: [{ type: 'text', text: 'Text content 2' }] }
+        ]
+      })
+    });
+
+    await spaceEventHandler.onMessage({
+      type: 'page_reordered',
+      payload: {
+        currentParentId: parentPage.id,
+        newIndex: 1,
+        newParentId: parentPage2.id,
+        pageId: childPageId
+      }
+    });
+
+    const previousParentPage = await prisma.page.findUniqueOrThrow({
+      where: {
+        id: parentPage.id
+      },
+      select: {
+        content: true
+      }
+    });
+
+    const newParentPage = await prisma.page.findUniqueOrThrow({
+      where: {
+        id: parentPage2.id
+      },
+      select: {
+        content: true
+      }
+    });
+
+    expect(previousParentPage.content).toStrictEqual({
+      type: 'doc',
+      content: [
+        {
+          type: 'paragraph',
+          content: [{ type: 'text', text: 'Text content 1' }],
+          attrs: {
+            track: []
+          }
+        },
+        {
+          type: 'paragraph',
+          content: [{ type: 'text', text: 'Text content 2' }],
+          attrs: {
+            track: []
+          }
+        }
+      ]
+    });
+
+    expect(newParentPage.content).toStrictEqual({
+      type: 'doc',
+      content: [
+        {
+          type: 'paragraph',
+          content: [{ type: 'text', text: 'Text content 1' }],
+          attrs: {
+            track: []
+          }
+        },
+        {
+          type: 'page',
+          attrs: {
+            type: null,
+            path: null,
+            track: [],
+            id: childPageId2
+          }
+        },
+        {
+          type: 'paragraph',
+          content: [{ type: 'text', text: 'Text content 2' }],
+          attrs: {
+            track: []
+          }
+        },
+        {
+          type: 'page',
+          attrs: {
+            track: [],
+            id: childPageId,
+            path: null,
+            type: null
+          }
+        },
+        {
+          type: 'paragraph',
+          attrs: {
+            track: []
+          }
+        }
+      ]
+    });
+    expect(socketEmitMockFn).not.toHaveBeenCalled();
+    expect(socketEmitMockFn2).not.toHaveBeenCalled();
+  });
+
+  it(`Move page from parent page to root level the parent document is being viewed`, async () => {
+    const { spaceEventHandler, parentPage, childPageId, socketEmitMockFn } = await socketSetup({
+      participants: true,
+      content: (_childPageId) => ({
+        type: 'doc',
+        content: [
+          { type: 'paragraph', content: [{ type: 'text', text: 'Text content 1' }] },
+          {
+            type: 'page',
+            attrs: {
+              track: [],
+              id: _childPageId
+            }
+          },
+          { type: 'paragraph', content: [{ type: 'text', text: 'Text content 2' }] }
+        ]
+      })
+    });
+
+    await spaceEventHandler.onMessage({
+      type: 'page_reordered',
+      payload: {
+        currentParentId: parentPage.id,
+        newIndex: 1,
+        newParentId: null,
+        pageId: childPageId
+      }
+    });
+
+    const previousParentPage = await prisma.page.findUniqueOrThrow({
+      where: {
+        id: parentPage.id
+      },
+      select: {
+        content: true
+      }
+    });
+
+    expect(previousParentPage.content).toStrictEqual({
+      type: 'doc',
+      content: [
+        {
+          type: 'paragraph',
+          content: [{ type: 'text', text: 'Text content 1' }],
+          attrs: {
+            track: []
+          }
+        },
+        {
+          type: 'paragraph',
+          content: [{ type: 'text', text: 'Text content 2' }],
+          attrs: {
+            track: []
+          }
+        }
+      ]
+    });
+
+    expect(socketEmitMockFn).toHaveBeenCalled();
+  });
+
+  it(`Move page from root level another parent when the parent document is not being viewed`, async () => {
+    const { spaceEventHandler, parentPage, childPageId, socketEmitMockFn } = await socketSetup({
+      content: () => ({
+        type: 'doc',
+        content: [
+          { type: 'paragraph', content: [{ type: 'text', text: 'Text content 1' }] },
+          { type: 'paragraph', content: [{ type: 'text', text: 'Text content 2' }] }
+        ]
+      })
+    });
+
+    await spaceEventHandler.onMessage({
+      type: 'page_reordered',
+      payload: {
+        currentParentId: null,
+        newIndex: 1,
+        newParentId: parentPage.id,
+        pageId: childPageId
+      }
+    });
+
+    const newParentPage = await prisma.page.findUniqueOrThrow({
+      where: {
+        id: parentPage.id
+      },
+      select: {
+        content: true
+      }
+    });
+
+    expect(newParentPage.content).toStrictEqual({
+      type: 'doc',
+      content: [
+        {
+          type: 'paragraph',
+          content: [{ type: 'text', text: 'Text content 1' }],
+          attrs: {
+            track: []
+          }
+        },
+        {
+          type: 'paragraph',
+          content: [{ type: 'text', text: 'Text content 2' }],
+          attrs: {
+            track: []
+          }
+        },
+        {
+          type: 'page',
+          attrs: {
+            track: [],
+            id: childPageId,
+            path: null,
+            type: null
+          }
+        },
+        {
+          type: 'paragraph',
+          attrs: {
+            track: []
+          }
+        }
+      ]
+    });
+
+    expect(socketEmitMockFn).not.toHaveBeenCalled();
   });
 });
