091505826c9bb1390c9ee01458afa3212406c55b
mattcasey <mattwad@gmail.com>
2023-08-22 20:15:20 -0600

    mock out method to get NFTs in tests to silence error

--- jest.setup.ts
+++ jest.setup.ts
@@ -6,10 +6,16 @@ afterAll(() => {
   prisma.$disconnect();
 });
 
+// Mock external requests globally
+
 jest.mock('lib/blockchain/getENSName', () => ({
   ...jest.requireActual('lib/blockchain/getENSName'),
   __esModule: true,
-  default: jest.fn().mockImplementation(() => {
-    return Promise.resolve(null);
-  })
+  default: jest.fn().mockImplementation(() => Promise.resolve(null))
+}));
+
+jest.mock('lib/alchemy/getNFTs', () => ({
+  ...jest.requireActual('lib/alchemy/getNFTs'),
+  __esModule: true,
+  getNFTs: jest.fn().mockImplementation(() => Promise.resolve([]))
 }));
