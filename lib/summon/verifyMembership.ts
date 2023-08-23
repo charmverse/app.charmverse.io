698c2045475e013b62df4cf186fac3bdacd65046
Safwan Shaheer <devorein00@gmail.com>
2023-08-23 15:57:34 +0600

    Fixed type issues

--- lib/summon/verifyMembership.ts
+++ lib/summon/verifyMembership.ts
@@ -57,7 +57,7 @@ export async function verifyMembership({
       });
       // check another user isnt already using this Summon account
       if (summonUserId) {
-        const existing = await prisma.user.findUnique({
+        const existing = await prisma.user.findFirst({
           where: {
             xpsEngineId: summonUserId
           }
--- testing/mocks/user.ts
+++ testing/mocks/user.ts
@@ -6,6 +6,7 @@ import type { LoggedInUser } from 'models';
 export function createMockUser(user?: Partial<LoggedInUser>): LoggedInUser {
   const id = uuid();
   return {
+    autoLensPublish: false,
     id: uuid(),
     avatar: null,
     avatarChain: null,
