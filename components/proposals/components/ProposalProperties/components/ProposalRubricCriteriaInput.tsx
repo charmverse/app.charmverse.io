ddb36d1644e1f3c4dd9bd77c08473c8729ed172c
Valentin <ludu.valentin@gmail.com>
2023-08-24 01:21:13 +0300

    Drag and drop rubric criteria (#2666)

--- components/proposals/components/ProposalProperties/components/ProposalRubricCriteriaInput.tsx
+++ components/proposals/components/ProposalProperties/components/ProposalRubricCriteriaInput.tsx
@@ -1,6 +1,7 @@
 import type { ProposalRubricCriteriaAnswer, ProposalStatus } from '@charmverse/core/prisma-client';
 import styled from '@emotion/styled';
 import { CloseOutlined as DeleteIcon } from '@mui/icons-material';
+import MenuIcon from '@mui/icons-material/Menu';
 import { Box, Grid, IconButton, Tooltip, Typography } from '@mui/material';
 import { useEffect, useState } from 'react';
 import { v4 as uuid } from 'uuid';
@@ -8,6 +9,7 @@ import { v4 as uuid } from 'uuid';
 import { AddAPropertyButton } from 'components/common/BoardEditor/components/properties/AddAProperty';
 import { TextInput } from 'components/common/BoardEditor/components/properties/TextInput';
 import ConfirmDeleteModal from 'components/common/Modal/ConfirmDeleteModal';
+import DraggableListItem from 'components/common/PageLayout/components/DraggableListItem';
 import type { ProposalRubricCriteriaAnswerWithTypedResponse } from 'lib/proposal/rubric/interfaces';
 import { getNumberFromString } from 'lib/utilities/numbers';
 
@@ -111,92 +113,110 @@ export function ProposalRubricCriteriaInput({ readOnly, value, onChange, proposa
       deleteCriteria(criteriaId);
     }
   }
+  async function changeOptionsOrder(draggedProperty: string, droppedOnProperty: string) {
+    const newOrder = [...value];
+    const propIndex = newOrder.findIndex((val) => val.id === draggedProperty); // find the property that was dragged
+    const deletedElements = newOrder.splice(propIndex, 1); // remove the dragged property from the array
+    const droppedOnIndex = newOrder.findIndex((val) => val.id === droppedOnProperty); // find the index of the space that was dropped on
+    newOrder.splice(droppedOnIndex, 0, deletedElements[0]); // add the property to the new index
+    setCriteriaList(newOrder);
+  }
 
   return (
     <>
       {criteriaList.map((criteria) => (
-        <Box key={criteria.id} display='flex' flexDirection='column'>
-          <CriteriaRow display='flex' alignItems='flex-start' gap={1} mb={1}>
-            <TextInput
-              inputProps={{ autoFocus: true }}
-              displayType='details'
-              fullWidth={false}
-              onChange={(title) => setCriteriaProperty(criteria.id, { title })}
-              placeholderText='Add a label...'
-              readOnly={readOnly}
-              value={criteria.title}
-            />
-            <TextInput
-              displayType='details'
-              multiline={true}
-              onChange={(description) => setCriteriaProperty(criteria.id, { description })}
-              placeholderText='Add a description...'
-              readOnly={readOnly}
-              sx={{ flexGrow: 1, width: '100%' }}
-              value={criteria.description ?? ''}
-            />
-            <Box display='flex' gap={1} alignItems='flex-start'>
-              <Grid container width={90} spacing={1}>
-                <Grid xs item>
-                  <div>
-                    <IntegerInput
-                      onChange={(min) => {
-                        setCriteriaProperty(criteria.id, {
-                          parameters: { ...criteria.parameters, min }
-                        });
-                      }}
-                      readOnly={readOnly}
-                      value={criteria.parameters.min}
-                    />
-                    <Typography
-                      align='center'
-                      component='div'
-                      className='show-on-hover'
-                      color='secondary'
-                      variant='caption'
-                    >
-                      min
-                    </Typography>
+        <DraggableListItem
+          key={criteria.id}
+          name='rubric-option'
+          itemId={criteria.id}
+          changeOrderHandler={changeOptionsOrder}
+        >
+          <Box display='flex'>
+            <MenuIcon fontSize='small' sx={{ mt: '7px' }} />
+            <Box key={criteria.id} display='flex' flexDirection='column'>
+              <CriteriaRow display='flex' alignItems='flex-start' gap={1} mb={1}>
+                <TextInput
+                  inputProps={{ autoFocus: true }}
+                  displayType='details'
+                  fullWidth={false}
+                  onChange={(title) => setCriteriaProperty(criteria.id, { title })}
+                  placeholderText='Add a label...'
+                  readOnly={readOnly}
+                  value={criteria.title}
+                />
+                <TextInput
+                  displayType='details'
+                  multiline={true}
+                  onChange={(description) => setCriteriaProperty(criteria.id, { description })}
+                  placeholderText='Add a description...'
+                  readOnly={readOnly}
+                  sx={{ flexGrow: 1, width: '100%' }}
+                  value={criteria.description ?? ''}
+                />
+                <Box display='flex' gap={1} alignItems='flex-start'>
+                  <Grid container width={90} spacing={1}>
+                    <Grid xs item>
+                      <div>
+                        <IntegerInput
+                          onChange={(min) => {
+                            setCriteriaProperty(criteria.id, {
+                              parameters: { ...criteria.parameters, min }
+                            });
+                          }}
+                          readOnly={readOnly}
+                          value={criteria.parameters.min}
+                        />
+                        <Typography
+                          align='center'
+                          component='div'
+                          className='show-on-hover'
+                          color='secondary'
+                          variant='caption'
+                        >
+                          min
+                        </Typography>
+                      </div>
+                    </Grid>
+                    <Grid xs item>
+                      <div className='to-pseudo-element'>
+                        <IntegerInput
+                          inputProps={{
+                            min: typeof criteria.parameters.min === 'number' ? criteria.parameters.min + 1 : undefined
+                          }}
+                          onChange={(max) => {
+                            setCriteriaProperty(criteria.id, {
+                              parameters: { ...criteria.parameters, max }
+                            });
+                          }}
+                          readOnly={readOnly}
+                          value={criteria.parameters.max}
+                        />
+                        <Typography
+                          align='center'
+                          component='div'
+                          className='show-on-hover'
+                          variant='caption'
+                          color='secondary'
+                        >
+                          max
+                        </Typography>
+                      </div>
+                    </Grid>
+                  </Grid>
+                </Box>
+                {!readOnly && (
+                  <div className='show-on-hover delete-icon'>
+                    <Tooltip title='Delete'>
+                      <IconButton size='small' onClick={() => handleClickDelete(criteria.id)}>
+                        <DeleteIcon color='secondary' fontSize='small' />
+                      </IconButton>
+                    </Tooltip>
                   </div>
-                </Grid>
-                <Grid xs item>
-                  <div className='to-pseudo-element'>
-                    <IntegerInput
-                      inputProps={{
-                        min: typeof criteria.parameters.min === 'number' ? criteria.parameters.min + 1 : undefined
-                      }}
-                      onChange={(max) => {
-                        setCriteriaProperty(criteria.id, {
-                          parameters: { ...criteria.parameters, max }
-                        });
-                      }}
-                      readOnly={readOnly}
-                      value={criteria.parameters.max}
-                    />
-                    <Typography
-                      align='center'
-                      component='div'
-                      className='show-on-hover'
-                      variant='caption'
-                      color='secondary'
-                    >
-                      max
-                    </Typography>
-                  </div>
-                </Grid>
-              </Grid>
+                )}
+              </CriteriaRow>
             </Box>
-            {!readOnly && (
-              <div className='show-on-hover delete-icon'>
-                <Tooltip title='Delete'>
-                  <IconButton size='small' onClick={() => handleClickDelete(criteria.id)}>
-                    <DeleteIcon color='secondary' fontSize='small' />
-                  </IconButton>
-                </Tooltip>
-              </div>
-            )}
-          </CriteriaRow>
-        </Box>
+          </Box>
+        </DraggableListItem>
       ))}
       {!readOnly && (
         <ConfirmDeleteModal
