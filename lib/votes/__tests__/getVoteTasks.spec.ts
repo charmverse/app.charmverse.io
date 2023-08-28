e19890fa03097d2811d5e8efa2c9e9ee6bc1ea8b
motechFR <mo@anyahq.co>
2023-08-28 18:24:33 +0300

    Add red test

--- lib/votes/__tests__/getVoteTasks.spec.ts
+++ lib/votes/__tests__/getVoteTasks.spec.ts
@@ -1,20 +1,28 @@
+import { testUtilsPages, testUtilsUser } from '@charmverse/core/test';
+
 import { createPage, createVote, generateUserAndSpaceWithApiToken } from 'testing/setupDatabase';
 
 import { getVoteTasks } from '../getVoteTasks';
 
 describe('getVoteTasks', () => {
-  it('should get votes tasks for a user', async () => {
-    const { space, user } = await generateUserAndSpaceWithApiToken(undefined, false);
+  it('should get votes tasks for a user only in the pages they have access to', async () => {
+    const { space, user: adminUser } = await testUtilsUser.generateUserAndSpace({ isAdmin: true });
 
-    const page = await createPage({
-      createdBy: user.id,
-      spaceId: space.id
+    const visiblePage = await testUtilsPages.generatePage({
+      createdBy: adminUser.id,
+      spaceId: space.id,
+      pagePermissions: [
+        {
+          permissionLevel: 'full_access',
+          assignee: { group: 'space', id: space.id }
+        }
+      ]
     });
 
     // Not included as the vote has been cancelled
-    await createVote({
-      pageId: page.id,
-      createdBy: user.id,
+    const invisibleVote = await createVote({
+      pageId: visiblePage.id,
+      createdBy: adminUser.id,
       spaceId: space.id,
       voteOptions: ['1', '2'],
       status: 'Cancelled',
@@ -22,9 +30,9 @@ describe('getVoteTasks', () => {
     });
 
     // Included even if the vote is past deadline
-    await createVote({
-      pageId: page.id,
-      createdBy: user.id,
+    const inProgressPastDeadlineVote = await createVote({
+      pageId: visiblePage.id,
+      createdBy: adminUser.id,
       spaceId: space.id,
       voteOptions: ['a', 'b'],
       status: 'InProgress',
@@ -32,9 +40,9 @@ describe('getVoteTasks', () => {
     });
 
     // Included since we show votes that have been casted
-    await createVote({
-      pageId: page.id,
-      createdBy: user.id,
+    const inProgressVote = await createVote({
+      pageId: visiblePage.id,
+      createdBy: adminUser.id,
       spaceId: space.id,
       voteOptions: ['a', 'b'],
       status: 'InProgress',
@@ -43,16 +51,38 @@ describe('getVoteTasks', () => {
     });
 
     const createdVote = await createVote({
-      pageId: page.id,
-      createdBy: user.id,
+      pageId: visiblePage.id,
+      createdBy: adminUser.id,
       spaceId: space.id,
       voteOptions: ['a', 'b'],
       deadline: new Date(Date.now() + 24 * 60 * 60 * 1000)
     });
 
-    const votes = await getVoteTasks(user.id);
+    const hiddenPage = await createPage({
+      createdBy: adminUser.id,
+      spaceId: space.id
+    });
+    // Same vote conditions as above, but on a page the user cannot see
+    const hiddenPageCreatedVote = await createVote({
+      pageId: hiddenPage.id,
+      createdBy: adminUser.id,
+      spaceId: space.id,
+      voteOptions: ['a', 'b'],
+      deadline: new Date(Date.now() + 24 * 60 * 60 * 1000)
+    });
 
-    expect(votes.unmarked[1].id).toBe(createdVote.id);
-    expect(votes.unmarked.length).toBe(3);
+    const spaceMember = await testUtilsUser.generateSpaceUser({ spaceId: space.id });
+
+    const votes = await getVoteTasks(spaceMember.id);
+
+    expect(votes.unmarked).toHaveLength(3);
+
+    expect(votes.unmarked).toMatchObject(
+      expect.arrayContaining([
+        expect.objectContaining({ id: createdVote.id }),
+        expect.objectContaining({ id: inProgressPastDeadlineVote.id }),
+        expect.objectContaining({ id: inProgressVote.id })
+      ])
+    );
   });
 });
