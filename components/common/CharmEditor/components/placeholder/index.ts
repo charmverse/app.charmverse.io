c4b3df691d14164dda3e89fc009a8084f7f43e6d
mattcasey <mattwad@gmail.com>
2023-09-05 15:41:23 -0600

    .

--- components/common/CharmEditor/components/placeholder/index.ts
+++ components/common/CharmEditor/components/placeholder/index.ts
@@ -6,13 +6,11 @@ import { Decoration, DecorationSet } from 'prosemirror-view';
 import { checkIsContentEmpty } from 'lib/prosemirror/checkIsContentEmpty';
 
 const defaultPlaceholderText = "Type '/' for commands";
-
 // source: https://gist.github.com/amk221/1f9657e92e003a3725aaa4cf86a07cc0
 // see also: https://discuss.prosemirror.net/t/how-to-input-like-placeholder-behavior/705/24
 export function placeholderPlugin(text: string = defaultPlaceholderText) {
   const update = (view: EditorView) => {
     const doc = view.state.doc;
-
     // Allow showing a placeholder (often used for permissions) but avoid showing the default edit-focused placeholder when editor is readonly
     if (view.editable || (!view.editable && text !== defaultPlaceholderText)) {
       const hasContent = !checkIsContentEmpty(doc.toJSON() as any);
@@ -37,12 +35,11 @@ export function placeholderPlugin(text: string = defaultPlaceholderText) {
         const selectedNode = state.doc.resolve(selectionPos).parent;
         const selectedNodeIsEmpty = checkIsContentEmpty(selectedNode.toJSON() as any);
         if (selectedNodeIsEmpty) {
+          const resolved = state.doc.resolve(selectionPos);
           return DecorationSet.create(state.doc, [
-            Decoration.widget(selectionPos, () => {
-              return createElement([
-                'div',
-                { contentEditable: false, class: 'charm-placeholder', 'data-placeholder': text }
-              ]);
+            Decoration.node(resolved.before(), resolved.after(), {
+              class: 'charm-placeholder',
+              'data-placeholder': text
             })
           ]);
         }
