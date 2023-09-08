51755adbc29955fe558ebe006c87991f110683c3
mattcasey <mattwad@gmail.com>
2023-09-08 01:17:16 -0600

    whtiespace

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,20 +8,24 @@ import { getSpacesOfUser } from 'lib/spaces/getSpacesOfUser';
 export const getServerSideProps: GetServerSideProps = withSessionSsr(async (context) => {
   const returnUrl = typeof context.query.returnUrl === 'string' ? context.query.returnUrl : undefined;
   const sessionUserId = context.req.session?.user?.id;
+
   if (!sessionUserId) {
     // show the normal login UI
     return {
       props: {}
     };
   }
+
   const [sortedSpaces, lastPageView] = await Promise.all([
     getSpacesOfUser(sessionUserId),
     getLastPageView(sessionUserId)
   ]);
-  const redirectUrl = getDefaultPage({ lastPageView, returnUrl, spaces: sortedSpaces, userId: sessionUserId });
+
+  const destination = getDefaultPage({ lastPageView, returnUrl, spaces: sortedSpaces, userId: sessionUserId });
+
   return {
     redirect: {
-      destination: redirectUrl,
+      destination,
       permanent: false
     }
   };
