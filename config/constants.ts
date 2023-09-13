ed73aaa06f85df21883b928b6032149d2a7b3fcd
yiwork Ying-Chi Connie I <cloudme@halfpast9.com>
2023-09-13 19:30:51 -0400

    making test runner all 4 cores and compare the run time. Also changed package.json and code to invalidate cache

--- config/constants.ts
+++ config/constants.ts
@@ -39,3 +39,4 @@ export const permissionsApiAuthKey = process.env.PERMISSIONS_API_AUTH_KEY ?? 'ke
 export const webhookEndpoint = 'api/v1/webhooks/addToDatabase';
 
 export const appSubdomain = 'app';
+export const webappDockerIMGTAG = process.env.IMGTAG ?? 'key';
