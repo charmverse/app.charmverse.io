1f35a05f8ad95639238f926529a29e74ce832059
Marek <m.sadura@outlook.com>
2023-09-14 09:44:54 +0200

    Supported chain list

--- connectors/config.ts
+++ connectors/config.ts
@@ -1,10 +1,18 @@
-import { createConfig, configureChains, mainnet } from 'wagmi';
+import { RPCList } from 'connectors/index';
+import { createConfig, configureChains } from 'wagmi';
+import * as wagmiChains from 'wagmi/chains';
 import { CoinbaseWalletConnector } from 'wagmi/connectors/coinbaseWallet';
 import { InjectedConnector } from 'wagmi/connectors/injected';
 import { WalletConnectConnector } from 'wagmi/connectors/walletConnect';
 import { publicProvider } from 'wagmi/providers/public';
 
-const { chains, publicClient } = configureChains([mainnet], [publicProvider()]);
+import { isTruthy } from 'lib/utilities/types';
+
+const wagmiChainList = Object.values(wagmiChains);
+// map our RPC list to the wagmi chain list
+const supportedChains = RPCList.map((rpc) => wagmiChainList.find((ch) => ch.id === rpc.chainId)).filter(isTruthy);
+
+const { chains, publicClient } = configureChains(supportedChains, [publicProvider()]);
 
 export const injectedConnector = new InjectedConnector({
   chains,
