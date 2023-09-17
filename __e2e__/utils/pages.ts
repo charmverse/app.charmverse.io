068ac9e690c0bb2f21a88d3dcf7f1501d5a2b1f2
Safwan Shaheer <devorein00@gmail.com>
2023-09-17 17:54:17 +0600

    Reinstated generatePageWithLinkedPage e2e utils function

--- __e2e__/utils/pages.ts
+++ __e2e__/utils/pages.ts
@@ -57,3 +57,16 @@ export function generatePage(
     }
   }) as Promise<PageWithPermissions>;
 }
+
+export function generatePageWithLinkedPage(
+  options: Partial<Page> &
+    Pick<Page, 'spaceId' | 'createdBy'> &
+    Partial<Omit<Page, 'content'>> & {
+      pagePermissions?: Prisma.PagePermissionCreateManyPageInput[];
+      linkedPageId: string;
+    }
+) {
+  const content = { type: 'doc', content: [{ type: 'page', attrs: { id: options.linkedPageId, track: [] } }] };
+
+  return generatePage({ ...options, content });
+}
