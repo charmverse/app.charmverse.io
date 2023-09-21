6e875f22eb0934bf1da99e29b185618ce5294021
motechFR <mo@anyahq.co>
2023-09-21 18:50:41 +0300

    Update postman API docs to link to new version

--- pages/api-docs.tsx
+++ pages/api-docs.tsx
@@ -13,7 +13,7 @@ const SwaggerUI = dynamic(() => import('swagger-ui-react'), {
   ssr: false
 });
 
-const postmanCollectionUrl = `https://github.com/charmverse/app.charmverse.io/blob/399108eff660fe4e3807fdbe0ccef2f1a775bffc/lib/public-api/CharmVerse%20API%20v1.postman_collection.json`;
+const postmanCollectionUrl = `https://github.com/charmverse/app.charmverse.io/blob/main/lib/public-api/CharmVerse%20API%20v1.postman_collection.json`;
 
 export const getStaticProps: GetStaticProps = async () => {
   // See this site for official OAS format https://swagger.io/docs/specification/basic-structure/
