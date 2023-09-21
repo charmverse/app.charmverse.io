008abf75a9ad60dac277f4a65154da93d830089e
Marek <m.sadura@outlook.com>
2023-09-21 12:33:32 +0200

    Switch to free tier

--- lib/subscription/updateToFreeTier.ts
+++ lib/subscription/updateToFreeTier.ts
@@ -1,16 +1,10 @@
 import { prisma } from '@charmverse/core/prisma-client';
 
-import { NotFoundError } from 'lib/middleware';
-
 import { getActiveSpaceSubscription } from './getActiveSpaceSubscription';
 import { stripeClient } from './stripe';
 
 export async function updateToFreeTier(spaceId: string, userId: string) {
   const subscription = await getActiveSpaceSubscription({ spaceId });
-
-  if (!subscription) {
-    throw new NotFoundError(`Subscription for space ${spaceId} not found`);
-  }
 
   const updatedSpace = await prisma.space.update({
     where: {
@@ -23,20 +17,22 @@ export async function updateToFreeTier(spaceId: string, userId: string) {
     }
   });
 
-  await stripeClient.subscriptions.cancel(subscription.subscriptionId, {
-    cancellation_details: {
-      comment: 'Downgraded to free plan'
-    }
-  });
+  if (subscription) {
+    await stripeClient.subscriptions.cancel(subscription.subscriptionId, {
+      cancellation_details: {
+        comment: 'Downgraded to free plan'
+      }
+    });
 
-  await prisma.stripeSubscription.update({
-    where: {
-      id: subscription.id
-    },
-    data: {
-      deletedAt: new Date()
-    }
-  });
+    await prisma.stripeSubscription.update({
+      where: {
+        id: subscription.id
+      },
+      data: {
+        deletedAt: new Date()
+      }
+    });
+  }
 
   return updatedSpace;
 }
