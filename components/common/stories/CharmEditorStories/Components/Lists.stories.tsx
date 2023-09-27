0cdbfd80d592b3771eb7f599391f821ded792cf6
mattcasey <mattwad@gmail.com>
2023-09-27 15:58:56 -0600

    add old lists to stories

--- components/common/stories/CharmEditorStories/Components/Lists.stories.tsx
+++ components/common/stories/CharmEditorStories/Components/Lists.stories.tsx
@@ -37,17 +37,45 @@ export function Lists() {
     _.bullet_list({ indent: 1 }, _.list_item(_.p('Cucumber')), _.list_item(_.p('Pumpkin'))),
     _.bullet_list({ indent: 2 }, _.list_item(_.p('Squash'))),
     _.bullet_list({ indent: 3 }, _.list_item(_.p('Zucchini'))),
+    _.heading({ level: 2 }, 'Old Bullet list'),
+    _.bulletList(
+      _.listItem(_.p('Bread')),
+      _.listItem(_.p('Milk')),
+      _.listItem(_.p('Vegetables')),
+      _.bulletList(
+        _.listItem(_.p('Cucumber')),
+        _.listItem(_.p('Pumpkin')),
+        _.bulletList(_.listItem(_.p('Squash')), _.bulletList(_.listItem(_.p('Zucchini'))))
+      )
+    ),
     _.heading({ level: 2 }, 'Ordered list'),
     _.ordered_list(_.list_item(_.p('Bread')), _.list_item(_.p('Milk')), _.list_item(_.p('Vegetables'))),
     _.ordered_list({ indent: 1 }, _.list_item(_.p('Cucumber')), _.list_item(_.p('Pumpkin'))),
     _.ordered_list({ indent: 2 }, _.list_item(_.p('Squash'))),
     _.ordered_list({ indent: 3 }, _.list_item(_.p('Zucchini'))),
+    _.heading({ level: 2 }, 'Old Ordered list'),
+    _.orderedList(
+      _.listItem(_.p('Bread')),
+      _.listItem(_.p('Milk')),
+      _.listItem(_.p('Vegetables')),
+      _.orderedList(
+        _.listItem(_.p('Cucumber')),
+        _.listItem(_.p('Pumpkin')),
+        _.orderedList(_.listItem(_.p('Squash')), _.orderedList(_.listItem(_.p('Zucchini'))))
+      )
+    ),
     _.heading({ level: 2 }, 'Todo list'),
     _.bullet_list(
       _.list_item({ todoChecked: true }, _.p('Task 1')),
       _.list_item({ todoChecked: false }, _.p('Task 2'))
     ),
-    _.bullet_list({ indent: 1 }, _.list_item({ todoChecked: false }, _.p('Nested task')))
+    _.bullet_list({ indent: 1 }, _.list_item({ todoChecked: false }, _.p('Nested task'))),
+    _.heading({ level: 2 }, 'Old Todo list'),
+    _.bulletList(
+      _.listItem({ todoChecked: true }, _.p('Task 1')),
+      _.listItem({ todoChecked: false }, _.p('Task 2')),
+      _.bulletList({ indent: 1 }, _.listItem({ todoChecked: false }, _.p('Nested task')))
+    )
   );
   return renderEditorWithContent({ content, title: 'Lists' });
 }
