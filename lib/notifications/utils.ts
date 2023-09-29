70b69eb322c5db8926330f5a0470f33a0b053e1a
Safwan Shaheer <devorein00@gmail.com>
2023-09-29 22:00:57 +0600

    Whitelist chris' id

--- lib/notifications/utils.ts
+++ lib/notifications/utils.ts
@@ -27,8 +27,9 @@ export const notificationMetadataIncludeStatement = {
 export const upgradedNotificationUserIds = process.env.USER_IDS
   ? process.env.USER_IDS.split(',')
   : [
-      '4e1d4522-6437-4393-8ed1-9c56e53235f4',
-      '5906c806-9497-43c7-9ffc-2eecd3c3a3ec',
+      'd5b4e5db-868d-47b0-bc78-ebe9b5b2c835', // chris in prod
+      '4e1d4522-6437-4393-8ed1-9c56e53235f4', // matt in prod
+      '5906c806-9497-43c7-9ffc-2eecd3c3a3ec', // safwan in prod
       'fb1013cc-d08a-471e-9835-6c86b760aff1', // safwan in staging
       'd7f22868-de36-4539-b820-f3c6a347ff61' // matt in staging
     ];
