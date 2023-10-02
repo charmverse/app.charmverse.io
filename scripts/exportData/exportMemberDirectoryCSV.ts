2e8724a6b0088b70ea25942b3c49db4838223a17
Marek <m.sadura@outlook.com>
2023-10-02 12:40:18 +0200

    Export member directory script (#2839)
    * Export member directory script
    
    * Move script

---
+++ scripts/exportData/exportMemberDirectoryCSV.ts
@@ -0,0 +1,25 @@
+
+import { getMembersExportData } from 'lib/members/getMembersExportData';
+import fs from 'node:fs/promises';
+import { stringify } from 'csv-stringify/sync';
+
+
+export async function exportMemberDirectory() {
+  const spaceId = ''
+  const data = await getMembersExportData(spaceId);
+  const csvContent = stringify(data, { 
+    cast: {
+      // cast objects and arrays to proper string values
+      object: (value) => {
+        if (Array.isArray(value)) {
+          return value.join(', ');
+        }
+
+        return JSON.stringify(value);
+      }
+  }});
+
+  await fs.writeFile(`${__dirname}/memberDirectory.csv`, csvContent);
+}
+
+exportMemberDirectory();
