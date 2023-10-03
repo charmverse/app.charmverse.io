87c7d11b8c14399b89f582d10ff83d7356831c3d
mattcasey <mattwad@gmail.com>
2023-10-03 12:54:01 -0600

    consider custom domains when redirect user based on old paths

---
+++ lib/utilities/domains/getPagePath.ts
@@ -0,0 +1,10 @@
+import type { GetServerSidePropsContext } from 'next';
+
+import { getCustomDomainFromHost } from './getCustomDomainFromHost';
+import { getSpaceDomainFromHost } from './getSpaceDomainFromHost';
+
+// Given a hostname, space domain, and path, return a path that can be used to open a page.
+export function getPagePath({ hostName, spaceDomain, path }: { hostName?: string; spaceDomain: string; path: string }) {
+  const isDomainInPath = !getCustomDomainFromHost(hostName) && !getSpaceDomainFromHost(hostName);
+  return encodeURI(`/${isDomainInPath ? `${spaceDomain}/` : ''}${path}`);
+}
