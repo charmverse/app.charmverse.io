111f85223fa26ac9d16a743c5d6de5edbde960cb
mattcasey <mattwad@gmail.com>
2023-10-03 12:59:56 -0600

    add tests

---
+++ lib/utilities/domains/__tests__/getPagePath.spec.ts
@@ -0,0 +1,32 @@
+import { getAppApexDomain } from 'lib/utilities/domains/getAppApexDomain';
+
+import { getPagePath } from '../getPagePath';
+
+describe('getPagePath', () => {
+  it('should add space domain when on localhost', () => {
+    const result = getPagePath({
+      spaceDomain: 'foobar',
+      path: 'members',
+      hostName: 'http://localhost:3000'
+    });
+    expect(result).toEqual('/foobar/members');
+  });
+
+  it('should add space domain when on a charmverse-hosted domain', () => {
+    const result = getPagePath({
+      spaceDomain: 'foobar',
+      path: 'members',
+      hostName: `app.${getAppApexDomain()}`
+    });
+    expect(result).toEqual('/foobar/members');
+  });
+
+  it('should not include space domain when on a custom host domain', () => {
+    const result = getPagePath({
+      spaceDomain: 'foobar',
+      path: 'members',
+      hostName: 'foobar.com'
+    });
+    expect(result).toEqual('/members');
+  });
+});
--- lib/utilities/domains/getPagePath.ts
+++ lib/utilities/domains/getPagePath.ts
@@ -1,5 +1,3 @@
-import type { GetServerSidePropsContext } from 'next';
-
 import { getCustomDomainFromHost } from './getCustomDomainFromHost';
 import { getSpaceDomainFromHost } from './getSpaceDomainFromHost';
 
