34cf3682b6a987188dc2511b55ac30973bdcf1c3
motechFR <mo@anyahq.co>
2023-10-06 13:02:56 +0300

    Update debug script

--- scripts/debugDuplicatePaths.ts
+++ scripts/debugDuplicatePaths.ts
@@ -1,7 +1,41 @@
 import { testUtilsPages, testUtilsUser } from "@charmverse/core/test";
-import { Page, prisma } from "@charmverse/core/prisma-client";
+import { Page, Space, prisma } from "@charmverse/core/prisma-client";
 import { isTestEnv } from "config/constants";
 import { writeToSameFolder } from "lib/utilities/file";
+
+
+async function seedDuplicatePages(): Promise<Space> {
+  if (!isTestEnv) {
+    throw new Error(`Not test env`)
+  }
+  const {space, user} = await testUtilsUser.generateUserAndSpace();
+
+ 
+
+  const duplicatePaths = ['path-0001', 'path-0002', 'path-0003'];
+
+  const pageWithDuplicate1 = await testUtilsPages.generatePage({
+    createdBy: user.id,
+    spaceId: space.id,
+    path: 'path-0001',
+    additionalPaths: [...duplicatePaths, ...duplicatePaths]
+  })
+
+  const pageWithDuplicate2 = await testUtilsPages.generatePage({
+    createdBy: user.id,
+    spaceId: space.id,
+    path: 'path-0002',
+    additionalPaths: [...duplicatePaths, ...duplicatePaths]
+  })
+
+  const pageWithoutDuplicate = await testUtilsPages.generatePage({
+    createdBy: user.id,
+    spaceId: space.id,
+    additionalPaths: [`random-${Math.random()}`]
+  })
+
+  return space;
+}
 
 type DebugRecord = {
   unnested: string;
@@ -9,34 +43,19 @@ type DebugRecord = {
   pageIds: Pick<Page, 'id' | 'path'>[] 
 }
 
-export async function debugDuplicatePaths(spaceId?: string) {
+export async function debugDuplicatePaths(spaceDomain?: string): Promise<DebugRecord[]> {
 
-  let querySpaceId = spaceId;
+  let querySpaceId = spaceDomain ? (await prisma.space.findUniqueOrThrow({where: {domain: spaceDomain}}))?.id : undefined;
 
-  if (isTestEnv) {
-
-    const {space, user} = await testUtilsUser.generateUserAndSpace();
-
-    const duplicatePaths = ['path-0001', 'path-0002', 'path-0003'];
-
-    const pageWithDuplicate1 = await testUtilsPages.generatePage({
-      createdBy: user.id,
-      spaceId: space.id,
-      additionalPaths: duplicatePaths
-    })
-
-    const pageWithDuplicate2 = await testUtilsPages.generatePage({
-      createdBy: user.id,
-      spaceId: space.id,
-      additionalPaths: duplicatePaths
-    })
-
-    const pageWithoutDuplicate = await testUtilsPages.generatePage({
-      createdBy: user.id,
-      spaceId: space.id,
-      additionalPaths: [`random-${Math.random()}`]
-    })
+  if (!isTestEnv && !spaceDomain) {
+    throw new Error('Space ID required in non-test env')
   }
+
+  /**
+   if (!spaceDomain) {
+     await seedDuplicatePages.then(space => querySpaceId = space.id)
+   }
+   */
 
   // const duplicates = await prisma
   // .$queryRaw`SELECT unnested, COUNT(*), array_agg(json_build_object('id', id, 'path', path)) AS "pageIds"
@@ -50,32 +69,55 @@ export async function debugDuplicatePaths(spaceId?: string) {
   GROUP BY unnested
   HAVING COUNT(*) > 1`;
 
+  await writeToSameFolder({data: JSON.stringify(duplicates, null, 2), fileName: `duplicates-${Date.now()}.json`})
+
   
-  return duplicates;
+  return duplicates as DebugRecord[];
 }
 
 
-debugDuplicatePaths("spaceid").then(async data => {
-  await writeToSameFolder({data: JSON.stringify(data, null, 2), fileName: 'debug-myosin.json'})
+async function resolveDuplicates(duplicates: DebugRecord[]) {
 
-  console.log((data as any[]).length, 'duplicate paths found')
-})
+  for (let i = 0; i < duplicates.length; i++) {
+    const dupe = duplicates[i];
+    const split = dupe.unnested.split('-');
+    const pageNumber = split[split.length - 1];
+
+    const map: Record<string, string> = {};
+
+    for (const pageMeta of dupe.pageIds) {
+
+      // Don't revisit pages
+      if (!pageMeta.path.match(pageNumber) && !map[pageMeta.id]) {
+        console.log('Fixing', pageMeta.path)
+        map[pageMeta.id] = pageMeta.id;
+        
+        const existingPage = await prisma.page.findUniqueOrThrow({
+          where: {
+            id: pageMeta.id
+          },
+          select: {
+            additionalPaths: true
+          }          
+        });
+
+        // Reset the paths to those not matching the unique scheme
+        const filteredPaths = existingPage.additionalPaths.filter(path => !path.match(pageNumber));
+
+        await prisma.page.update({
+            where: {
+              id: pageMeta.id
+            },
+            data: {
+              additionalPaths: filteredPaths
+            }
+          })
+        
+      }
+    }
+    console.log('Processed duplicates:',i + 1, '/', duplicates.length)
+  }
+}
 
 
-// prisma.page.updateMany({
-//   where: {
-//     id: ''
-//   },
-//   data: {
-//     additionalPaths: {
-
-//     }
-//   }
-// })
-// prisma.page.deleteMany({
-//   where: {
-//     additionalPaths: {
-//       hasSome: ['path-0001', 'path-0002', 'path-0003']
-//     }
-//   }
-// }).then(() => console.log('Deleted'))
+debugDuplicatePaths("spacename").then(resolveDuplicates)
