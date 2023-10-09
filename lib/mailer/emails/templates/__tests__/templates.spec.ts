ec4b68b9b61c93caa1707ea76a1336209f000916
Safwan Shaheer <devorein00@gmail.com>
2023-10-09 20:53:57 +0600

    rename tasks with notification

--- lib/mailer/emails/templates/__tests__/templates.spec.ts
+++ lib/mailer/emails/templates/__tests__/templates.spec.ts
@@ -3,12 +3,12 @@ import { notificationsRequiresYourAttention } from '../PendingNotificationsTempl
 describe('Email template unit tests', () => {
   it('Subject line should be formatted properly for one task', async () => {
     const subject = notificationsRequiresYourAttention({ count: 1 });
-    expect(subject).toEqual('1 task needs your attention');
+    expect(subject).toEqual('1 notification needs your attention');
   });
 
   it('Subject line should include CharmVerse name', async () => {
     const subject = notificationsRequiresYourAttention({ count: 1, includeName: true });
-    expect(subject).toEqual('1 CharmVerse task needs your attention');
+    expect(subject).toEqual('1 CharmVerse notification needs your attention');
   });
 
   it('Subject line should be formatted properly for >1 task', async () => {
