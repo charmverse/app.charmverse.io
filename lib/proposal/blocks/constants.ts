ebb754bfa7513fedf1df883c6d8f36ff8c0f1a97
Safwan Shaheer <devorein00@gmail.com>
2023-10-17 17:05:32 +0600

    Enable renaming title column in boards (#2941)

--- lib/proposal/blocks/constants.ts
+++ lib/proposal/blocks/constants.ts
@@ -1,19 +1,7 @@
 export const DEFAULT_BOARD_BLOCK_ID = '__defaultBoard';
 export const CATEGORY_BLOCK_ID = '__category';
-export const TITLE_BLOCK_ID = '__title';
 export const DEFAULT_VIEW_BLOCK_ID = '__defaultView';
 export const STATUS_BLOCK_ID = '__status';
 export const EVALUATION_TYPE_BLOCK_ID = '__evaluationType';
 export const AUTHORS_BLOCK_ID = '__authors';
 export const REVIEWERS_BLOCK_ID = '__reviewers';
-
-export const DEFAULT_BLOCK_IDS = [
-  DEFAULT_BOARD_BLOCK_ID,
-  CATEGORY_BLOCK_ID,
-  TITLE_BLOCK_ID,
-  DEFAULT_VIEW_BLOCK_ID,
-  STATUS_BLOCK_ID,
-  EVALUATION_TYPE_BLOCK_ID,
-  AUTHORS_BLOCK_ID,
-  REVIEWERS_BLOCK_ID
-];
--- lib/rewards/blocks/constants.ts
+++ lib/rewards/blocks/constants.ts
@@ -1,5 +1,4 @@
 export const DEFAULT_BOARD_BLOCK_ID = '__defaultBoard';
-export const TITLE_BLOCK_ID = '__title';
 export const DEFAULT_VIEW_BLOCK_ID = '__defaultView';
 export const REWARD_STATUS_BLOCK_ID = '__rewardStatus';
 export const REWARDER_BLOCK_ID = '__rewarder';
@@ -8,14 +7,3 @@ export const ASSIGNEES_BLOCK_ID = '__applicants';
 export const REWARD_REVIEWERS_BLOCK_ID = '__reviewers';
 export const REWARDS_AVAILABLE_BLOCK_ID = '__available';
 export const DUE_DATE_ID = '__limit';
-
-export const DEFAULT_BLOCK_IDS = [
-  DEFAULT_BOARD_BLOCK_ID,
-  TITLE_BLOCK_ID,
-  DEFAULT_VIEW_BLOCK_ID,
-  REWARD_STATUS_BLOCK_ID,
-  ASSIGNEES_BLOCK_ID,
-  REWARD_REVIEWERS_BLOCK_ID,
-  REWARDS_AVAILABLE_BLOCK_ID,
-  DUE_DATE_ID
-];
