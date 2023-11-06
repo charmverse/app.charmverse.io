93185cd7693ec47b24a3b496cc741a7204330f5d
Safwan Shaheer <devorein00@gmail.com>
2023-11-06 21:39:02 +0600

    Fix dragAndDrop e2e test (#3044)

--- __e2e__/charmEditor/dragAndDrop.spec.ts
+++ __e2e__/charmEditor/dragAndDrop.spec.ts
@@ -82,7 +82,13 @@ test('Drag and drop one paragraph over another in the CharmEditor', async ({ doc
     }
   });
 
-  await documentPage.page.waitForTimeout(150);
+  await documentPage.page.waitForFunction(
+    () => document.querySelector('.bangle-editor')?.textContent === 'Item 2Item 1',
+    {},
+    {
+      timeout: 2500
+    }
+  );
 
   const page = await prisma.page.findUniqueOrThrow({
     where: {
