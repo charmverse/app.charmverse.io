43e2db7c116bd9afcc77fbc28c76f8146aef0d89
Matt Casey <mattwad@gmail.com>
2023-11-06 14:47:23 -0700

    Use esbuild for websockets and cron instead of next.js (#3041)
    * build websockets separately
    
    * use esbuild for websockets and cron
    
    * build in ci
    
    * clean up config
    
    * bring back esbuild
    
    * fix tsconfig
    
    * transpile wagmi
    
    * remove unused tsconfig
    
    * add esbuild to regular npm build command
    
    * fix build
    
    * use ts for config
    
    * set storybook script name
    
    * support meta export
    
    * bump serverless esbuild

---
+++ esbuild.config.ts
@@ -0,0 +1,25 @@
+import * as fs from 'fs';
+
+import * as esbuild from 'esbuild';
+
+esbuild
+  .build({
+    bundle: true,
+    entryPoints: ['./background/initWebsockets.ts', './background/cron.ts'],
+    // metafile: true, // uncomment to analyize build file contents
+    outdir: './dist',
+    packages: 'external',
+    platform: 'node',
+    sourcemap: true,
+    target: 'node18'
+  })
+  .catch(() => process.exit(1))
+  .then(async (result) => {
+    if (result.metafile) {
+      // upload output to https://esbuild.github.io/analyze/
+      fs.writeFileSync('dist/meta.json', JSON.stringify(result.metafile));
+      // review this output to see why a file was included
+      const depsTreeOutput = await esbuild.analyzeMetafile(result.metafile, { verbose: true });
+      fs.writeFileSync('dist/dependencies.txt', depsTreeOutput);
+    }
+  });
