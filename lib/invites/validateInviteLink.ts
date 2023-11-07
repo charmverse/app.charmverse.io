6245a802e3e403a8c3def3694caaebfc0245de67
mattcasey <mattwad@gmail.com>
2023-11-06 22:01:19 -0700

    fix invite code page, broken with next 14

--- lib/invites/validateInviteLink.ts
+++ lib/invites/validateInviteLink.ts
@@ -2,27 +2,26 @@ import type { InviteLinkPopulated } from './getInviteLink';
 
 export type InvalidReason = 'expired' | 'maxUsesExceeded' | 'publicProposalsDisabled';
 
-export type ValidatedLink = {
-  invite: InviteLinkPopulated;
+export type ValidatedLinkResults = {
   valid: boolean;
   reason?: InvalidReason;
 };
 
-function isOutOfDate({ invite }: { invite: InviteLinkPopulated }): boolean {
+function isOutOfDate({ invite }: { invite: { createdAt: Date; maxAgeMinutes: number } }): boolean {
   const timePassed = Date.now() - invite.createdAt.getTime();
   const expired = timePassed > invite.maxAgeMinutes * 60 * 1000;
   return expired;
 }
 
-export function validateInviteLink({ invite }: { invite: InviteLinkPopulated }): ValidatedLink {
+export function validateInviteLink({ invite }: { invite: InviteLinkPopulated }): ValidatedLinkResults {
   if (invite.visibleOn === 'proposals') {
     const isValid = invite.space.publicProposals === true;
-    return { invite, valid: isValid, reason: isValid ? undefined : 'publicProposalsDisabled' };
+    return { valid: isValid, reason: isValid ? undefined : 'publicProposalsDisabled' };
   } else if (invite.maxUses > 0 && invite.useCount >= invite.maxUses) {
-    return { invite, valid: false, reason: 'maxUsesExceeded' };
+    return { valid: false, reason: 'maxUsesExceeded' };
   } else if (invite.maxAgeMinutes > 0 && isOutOfDate({ invite })) {
-    return { invite, valid: false, reason: 'expired' };
+    return { valid: false, reason: 'expired' };
   } else {
-    return { invite, valid: true };
+    return { valid: true };
   }
 }
