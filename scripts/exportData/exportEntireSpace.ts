720cf3bb070daaf3f1c0e6bfa75c0168a3393c05
motechFR <mo@anyahq.co>
2023-11-09 00:04:43 +0200

    Add export scripts and charmverse demo

---
+++ scripts/exportData/exportEntireSpace.ts
@@ -0,0 +1,9 @@
+import {exportSpaceData} from 'lib/templates/exportSpaceData';
+import { importSpaceData } from 'lib/templates/importSpaceData';
+
+
+// exportSpaceData({spaceIdOrDomain: 'charmverse-demo', filename: 'charmverse-demo.json'})
+// .then(() => console.log('Completed!'))
+
+// importSpaceData({targetSpaceIdOrDomain: 'pleasant-amethyst-rodent', exportName: 'charmverse-demo.json'})
+// .then(() => console.log('Completed!'))
