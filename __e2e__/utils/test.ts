e96ab95f2b6cdc1113065be38500e8e93a784e28
motechFR <mo@anyahq.co>
2023-11-09 16:50:32 +0200

    fix broken e2e test

--- __e2e__/utils/test.ts
+++ __e2e__/utils/test.ts
@@ -1,4 +1,6 @@
 import { test as base } from '@playwright/test';
+import { ProposalPage } from '__e2e__/po/proposalPage.po';
+import { ProposalsListPage } from '__e2e__/po/proposalsList.po';
 
 import { BountyBoardPage } from '../po/bountyBoard.po';
 import { DocumentPage } from '../po/document.po';
@@ -6,11 +8,15 @@ import { DocumentPage } from '../po/document.po';
 type Fixtures = {
   bountyBoardPage: BountyBoardPage;
   documentPage: DocumentPage;
+  proposalListPage: ProposalsListPage;
+  proposalPage: ProposalPage;
 };
 
 export const test = base.extend<Fixtures>({
   bountyBoardPage: ({ page }, use) => use(new BountyBoardPage(page)),
-  documentPage: ({ page }, use) => use(new DocumentPage(page))
+  documentPage: ({ page }, use) => use(new DocumentPage(page)),
+  proposalListPage: ({ page }, use) => use(new ProposalsListPage(page)),
+  proposalPage: ({ page }, use) => use(new ProposalPage(page))
 });
 
 export { chromium, expect } from '@playwright/test';
