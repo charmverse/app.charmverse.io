4d0d55ca35346d0b9735a2b14015f730873080c0
motechFR <mo@anyahq.co>
2023-11-09 22:26:15 +0200

    Fix loop along with typecasting error

--- components/settings/subscription/loadStripe.ts
+++ components/settings/subscription/loadStripe.ts
@@ -2,7 +2,9 @@ import env from '@beam-australia/react-env';
 import type { Stripe } from '@stripe/stripe-js';
 import { loadStripe as _loadStripe } from '@stripe/stripe-js';
 
-const stripePublicKey = env('STRIPE_PUBLIC_KEY');
+// This typecasting prevents stripe throwing an error
+// https://stackoverflow.com/a/63784104
+const stripePublicKey = env('STRIPE_PUBLIC_KEY') as string;
 
 let stripePromise: Promise<Stripe | null>;
 
