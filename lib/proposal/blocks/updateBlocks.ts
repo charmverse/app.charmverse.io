10edb807a4782124dc6556a20aee27f5ed14f604
Marek <m.sadura@outlook.com>
2023-11-13 11:13:05 +0100

    Fix udpating custom proerties for proposals and rewards from table view

--- lib/proposal/blocks/updateBlocks.ts
+++ lib/proposal/blocks/updateBlocks.ts
@@ -1,7 +1,15 @@
+import { log } from '@charmverse/core/log';
 import { prisma } from '@charmverse/core/prisma-client';
 
-import type { ProposalBlockUpdateInput, ProposalBlockWithTypedFields } from 'lib/proposal/blocks/interfaces';
+import { filterInternalProperties } from 'lib/focalboard/utilities';
+import type {
+  ProposalBlockUpdateInput,
+  ProposalBlockWithTypedFields,
+  ProposalPropertiesField,
+  ProposalPropertyValues
+} from 'lib/proposal/blocks/interfaces';
 import { updateBlock } from 'lib/proposal/blocks/updateBlock';
+import { updateProposal } from 'lib/proposal/updateProposal';
 
 export async function updateBlocks({
   blocksData,
@@ -12,7 +20,29 @@ export async function updateBlocks({
   userId: string;
   spaceId: string;
 }) {
-  return prisma.$transaction(blocksData.map((data) => updateBlock({ data, userId, spaceId }))) as Promise<
+  const blocks = blocksData.filter((block) => block.type !== 'card');
+  const proposals = blocksData.filter((block) => block.type === 'card');
+
+  try {
+    const promises = proposals.map((proposal) =>
+      updateProposal({
+        proposalId: proposal.id,
+        fields: {
+          ...proposal.fields,
+          properties: filterInternalProperties<ProposalPropertiesField>(
+            (proposal.fields as ProposalPropertyValues).properties
+          )
+        }
+      })
+    );
+
+    await Promise.allSettled(promises);
+  } catch (error) {
+    log.error('Error updating proposal block fields', { error });
+    throw error;
+  }
+
+  return prisma.$transaction(blocks.map((data) => updateBlock({ data, userId, spaceId }))) as Promise<
     ProposalBlockWithTypedFields[]
   >;
 }
