21e5a17c3680da6b7e482e86967fe306b962e887
Safwan Shaheer <devorein00@gmail.com>
2023-11-19 14:20:02 +0600

    Fixed cryptoprice component not working in public pages

--- lib/middleware/requireUser.ts
+++ lib/middleware/requireUser.ts
@@ -8,24 +8,3 @@ export function requireUser(req: NextApiRequest, res: NextApiResponse, next: Nex
     next();
   }
 }
-
-/**
- * Allow an endpoint to be consumed if it originates from a share page
- */
-export function requireUserOrSharePage(req: NextApiRequest, res: NextApiResponse, next: NextHandler) {
-  const referer = req.headers.referer as string;
-
-  const url = new URL(referer);
-  url.hash = '';
-  url.search = '';
-  const pathnameParts = referer ? url.pathname.split('/') : [];
-  const spaceDomain = pathnameParts[1];
-
-  if (spaceDomain === 'share') {
-    next();
-  } else if (!req.session.user) {
-    res.status(401).send({ error: 'Please log in' });
-  } else {
-    next();
-  }
-}
