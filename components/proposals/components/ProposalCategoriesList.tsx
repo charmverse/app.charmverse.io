c349e099d264f209902e49ef87452820d79acf2c
Safwan Shaheer <devorein00@gmail.com>
2023-11-20 20:35:19 +0600

    Add color to proposal category when creating

--- components/proposals/components/ProposalCategoriesList.tsx
+++ components/proposals/components/ProposalCategoriesList.tsx
@@ -1,21 +1,36 @@
+import { useTheme } from '@emotion/react';
+import styled from '@emotion/styled';
 import AddIcon from '@mui/icons-material/Add';
-import { MenuItem, MenuList, TextField, Typography } from '@mui/material';
-import { usePopupState } from 'material-ui-popup-state/hooks';
+import CheckIcon from '@mui/icons-material/Check';
+import { Box, Menu, MenuItem, MenuList, Paper, Stack, TextField, Typography } from '@mui/material';
+import { bindMenu, bindTrigger, usePopupState } from 'material-ui-popup-state/hooks';
 import { useState } from 'react';
 
 import { Button } from 'components/common/Button';
+import FieldLabel from 'components/common/form/FieldLabel';
 import Modal from 'components/common/Modal';
 import { ProposalCategoryChip } from 'components/proposals/components/ProposalChip';
 import { ProposalCategoryContextMenu } from 'components/proposals/components/ProposalViewOptions/components/ProposalCategoryContextMenu';
 import { useProposalCategories } from 'components/proposals/hooks/useProposalCategories';
 import { useIsAdmin } from 'hooks/useIsAdmin';
 import { useSnackbar } from 'hooks/useSnackbar';
+import type { BrandColor } from 'theme/colors';
+import { brandColorNames } from 'theme/colors';
 import { getRandomThemeColor } from 'theme/utils/getRandomThemeColor';
+
+const ColorBox = styled(Box)`
+  width: 40px;
+  height: 40px;
+  border-radius: 4px;
+  cursor: pointer;
+`;
 
 export function ProposalCategoriesList() {
   const { categories } = useProposalCategories();
-
+  const [categoryColor, setCategoryColor] = useState<BrandColor>(getRandomThemeColor());
   const addCategoryPopupState = usePopupState({ variant: 'popover', popupId: 'add-category' });
+  const theme = useTheme();
+  const popupState = usePopupState({ variant: 'popover', popupId: `change-category-color` });
 
   const isAdmin = useIsAdmin();
 
@@ -24,9 +39,10 @@ export function ProposalCategoriesList() {
   const { addCategory } = useProposalCategories();
 
   async function createCategory() {
-    addCategory({ title: newCategoryName, color: getRandomThemeColor() })
+    addCategory({ title: newCategoryName, color: categoryColor })
       .then(() => {
         setNewCategoryName('');
+        setCategoryColor(getRandomThemeColor());
         addCategoryPopupState.close();
       })
       .catch((err) => {
@@ -73,26 +89,70 @@ export function ProposalCategoriesList() {
         }}
         title='Add proposal category'
       >
-        <TextField
-          data-test='add-category-input'
-          sx={{
-            mb: 1
-          }}
-          autoFocus
-          fullWidth
-          value={newCategoryName}
-          onChange={(e) => {
-            setNewCategoryName(e.target.value);
-          }}
-          onKeyPress={(e) => {
-            if (e.key === 'Enter') {
-              createCategory();
-            }
-          }}
-        />
+        <Stack gap={1} mb={2} flexDirection='row'>
+          <Stack gap={0.5} flexGrow={1}>
+            <FieldLabel variant='subtitle2'>Name</FieldLabel>
+            <TextField
+              data-test='add-category-input'
+              autoFocus
+              fullWidth
+              value={newCategoryName}
+              onChange={(e) => {
+                setNewCategoryName(e.target.value);
+              }}
+              onKeyPress={(e) => {
+                if (e.key === 'Enter') {
+                  createCategory();
+                }
+              }}
+            />
+          </Stack>
+
+          <Stack gap={0.5}>
+            <FieldLabel variant='subtitle2'>Color</FieldLabel>
+            <ColorBox
+              sx={{
+                backgroundColor: theme.palette[categoryColor].main
+              }}
+              {...bindTrigger(popupState)}
+            />
+            <Menu
+              {...bindMenu(popupState)}
+              PaperProps={{
+                sx: { width: 300 }
+              }}
+            >
+              {brandColorNames.map((color) => (
+                <MenuItem
+                  key={color}
+                  sx={{ textTransform: 'capitalize', display: 'flex', gap: 1, justifyContent: 'space-between' }}
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    setCategoryColor(color);
+                    popupState.close();
+                  }}
+                >
+                  <Stack flexDirection='row' gap={1} alignContent='center'>
+                    <div
+                      style={{
+                        width: 20,
+                        height: 20,
+                        borderRadius: '20%',
+                        backgroundColor: theme.palette[color].main
+                      }}
+                    />
+                    <Typography variant='subtitle1'>{color}</Typography>
+                  </Stack>
+
+                  {color === categoryColor && <CheckIcon fontSize='small' />}
+                </MenuItem>
+              ))}
+            </Menu>
+          </Stack>
+        </Stack>
         <Button
           data-test='confirm-new-category-button'
-          disabled={categories.find((category) => category.title === newCategoryName)}
+          disabled={categories.find((category) => category.title === newCategoryName) || !newCategoryName}
           onClick={createCategory}
         >
           Add
