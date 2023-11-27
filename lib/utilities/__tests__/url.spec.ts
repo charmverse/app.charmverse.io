6db07489789ef28c65645b56c219543d96cb0701
mattcasey <mattwad@gmail.com>
2023-11-27 13:30:39 -0700

    .

---
+++ lib/utilities/__tests__/url.spec.ts
@@ -0,0 +1,15 @@
+import { replaceS3Domain } from '../url';
+
+describe('replaceS3Domain', () => {
+  it('should not replace a dev bucket domain', () => {
+    const testValue = 'https://s3.amazonaws.com/charm.public.dev/user-content/1234';
+
+    expect(replaceS3Domain(testValue)).toBe(testValue);
+  });
+
+  it('should replace the prod bucket', () => {
+    const testValue = 'https://s3.amazonaws.com/charm.public/user-content/1234';
+
+    expect(replaceS3Domain(testValue)).toBe('https://s3.amazonaws.com/charm.public.prd/user-content/1234');
+  });
+});
