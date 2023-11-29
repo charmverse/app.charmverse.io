0f7e32e02c85d1c5c76f37ad20b1b048ebc2a82c
motechFR <mo@anyahq.co>
2023-11-30 00:55:02 +0200

    Bug/gnosis tx ordering (#3197)
    * Setup seed script
    
    * Add sepolia gnosis support, and allow auto-sync of Safes from reward button
    
    * Fix Gnosis to allow preparing a single payment
    
    * Change how we calculate devenv to fix wagmi locally
    
    * Fix nonce construction
    
    * Fix
    
    * Reset package-lock
    
    * Remove snackbar from useGnosisPayment
    
    * Fix rewards seed
    
    * Fix gnosis payment
    
    * Use safe api client to avoid dependency on signer for read operations
    
    * Show pending Safe transaction
    
    * Fix broken tests

--- lib/gnosis/utils.ts
+++ lib/gnosis/utils.ts
@@ -4,6 +4,10 @@ export function getGnosisTransactionQueueUrl(address: string, chainId: number) {
   return `https://app.safe.global/${getChainShortname(chainId)}:${address}/transactions/queue`;
 }
 
+export function getGnosisTransactionUrl(address: string, chainId: number, safeTxHash: string) {
+  return `https://app.safe.global/transactions/tx?safe=${getChainShortname(chainId)}:${address}&id=${safeTxHash}`;
+}
+
 export function getGnosisSafeUrl(address: string, chainId: number) {
   return `https://app.safe.global/${getChainShortname(chainId)}:${address}`;
 }
