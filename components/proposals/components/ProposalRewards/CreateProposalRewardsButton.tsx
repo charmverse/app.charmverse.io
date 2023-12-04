9d8279c6bd7062308b7c2ca702fcfe975d81ba7e
Marek <m.sadura@outlook.com>
2023-12-04 22:12:55 +0100

    Fir refreshing rewards

--- components/proposals/components/ProposalRewards/CreateProposalRewardsButton.tsx
+++ components/proposals/components/ProposalRewards/CreateProposalRewardsButton.tsx
@@ -1,6 +1,8 @@
-import { useCreateProposalRewards } from 'charmClient/hooks/proposals';
+import { useCreateProposalRewards, useGetProposalDetails } from 'charmClient/hooks/proposals';
 import { Button } from 'components/common/Button';
 import { useProposals } from 'components/proposals/hooks/useProposals';
+import { useRewards } from 'components/rewards/hooks/useRewards';
+import { useSnackbar } from 'hooks/useSnackbar';
 
 type Props = {
   proposalId: string;
@@ -9,10 +11,17 @@ type Props = {
 export function CreateProposalRewardsButton({ proposalId }: Props) {
   const { trigger, isMutating } = useCreateProposalRewards(proposalId);
   const { refreshProposal } = useProposals();
+  const { mutate } = useGetProposalDetails(proposalId);
+  const { showMessage } = useSnackbar();
+  const { mutateRewards } = useRewards();
 
   const createRewards = async () => {
     await trigger();
-    await refreshProposal(proposalId);
+    showMessage('Rewards created', 'success');
+
+    mutate();
+    refreshProposal(proposalId);
+    mutateRewards();
   };
 
   return (
