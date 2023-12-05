37b305c257504b7d25622a67a5710009cb4e5373
Valentin L <ludu.valentin@gmail.com>
2023-12-05 16:28:08 +0200

    Fix value not showing on member directory custom props

--- components/common/form/fields/TextInputField.tsx
+++ components/common/form/fields/TextInputField.tsx
@@ -7,7 +7,7 @@ import type { ControlFieldProps, FieldProps } from 'components/common/form/inter
 type Props = ControlFieldProps & FieldProps & { multiline?: boolean; rows?: number };
 
 export const TextInputField = forwardRef<HTMLDivElement, Props>(
-  ({ label, iconLabel, inline, error, multiline = false, required, value, ...inputProps }, ref) => {
+  ({ label, iconLabel, inline, error, multiline = false, required, ...inputProps }, ref) => {
     return (
       <FieldWrapper required={required} label={label} inline={inline} iconLabel={iconLabel}>
         <TextField fullWidth required={required} error={!!error} multiline={multiline} {...inputProps} ref={ref} />
