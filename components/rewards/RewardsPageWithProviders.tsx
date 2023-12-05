98219c2d412cc952af39e8d8c0e5bca4352329fa
Safwan Shaheer <devorein00@gmail.com>
2023-12-05 18:45:27 +0600

    Create test reward and proposal when creating space (#3226)

--- components/rewards/RewardsPageWithProviders.tsx
+++ components/rewards/RewardsPageWithProviders.tsx
@@ -3,7 +3,6 @@ import { PageDialogGlobal } from 'components/common/PageDialog/PageDialogGlobal'
 import { RewardsProvider } from 'components/rewards/hooks/useRewards';
 import { RewardsBoardProvider } from 'components/rewards/hooks/useRewardsBoard';
 import { RewardsPage } from 'components/rewards/RewardsPage';
-import { DbViewSettingsProvider } from 'hooks/useLocalDbViewSettings';
 import { setTitle } from 'hooks/usePageTitle';
 import { RewardBlocksProvider } from 'hooks/useRewardBlocks';
 import { useSpaceFeatures } from 'hooks/useSpaceFeatures';
