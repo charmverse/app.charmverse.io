b623dc79d7e525b259198d0e96bf10a97ffbda5d
mattcasey <mattwad@gmail.com>
2023-12-05 23:00:59 -0700

    eh

--- components/proposals/components/ProposalProperties/components/ProposalEvaluationForm.tsx
+++ components/proposals/components/ProposalProperties/components/ProposalEvaluationForm.tsx
@@ -6,6 +6,7 @@ import useMediaQuery from '@mui/material/useMediaQuery';
 
 import { PropertyLabel } from 'components/common/BoardEditor/components/properties/PropertyLabel';
 import { UserAndRoleSelect } from 'components/common/BoardEditor/components/properties/UserAndRoleSelect';
+import { evaluationIcons } from 'components/settings/proposals/constants';
 
 import { ProposalRubricCriteriaInput } from './ProposalRubricCriteriaInput';
 import type { RangeProposalCriteria } from './ProposalRubricCriteriaInput';
@@ -25,9 +26,12 @@ export function ProposalEvaluationForm({ evaluation, categoryId, onChange }: Pro
   const isMobile = useMediaQuery<Theme>((theme) => theme.breakpoints.down('sm'));
   return (
     <Box justifyContent='space-between' gap={2} alignItems='center' mb='6px'>
-      <PropertyLabel highlighted fullWidth>
-        Evaluation step: {evaluation.title}
-      </PropertyLabel>
+      <Box display='flex' alignItems='center' gap={1}>
+        {evaluationIcons[evaluation.type]()}
+        <PropertyLabel highlighted fullWidth>
+          {evaluation.title}
+        </PropertyLabel>
+      </Box>
       <Box display='flex' height='fit-content' flex={1} className='octo-propertyrow'>
         <PropertyLabel readOnly required>
           Reviewer
