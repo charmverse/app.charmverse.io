16dfbb2a3fe52ba4870e78859fd4bf22308b7b61
motechFR <mo@anyahq.co>
2023-12-06 15:38:36 +0200

    Create proposal permissions

--- lib/proposal/createProposal.ts
+++ lib/proposal/createProposal.ts
@@ -1,7 +1,13 @@
 import { InsecureOperationError, InvalidInputError } from '@charmverse/core/errors';
 import type { PageWithPermissions } from '@charmverse/core/pages';
+import { ProposalSystemRole } from '@charmverse/core/prisma';
 import type { Page, ProposalStatus, PageType } from '@charmverse/core/prisma';
-import type { ProposalEvaluationType, ProposalEvaluation, WorkspaceEvent } from '@charmverse/core/prisma-client';
+import type {
+  ProposalEvaluationType,
+  ProposalEvaluation,
+  WorkspaceEvent,
+  Prisma
+} from '@charmverse/core/prisma-client';
 import { prisma } from '@charmverse/core/prisma-client';
 import type { ProposalWithUsers, ProposalReviewerInput } from '@charmverse/core/proposals';
 import { arrayUtils } from '@charmverse/core/utilities';
@@ -81,8 +87,39 @@ export async function createProposal({
     throw new InsecureOperationError(`You cannot create a proposal with authors or reviewers outside the space`);
   }
   const evaluationIds = evaluations.map(() => uuid());
+
+  const evaluationReviewerPermissionsToCreate: Prisma.ProposalEvaluationPermissionCreateManyInput[] = [];
+  const proposalReviewersToCreate: Prisma.ProposalReviewerCreateManyInput[] = [];
+
   // apply evaluation ids to reviewers
   if (evaluationIds.length > 0) {
+    for (let i = 0; i < evaluations.length; i++) {
+      const evalStep = { ...evaluations[i], id: evaluationIds[i] };
+      evaluationReviewerPermissionsToCreate.push(
+        ...evalStep.reviewers.map(
+          (r) =>
+            ({
+              evaluationId: evalStep.id,
+              roleId: r.group === 'role' ? r.id : undefined,
+              userId: r.group === 'user' ? r.id : undefined,
+              operation: 'review',
+              systemRole: ProposalSystemRole[r.group as ProposalSystemRole] ? r.group : undefined
+            } as Prisma.ProposalEvaluationPermissionCreateManyInput)
+        )
+      );
+      proposalReviewersToCreate.push(
+        ...evalStep.reviewers.map(
+          (r) =>
+            ({
+              proposalId,
+              evaluationId: evalStep.id,
+              roleId: r.group === 'role' ? r.id : undefined,
+              userId: r.group === 'user' ? r.id : undefined
+            } as Prisma.ProposalReviewerCreateManyInput)
+        )
+      );
+    }
+
     reviewers = evaluations.flatMap((evaluation, index) =>
       evaluation.reviewers.map((reviewer) => ({
         ...reviewer,
@@ -92,71 +129,69 @@ export async function createProposal({
     proposalStatus = 'published'; // TODO: implement support for drafts
   }
   // Using a transaction to ensure both the proposal and page gets created together
-  const [proposal, , page] = await prisma.$transaction([
-    prisma.proposal.create({
-      data: {
-        // Add page creator as the proposal's first author
-        createdBy: userId,
-        id: proposalId,
-        space: { connect: { id: spaceId } },
-        status: proposalStatus,
-        category: { connect: { id: categoryId } },
-        evaluationType,
-        publishToLens,
-        authors: {
-          createMany: {
-            data: authorsList.map((author) => ({ userId: author }))
-          }
-        },
-        reviewers: reviewers
-          ? {
-              createMany: {
-                data: reviewers.map((reviewer) => ({
-                  userId: reviewer.group === 'user' ? reviewer.id : undefined,
-                  roleId: reviewer.group === 'role' ? reviewer.id : undefined
-                }))
-              }
+  const [proposal, proposalEvaluations, proposalEvaluationPermissions, proposalReviewers, page] =
+    await prisma.$transaction([
+      prisma.proposal.create({
+        data: {
+          // Add page creator as the proposal's first author
+          createdBy: userId,
+          id: proposalId,
+          space: { connect: { id: spaceId } },
+          status: proposalStatus,
+          category: { connect: { id: categoryId } },
+          evaluationType,
+          publishToLens,
+          authors: {
+            createMany: {
+              data: authorsList.map((author) => ({ userId: author }))
             }
-          : undefined,
-        fields
-      },
-      include: {
-        authors: true,
-        reviewers: true,
-        category: true
-      }
-    }),
-    prisma.proposalEvaluation.createMany({
-      // we dont save evaluations as part of the template, since they link to workflow id instead
-      data:
-        pageProps?.type === 'proposal_template'
-          ? []
-          : evaluations.map((evaluation, index) => ({
-              id: evaluationIds[index],
-              index: evaluation.index,
-              title: evaluation.title,
-              type: evaluation.type,
-              proposalId
-            }))
-    }),
-    createPage({
-      data: {
-        content: pageProps?.content ?? undefined,
-        createdBy: userId,
-        contentText: pageProps?.contentText ?? '',
-        headerImage: pageProps?.headerImage,
-        icon: pageProps?.icon,
-        id: proposalId,
-        path: getPagePath(),
-        proposalId,
-        sourceTemplateId: pageProps?.sourceTemplateId,
-        title: pageProps?.title ?? '',
-        type: pageProps?.type ?? 'proposal',
-        updatedBy: userId,
-        spaceId
-      }
-    })
-  ]);
+          },
+          fields
+        },
+        include: {
+          authors: true,
+          reviewers: true,
+          category: true
+        }
+      }),
+      prisma.proposalEvaluation.createMany({
+        // we dont save evaluations as part of the template, since they link to workflow id instead
+        data:
+          pageProps?.type === 'proposal_template'
+            ? []
+            : evaluations.map((evaluation, index) => ({
+                id: evaluationIds[index],
+                index: evaluation.index,
+                title: evaluation.title,
+                type: evaluation.type,
+                proposalId
+              }))
+      }),
+      prisma.proposalEvaluationPermission.createMany({
+        // we dont save evaluations as part of the template, since they link to workflow id instead
+        data: pageProps?.type === 'proposal_template' ? [] : evaluationReviewerPermissionsToCreate
+      }),
+      prisma.proposalReviewer.createMany({
+        data: proposalReviewersToCreate
+      }),
+      createPage({
+        data: {
+          content: pageProps?.content ?? undefined,
+          createdBy: userId,
+          contentText: pageProps?.contentText ?? '',
+          headerImage: pageProps?.headerImage,
+          icon: pageProps?.icon,
+          id: proposalId,
+          path: getPagePath(),
+          proposalId,
+          sourceTemplateId: pageProps?.sourceTemplateId,
+          title: pageProps?.title ?? '',
+          type: pageProps?.type ?? 'proposal',
+          updatedBy: userId,
+          spaceId
+        }
+      })
+    ]);
   trackUserAction('new_proposal_created', { userId, pageId: page.id, resourceId: proposal.id, spaceId });
 
   const upsertedCriteria = rubricCriteria
