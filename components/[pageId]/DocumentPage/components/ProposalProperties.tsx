30a8cd58510ed5687bd203fb7f21581ef4dde9b3
mattcasey <mattwad@gmail.com>
2023-12-20 15:06:05 -0700

    fix tests

--- components/[pageId]/DocumentPage/components/ProposalProperties.tsx
+++ components/[pageId]/DocumentPage/components/ProposalProperties.tsx
@@ -1,17 +1,10 @@
 import type { PagePermissionFlags } from '@charmverse/core/permissions';
 import type { ProposalStatus } from '@charmverse/core/prisma';
 import { Box } from '@mui/material';
-import { debounce } from 'lodash';
-import { useCallback, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import charmClient from 'charmClient';
-import {
-  useGetIsReviewer,
-  useGetProposalDetails,
-  useGetProposalFlowFlags,
-  useUpsertRubricCriteria,
-  useUpdateProposal
-} from 'charmClient/hooks/proposals';
+import { useGetIsReviewer, useGetProposalFlowFlags, useUpdateProposal } from 'charmClient/hooks/proposals';
 import { useNotifications } from 'components/nexus/hooks/useNotifications';
 import { useProposals } from 'components/proposals/hooks/useProposals';
 import { useProposalTemplates } from 'components/proposals/hooks/useProposalTemplates';
@@ -71,7 +64,6 @@ export function ProposalProperties({
 
   const { data: isReviewer } = useGetIsReviewer(pageId || undefined);
   const { data: proposalFlowFlags, mutate: refreshProposalFlowFlags } = useGetProposalFlowFlags(proposalId);
-  const { trigger: upsertRubricCriteria } = useUpsertRubricCriteria({ proposalId });
   const isAdmin = useIsAdmin();
 
   // further restrict readOnly if user cannot update proposal properties specifically
@@ -79,7 +71,6 @@ export function ProposalProperties({
   const readOnlyProperties = readOnly || !(pagePermissions?.edit_content || isAdmin);
 
   const isFromTemplateSource = Boolean(proposal?.page?.sourceTemplateId);
-  const isTemplate = proposalPage.type === 'proposal_template';
   const sourceTemplate = isFromTemplateSource
     ? proposalTemplates?.find((template) => template.id === proposal?.page?.sourceTemplateId)
     : undefined;
@@ -133,13 +124,6 @@ export function ProposalProperties({
       mutateProposals();
     }
   }
-
-  async function onChangeRubricCriteria(rubricCriteria: ProposalPropertiesInput['rubricCriteria']) {
-    // @ts-ignore TODO: unify types for rubricCriteria
-    await upsertRubricCriteria({ rubricCriteria });
-    refreshProposal();
-  }
-
   async function onChangeProperties(values: Partial<ProposalPropertiesInput>) {
     if (proposal) {
       await updateProposal({
@@ -156,11 +140,7 @@ export function ProposalProperties({
     mutateProposals();
   }
 
-  const onChangeRubricCriteriaDebounced = useCallback(debounce(onChangeRubricCriteria, 300), [proposal?.status]);
   const readOnlyCategory = !isAdmin && (!proposalPermissions?.edit || !!proposal?.page?.sourceTemplateId);
-  const readOnlyReviewers = readOnlyProperties || (!isAdmin && !!sourceTemplate);
-  // rubric criteria can always be updated by reviewers and admins, but criteria from a template are only editable by admin
-  const readOnlyRubricCriteria = !isAdmin && ((readOnlyProperties && !isReviewer) || isFromTemplateSource);
 
   const canSeeEvaluation =
     (proposal?.status === 'evaluation_active' || proposal?.status === 'evaluation_closed') && isReviewer;
@@ -184,9 +164,7 @@ export function ProposalProperties({
     >
       <div className='octo-propertylist'>
         <ProposalPropertiesBase
-          canSeeEvaluation={canSeeEvaluation}
           proposalLensLink={proposal?.lensPostLink ?? undefined}
-          archived={!!proposal?.archived}
           isFromTemplate={!!proposal?.page?.sourceTemplateId}
           proposalFlowFlags={proposalFlowFlags}
           proposalStatus={proposal?.status}
@@ -195,29 +173,14 @@ export function ProposalProperties({
           readOnlyAuthors={readOnlyProperties}
           readOnlyCategory={readOnlyCategory}
           isAdmin={isAdmin}
-          readOnlyRubricCriteria={readOnlyRubricCriteria}
-          readOnlyProposalEvaluationType={
-            readOnlyProperties ||
-            // dont let users change type after status moves to Feedback, and forward
-            (proposal?.status !== 'draft' && !isTemplate) ||
-            isFromTemplateSource
-          }
-          readOnlyReviewers={readOnlyReviewers}
-          rubricAnswers={proposal?.rubricAnswers}
-          isTemplate={isTemplate}
-          rubricCriteria={proposal?.rubricCriteria}
           snapshotProposalId={snapshotProposalId}
           updateProposalStatus={updateProposalStatus}
-          onChangeRubricCriteria={onChangeRubricCriteriaDebounced}
           proposalFormInputs={proposalFormInputs}
           setProposalFormInputs={onChangeProperties}
           isPublishingToLens={isPublishingToLens}
           readOnlyCustomProperties={readOnlyCustomProperties}
-          isEvaluationSidebarOpen={isEvaluationSidebarOpen}
-          openEvaluation={openEvaluation}
           isReviewer={isReviewer}
           rewardIds={proposal?.rewardIds}
-          isCharmVerse={isCharmVerse}
         />
         {isPublishingToLens && (
           <CreateLensPublication
