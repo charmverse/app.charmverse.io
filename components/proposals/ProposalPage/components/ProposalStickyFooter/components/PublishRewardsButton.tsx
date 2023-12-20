6bf3917302e78682c3938c63bfeff55ebbb1cca6
Matt Casey <mattwad@gmail.com>
2023-12-20 12:47:49 -0700

    Allow going back in proposal steps (#3291)
    * reset evaluation view when id changes
    
    * allow going backwards in stesp
    
    * add confirmation to publish rewards
    
    * .

--- components/proposals/ProposalPage/components/ProposalStickyFooter/components/PublishRewardsButton.tsx
+++ components/proposals/ProposalPage/components/ProposalStickyFooter/components/PublishRewardsButton.tsx
@@ -1,10 +1,12 @@
 import BountyIcon from '@mui/icons-material/RequestPageOutlined';
+import { Typography } from '@mui/material';
+import { useState } from 'react';
 
-import { useCreateProposalRewards, useGetProposalDetails } from 'charmClient/hooks/proposals';
+import { useCreateProposalRewards } from 'charmClient/hooks/proposals';
 import { Button } from 'components/common/Button';
-import { useProposals } from 'components/proposals/hooks/useProposals';
-import { useRewards } from 'components/rewards/hooks/useRewards';
+import ModalWithButtons from 'components/common/Modal/ModalWithButtons';
 import { useSnackbar } from 'hooks/useSnackbar';
+import { useSpaceFeatures } from 'hooks/useSpaceFeatures';
 
 export type Props = {
   disabled: boolean;
@@ -13,23 +15,45 @@ export type Props = {
 };
 
 export function PublishRewardsButton({ proposalId, disabled, onSubmit }: Props) {
+  const [showConfirmation, setShowConfirmation] = useState<boolean>(false);
   const { trigger, isMutating } = useCreateProposalRewards(proposalId);
   const { showMessage } = useSnackbar();
+  const { mappedFeatures } = useSpaceFeatures();
+  const rewardsTitle = mappedFeatures.rewards.title;
 
-  const createRewards = async () => {
+  async function createRewards() {
     try {
       await trigger();
       showMessage('Rewards created', 'success');
       onSubmit();
       // mutateRewards();
     } catch (e) {
-      showMessage('Error creating rewards', 'error');
+      showMessage(`Error creating ${rewardsTitle}`, 'error');
     }
-  };
+  }
 
   return (
-    <Button disabled={disabled} color='success' endIcon={<BountyIcon />} loading={isMutating} onClick={createRewards}>
-      Publish Rewards
-    </Button>
+    <>
+      <Button
+        disabled={disabled}
+        disabledTooltip={`Only reviewers can publish ${rewardsTitle}`}
+        color='success'
+        endIcon={<BountyIcon />}
+        loading={isMutating}
+        onClick={() => setShowConfirmation(true)}
+      >
+        Publish {rewardsTitle}
+      </Button>
+      <ModalWithButtons
+        open={showConfirmation}
+        title={`Publish ${rewardsTitle}?`}
+        buttonText='Publish'
+        onClose={() => setShowConfirmation(false)}
+        // wrap the function so it does not return a promise to the confirmation modal
+        onConfirm={() => createRewards()}
+      >
+        <Typography>This action cannot be done</Typography>
+      </ModalWithButtons>
+    </>
   );
 }
