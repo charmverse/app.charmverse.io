3d6ba898c5ccbc42b578b7b56762030a2c38e8ef
motechFR <mo@anyahq.co>
2023-12-21 15:33:18 +0200

    Fix types

--- lib/proposal/updateProposalStatusOnly.ts
+++ lib/proposal/updateProposalStatusOnly.ts
@@ -2,7 +2,6 @@ import { ProposalStatus } from '@charmverse/core/prisma';
 import { prisma } from '@charmverse/core/prisma-client';
 
 import { InvalidStateError } from 'lib/middleware';
-import { getPermissionsClient } from 'lib/permissions/api';
 import { permissionsApiClient } from 'lib/permissions/api/client';
 import { InvalidInputError } from 'lib/utilities/errors';
 
