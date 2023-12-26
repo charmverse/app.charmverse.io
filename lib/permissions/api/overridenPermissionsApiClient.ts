55186f04ce3c1d75ec00f05f6b465f9b31104aa2
motechFR <mo@anyahq.co>
2023-12-27 00:40:30 +0200

    Add permissions API Client overrides

--- lib/permissions/api/overridenPermissionsApiClient.ts
+++ lib/permissions/api/overridenPermissionsApiClient.ts
@@ -2,15 +2,22 @@ import type { PagesRequest } from '@charmverse/core/pages';
 import type {
   BulkPagePermissionCompute,
   PermissionCompute,
-  ProposalPermissionsSwitch
+  ProposalPermissionsSwitch,
+  Resource
 } from '@charmverse/core/permissions';
 import { PermissionsApiClient, getSpaceInfoViaResource } from '@charmverse/core/permissions';
+import type { Space } from '@charmverse/core/prisma-client';
 
 import { permissionsApiAuthKey, permissionsApiUrl } from 'config/constants';
+import { isCharmVerseSpace } from 'lib/featureFlag/isCharmVerseSpace';
 
 // Injected method for expanding args
-function withUseProposalPermissionsArgs<T>(args: T): Required<ProposalPermissionsSwitch> & T {
-  return { useProposalEvaluationPermissions: true, ...args };
+function withUseProposalPermissionsArgs<T>(
+  space: Pick<Space, 'domain'> | undefined,
+  args: T
+): Required<ProposalPermissionsSwitch> & T {
+  const useProposalEvaluationPermissions = isCharmVerseSpace({ space });
+  return { useProposalEvaluationPermissions, ...args };
 }
 
 export class PermissionsApiClientWithPermissionsSwitch extends PermissionsApiClient {
@@ -27,7 +34,7 @@ export class PermissionsApiClientWithPermissionsSwitch extends PermissionsApiCli
     pages.computePagePermissions = async function (args: PermissionCompute) {
       const space = await getSpaceInfoViaResource({ resourceId: args.resourceId, resourceIdType: 'page' });
 
-      const injectedArgs = withUseProposalPermissionsArgs(args);
+      const injectedArgs = withUseProposalPermissionsArgs(space, args);
 
       const permissions = await originalComputePagePermissions.apply(this, [injectedArgs]);
       return permissions;
@@ -38,10 +45,46 @@ export class PermissionsApiClientWithPermissionsSwitch extends PermissionsApiCli
     pages.bulkComputePagePermissions = async function (args: BulkPagePermissionCompute) {
       const space = await getSpaceInfoViaResource({ resourceId: args.pageIds[0], resourceIdType: 'page' });
 
-      const injectedArgs = withUseProposalPermissionsArgs(args);
+      const injectedArgs = withUseProposalPermissionsArgs(space, args);
 
       const permissions = await originalBulkComputePagePermissions.apply(this, [injectedArgs]);
       return permissions;
+    };
+
+    // Override getReviewerPool method
+    const originalGetProposalReviewerPool = proposals.getProposalReviewerPool;
+
+    proposals.getProposalReviewerPool = async function (args: Resource) {
+      const space = await getSpaceInfoViaResource({ resourceId: args.resourceId, resourceIdType: 'proposalCategory' });
+
+      const injectedArgs = withUseProposalPermissionsArgs(space, args);
+
+      const reviewerPool = await originalGetProposalReviewerPool.apply(this, [injectedArgs]);
+      return reviewerPool;
+    };
+
+    // Override computeProposalCategoryPermissions method
+    const originalComputeProposalCategoryPermissions = proposals.computeProposalCategoryPermissions;
+
+    proposals.computeProposalCategoryPermissions = async function (args: PermissionCompute) {
+      const space = await getSpaceInfoViaResource({ resourceId: args.resourceId, resourceIdType: 'proposalCategory' });
+
+      const injectedArgs = withUseProposalPermissionsArgs(space, args);
+
+      const permissions = await originalComputeProposalCategoryPermissions.apply(this, [injectedArgs]);
+      return permissions;
+    };
+
+    // Override getAccessibleProposalCategories method
+    const originalGetAccessibleCategories = proposals.getAccessibleProposalCategories;
+
+    proposals.getAccessibleProposalCategories = async function (args: PagesRequest) {
+      const space = await getSpaceInfoViaResource({ resourceId: args.spaceId, resourceIdType: 'space' });
+
+      const injectedArgs = withUseProposalPermissionsArgs(space, args);
+
+      const pageIds = await originalGetAccessibleCategories.apply(this, [injectedArgs]);
+      return pageIds;
     };
 
     // Override getAccessiblePageIds method
@@ -50,7 +93,7 @@ export class PermissionsApiClientWithPermissionsSwitch extends PermissionsApiCli
     pages.getAccessiblePageIds = async function (args: PagesRequest) {
       const space = await getSpaceInfoViaResource({ resourceId: args.spaceId, resourceIdType: 'space' });
 
-      const injectedArgs = withUseProposalPermissionsArgs(args);
+      const injectedArgs = withUseProposalPermissionsArgs(space, args);
 
       const pageIds = await originalGetAccessiblePageIds.apply(this, [injectedArgs]);
       return pageIds;
@@ -62,7 +105,7 @@ export class PermissionsApiClientWithPermissionsSwitch extends PermissionsApiCli
     proposals.computeProposalPermissions = async function (args: PermissionCompute) {
       const space = await getSpaceInfoViaResource({ resourceId: args.resourceId, resourceIdType: 'proposal' });
 
-      const injectedArgs = withUseProposalPermissionsArgs(args);
+      const injectedArgs = withUseProposalPermissionsArgs(space, args);
 
       const permissions = await originalComputeProposalPermissions.apply(this, [injectedArgs]);
       return permissions;
@@ -74,7 +117,7 @@ export class PermissionsApiClientWithPermissionsSwitch extends PermissionsApiCli
     proposals.getAccessibleProposalIds = async function (args: PagesRequest) {
       const space = await getSpaceInfoViaResource({ resourceId: args.spaceId, resourceIdType: 'space' });
 
-      const injectedArgs = withUseProposalPermissionsArgs(args);
+      const injectedArgs = withUseProposalPermissionsArgs(space, args);
 
       const pageIds = await originalGetAccessibleProposalIds.apply(this, [injectedArgs]);
       return pageIds;
