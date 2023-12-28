9fb4b63b5198d73445d03b2f94e2bd71c0a80954
Valentin L <ludu.valentin@gmail.com>
2023-12-28 16:31:57 +0200

    Add alchemy rpcs

--- lib/tokenGates/unlock/client.ts
+++ lib/tokenGates/unlock/client.ts
@@ -2,16 +2,20 @@ import { InvalidInputError } from '@charmverse/core/errors';
 import { getChainById } from 'connectors/chains';
 import { createPublicClient, http } from 'viem';
 
+import { getAlchemyBaseUrl } from 'lib/blockchain/provider/alchemy/client';
+
 export const getPublicClient = (chainId: number) => {
   const chainDetails = getChainById(chainId);
-  const chain = chainDetails?.viem;
 
-  if (!chain) {
+  if (!chainDetails) {
     throw new InvalidInputError('Chain not supported');
   }
 
+  const provider = chainDetails.alchemyUrl ? getAlchemyBaseUrl(chainDetails.chainId) : chainDetails.rpcUrls[0];
+  const chain = chainDetails.viem;
+
   return createPublicClient({
     chain,
-    transport: http()
+    transport: http(provider)
   });
 };
