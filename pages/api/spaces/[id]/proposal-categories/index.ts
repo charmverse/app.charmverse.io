6846c2974c2509973c3a7005265f72f2e310d2b5
motechFR <mo@anyahq.co>
2023-12-31 06:12:00 +0200

    Fix using permissions for everyone in proposal categories

--- pages/api/spaces/[id]/proposal-categories/index.ts
+++ pages/api/spaces/[id]/proposal-categories/index.ts
@@ -1,8 +1,12 @@
-import type { ProposalCategoryWithPermissions } from '@charmverse/core/permissions';
+import {
+  AvailableProposalCategoryPermissions,
+  type ProposalCategoryWithPermissions
+} from '@charmverse/core/permissions';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import nc from 'next-connect';
 
 import { onError, onNoMatch, requireSpaceMembership } from 'lib/middleware';
+import { permissionsApiClient } from 'lib/permissions/api/client';
 import { providePermissionClients } from 'lib/permissions/api/permissionsClientMiddleware';
 import { createProposalCategory } from 'lib/proposal/createProposalCategory';
 import type { ProposalCategory } from 'lib/proposal/interface';
@@ -10,17 +14,14 @@ import { withSessionRoute } from 'lib/session/withSession';
 
 const handler = nc<NextApiRequest, NextApiResponse>({ onError, onNoMatch });
 
-handler
-  .use(providePermissionClients({ key: 'id', location: 'query', resourceIdType: 'space' }))
-  .get(getCategories)
-  .post(requireSpaceMembership({ adminOnly: true, spaceIdKey: 'id' }), createCategory);
+handler.get(getCategories).post(requireSpaceMembership({ adminOnly: true, spaceIdKey: 'id' }), createCategory);
 
 async function getCategories(req: NextApiRequest, res: NextApiResponse<ProposalCategoryWithPermissions[]>) {
   const spaceId = req.query.id as string;
 
   const userId = req.session.user?.id;
 
-  const categories = await req.basePermissionsClient.proposals.getAccessibleProposalCategories({
+  const categories = await permissionsApiClient.proposals.getAccessibleProposalCategories({
     spaceId,
     userId
   });
@@ -33,17 +34,7 @@ async function createCategory(req: NextApiRequest, res: NextApiResponse<Proposal
   const categoryData = req.body as Omit<ProposalCategory, 'id' | 'spaceId'>;
   const category = await createProposalCategory({ data: { ...categoryData, spaceId } });
 
-  if (req.spacePermissionsEngine === 'premium') {
-    await req.premiumPermissionsClient.proposals.assignDefaultProposalCategoryPermissions({
-      resourceId: category.id
-    });
-  }
-  const permissions = await req.basePermissionsClient.proposals.computeProposalCategoryPermissions({
-    resourceId: category.id,
-    userId: req.session.user?.id
-  });
-
-  return res.status(201).json({ ...category, permissions });
+  return res.status(201).json({ ...category, permissions: new AvailableProposalCategoryPermissions().full });
 }
 
 export default withSessionRoute(handler);
