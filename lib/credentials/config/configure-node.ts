900d8ef831987672a56af950716481660613a941
motechFR <mo@anyahq.co>
2024-01-01 19:24:50 +0200

    Commit node config

--- lib/credentials/config/configure-node.ts
+++ lib/credentials/config/configure-node.ts
@@ -1,21 +1,27 @@
-import { createComposite, writeEncodedComposite, readEncodedComposite } from '@composedb/devtools-node';
+import { createComposite, readEncodedComposite, writeEncodedComposite } from '@composedb/devtools-node';
 
 import { getCeramicClient } from './authenticate';
 
 async function generateCompositeFile() {
   const client = await getCeramicClient();
 
-  const models = await client.admin.getIndexedModels();
+  // await client.admin.pin.rm('kjzl6hvfrbw6c7nm0gnd3o3m5eep0zi0u6kbf6o2nfl3pxnn40mzcjhyezfs6ox' as any);
 
-  console.log({ models });
+  // const models = await client.admin.pin.ls();
 
-  // const compo = await readEncodedComposite(client, 'modelData');
+  // for (let i = 0; i < 3; i++) {
+  //   const item = await models[Symbol.asyncIterator]().next();
+  //   console.log({ item });
+  // }
+
+  // const compo = await readEncodedComposite(client, 'lib/credentials/config/credentials.gql');
 
   // console.log({ compo });
-  // // Replace by the path to the source schema file
-  const composite = await createComposite(client, 'lib/credentials/config/credentials.gql');
-  // Replace by the path to the encoded composite file
-  await writeEncodedComposite(composite, 'lib/credentials/config/composite.json');
+
+  // const composite = await createComposite(client, 'lib/credentials/config/credentials.gql');
+  // console.log({ composite });
+  // // Replace by the path to the encoded composite file
+  // await writeEncodedComposite(composite, 'lib/credentials/config/composite.json');
 }
 
 // eslint-disable-next-line no-console
