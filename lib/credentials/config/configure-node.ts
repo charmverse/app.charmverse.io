4b0e74fa1502d09e32263530569ba11397b99ef5
motechFR <mo@anyahq.co>
2024-01-01 18:54:08 +0200

    Setup basic composite deployment

--- lib/credentials/config/configure-node.ts
+++ lib/credentials/config/configure-node.ts
@@ -1,12 +1,22 @@
-import type { CeramicClient } from '@ceramicnetwork/http-client';
-import { readEncodedComposite, writeEncodedComposite } from '@composedb/devtools-node';
-import { DID } from 'dids';
-import { Ed25519Provider } from 'key-did-provider-ed25519';
-import { getResolver } from 'key-did-resolver';
+import { createComposite, writeEncodedComposite, readEncodedComposite } from '@composedb/devtools-node';
 
-async function createComposite(ceramic: CeramicClient, schemaPath: string) {
-  // Replace by the path to the source schema file
-  const composite = await createComposite(ceramic, './credentials.graphql');
+import { getCeramicClient } from './authenticate';
+
+async function generateCompositeFile() {
+  const client = await getCeramicClient();
+
+  const models = await client.admin.getIndexedModels();
+
+  console.log({ models });
+
+  // const compo = await readEncodedComposite(client, 'modelData');
+
+  // console.log({ compo });
+  // // Replace by the path to the source schema file
+  const composite = await createComposite(client, 'lib/credentials/config/credentials.gql');
   // Replace by the path to the encoded composite file
-  await writeEncodedComposite(composite, './credentials-composite.json');
+  await writeEncodedComposite(composite, 'lib/credentials/config/composite.json');
 }
+
+// eslint-disable-next-line no-console
+generateCompositeFile().then(() => console.log('Done!'));
