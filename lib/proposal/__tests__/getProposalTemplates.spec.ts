a267b2b787bd8faf4938cd0fa5786924d123c29b
motechFR <mo@anyahq.co>
2024-01-02 16:09:35 +0200

    Force usage of overriden permissions API (#3339)
    * Force usage of overriden permissions API
    
    * Fix tests
    
    * Fix tests

--- lib/proposal/__tests__/getProposalTemplates.spec.ts
+++ lib/proposal/__tests__/getProposalTemplates.spec.ts
@@ -4,52 +4,10 @@ import { testUtilsProposals, testUtilsUser } from '@charmverse/core/test';
 import { getProposalTemplates } from '../getProposalTemplates';
 
 describe('getProposalTemplates', () => {
-  it('should return only templates in proposal categories user can create a proposal in within a paid space', async () => {
+  it('should return all templates within a paid space', async () => {
     const { space, user: adminUser } = await testUtilsUser.generateUserAndSpace({
       isAdmin: true,
       spacePaidTier: 'community'
-    });
-
-    const createableCategory = await testUtilsProposals.generateProposalCategory({
-      spaceId: space.id,
-      proposalCategoryPermissions: [
-        {
-          permissionLevel: 'full_access',
-          assignee: { group: 'space', id: space.id }
-        }
-      ]
-    });
-
-    await testUtilsProposals.generateProposalTemplate({
-      spaceId: space.id,
-      userId: adminUser.id,
-      categoryId: createableCategory.id
-    });
-
-    const readonlyCategory = await testUtilsProposals.generateProposalCategory({
-      spaceId: space.id,
-      proposalCategoryPermissions: [
-        {
-          permissionLevel: 'view',
-          assignee: { group: 'space', id: space.id }
-        }
-      ]
-    });
-
-    await testUtilsProposals.generateProposalTemplate({
-      spaceId: space.id,
-      userId: adminUser.id,
-      categoryId: readonlyCategory.id
-    });
-
-    const invisibleCategory = await testUtilsProposals.generateProposalCategory({
-      spaceId: space.id
-    });
-
-    await testUtilsProposals.generateProposalTemplate({
-      spaceId: space.id,
-      userId: adminUser.id,
-      categoryId: invisibleCategory.id
     });
 
     const spaceMember = await testUtilsUser.generateSpaceUser({
@@ -57,22 +15,33 @@ describe('getProposalTemplates', () => {
       isAdmin: false
     });
 
+    const createableCategory = await testUtilsProposals.generateProposalCategory({
+      spaceId: space.id
+    });
+
+    const firstTemplate = await testUtilsProposals.generateProposalTemplate({
+      spaceId: space.id,
+      userId: adminUser.id,
+      categoryId: createableCategory.id
+    });
+
+    const secondCategory = await testUtilsProposals.generateProposalCategory({
+      spaceId: space.id
+    });
+
+    const secondTemplate = await testUtilsProposals.generateProposalTemplate({
+      spaceId: space.id,
+      userId: adminUser.id,
+      categoryId: createableCategory.id
+    });
+
     const templates = await getProposalTemplates({
       spaceId: space.id,
       userId: spaceMember.id
     });
 
-    expect(templates).toHaveLength(1);
-    expect(templates[0]).toMatchObject(
-      expect.objectContaining<Partial<ProposalWithUsers>>({
-        authors: expect.any(Array),
-        reviewers: expect.any(Array),
-        category: createableCategory,
-        categoryId: createableCategory.id,
-        createdBy: adminUser.id,
-        id: expect.any(String)
-      })
-    );
+    expect(templates).toHaveLength(2);
+    expect(templates.map((t) => t.id)).toMatchObject(expect.arrayContaining([firstTemplate.id, secondTemplate.id]));
   });
 
   it('should return an empty list for people outside the space since they cannot create proposals', async () => {
