0db3efb0d449df6aaf2e9ccfe51cebef74c03f9c
Valentin L <ludu.valentin@gmail.com>
2024-01-08 15:35:19 +0200

    Builder Protocol DAO Token Gate

--- components/common/TokenGateModal/components/TokenGateDao.tsx
+++ components/common/TokenGateModal/components/TokenGateDao.tsx
@@ -1,20 +1,30 @@
 import { yupResolver } from '@hookform/resolvers/yup';
+import { MenuItem, Select } from '@mui/material';
 import { litDaoChains } from 'connectors/chains';
 import { useForm } from 'react-hook-form';
 import * as yup from 'yup';
 
+import { FieldWrapper } from 'components/common/form/fields/FieldWrapper';
 import { TextInputField } from 'components/common/form/fields/TextInputField';
 import type { TokenGateConditions } from 'lib/tokenGates/interfaces';
 import { isValidChainAddress } from 'lib/tokens/validation';
 
 import { useTokenGateModal } from '../hooks/useTokenGateModalContext';
 import { getDaoUnifiedAccessControlConditions } from '../utils/getDaoUnifiedAccessControlConditions';
+import { daoCheck } from '../utils/utils';
 
 import { TokenGateBlockchainSelect } from './TokenGateBlockchainSelect';
 import { TokenGateFooter } from './TokenGateFooter';
 
+type DaoOptions = (typeof daoCheck)[number]['id'];
+
 const schema = yup.object({
-  chain: yup.string().required('Chain is required'),
+  chain: yup.string().when('check', {
+    is: (val: DaoOptions) => val === 'moloch',
+    then: () => yup.string().required('Chain is required'),
+    otherwise: () => yup.string()
+  }),
+  check: yup.string<DaoOptions>().required('DAO type is required'),
   contract: yup
     .string()
     .required('Contract is required')
@@ -28,12 +38,15 @@ export function TokenGateDao() {
     register,
     getValues,
     reset,
+    watch,
     formState: { errors, isValid }
   } = useForm<FormValues>({
     resolver: yupResolver(schema),
     mode: 'onChange',
-    defaultValues: { contract: '', chain: '' }
+    defaultValues: { contract: '', chain: '', check: undefined }
   });
+
+  const check = watch('check');
 
   const { setDisplayedPage, handleTokenGate } = useTokenGateModal();
 
@@ -52,12 +65,28 @@ export function TokenGateDao() {
 
   return (
     <>
-      <TokenGateBlockchainSelect
-        error={!!errors.chain?.message}
-        helperMessage={errors.chain?.message}
-        chains={litDaoChains}
-        {...register('chain')}
-      />
+      <FieldWrapper label='Select a DAO Membership'>
+        <Select<FormValues['check']>
+          displayEmpty
+          fullWidth
+          renderValue={(selected) => daoCheck.find((c) => c.id === selected)?.name || selected || 'Select...'}
+          {...register('check')}
+        >
+          {daoCheck.map((type) => (
+            <MenuItem key={type.id} value={type.id}>
+              {type.name}
+            </MenuItem>
+          ))}
+        </Select>
+      </FieldWrapper>
+      {check === 'moloch' && (
+        <TokenGateBlockchainSelect
+          error={!!errors.chain?.message}
+          helperMessage={errors.chain?.message}
+          chains={litDaoChains}
+          {...register('chain')}
+        />
+      )}
       <TextInputField
         label='DAO Contract Address'
         error={errors.contract?.message}
--- components/common/TokenGateModal/utils/getDaoUnifiedAccessControlConditions.ts
+++ components/common/TokenGateModal/utils/getDaoUnifiedAccessControlConditions.ts
@@ -4,21 +4,40 @@ import { getChainById } from 'connectors/chains';
 import type { FormValues } from '../components/TokenGateDao';
 
 export function getDaoUnifiedAccessControlConditions(values: FormValues): UnifiedAccessControlConditions | undefined {
-  const { chain, contract } = values;
+  const { chain, contract, check } = values;
   const chainName = getChainById(Number(chain))?.litNetwork || 'ethereum';
 
-  return [
-    {
-      conditionType: 'evmBasic',
-      contractAddress: contract,
-      standardContractType: 'MolochDAOv2.1',
-      chain: chainName,
-      method: 'members',
-      parameters: [':userAddress'],
-      returnValueTest: {
-        comparator: '=',
-        value: 'true'
+  if (check === 'moloch') {
+    return [
+      {
+        conditionType: 'evmBasic',
+        contractAddress: contract,
+        standardContractType: 'MolochDAOv2.1',
+        chain: chainName,
+        method: 'members',
+        parameters: [':userAddress'],
+        returnValueTest: {
+          comparator: '=',
+          value: 'true'
+        }
       }
-    }
-  ];
+    ];
+  } else if (check === 'builder') {
+    return [
+      {
+        conditionType: 'evmBasic' as const,
+        contractAddress: contract,
+        standardContractType: 'ERC721',
+        chain: 'ethereum',
+        method: 'balanceOf',
+        parameters: [':userAddress'],
+        returnValueTest: {
+          comparator: '>=',
+          value: '1'
+        }
+      }
+    ];
+  }
+
+  return [];
 }
