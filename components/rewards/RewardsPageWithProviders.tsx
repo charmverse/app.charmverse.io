8eb8515360443e4a85ce9e3adc52eb7c6ab6184f
mattcasey <mattwad@gmail.com>
2024-01-09 19:06:45 -0700

    move rewards block context up as well

--- components/rewards/RewardsPageWithProviders.tsx
+++ components/rewards/RewardsPageWithProviders.tsx
@@ -1,21 +1,13 @@
 import { PageDialogProvider } from 'components/common/PageDialog/hooks/usePageDialog';
 import { PageDialogGlobal } from 'components/common/PageDialog/PageDialogGlobal';
 import { RewardsProvider } from 'components/rewards/hooks/useRewards';
-import { RewardsBoardProvider } from 'components/rewards/hooks/useRewardsBoard';
 import { RewardsPage } from 'components/rewards/RewardsPage';
 import { useStaticPageTitle } from 'hooks/usePageTitle';
-import { RewardBlocksProvider } from 'hooks/useRewardBlocks';
 import { useSpaceFeatures } from 'hooks/useSpaceFeatures';
 
 export function RewardsPageWithProviders() {
   const { mappedFeatures } = useSpaceFeatures();
   const rewardsTitle = mappedFeatures.rewards.title;
   useStaticPageTitle(rewardsTitle);
-  return (
-    <RewardBlocksProvider>
-      <RewardsBoardProvider>
-        <RewardsPage title={rewardsTitle} />
-      </RewardsBoardProvider>
-    </RewardBlocksProvider>
-  );
+  return <RewardsPage title={rewardsTitle} />;
 }
