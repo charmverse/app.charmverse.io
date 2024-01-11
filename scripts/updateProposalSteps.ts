2e9c3ebe8197ab1a37e28973c0d818172ec751ee
mattcasey <mattwad@gmail.com>
2024-01-11 12:50:05 -0700

    fix script

--- scripts/updateProposalSteps.ts
+++ scripts/updateProposalSteps.ts
@@ -1,32 +1,26 @@
-import { Page, Proposal, Space, Vote, VoteStatus } from '@charmverse/core/prisma';
+import { Page, Proposal, ProposalReviewer, Vote, VoteStatus } from '@charmverse/core/prisma';
 import { prisma } from '@charmverse/core/prisma-client';
 import { DataNotFoundError, InvalidInputError } from 'lib/utilities/errors';
 import { getVote } from 'lib/votes/getVote';
 import { ExtendedVote } from 'lib/votes/interfaces';
 
-export async function updateProposalSteps({
-  spaceDomain,
-  templateTitles,
-  validateOnly
-}: {
-  spaceDomain: string;
-  templateTitles: string[];
-  validateOnly: true;
-}) {
+export async function updateProposalSteps({ spaceDomain }: { spaceDomain: string }) {
   const templates = await prisma.page.findMany({
     where: {
       space: {
         domain: spaceDomain
       },
-      type: 'proposal_template',
-      title: {
-        in: templateTitles
-      }
+      type: 'proposal_template'
     },
     include: {
       proposal: {
         include: {
-          evaluations: true
+          evaluations: {
+            include: {
+              rubricCriteria: true,
+              reviewers: true
+            }
+          }
         }
       }
     }
@@ -40,10 +34,17 @@ export async function updateProposalSteps({
         }
       },
       include: {
-        evaluations: true
+        evaluations: {
+          include: {
+            rubricCriteria: true,
+            reviewers: true
+          }
+        }
       }
     });
-    const evaluationIndicesToUpdate: { evaluationId: string; index: number }[] = [];
+    const evaluationIndicesToUpdate: { id: string; index: number }[] = [];
+    const reviewersToSet: { evaluationId: string; reiewers: ProposalReviewer[] }[] = [];
+    let proposalsToUpdate = new Set();
     for (let proposal of proposals) {
       // Match the evaluations of each proposal with the template evaluations and make sure the index is correct
       const templateEvaluations = template.proposal?.evaluations;
@@ -54,40 +55,85 @@ export async function updateProposalSteps({
       if (!proposalEvaluations) {
         throw new DataNotFoundError('Proposal evaluations not found');
       }
-      if (templateEvaluations.length !== proposalEvaluations.length) {
-        throw new InvalidInputError('Template evaluations length does not match proposal evaluations length');
-      }
+      // if (templateEvaluations.length !== proposalEvaluations.length) {
+      //   throw new InvalidInputError('Template evaluations length does not match proposal evaluations length');
+      // }
+      // let hasMatch = 0;
+      // let hasCorrectIndex = 0;
+      // templateEvaluations.forEach((templateEvaluation) => {
+      //   const match = proposalEvaluations.find((e) => e.title === templateEvaluation.title);
+      //   if (match) {
+      //     hasMatch++;
+      //   }
+      //   if (match?.index === templateEvaluation.index) {
+      //     hasCorrectIndex++;
+      //   }
+      // });
+      // const everyStepHasMatch = hasMatch === templateEvaluations.length;
+      // // if (hasMatch === templateEvaluations.length && hasCorrectIndex !== templateEvaluations.length) {
+      // //   console.log('Fix indicies', {
+      // //     template: template.title,
+      // //     proposal: proposal.title
+      // //   });
+      // //   continue;
+      // // }
       for (const templateEvaluation of templateEvaluations) {
         const proposalEvaluation = proposalEvaluations.find((e) => e.title === templateEvaluation.title);
         if (!proposalEvaluation) {
-          console.log('DUMP: template evaluations', templateEvaluations);
-          console.log('DUMP: proposal evaluations', proposalEvaluations);
-          throw new InvalidInputError('Could not find a matching evaluation for the template evaluation');
+          continue;
         }
-        if (proposalEvaluation.index !== templateEvaluation.index) {
-          evaluationIndicesToUpdate.push({
+        if (proposalEvaluation.type === 'rubric') {
+          const everyCriteriaHasMatch = templateEvaluation.rubricCriteria.every((criteria) =>
+            proposalEvaluation.rubricCriteria.some((c) => c.title === criteria.title)
+          );
+          if (!everyCriteriaHasMatch) {
+            throw new Error('Invalid criteria match');
+          }
+          for (const criteria of templateEvaluation.rubricCriteria) {
+            // compare index to matching criteria from proposal
+            const match = proposalEvaluation.rubricCriteria.find((c) => c.title === criteria.title);
+            if (criteria.index !== match!.index) {
+              evaluationIndicesToUpdate.push({
+                id: match!.id,
+                index: criteria.index
+              });
+              proposalsToUpdate.add(proposal.id);
+            }
+          }
+        }
+        const sameReviewers =
+          templateEvaluation.reviewers.length === proposalEvaluation.reviewers.length &&
+          templateEvaluation.reviewers.every((reviewer) => {
+            return proposalEvaluation.reviewers.some(
+              (r) =>
+                r.userId === reviewer.userId || r.roleId === reviewer.roleId || r.systemRole === reviewer.systemRole
+            );
+          });
+        if (!sameReviewers) {
+          reviewersToSet.push({
             evaluationId: proposalEvaluation.id,
-            index: templateEvaluation.index
+            reiewers: templateEvaluation.reviewers
           });
-        } else {
-          console.log('Evaluation index is correct', {
-            template: templateEvaluation.title,
-            templateEval: templateEvaluation.index,
-            proposalEval: proposalEvaluation.index
-          });
+          proposalsToUpdate.add(proposal.id);
         }
       }
     }
     console.log('Changes to be made', {
       template: template.title,
       proposals: proposals.length,
-      evaluationIndicesToUpdate: evaluationIndicesToUpdate.length
+      proposalsToUpdate: proposalsToUpdate.size,
+      evaluationIndicesToUpdate: evaluationIndicesToUpdate.length,
+      reviewersToSet: reviewersToSet.length
     });
+    // await Promise.all(
+    //   evaluationIndicesToUpdate.map((e) =>
+    //     prisma.proposalRubricCriteria.update({ where: { id: e.id }, data: { index: e.index } })
+    //   )
+    // );
   }
 }
 
 updateProposalSteps({
   spaceDomain: 'taiko',
-  templateTitles: ['Community Track', 'Partner Track', 'RFP-001'],
   validateOnly: true
 }).catch(console.error);
