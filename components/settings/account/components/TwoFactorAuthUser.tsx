41be13f2ffb56f1d56982cd46d634ee5ad9d2f7e
Valentin L <ludu.valentin@gmail.com>
2024-01-15 12:31:09 +0200

    Add feature flag

--- components/settings/account/components/TwoFactorAuthUser.tsx
+++ components/settings/account/components/TwoFactorAuthUser.tsx
@@ -7,12 +7,19 @@ import { Button } from 'components/common/Button';
 import { TwoFactorAuthSetupModal } from 'components/settings/account/components/otp/components/TwoFactorAuthModal';
 import { TwoFactorAuthProvider } from 'components/settings/account/components/otp/hooks/useTwoFactorAuth';
 import Legend from 'components/settings/Legend';
+import { useIsCharmverseSpace } from 'hooks/useIsCharmverseSpace';
 import { useUser } from 'hooks/useUser';
 
 export function TwoFactorAuthUser() {
   const otpSetupModal = usePopupState({ variant: 'popover', popupId: 'otp-setup' });
   const { user } = useUser();
   const activeOtp = !!user?.userOTP?.activatedAt;
+
+  const isCharmverseSpace = useIsCharmverseSpace();
+
+  if (!isCharmverseSpace) {
+    return null;
+  }
 
   return (
     <TwoFactorAuthProvider onClose={otpSetupModal.close}>
