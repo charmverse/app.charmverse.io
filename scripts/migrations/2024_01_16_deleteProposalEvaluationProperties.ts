b71003cf74f3ceb6e82cac4778490a7faf32e206
Safwan Shaheer <devorein00@gmail.com>
2024-01-16 20:56:11 +0600

    Added migration script to delete previous proposal evaluation properties

---
+++ scripts/migrations/2024_01_16_deleteProposalEvaluationProperties.ts
@@ -0,0 +1,43 @@
+import { prisma } from "@charmverse/core/prisma-client";
+import { BoardFields } from 'lib/focalboard/board';
+
+async function deleteProposalEvaluationProperties() {
+  const proposalBoardBlocks = await prisma.block.findMany({
+    where: {
+      type: "board",
+      fields: {
+        path: ["sourceType"],
+        equals: "proposals"
+      }
+    },
+    select: {
+      id: true,
+      fields: true,
+    }
+  })
+
+  const totalBoardBlocks = proposalBoardBlocks.length;
+  let currentBlock = 0;
+  for (const proposalBoardBlock of proposalBoardBlocks) {
+    try {
+      await prisma.block.update({
+        where: {
+          id: proposalBoardBlock.id
+        },
+        data: {
+          fields: {
+            ...proposalBoardBlock.fields as unknown as BoardFields,
+            cardProperties: ((proposalBoardBlock.fields as unknown as BoardFields).cardProperties ?? []).filter(boardCardProperty => boardCardProperty.type !== "proposalEvaluatedBy" && boardCardProperty.type !== "proposalEvaluationAverage" && boardCardProperty.type !== "proposalEvaluationTotal")
+          } as any
+        }
+      })
+    } catch (_) {
+      console.log(`Failed to update board block ${proposalBoardBlock.id}`)
+    } finally {
+      currentBlock++;
+      console.log(`Updated ${currentBlock} of ${totalBoardBlocks} board blocks`)
+    }
+  }
+}
+
+deleteProposalEvaluationProperties()
