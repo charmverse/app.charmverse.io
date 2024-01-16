9df0bd67205031de6d4b1a6e3efb5530f4c0c126
Valentin L <ludu.valentin@gmail.com>
2024-01-16 18:25:54 +0200

    Add deletedAt on deletion

--- lib/profile/otp/deleteUserOtp.ts
+++ lib/profile/otp/deleteUserOtp.ts
@@ -1,9 +1,28 @@
 import { prisma } from '@charmverse/core/prisma-client';
 
 export async function deleteUserOtp(userId: string) {
-  await prisma.userOTP.delete({
+  const userOtp = await prisma.userOTP.update({
     where: {
       userId
+    },
+    data: {
+      deletedAt: new Date(),
+      recoveryCode: {
+        update: {
+          deletedAt: new Date()
+        }
+      }
+    }
+  });
+
+  await prisma.user.update({
+    where: {
+      id: userId
+    },
+    data: {
+      userOTP: {
+        disconnect: true
+      }
     }
   });
 }
