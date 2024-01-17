b8fd1664e812a649a3ee0f8f37cae2777cf2df90
Marek Epicode <m.sadura@outlook.com>
2024-01-17 16:17:17 +0100

    Archive proposals script

---
+++ scripts/archiveSpaceProposals.ts
@@ -0,0 +1,46 @@
+import { prisma } from '@charmverse/core/prisma-client';
+import { archiveProposal } from 'lib/proposal/archiveProposal';
+import { createBotUser } from 'lib/spaces/createBotUser';
+
+
+const spaceDomain = 'op-grants';
+
+async function archiveSpaceProposals() {
+  if (!spaceDomain) {
+    throw new Error('Please set spaceDomain');
+  }
+
+  const space = await prisma.space.findUnique({ where: { domain: spaceDomain }, select: { id: true } });
+
+  if (!space) {
+    throw new Error(`Space with domain "${spaceDomain}" not found`);
+  }
+
+  const botUser = await prisma.spaceRole.findFirst({ where: { spaceId: space.id, user: { isBot: true } } });
+  let actorId = botUser?.userId;
+
+  if (!botUser) {
+    const bot = await createBotUser(space.id);
+    actorId = bot.id;
+  }
+
+  if (!actorId) {
+    throw new Error(`Bot user for the space not found`);
+  }
+
+  const proposalsToArchive = await prisma.proposal.findMany({ where: { archived: false, spaceId: space.id }, select: { id: true } });
+
+  console.log('🔥', 'Number of proposals to be archived:', proposalsToArchive.length);
+
+  let numOfArchivedProposals = 0;
+  for (const proposal of proposalsToArchive) {
+    await archiveProposal({ archived: true, proposalId: proposal.id, actorId });
+
+    numOfArchivedProposals++;
+    console.log('🔥', 'Archived proposal no:', numOfArchivedProposals);
+  }
+}
+
+archiveSpaceProposals().then(() => {
+  console.log('🔥', 'done');
+});
