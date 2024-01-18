909da00f2c17ff797d555b28abdfe5ff9a13600f
motechFR <mo@anyahq.co>
2024-01-19 00:45:51 +0200

    Add feature flag on tabs

--- components/members/components/MemberProfile/components/ProfileTabs.tsx
+++ components/members/components/MemberProfile/components/ProfileTabs.tsx
@@ -1,8 +1,10 @@
 import { Stack } from '@mui/material';
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 
+import type { TabConfig } from 'components/common/MultiTabs';
 import MultiTabs from 'components/common/MultiTabs';
 import { ProfileWidgets } from 'components/members/components/MemberProfile/components/ProfileWidgets/ProfileWidgets';
+import { useIsCharmverseSpace } from 'hooks/useIsCharmverseSpace';
 import type { Member } from 'lib/members/interfaces';
 import type { LoggedInUser } from 'models';
 import type { PublicUser } from 'pages/api/public/profile/[userId]';
@@ -13,22 +15,27 @@ import { UserSpacesList } from './UserSpacesList/UserSpacesList';
 export function ProfileTabs(props: { user: Member | PublicUser | LoggedInUser; readOnly?: boolean }) {
   const { readOnly } = props;
 
+  const showCredentialsTab = useIsCharmverseSpace();
+
   const [activeTab, setActiveTab] = useState(0);
-  return (
-    <MultiTabs
-      activeTab={activeTab}
-      setActiveTab={setActiveTab}
-      tabs={[
-        ['Profile', <ProfileWidgets key='profile' readOnly={readOnly} userId={props.user.id} />, { sx: { px: 0 } }],
-        [
-          'Organizations',
-          <Stack key='organizations'>
-            <UserSpacesList userId={props.user.id} />
-          </Stack>,
-          { sx: { px: 0 } }
-        ],
-        ['Credentials', <UserCredentialsList key='credentials' userId={props.user.id} />]
-      ]}
-    />
-  );
+
+  const tabs: TabConfig[] = useMemo(() => {
+    const _tabs = [
+      ['Profile', <ProfileWidgets key='profile' readOnly={readOnly} userId={props.user.id} />, { sx: { px: 0 } }],
+      [
+        'Organizations',
+        <Stack key='organizations'>
+          <UserSpacesList userId={props.user.id} />
+        </Stack>,
+        { sx: { px: 0 } }
+      ]
+    ];
+
+    if (showCredentialsTab) {
+      _tabs.push(['Credentials', <UserCredentialsList key='credentials' userId={props.user.id} />]);
+    }
+    return _tabs as TabConfig[];
+  }, [readOnly, props.user.id, showCredentialsTab]);
+
+  return <MultiTabs activeTab={activeTab} setActiveTab={setActiveTab} tabs={tabs} />;
 }
