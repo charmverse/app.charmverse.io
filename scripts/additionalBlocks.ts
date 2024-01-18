e2abb44ab2a0304e41771f561bf4c7b778c544d3
Safwan Shaheer <devorein00@gmail.com>
2024-01-19 01:45:55 +0600

    PR comments resolved

--- scripts/additionalBlocks.ts
+++ scripts/additionalBlocks.ts
@@ -1,12 +1,12 @@
 import { prisma } from '@charmverse/core/prisma-client';
 import { DateTime } from 'luxon';
 
-async function additionalBlockQuotasForSpaces() {
-  const spaceDomain = "cvt-demo-space";
-  const blockCount = 20;
-  // One year from today, keep it empty to allow perpetual blocks
-  const expiresAt = DateTime.local().plus({ years: 1 }).endOf('day').toJSDate();
+const spaceDomain = "cvt-demo-space";
+const blockCount = 20;
+// One year from today, keep it empty/undefined to allow perpetual blocks
+const expiresAt = DateTime.local().plus({ years: 1 }).endOf('day').toJSDate();
 
+async function additionalBlockQuotasForSpaces() {
   const space = await prisma.space.findUniqueOrThrow({
     where: {
       domain: spaceDomain
@@ -16,7 +16,7 @@ async function additionalBlockQuotasForSpaces() {
     }
   })
 
-  await prisma.additionalBlockQuota.createMany({
+  await prisma.additionalBlockQuota.create({
     data: {
       spaceId: space.id,
       blockCount,
