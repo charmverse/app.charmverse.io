7f3f7bfd8852db39084f1507beb68d9ad011794d
Valentin L <ludu.valentin@gmail.com>
2024-01-18 20:04:51 +0200

    Add custom text for setup modal

--- components/settings/account/components/otp/components/SetupTwoFactorAuthGlobal.tsx
+++ components/settings/account/components/otp/components/SetupTwoFactorAuthGlobal.tsx
@@ -1,3 +1,5 @@
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 import { bindPopover, usePopupState } from 'material-ui-popup-state/hooks';
 import { useEffect } from 'react';
 
@@ -32,5 +34,18 @@ export function SetupTwoFactorAuthGlobal() {
 
   const onClose = activeOtp && spaceRequiresOtp ? otpSetupModal.close : undefined;
 
-  return <TwoFactorAuthSetupModal {...bindPopover(otpSetupModal)} onClose={onClose} />;
+  return (
+    <TwoFactorAuthSetupModal
+      {...bindPopover(otpSetupModal)}
+      onClose={onClose}
+      title={
+        <Box>
+          <Typography variant='h5' mb={1}>
+            Secure your account
+          </Typography>
+          <Typography variant='body2'>This space requires two factor authentication</Typography>
+        </Box>
+      }
+    />
+  );
 }
