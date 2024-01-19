4dc39ee4adf131e0fc0ebba8f7607255f78af478
Marek Epicode <m.sadura@outlook.com>
2024-01-19 12:18:05 +0100

    Fix wrapping db columns

--- components/common/BoardEditor/components/properties/UserAndRoleSelect.tsx
+++ components/common/BoardEditor/components/properties/UserAndRoleSelect.tsx
@@ -267,7 +267,7 @@ export function UserAndRoleSelect<T extends { id: string; group: string } = Sele
         onClick={onClickToEdit}
       >
         <Tooltip title={readOnlyMessage ?? null}>
-          <Box display='inline-flex' flexWrap='wrap' gap={0.5}>
+          <Box display='inline-flex' flexWrap={wrapColumn ? 'wrap' : 'nowrap'} gap={0.5}>
             {applicableValues.length === 0 ? (
               showEmptyPlaceholder && <EmptyPlaceholder>{emptyPlaceholderContent}</EmptyPlaceholder>
             ) : (
