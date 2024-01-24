80493297430657b4eee31d16daf2fa5384df91d7
mattcasey <mattwad@gmail.com>
2024-01-24 13:37:29 -0700

    allow clearing out authors for proposal templates

--- lib/proposal/__tests__/updateProposal.spec.ts
+++ lib/proposal/__tests__/updateProposal.spec.ts
@@ -1,4 +1,5 @@
 import type { Space, User } from '@charmverse/core/prisma';
+import { prisma } from '@charmverse/core/prisma-client';
 import { testUtilsProposals } from '@charmverse/core/test';
 
 import { InvalidStateError } from 'lib/middleware';
@@ -11,7 +12,6 @@ let author2: User;
 let reviewer1: User;
 let reviewer2: User;
 let space: Space;
-
 beforeAll(async () => {
   const { user: user1, space: generatedSpace } = await generateUserAndSpaceWithApiToken();
   const { user: user2 } = await generateUserAndSpaceWithApiToken();
@@ -40,5 +40,28 @@ describe('Update proposal specific data', () => {
         actorId: author1.id
       })
     ).rejects.toBeInstanceOf(InvalidStateError);
+  });
+
+  it('Should not throw error if at least one author is not selected for a proposal template', async () => {
+    // Create a test proposal first
+    const result = await testUtilsProposals.generateProposal({
+      spaceId: space.id,
+      userId: author1.id
+    });
+    // TODO: allow setting page type in mock util
+    await prisma.page.update({
+      where: {
+        id: result.page.id
+      },
+      data: {
+        type: 'proposal_template'
+      }
+    });
+
+    await updateProposal({
+      proposalId: result.id,
+      authors: [],
+      actorId: author1.id
+    });
   });
 });
