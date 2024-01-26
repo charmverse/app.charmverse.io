562a1b893899b4bd7ddc85a52cfe1c9fb4f096fe
Matt Casey <mattwad@gmail.com>
2024-01-26 07:16:47 -0700

    add ability to add a workflow or reward template from proposal page (#3510)
    * add ability to add a workflow or reward template from proposal page
    
    * .
    
    * disable add new for rewards
    
    * update snapshots
    
    * fix class
    
    * remove prop
    
    * remove unused prop

--- components/proposals/ProposalPage/components/WorkflowSelect.tsx
+++ components/proposals/ProposalPage/components/WorkflowSelect.tsx
@@ -5,9 +5,12 @@ import { useState } from 'react';
 import { useGetProposalWorkflows } from 'charmClient/hooks/spaces';
 import { PropertyLabel } from 'components/common/BoardEditor/components/properties/PropertyLabel';
 import { TagSelect } from 'components/common/BoardEditor/components/properties/TagSelect/TagSelect';
+import { Button } from 'components/common/Button';
 import { useConfirmationModal } from 'hooks/useConfirmationModal';
 import { useCurrentSpace } from 'hooks/useCurrentSpace';
+import { useSettingsDialog } from 'hooks/useSettingsDialog';
 import { useSnackbar } from 'hooks/useSnackbar';
+import type { IPropertyOption } from 'lib/focalboard/board';
 
 type Props = {
   onChange?: (value: ProposalWorkflowTyped) => void;
@@ -18,15 +21,23 @@ type Props = {
 };
 
 export function WorkflowSelect({ onChange, value, readOnly, required, requireConfirmation }: Props) {
+  const { openSettings } = useSettingsDialog();
   const { space: currentSpace } = useCurrentSpace();
   const { data: workflowOptions } = useGetProposalWorkflows(currentSpace?.id);
   const { showConfirmation } = useConfirmationModal();
   const { showMessage } = useSnackbar();
-  const propertyOptions = (workflowOptions || []).map((option) => ({
+  const propertyOptions: IPropertyOption[] = (workflowOptions || []).map((option) => ({
     id: option.id,
     value: option.title,
     color: 'grey'
   }));
+
+  propertyOptions.push({
+    id: 'add_new',
+    value: '+ Add New',
+    color: 'gray',
+    variant: 'plain'
+  });
 
   async function changeWorkflow(newValue: string) {
     const option = workflowOptions?.find(({ id }) => id === newValue);
@@ -42,6 +53,15 @@ export function WorkflowSelect({ onChange, value, readOnly, required, requireCon
   async function onConfirmValueChange(values: string | string[]) {
     const newValue = Array.isArray(values) ? values[0] : values;
     if (!newValue) {
+      return;
+    }
+    if (newValue === 'add_new') {
+      openSettings('proposals');
+      // open the new workflow input after the settings dialog is open
+      setTimeout(() => {
+        const btnElement = document.getElementById('new-workflow-btn');
+        btnElement?.click();
+      }, 100);
       return;
     }
     // no confirmation needed for draft or feedback
