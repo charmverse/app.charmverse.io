9647a5df6acc37e5e5e8c01ceccdbe9fb019c885
Safwan Shaheer <devorein00@gmail.com>
2024-01-29 17:38:16 +0600

    Update proposal list endpoint response structure

--- pages/api/v1/proposals/index.ts
+++ pages/api/v1/proposals/index.ts
@@ -1,9 +1,10 @@
-import type { ProposalStatus } from '@charmverse/core/prisma';
+import type { ProposalEvaluationType } from '@charmverse/core/prisma';
 import { prisma } from '@charmverse/core/prisma-client';
 import { getCurrentEvaluation } from '@charmverse/core/proposals';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 import { InvalidStateError } from 'lib/middleware';
+import type { ProposalEvaluationResultExtended } from 'lib/proposal/interface';
 import { generateMarkdown } from 'lib/prosemirror/plugins/markdown/generateMarkdown';
 import { apiHandler } from 'lib/public-api/handler';
 import { withSessionRoute } from 'lib/session/withSession';
@@ -77,13 +78,6 @@ type ProposalReviewer = {
  *          type: array
  *          items:
  *            $ref: '#/components/schemas/ProposalReviewer'
- *        status:
- *          type: string
- *          example: vote_active
- *          enum:
- *            - draft
- *            - published
- *            - vote_active
  *        title:
  *          type: string
  *          example: EIP-4361 Sign in with Ethereum
@@ -94,7 +88,28 @@ type ProposalReviewer = {
  *          type: array
  *          items:
  *            type: string
- *
+ *        currentStep:
+ *          type: object
+ *          nullable: true
+ *          properties:
+ *            title:
+ *              type: string
+ *              example: Vote
+ *            result:
+ *              type: string
+ *              example: in_progress
+ *              enum:
+ *                - in_progress
+ *                - pass
+ *                - fail
+ *            type:
+ *              type: string
+ *              example: vote
+ *              enum:
+ *                - vote
+ *                - rubric
+ *                - pass_fail
+ *                - feedback
  */
 export type PublicApiProposal = {
   id: string;
@@ -105,10 +120,14 @@ export type PublicApiProposal = {
   };
   authors: ProposalAuthor[];
   reviewers: ProposalReviewer[];
-  status: ProposalStatus | 'vote_active';
   title: string;
   url: string;
   voteOptions?: string[];
+  currentStep: {
+    title: string;
+    result: ProposalEvaluationResultExtended;
+    type: ProposalEvaluationType;
+  } | null;
 };
 
 handler.get(listProposals);
@@ -156,7 +175,6 @@ async function listProposals(req: NextApiRequest, res: NextApiResponse<PublicApi
       },
       select: {
         id: true,
-        status: true,
         evaluations: true,
         page: {
           select: {
@@ -222,7 +240,6 @@ async function listProposals(req: NextApiRequest, res: NextApiResponse<PublicApi
 
   const publicApiProposalList: PublicApiProposal[] = proposals.map((proposal, index) => {
     const currentEvaluation = getCurrentEvaluation(proposal.evaluations);
-    const isActiveVote = currentEvaluation?.result === null && currentEvaluation?.type === 'vote';
     const apiProposal: PublicApiProposal = {
       id: proposal.id,
       createdAt: proposal.page?.createdAt as any,
@@ -232,7 +249,13 @@ async function listProposals(req: NextApiRequest, res: NextApiResponse<PublicApi
         text: proposal.page?.contentText ?? '',
         markdown: markdownTexts[index]
       },
-      status: isActiveVote ? 'vote_active' : proposal.status,
+      currentStep: currentEvaluation
+        ? {
+            result: currentEvaluation.result ?? 'in_progress',
+            title: currentEvaluation.title,
+            type: currentEvaluation.type
+          }
+        : null,
       authors: proposal.authors.map((author) => ({
         userId: author.author?.id,
         address: author.author?.wallets[0]?.address,
