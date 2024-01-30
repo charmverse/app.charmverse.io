f4b5cc570531b5b907c69fd270c897b20ecceccb
motechFR <mo@anyahq.co>
2024-01-30 18:02:14 +0200

    Cleaner approach

--- components/proposals/ProposalPage/components/EvaluationSettingsSidebar/components/VoteSettings.tsx
+++ components/proposals/ProposalPage/components/EvaluationSettingsSidebar/components/VoteSettings.tsx
@@ -43,14 +43,6 @@ export function VoteSettings({ readOnly, value, onChange }: CreateVoteModalProps
   const [durationDays, setDurationDays] = useState(value?.durationDays ?? 5);
   const [publishToSnapshot, setPublishToSnapshot] = useState(value?.publishToSnapshot ?? false);
 
-  useEffect(() => {
-    if (voteType === VoteType.SingleChoice && !value?.options?.length) {
-      setOptions(['Option 1', 'Option 2', 'Abstain']);
-    } else if (voteType === VoteType.Approval) {
-      setOptions(['Yes', 'No', 'Abstain']);
-    }
-  }, [voteType]);
-
   // useEffect on the values to call onChange() doesnt seem ideal and triggers on the first load, but it works for now. TODO: use react-hook-form?
   useEffect(() => {
     if (onChange) {
@@ -71,6 +63,17 @@ export function VoteSettings({ readOnly, value, onChange }: CreateVoteModalProps
       }
     }
   }, [voteType, options, maxChoices, durationDays, passThreshold, publishToSnapshot]);
+
+  function handleVoteTypeChange(_voteType: VoteType) {
+    if (_voteType !== value?.type) {
+      setVoteType(_voteType);
+      if (_voteType === VoteType.Approval) {
+        setOptions(['Yes', 'No', 'Abstain']);
+      } else if (_voteType === VoteType.SingleChoice) {
+        setOptions(['Option 1', 'Option 2', 'Abstain']);
+      }
+    }
+  }
 
   return (
     <StyledVoteSettings data-test='evaluation-vote-settings'>
@@ -121,7 +124,7 @@ export function VoteSettings({ readOnly, value, onChange }: CreateVoteModalProps
             defaultValue={voteType}
             value={voteType}
             onChange={(e) => {
-              setVoteType(e.target.value as VoteType);
+              handleVoteTypeChange(e.target.value as VoteType);
             }}
             sx={{ mb: 1 }}
           >
