5c58d972bafb5f12157c72884cfb00063ee895f8
mattcasey <mattwad@gmail.com>
2024-01-30 14:31:16 -0700

    enable comments for structured proposals

--- components/common/PageActions/components/DocumentPageActionList.tsx
+++ components/common/PageActions/components/DocumentPageActionList.tsx
@@ -237,7 +237,7 @@ export function DocumentPageActionList({
           label={<Typography variant='body2'>Full width</Typography>}
         />
       </ListItemButton>
-      {!isInsideDialog && !isStructuredProposal && (
+      {!isInsideDialog && (
         <>
           <Divider />
           <ListItemButton
@@ -252,17 +252,19 @@ export function DocumentPageActionList({
             </ListItemIcon>
             <ListItemText primary='View comments' />
           </ListItemButton>
-          <ListItemButton
-            onClick={() => {
-              setActiveView('suggestions');
-              onComplete();
-            }}
-          >
-            <ListItemIcon>
-              <RateReviewOutlinedIcon fontSize='small' />
-            </ListItemIcon>
-            <ListItemText primary='View suggestions' />
-          </ListItemButton>
+          {!isStructuredProposal && (
+            <ListItemButton
+              onClick={() => {
+                setActiveView('suggestions');
+                onComplete();
+              }}
+            >
+              <ListItemIcon>
+                <RateReviewOutlinedIcon fontSize='small' />
+              </ListItemIcon>
+              <ListItemText primary='View suggestions' />
+            </ListItemButton>
+          )}
         </>
       )}
       <Divider />
