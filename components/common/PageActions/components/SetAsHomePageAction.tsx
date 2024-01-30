1d3b5442461fe83a88df7619431a0c56c7087188
mattcasey <mattwad@gmail.com>
2024-01-30 16:02:51 -0700

    insert button

--- components/common/PageActions/components/SetAsHomePageAction.tsx
+++ components/common/PageActions/components/SetAsHomePageAction.tsx
@@ -4,9 +4,11 @@ import { MenuItem, ListItemIcon, ListItemText } from '@mui/material';
 
 import { useUpdateSpace } from 'charmClient/hooks/spaces';
 import { useCurrentSpace } from 'hooks/useCurrentSpace';
+import { useIsAdmin } from 'hooks/useIsAdmin';
 
 export function SetAsHomePageAction({ pageId, onComplete }: { pageId: string; onComplete: VoidFunction }) {
   const { space } = useCurrentSpace();
+  const { isAdmin } = useIsAdmin();
   const { trigger: updateSpace } = useUpdateSpace(space?.id);
   const isHomePage = space?.homePageId === pageId;
 
@@ -14,6 +16,9 @@ export function SetAsHomePageAction({ pageId, onComplete }: { pageId: string; on
     updateSpace({ homePageId: isHomePage ? null : pageId }).then(() => {
       onComplete();
     });
+  }
+  if (!isAdmin) {
+    return null;
   }
 
   return (
