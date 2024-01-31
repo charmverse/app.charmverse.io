5215ccf0dcf698fee1ba5eff34fd9694faa89970
motechFR <mo@anyahq.co>
2024-01-31 14:44:45 +0200

    Fix redirect for non logged in users

--- lib/session/getDefaultPageForSpace.ts
+++ lib/session/getDefaultPageForSpace.ts
@@ -4,10 +4,10 @@ import type { Space } from '@charmverse/core/prisma';
 import { PageType } from '@charmverse/core/prisma';
 import { prisma } from '@charmverse/core/prisma-client';
 
-import type { StaticPageType, PageEventMap } from 'lib/metrics/mixpanel/interfaces/PageEvent';
+import type { PageEventMap, StaticPageType } from 'lib/metrics/mixpanel/interfaces/PageEvent';
 import { filterVisiblePages } from 'lib/pages/filterVisiblePages';
-import { getPermissionsClient, permissionsApiClient } from 'lib/permissions/api/client';
-import { getSubdomainPath, getSpaceUrl, fullyDecodeURI } from 'lib/utilities/browser';
+import { permissionsApiClient } from 'lib/permissions/api/client';
+import { fullyDecodeURI, getSpaceUrl, getSubdomainPath } from 'lib/utilities/browser';
 
 type ViewMeta = PageEventMap['page_view']['meta'];
 
@@ -27,12 +27,46 @@ export async function getDefaultPageForSpace({
 }: {
   space: Pick<Space, 'id' | 'domain' | 'customDomain' | 'homePageId'>;
   host?: string;
-  userId: string;
+  userId?: string;
 }) {
-  const defaultPage = await getDefaultPageForSpaceRaw({ space, host, userId });
+  const defaultPage = await (userId
+    ? getDefaultPageForSpaceRaw({ space, host, userId })
+    : getDefaultPageForSpaceWithNonLoggedInUser({ space, host }));
   // encode to handle Japanese characters
   // call fullyDecodeURI to handle cases where we saved the pathname with encoded characters
   return encodeURI(fullyDecodeURI(defaultPage));
+}
+
+async function getDefaultPageForSpaceWithNonLoggedInUser({
+  space,
+  host
+}: {
+  space: Pick<Space, 'id' | 'domain' | 'customDomain' | 'homePageId'>;
+  host?: string;
+}): Promise<string> {
+  const defaultReturnValue = `join?domain=${space.domain}`;
+  const defaultSpaceUrl = getSpaceUrl(space, host);
+
+  if (!space.homePageId) {
+    return defaultReturnValue;
+  }
+
+  const page = await prisma.page.findFirst({
+    where: {
+      id: space.homePageId,
+      permissions: {
+        some: {
+          public: true
+        }
+      }
+    }
+  });
+
+  if (page) {
+    return `${defaultSpaceUrl}/${page.path}`;
+  } else {
+    return defaultReturnValue;
+  }
 }
 
 // get default page when we have a space domain
