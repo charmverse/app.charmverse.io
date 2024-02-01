68fa9103bd1fbd69f89764cd2421b665266c73ee
motechFR <mo@anyahq.co>
2024-02-01 14:44:10 +0200

    Refresh focalboard

--- lib/focalboard/utilities.ts
+++
@@ -1,9 +0,0 @@
-export function filterInternalProperties<T>(propertyMap: Record<string, any>): T {
-  return Object.entries(propertyMap).reduce((acc, [key, value]) => {
-    if (key.startsWith('__')) {
-      return acc;
-    }
-
-    return { ...acc, [key]: value };
-  }, {} as T);
-}
