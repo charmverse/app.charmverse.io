8f88b8b9fcd01f9c33882fbabf8f8520e22a39a7
mattcasey <mattwad@gmail.com>
2024-02-01 14:02:18 -0700

    add links for everything

--- components/common/PageLayout/components/Sidebar/components/FeatureLink.tsx
+++ components/common/PageLayout/components/Sidebar/components/FeatureLink.tsx
@@ -1,3 +1,9 @@
+import { ListItemIcon, ListItemText, Menu, MenuItem } from '@mui/material';
+import { usePopupState } from 'material-ui-popup-state/hooks';
+import { useState } from 'react';
+import type { MouseEvent } from 'react';
+
+import Link from 'components/common/Link';
 import { useCharmRouter } from 'hooks/useCharmRouter';
 import { useSpaceFeatures } from 'hooks/useSpaceFeatures';
 import type { MappedFeature } from 'lib/features/constants';
@@ -7,9 +13,26 @@ import { PageIcon } from '../../../../PageIcon';
 import { AddIconButton } from './AddIconButton';
 import { SidebarLink } from './SidebarButton';
 
+type PopupMenu = 'rewards' | 'proposals';
+
 export function FeatureLink({ feature, onClick }: { feature: MappedFeature; onClick?: VoidFunction }) {
-  const { navigateToSpacePath, router } = useCharmRouter();
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const { router } = useCharmRouter();
   const { getFeatureTitle } = useSpaceFeatures();
+  const [visibleMenu, setVisibleMenu] = useState<PopupMenu | false>(false);
+
+  const handleClick = (event: MouseEvent<HTMLButtonElement>, menu: PopupMenu) => {
+    setVisibleMenu(menu);
+    setAnchorEl(event.currentTarget);
+    event.preventDefault();
+    event.stopPropagation();
+  };
+
+  const handleClose = (event?: any) => {
+    setAnchorEl(null);
+    event?.stopPropagation();
+  };
+
   return (
     <SidebarLink
       href={`/${feature.path}`}
@@ -20,12 +43,43 @@ export function FeatureLink({ feature, onClick }: { feature: MappedFeature; onCl
       data-test={`sidebar-link-${feature.path}`}
       sx={{ pr: '8px !important' }}
     >
-      {feature.id === 'forum' && <AddIconButton className='add-a-page' tooltip='Add a post' />}
-      {feature.id === 'rewards' && (
-        <AddIconButton className='add-a-page' tooltip={`Add a ${getFeatureTitle('reward')}`} />
+      <span className='add-a-page'>
+        {feature.id === 'forum' && (
+          <Link href='/forum?new=1'>
+            <AddIconButton tooltip='Add a post' />
+          </Link>
+        )}
+        {feature.id === 'rewards' && (
+          <AddIconButton onClick={(e) => handleClick(e, 'rewards')} tooltip={`Add a ${getFeatureTitle('reward')}`} />
+        )}
+        {feature.id === 'proposals' && (
+          <AddIconButton
+            onClick={(e) => handleClick(e, 'proposals')}
+            tooltip={`Add a ${getFeatureTitle('proposal')}`}
+          />
+        )}
+      </span>
+      {visibleMenu === 'proposals' && (
+        <Menu anchorEl={anchorEl} open={!!anchorEl} onClose={handleClose}>
+          <MenuItem component={Link} href='/proposals/new' onClick={handleClose}>
+            <ListItemText>Add {getFeatureTitle('Proposal')}</ListItemText>
+          </MenuItem>
+          {/* create a linked board page by default, which can be changed to 'board' by the user */}
+          <MenuItem component={Link} href='/proposals/new?type=proposal_template' onClick={handleClose}>
+            <ListItemText>Add {getFeatureTitle('Proposal')} Template</ListItemText>
+          </MenuItem>
+        </Menu>
       )}
-      {feature.id === 'proposals' && (
-        <AddIconButton className='add-a-page' tooltip={`Add a ${getFeatureTitle('proposal')}`} />
+      {visibleMenu === 'rewards' && (
+        <Menu anchorEl={anchorEl} open={!!anchorEl} onClose={handleClose}>
+          <MenuItem component={Link} href='/rewards?new=1' onClick={handleClose}>
+            <ListItemText>Add {getFeatureTitle('Reward')}</ListItemText>
+          </MenuItem>
+          {/* create a linked board page by default, which can be changed to 'board' by the user */}
+          <MenuItem component={Link} href='/rewards?new_template=1' onClick={handleClose}>
+            <ListItemText>Add {getFeatureTitle('Reward')} Template</ListItemText>
+          </MenuItem>
+        </Menu>
       )}
     </SidebarLink>
   );
--- components/common/PageLayout/components/Sidebar/components/PageNavigation/components/NewPageMenu.tsx
+++ components/common/PageLayout/components/Sidebar/components/PageNavigation/components/NewPageMenu.tsx
@@ -1,23 +1,13 @@
 import type { Page } from '@charmverse/core/prisma';
-import styled from '@emotion/styled';
 import ArticleIcon from '@mui/icons-material/InsertDriveFileOutlined';
-import ListItemIcon from '@mui/material/ListItemIcon';
+import DatabaseIcon from '@mui/icons-material/TableChart';
+import { ListItemIcon, ListItemText } from '@mui/material';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
-import Typography from '@mui/material/Typography';
 import type { MouseEvent } from 'react';
 import { memo, useState } from 'react';
 
-import { greyColor2 } from 'theme/colors';
-
-import { StyledDatabaseIcon } from '../../../../../../PageIcon';
 import { AddIconButton } from '../../AddIconButton';
-
-export const StyledArticleIcon = styled(ArticleIcon)`
-  color: ${greyColor2};
-  opacity: 0.5;
-  font-size: 22px;
-`;
 
 type Props = { addPage: (p: Partial<Page>) => void; tooltip: string };
 
@@ -46,16 +36,16 @@ function NewPageMenu({ addPage, tooltip }: Props) {
       <Menu anchorEl={anchorEl} open={open} onClose={handleClose}>
         <MenuItem onClick={(e) => createPage(e, { type: 'page' })}>
           <ListItemIcon>
-            <StyledArticleIcon fontSize='small' />
+            <ArticleIcon fontSize='small' />
           </ListItemIcon>
-          <Typography sx={{ fontSize: 15, fontWeight: 600 }}>Add Page</Typography>
+          <ListItemText>Add Page</ListItemText>
         </MenuItem>
         {/* create a linked board page by default, which can be changed to 'board' by the user */}
         <MenuItem data-test='menu-add-database' onClick={(e) => createPage(e, { type: 'linked_board' })}>
           <ListItemIcon>
-            <StyledDatabaseIcon fontSize='small' />
+            <DatabaseIcon fontSize='small' />
           </ListItemIcon>
-          <Typography sx={{ fontSize: 15, fontWeight: 600 }}>Add Database</Typography>
+          <ListItemText>Add Database</ListItemText>
         </MenuItem>
       </Menu>
     </>
