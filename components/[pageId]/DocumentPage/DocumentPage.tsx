5e89422ac8c3359c99fcbaf3879882d3a73e00e5
mattcasey <mattwad@gmail.com>
2024-02-02 12:40:49 -0700

    set readonly on structured proposals

--- components/[pageId]/DocumentPage/DocumentPage.tsx
+++ components/[pageId]/DocumentPage/DocumentPage.tsx
@@ -450,7 +450,7 @@ function DocumentPageComponent({
                     reviewers={proposal.evaluations.map((e) => e.reviewers.filter((r) => !r.systemRole)).flat()}
                     assignedSubmitters={proposal.authors.map((a) => a.userId)}
                     variant='solid_button'
-                    readOnlyRewards={!proposal.permissions.edit}
+                    readOnly={!proposal.permissions.edit}
                     rewardIds={proposal.rewardIds || []}
                     onSave={(pendingReward) => {
                       const isExisting = proposal.fields?.pendingRewards?.find(
