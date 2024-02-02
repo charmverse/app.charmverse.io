53151e646fea4d781e8152a2b24a237f6dbc59da
Safwan Shaheer <devorein00@gmail.com>
2024-02-03 04:25:59 +0600

    Update proposal public api endpoints (#3532)
    * Update proposal list endpoint response structure
    
    * fix tests
    
    * .
    
    ---------
    
    Co-authored-by: mattcasey <mattwad@gmail.com>

--- pages/api/v1/proposals/index.ts
+++ pages/api/v1/proposals/index.ts
@@ -1,9 +1,10 @@
-import type { ProposalStatus } from '@charmverse/core/prisma';
+import type { ProposalEvaluationType, ProposalStatus } from '@charmverse/core/prisma';
 import { prisma } from '@charmverse/core/prisma-client';
 import { getCurrentEvaluation } from '@charmverse/core/proposals';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 import { InvalidStateError } from 'lib/middleware';
+import type { ProposalEvaluationResultExtended } from 'lib/proposal/interface';
 import { generateMarkdown } from 'lib/prosemirror/plugins/markdown/generateMarkdown';
 import { apiHandler } from 'lib/public-api/handler';
 import { withSessionRoute } from 'lib/session/withSession';
@@ -77,13 +78,6 @@ type ProposalReviewer = {
  *          type: array
  *          items:
  *            $ref: '#/components/schemas/ProposalReviewer'
- *        status:
- *          type: string
- *          example: vote_active
- *          enum:
- *            - draft
- *            - published
- *            - vote_active
  *        title:
  *          type: string
  *          example: EIP-4361 Sign in with Ethereum
@@ -94,7 +88,28 @@ type ProposalReviewer = {
  *          type: array
  *          items:
  *            type: string
- *
+ *        currentStep:
+ *          type: object
+ *          properties:
+ *            title:
+ *              type: string
+ *              example: Vote
+ *            result:
+ *              type: string
+ *              example: in_progress
+ *              enum:
+ *                - in_progress
+ *                - pass
+ *                - fail
+ *            type:
+ *              type: string
+ *              example: vote
+ *              enum:
+ *                - draft
+ *                - vote
+ *                - rubric
+ *                - pass_fail
+ *                - feedback
  */
 export type PublicApiProposal = {
   id: string;
@@ -109,6 +124,11 @@ export type PublicApiProposal = {
   title: string;
   url: string;
   voteOptions?: string[];
+  currentStep: {
+    title: string;
+    result: ProposalEvaluationResultExtended;
+    type: ProposalEvaluationType | 'draft';
+  };
 };
 
 handler.get(listProposals);
@@ -232,6 +252,17 @@ async function listProposals(req: NextApiRequest, res: NextApiResponse<PublicApi
         text: proposal.page?.contentText ?? '',
         markdown: markdownTexts[index]
       },
+      currentStep: currentEvaluation
+        ? {
+            result: currentEvaluation.result ?? 'in_progress',
+            title: currentEvaluation.title,
+            type: currentEvaluation.type
+          }
+        : {
+            result: 'in_progress',
+            title: 'Draft',
+            type: 'draft'
+          },
       status: isActiveVote ? 'vote_active' : proposal.status,
       authors: proposal.authors.map((author) => ({
         userId: author.author?.id,
