685b613b9149373361081e3cf4c8a0550402f333
mattcasey <mattwad@gmail.com>
2024-02-05 21:08:12 -0700

    fix e2e

--- components/[pageId]/DocumentPage/DocumentPageProviders.tsx
+++ components/[pageId]/DocumentPage/DocumentPageProviders.tsx
@@ -1,6 +1,3 @@
-import { Box } from '@mui/material';
-import { memo } from 'react';
-
 import { PageSidebarProvider } from 'components/[pageId]/DocumentPage/hooks/usePageSidebar';
 import { ProposalsProvider } from 'components/proposals/hooks/useProposals';
 import { CharmEditorProvider } from 'hooks/useCharmEditor';
@@ -8,27 +5,22 @@ import { CharmEditorViewProvider } from 'hooks/useCharmEditorView';
 import { CurrentPageProvider } from 'hooks/useCurrentPage';
 import { ThreadsProvider } from 'hooks/useThreads';
 import { VotesProvider } from 'hooks/useVotes';
+
 // context that is needed for DocumentPage to work
-
-// a memoized layer to prevent rendering when CharmEditorView changes
-const MemoizedBlock = memo(Box);
-
 export function DocumentPageProviders({ children }: { children: React.ReactNode }) {
   return (
-    <CurrentPageProvider>
-      <CharmEditorProvider>
-        <ThreadsProvider>
-          <VotesProvider>
-            <ProposalsProvider>
-              <CharmEditorViewProvider>
-                <MemoizedBlock>
-                  <PageSidebarProvider>{children}</PageSidebarProvider>
-                </MemoizedBlock>
-              </CharmEditorViewProvider>
-            </ProposalsProvider>
-          </VotesProvider>
-        </ThreadsProvider>
-      </CharmEditorProvider>
-    </CurrentPageProvider>
+    <CharmEditorViewProvider>
+      <CurrentPageProvider>
+        <CharmEditorProvider>
+          <ThreadsProvider>
+            <VotesProvider>
+              <ProposalsProvider>
+                <PageSidebarProvider>{children}</PageSidebarProvider>
+              </ProposalsProvider>
+            </VotesProvider>
+          </ThreadsProvider>
+        </CharmEditorProvider>
+      </CurrentPageProvider>
+    </CharmEditorViewProvider>
   );
 }
--- hooks/useCharmEditorView.tsx
+++ hooks/useCharmEditorView.tsx
@@ -13,6 +13,9 @@ export const CharmEditorViewContext = createContext<IContext | null>(null);
 export function CharmEditorViewProvider({ children }: { children: ReactNode }) {
   const view = useRef<EditorView | null>(null);
 
+  // memoize the children so they do not re-render whenever context changes
+  const memoizedChildren = useMemo(() => children, []);
+
   // keep track of a counter so we can trigger re-render in React
   const [counter, setCounter] = useState(0);
 
@@ -33,7 +36,7 @@ export function CharmEditorViewProvider({ children }: { children: ReactNode }) {
     [counter, view, setView]
   );
 
-  return <CharmEditorViewContext.Provider value={value}>{children}</CharmEditorViewContext.Provider>;
+  return <CharmEditorViewContext.Provider value={value}>{memoizedChildren}</CharmEditorViewContext.Provider>;
 }
 
 export const useCharmEditorView = () => {
