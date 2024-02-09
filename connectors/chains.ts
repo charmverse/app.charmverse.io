b2e3f5f874326093e27997f5184e7b2e31d2856f
Matt Casey <mattwad@gmail.com>
2024-02-09 15:17:22 -0700

    Update chains.ts

--- connectors/chains.ts
+++ connectors/chains.ts
@@ -438,7 +438,7 @@ const RPC: Record<string, IChainDetails> = {
 export type Blockchain = keyof typeof RPC;
 
 export const RPCList = Object.values(RPC)
-  // filter out testnets in prod, except for Goerli
+  // filter out testnets in prod, except for Goerli and Sepolia
   .filter((chain) => !isProdEnv || !chain.testnet || chain.chainId === goerli.id || chain.chainId === sepolia.id);
 
 export function getChainShortname(chainId: string | number): string {
