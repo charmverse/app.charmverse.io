1bf178e2bd1098a8e553e999121e3dd94b209923
motechFR <mo@anyahq.co>
2024-02-15 19:37:21 +0200

    Cleanup feedback evaluation

--- components/proposals/ProposalPage/components/ProposalEvaluations/components/Review/components/FeedbackEvaluation.tsx
+++ components/proposals/ProposalPage/components/ProposalEvaluations/components/Review/components/FeedbackEvaluation.tsx
@@ -11,23 +11,14 @@ import { getRelativeTimeInThePast } from 'lib/utilities/dates';
 
 export type Props = {
   proposalId?: string;
-  evaluation: Pick<PopulatedEvaluation, 'id' | 'completedAt' | 'reviewers' | 'result' | 'title'>;
+  evaluation: Pick<PopulatedEvaluation, 'id' | 'completedAt' | 'reviewers' | 'result' | 'title' | 'isReviewer'>;
   isCurrent: boolean;
-  hasEvaluatePermission: boolean;
   nextStep?: { title: string };
   onSubmit?: VoidFunction;
   archived?: boolean;
 };
 
-export function FeedbackEvaluation({
-  hasEvaluatePermission,
-  proposalId,
-  evaluation,
-  isCurrent,
-  nextStep,
-  onSubmit,
-  archived
-}: Props) {
+export function FeedbackEvaluation({ proposalId, evaluation, isCurrent, nextStep, onSubmit, archived }: Props) {
   const { showMessage } = useSnackbar();
   const { trigger, isMutating } = useSubmitEvaluationResult({ proposalId });
   const reviewerOptions: SelectOption[] = evaluation.reviewers.map((reviewer) => ({
@@ -38,7 +29,7 @@ export function FeedbackEvaluation({
   const completedDate = evaluation.completedAt ? getRelativeTimeInThePast(new Date(evaluation.completedAt)) : null;
   const disabledTooltip = !isCurrent
     ? 'This evaluation step is not active'
-    : !hasEvaluatePermission
+    : !evaluation.isReviewer
     ? 'You do not have permission to pass or reject this proposal'
     : archived
     ? 'You cannot pass or reject an archived proposal'
