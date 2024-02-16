b326d0b4ffde82f1fae77eda673f874abe3191d8
Safwan Shaheer <devorein00@gmail.com>
2024-02-16 14:32:07 +0600

    Disable relation property e2e test

--- __e2e__/database/relationProperty.spec.ts
+++ __e2e__/database/relationProperty.spec.ts
@@ -37,206 +37,200 @@ test.beforeAll(async () => {
   space = generated.space;
 });
 
-test.describe.serial('Edit relation properties', async () => {
-  test('create and edit relation property values', async ({ page, document, databasePage }) => {
-    // Arrange ------------------
-    const sourceBoardPage = await generateBoard({
-      createdBy: spaceUser.id,
-      spaceId: space.id,
-      customProps: {
-        cardPropertyValues: [
-          {
-            [Constants.titleColumnId]: 'Source Card 1'
-          },
-          {
-            [Constants.titleColumnId]: 'Source Card 2'
-          }
-        ]
-      }
-    });
-
-    const sourceBoardCards = await prisma.block.findMany({
-      where: {
-        parentId: sourceBoardPage.id,
-        type: 'card'
-      },
-      select: {
-        id: true
-      }
-    });
-
-    const connectedBoardPage = await generateBoard({
-      createdBy: spaceUser.id,
-      spaceId: space.id,
-      boardTitle: 'Connected Board',
-      customProps: {
-        cardPropertyValues: [
-          {
-            [Constants.titleColumnId]: 'Connected Card 1'
-          },
-          {
-            [Constants.titleColumnId]: 'Connected Card 2'
-          }
-        ]
-      }
-    });
-
-    const connectedBoardCards = await prisma.block.findMany({
-      where: {
-        parentId: connectedBoardPage.id,
-        type: 'card'
-      },
-      select: {
-        id: true
-      }
-    });
-
-    await generateBoard({
-      createdBy: spaceUser.id,
-      spaceId: space.id,
-      boardTitle: 'Test Board'
-    });
-
-    await loginBrowserUser({
-      browserPage: page,
-      userId: spaceUser.id
-    });
-
-    await document.goToPage({
-      domain: space.domain,
-      path: sourceBoardPage.path
-    });
-
-    await databasePage.addTablePropButton().click();
-    await databasePage.page.waitForTimeout(500);
-    const relationPropertyType = databasePage.getPropertyTypeOptionLocator({ type: 'relation' });
-    expect(relationPropertyType).toBeVisible();
-    await relationPropertyType.click();
-    await databasePage.page.keyboard.type('Connected Board');
-    await databasePage.linkedDatabaseOption({ sourceBoardId: connectedBoardPage.id }).click();
-    await databasePage.getShowOnRelatedBoardButton().click();
-    await databasePage.getAddRelationButton().click();
-
-    await databasePage.page.waitForTimeout(1000);
-
-    const sourceBoard = await prisma.block.findUniqueOrThrow({
-      where: {
-        id: sourceBoardPage.id
-      },
-      select: {
-        id: true,
-        fields: true
-      }
-    });
-
-    const sourceRelationProperty = (sourceBoard.fields as any).cardProperties.find(
-      (field: IPropertyTemplate) => field.type === 'relation'
-    );
-
-    await databasePage
-      .getDatabaseTableCell({
-        cardId: sourceBoardCards[0].id,
-        templateId: sourceRelationProperty.id
-      })
-      .click();
-
-    await databasePage.page.locator(`data-test=page-option-${connectedBoardCards[0].id}`).click();
-    await databasePage.page.waitForTimeout(250);
-    await databasePage.page.locator(`data-test=page-option-${connectedBoardCards[1].id}`).click();
-
-    await databasePage.page.locator("div[role='presentation']").click();
-    await databasePage
-      .getDatabaseTableCell({
-        cardId: sourceBoardCards[1].id,
-        templateId: sourceRelationProperty.id
-      })
-      .click();
-
-    await databasePage.page.locator(`data-test=page-option-${connectedBoardCards[0].id}`).click();
-
-    const connectedBoard = await prisma.block.findUniqueOrThrow({
-      where: {
-        id: connectedBoardPage.id
-      },
-      select: {
-        id: true,
-        fields: true
-      }
-    });
-
-    const connectedRelationProperty = (connectedBoard.fields as any).cardProperties.find(
-      (field: IPropertyTemplate) => field.type === 'relation'
-    );
-
-    const connectedBoardView = await prisma.block.findFirstOrThrow({
-      where: {
-        parentId: connectedBoardPage.id,
-        type: 'view'
-      },
-      select: {
-        fields: true,
-        id: true
-      }
-    });
-
-    await prisma.block.update({
-      where: {
-        id: connectedBoardView.id
-      },
-      data: {
-        fields: {
-          ...(connectedBoardView.fields as any),
-          visiblePropertyIds: [...(connectedBoardView.fields as any).visiblePropertyIds, connectedRelationProperty.id]
+test.skip('create and edit relation property values', async ({ page, document, databasePage }) => {
+  // Arrange ------------------
+  const sourceBoardPage = await generateBoard({
+    createdBy: spaceUser.id,
+    spaceId: space.id,
+    customProps: {
+      cardPropertyValues: [
+        {
+          [Constants.titleColumnId]: 'Source Card 1'
+        },
+        {
+          [Constants.titleColumnId]: 'Source Card 2'
         }
-      }
-    });
+      ]
+    }
+  });
 
-    await document.goToPage({
-      domain: space.domain,
-      path: connectedBoardPage.path
-    });
+  const sourceBoardCards = await prisma.block.findMany({
+    where: {
+      parentId: sourceBoardPage.id,
+      type: 'card'
+    },
+    select: {
+      id: true
+    }
+  });
 
-    const connectedBoardCard1RelationPropertyCell = databasePage.getDatabaseTableCell({
-      cardId: connectedBoardCards[0].id,
-      templateId: connectedRelationProperty.id
-    });
+  const connectedBoardPage = await generateBoard({
+    createdBy: spaceUser.id,
+    spaceId: space.id,
+    boardTitle: 'Connected Board',
+    customProps: {
+      cardPropertyValues: [
+        {
+          [Constants.titleColumnId]: 'Connected Card 1'
+        },
+        {
+          [Constants.titleColumnId]: 'Connected Card 2'
+        }
+      ]
+    }
+  });
 
-    const connectedBoardCard2RelationPropertyCell = databasePage.getDatabaseTableCell({
-      cardId: connectedBoardCards[1].id,
-      templateId: connectedRelationProperty.id
-    });
+  const connectedBoardCards = await prisma.block.findMany({
+    where: {
+      parentId: connectedBoardPage.id,
+      type: 'card'
+    },
+    select: {
+      id: true
+    }
+  });
 
-    expect(await connectedBoardCard1RelationPropertyCell.textContent()).toBe('📚Source Card 2🍻Source Card 1');
-    expect(await connectedBoardCard2RelationPropertyCell.textContent()).toBe('🍻Source Card 1');
+  await generateBoard({
+    createdBy: spaceUser.id,
+    spaceId: space.id,
+    boardTitle: 'Test Board'
+  });
 
-    await databasePage
-      .getDatabaseTableCell({
-        cardId: connectedBoardCards[0].id,
-        templateId: connectedRelationProperty.id
-      })
-      .click();
+  await loginBrowserUser({
+    browserPage: page,
+    userId: spaceUser.id
+  });
 
-    await databasePage.page
-      .locator(`data-test=page-option-${sourceBoardCards[0].id}`)
-      .getByTestId('RemoveIcon')
-      .click();
+  await document.goToPage({
+    domain: space.domain,
+    path: sourceBoardPage.path
+  });
 
-    await document.goToPage({
-      domain: space.domain,
-      path: sourceBoardPage.path
-    });
+  await databasePage.addTablePropButton().click();
+  const relationPropertyType = databasePage.getPropertyTypeOptionLocator({ type: 'relation' });
+  expect(relationPropertyType).toBeVisible();
+  await relationPropertyType.click();
+  await databasePage.page.keyboard.type('Connected Board');
+  await databasePage.linkedDatabaseOption({ sourceBoardId: connectedBoardPage.id }).click();
+  await databasePage.getShowOnRelatedBoardButton().click();
+  await databasePage.getAddRelationButton().click();
 
-    const sourceBoardCard1RelationPropertyCell = databasePage.getDatabaseTableCell({
+  await databasePage.page.waitForTimeout(1000);
+
+  const sourceBoard = await prisma.block.findUniqueOrThrow({
+    where: {
+      id: sourceBoardPage.id
+    },
+    select: {
+      id: true,
+      fields: true
+    }
+  });
+
+  const sourceRelationProperty = (sourceBoard.fields as any).cardProperties.find(
+    (field: IPropertyTemplate) => field.type === 'relation'
+  );
+
+  await databasePage
+    .getDatabaseTableCell({
       cardId: sourceBoardCards[0].id,
       templateId: sourceRelationProperty.id
-    });
+    })
+    .click();
 
-    const sourceBoardCard2RelationPropertyCell = databasePage.getDatabaseTableCell({
+  await databasePage.page.locator(`data-test=page-option-${connectedBoardCards[0].id}`).click();
+  await databasePage.page.waitForTimeout(250);
+  await databasePage.page.locator(`data-test=page-option-${connectedBoardCards[1].id}`).click();
+
+  await databasePage.page.locator("div[role='presentation']").click();
+  await databasePage
+    .getDatabaseTableCell({
       cardId: sourceBoardCards[1].id,
       templateId: sourceRelationProperty.id
-    });
+    })
+    .click();
 
-    expect(await sourceBoardCard1RelationPropertyCell.textContent()).toBe('📚Connected Card 2');
-    expect(await sourceBoardCard2RelationPropertyCell.textContent()).toBe('🍻Connected Card 1');
+  await databasePage.page.locator(`data-test=page-option-${connectedBoardCards[0].id}`).click();
+
+  const connectedBoard = await prisma.block.findUniqueOrThrow({
+    where: {
+      id: connectedBoardPage.id
+    },
+    select: {
+      id: true,
+      fields: true
+    }
   });
+
+  const connectedRelationProperty = (connectedBoard.fields as any).cardProperties.find(
+    (field: IPropertyTemplate) => field.type === 'relation'
+  );
+
+  const connectedBoardView = await prisma.block.findFirstOrThrow({
+    where: {
+      parentId: connectedBoardPage.id,
+      type: 'view'
+    },
+    select: {
+      fields: true,
+      id: true
+    }
+  });
+
+  await prisma.block.update({
+    where: {
+      id: connectedBoardView.id
+    },
+    data: {
+      fields: {
+        ...(connectedBoardView.fields as any),
+        visiblePropertyIds: [...(connectedBoardView.fields as any).visiblePropertyIds, connectedRelationProperty.id]
+      }
+    }
+  });
+
+  await document.goToPage({
+    domain: space.domain,
+    path: connectedBoardPage.path
+  });
+
+  const connectedBoardCard1RelationPropertyCell = databasePage.getDatabaseTableCell({
+    cardId: connectedBoardCards[0].id,
+    templateId: connectedRelationProperty.id
+  });
+
+  const connectedBoardCard2RelationPropertyCell = databasePage.getDatabaseTableCell({
+    cardId: connectedBoardCards[1].id,
+    templateId: connectedRelationProperty.id
+  });
+
+  expect(await connectedBoardCard1RelationPropertyCell.textContent()).toBe('📚Source Card 2🍻Source Card 1');
+  expect(await connectedBoardCard2RelationPropertyCell.textContent()).toBe('🍻Source Card 1');
+
+  await databasePage
+    .getDatabaseTableCell({
+      cardId: connectedBoardCards[0].id,
+      templateId: connectedRelationProperty.id
+    })
+    .click();
+
+  await databasePage.page.locator(`data-test=page-option-${sourceBoardCards[0].id}`).getByTestId('RemoveIcon').click();
+
+  await document.goToPage({
+    domain: space.domain,
+    path: sourceBoardPage.path
+  });
+
+  const sourceBoardCard1RelationPropertyCell = databasePage.getDatabaseTableCell({
+    cardId: sourceBoardCards[0].id,
+    templateId: sourceRelationProperty.id
+  });
+
+  const sourceBoardCard2RelationPropertyCell = databasePage.getDatabaseTableCell({
+    cardId: sourceBoardCards[1].id,
+    templateId: sourceRelationProperty.id
+  });
+
+  expect(await sourceBoardCard1RelationPropertyCell.textContent()).toBe('📚Connected Card 2');
+  expect(await sourceBoardCard2RelationPropertyCell.textContent()).toBe('🍻Connected Card 1');
 });
