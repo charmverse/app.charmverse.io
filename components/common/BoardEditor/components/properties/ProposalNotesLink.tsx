2005c920183e2743ab9a11e562331f95743dc9c1
mattcasey <mattwad@gmail.com>
2024-02-16 21:55:30 -0700

    fast follower UI tweaks for proposal notes in proposal source

--- components/common/BoardEditor/components/properties/ProposalNotesLink.tsx
+++ components/common/BoardEditor/components/properties/ProposalNotesLink.tsx
@@ -2,29 +2,29 @@ import styled from '@emotion/styled';
 import type { MouseEvent } from 'react';
 
 import { useGetOrCreateProposalNotesId } from 'charmClient/hooks/proposals';
-import {
-  StyledLink,
-  StyledTypography
-} from 'components/common/CharmEditor/components/nestedPage/components/NestedPage';
+import { StyledTypography } from 'components/common/CharmEditor/components/nestedPage/components/NestedPage';
 import { usePageDialog } from 'components/common/PageDialog/hooks/usePageDialog';
 import { PageIcon } from 'components/common/PageIcon';
+import { usePages } from 'hooks/usePages';
 import { useSnackbar } from 'hooks/useSnackbar';
 
-const StyledPageLink = styled(StyledLink)`
+const StyledPageLink = styled.div`
   svg {
     font-size: 20px;
   }
 `;
 
-export function ProposalNotesLink({ proposalId }: { proposalId?: string }) {
+export function ProposalNotesLink({ pageId }: { pageId: string }) {
   const { showPage } = usePageDialog();
   const { showError } = useSnackbar();
+  const { pages } = usePages();
+  const syncedPageId = pages[pageId]?.syncWithPageId;
 
   const { trigger: getNotesPageId } = useGetOrCreateProposalNotesId();
 
-  async function onClickInternalLink(e: MouseEvent<HTMLAnchorElement>) {
+  async function onClickInternalLink(e: MouseEvent) {
     try {
-      const result = await getNotesPageId({ proposalId });
+      const result = await getNotesPageId({ pageId: syncedPageId });
       if (result?.pageId) {
         showPage({ pageId: result.pageId });
         e.preventDefault();
