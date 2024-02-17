25ad5805504c42bd321255a91e87720fcc319a39
mattcasey <mattwad@gmail.com>
2024-02-17 08:04:43 -0700

    fix prisma

--- pages/api/proposals/reviewer-notes.ts
+++ pages/api/proposals/reviewer-notes.ts
@@ -1,4 +1,4 @@
-import { prisma } from '@charmverse/core/dist/cjs/prisma-client';
+import { prisma } from '@charmverse/core/prisma-client';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import nc from 'next-connect';
 
