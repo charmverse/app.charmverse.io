d95e46b47ac6780c3c652efdc285b0218e6a74f5
Safwan Shaheer <devorein00@gmail.com>
2024-02-20 14:48:45 +0600

    Reset required value for previous identity member property (#3671)
    * Reset required value for previous identity member property
    
    * Added unit tests
    
    * Add member properties when creating space
    
    * Remove adding member property in space test generator

--- testing/setupDatabase.ts
+++ testing/setupDatabase.ts
@@ -26,6 +26,7 @@ import { v4 } from 'uuid';
 
 import type { DataSourceType } from 'lib/focalboard/board';
 import type { IViewType } from 'lib/focalboard/boardView';
+import { generateDefaultPropertiesInput } from 'lib/members/generateDefaultPropertiesInput';
 import { provisionApiKey } from 'lib/middleware/requireApiKey';
 import type { NotificationToggles } from 'lib/notifications/notificationToggles';
 import { createPage as createPageDb } from 'lib/pages/server/createPage';
