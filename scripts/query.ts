202ca87cbfe7c13fd8c90c078c72979ad2534138
Valentin L <ludu.valentin@gmail.com>
2024-02-20 13:19:32 +0200

    Revert query

--- scripts/query.ts
+++ scripts/query.ts
@@ -5,20 +5,13 @@ import { uniq } from 'lodash';
  */
 
 async function search() {
-  const acc = await prisma.tokenGate.findMany({
+  const acc = await prisma.user.findMany({
     where: {
-      type: 'lit'
-    },
-    select: {
-      id: true,
-      space: true,
-      conditions: true
+      xpsEngineId: {
+        not: null
+      }
     }
   });
-  const found = acc.find((a) =>
-    a.conditions?.unifiedAccessControlConditions?.find((u) => u.standardContractType === 'MolochDAOv2.1')
-  );
-  console.log('xxx', found?.conditions.unifiedAccessControlConditions, found?.space.name);
 }
 
 search().then(() => console.log('Done'));
