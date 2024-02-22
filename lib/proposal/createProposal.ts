c0b6c81ffb5be0fc90b0bbd9b0673347eeb4c18d
mattcasey <mattwad@gmail.com>
2024-02-22 15:16:29 -0700

    .

--- lib/proposal/createProposal.ts
+++ lib/proposal/createProposal.ts
@@ -3,7 +3,7 @@ import type { PageWithPermissions } from '@charmverse/core/pages';
 import type { Page, ProposalReviewer, ProposalStatus } from '@charmverse/core/prisma';
 import type { Prisma, ProposalEvaluation } from '@charmverse/core/prisma-client';
 import { prisma } from '@charmverse/core/prisma-client';
-import type { ProposalWorkflowTyped, WorkflowEvaluationJson } from '@charmverse/core/proposals';
+import type { ProposalWorkflowTyped } from '@charmverse/core/proposals';
 import { arrayUtils } from '@charmverse/core/utilities';
 import { v4 as uuid } from 'uuid';
 
