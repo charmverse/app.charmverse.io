da2ab2e6dae93aa9579075182d76e56207df7486
Valentin L <ludu.valentin@gmail.com>
2024-03-01 13:18:52 +0200

    Fix polygon_zkevm information

--- lib/blockchain/provider/ankr/config.ts
+++ lib/blockchain/provider/ankr/config.ts
@@ -8,17 +8,18 @@ export const ankrAdvancedApis = {
   // 10: 'optimism',
   56: 'bsc',
   // 137: 'polygon',
+  1101: 'polygon_zkevm',
   250: 'fantom',
   // 42161: 'arbitrum',
   43114: 'avalanche',
   5000: 'mantle'
 } as const;
 // https://docs.alchemy.com/docs/why-use-alchemy#-blockchains-supported
-export const supportedChainIds = typedKeys(ankrAdvancedApis);
+export const supportedChainIds = typedKeys(ankrAdvancedApis).map((n) => Number(n) as keyof typeof ankrAdvancedApis);
 export type SupportedChainId = (typeof supportedChainIds)[number];
 // We can still support chains that dont have an advanced api
 export const rpcApis: SupportedChainId[] = [5000];
-export const supportedMainnets: SupportedChainId[] = [56, 250, 43114, 5000];
+export const supportedMainnets: SupportedChainId[] = supportedChainIds;
 
 export const advancedAPIEndpoint = `https://rpc.ankr.com/multichain/${process.env.ANKR_API_ID}`;
 
