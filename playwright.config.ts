e0a5809056212f5648d2c414375dcb9040535d09
mattcasey <mattwad@gmail.com>
2024-03-18 13:31:04 -0600

    run docker command myself

--- playwright.config.ts
+++ playwright.config.ts
@@ -104,7 +104,7 @@ const config: PlaywrightTestConfig = {
     command: 'npm run start:test:ci',
     // port: 3335,
     url: 'http://localhost:3335',
-    reuseExistingServer: !process.env.CI
+    reuseExistingServer: true // !!process.env.CI
   }
 };
 
