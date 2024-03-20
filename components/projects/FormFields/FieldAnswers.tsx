b7f5fb24ce82595f9ee9fc9a2a89c689a14c99ad
Safwan Shaheer <devorein00@gmail.com>
2024-03-20 18:12:26 +0600

    Hide project editor and answers fields based on the hidden fieldConfig

--- components/projects/FormFields/FieldAnswers.tsx
+++ components/projects/FormFields/FieldAnswers.tsx
@@ -17,23 +17,29 @@ export function FieldAnswers<Values extends Record<string, any> = Record<string,
 }) {
   return (
     <Stack display='flex' flexDirection='column' gap={2} width='100%'>
-      {properties.map((property) => (
-        <TextInputField
-          key={property.label}
-          label={property.label}
-          multiline={property.multiline}
-          rows={property.rows ?? 1}
-          required={fieldConfig?.[property.field]?.required ?? false}
-          disabled={onChange === undefined}
-          value={values?.[property.field]}
-          onChange={(e) => {
-            onChange?.({
-              ...values,
-              [property.field]: e.target.value
-            });
-          }}
-        />
-      ))}
+      {properties.map((property) => {
+        const isHidden = fieldConfig?.[property.field]?.hidden ?? false;
+        if (isHidden) {
+          return null;
+        }
+        return (
+          <TextInputField
+            key={property.label}
+            label={property.label}
+            multiline={property.multiline}
+            rows={property.rows ?? 1}
+            required={fieldConfig?.[property.field]?.required ?? true}
+            disabled={onChange === undefined}
+            value={values?.[property.field]}
+            onChange={(e) => {
+              onChange?.({
+                ...values,
+                [property.field]: e.target.value
+              });
+            }}
+          />
+        );
+      })}
     </Stack>
   );
 }
--- components/projects/FormFields/FieldsEditor.tsx
+++ components/projects/FormFields/FieldsEditor.tsx
@@ -15,56 +15,64 @@ export function FieldsEditor<Values extends Record<string, any> = Record<string,
 }) {
   return (
     <Stack display='flex' flexDirection='column' gap={2}>
-      {properties.map((property) => (
-        <Stack gap={1} key={property.label}>
-          <TextInputField
-            label={property.label}
-            multiline={property.multiline}
-            rows={property.rows ?? 1}
-            disabled
-            required={values?.[property.field]?.required ?? true}
-          />
-          {onChange && (
-            <Stack gap={1}>
-              <Stack gap={0.5} flexDirection='row' alignItems='center'>
-                <Switch
-                  size='small'
-                  checked={values?.[property.field]?.hidden ?? false}
-                  onChange={(e) => {
-                    const isChecked = e.target.checked;
-                    onChange({
-                      ...(values ?? {}),
-                      [property.field]: {
-                        required: isChecked ? false : values?.[property.field]?.required ?? true,
-                        hidden: isChecked
-                      }
-                    });
-                  }}
-                />
-                <Typography>Hidden</Typography>
-              </Stack>
+      {properties.map((property) => {
+        // If in edit mode always show the field,
+        // otherwise only show the field if it's not hidden
+        const isHidden = !onChange && values?.[property.field]?.hidden;
+        if (isHidden) {
+          return null;
+        }
+        return (
+          <Stack gap={1} key={property.label}>
+            <TextInputField
+              label={property.label}
+              multiline={property.multiline}
+              rows={property.rows ?? 1}
+              disabled
+              required={values?.[property.field]?.required ?? true}
+            />
+            {onChange && (
+              <Stack gap={1}>
+                <Stack gap={0.5} flexDirection='row' alignItems='center'>
+                  <Switch
+                    size='small'
+                    checked={values?.[property.field]?.hidden ?? false}
+                    onChange={(e) => {
+                      const isChecked = e.target.checked;
+                      onChange({
+                        ...(values ?? {}),
+                        [property.field]: {
+                          required: isChecked ? false : values?.[property.field]?.required ?? true,
+                          hidden: isChecked
+                        }
+                      });
+                    }}
+                  />
+                  <Typography>Hidden</Typography>
+                </Stack>
 
-              <Stack gap={0.5} flexDirection='row' alignItems='center'>
-                <Switch
-                  size='small'
-                  disabled={values?.[property.field]?.hidden === true}
-                  checked={values?.[property.field]?.required ?? true}
-                  onChange={(e) => {
-                    onChange({
-                      ...(values ?? {}),
-                      [property.field]: {
-                        ...values?.[property.field],
-                        required: e.target.checked
-                      }
-                    });
-                  }}
-                />
-                <Typography>Required</Typography>
+                <Stack gap={0.5} flexDirection='row' alignItems='center'>
+                  <Switch
+                    size='small'
+                    disabled={values?.[property.field]?.hidden === true}
+                    checked={values?.[property.field]?.required ?? true}
+                    onChange={(e) => {
+                      onChange({
+                        ...(values ?? {}),
+                        [property.field]: {
+                          ...values?.[property.field],
+                          required: e.target.checked
+                        }
+                      });
+                    }}
+                  />
+                  <Typography>Required</Typography>
+                </Stack>
               </Stack>
-            </Stack>
-          )}
-        </Stack>
-      ))}
+            )}
+          </Stack>
+        );
+      })}
     </Stack>
   );
 }
