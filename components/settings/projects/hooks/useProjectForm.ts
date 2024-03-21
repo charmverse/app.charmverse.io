c252934e3a481746a5248a96d9d3949f66f61e85
Safwan Shaheer <devorein00@gmail.com>
2024-03-21 22:32:51 +0600

    Add validation for all other project fields

--- components/settings/projects/hooks/useProjectForm.ts
+++ components/settings/projects/hooks/useProjectForm.ts
@@ -1,13 +1,55 @@
 import { yupResolver } from '@hookform/resolvers/yup';
+import { ethers } from 'ethers';
 import { useRef } from 'react';
 import { useForm } from 'react-hook-form';
 import * as yup from 'yup';
 
+import {
+  GITHUB_URL_REGEX,
+  LINKEDIN_URL_REGEX,
+  TWITTER_URL_REGEX
+} from 'components/members/hooks/useRequiredMemberProperties';
 import type { ProjectEditorFieldConfig, ProjectValues } from 'components/projects/interfaces';
 import type { ProjectField } from 'components/projects/ProjectFields';
 import { ProjectFieldProperties } from 'components/projects/ProjectFields';
 import type { ProjectMemberField } from 'components/projects/ProjectMemberFields';
 import { ProjectMemberFieldProperties } from 'components/projects/ProjectMemberFields';
+
+function addMatchersToSchema({
+  fieldType,
+  isRequired,
+  schemaObject
+}: {
+  fieldType: ProjectField | ProjectMemberField;
+  isRequired: boolean;
+  schemaObject: Record<string, yup.StringSchema>;
+}) {
+  if (fieldType === 'twitter') {
+    schemaObject[fieldType] = schemaObject[fieldType]!.matches(TWITTER_URL_REGEX, 'Invalid Twitter URL');
+  } else if (fieldType === 'github') {
+    schemaObject[fieldType] = schemaObject[fieldType]!.matches(GITHUB_URL_REGEX, 'Invalid GitHub URL');
+  } else if (fieldType === 'walletAddress') {
+    schemaObject[fieldType] = schemaObject[fieldType]!.test('is-valid-address', 'Invalid wallet address', (value) => {
+      try {
+        if (isRequired && !value) {
+          return false;
+        }
+
+        if (value) {
+          return ethers.utils.isAddress(value);
+        }
+
+        return true;
+      } catch {
+        return false;
+      }
+    });
+  } else if (fieldType === 'linkedin') {
+    schemaObject[fieldType] = schemaObject[fieldType]!.matches(LINKEDIN_URL_REGEX, 'Invalid LinkedIn URL');
+  } else if (fieldType === 'email') {
+    schemaObject[fieldType] = schemaObject[fieldType]!.email('Invalid email');
+  }
+}
 
 export function createProjectYupSchema({
   fieldConfig,
@@ -30,6 +72,12 @@ export function createProjectYupSchema({
       } else {
         yupProjectSchemaObject[projectFieldProperty.field] = yup.string();
       }
+
+      addMatchersToSchema({
+        fieldType: projectFieldProperty.field,
+        isRequired: !!projectFieldConfig.required,
+        schemaObject: yupProjectSchemaObject
+      });
     }
   });
 
@@ -44,6 +92,12 @@ export function createProjectYupSchema({
       } else {
         yupProjectMemberSchemaObject[projectMemberFieldProperty.field] = yup.string();
       }
+
+      addMatchersToSchema({
+        fieldType: projectMemberFieldProperty.field,
+        isRequired: !!projectMemberFieldConfig.required,
+        schemaObject: yupProjectMemberSchemaObject
+      });
     }
   });
 
