b777e0a6452edb72373b2dfafd519d317c07387c
motechFR <mo@anyahq.co>
2024-03-22 15:42:04 +0200

    Cleanup and bump core

--- lib/credentials/connectors.ts
+++ lib/credentials/connectors.ts
@@ -18,7 +18,6 @@ type EASConnector = {
   attestationContract: string;
   schemaRegistryContract: string;
   attestationExplorerUrl: string;
-  isTestnet?: boolean;
 };
 
 // See https://github.com/ethereum-attestation-service/eas-contracts
@@ -26,32 +25,27 @@ export const easConnectors: Record<EasSchemaChain | ExternalCredentialChain, EAS
   [optimism.id]: {
     attestationContract: '0x4200000000000000000000000000000000000021',
     schemaRegistryContract: '0x4200000000000000000000000000000000000020',
-    attestationExplorerUrl: 'https://optimism.easscan.org',
-    isTestnet: false
+    attestationExplorerUrl: 'https://optimism.easscan.org'
   },
   [arbitrum.id]: {
     attestationContract: '0xbd75f629a22dc1ced33dda0b68c546a1c035c458',
     schemaRegistryContract: '0xa310da9c5b885e7fb3fba9d66e9ba6df512b78eb',
-    attestationExplorerUrl: 'https://arbitrum.easscan.org',
-    isTestnet: false
+    attestationExplorerUrl: 'https://arbitrum.easscan.org'
   },
   [base.id]: {
     attestationContract: '0x4200000000000000000000000000000000000021',
     schemaRegistryContract: '0x4200000000000000000000000000000000000020',
-    attestationExplorerUrl: 'https://base.easscan.org',
-    isTestnet: false
+    attestationExplorerUrl: 'https://base.easscan.org'
   },
   [sepolia.id]: {
     attestationContract: '0xC2679fBD37d54388Ce493F1DB75320D236e1815e',
     schemaRegistryContract: '0x0a7E2Ff54e76B8E6659aedc9103FB21c038050D0',
-    attestationExplorerUrl: 'https://sepolia.easscan.org',
-    isTestnet: true
+    attestationExplorerUrl: 'https://sepolia.easscan.org'
   },
   [optimismSepolia.id]: {
     attestationContract: '0x4200000000000000000000000000000000000021',
     schemaRegistryContract: '0x4200000000000000000000000000000000000020',
-    attestationExplorerUrl: 'https://optimism-sepolia.easscan.org',
-    isTestnet: true
+    attestationExplorerUrl: 'https://optimism-sepolia.easscan.org'
   }
 };
 
