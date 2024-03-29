da4272e8ae39efe502d87094c8c4141088c4754a
mattcasey <mattwad@gmail.com>
2024-03-28 23:00:08 -0600

    dont change query script

--- scripts/query.ts
+++ scripts/query.ts
@@ -1,108 +1,43 @@
 import { prisma } from '@charmverse/core/prisma-client';
 import { uniq } from 'lodash';
+/**
+ * Use this script to perform database searches.
+ */
 
-import { appendFileSync, readFileSync } from 'fs';
-import { isTruthy } from 'lib/utils/types';
-const FILE_PATH = './orphans';
-
-const totalPages = 404701;
-const perBatch = 1000;
-
-let counts = {
-  pages: 0,
-  boards: 0,
-  deletedBoards: 0,
-  cards: 0
-};
+const userId = '4e1d4522-6437-4393-8ed1-9c56e53235f4';
 
 async function search() {
-  const pageIds = readFileSync(FILE_PATH + '.txt', 'utf-8')
-    .split('\n')
-    .filter(Boolean);
-  const pages = await prisma.page.findMany({
+  const acc = await prisma.userSpaceAction.findFirst({
     where: {
-      id: {
-        in: pageIds
-      },
-      parentId: {
-        not: null
-      }
+      createdBy: userId,
+      action: 'view_page',
+      // pageType: {
+      //   in: pageTypes
+      // },
+      spaceId: 'bc9e8464-4166-4f7c-8a14-bb293cc30d2a'
     },
-    // include: {
-    //   card: true
-    // },
+    orderBy: {
+      createdAt: 'desc'
+    },
     select: {
-      type: true,
-      id: true,
-      updatedAt: true,
-      title: true,
-      contentText: true,
-      boardId: true,
-      cardId: true
+      meta: true,
+      spaceId: true,
+      pageType: true,
+      page: {
+        select: {
+          id: true,
+          path: true
+        }
+      },
+      post: {
+        select: {
+          id: true,
+          path: true
+        }
+      }
     }
   });
-  console.log(pages);
-
-  for (let page of pages) {
-    console.log('processing', page.type, page.id, page.boardId, page.cardId);
-    if (page.type.includes('board')) {
-      // console.log('board', page.boardId);
-      const block = await prisma.block.findFirst({
-        where: {
-          id: page.boardId || page.id,
-          deletedAt: null
-        }
-      });
-      if (block) {
-        counts.boards++;
-        await prisma.page.update({
-          where: {
-            id: page.id
-          },
-          data: {
-            parentId: null
-          }
-        });
-      } else {
-        counts.deletedBoards++;
-        await prisma.page.delete({
-          where: {
-            id: page.id
-          }
-        });
-      }
-      //console.log('has block', page.boardId, !!block);
-    } else if (page.type.includes('card')) {
-      counts.cards++;
-      if (page.cardId) {
-        await prisma.block.delete({
-          where: {
-            id: page.cardId || page.id
-          }
-        });
-      } else {
-        await prisma.page.delete({
-          where: {
-            id: page.id
-          }
-        });
-      }
-
-      //  console.log('has card', page.cardId, !!card);
-    } else {
-      counts.pages++;
-      await prisma.page.update({
-        where: {
-          id: page.id
-        },
-        data: {
-          parentId: null
-        }
-      });
-    }
-  }
-  console.log(counts);
-  //console.log(pages.map((p) => p.space.domain));
+  console.log(acc);
 }
 
 search().then(() => console.log('Done'));
