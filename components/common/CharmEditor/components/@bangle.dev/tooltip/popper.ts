7bef3fa62a934015b5eb0969df9debbd2e58e126
mattcasey <mattwad@gmail.com>
2024-04-01 18:58:06 -0600

    fix popper exports

--- components/common/CharmEditor/components/@bangle.dev/tooltip/popper.ts
+++ components/common/CharmEditor/components/@bangle.dev/tooltip/popper.ts
@@ -1,6 +1,8 @@
-export { default as arrow } from '@popperjs/core/lib/modifiers/arrow';
-export { default as flip } from '@popperjs/core/lib/modifiers/flip';
-export { default as offset } from '@popperjs/core/lib/modifiers/offset';
-export { default as popperOffsets } from '@popperjs/core/lib/modifiers/popperOffsets';
-export { default as preventOverflow } from '@popperjs/core/lib/modifiers/preventOverflow';
-export { createPopper } from '@popperjs/core/lib/popper-lite';
+export { arrow } from '@popperjs/core';
+
+export { createPopper } from '@popperjs/core';
+
+export { flip } from '@popperjs/core';
+export { offset } from '@popperjs/core';
+export { popperOffsets } from '@popperjs/core';
+export { preventOverflow } from '@popperjs/core';
