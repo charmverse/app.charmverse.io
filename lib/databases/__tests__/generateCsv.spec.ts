c9776b1091237b1c4c3952a1ea8e56ea35ddb97c
Safwan Shaheer <devorein00@gmail.com>
2024-04-02 19:07:58 +0600

    Skip hidden columns during database export (#3890)

--- lib/databases/__tests__/generateCsv.spec.ts
+++ lib/databases/__tests__/generateCsv.spec.ts
@@ -1,9 +1,11 @@
+import { prisma } from '@charmverse/core/prisma-client';
 import { testUtilsUser } from '@charmverse/core/test';
 import { v4 } from 'uuid';
 
 import { generateBoard } from 'testing/setupDatabase';
 
 import type { IPropertyOption, IPropertyTemplate } from '../board';
+import type { BoardViewFields } from '../boardView';
 import { Constants } from '../constants';
 import { loadAndGenerateCsv } from '../generateCsv';
 
@@ -12,7 +14,7 @@ describe('loadAndGenerateCsv()', () => {
     await expect(loadAndGenerateCsv({ userId: 'test-user' })).rejects.toThrow('databaseId is required');
   });
 
-  it('should return a csv string and childPageIds when generating csv for a database', async () => {
+  it('should return a csv string and childPageIds without hidden columns when generating csv for a database', async () => {
     const { space, user } = await testUtilsUser.generateUserAndSpace();
     const selectOptions: IPropertyOption[] = [
       {
@@ -76,17 +78,41 @@ describe('loadAndGenerateCsv()', () => {
       }
     });
 
-    const { csvData } = await loadAndGenerateCsv({ databaseId: generatedBoard.id, userId: user.id });
+    const view = await prisma.block.findFirstOrThrow({
+      where: {
+        type: 'view',
+        parentId: generatedBoard.id
+      },
+      select: {
+        fields: true,
+        id: true
+      }
+    });
+
+    const viewFields = view.fields as BoardViewFields;
+    await prisma.block.update({
+      where: {
+        id: view.id
+      },
+      data: {
+        fields: {
+          ...viewFields,
+          visiblePropertyIds: [Constants.titleColumnId, propertyTemplates[0].id, propertyTemplates[2].id]
+        }
+      }
+    });
+
+    const { csvData } = await loadAndGenerateCsv({ viewId: view.id, databaseId: generatedBoard.id, userId: user.id });
     expect(
       csvData
         .trim()
         .split('\n')
         .map((c) => c.trim())
     ).toStrictEqual([
-      'Title,Text,Checkbox,Select',
-      '"Card 1","Card 1 Text","false","Blue"',
-      '"Card 2","Card 2 Text","false","Red"',
-      '"Card 3","Card 3 Text","true",""'
+      'Title,Text,Select',
+      '"Card 1","Card 1 Text","Blue"',
+      '"Card 2","Card 2 Text","Red"',
+      '"Card 3","Card 3 Text",""'
     ]);
   });
 });
