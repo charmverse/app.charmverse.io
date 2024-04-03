10ad757b03465fd2ae5f78c8028a2fc638c2b0a8
motechFR <mo@anyahq.co>
2024-04-03 18:30:13 +0200

    Add initial setup for more resilient e2e

--- __e2e__/charmEditor/rangeDeleteBlocks.spec.ts
+++ __e2e__/charmEditor/rangeDeleteBlocks.spec.ts
@@ -1,8 +1,10 @@
 import { prisma } from '@charmverse/core/prisma-client';
+import type { WebSocket } from '@playwright/test';
 import { test as base, expect } from '@playwright/test';
 import { DocumentPage } from '__e2e__/po/document.po';
 import { generateUserAndSpace, loginBrowserUser } from '__e2e__/utils/mocks';
 
+import { websocketsHost } from 'config/constants';
 import { _ } from 'testing/prosemirror/builders';
 
 type Fixtures = {
@@ -31,7 +33,21 @@ test('Select and delete all blocks in a document', async ({ documentPage }) => {
 
   await documentPage.charmEditor.click();
   await documentPage.page.keyboard.press(`${process.platform === 'darwin' ? 'Meta' : 'Control'}+A`);
+
+  // Promisify this, and ensure we don't accidentally spawn a bunch of listeners
+  await documentPage.page.on('websocket', async (socket: WebSocket) => {
+    await socket.waitForEvent('framereceived', (data) => {
+      // Assertion on data shape
+      console.log('data', data);
+
+      // Make sure it matches confirm_diff
+      return true;
+    });
+  });
+
   await documentPage.page.keyboard.press('Backspace');
+
+  await documentPage.page.pause();
 
   await documentPage.page.waitForTimeout(500);
 
