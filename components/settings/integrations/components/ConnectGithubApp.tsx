cb1bc64936cac33868b1a03b08a4612e6c895d20
Safwan Shaheer <devorein00@gmail.com>
2024-04-09 18:38:39 +0600

    Disconnect connect github application

--- components/settings/integrations/components/ConnectGithubApp.tsx
+++ components/settings/integrations/components/ConnectGithubApp.tsx
@@ -1,35 +1,78 @@
-import { Grid, Typography } from '@mui/material';
+import { log } from '@charmverse/core/log';
+import { Grid, Stack, Typography } from '@mui/material';
 
+import { useDisconnectGithubApplication, useGetGithubApplicationData } from 'charmClient/hooks/spaces';
 import { Button } from 'components/common/Button';
-import { useCurrentSpace } from 'hooks/useCurrentSpace';
 import { useGithubApp } from 'hooks/useGithubApp';
 import { useIsAdmin } from 'hooks/useIsAdmin';
+import { useSnackbar } from 'hooks/useSnackbar';
 
-export function ConnectGithubApp() {
+export function ConnectGithubApp({ spaceId, spaceDomain }: { spaceId: string; spaceDomain: string }) {
   const isAdmin = useIsAdmin();
+  const { trigger: disconnectGithubApplication, isMutating: isDisconnectingGithubApplication } =
+    useDisconnectGithubApplication(spaceId);
+  const { data, isLoading: isLoadingGithubApplicationData, mutate } = useGetGithubApplicationData(spaceId);
+  const { showMessage } = useSnackbar();
+
+  async function handleDisconnect() {
+    if (!data) {
+      return;
+    }
+
+    try {
+      await disconnectGithubApplication();
+      showMessage('Github application disconnected', 'success');
+      mutate(() => undefined, {
+        revalidate: false
+      });
+    } catch (err) {
+      showMessage('Failed to disconnect Github application', 'error');
+      log.error('Failed to disconnect Github application', {
+        installationId: data.spaceGithubCredential.installationId,
+        spaceId
+      });
+    }
+  }
+
   const { loading } = useGithubApp();
-  const { space } = useCurrentSpace();
   return (
-    <Grid container direction='row' gap={2} justifyContent='space-between' alignItems='center'>
+    <Grid container direction='row' gap={1} justifyContent='space-between' alignItems='center'>
       <Grid item>
         <Typography variant='body2'>Connect your space to Github to sync rewards and issues.</Typography>
       </Grid>
-      {isAdmin && space && (
-        <Grid item>
-          <Button
-            loading={loading}
-            disabledTooltip={loading ? 'Connecting with CharmVerse Github App' : undefined}
-            external
-            href={`https://github.com/apps/dev-charmverse-integration/installations/new?state=${encodeURIComponent(
-              JSON.stringify({
-                redirect: `${window?.location.origin}/${space.domain as string}`
-              })
-            )}`}
-          >
-            Connect
-          </Button>
-        </Grid>
-      )}
+      {isAdmin &&
+        (!data ? (
+          <Grid item>
+            <Button
+              loading={loading || isLoadingGithubApplicationData}
+              disabledTooltip={loading ? 'Connecting with CharmVerse Github App' : undefined}
+              external
+              href={`https://github.com/apps/dev-charmverse-integration/installations/new?state=${encodeURIComponent(
+                JSON.stringify({
+                  redirect: `${window?.location.origin}/${spaceDomain as string}`
+                })
+              )}`}
+            >
+              Connect
+            </Button>
+          </Grid>
+        ) : (
+          <Grid item xs={12}>
+            <Stack flexDirection='row' justifyContent='space-between' alignItems='center' gap={1}>
+              <Typography>
+                Connected to <strong>{data.spaceGithubCredential.name}</strong>
+              </Typography>
+              <Button
+                variant='outlined'
+                color='error'
+                loading={isDisconnectingGithubApplication}
+                onClick={handleDisconnect}
+              >
+                Disconnect
+              </Button>
+            </Stack>
+          </Grid>
+        ))}
     </Grid>
   );
 }
---
+++ pages/api/spaces/[id]/github/disconnect.ts
@@ -0,0 +1,64 @@
+import { prisma } from '@charmverse/core/prisma-client';
+import { createAppAuth } from '@octokit/auth-app';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import nc from 'next-connect';
+import { Octokit } from 'octokit';
+
+import { onError, onNoMatch, requireUser } from 'lib/middleware';
+import { withSessionRoute } from 'lib/session/withSession';
+import { hasAccessToSpace } from 'lib/users/hasAccessToSpace';
+
+const handler = nc({
+  onError,
+  onNoMatch
+});
+
+handler.use(requireUser).delete(disconnectGithub);
+
+async function disconnectGithub(req: NextApiRequest, res: NextApiResponse) {
+  const spaceId = req.query.id as string;
+  const userId = req.session.user.id;
+
+  const { error } = await hasAccessToSpace({
+    adminOnly: true,
+    spaceId,
+    userId
+  });
+
+  if (error) {
+    throw error;
+  }
+
+  const { installationId, id } = await prisma.spaceGithubCredential.findFirstOrThrow({
+    where: {
+      spaceId
+    },
+    select: {
+      id: true,
+      installationId: true
+    }
+  });
+
+  const appOctokit = new Octokit({
+    authStrategy: createAppAuth,
+    auth: {
+      appId: Number(process.env.GITHUB_APP_ID),
+      privateKey: process.env.GITHUB_APP_PRIVATE_KEY!.replace(/\\n/g, '\n'),
+      installationId
+    }
+  });
+
+  await appOctokit.request('DELETE /app/installations/{installation_id}', {
+    installation_id: Number(installationId)
+  });
+
+  await prisma.spaceGithubCredential.delete({
+    where: {
+      id
+    }
+  });
+
+  return res.status(200).end();
+}
+
+export default withSessionRoute(handler);
