55972bc1d7b705b3965328c2939d1cb89ae18bda
Safwan Shaheer <devorein00@gmail.com>
2024-04-10 18:10:50 +0600

    Removed unnecessary files

--- lib/github/constants.ts
+++ lib/github/constants.ts
@@ -1,1 +1,7 @@
+import { isProdEnv } from 'config/constants';
+
 export const INSTALLATION_ID_COOKIE = 'github-installation-id';
+
+export const SQS_WEBHOOK_GITHUB_QUEUE_NAME = isProdEnv
+  ? 'https://sqs.us-east-1.amazonaws.com/310849459438/prd-webhook-github'
+  : 'https://sqs.us-east-1.amazonaws.com/310849459438/stg-webhook-github';
--- lib/github/createIssueFromReward.ts
+++
@@ -1,3 +0,0 @@
-export async function publishRewardAsIssue() {
-  return true;
-}
