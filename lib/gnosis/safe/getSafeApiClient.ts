c6d765296a7e8373cd73cd929d154e87ca0fec85
mattcasey <mattwad@gmail.com>
2024-04-11 13:11:28 -0600

    add comment

--- lib/gnosis/safe/getSafeApiClient.ts
+++ lib/gnosis/safe/getSafeApiClient.ts
@@ -6,6 +6,7 @@ import { getChainById } from 'connectors/chains';
 export async function getSafeApiClient({ chainId }: { chainId: number }): Promise<ISafeApiKit> {
   let { default: SafeApiKit } = await import('@safe-global/api-kit');
   // For some reason, when running compiled js code in node or using tsx, SafeApiKit is nested in a second default property
+  // posted issue here: https://github.com/safe-global/safe-core-sdk/issues/514
   SafeApiKit = (SafeApiKit as any).default || SafeApiKit;
   const txServiceUrl = getChainById(chainId)?.gnosisUrl;
 
