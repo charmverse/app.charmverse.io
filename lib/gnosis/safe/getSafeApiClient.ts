26aff9df8712cb1cb42e7b46821a7151d8aec0f8
mattcasey <mattwad@gmail.com>
2024-04-11 13:07:36 -0600

    sometimes SafeKit gets imported with an extra level of default, cant figure out why

--- lib/gnosis/safe/getSafeApiClient.ts
+++ lib/gnosis/safe/getSafeApiClient.ts
@@ -1,11 +1,12 @@
 import type ISafeApiKit from '@safe-global/api-kit';
 import { getChainById } from 'connectors/chains';
-
 /**
  * Api client for readonly Gnosis Safe operations
  */
 export async function getSafeApiClient({ chainId }: { chainId: number }): Promise<ISafeApiKit> {
-  const SafeApiKit = (await import('@safe-global/api-kit')).default;
+  let { default: SafeApiKit } = await import('@safe-global/api-kit');
+  // For some reason, when running compiled js code in node or using tsx, SafeApiKit is nested in a second default property
+  SafeApiKit = (SafeApiKit as any).default || SafeApiKit;
   const txServiceUrl = getChainById(chainId)?.gnosisUrl;
 
   return new SafeApiKit({
