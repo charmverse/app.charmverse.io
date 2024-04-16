c185e25494e3eafab91b863cb2a70a07b3568a55
Safwan Shaheer <devorein00@gmail.com>
2024-04-16 17:39:11 +0600

    Fixed broken e2e test

--- __e2e__/proposals/structuredProposalWithProject.spec.ts
+++ __e2e__/proposals/structuredProposalWithProject.spec.ts
@@ -284,6 +284,9 @@ test.describe.serial('Structured proposal template with project', () => {
     // Add a new project member and update their project fields
     await proposalPage.projectTeamMembersSelect.click();
     await proposalPage.getProjectMemberOption(0).click();
+    await proposalPage.page.waitForResponse((response) => {
+      return response.request().method() === 'PUT' && response.url().endsWith('/form/answers');
+    });
     await proposalFormFieldPage.fillProjectField({ fieldName: 'projectMembers[0].email', content: 'doe@gmail.com' });
     await proposalFormFieldPage.fillProjectField({
       fieldName: 'projectMembers[1].email',
