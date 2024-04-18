4e9448dca53b2a4b43958ee4320a20857cdc33bc
Safwan Shaheer <devorein00@gmail.com>
2024-04-18 20:32:26 +0600

    Show assigned applicants for assigned workflow

--- components/rewards/components/RewardEvaluations/components/Settings/components/SubmitSettings.tsx
+++ components/rewards/components/RewardEvaluations/components/Settings/components/SubmitSettings.tsx
@@ -2,9 +2,11 @@ import styled from '@emotion/styled';
 import { Box, FormLabel, Stack, Typography } from '@mui/material';
 import clsx from 'clsx';
 import { DateTime } from 'luxon';
+import { useCallback } from 'react';
 
 import { StyledPropertyTextInput } from 'components/common/DatabaseEditor/components/properties/TextInput';
 import { UserAndRoleSelect } from 'components/common/DatabaseEditor/components/properties/UserAndRoleSelect';
+import { UserSelect } from 'components/common/DatabaseEditor/components/properties/UserSelect';
 import Checkbox from 'components/common/DatabaseEditor/widgets/checkbox';
 import { DateTimePicker } from 'components/common/DateTimePicker';
 
@@ -22,12 +24,22 @@ export function SubmitStepSettings({
   readOnly,
   onChange
 }: Omit<EvaluationStepSettingsProps, 'evaluation' | 'rewardStatus'>) {
+  const isAssignedReward = !!reward?.assignedSubmitters;
+
+  const updateAssignedSubmitters = useCallback((submitters: string[]) => {
+    onChange({
+      assignedSubmitters: submitters,
+      approveSubmitters: false,
+      allowMultipleApplications: false
+    });
+  }, []);
+
   return (
     <Stack gap={1.5}>
       <RowStack mb={0.5}>
         <FormLabel>
           <Typography component='span' variant='subtitle1'>
-            Duration (days)
+            Due date
           </Typography>
         </FormLabel>
         <Box width='fit-content'>
@@ -51,78 +63,100 @@ export function SubmitStepSettings({
           />
         </Box>
       </RowStack>
-      <RowStack>
-        <FormLabel>
-          <Typography component='span' variant='subtitle1'>
-            Allow multiple entries
-          </Typography>
-        </FormLabel>
-        <Box width='fit-content'>
-          <Checkbox
-            isOn={Boolean(reward?.allowMultipleApplications)}
-            onChanged={(isOn) => {
-              onChange({
-                allowMultipleApplications: !!isOn
-              });
-            }}
-            disabled={readOnly}
-            readOnly={readOnly}
-          />
-        </Box>
-      </RowStack>
-      <RowStack>
-        <FormLabel>
-          <Typography component='span' variant='subtitle1'>
-            Applicant Roles
-          </Typography>
-        </FormLabel>
-        <Box width='fit-content'>
-          <UserAndRoleSelect
-            type='role'
-            readOnly={readOnly}
-            value={(reward?.allowedSubmitterRoles ?? []).map((roleId) => ({ group: 'role', id: roleId }))}
-            onChange={(options) => {
-              const roleIds = options.filter((option) => option.group === 'role').map((option) => option.id);
+      {!isAssignedReward ? (
+        <>
+          <RowStack>
+            <FormLabel>
+              <Typography component='span' variant='subtitle1'>
+                Allow multiple entries
+              </Typography>
+            </FormLabel>
+            <Box width='fit-content'>
+              <Checkbox
+                isOn={Boolean(reward?.allowMultipleApplications)}
+                onChanged={(isOn) => {
+                  onChange({
+                    allowMultipleApplications: !!isOn
+                  });
+                }}
+                disabled={readOnly}
+                readOnly={readOnly}
+              />
+            </Box>
+          </RowStack>
+          <RowStack>
+            <FormLabel>
+              <Typography component='span' variant='subtitle1'>
+                Applicant Roles
+              </Typography>
+            </FormLabel>
+            <Box width='fit-content'>
+              <UserAndRoleSelect
+                type='role'
+                readOnly={readOnly}
+                value={(reward?.allowedSubmitterRoles ?? []).map((roleId) => ({ group: 'role', id: roleId }))}
+                onChange={(options) => {
+                  const roleIds = options.filter((option) => option.group === 'role').map((option) => option.id);
 
-              onChange({
-                allowedSubmitterRoles: roleIds
-              });
-            }}
-          />
-        </Box>
-      </RowStack>
-      <RowStack>
-        <FormLabel>
-          <Typography component='span' variant='subtitle1'>
-            # Available
-          </Typography>
-        </FormLabel>
-        <Box width='fit-content'>
-          <StyledPropertyTextInput
-            onChange={(e) => {
-              const value = Number(e.target.value);
-              onChange({
-                maxSubmissions: value <= 0 ? null : value
-              });
-            }}
-            required
-            defaultValue={reward?.maxSubmissions}
-            type='number'
-            size='small'
-            inputProps={{
-              step: 1,
-              min: 1,
-              style: { height: 'auto' },
-              className: clsx('Editable octo-propertyvalue', { readonly: readOnly })
-            }}
-            sx={{
-              width: '100%'
-            }}
-            disabled={readOnly}
-            placeholder='Unlimited'
-          />
-        </Box>
-      </RowStack>
+                  onChange({
+                    allowedSubmitterRoles: roleIds
+                  });
+                }}
+              />
+            </Box>
+          </RowStack>
+          <RowStack>
+            <FormLabel>
+              <Typography component='span' variant='subtitle1'>
+                # Available
+              </Typography>
+            </FormLabel>
+            <Box width='fit-content'>
+              <StyledPropertyTextInput
+                onChange={(e) => {
+                  const value = Number(e.target.value);
+                  onChange({
+                    maxSubmissions: value <= 0 ? null : value
+                  });
+                }}
+                required
+                defaultValue={reward?.maxSubmissions}
+                type='number'
+                size='small'
+                inputProps={{
+                  step: 1,
+                  min: 1,
+                  style: { height: 'auto' },
+                  className: clsx('Editable octo-propertyvalue', { readonly: readOnly })
+                }}
+                sx={{
+                  width: '100%'
+                }}
+                disabled={readOnly}
+                placeholder='Unlimited'
+              />
+            </Box>
+          </RowStack>
+        </>
+      ) : (
+        <RowStack>
+          <FormLabel>
+            <Typography component='span' variant='subtitle1'>
+              Assigned applicants
+            </Typography>
+          </FormLabel>
+          <Box width='fit-content'>
+            <UserSelect
+              memberIds={reward.assignedSubmitters ?? []}
+              readOnly={readOnly}
+              onChange={updateAssignedSubmitters}
+              wrapColumn
+              showEmptyPlaceholder
+              error={!reward.assignedSubmitters?.length && !readOnly ? 'Requires at least one assignee' : undefined}
+            />
+          </Box>
+        </RowStack>
+      )}
     </Stack>
   );
 }
