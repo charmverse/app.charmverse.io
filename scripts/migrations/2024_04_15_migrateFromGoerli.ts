e69979ec74172c47b659198b10a3febb9847605b
motechFR <mo@anyahq.co>
2024-04-18 18:37:15 +0200

    Migration script

--- scripts/migrations/2024_04_15_migrateFromGoerli.ts
+++ scripts/migrations/2024_04_15_migrateFromGoerli.ts
@@ -2,7 +2,7 @@ import { arrayUtils } from "@charmverse/core/utilities";
 import { PaymentMethod, prisma } from "@charmverse/core/prisma-client";
 import { baseUrl } from "config/constants";
 import { lowerCaseEqual, prettyPrint } from "lib/utils/strings";
-import { goerli } from "viem/chains";
+import { goerli, sepolia } from "viem/chains";
 import { UnsignedTransaction } from "@lens-protocol/domain/entities";
 
 
@@ -11,120 +11,46 @@ async function migratePaymentMethods() {
   const usdcGoerliContract = '0x07865c6e87b9f70255377e024ace6630c1eaa37f';
 
   const usdcSepoliaContract = '0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238';
-  // const count = await prisma.paymentMethod.count({
-  //   where: {
-  //     chainId: goerli.id
-  //   }
-  // });
 
-  // const usdcCount = await prisma.paymentMethod.count({
-  //   where: {
-  //     chainId: goerli.id,
-  //     contractAddress: '0x07865c6e87b9f70255377e024ace6630c1eaa37f'
-  //   }
-  // });
-
-    // console.log({
-  //   count,
-  //   usdcCount
-  // })
-
-  const paymentMethodsRequiringResolution = await prisma.paymentMethod.findMany({
+  await prisma.paymentMethod.updateMany({
     where: {
-      chainId: goerli.id,
-      contractAddress: {
-        not: usdcGoerliContract
-      }
+      contractAddress: usdcGoerliContract,
+      chainId: goerli.id
     },
-    select: {
-      id: true,
-      contractAddress: true,
-      tokenName: true,
-      tokenSymbol: true,
-    }
-  });
-
-  const rewards = await prisma.bounty.findMany({
-    where: {
-      rewardToken: {
-        in: paymentMethodsRequiringResolution.map(pm => pm.contractAddress).filter(Boolean) as string[]
-      },
-      applications: {
-        some: {
-          status: {
-            in: ['complete', 'paid', 'processing']
-          }
-        }
-      }
-    },
-    select: {
-      rewardToken: true,
-      chainId: true,
-      id: true,
-      page: {
-        select: {
-          path: true,
-          title: true
-        }
-      },
-      space: {
-        select: {
-          domain: true
-        }
-      }
-    },
-    orderBy: {
-      spaceId: 'asc'
+    data: {
+      contractAddress: usdcSepoliaContract,
+      chainId: sepolia.id
     }
   })
 
-  const uniqueSpaces = [] as string[];
-
-  const rewardsByPaymentMethod = paymentMethodsRequiringResolution.reduce((acc, method) => {
-    const relevantRewards = rewards.filter(reward =>  !!reward.rewardToken && lowerCaseEqual(reward.rewardToken, method.contractAddress));
-
-    for (const reward of relevantRewards) {
-      if (!uniqueSpaces.includes(reward.space.domain)) {
-        uniqueSpaces.push(reward.space.domain)
-      }
+  await prisma.paymentMethod.deleteMany({
+    where: {
+      chainId: goerli.id
     }
-
-    if (relevantRewards.length > 0) {
-        relevantRewards.sort((a, b) => b.id.localeCompare(a.id)); // Sort by id in descending order
-        acc.used.push({
-            ...method,
-            rewards: relevantRewards.map(reward => ({
-              ...reward,
-              fullPath: `${baseUrl}/${reward.space.domain}/${reward.page?.path}`
-            }))
-        });
-    } else {
-        acc.unused.push(method);
-    }
-    return acc;
-}, { used: [] as (Pick<PaymentMethod, 'contractAddress'> & {rewards: any})[], unused: [] as any });
-
-
-  const uniqueUsedTokens = arrayUtils.uniqueValues(rewardsByPaymentMethod.used.map(pm => pm.contractAddress?.toLowerCase()))
-
-
-  // console.log('\r\n---------------\r\n')
-  prettyPrint({
-    rewardsByPaymentMethod,
-    uniqueUsedTokens,
-    uniqueSpaces
-  });
-
- 
-
-  prettyPrint({
-    uniqueUsedTokens
   });
 
 }
 
-migratePaymentMethods().then(() => console.log('Done'));
 
+async function clearOldRewardsToken () {
+  await prisma.bounty.updateMany({
+    where: {
+      chainId: goerli.id
+    },
+    data: {
+      chainId: null,
+      rewardToken: null,
+      rewardAmount: null,
+      customReward: null
+    }
+  })
+}
+
+
+async function migrateFromGoerli() {
+  await migratePaymentMethods();
+  await clearOldRewardsToken();
+}
 
 // https://www.prisma.io/docs/orm/prisma-client/special-fields-and-types/working-with-json-fields#filtering-on-object-key-value-inside-array
 
