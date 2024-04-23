c81e473ef40b3a5062122f753f3f24c870daa3fb
mattcasey <mattwad@gmail.com>
2024-04-23 13:43:10 -0600

    fix test

--- __integration-tests__/server/pages/api/pages/[id]/comments/index.spec.ts
+++ __integration-tests__/server/pages/api/pages/[id]/comments/index.spec.ts
@@ -7,12 +7,18 @@ import { createPage, generatePageComment } from 'testing/setupDatabase';
 
 describe('GET /api/pages/[id]/comments - fetch comments of a page', () => {
   it('should allow a user with access to the space to fetch comments with status code 200', async () => {
-    const { user, space } = await testUtilsUser.generateUserAndSpace({ isAdmin: true });
+    const { user, space } = await testUtilsUser.generateUserAndSpace({ isAdmin: false });
     const userCookie = await loginUser(user.id);
 
     const page = await createPage({
       spaceId: space.id,
-      createdBy: user.id
+      createdBy: user.id,
+      pagePermissions: [
+        {
+          userId: user.id,
+          permissionLevel: 'view'
+        }
+      ]
     });
     const comment = await generatePageComment({
       createdBy: user.id,
@@ -24,7 +30,7 @@ describe('GET /api/pages/[id]/comments - fetch comments of a page', () => {
   });
 
   it('should not allow a user without access to the space to fetch comments and respond with 401', async () => {
-    const { user, space } = await testUtilsUser.generateUserAndSpace({ isAdmin: true });
+    const { user, space } = await testUtilsUser.generateUserAndSpace();
     const otherUser = await testUtilsUser.generateUser();
     const userCookie = await loginUser(otherUser.id);
     const { page } = await testUtilsPages.generateCommentWithThreadAndPage({
