14f34ab7ba086dc2c7fce33da877d5f627be648f
Safwan Shaheer <devorein00@gmail.com>
2024-04-25 05:23:29 +0600

    Open new tab to create reward template in proposal rewards step

--- components/proposals/ProposalPage/components/ProposalEvaluations/components/Settings/components/RewardSettings.tsx
+++ components/proposals/ProposalPage/components/ProposalEvaluations/components/Settings/components/RewardSettings.tsx
@@ -1,16 +1,11 @@
-import { Box, Typography, FormLabel } from '@mui/material';
+import { Box, FormLabel, Typography } from '@mui/material';
 
 import type { SelectOptionType } from 'components/common/form/fields/Select/interfaces';
 import { SelectField } from 'components/common/form/fields/SelectField';
-import { NewDocumentPage } from 'components/common/PageDialog/components/NewDocumentPage';
-import { useNewPage } from 'components/common/PageDialog/hooks/useNewPage';
-import { NewPageDialog } from 'components/common/PageDialog/NewPageDialog';
-import { MilestonePropertiesForm } from 'components/rewards/components/RewardProperties/MilestonePropertiesForm';
-import { RewardPropertiesForm } from 'components/rewards/components/RewardProperties/RewardPropertiesForm';
-import { useNewReward } from 'components/rewards/hooks/useNewReward';
 import { useRewardTemplates } from 'components/rewards/hooks/useRewardTemplates';
+import { useCurrentSpace } from 'hooks/useCurrentSpace';
 import type { ProposalFields } from 'lib/proposals/interfaces';
-import { getRewardErrors } from 'lib/rewards/getRewardErrors';
+import { getAbsolutePath } from 'lib/utils/browser';
 
 type RewardOptions = Pick<ProposalFields, 'rewardsTemplateId' | 'enableRewards'>;
 
@@ -26,9 +21,6 @@ export function RewardSettings({ value, onChange }: RewardSettingsProps) {
     name: template.page.title,
     color: 'default'
   }));
-  const { isDirty, clearNewPage, openNewPage, newPageValues, updateNewPageValues } = useNewPage();
-  const { clearRewardValues, contentUpdated, rewardValues, setRewardValues, createReward, isSavingReward } =
-    useNewReward();
   // when we are ready to offer this option with a modal inside the page
   options.push({
     id: 'add_new',
@@ -36,35 +28,10 @@ export function RewardSettings({ value, onChange }: RewardSettingsProps) {
     color: 'gray',
     variant: 'plain'
   });
+  const { space } = useCurrentSpace();
 
   function onChangeSettings(updates: Partial<RewardOptions>) {
     onChange({ ...value, ...updates });
-  }
-
-  function closeDialog() {
-    clearRewardValues();
-    clearNewPage();
-  }
-
-  const errors = getRewardErrors({
-    reward: rewardValues,
-    rewardType: rewardValues.rewardType,
-    page: {
-      title: newPageValues?.title || '',
-      type: newPageValues?.type || 'bounty'
-    }
-  });
-  const disabledTooltip = errors.join(', ');
-
-  async function saveForm() {
-    const createdReward = await createReward(newPageValues);
-    if (createdReward) {
-      onChange({
-        rewardsTemplateId: createdReward.id,
-        enableRewards: true
-      });
-      closeDialog();
-    }
   }
 
   return (
@@ -83,41 +50,14 @@ export function RewardSettings({ value, onChange }: RewardSettingsProps) {
           options={options}
           onChange={async (rewardsTemplateId: string | string[]) => {
             if (rewardsTemplateId === 'add_new') {
-              openNewPage({
-                type: 'bounty_template'
-              });
+              const absolutePath = space ? getAbsolutePath('/rewards/new?type=bounty_template', space.domain) : '';
+              window.open(absolutePath, '_blank');
             } else if (typeof rewardsTemplateId === 'string') {
               onChangeSettings({ rewardsTemplateId: rewardsTemplateId ?? null });
             }
           }}
         />
       </Box>
-
-      <NewPageDialog
-        contentUpdated={contentUpdated || isDirty}
-        disabledTooltip={disabledTooltip}
-        isOpen={!!newPageValues}
-        onClose={closeDialog}
-        onSave={saveForm}
-        isSaving={isSavingReward}
-      >
-        <NewDocumentPage
-          key={newPageValues?.templateId}
-          titlePlaceholder='Title (required)'
-          values={newPageValues}
-          onChange={updateNewPageValues}
-        >
-          <MilestonePropertiesForm
-            onChange={setRewardValues}
-            values={rewardValues}
-            isTemplate
-            isProposalTemplate
-            expandedByDefault
-            selectTemplate={() => {}}
-            templateId={newPageValues?.templateId}
-          />
-        </NewDocumentPage>
-      </NewPageDialog>
     </>
   );
 }
