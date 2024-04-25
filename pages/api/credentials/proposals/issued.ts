41b15398f428bcde8949031f59664c305edabb89
Safwan Shaheer <devorein00@gmail.com>
2024-04-26 02:39:06 +0600

    Fixed broken type issues

--- pages/api/credentials/proposals/issued.ts
+++ pages/api/credentials/proposals/issued.ts
@@ -2,7 +2,7 @@ import { InvalidInputError } from '@charmverse/core/errors';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import nc from 'next-connect';
 
-import { getProposalCredentials } from 'lib/credentials/getProposalOrApplicationCredentials';
+import { getProposalOrApplicationCredentials } from 'lib/credentials/getProposalOrApplicationCredentials';
 import { onError, onNoMatch, requireKeys, requireUser } from 'lib/middleware';
 import { permissionsApiClient } from 'lib/permissions/api/client';
 import { withSessionRoute } from 'lib/session/withSession';
@@ -22,7 +22,7 @@ async function getIssuedCredentialsController(req: NextApiRequest, res: NextApiR
     throw new InvalidInputError('User does not have permission to view credentials for this proposal');
   }
 
-  const issuedCredentials = await getProposalCredentials({ proposalId });
+  const issuedCredentials = await getProposalOrApplicationCredentials({ proposalId });
   return res.status(200).json(issuedCredentials);
 }
 
