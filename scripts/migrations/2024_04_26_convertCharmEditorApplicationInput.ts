9df244a0934a51020ee3bcfa13f7cd5909cbea4e
Safwan Shaheer <devorein00@gmail.com>
2024-04-26 06:32:56 +0600

    Migration script to convert charm editor application input

---
+++ scripts/migrations/2024_04_26_convertCharmEditorApplicationInput.ts
@@ -0,0 +1,53 @@
+import { prisma } from "@charmverse/core/prisma-client";
+
+async function convertCharmEditorApplicationInput() {
+  const applications = await prisma.application.findMany({
+    where: {
+      message: {
+        not: null
+      },
+    },
+    select: {
+      id: true,
+      message: true
+    }
+  })
+
+  const total = applications.length;
+  let current = 0;
+
+  for (const application of applications) {
+    current++;
+    try {
+      if (application.message) {
+        await prisma.application.update({
+          where: {
+            id: application.id
+          },
+          data: {
+            messageNodes: JSON.stringify({
+              type: 'doc',
+              content: [
+                {
+                  type: 'paragraph',
+                  content: [
+                    {
+                      type: 'text',
+                      text: application.message
+                    },
+                  ]
+                }
+              ]
+            })
+          }
+        })
+      }
+    } catch (err) {
+      console.error(`Failed to convert message for application ${application.id}`, err)
+    } finally {
+      console.log(`Processed ${current} of ${total} applications`)
+    }
+  }
+}
+
+convertCharmEditorApplicationInput().then(() => null);
