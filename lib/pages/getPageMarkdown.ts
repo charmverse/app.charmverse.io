15fb297cdd9f3ccebcfd312055c1f40269b3d1c5
mattcasey <mattwad@gmail.com>
2024-04-30 01:04:28 -0600

    support rewards

--- lib/pages/getPageMarkdown.ts
+++ lib/pages/getPageMarkdown.ts
@@ -1,14 +1,14 @@
 import { prisma } from '@charmverse/core/prisma-client';
+import { getChainById } from 'connectors/chains';
 
 import type { SelectOptionType } from 'components/common/form/fields/Select/interfaces';
 import type { FormFieldValue, LongTextValue } from 'lib/forms/interfaces';
 import type { ProjectField, ProjectMemberField, ProjectMemberFieldConfig, FieldConfig } from 'lib/projects/formField';
 import { getFieldConfig, projectMemberFieldProperties, projectFieldProperties } from 'lib/projects/formField';
+import type { ProposalFields } from 'lib/proposals/interfaces';
 import { _ } from 'lib/prosemirror/builders';
 import { getNodeFromJson } from 'lib/prosemirror/getNodeFromJson';
-import type { PageContent } from 'lib/prosemirror/interfaces';
 import { generateMarkdown } from 'lib/prosemirror/markdown/generateMarkdown';
-import { isTruthy } from 'lib/utils/types';
 
 export async function getPageMarkdown({
   pageId,
@@ -60,69 +60,96 @@ export async function getPageMarkdown({
     const formAnswersMap = new Map(formAnswers.map((answer) => [answer.fieldId, answer.value]));
 
     // add form fields to content
-    const content = _.doc(
-      ...formFields
-        .map((field) => {
-          if (field.private && !includePrivateFields) {
-            return [];
-          }
-          if (field.type === 'label') {
-            return [
-              _.heading({ level: 2 }, field.name),
-              ...(field.description ? (getNodeFromJson(field.description).content as any).content : [])
-            ];
-          } else if (field.type === 'project_profile') {
-            const fieldConfig = field.fieldConfig as FieldConfig;
-            const memberConfig = fieldConfig.projectMember as ProjectMemberFieldConfig;
-            const fields: any[] = [_.heading({ level: 3 }, 'Project Profile')];
-            projectFieldProperties.forEach((property) => {
-              const config = getFieldConfig(fieldConfig[property.field]);
+    const formFieldNodes = formFields
+      .map((field) => {
+        if (field.private && !includePrivateFields) {
+          return [];
+        }
+        if (field.type === 'label') {
+          return [
+            _.heading({ level: 2 }, field.name),
+            ...(field.description ? (getNodeFromJson(field.description).content as any).content : [])
+          ];
+        } else if (field.type === 'project_profile') {
+          const fieldConfig = field.fieldConfig as FieldConfig;
+          const memberConfig = fieldConfig.projectMember as ProjectMemberFieldConfig;
+          const fields: any[] = [_.heading({ level: 3 }, 'Project Profile')];
+          projectFieldProperties.forEach((property) => {
+            const config = getFieldConfig(fieldConfig[property.field]);
+            if (config.show && (!config.private || includePrivateFields)) {
+              const answer = page.proposal?.project?.[property.field as ProjectField];
+              fields.push(_.bullet_list({ indent: 0 }, _.list_item(_.p(`${property.label}: ${answer || 'N/A'}`))));
+            }
+          });
+
+          fields.push(_.bullet_list({ indent: 0 }, _.list_item(_.p('Project Members'))));
+          page.proposal?.project?.projectMembers.forEach((member) => {
+            fields.push(_.bullet_list({ indent: 1 }, _.list_item(_.p(member.name))));
+            projectMemberFieldProperties.forEach((property) => {
+              const config = getFieldConfig(memberConfig[property.field]);
               if (config.show && (!config.private || includePrivateFields)) {
-                const answer = page.proposal?.project?.[property.field as ProjectField];
-                fields.push(_.bullet_list({ indent: 0 }, _.list_item(_.p(`${property.label}: ${answer || 'N/A'}`))));
+                const answer = member[property.field as ProjectMemberField];
+                fields.push(_.bullet_list({ indent: 2 }, _.list_item(_.p(`${property.label}: ${answer || 'N/A'}`))));
               }
             });
+          });
 
-            fields.push(_.bullet_list({ indent: 0 }, _.list_item(_.p('Project Members'))));
-            page.proposal?.project?.projectMembers.forEach((member) => {
-              fields.push(_.bullet_list({ indent: 1 }, _.list_item(_.p(member.name))));
-              projectMemberFieldProperties.forEach((property) => {
-                const config = getFieldConfig(memberConfig[property.field]);
-                if (config.show && (!config.private || includePrivateFields)) {
-                  const answer = member[property.field as ProjectMemberField];
-                  fields.push(_.bullet_list({ indent: 2 }, _.list_item(_.p(`${property.label}: ${answer || 'N/A'}`))));
-                }
-              });
-            });
+          return fields;
+        }
+        const answer = formAnswersMap.get(field.id) as FormFieldValue;
+        if ((answer as LongTextValue)?.content) {
+          const contentNode = getNodeFromJson((answer as LongTextValue).content);
+          return [_.heading({ level: 3 }, field.name), ...((contentNode.content as any).content || [])];
+        }
+        const answerArray = answer && typeof answer === 'string' ? [answer] : Array.isArray(answer) ? answer : [];
+        if (answerArray.length > 0) {
+          return [
+            _.heading({ level: 3 }, field.name),
+            _.p(
+              ...answerArray
+                .map((value) => {
+                  // handle select/multiselect type values
+                  const mappedValue = (field.options as SelectOptionType[])?.find(
+                    (option) => option.id === value
+                  )?.name;
+                  return mappedValue || value;
+                })
+                .join(', ')
+            )
+          ];
+        }
+        return [];
+      })
+      .flat();
 
-            return fields;
-          }
-          const answer = formAnswersMap.get(field.id) as FormFieldValue;
-          if ((answer as LongTextValue)?.content) {
-            const contentNode = getNodeFromJson((answer as LongTextValue).content);
-            return [_.heading({ level: 3 }, field.name), ...((contentNode.content as any).content || [])];
-          }
-          const answerArray = answer && typeof answer === 'string' ? [answer] : Array.isArray(answer) ? answer : [];
-          if (answerArray.length > 0) {
-            return [
-              _.heading({ level: 3 }, field.name),
+    const rewardNodes: any[] = [];
+    const proposalFields = page.proposal.fields as ProposalFields;
+
+    if (proposalFields.pendingRewards?.length) {
+      rewardNodes.push(_.heading({ level: 3 }, 'Milestones'));
+      proposalFields.pendingRewards.forEach((reward) => {
+        rewardNodes.push(
+          _.bullet_list(
+            { indent: 0 },
+            _.list_item(
               _.p(
-                ...answerArray
-                  .map((value) => {
-                    // handle select/multiselect type values
-                    const mappedValue = (field.options as SelectOptionType[])?.find(
-                      (option) => option.id === value
-                    )?.name;
-                    return mappedValue || value;
-                  })
-                  .join(', ')
+                `${reward.page.title || 'Untitled'}  \n${
+                  reward.reward.rewardType === 'token'
+                    ? `Token reward: ${reward.reward.rewardAmount} ${reward.reward.rewardToken} on ${
+                        getChainById(reward.reward.chainId!)?.chainName || 'Unknown Chain'
+                      }`
+                    : reward.reward.rewardType === 'custom'
+                    ? `Reward: ${reward.reward.customReward}`
+                    : ``
+                }`
               )
-            ];
-          }
-          return [];
-        })
-        .flat()
-    );
+            )
+          )
+        );
+      });
+    }
+
+    const content = _.doc(...formFieldNodes, ...rewardNodes);
 
     return generateMarkdown({
       content: content.toJSON(),
