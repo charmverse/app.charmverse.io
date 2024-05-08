443fd2620d795f99c001302046668a4c4f74fc95
mattcasey <mattwad@gmail.com>
2024-05-08 00:17:48 -0600

    fix errors

--- lib/proposals/getProposalErrors.ts
+++ lib/proposals/getProposalErrors.ts
@@ -39,7 +39,6 @@ export function getProposalErrors({
     errors.push('Workflow is required');
   }
 
-  // non-templates
   if (page.type === 'proposal') {
     if (requireTemplates && !page.sourceTemplateId) {
       errors.push('Template is required');
@@ -50,40 +49,40 @@ export function getProposalErrors({
     if (proposal.fields?.pendingRewards?.some((r) => r.reward.rewardType === 'token' && !r.reward.rewardAmount)) {
       errors.push('Token amount is required for milestones');
     }
-  }
-
-  if (contentType === 'structured') {
-    if (page.type === 'proposal_template') {
-      // creating template - check if form fields exists
-      errors.push(...[checkFormFieldErrors(proposal.formFields ?? [])].filter(isTruthy));
-    } else if (proposal.formFields && proposal.formAnswers) {
+    if (contentType === 'structured' && proposal.formFields && proposal.formAnswers) {
       const isValid = validateAnswers(proposal.formAnswers || [], proposal.formFields || []);
       // saving proposal - check if required answers are filled
       if (!isValid) {
         errors.push('All required fields must be answered');
       }
+      if (requireMilestone) {
+        if (!proposal.fields?.pendingRewards?.length) {
+          errors.push('At least one milestone is required');
+        }
+      }
+      if (project) {
+        try {
+          validateProposalProject({
+            formAnswers: proposal.formAnswers,
+            formFields: proposal.formFields,
+            project
+          });
+        } catch (error) {
+          errors.push((error as Error).message);
+        }
+      }
     }
-  } else if (contentType === 'free_form' && page.type === 'proposal_template' && !page.hasContent) {
-    errors.push('Content is required for free-form proposals');
+  }
+  // validate templates
+  else if (page.type === 'proposal_template') {
+    if (contentType === 'structured') {
+      // creating template - check if form fields exists
+      errors.push(...[checkFormFieldErrors(proposal.formFields ?? [])].filter(isTruthy));
+    } else if (contentType === 'free_form' && !page.hasContent) {
+      errors.push('Content is required for free-form proposals');
+    }
   }
 
-  if (requireMilestone && page.type !== 'proposal_template' && contentType === 'structured') {
-    if (!proposal.fields?.pendingRewards?.length) {
-      errors.push('At least one milestone is required');
-    }
-  }
-
-  if (project && contentType === 'structured' && proposal.formAnswers) {
-    try {
-      validateProposalProject({
-        formAnswers: proposal.formAnswers,
-        formFields: proposal.formFields,
-        project
-      });
-    } catch (error) {
-      errors.push((error as Error).message);
-    }
-  }
   // check evaluation configurations
   errors.push(...proposal.evaluations.map(getEvaluationFormError).filter(isTruthy));
 
