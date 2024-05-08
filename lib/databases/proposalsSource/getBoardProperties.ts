04a808ec3bfc4884c0b0c32c347a3913b71b11c9
motechFR <mo@anyahq.co>
2024-05-08 15:35:51 +0200

    Handle case where proposal blocks are undefined

--- lib/databases/proposalsSource/getBoardProperties.ts
+++ lib/databases/proposalsSource/getBoardProperties.ts
@@ -118,6 +118,10 @@ function applyProjectProfileProperties(
   boardProperties: IPropertyTemplate[],
   fieldConfig: ProjectAndMembersFieldConfig
 ) {
+  if (!fieldConfig) {
+    return;
+  }
+
   projectFieldProperties.forEach((field) => {
     const config = getFieldConfig(fieldConfig[field.field]);
     if (config.show) {
