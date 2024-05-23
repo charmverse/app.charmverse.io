717dd6616533b2dc98c076e375bcad8edd373473
Safwan Shaheer <devorein00@gmail.com>
2024-05-23 21:18:01 +0600

    Better logic for validating domain names (#4089)
    * Better logic for validating domain names
    
    * Added more test cases

--- lib/utils/domains/__tests__/isValidDomainName.spec.ts
+++ lib/utils/domains/__tests__/isValidDomainName.spec.ts
@@ -3,14 +3,15 @@ import { isValidDomainName } from '../isValidDomainName';
 describe('isValidDomainName', () => {
   it('should validate top-level domain names', () => {
     expect(isValidDomainName('google.com')).toBe(true);
-    expect(isValidDomainName('chamrverse.io')).toBe(true);
+    expect(isValidDomainName('charmverse.io')).toBe(true);
     expect(isValidDomainName('example.xyz')).toBe(true);
     expect(isValidDomainName('123-value.com')).toBe(true);
   });
 
   it('should validate subdomain names', () => {
     expect(isValidDomainName('asd.google.com')).toBe(true);
-    expect(isValidDomainName('app.chamrverse.io')).toBe(true);
+    expect(isValidDomainName('asd.new-google.com')).toBe(true);
+    expect(isValidDomainName('app.charmverse.io')).toBe(true);
     expect(isValidDomainName('qq.example.xyz')).toBe(true);
     expect(isValidDomainName('qwe.qq.example.xyz')).toBe(true);
     expect(isValidDomainName('qwe.0xcharmverse.xyz')).toBe(true);
@@ -25,11 +26,15 @@ describe('isValidDomainName', () => {
   it('should not validate domain with protocol or port', () => {
     expect(isValidDomainName('http://google.com')).toBe(false);
     expect(isValidDomainName('https://app.chamrverse.io')).toBe(false);
-    expect(isValidDomainName('app.chamrverse.io:3000')).toBe(false);
+    expect(isValidDomainName('app.charmverse.io:3000')).toBe(false);
+    expect(isValidDomainName('')).toBe(false);
+    expect(isValidDomainName('app.')).toBe(false);
   });
 
   it('should not validate domain with incorrect characters', () => {
     expect(isValidDomainName('google.com/')).toBe(false);
     expect(isValidDomainName('google@.com')).toBe(false);
+    expect(isValidDomainName('google-.com')).toBe(false);
+    expect(isValidDomainName('-google.com')).toBe(false);
   });
 });
