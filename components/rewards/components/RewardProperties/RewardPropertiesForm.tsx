97e01b97fd42b606e2017eb665bd07f9c72d2821
Safwan Shaheer <devorein00@gmail.com>
2024-05-23 17:56:35 +0600

    Show custom properties for rewards

--- components/rewards/components/RewardProperties/RewardPropertiesForm.tsx
+++ components/rewards/components/RewardProperties/RewardPropertiesForm.tsx
@@ -1,14 +1,10 @@
 import type { BountyStatus, Prisma } from '@charmverse/core/prisma-client';
-import { Box, Collapse, Divider, Stack } from '@mui/material';
+import { KeyboardArrowDown } from '@mui/icons-material';
+import { Box, Collapse, Divider, IconButton, Stack, Typography } from '@mui/material';
 import { useState } from 'react';
 
-import { PropertyLabel } from 'components/common/DatabaseEditor/components/properties/PropertyLabel';
-import { TemplateSelect } from 'components/common/WorkflowSidebar/components/TemplateSelect';
-import { useRewardTemplates } from 'components/rewards/hooks/useRewardTemplates';
 import type { RewardPropertiesField } from 'lib/rewards/blocks/interfaces';
 import type { RewardTemplate } from 'lib/rewards/getRewardTemplate';
-import { getRewardType } from 'lib/rewards/getRewardType';
-import type { RewardType } from 'lib/rewards/interfaces';
 import type { UpdateableRewardFields } from 'lib/rewards/updateRewardSettings';
 import { isTruthy } from 'lib/utils/types';
 
@@ -45,8 +41,6 @@ export function RewardPropertiesForm({
   rewardStatus
 }: Props) {
   const [isExpanded, setIsExpanded] = useState(!!expandedByDefault);
-  const { templates: rewardTemplates = [] } = useRewardTemplates();
-
   async function applyUpdates(updates: Partial<UpdateableRewardFields>) {
     if ('customReward' in updates) {
       const customReward = updates.customReward;
@@ -58,29 +52,6 @@ export function RewardPropertiesForm({
     }
 
     onChange(updates);
-  }
-
-  function setRewardType(rewardType: RewardType) {
-    if (rewardType === 'none' && rewardStatus !== 'paid') {
-      applyUpdates({
-        rewardAmount: null,
-        chainId: null,
-        rewardToken: null,
-        customReward: null
-      });
-    }
-
-    onChange({
-      rewardType,
-      customReward: rewardType === 'custom' ? values.customReward : null,
-      rewardAmount: rewardType === 'token' ? values.rewardAmount : null,
-      rewardToken: rewardType === 'token' ? values.rewardToken : null,
-      chainId: rewardType === 'token' ? values.chainId : null
-    });
-  }
-
-  if (!values) {
-    return null;
   }
 
   return (
@@ -107,45 +78,32 @@ export function RewardPropertiesForm({
           readOnly={readOnly || !pageId}
         />
 
-        <Collapse in={isExpanded} timeout='auto' unmountOnExit>
-          <>
-            {/* {!isTemplate && isNewReward && (
-              <Box justifyContent='space-between' gap={2} alignItems='center' mb='6px'>
-                <Box display='flex' height='fit-content' flex={1} className='octo-propertyrow'>
-                  <PropertyLabel readOnly highlighted>
-                    Template
-                  </PropertyLabel>
-                  <Box display='flex' flex={1}>
-                    <TemplateSelect
-                      options={rewardTemplates.map((rewardTemplate) => rewardTemplate.page)}
-                      disabled={readOnlyTemplate || readOnly}
-                      value={templateId}
-                      onChange={(templatePage) => {
-                        if (!templatePage) {
-                          selectTemplate(null);
-                        }
-                        const selected = rewardTemplates.find((_template) => _template.page.id === templatePage?.id);
-                        if (selected && selectTemplate) {
-                          selectTemplate(selected);
-                        }
-                        setRewardType(getRewardType(selected || values, isNewReward, !!templatePage));
-                      }}
-                    />
-                  </Box>
-                </Box>
-              </Box>
-            )} */}
-
-            <CustomPropertiesAdapter
-              readOnly={readOnly}
-              reward={values as unknown as BoardReward}
-              onChange={(properties: RewardPropertiesField) => {
-                applyUpdates({
-                  fields: { properties: properties ? { ...properties } : {} } as Prisma.JsonValue
-                });
-              }}
+        <Stack
+          direction='row'
+          gap={1}
+          alignItems='center'
+          sx={{ cursor: 'pointer' }}
+          onClick={() => setIsExpanded((v) => !v)}
+        >
+          <Typography fontWeight='bold'>Details</Typography>
+          <IconButton size='small'>
+            <KeyboardArrowDown
+              fontSize='small'
+              sx={{ transform: `rotate(${isExpanded ? 180 : 0}deg)`, transition: 'all 0.2s ease' }}
             />
-          </>
+          </IconButton>
+        </Stack>
+
+        <Collapse in={isExpanded} timeout='auto' unmountOnExit>
+          <CustomPropertiesAdapter
+            readOnly={readOnly}
+            reward={values as unknown as BoardReward}
+            onChange={(properties: RewardPropertiesField) => {
+              applyUpdates({
+                fields: { properties: properties ? { ...properties } : {} } as Prisma.JsonValue
+              });
+            }}
+          />
         </Collapse>
       </Stack>
     </Box>
