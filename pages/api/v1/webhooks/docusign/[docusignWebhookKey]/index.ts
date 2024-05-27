fb8920f31c215fe4149fde01b1df8041c8677840
motechFR <mo@anyahq.co>
2024-05-27 16:05:43 +0200

    Fix build

--- pages/api/v1/webhooks/docusign/[docusignWebhookKey]/index.ts
+++ pages/api/v1/webhooks/docusign/[docusignWebhookKey]/index.ts
@@ -1,4 +1,4 @@
-import { log } from '@charmverse/core/dist/cjs/lib/log';
+import { log } from '@charmverse/core/log';
 import { prisma } from '@charmverse/core/prisma-client';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import nc from 'next-connect';
