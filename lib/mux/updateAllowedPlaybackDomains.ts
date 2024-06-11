39cc1624dc8eb6727b095c157dced6f4126b84b5
mattcasey <mattwad@gmail.com>
2024-06-11 10:13:57 -0600

    limit the number of domains sent to Mux

--- lib/mux/updateAllowedPlaybackDomains.ts
+++ lib/mux/updateAllowedPlaybackDomains.ts
@@ -8,6 +8,8 @@ import { mux } from './muxClient';
 
 const charmVerseDomains = ['*.charmverse.io', '*.charmverse.co'];
 
+const domainLimit = 100; // 100 is the maximum number of allowed domains from Mux
+
 // update referrers to include all custom subdomains
 export async function updateAllowedPlaybackDomains() {
   if (mux && playbackRestrictionId) {
@@ -20,7 +22,7 @@ export async function updateAllowedPlaybackDomains() {
     });
     const customDomains = spaces.map((space) => space.customDomain).filter(isTruthy);
     mux.video.playbackRestrictions.updateReferrer(playbackRestrictionId, {
-      allowed_domains: charmVerseDomains.concat(customDomains)
+      allowed_domains: charmVerseDomains.concat(customDomains).slice(0, domainLimit)
     });
     log.info('Updated referrers for mux playback restriction', { customDomains });
   }
