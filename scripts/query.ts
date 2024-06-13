939b05514bc9a72b2a4e10c980eeff8d5c540529
mattcasey <mattwad@gmail.com>
2024-06-13 15:09:39 -0600

    update scripts

--- scripts/query.ts
+++ scripts/query.ts
@@ -1,94 +1,13 @@
 import { prisma } from '@charmverse/core/prisma-client';
-import { createDraftProposal } from 'lib/proposals/createDraftProposal';
 
 /**
  * Use this script to perform database searches.
  */
 
-const spaceId = 'f3ddde2e-17e9-42b1-803d-0c72880e4669';
-
-async function replaceReviewer(originalUser: string, newUser: string) {
-  const reviewers = await prisma.proposalReviewer.updateMany({
-    where: {
-      proposal: { spaceId },
-      userId: originalUser
-    },
-    data: {
-      userId: newUser
-    }
-  });
-  const spaceRole = await prisma.spaceRole
-    .updateMany({
-      where: {
-        spaceId,
-        userId: originalUser
-      },
-      data: {
-        userId: newUser
-      }
-    })
-    .catch((e) => {
-      console.error('could not update space role');
-      // return prisma.spaceRole.deleteMany({
-      //   where: {
-      //     spaceId,
-      //     userId: originalUser
-      //   }
-      // });
-    });
-  const appealreviewers = await prisma.proposalAppealReviewer.updateMany({
-    where: {
-      proposal: { spaceId },
-      userId: originalUser
-    },
-    data: {
-      userId: newUser
-    }
-  });
-  const emails = await prisma.verifiedEmail.updateMany({
-    where: {
-      userId: originalUser
-    },
-    data: {
-      userId: newUser
-    }
-  });
-  const googleAccounts = await prisma.googleAccount.updateMany({
-    where: {
-      userId: originalUser
-    },
-    data: {
-      userId: newUser
-    }
-  });
-  console.log('updated ', originalUser, '->', newUser, {
-    emails,
-    googleAccounts,
-    spaceRole,
-    reviewers,
-    appealreviewers
-  });
-}
-
 async function search() {
-  const original = await prisma.user.findMany({
+  const result = await prisma.user.findFirst({
     where: {
-      verifiedEmails: {
-        some: {
-          email: 'tamara@helenius.com'
-        }
-      }
-    },
-    include: {
-      verifiedEmails: true,
-      googleAccounts: true,
-      wallets: true,
-      spaceRoles: true
-    }
-  });
-  const replacement = await prisma.user.findMany({
-    where: {
-      username: 'tamarandom.eth'
+      username: 'mattcasey.eth'
     },
     include: {
       verifiedEmails: true,
@@ -97,22 +16,7 @@ async function search() {
       spaceRoles: true
     }
   });
-
-  // await replaceReviewer(original[0].id, replacement[0].id);
-
-  // const proposal = await prisma.proposal.findUniqueOrThrow({
-  //   where: {
-  //     id: 'a8ac2799-5c79-45f7-9527-a1d52d717625'
-  //   },
-  //   include: {
-  //     form: {
-  //       include: {
-  //         formFields: true
-  //       }
-  //     }
-  //   }
-  // });
-  // console.log(proposal.form.formFields);
+  console.log(result);
 }
 
 search().then(() => console.log('Done'));
