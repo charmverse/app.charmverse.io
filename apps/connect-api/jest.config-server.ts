f9e9f4f5268243de3fbdd460e715a86068672a96
motechFR <mo@anyahq.co>
2024-06-13 14:40:12 +0200

    Cleanup connect API test command for CI

--- apps/connect-api/jest.config-server.ts
+++
@@ -1,9 +0,0 @@
-import type { JestConfigWithTsJest } from 'ts-jest';
-
-import defaultJestConfig from './jest.config';
-
-const serverJestConfig: JestConfigWithTsJest = {
-  ...defaultJestConfig,
-  testMatch: ['**/__integration-tests__/**/*.spec.ts']
-};
-export default serverJestConfig;
