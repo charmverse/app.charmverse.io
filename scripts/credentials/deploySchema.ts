3d98c8fe378d1182788637a2f5bac359b4ddb0c7
mattcasey <mattwad@gmail.com>
2024-06-18 14:18:44 -0600

    update schema

--- scripts/credentials/deploySchema.ts
+++ scripts/credentials/deploySchema.ts
@@ -20,42 +20,57 @@ if (!privateKey) {
 
 // const schema = 'uint256 eventId, uint8 tokenWeight';
 
-async function deploy(chain: Chain, _schema: string) {
+const RESOLVER_ADDRESS = '0x3354B452e319E03de8eC4047cB56209304DFA645';
+const RECIPIENT_ADDRESS = '0x66525057AC951a0DB5C9fa7fAC6E056D6b8997E2';
+
+async function deploy(chain: Chain, schema: string, resolverAddress: string = NULL_ADDRESS) {
   // get registry contract
   const schemaRegistry = new SchemaRegistry(easConnectors[chain.id as EasSchemaChain].schemaRegistryContract);
   // connect wallet
   schemaRegistry.connect(_getSigner(privateKey, chain));
 
   // SchemaUID is deterministic
-  const schemaUid = getSchemaUID(_schema, NULL_ADDRESS, true);
-  console.log('Schema UID:', schemaUid);
+  const schemaUid = getSchemaUID(schema, resolverAddress, true);
+  console.log('Deploying Schema', schema, 'UID:', schemaUid);
 
-  const deployedSchema = await schemaRegistry.getSchema({ uid: schemaUid }).catch((err) => null);
+  const deployedSchema = await schemaRegistry.getSchema({ uid: schemaUid }).catch((err) => {
+    // console.error('error retrieving schema', err.message);
+  });
   if (deployedSchema) {
-    console.log('Schema already deployed:');
+    console.log('Schema already deployed!');
     return deployedSchema;
   }
+  // register a new schema
   const transaction = await schemaRegistry.register({
-    schema: _schema,
-    resolverAddress: NULL_ADDRESS,
+    schema: schema,
+    resolverAddress,
     revocable: true
   });
   return transaction;
 }
 
-async function attest(
-  chain: Chain,
-  _schema: string,
-  data: { name: string; value: number | string; type: string }[],
-  refUID?: string
-) {
+async function attest({
+  chain,
+  schema,
+  data,
+  refUID,
+  resolverAddress = NULL_ADDRESS,
+  recipient = RECIPIENT_ADDRESS
+}: {
+  chain: Chain;
+  schema: string;
+  data: { name: string; value: number | string; type: string }[];
+  refUID?: string;
+  resolverAddress?: string;
+  recipient?: string;
+}) {
   // get attestation contract
   const eas = getEasInstance(chain.id as EasSchemaChain);
   // connect wallet
   eas.connect(_getSigner(privateKey, chain));
 
   // Initialize SchemaEncoder with the schema string
-  const schemaEncoder = new SchemaEncoder(_schema);
+  const schemaEncoder = new SchemaEncoder(schema);
   const encodedData = schemaEncoder.encodeData(data);
   // [
   //   { name: 'eventId', value: 1, type: 'uint256' },
@@ -63,19 +78,24 @@ async function attest(
   // ]);
 
   // SchemaUID is deterministic
-  const schemaUid = getSchemaUID(_schema, NULL_ADDRESS, true);
+  const schemaUid = getSchemaUID(schema, resolverAddress, true);
 
-  const response = await eas.attest({
-    schema: schemaUid,
-    data: {
-      recipient: NULL_ADDRESS,
-      // recipient: '0xFD50b031E778fAb33DfD2Fc3Ca66a1EeF0652165',
-      expirationTime: 0,
-      revocable: true, // Be aware that if your schema is not revocable, this MUST be false
-      data: encodedData,
-      refUID: refUID
+  const response = await eas.attest(
+    {
+      schema: schemaUid,
+      data: {
+        recipient,
+        // recipient: '0xFD50b031E778fAb33DfD2Fc3Ca66a1EeF0652165',
+        expirationTime: 0,
+        revocable: true, // Be aware that if your schema is not revocable, this MUST be false
+        data: encodedData,
+        refUID: refUID
+      }
+    },
+    {
+      gasLimit: 1000000
     }
-  });
+  );
 
   const newAttestationUID = await response.wait();
 
@@ -88,7 +108,7 @@ async function getAttest() {
   // const uid = '0xf99e959ae394ff73f9223945e6444f9b1055abd5ee484570d67eb47ade949715';
   // const schema = 'string appName,uint8 score,string appReview';
   // sepolia example
-  const chain = sepolia;
+  const chain = optimismSepolia;
   const uid = '0xc72cc61df805f7a1e2c0477702807f1a8cfbe195f1001022e5a5c5b2ded5f0f9';
   const schema = 'string post';
 
@@ -127,34 +147,39 @@ async function doStuff() {
   const chain = optimismSepolia;
   const uidSchema = 'string uid';
   const profileSchema = 'string name';
-  const eventSchema = 'string event, uint8 type';
-  // deploy
-  console.log('Deploy uid schema', await deploy(chain, uidSchema));
-  console.log('Deploy profile schema', await deploy(chain, profileSchema));
-  console.log('Deploy event schema', await deploy(chain, eventSchema));
+  const uniqueField = 'V123456'; // + Date.now();
+  const eventSchema = 'uint256 Value,string Event,uint8 Category,string ' + uniqueField;
+  // // deploy
+  //console.log('Deploy uid schema', await deploy(chain, uidSchema));
+  // console.log('Deploy profile schema', await deploy(chain, profileSchema));
+  console.log('Deploy event schema', await deploy(chain, eventSchema, RESOLVER_ADDRESS));
 
-  const uidAttestation = await attest(chain, uidSchema, [{ name: 'uid', value: '0x123', type: 'string' }]);
-  console.log('UID Attestation:', uidAttestation);
-  console.log(
-    'UID Attestation Link:',
-    getOnChainAttestationUrl({ chainId: chain.id, attestationId: uidAttestation.uid })
-  );
-  const profileAttestation = await attest(
+  // const uidAttestation = await attest(chain, uidSchema, [{ name: 'uid', value: '0x123', type: 'string' }]);
+  // console.log('UID Attestation:', uidAttestation);
+  // console.log(
+  //   'UID Attestation Link:',
+  //   getOnChainAttestationUrl({ chainId: chain.id, attestationId: uidAttestation.uid })
+  // );
+  // const profileAttestation = await attest(
+  //   chain,
+  //   profileSchema,
+  //   [{ name: 'name', value: 'Wild Thang', type: 'string' }],
+  //   uidAttestation.uid
+  // );
+  // console.log('Profile Attestation:', profileAttestation);
+  const eventAttestation = await attest({
     chain,
-    profileSchema,
-    [{ name: 'name', value: 'Wild Thang', type: 'string' }],
-    uidAttestation.uid
-  );
-  console.log('Profile Attestation:', profileAttestation);
-  const eventAttestation = await attest(
-    chain,
-    eventSchema,
-    [
-      { name: 'event', value: 'ETH Denver sign-up', type: 'string' },
-      { name: 'type', value: 1, type: 'uint8' }
-    ],
-    uidAttestation.uid
-  );
+    schema: eventSchema,
+    resolverAddress: RESOLVER_ADDRESS,
+    data: [
+      { name: 'Value', value: 12, type: 'uint256' },
+      { name: 'Event', value: 'ETH Denver sign-up', type: 'string' },
+      { name: 'Category', value: 1, type: 'uint8' },
+      { name: uniqueField, value: '', type: 'string' }
+    ]
+    //uidAttestation.uid
+    // '0x84027c80e2017d462c549f1a0b62505acdba029147dfd60fb501507635f858b4'
+  });
   console.log('Event Attestation:', eventAttestation);
 }
 
