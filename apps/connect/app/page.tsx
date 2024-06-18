7788c128187a630f817d0291325e4be6f22c322e
Valentin L <ludu.valentin@gmail.com>
2024-06-18 17:31:10 +0300

    Update layout and metadata

--- apps/connect/app/page.tsx
+++ apps/connect/app/page.tsx
@@ -1,6 +1,13 @@
+import { redirect } from 'next/navigation';
+
 import { HomePage } from 'components/pages/HomePage';
 
 export default function Home() {
-  const user = {}; // server
+  const user = {}; // server action
+
+  if (!user) {
+    redirect('/welcome');
+  }
+
   return <HomePage user={user} />;
 }
