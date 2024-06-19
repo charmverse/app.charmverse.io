a14acfde4d139e13e3efe120ccf6354cab986688
Valentin L <ludu.valentin@gmail.com>
2024-06-19 17:22:53 +0300

    Update page home

--- apps/connect/app/page.tsx
+++ apps/connect/app/page.tsx
@@ -3,7 +3,7 @@ import { redirect } from 'next/navigation';
 import { HomePage } from 'components/home/HomePage';
 
 export default function Home() {
-  const user = {}; // server action
+  const user = undefined; // server action
 
   if (user) {
     redirect('/dashboard');
