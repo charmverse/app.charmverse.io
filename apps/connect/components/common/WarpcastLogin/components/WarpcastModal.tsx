9a803f7052dae4549162b559d4a2822e45b32570
Valentin L <ludu.valentin@gmail.com>
2024-06-25 20:32:49 +0300

    Fix modal issue

--- apps/connect/components/common/WarpcastLogin/components/WarpcastModal.tsx
+++ apps/connect/components/common/WarpcastLogin/components/WarpcastModal.tsx
@@ -2,15 +2,17 @@
 
 import { CanvasQRCode } from '@connect/components/common/CanvasQrCode';
 import Box from '@mui/material/Box';
+import type { DialogProps } from '@mui/material/Dialog';
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
 import Link from '@mui/material/Link';
-import type { ModalProps } from '@mui/material/Modal';
-import Modal from '@mui/material/Modal';
 import Typography from '@mui/material/Typography';
 
-export function FarcasterLoginModal({ url, ...props }: Omit<ModalProps, 'children'> & { url?: string }) {
+export function FarcasterLoginModal({ url, ...props }: Omit<DialogProps, 'children'> & { url?: string }) {
   return (
-    <Modal open={props.open && !!url} onClose={props.onClose} title='Sign in with Warpcast'>
-      <>
+    <Dialog open={props.open && !!url} onClose={props.onClose} title='Sign in with Warpcast'>
+      <DialogTitle>Sign in with Warpcast</DialogTitle>
+      <Box px={3} pb={3}>
         <Typography variant='body1'>Scan with your phone's camera to continue.</Typography>
         <Box display='flex' justifyContent='center' my={2}>
           <CanvasQRCode uri={url || ''} />
@@ -18,7 +20,7 @@ export function FarcasterLoginModal({ url, ...props }: Omit<ModalProps, 'childre
         <Link href={url} sx={{ p: 0 }}>
           I'm using my phone →
         </Link>
-      </>
-    </Modal>
+      </Box>
+    </Dialog>
   );
 }
