93be61810cadfc201ae3dd9a2a0baa0b44a1a2b5
Valentin L <ludu.valentin@gmail.com>
2024-06-26 15:52:19 +0300

    Add more dialog elements

--- apps/connect/components/common/WarpcastLogin/components/WarpcastModal.tsx
+++ apps/connect/components/common/WarpcastLogin/components/WarpcastModal.tsx
@@ -1,26 +1,22 @@
 'use client';
 
 import { CanvasQRCode } from '@connect/components/common/CanvasQrCode';
-import Box from '@mui/material/Box';
+import { Dialog, DialogTitle, DialogActions, DialogContent, DialogContentText, Link } from '@mui/material';
 import type { DialogProps } from '@mui/material/Dialog';
-import Dialog from '@mui/material/Dialog';
-import DialogTitle from '@mui/material/DialogTitle';
-import Link from '@mui/material/Link';
-import Typography from '@mui/material/Typography';
 
 export function FarcasterLoginModal({ url, ...props }: Omit<DialogProps, 'children'> & { url?: string }) {
   return (
     <Dialog open={props.open && !!url} onClose={props.onClose} title='Sign in with Warpcast'>
       <DialogTitle>Sign in with Warpcast</DialogTitle>
-      <Box px={3} pb={3}>
-        <Typography variant='body1'>Scan with your phone's camera to continue.</Typography>
-        <Box display='flex' justifyContent='center' my={2}>
-          <CanvasQRCode uri={url || ''} />
-        </Box>
-        <Link href={url} sx={{ p: 0 }}>
-          I'm using my phone →
-        </Link>
-      </Box>
+      <DialogContent>
+        <DialogContentText variant='body1'>Scan with your phone's camera to continue.</DialogContentText>
+        <CanvasQRCode uri={url || ''} />
+        <DialogActions sx={{ justifyContent: 'start' }}>
+          <Link href={url} sx={{ p: 0 }}>
+            I'm using my phone →
+          </Link>
+        </DialogActions>
+      </DialogContent>
     </Dialog>
   );
 }
