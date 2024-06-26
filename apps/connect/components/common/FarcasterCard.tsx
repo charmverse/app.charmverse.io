a0e02e5fe9a1cccece7fbcbd366f14f35c8dccce
Valentin L <ludu.valentin@gmail.com>
2024-06-26 15:51:55 +0300

    Farcaster card props should be more verbose

--- apps/connect/components/common/FarcasterCard.tsx
+++ apps/connect/components/common/FarcasterCard.tsx
@@ -4,11 +4,11 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 
-export function FarcasterCard({ username, pfpUrl, bio }: any) {
+export function FarcasterCard({ username, avatar, bio }: { bio: string; username: string; avatar: string }) {
   return (
     <Card>
       <CardContent sx={{ display: 'flex', gap: 2 }}>
-        <Avatar size='xLarge' name={username} avatar={pfpUrl} />
+        <Avatar size='xLarge' name={username} avatar={avatar} />
         <Box>
           <Typography>{username}</Typography>
           <Typography>{bio}</Typography>
