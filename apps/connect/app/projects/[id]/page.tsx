ac7a353b030352dad1872b424be365e40756440d
Safwan Shaheer <devorein00@gmail.com>
2024-06-27 15:52:25 +0600

    Skeleton for project details page

--- apps/connect/app/projects/[id]/page.tsx
+++ apps/connect/app/projects/[id]/page.tsx
@@ -1,10 +1,10 @@
-import { PageWrapper } from '@connect/components/common/PageWrapper';
+import { ProjectDetailsSkeleton } from '@connect/components/projects/components/ProjectDetailsSkeleton';
 import { ProjectDetails } from '@connect/components/projects/ProjectDetails';
 import { Suspense } from 'react';
 
 export default function ProjectPage({ params }: { params: { id: string } }) {
   return (
-    <Suspense fallback={<PageWrapper>Loading...</PageWrapper>}>
+    <Suspense fallback={<ProjectDetailsSkeleton />}>
       <ProjectDetails projectId={params.id} />
     </Suspense>
   );
