b7944de255489fdc467406c2457a688d2b084d1c
Safwan Shaheer <devorein00@gmail.com>
2024-06-28 20:12:20 +0600

    Expand getFarcasterUsers lib to support wallets and fid

--- apps/connect-api/src/api/farcaster/get-by-username.ts
+++ apps/connect-api/src/api/farcaster/get-by-username.ts
@@ -1,13 +1,15 @@
 import type { RouterContext } from '@connect-api/koa';
 import { PathBasedRouter } from '@connect-api/lib/pathBasedRouter';
-import type { FarcasterUser } from 'lib/farcaster/getFarcasterUsersByUsername';
-import { getFarcasterUsersByUsername } from 'lib/farcaster/getFarcasterUsersByUsername';
+import type { FarcasterUser } from 'lib/farcaster/getFarcasterUsers';
+import { getFarcasterUsers } from 'lib/farcaster/getFarcasterUsers';
 
 const router = new PathBasedRouter();
 
 router.GET(async (ctx: RouterContext<undefined, FarcasterUser[]>) => {
   const username = ctx.query.username as string;
-  ctx.body = await getFarcasterUsersByUsername(username);
+  ctx.body = await getFarcasterUsers({
+    username
+  });
 });
 
 export default router;
