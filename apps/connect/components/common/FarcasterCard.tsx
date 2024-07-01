07764c6ea695c85fcd711797c45cbe024c0f895b
Safwan Shaheer <devorein00@gmail.com>
2024-07-01 18:46:17 +0600

    Connect user profile page

--- apps/connect/components/common/FarcasterCard.tsx
+++ apps/connect/components/common/FarcasterCard.tsx
@@ -1,7 +1,7 @@
 import type { AvatarSize } from '@connect/components/common/Avatar';
 import { Avatar } from '@connect/components/common/Avatar';
 import { DeleteOutline } from '@mui/icons-material';
-import { Box, Card, CardContent, IconButton, Typography } from '@mui/material';
+import { Box, Card, CardActionArea, CardContent, IconButton, Link, Typography } from '@mui/material';
 import { Stack } from '@mui/system';
 
 export function FarcasterCard({
@@ -23,40 +23,42 @@ export function FarcasterCard({
 }) {
   return (
     <Card>
-      <CardContent
-        sx={{
-          display: 'flex',
-          gap: 2,
-          alignItems: 'center',
-          flexDirection: {
-            xs: 'column',
-            sm: 'row'
-          }
-        }}
-      >
-        <Stack
-          alignItems={{
-            xs: 'center',
-            sm: 'flex-start'
+      <CardActionArea LinkComponent={Link} href={`/u/${username}`} hrefLang='en'>
+        <CardContent
+          sx={{
+            display: 'flex',
+            gap: 2,
+            alignItems: 'center',
+            flexDirection: {
+              xs: 'column',
+              sm: 'row'
+            }
           }}
         >
-          <Avatar size={avatarSize} name={username || 'N/A'} avatar={avatar} />
-        </Stack>
-        <Box width='100%'>
-          <Stack direction='row' justifyContent='space-between' width='100%' alignItems='center'>
-            <Typography variant='h6'>{name || 'N/A'}</Typography>
-            {onDelete && (
-              <IconButton size='small'>
-                <DeleteOutline color='error' onClick={onDelete} fontSize='small' />
-              </IconButton>
-            )}
+          <Stack
+            alignItems={{
+              xs: 'center',
+              sm: 'flex-start'
+            }}
+          >
+            <Avatar size={avatarSize} name={username || 'N/A'} avatar={avatar} />
           </Stack>
-          <Typography variant='subtitle1' color='secondary'>
-            {username || 'N/A'} #{fid || 'N/A'}
-          </Typography>
-          <Typography>{bio}</Typography>
-        </Box>
-      </CardContent>
+          <Box width='100%'>
+            <Stack direction='row' justifyContent='space-between' width='100%' alignItems='center'>
+              <Typography variant='h6'>{name || 'N/A'}</Typography>
+              {onDelete && (
+                <IconButton size='small'>
+                  <DeleteOutline color='error' onClick={onDelete} fontSize='small' />
+                </IconButton>
+              )}
+            </Stack>
+            <Typography variant='subtitle1' color='secondary'>
+              {username || 'N/A'} #{fid || 'N/A'}
+            </Typography>
+            <Typography>{bio}</Typography>
+          </Box>
+        </CardContent>
+      </CardActionArea>
     </Card>
   );
 }
