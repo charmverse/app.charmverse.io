307570f80a555438e2beada7587327c396b63878
mattcasey <mattwad@gmail.com>
2024-07-08 12:17:42 -0600

    add test for non-editors

--- __e2e__/proposals/convertPageToProposal.spec.ts
+++ __e2e__/proposals/convertPageToProposal.spec.ts
@@ -1,10 +1,11 @@
 import { PageHeader } from '__e2e__/po/pageHeader.po';
 import { expect, test as base } from '__e2e__/utils/test';
 
+import { createPage } from 'testing/setupDatabase';
 import { generateProposalWorkflowWithEvaluations } from 'testing/utils/proposals';
 
 import { DocumentPage } from '../po/document.po';
-import { generateUserAndSpace } from '../utils/mocks';
+import { generateUser, generateUserAndSpace } from '../utils/mocks';
 import { login } from '../utils/session';
 
 type Fixtures = {
@@ -44,9 +45,35 @@ test('convert page to proposal - create a page, convert that page to proposal an
     domain: space.domain,
     path: generatedPage.path
   });
+
   await expect(documentPage.proposalBanner).toBeVisible();
 
-  // const isEditable = await documentPage.isPageEditable();
+  const isEditable = await documentPage.isPageEditable();
 
-  // expect(isEditable).toBe(false);
+  expect(isEditable).toBe(false);
+});
+
+test('convert page to proposal - does not allow a non-editor', async ({ documentPage, proposalPage }) => {
+  const { space, user, page: generatedPage } = await generateUserAndSpace({ isAdmin: true });
+  const nonEditor = await generateUser({ space: { id: space.id } });
+
+  const page = await createPage({
+    spaceId: space.id,
+    createdBy: user.id,
+    title: 'Test Page',
+    pagePermissions: []
+  });
+  await generateProposalWorkflowWithEvaluations({ spaceId: space.id });
+
+  await login({
+    page: documentPage.page,
+    userId: nonEditor.id
+  });
+
+  await documentPage.goToPage({
+    domain: space.domain,
+    path: `/proposals/new?sourcePageId=${page.id}`
+  });
+
+  await expect(documentPage.errorPage).toBeVisible();
 });
