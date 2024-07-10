8f67027d461e55af07d3ab8efe3247b594d521b1
Safwan Shaheer <devorein00@gmail.com>
2024-07-10 16:16:46 +0600

    Project & user not found page layout

--- apps/connect/app/u/[username]/page.tsx
+++ apps/connect/app/u/[username]/page.tsx
@@ -1,9 +1,13 @@
 import { ProfileDetailsPage } from '@connect/components/profile/[username]/ProfileDetailsPage';
 import { fetchUserByFarcasterUsername } from '@connect/lib/farcaster/fetchUserByFarcasterUsername';
+import { notFound } from 'next/navigation';
 
 export const dynamic = 'force-dynamic';
 
 export default async function Profile({ params }: { params: { username: string } }) {
   const farcasterUser = await fetchUserByFarcasterUsername(params.username);
+  if (!farcasterUser) {
+    return notFound();
+  }
   return <ProfileDetailsPage user={farcasterUser} />;
 }
---
+++ apps/connect/components/common/NotFoundPage.tsx
@@ -0,0 +1,21 @@
+import Bolt from '@connect/public/images/lightning_bolt.svg';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
+export function NotFoundPage({ content = 'The page you are looking for does not exist!' }: { content?: string }) {
+  return (
+    <Box
+      height='100vh'
+      width='100%'
+      display='flex'
+      alignItems='center'
+      justifyContent='center'
+      overflow='hidden'
+      flexDirection='column'
+      gap={5}
+    >
+      <Bolt />
+      <Typography variant='subtitle1'>{content}</Typography>
+    </Box>
+  );
+}
