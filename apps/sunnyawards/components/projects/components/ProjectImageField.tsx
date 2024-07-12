82c165307bf1d21e12abd765325214d8622056c8
mattcasey <mattwad@gmail.com>
2024-07-12 10:43:58 -0600

    remove alt text on image

--- apps/sunnyawards/components/projects/components/ProjectImageField.tsx
+++ apps/sunnyawards/components/projects/components/ProjectImageField.tsx
@@ -79,8 +79,8 @@ export function ProjectImageField({
               <CircularProgress color='secondary' size={40} />
             ) : field.value ? (
               <Image
+                alt=''
                 src={field.value as string}
-                alt={`Add ${type}`}
                 width={500}
                 height={height}
                 sizes='100vw'
