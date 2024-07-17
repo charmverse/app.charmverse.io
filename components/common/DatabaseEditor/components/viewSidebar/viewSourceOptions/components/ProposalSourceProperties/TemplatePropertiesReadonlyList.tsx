996a5d9139d8f3c10aa397c7324b863d85f97a55
Safwan Shaheer <devorein00@gmail.com>
2024-07-17 16:04:45 +0600

    Proposal source board group rubric evaluation and criteria properties with templates (#4261)
    * Support for per template based form field and rubric evaluation properties
    
    * feat: Add RubricEvaluationProperty type and update TemplatePropertiesReadonlyList component
    
    * feat: Add templateId property to IPropertyTemplate interface
    
    * Fixed issue with evaluation properties not showing up
    
    * Fixed form fields not being shown as columns
    
    * Fixed proposal step not shown in proposal as a source bd
    
    * Hide board evaluation properties not part of proposal templates anymore
    
    * Fixed per reviewer rubric evaluation properties
    
    * Updated filterBoardProperties tests
    
    * Fixed broken type issues
    
    * Fixed broken tests for getBoardProperties
    
    * Fixed broken getCardProperties test
    
    * Added tests for createSelectedPropertiesStateFromBoardProperties
    
    * Fixed broken tests
    
    * Migration script
    
    * Fixed form fields and duplicate rubric properties
    
    * Fixed broken tests and types

---
+++ components/common/DatabaseEditor/components/viewSidebar/viewSourceOptions/components/ProposalSourceProperties/TemplatePropertiesReadonlyList.tsx
@@ -0,0 +1,78 @@
+import { Stack, Typography } from '@mui/material';
+
+import { useProposalTemplates } from 'components/proposals/hooks/useProposalTemplates';
+
+import type { RubricEvaluationProperty, SelectedProposalProperties } from './ProposalSourcePropertiesDialog';
+import { SelectedPropertiesList } from './SelectedPropertiesList';
+
+const RubricPropertyLabel: Record<RubricEvaluationProperty, string> = {
+  average: 'Step Average',
+  total: 'Step Total',
+  reviewers: 'Step Reviewers',
+  criteriaTotal: 'Criteria Total',
+  criteriaAverage: 'Criteria Average',
+  reviewerScore: 'Individual Reviewer Scores',
+  reviewerComment: 'Individual Reviewer Comments',
+  reviewerAverage: 'Reviewer Average'
+};
+
+export function TemplatePropertiesReadonlyList({
+  selectedProperties
+}: {
+  selectedProperties: SelectedProposalProperties;
+}) {
+  const { proposalTemplates } = useProposalTemplates({
+    detailed: true
+  });
+
+  if (selectedProperties.templateProperties.length === 0) {
+    return null;
+  }
+
+  return (
+    <Stack gap={1}>
+      <Typography fontWeight='bold' variant='h6'>
+        Template Properties
+      </Typography>
+      <Stack gap={2}>
+        {selectedProperties.templateProperties.map((templateProperty) => {
+          const proposalTemplate = proposalTemplates?.find(
+            (template) => template.pageId === templateProperty.templateId
+          );
+          if (!proposalTemplate) {
+            return null;
+          }
+
+          return (
+            <Stack key={templateProperty.templateId} gap={1}>
+              <Typography fontWeight='bold'>{proposalTemplate.title}</Typography>
+              {templateProperty.formFields.length ? (
+                <SelectedPropertiesList
+                  items={
+                    proposalTemplate.formFields
+                      ?.filter((formField) => templateProperty.formFields.includes(formField.id))
+                      .map((formField) => formField.name) ?? []
+                  }
+                  title='Form fields'
+                  titleVariant='body2'
+                  hideDivider
+                />
+              ) : null}
+
+              {templateProperty.rubricEvaluations.map((rubricEvaluation) => (
+                <Stack key={rubricEvaluation.evaluationId}>
+                  <SelectedPropertiesList
+                    items={rubricEvaluation.properties.map((property) => RubricPropertyLabel[property])}
+                    title={rubricEvaluation.title}
+                    titleVariant='body2'
+                    hideDivider
+                  />
+                </Stack>
+              ))}
+            </Stack>
+          );
+        })}
+      </Stack>
+    </Stack>
+  );
+}
