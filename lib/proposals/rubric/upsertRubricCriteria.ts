828c0abd7575b665d52b4729e0af7b0c69261bb9
mattcasey <mattwad@gmail.com>
2024-07-22 16:32:47 -0600

    fix index order

--- lib/proposals/rubric/upsertRubricCriteria.ts
+++ lib/proposals/rubric/upsertRubricCriteria.ts
@@ -81,7 +81,7 @@ export async function upsertRubricCriteria({
           },
           create: {
             id: rubricCriteriaId,
-            index: rubric.index || index,
+            index: rubric.index ?? index,
             title: rubric.title,
             description: rubric.description,
             type: rubric.type,
