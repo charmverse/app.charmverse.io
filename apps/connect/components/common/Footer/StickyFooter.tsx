b94379c9cb8c098b1c658fc7b0f9d934f30acfda
mattcasey <mattwad@gmail.com>
2024-07-29 15:50:04 -0600

    add spacer

--- apps/connect/components/common/Footer/StickyFooter.tsx
+++ apps/connect/components/common/Footer/StickyFooter.tsx
@@ -3,27 +3,36 @@
 import ProfileIcon from '@mui/icons-material/AccountBox';
 import GrantIcon from '@mui/icons-material/Feed';
 import FeedIcon from '@mui/icons-material/Newspaper';
-import { BottomNavigation, BottomNavigationAction, Paper } from '@mui/material';
+import { Box, BottomNavigation, BottomNavigationAction, Paper } from '@mui/material';
 import { usePathname } from 'next/navigation';
-import { useState } from 'react';
 
 export function StickyFooter() {
   const pathname = usePathname();
 
   return (
-    <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
-      <BottomNavigation showLabels value={getActiveButton(pathname)}>
-        <BottomNavigationAction
-          label='Profile'
-          href='/profile'
-          value='profile'
-          icon={<ProfileIcon fontSize='small' />}
-          sx={{ gap: '4px' }}
-        />
-        <BottomNavigationAction label='Feed' href='feed' value='feed' icon={<FeedIcon />} sx={{ gap: '4px' }} />
-        <BottomNavigationAction label='Grants' href='grants' value='grants' icon={<GrantIcon />} sx={{ gap: '4px' }} />
-      </BottomNavigation>
-    </Paper>
+    <>
+      {/* include element to add spacing at the bottom of the page */}
+      <Box height='56px' />
+      <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
+        <BottomNavigation showLabels value={getActiveButton(pathname)}>
+          <BottomNavigationAction
+            label='Profile'
+            href='/profile'
+            value='profile'
+            icon={<ProfileIcon fontSize='small' />}
+            sx={{ gap: '4px' }}
+          />
+          <BottomNavigationAction label='Feed' href='feed' value='feed' icon={<FeedIcon />} sx={{ gap: '4px' }} />
+          <BottomNavigationAction
+            label='Grants'
+            href='grants'
+            value='grants'
+            icon={<GrantIcon />}
+            sx={{ gap: '4px' }}
+          />
+        </BottomNavigation>
+      </Paper>
+    </>
   );
 }
 
