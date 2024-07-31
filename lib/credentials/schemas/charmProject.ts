e48c8acc43cf3c0f57eb2b95bf0a6aef06f660c1
motechFR <mo@anyahq.co>
2024-07-31 15:33:43 +0200

    WIP schema

--- lib/credentials/schemas/charmProject.ts
+++ lib/credentials/schemas/charmProject.ts
@@ -1,36 +1,23 @@
 import type { QualifyingEventType } from '@charmverse/core/prisma-client';
-import { SchemaEncoder } from '@ethereum-attestation-service/eas-sdk';
+import { SchemaEncoder, getSchemaUID } from '@ethereum-attestation-service/eas-sdk';
+
+import { NULL_ADDRESS } from '../constants';
 
 import type { TypedSchemaItem } from './interfaces';
 
 // This schema ID is bound to change as we move through resolver contract versions
 export const charmProjectSchemaId = '0xffebe1dbfc8c97cd866b63e442d6c725b78bc4c2de0683d332d5de5e6c11bd19';
 
-export const charmProjectSchemaDefinition =
-  'string name,string category,string projectRefUID,string metadataSnapshotRefUID,string userRefUID';
+export const charmProjectSchemaDefinition = 'string projectUid';
 
 export type CharmProject = {
-  name: string;
-  category: QualifyingEventType;
-  projectRefUID: string;
-  metadataSnapshotRefUID: string;
-  userRefUID: string;
+  projectUid: string;
 };
 
-export function encodeCharmProject({
-  name,
-  category,
-  projectRefUID,
-  metadataSnapshotRefUID,
-  userRefUID
-}: CharmProject) {
+export function encodeCharmProject({ projectUid }: CharmProject) {
   const encoder = new SchemaEncoder(charmProjectSchemaDefinition);
   const encodedData = encoder.encodeData([
-    { name: 'name', value: name, type: 'string' },
-    { name: 'category', value: category, type: 'string' },
-    { name: 'projectRefUID', value: projectRefUID, type: 'string' },
-    { name: 'metadataSnapshotRefUID', value: metadataSnapshotRefUID, type: 'string' },
-    { name: 'userRefUID', value: userRefUID, type: 'string' }
+    { name: 'projectUid', value: projectUid, type: 'string' }
   ] as TypedSchemaItem<CharmProject>[]);
 
   return encodedData;
@@ -47,3 +34,5 @@ export function decodeCharmProject(rawData: string): CharmProject {
 
   return values as CharmProject;
 }
+
+// console.log(getSchemaUID(charmProjectSchemaId, NULL_ADDRESS, false));
