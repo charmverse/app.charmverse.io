82bf5286cf2b5f155912c5f794442fa3f790259a
mattcasey <mattwad@gmail.com>
2024-08-01 18:12:23 -0600

    attempt to fix flashing of colors in table

--- theme/darkMode.ts
+++ theme/darkMode.ts
@@ -1,6 +1,14 @@
+import type { PaletteMode } from '@mui/material';
+
 export function setDarkMode(darkMode: boolean) {
-  const currentTheme = darkMode ? 'dark' : 'light';
+  const currentTheme: PaletteMode = darkMode ? 'dark' : 'light';
   if (typeof document !== 'undefined') {
     document.documentElement.setAttribute('data-theme', currentTheme);
   }
+}
+export function getDarkMode(): PaletteMode {
+  if (typeof document !== 'undefined') {
+    return (document.documentElement.getAttribute('data-theme') as PaletteMode) || 'light';
+  }
+  return 'light';
 }
