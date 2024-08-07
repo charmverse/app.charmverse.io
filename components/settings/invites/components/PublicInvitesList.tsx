beb592deb3956cdf208337a1301582fb755ab3e7
mattcasey <mattwad@gmail.com>
2024-08-07 09:31:39 -0600

    fix table layout

--- components/settings/invites/components/PublicInvitesList.tsx
+++ components/settings/invites/components/PublicInvitesList.tsx
@@ -93,22 +93,20 @@ function InviteRow({ invite, isAdmin, updateInviteLinkRoles, deleteInviteLink }:
           />
         </TableCell>
         <TableCell width={90}>
-          {isAdmin && (
-            <Tooltip arrow placement='top' title={copied ? 'Copied!' : 'Click to copy link'} disableInteractive>
-              <Box component='span'>
-                <CopyToClipboard text={getInviteLink(invite.code)} onCopy={onCopy}>
-                  <Chip
-                    sx={{ width: 90 }}
-                    clickable
-                    color='secondary'
-                    size='small'
-                    variant='outlined'
-                    label={copied ? 'Copied!' : 'Copy Link'}
-                  />
-                </CopyToClipboard>
-              </Box>
-            </Tooltip>
-          )}
+          <Tooltip arrow placement='top' title={copied ? 'Copied!' : 'Click to copy link'} disableInteractive>
+            <Box component='span'>
+              <CopyToClipboard text={getInviteLink(invite.code)} onCopy={onCopy}>
+                <Chip
+                  sx={{ width: 90 }}
+                  clickable
+                  color='secondary'
+                  size='small'
+                  variant='outlined'
+                  label={copied ? 'Copied!' : 'Copy Link'}
+                />
+              </CopyToClipboard>
+            </Box>
+          </Tooltip>
         </TableCell>
         <TableCell width={30}>
           {isAdmin && (
