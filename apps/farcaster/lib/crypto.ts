1295e74019907456e228aa00a5608b1d4ffaced1
Safwan Shaheer <devorein00@gmail.com>
2024-08-09 22:25:12 +0600

    getKey function to get auth secret encrypted key

--- apps/farcaster/lib/crypto.ts
+++ apps/farcaster/lib/crypto.ts
@@ -2,25 +2,22 @@ import crypto from 'node:crypto';
 
 import { authSecret } from '@root/config/constants';
 
-const IV = new Uint8Array(Buffer.alloc(16, 0));
-
-export function encrypt(data: string): string {
+function getKey() {
   if (!authSecret) {
     throw new Error('The AUTH_SECRET env var is required to encrypt data');
   }
-  const KEY = new Uint8Array(crypto.createHash('sha256').update(authSecret).digest());
-  const cipher = crypto.createCipheriv('aes-256-cbc', KEY, IV);
+  return new Uint8Array(crypto.createHash('sha256').update(authSecret).digest());
+}
+
+export function encrypt(data: string): string {
+  const cipher = crypto.createCipheriv('aes-256-cbc', getKey(), new Uint8Array(Buffer.alloc(16, 0)));
   let encrypted = cipher.update(data, 'utf8', 'hex');
   encrypted += cipher.final('hex');
   return encrypted;
 }
 
 export function decrypt(encryptedData: string): string {
-  if (!authSecret) {
-    throw new Error('The AUTH_SECRET env var is required to encrypt data');
-  }
-  const KEY = new Uint8Array(crypto.createHash('sha256').update(authSecret).digest());
-  const decipher = crypto.createDecipheriv('aes-256-cbc', KEY, IV);
+  const decipher = crypto.createDecipheriv('aes-256-cbc', getKey(), new Uint8Array(Buffer.alloc(16, 0)));
   let decrypted = decipher.update(encryptedData, 'hex', 'utf8');
   decrypted += decipher.final('utf8');
   return decrypted;
