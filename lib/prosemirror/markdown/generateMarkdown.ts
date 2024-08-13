b07de2b6841d797f872417e8f69a1534e84a44bd
motechFR <mo@anyahq.co>
2024-08-13 16:35:36 +0200

    Fix markdown export

--- lib/prosemirror/markdown/generateMarkdown.ts
+++ lib/prosemirror/markdown/generateMarkdown.ts
@@ -1,6 +1,9 @@
 import type { User } from '@charmverse/core/prisma';
+import { prettyPrint } from '@root/lib/utils/strings';
 import { Node } from 'prosemirror-model';
 
+import { mentionSuggestMarkName } from 'components/common/CharmEditor/components/mention';
+import { mentionSuggestSpec } from 'components/common/CharmEditor/components/mention/mention.specs';
 import { replaceNestedPages } from 'components/common/CharmEditor/components/nestedPage/nestedPage.utils';
 import { specRegistry } from 'components/common/CharmEditor/specRegistry';
 
@@ -19,9 +22,15 @@ export async function generateMarkdown({
   content: any;
   generatorOptions?: CharmMarkdownGeneratorOptions;
 }): Promise<string> {
+  prettyPrint({ title, content, generatorOptions });
+
   const serializer = markdownSerializer(specRegistry);
 
   (serializer.options as any).charmOptions = generatorOptions;
+
+  serializer.marks[mentionSuggestMarkName] = mentionSuggestSpec().markdown?.toMarkdown as any;
+
+  // TODO - Clean up the nodes here
 
   let markdown = content ? serializer.serialize(Node.fromJSON(specRegistry.schema, content)) : '';
 
