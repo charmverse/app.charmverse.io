a64368057334a7752407a37d711459cf72e6b6e4
mattcasey <mattwad@gmail.com>
2024-08-13 23:09:05 -0600

    make viewing the product updates support fid

--- apps/farcaster/app/product-updates/page.tsx
+++ apps/farcaster/app/product-updates/page.tsx
@@ -2,23 +2,25 @@ import { PageWrapper } from '@connect-shared/components/common/PageWrapper';
 import { getFarcasterUsers } from '@root/lib/farcaster/getFarcasterUsers';
 
 import { ProductUpdatesComposerAction } from 'components/product-updates/ProductUpdatesComposerAction';
-import { decrypt } from 'lib/crypto';
+import { encrypt, decrypt } from 'lib/crypto';
 import { getConnectProjectsByFid } from 'lib/projects/getConnectProjectsByFid';
 
 export default async function Home({
   searchParams
 }: {
   searchParams: {
-    token: string;
+    fid?: string;
+    token?: string;
   };
 }) {
-  const token = searchParams.token || null;
+  const token = searchParams.token;
+  const fidParam = searchParams.fid;
 
-  if (!token) {
-    throw new Error('Invalid state');
+  const fidStr = token ? decrypt(token) : fidParam;
+  if (!fidStr) {
+    throw new Error('Missing user query');
   }
-
-  const fid = parseInt(decrypt(token.toString()));
+  const fid = parseInt(fidStr);
 
   const [farcasterUser] = await getFarcasterUsers({ fids: [fid] });
 
