fa0b00245576267d2568a07bf783e0ba121effc6
mattcasey <mattwad@gmail.com>
2024-08-15 16:50:49 -0600

    require email to continue

--- apps/sunnyawards/lib/profile/form.ts
+++ apps/sunnyawards/lib/profile/form.ts
@@ -1,7 +1,14 @@
 import * as yup from 'yup';
 
 export const schema = yup.object({
-  email: yup.string().email('Invalid email').nullable(),
+  email: yup
+    .string()
+    .email('Invalid email')
+    .nullable()
+    .when('notify', {
+      is: true,
+      then: (s) => s.required('Must enter email address')
+    }),
   terms: yup.bool().required('Terms are Required').oneOf([true], 'You need to accept the terms and conditions.'),
   notify: yup.bool()
 });
