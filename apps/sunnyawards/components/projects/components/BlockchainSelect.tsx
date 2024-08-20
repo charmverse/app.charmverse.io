7dcf2284bc6789de08f00dcc8faa88ccf5195573
mattcasey <mattwad@gmail.com>
2024-08-20 17:44:18 -0600

    fix images

--- apps/sunnyawards/components/projects/components/BlockchainSelect.tsx
+++ apps/sunnyawards/components/projects/components/BlockchainSelect.tsx
@@ -15,12 +15,12 @@ const chainOptions: { name: string; id: number; icon: string }[] = [
   { name: 'Fraxtal', id: 2522, icon: '/images/crypto/frax64.png' },
   { name: 'Redstone', id: 690, icon: '/images/crypto/redstone64.png' },
   { name: 'Cyber', id: 7560, icon: '/images/crypto/cyber64.png' },
-  { name: 'Polynomial', id: 8008, icon: '/images/crypto/polynomial.jpeg' },
-  { name: 'Xterio', id: 2702128, icon: '/images/crypto/xterio.jpeg' },
-  { name: 'Kroma', id: 255, icon: '/images/crypto/kroma.jpeg' },
-  { name: 'Swan', id: 254, icon: '/images/crypto/swanchain.jpeg' },
-  { name: 'BOB', id: 6080, icon: '/images/crypto/bob.jpeg' },
-  { name: 'Orderly Network', id: 291, icon: '/images/crypto/orderly.jpeg' }
+  { name: 'Polynomial', id: 8008, icon: '/images/crypto/polynomial.png' },
+  { name: 'Xterio', id: 2702128, icon: '/images/crypto/xterio.png' },
+  { name: 'Kroma', id: 255, icon: '/images/crypto/kroma.png' },
+  { name: 'Swan', id: 254, icon: '/images/crypto/swan.png' },
+  { name: 'BOB', id: 6080, icon: '/images/crypto/bob.png' },
+  { name: 'Orderly Network', id: 291, icon: '/images/crypto/orderly.png' }
 ];
 
 function SelectField(props: SelectProps<string> & { helperMessage?: ReactNode }, ref: Ref<unknown>) {
