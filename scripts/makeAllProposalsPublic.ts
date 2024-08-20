23cbcbcfdfb841ba3fd488208003b2a19619ce0a
motechFR <mo@anyahq.co>
2024-08-20 20:11:12 +0100

    Script for making all proposals public (#4463)

---
+++ scripts/makeAllProposalsPublic.ts
@@ -0,0 +1,89 @@
+import { ProposalOperation, prisma } from "@charmverse/core/prisma-client"
+import { ProposalWorkflowTyped } from "@charmverse/core/proposals";
+
+
+
+const spaceDomain = 'demo-space'
+
+
+async function makeAllProposalsPublic() {
+  const spaceId = await prisma.space.findUniqueOrThrow({
+    where: {
+      domain: spaceDomain
+    },
+    select: {
+      id: true
+    }
+  }).then(space => space.id);
+
+
+  const workflows = (await prisma.proposalWorkflow.findMany({
+    where: {
+      spaceId: spaceId
+    }
+  })) as ProposalWorkflowTyped[];
+
+  let i = 0;
+
+  for (const workflow of workflows) {
+    i+=1;
+    const updatedEvaluations = workflow.evaluations.map(evaluation => {
+      const existingPublicEvaluationPermission = evaluation.permissions.find(permission => permission.systemRole === 'public');
+
+      if (!existingPublicEvaluationPermission) {
+        return {
+          ...evaluation,
+          permissions: [
+            ...evaluation.permissions,
+            {
+              systemRole: 'public',
+              operation: 'view' as ProposalOperation
+            }
+          ]
+        };
+      }
+      return evaluation;
+    });
+    await prisma.proposalWorkflow.update({
+      where: {
+        id: workflow.id
+      },
+      data: {
+        evaluations: updatedEvaluations
+      }
+    });
+
+    console.log(`Updated proposal workflow ${i} / ${workflows.length}`);
+  }
+
+  const evaluationsWithoutPublicPermission = await prisma.proposalEvaluation.findMany({
+    where: {
+      proposal: {
+        spaceId: spaceId
+      },
+      permissions: {
+        none: {
+          systemRole: 'public'
+        }
+      }
+    },
+    select: {
+      id: true
+    }
+  });
+
+  const createdPermissions = await prisma.proposalEvaluationPermission.createMany({
+
+    data: evaluationsWithoutPublicPermission.map(evaluation => ({
+      evaluationId: evaluation.id,
+      systemRole: 'public',
+      operation: 'view' as ProposalOperation
+    }))
+  })
+  
+  console.log(`Updated ${createdPermissions.count} evaluations with public permissions`);
+
+
+}
+
+makeAllProposalsPublic()
