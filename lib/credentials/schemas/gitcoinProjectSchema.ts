f174c5c839a791bad4553d3842fa15cc47c42eab
motechFR <mo@anyahq.co>
2024-08-22 20:44:54 +0200

    Fix schema definition

--- lib/credentials/schemas/gitcoinProjectSchema.ts
+++ lib/credentials/schemas/gitcoinProjectSchema.ts
@@ -1,7 +1,7 @@
 export const gitcoinProjectCredentialSchemaId = '0xf8757b1e38ff1b0c1893e47f7d815367332bec28fea4b4abdcdfda3b1d1b9061';
 
 export const gitcoinProjectCredentialSchemaDefinition =
-  'string name,string metadataPtr,uint256 metadataType,bytes32 type,bytes32 round';
+  'string name,string metadataPtr,uint256 metadataType,bytes32 type,bytes32 round,string uuid';
 
 export type GitcoinProjectCredential = {
   name: string;
