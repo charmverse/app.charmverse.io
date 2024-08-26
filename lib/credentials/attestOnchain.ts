87e90a6e298482d76b24280806576a4e29ab803a
motechFR <mo@anyahq.co>
2024-08-27 01:12:45 +0200

    Revert to EAS SDK and add gasPrice argument

--- lib/credentials/attestOnchain.ts
+++ lib/credentials/attestOnchain.ts
@@ -3,13 +3,14 @@ import type { CredentialEventType } from '@charmverse/core/prisma-client';
 import { prisma } from '@charmverse/core/prisma-client';
 import { credentialsWalletPrivateKey } from '@root/config/constants';
 import { getChainById } from '@root/connectors/chains';
-import { encodeFunctionData } from 'viem';
+import { Wallet } from 'ethers';
 
 import { getCurrentGasPrice } from '../blockchain/getCurrentGasPrice';
-import { getWalletClient } from '../blockchain/walletClient';
+import { getEthersProvider } from '../blockchain/getEthersProvider';
 
-import { getEasConnector, type EasSchemaChain } from './connectors';
-import type { CredentialDataInput, ExtendedAttestationType } from './schemas';
+import type { EasSchemaChain } from './connectors';
+import { getEasInstance } from './getEasInstance';
+import type { ExtendedAttestationType, CredentialDataInput } from './schemas';
 import { attestationSchemaIds, encodeAttestation } from './schemas';
 
 export type OnChainAttestationInput<T extends ExtendedAttestationType = ExtendedAttestationType> = {
@@ -20,154 +21,40 @@ export type OnChainAttestationInput<T extends ExtendedAttestationType = Extended
 
 const NULL_ADDRESS = '0x0000000000000000000000000000000000000000';
 
-const NULL_BYTES32 = '0x0000000000000000000000000000000000000000000000000000000000000000';
-
-const easAbi = [
-  {
-    name: 'attest',
-    outputs: [
-      {
-        internalType: 'bytes32',
-        name: '',
-        type: 'bytes32'
-      }
-    ],
-    stateMutability: 'payable',
-    type: 'function',
-    inputs: [
-      {
-        components: [
-          {
-            internalType: 'bytes32',
-            name: 'schema',
-            type: 'bytes32'
-          },
-          {
-            components: [
-              {
-                internalType: 'address',
-                name: 'recipient',
-                type: 'address'
-              },
-              {
-                internalType: 'uint64',
-                name: 'expirationTime',
-                type: 'uint64'
-              },
-              {
-                internalType: 'bool',
-                name: 'revocable',
-                type: 'bool'
-              },
-              {
-                internalType: 'bytes32',
-                name: 'refUID',
-                type: 'bytes32'
-              },
-              {
-                internalType: 'bytes',
-                name: 'data',
-                type: 'bytes'
-              },
-              {
-                internalType: 'uint256',
-                name: 'value',
-                type: 'uint256'
-              }
-            ],
-            internalType: 'struct AttestationRequestData',
-            name: 'data',
-            type: 'tuple'
-          }
-        ],
-        internalType: 'struct AttestationRequest',
-        name: 'request',
-        type: 'tuple'
-      }
-    ]
-  },
-  {
-    anonymous: false,
-    inputs: [
-      {
-        indexed: true,
-        internalType: 'address',
-        name: 'recipient',
-        type: 'address'
-      },
-      {
-        indexed: true,
-        internalType: 'address',
-        name: 'attester',
-        type: 'address'
-      },
-      {
-        indexed: false,
-        internalType: 'bytes32',
-        name: 'uid',
-        type: 'bytes32'
-      },
-      {
-        indexed: true,
-        internalType: 'bytes32',
-        name: 'schema',
-        type: 'bytes32'
-      }
-    ],
-    name: 'Attested',
-    type: 'event'
-  }
-];
-
 export async function attestOnchain<T extends ExtendedAttestationType = ExtendedAttestationType>({
   credentialInputs,
   type,
   chainId
 }: OnChainAttestationInput<T>): Promise<string> {
   const schemaId = attestationSchemaIds[type];
+  const rpcUrl = getChainById(chainId)?.rpcUrls[0] as string;
 
-  const walletClient = getWalletClient({ chainId, privateKey: credentialsWalletPrivateKey as string });
+  const provider = getEthersProvider({ rpcUrl });
 
-  const gasPrice = await getCurrentGasPrice({ chainId });
+  const wallet = new Wallet(credentialsWalletPrivateKey as string, provider);
 
-  const easContract = getEasConnector(chainId).attestationContract;
+  const eas = getEasInstance(chainId);
 
-  // Prepare the attestation data according to the provided ABI
-  const attestationData = {
-    schema: schemaId,
-    data: {
-      recipient: credentialInputs.recipient ?? NULL_ADDRESS,
-      expirationTime: 0, // Use appropriate value
-      revocable: true, // Use appropriate value
-      refUID: NULL_BYTES32, // Use zero bytes32 if not provided
-      data: encodeAttestation({ type, data: credentialInputs.data }),
-      value: 0 // Use appropriate value
-    }
-  };
+  const currentGasPrice = await getCurrentGasPrice({ chainId });
 
-  const txData = encodeFunctionData({
-    abi: easAbi, // The provided ABI
-    functionName: 'attest',
-    args: [attestationData]
-  });
+  eas.connect(wallet);
 
-  // Send the transaction using viem
-  const txHash = await walletClient.sendTransaction({
-    to: easContract, // Your EAS contract address
-    data: txData,
-    gasPrice, // Set gas price using the fetched value
-    value: BigInt(0) // Set value using the appropriate value
-  });
+  const attestationUid = await eas
+    .attest(
+      {
+        schema: schemaId,
+        data: {
+          recipient: credentialInputs.recipient ?? NULL_ADDRESS,
+          data: encodeAttestation({ type, data: credentialInputs.data })
+        }
+      },
+      { gasPrice: currentGasPrice }
+    )
+    .then((tx) => tx.wait());
 
-  // Wait for the transaction receipt
-  const receipt = await walletClient.waitForTransactionReceipt({ hash: txHash as `0x${string}` });
+  log.info(`Issued ${type} credential on chain ${chainId}`);
 
-  // Decode the event logs to get the attestationUid
-  const attestationUid = receipt.logs[0].data;
-
-  log.info(`Issued ${type} credential on chain ${chainId} with attestationUid: ${attestationUid}`);
-
-  return attestationUid as any;
+  return attestationUid;
 }
 
 export type OnChainAttestationInputWithMetadata<T extends ExtendedAttestationType = ExtendedAttestationType> = {
