f29db4dc8ff1ab36032af6e84809f2f19c986026
Matt Casey <mattwad@gmail.com>
2024-08-26 12:43:08 -0500

    test a faster way to generate cache keys for CI (#4500)
    * test a faster way to generate cache keys for CI
    
    * .
    
    * trigger build in farcaster
    
    * get all git history

--- apps/farcaster/lib/postCreateCastMessage.ts
+++ apps/farcaster/lib/postCreateCastMessage.ts
@@ -1,3 +1,4 @@
+// send a message to the parent window to create a new cast
 export function postCreateCastMessage({ embeds, text }: { text: string; embeds: string[] }) {
   window.parent.postMessage(
     {
