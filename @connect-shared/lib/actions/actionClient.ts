1e66f75c5b175de5964bae4c5045236d58797e71
Valentin L <ludu.valentin@gmail.com>
2024-08-26 16:29:25 +0300

    Revert actionClient

--- @connect-shared/lib/actions/actionClient.ts
+++ @connect-shared/lib/actions/actionClient.ts
@@ -1,5 +1,4 @@
 import { UnauthorisedActionError } from '@charmverse/core/errors';
-import { log } from '@charmverse/core/log';
 import { prisma } from '@charmverse/core/prisma-client';
 import { headers } from 'next/headers';
 import { createSafeActionClient } from 'next-safe-action';
@@ -38,21 +37,15 @@ export const actionClient = createSafeActionClient({
 
 export const authActionClient = actionClient.use(async ({ next, ctx }) => {
   const user = ctx.session.user;
-  const userId = user?.id;
 
-  if (!user) {
+  if (!user?.id) {
     throw new UnauthorisedActionError('You are not logged in. Please try to login');
   }
 
-  const data = await prisma.user.findUnique({
-    where: { id: userId },
+  await prisma.user.findUniqueOrThrow({
+    where: { id: user.id },
     select: { id: true }
   });
-
-  if (!data) {
-    ctx.session.destroy();
-    log.warn('User has a session that is not found in the db', { userId });
-  }
 
   return next({
     ctx: { ...ctx, session: { ...ctx.session, user } }
