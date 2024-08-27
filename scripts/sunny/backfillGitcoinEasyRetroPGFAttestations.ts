7a1ab591d4fe39a29e9587b037422052a7cc5b9a
motechFR <mo@anyahq.co>
2024-08-27 02:15:09 +0200

    Add script for backfilling gitcoin attestations

---
+++ scripts/sunny/backfillGitcoinEasyRetroPGFAttestations.ts
@@ -0,0 +1,43 @@
+import { prisma } from '@charmverse/core/prisma-client';
+import { projectAttestationChainId } from '@connect-shared/lib/attestations/constants';
+import { storeProjectMetadataAndPublishGitcoinAttestation } from '@connect-shared/lib/attestations/storeProjectMetadataAndPublishToGitcoin';
+import { optimism } from 'viem/chains';
+
+/**
+ * Use this script to perform database searches.
+ */
+
+async function backfillGitcoinEasyRetroPGFAttestations() {
+
+  if (projectAttestationChainId !== optimism.id) {
+    throw new Error('Attestations must be backfilled on OP mainnet');
+  }
+
+  const projects = await prisma.project.findMany({
+    where: {
+      source: 'sunny_awards',
+      gitcoinProjectAttestations: {
+        none: {}
+      }
+    },
+    select: {
+      id: true,
+      createdBy: true,
+    }
+  });
+
+  const totalProjects = projects.length;
+
+  for (let i = 0; i < projects.length; i++) {
+    const project = projects[i];
+    await storeProjectMetadataAndPublishGitcoinAttestation({
+      userId: project.createdBy,
+      projectIdOrPath: project.id
+    });
+
+    console.log(`Project attestation stored for project ${i} / ${totalProjects}:`, project.id);
+  }
+
+}
+
+backfillGitcoinEasyRetroPGFAttestations();
