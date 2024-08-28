7ac0f46973893bef5dfc5d619b6e4ddb68f160b3
mattcasey <mattwad@gmail.com>
2024-08-28 17:45:05 -0600

    fix types

--- packages/charmeditor/src/plugins.ts
+++ packages/charmeditor/src/plugins.ts
@@ -1,10 +1,11 @@
 import { react } from '@nytimes/react-prosemirror';
 import type { Schema } from 'prosemirror-model';
+import type { Plugin } from 'prosemirror-state';
 
 import { buildPlugins } from './buildPlugins';
 import { plugins as listItemPlugins } from './plugins/listItem/listItemPlugins';
 
-export const plugins = (schema: Schema) => [
+export const plugins = (schema: Schema): Plugin[] => [
   // You must add the react plugin if you use
   // the useNodeViews or useNodePos hook.
   react(),
