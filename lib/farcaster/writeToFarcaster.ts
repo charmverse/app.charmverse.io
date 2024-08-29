037d66952f18129bb5cb93653a130ec592f2a4d7
motechFR <mo@anyahq.co>
2024-08-29 20:22:50 +0200

    Add initial files

---
+++ lib/farcaster/writeToFarcaster.ts
@@ -0,0 +1,21 @@
+// docs.neynar.com/reference/publish-message
+
+import { POST } from '@root/adapters/http';
+
+import { NEYNAR_API_BASE_URL } from './constants';
+
+// TBD - https://github.com/neynarxyz/farcaster-examples/tree/main/managed-signers
+
+// https://docs.neynar.com/reference/post-cast
+// https://github.com/neynarxyz/farcaster-examples/tree/main/gm-bot
+
+// export function writeToFarcaster({message}: {message: string}) {
+
+//   const baseUrl = `${NEYNAR_API_BASE_URL}/message`
+
+//   return (
+//     <div>
+//       <h1>writeToFarcaster</h1>
+//     </div>
+//   )
+// }
