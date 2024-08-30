2a983f076422af3060a022ffedadeee7b68578b2
motechFR <mo@anyahq.co>
2024-08-31 00:32:47 +0300

    Remove last traces of waitlist in farcaster app

--- apps/farcaster/app/waitlist/[fid]/page.tsx
+++
@@ -1,7 +0,0 @@
-import { JoinWaitlistHome } from 'components/waitlist/joinWaitlist/JoinWaitlistHome';
-
-export default async function WaitlistPage({ params }: { params: { fid: string } }) {
-  // const src = 'https://www.gettyimages.fr/gi-resources/images/Embed/new/embed1.jpg';
-
-  return <JoinWaitlistHome referrerFid={params.fid} />;
-}
