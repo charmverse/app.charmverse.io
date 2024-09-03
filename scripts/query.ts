24ee9272bf53b53c3675a8f438c1ee9eaaa81c3d
mattcasey <mattwad@gmail.com>
2024-09-03 15:38:42 -0600

    update github user script

--- scripts/query.ts
+++ scripts/query.ts
@@ -5,24 +5,93 @@ import { uniqBy, uniq } from 'lodash';
  * Use this script to perform database searches.
  */
 import { readEcosystems } from './github/query';
+import { getGithubUsers } from './github/getGithubUsers';
+
+const bots = [
+  'github-actions',
+  'release-please-for-lemonade',
+  'mend-bolt-for-github',
+  'dependabot[bot]',
+  'nero-alpha',
+  'app-token-issuer-functions',
+  'allcontributors',
+  'novasama-bot',
+  'nips-ja-sync',
+  'layerone-renovate',
+  'depfu',
+  'duwenjieG',
+  'hercules-ci',
+  'core-repository-dispatch-app',
+  'pr-action',
+  'moonpay-github-security',
+  'renovate',
+  'app-token-issuer-infra-releng',
+  'app-token-issuer-releng-renovate',
+  'ellipsis-dev',
+  'term-finance-publisher',
+  'transifex-integration',
+  'aaronyuai',
+  'api3-ecosystem-pr-bot',
+  'sweep-ai',
+  'stack-file',
+  'devin-ai-integration',
+  'cybersecurity-github-actions-ci',
+  'pre-commit-ci',
+  'runway-github',
+  'akeyless-target-app',
+  'finschia-auto-pr',
+  'bitgo-renovate-bot',
+  'sui-merge-bot',
+  'stainless-app',
+  'ipfs-shipyard-mgmt-read-write',
+  'azure-pipelines',
+  'penify-dev',
+  'term-finance-publisher',
+  'live-github-bot',
+  'paritytech-subxt-pr-maker',
+  'smartdeploy-deployer',
+  'dependabot-preview',
+  'petr-hanzl',
+  'paritytech-polkadotsdk-templatebot',
+  'snyk-io',
+  'galoybot-app',
+  'figure-renovate',
+  'corda-jenkins-ci02',
+  'dependabot',
+  'ipfs-mgmt-read-write',
+  'codefactor-io',
+  'libp2p-mgmt-read-write',
+  'graphops-renovate',
+  'filplus-github-bot-read-write',
+  'imgbot',
+  'tokenlistform',
+  'pyca-boringbot'
+];
 
 const cutoffDate = new Date('2024-01-01');
 async function query() {
-  const allEcos = await prisma.cryptoEcosystem.findMany({
-    include: {
-      repos: true
-    }
-  });
-
-  const reposFromEcos = allEcos.flatMap((e) => e.repos);
-  const allRepos = await prisma.cryptoEcosystemRepo.findMany({});
-  console.log('allRepos', allRepos.length);
-  console.log('reposFromEcos', reposFromEcos.length);
-  const urlMap = allRepos.reduce<Record<string, boolean>>((acc, r) => {
-    acc[r.url] = true;
-    return acc;
-  }, {});
-  console.log('no match by url', reposFromEcos.filter((r) => !urlMap[r.url]).length);
+  const dbUsers = await prisma.cryptoEcosystemAuthor.findMany();
+  console.log(dbUsers.length);
+  const logins = dbUsers.map((user) => user.login).filter((login) => !bots.includes(login));
+  const users = await getGithubUsers({ logins });
+  console.log('retrieved users', users.length);
+  for (let user of users) {
+    await prisma.cryptoEcosystemAuthor.update({
+      where: {
+        login: user.login
+      },
+      data: {
+        email: user.email,
+        xtra: {
+          isHireable: user.isHireable,
+          location: user.location
+        },
+        name: user.name,
+        twitter: user.twitter
+      }
+    });
+  }
+  console.log('DONE');
   // const result2 = await prisma.cryptoEcosystemAuthor.count({});
   // console.log(result2);
 }
