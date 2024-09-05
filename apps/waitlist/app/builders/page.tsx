effe3f5d4d1a0667c3c66fea3c409a810799f75a
motechFR <mo@anyahq.co>
2024-09-05 16:41:43 +0300

    Clean up

--- apps/waitlist/app/builders/page.tsx
+++ apps/waitlist/app/builders/page.tsx
@@ -1,6 +1,4 @@
-import { Button, Typography } from '@mui/material';
-import { getGithubAppCallbackUrl } from '@root/lib/github/oauth';
-import dynamic from 'next/dynamic';
+import { Typography } from '@mui/material';
 import { Suspense } from 'react';
 
 import { JoinGithubButton } from 'components/github/JoinGithubButton';
