75f2530f1bc52f1d87311a1ea4aaeaf2b6daa212
Matt Casey <mattwad@gmail.com>
2024-09-06 09:53:50 -0500

    Setup script for downloading data (#4531)
    * Setup script for downloading data
    
    * move files around
    
    * add more scripts
    
    * update github user script
    
    * updates
    
    * update cutoff
    
    * bump the limit
    
    * update script
    
    * .
    
    * update lock
    
    * .
    
    ---------
    
    Co-authored-by: motechFR <mo@anyahq.co>

--- scripts/query.ts
+++ scripts/query.ts
@@ -1,16 +1,8 @@
 import { prisma } from '@charmverse/core/prisma-client';
 import { prettyPrint } from 'lib/utils/strings';
 
-/**
- * Use this script to perform database searches.
- */
-
 async function query() {
-  const result = await prisma.connectWaitlistSlot.findUniqueOrThrow({
-    where: {
-      fid: 4339
-    }
-  });
+  const result = await prisma.scout.findMany();
   console.log(result);
 }
 
