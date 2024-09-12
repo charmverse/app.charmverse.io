1c4aa11b06972a847d39c95a2ab65b236eb1168d
mattcasey <mattwad@gmail.com>
2024-09-12 14:49:33 -0600

    fix link color in disabled inputs

--- theme/index.ts
+++ theme/index.ts
@@ -433,7 +433,9 @@ export const createThemeLightSensitive = (mode: PaletteMode) => {
         styleOverrides: {
           root: {
             color: darkBlueColor,
+            '-webkit-text-fill-color': darkBlueColor, // override disabled color of text inputs
             '&:hover': {
+              '-webkit-text-fill-color': darken(darkBlueColor, 0.2), // override disabled color of text inputs
               color: darken(darkBlueColor, 0.2)
             }
           }
