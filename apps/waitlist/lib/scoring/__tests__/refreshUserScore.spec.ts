38416754ba0de50d22257407077e2feced1000f3
motechFR <mo@anyahq.co>
2024-09-12 18:22:47 +0300

    Account for github when calculating score

--- apps/waitlist/lib/scoring/__tests__/refreshUserScore.spec.ts
+++ apps/waitlist/lib/scoring/__tests__/refreshUserScore.spec.ts
@@ -44,4 +44,46 @@ describe('refreshUserScore', () => {
 
     expect(updatedWaitlistSlot?.score).toBe(formulaOutput);
   });
+
+  it('should count 10 extra clicks if the user has a github account connected', async () => {
+    const targetFid = randomFid();
+
+    const initialPosition = 10;
+
+    const waitlistSlot = await prisma.connectWaitlistSlot.create({
+      data: {
+        fid: targetFid,
+        username: 'test',
+        referredByFid: null,
+        score: 0,
+        initialPosition,
+        githubLogin: 'xyz'
+      }
+    });
+
+    const totalReferrals = 5;
+
+    const referrals = await prisma.connectWaitlistSlot.createMany({
+      data: Array.from({ length: totalReferrals }, (_, index) => ({
+        fid: randomFid(),
+        username: `test${index}`,
+        referredByFid: targetFid,
+        score: 0,
+        initialPosition: 0
+      }))
+    });
+
+    // Core formula we are testing
+    const formulaOutput = initialPosition - (totalReferrals + 10) * 100;
+
+    await refreshUserScore({ fid: targetFid });
+
+    const updatedWaitlistSlot = await prisma.connectWaitlistSlot.findUnique({
+      where: {
+        fid: targetFid
+      }
+    });
+
+    expect(updatedWaitlistSlot?.score).toBe(formulaOutput);
+  });
 });
