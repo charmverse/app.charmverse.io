d5b8f39137cb3a9fae3703aaf709915c83196aa7
mattcasey <mattwad@gmail.com>
2024-09-18 15:33:39 -0600

    only users from the waitlist can sign in

---
+++ apps/scoutgame/lib/session/requireWaitlistUser.ts
@@ -0,0 +1,20 @@
+import { UnauthorisedActionError } from '@charmverse/core/errors';
+import { log } from '@charmverse/core/log';
+import { prisma } from '@charmverse/core/prisma-client';
+import { isDevEnv, isStagingEnv } from '@root/config/constants';
+
+// check htat user signed up to the waitlist
+export async function requireWaitlistUser({ fid }: { fid: number }) {
+  if (isDevEnv || isStagingEnv) {
+    log.debug('Skip whitelist check in dev/staging');
+    return;
+  }
+  const waitlistRecord = await prisma.connectWaitlistSlot.count({
+    where: {
+      fid
+    }
+  });
+  if (waitlistRecord === 0) {
+    throw new UnauthorisedActionError('Scout Game is in private beta');
+  }
+}
