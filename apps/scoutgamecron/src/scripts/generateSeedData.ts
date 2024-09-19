77a7e4a48f3aeb9553b7ce473afb940cf26c6f4a
Safwan Shaheer <devorein00@gmail.com>
2024-09-20 03:14:13 +0600

    Refactored seeder code

--- apps/scoutgamecron/src/scripts/generateSeedData.ts
+++ apps/scoutgamecron/src/scripts/generateSeedData.ts
@@ -7,7 +7,8 @@ import { processClosedPullRequest } from '../tasks/processPullRequests/processCl
 import { processMergedPullRequest } from '../tasks/processPullRequests/processMergedPullRequest';
 import { v4 } from 'uuid';
 
-async function generateScout({ isBuilder }: { isBuilder: boolean }) {
+async function generateScout(params: { isBuilder: boolean } = { isBuilder: false }) {
+  const { isBuilder } = params;
   const displayName = faker.name.fullName();
   const username = faker.internet.userName();
   const email = faker.internet.email();
@@ -56,17 +57,9 @@ async function generateBuilder() {
   };
 }
 
-export async function generateSeedData() {
-  // 20 - 50 percent builders out of all the users
-  const builderPercentage = faker.number.int({ min: 20, max: 50 });
-  const totalUsers = faker.number.int({ min: 100, max: 250 });
-  const totalBuilders = Math.floor(totalUsers * builderPercentage / 100);
-
-  // 100 - 250 github repos
-  const totalGithubRepos = faker.number.int({ min: 100, max: 250 });
-
+async function generateGithubRepos(totalGithubRepos: number): Promise<[GithubRepo[], Map<number, number>]> {
   const githubRepos: GithubRepo[] = [];
-  const repoPRCounters = new Map<number, number>(); // Map to track PR numbers for each repo
+  const repoPRCounters = new Map<number, number>();
 
   for (let i = 0; i < totalGithubRepos; i++) {
     const githubRepo = await prisma.githubRepo.create({
@@ -78,62 +71,90 @@ export async function generateSeedData() {
       }
     });
     githubRepos.push(githubRepo);
-    repoPRCounters.set(githubRepo.id, 0); // Initialize PR counter for this repo
+    repoPRCounters.set(githubRepo.id, 0);
   }
+
+  return [githubRepos, repoPRCounters];
+}
+
+function generatePullRequest(
+  githubRepo: GithubRepo,
+  githubUser: GithubUser,
+  pullRequestNumber: number,
+  day: number,
+  closedPullRequestChance: number
+): PullRequest {
+  const nameWithOwner = `${githubRepo.owner}/${githubRepo.name}`;
+  return {
+    baseRefName: 'main',
+    author: {
+      id: githubUser.id,
+      login: githubUser.login,
+    },
+    title: faker.lorem.sentence(),
+    url: `https://github.com/${nameWithOwner}/pull/${pullRequestNumber}`,
+    createdAt: DateTime.fromJSDate(new Date(new Date().setDate(day - 1)), {zone: timezone}).toISO(),
+    mergedAt: DateTime.fromJSDate(new Date(new Date().setDate(day)), {zone: timezone}).toISO(),
+    number: pullRequestNumber,
+    repository: {
+      id: githubRepo.id,
+      nameWithOwner
+    },
+    state: faker.number.int({min: 1, max: 100}) <= closedPullRequestChance ? 'CLOSED' : 'MERGED'
+  };
+}
+
+async function processPullRequest(pullRequest: PullRequest, githubRepo: GithubRepo) {
+  if (pullRequest.state === 'CLOSED') {
+    await processClosedPullRequest({
+      pullRequest,
+      repo: githubRepo,
+      prClosedBy: v4()
+    });
+  } else if (pullRequest.state === 'MERGED') {
+    await processMergedPullRequest({
+      pullRequest,
+      repo: githubRepo,
+      isFirstMergedPullRequest: false,
+    });
+  }
+}
+
+async function generateBuilderEvents(
+  githubUser: GithubUser,
+  githubRepos: GithubRepo[],
+  repoPRCounters: Map<number, number>
+) {
+  const closedPullRequestChance = faker.number.int({ min: 10, max: 20 });
+  const weekDay = DateTime.fromJSDate(new Date(), {zone: timezone}).weekday % 7;
+
+  for (let day = 0; day <= weekDay; day++) {
+    const dailyGithubEvents = faker.number.int({ min: 3, max: 5 });
+    for (let j = 0; j < dailyGithubEvents; j++) {
+      const githubRepo = faker.helpers.arrayElement(githubRepos);
+      const pullRequestNumber = repoPRCounters.get(githubRepo.id)! + 1;
+      repoPRCounters.set(githubRepo.id, pullRequestNumber);
+
+      const pullRequest = generatePullRequest(githubRepo, githubUser, pullRequestNumber, day, closedPullRequestChance);
+      await processPullRequest(pullRequest, githubRepo);
+    }
+  }
+}
+
+export async function generateSeedData() {
+  const builderPercentage = faker.number.int({ min: 20, max: 50 });
+  const totalUsers = faker.number.int({ min: 100, max: 250 });
+  const totalBuilders = Math.floor(totalUsers * builderPercentage / 100);
+  const totalGithubRepos = faker.number.int({ min: 100, max: 250 });
+
+  const [githubRepos, repoPRCounters] = await generateGithubRepos(totalGithubRepos);
 
   for (let i = 0; i < totalBuilders; i++) {
     const {githubUser} = await generateBuilder();
-    // This builder has a 10 - 20 percent chance of having a closed pull request
-    const closedPullRequestChance = faker.number.int({ min: 10, max: 20 });
-    // generate events for the week
-    const weekDay = DateTime.fromJSDate(new Date(), {zone: timezone}).weekday % 7;
-    for (let day = 0; day <= weekDay; day++) {
-      const dailyGithubEvents = faker.number.int({ min: 3, max: 5 });
-      for (let j = 0; j < dailyGithubEvents; j++) {
-        const githubRepo = faker.helpers.arrayElement(githubRepos);
-        const pullRequestNumber = repoPRCounters.get(githubRepo.id) as number + 1; // Get next PR number for this repo
-        repoPRCounters.set(githubRepo.id, pullRequestNumber); // Update the counter
-        const nameWithOwner = `${githubRepo.owner}/${githubRepo.name}`;
-        const pullRequest: PullRequest = {
-          baseRefName: 'main',
-          author: {
-            id: githubUser.id,
-            login: githubUser.login,
-          },
-          title: faker.lorem.sentence(),
-          url: `https://github.com/${nameWithOwner}/pull/${pullRequestNumber}`,
-          // Pull request created one day before the current day
-          createdAt: DateTime.fromJSDate(new Date(new Date().setDate(day - 1)), {zone: timezone}).toISO(),
-          // Pull request merged on the current day
-          mergedAt: DateTime.fromJSDate(new Date(new Date().setDate(day)), {zone: timezone}).toISO(),
-          number: pullRequestNumber,
-          repository: {
-            id: githubRepo.id,
-            nameWithOwner
-          },
-          state: faker.number.int({min: 1, max: 100}) <= closedPullRequestChance ? 'CLOSED' : 'MERGED'
-        }
-
-        if (pullRequest.state === 'CLOSED') {
-          await processClosedPullRequest({
-            pullRequest,
-            repo: githubRepo,
-            // Use a random github user login to avoid triggering the check to see if the author closed the PR
-            prClosedBy: v4()
-          })
-        } else if (pullRequest.state === 'MERGED') {
-          await processMergedPullRequest({
-            pullRequest,
-            repo: githubRepo,
-            // Manually mark this pull request as not the first merged pull request to avoid calling the Github API
-            isFirstMergedPullRequest: false,
-          })
-        }
-      }
-    }
+    await generateBuilderEvents(githubUser, githubRepos, repoPRCounters);
   }
 
   for (let i = 0; i < totalUsers - totalBuilders; i++) {
-    await generateScout({ isBuilder: false });
+    await generateScout();
   }
 }
