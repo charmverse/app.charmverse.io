cb8eb9cf1d14bbe3543dfa5f4af598c3b5787d29
mattcasey <mattwad@gmail.com>
2024-09-20 08:05:30 -0600

    fix ts

--- apps/scoutgame/components/layout/NotFoundPage.tsx
+++ apps/scoutgame/components/layout/NotFoundPage.tsx
@@ -2,10 +2,8 @@ import Box from '@mui/material/Box';
 import CssBaseline from '@mui/material/CssBaseline';
 import Typography from '@mui/material/Typography';
 import Image from 'next/image';
-import type { ReactNode } from 'react';
 
 const imageSrc = '/images/space_binoculars.webp';
-
 export function NotFoundPage({ content = 'The page you are looking for does not exist!' }: { content?: string }) {
   return (
     <>
@@ -18,10 +16,12 @@ export function NotFoundPage({ content = 'The page you are looking for does not 
         justifyContent='center'
         overflow='hidden'
         flexDirection='column'
-        backgroundColor='#2b2645'
+        sx={{
+          backgroundColor: '#2b2645'
+        }}
         gap={5}
       >
-        <Image src={imageSrc} sx={{ height: '100%', width: '100%' }} width={300} height={300} alt='' />
+        <Image src={imageSrc} width={300} height={300} alt='' />
         <Typography variant='subtitle1'>{content}</Typography>
       </Box>
     </>
