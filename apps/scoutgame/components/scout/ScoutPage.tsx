5e116109c0075f06f1c5db490e6e56097b0c4376
Safwan Shaheer <devorein00@gmail.com>
2024-09-25 23:22:42 +0600

    ScoutPage builders gallery async loading

--- apps/scoutgame/components/scout/ScoutPage.tsx
+++ apps/scoutgame/components/scout/ScoutPage.tsx
@@ -1,22 +1,18 @@
-import 'server-only';
-
 import { Box } from '@mui/material';
 
-import { BuildersGallery } from 'components/common/Gallery/BuildersGallery';
-import type { BuildersSort } from 'lib/builders/getSortedBuilders';
-import { getSortedBuilders } from 'lib/builders/getSortedBuilders';
-
+import { ScoutPageBuildersGallery } from './components/ScoutPageBuildersGallery';
 import { SearchBuildersInput } from './components/SearchBuildersInput';
 import { SortOptionTabs, sortOptions } from './components/SortOptionTabs';
 
-export async function ScoutPage({ sort, user }: { sort: string; user?: { username: string } | null }) {
+export function ScoutPage({ sort, user }: { sort: string; user?: { username: string } | null }) {
   const currentSort = sortOptions.some((t) => t.value === sort) ? sort : 'top';
-  const builders = await getSortedBuilders({ sort: currentSort as BuildersSort, limit: 10 });
   return (
     <Box p={1}>
       <SearchBuildersInput />
       <SortOptionTabs value={currentSort} />
-      <BuildersGallery builders={builders} user={user} />
+      <Box px={10}>
+        <ScoutPageBuildersGallery sort={currentSort} user={user} />
+      </Box>
     </Box>
   );
 }
