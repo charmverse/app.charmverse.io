69336d1b4f662184aefcbf29366bd7ec48d2beb1
mattcasey <mattwad@gmail.com>
2024-09-25 13:13:28 -0600

    ..

--- apps/scoutgame/__e2e__/test.ts
+++ apps/scoutgame/__e2e__/test.ts
@@ -15,6 +15,27 @@ type Fixtures = {
 };
 
 export const test = base.extend<Fixtures>({
+  page: async ({ page }, use) => {
+    // Set up routing for all requests
+    await page.route('**/*', async (route) => {
+      const url = route.request().url();
+      if (url.startsWith('https://cdn.charmverse.io/')) {
+        const newUrl = url.replace('https://cdn.charmverse.io/', 'http://localhost:3337/');
+        // console.log(`Redirecting ${url} to ${newUrl}`);
+        await route.fulfill({
+          status: 301,
+          headers: {
+            Location: newUrl
+          }
+        });
+      } else {
+        await route.continue();
+      }
+    });
+
+    // Use the page with the custom routing
+    await use(page);
+  },
   homePage: ({ page }, use) => use(new HomePage(page)),
   loginPage: ({ page }, use) => use(new LoginPage(page)),
   profilePage: ({ page }, use) => use(new ProfilePage(page)),
