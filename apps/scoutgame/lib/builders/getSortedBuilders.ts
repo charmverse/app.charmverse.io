6e7248c06dc373289db284d295a8961418a09311
Matt Casey <mattwad@gmail.com>
2024-09-26 12:22:17 -0600

    revert changes to scouts page query (#4651)

--- apps/scoutgame/lib/builders/getSortedBuilders.ts
+++ apps/scoutgame/lib/builders/getSortedBuilders.ts
@@ -1,8 +1,6 @@
 import { prisma } from '@charmverse/core/prisma-client';
-import { currentSeason } from '@packages/scoutgame/builderNfts/constants';
-import { getLastWeek } from '@packages/scoutgame/utils';
+import { currentSeason, getCurrentWeek, getLastWeek } from '@packages/scoutgame/utils';
 
-import { getBuilders } from './getBuilders';
 import type { BuilderInfo } from './interfaces';
 
 export type BuildersSort = 'top' | 'hot' | 'new';
@@ -17,70 +15,182 @@ export async function getSortedBuilders({
   // new is based on the most recent builder
   // top is based on the most gems earned in their user week stats
   // hot is based on the most points earned in the previous user week stats
+  let builders: BuilderInfo[];
 
   switch (sort) {
     case 'new':
-      return getBuilders({
-        orderBy: {
-          createdAt: 'desc'
-        },
-        limit,
-        where: {
-          season: currentSeason,
-          builder: {
-            bannedAt: null
-          }
-        }
-      });
-
-    case 'top':
-      // eslint-disable-next-line no-case-declarations
-      const topUsers = await prisma.userWeeklyStats.findMany({
-        orderBy: {
-          gemsCollected: 'desc'
-        },
-        take: limit
-      });
-
-      return getBuilders({
-        limit,
-        where: {
-          builderId: {
-            in: topUsers.map((userStats) => userStats.userId)
-          }
-        },
-        orderBy: {
-          builder: {
-            gemsPayoutEvents: {}
-          }
-        }
-      });
-
-    case 'hot': {
-      const previousWeek = getLastWeek();
-
-      return prisma.userWeeklyStats
+      builders = await prisma.scout
         .findMany({
           where: {
-            week: previousWeek
+            bannedAt: null,
+            builder: true
           },
           orderBy: {
-            // TODO - Use points instead
-            gemsCollected: 'desc'
+            createdAt: 'desc'
+          },
+          take: limit,
+          select: {
+            id: true,
+            username: true,
+            avatar: true,
+            createdAt: true,
+            builderNfts: {
+              where: {
+                season: currentSeason
+              },
+              select: {
+                currentPrice: true,
+                nftSoldEvents: {
+                  distinct: 'scoutId'
+                }
+              }
+            },
+            userWeeklyStats: {
+              where: {
+                week: getCurrentWeek()
+              },
+              select: {
+                gemsCollected: true
+              }
+            },
+            userAllTimeStats: {
+              select: {
+                pointsEarnedAsBuilder: true
+              }
+            }
+          }
+        })
+        .then((scouts) =>
+          scouts.map((scout) => ({
+            id: scout.id,
+            avatar: scout.avatar,
+            username: scout.username,
+            displayName: scout.username,
+            builderPoints: scout.userAllTimeStats[0]?.pointsEarnedAsBuilder ?? 0,
+            price: scout.builderNfts?.[0]?.currentPrice ?? 0,
+            scoutedBy: scout.builderNfts?.[0]?.nftSoldEvents?.length ?? 0,
+            gems: scout.userWeeklyStats[0]?.gemsCollected ?? 0,
+            builder: true
+          }))
+        );
+      break;
+
+    case 'top':
+      builders = await prisma.userWeeklyStats
+        .findMany({
+          where: {
+            user: {
+              bannedAt: null,
+              builder: true
+            },
+            week: getCurrentWeek()
+          },
+          orderBy: {
+            rank: 'asc'
           },
           take: limit,
           select: {
             user: {
               select: {
-                id: true
+                id: true,
+                username: true,
+                avatar: true,
+                builderNfts: {
+                  where: {
+                    season: currentSeason
+                  },
+                  select: {
+                    currentPrice: true,
+                    nftSoldEvents: {
+                      distinct: 'scoutId'
+                    }
+                  }
+                },
+                userAllTimeStats: {
+                  select: {
+                    pointsEarnedAsBuilder: true
+                  }
+                }
               }
-            }
+            },
+            gemsCollected: true
           }
         })
-        .then((stats) => getBuilders({ limit, where: { builderId: { in: stats.map((s) => s.user.id) } } }));
+        .then((stats) =>
+          stats.map((stat) => ({
+            id: stat.user.id,
+            avatar: stat.user.avatar,
+            username: stat.user.username,
+            displayName: stat.user.username,
+            builderPoints: stat.user.userAllTimeStats[0]?.pointsEarnedAsBuilder ?? 0,
+            price: stat.user.builderNfts?.[0]?.currentPrice ?? 0,
+            scoutedBy: stat.user.builderNfts?.[0]?.nftSoldEvents?.length ?? 0,
+            gems: stat.gemsCollected,
+            builder: true
+          }))
+        );
+      break;
+
+    case 'hot': {
+      const previousWeek = getLastWeek();
+
+      builders = await prisma.userWeeklyStats
+        .findMany({
+          where: {
+            week: previousWeek,
+            user: {
+              bannedAt: null,
+              builder: true
+            }
+          },
+          orderBy: [{ week: 'desc' }, { rank: 'asc' }],
+          take: limit,
+          select: {
+            user: {
+              select: {
+                id: true,
+                username: true,
+                avatar: true,
+                userAllTimeStats: {
+                  select: {
+                    pointsEarnedAsBuilder: true
+                  }
+                },
+                builderNfts: {
+                  where: {
+                    season: currentSeason
+                  },
+                  select: {
+                    currentPrice: true,
+                    nftSoldEvents: {
+                      distinct: 'scoutId'
+                    }
+                  }
+                }
+              }
+            },
+            gemsCollected: true
+          }
+        })
+        .then((stats) =>
+          stats.map((stat) => ({
+            id: stat.user.id,
+            avatar: stat.user.avatar,
+            username: stat.user.username,
+            displayName: stat.user.username,
+            builderPoints: stat.user.userAllTimeStats[0]?.pointsEarnedAsBuilder ?? 0,
+            price: stat.user.builderNfts?.[0]?.currentPrice ?? 0,
+            scoutedBy: stat.user.builderNfts?.[0]?.nftSoldEvents?.length ?? 0,
+            gems: stat.gemsCollected,
+            builder: true
+          }))
+        );
+      break;
     }
 
     default:
       throw new Error(`Invalid sort option: ${sort}`);
   }
+
+  return builders;
 }
