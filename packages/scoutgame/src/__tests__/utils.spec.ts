84de2f5f6860ea3871237971a83b8617ef68c80c
mattcasey <mattwad@gmail.com>
2024-09-26 14:06:56 -0600

    use isoweek for builder events

--- packages/scoutgame/src/__tests__/utils.spec.ts
+++ packages/scoutgame/src/__tests__/utils.spec.ts
@@ -1,28 +1,28 @@
 import { DateTime } from 'luxon';
 
-import { getWeek, getFormattedWeek, getWeekStartEnd, getSeasonWeekNumberFromWeek, timezone } from '../utils';
+import { getFormattedWeek, getWeekStartEnd, getSeasonWeekNumberFromWeek } from '../utils';
 
-describe('getWeek should return the correct week', () => {
-  it('when the first day of the year is a Sunday', () => {
-    const dec31 = new Date('2023-01-01T07:00:00.000Z'); // Dec 31, 2023 is a Sunday in NY
-    expect(getWeek(dec31)).toEqual({ week: 1, year: 2023 });
-  });
+// describe('getWeek should return the correct week', () => {
+//   it('when the first day of the year is a Sunday', () => {
+//     const dec31 = new Date('2023-01-01T07:00:00.000Z'); // Dec 31, 2023 is a Sunday in NY
+//     expect(getWeek(dec31)).toEqual({ week: 1, year: 2023 });
+//   });
 
-  it('when the last day of the year is a Sunday in a leap year with 53 Sundays', () => {
-    const dec31 = new Date('2023-12-31T07:00:00.000Z'); // Dec 31, 2023 is a Sunday in NY
-    expect(getWeek(dec31)).toEqual({ week: 53, year: 2023 });
-  });
+//   it('when the last day of the year is a Sunday in a leap year with 53 Sundays', () => {
+//     const dec31 = new Date('2023-12-31T07:00:00.000Z'); // Dec 31, 2023 is a Sunday in NY
+//     expect(getWeek(dec31)).toEqual({ week: 53, year: 2023 });
+//   });
 
-  it('when the first day of the year if it falls in the middle of a week', () => {
-    const jan01 = new Date('2024-01-01T07:00:00.000Z');
-    expect(getWeek(jan01)).toEqual({ week: 52, year: 2023 });
-  });
+//   it('when the first day of the year if it falls in the middle of a week', () => {
+//     const jan01 = new Date('2024-01-01T07:00:00.000Z');
+//     expect(getWeek(jan01)).toEqual({ week: 52, year: 2023 });
+//   });
 
-  it('when the first day of the year starts on the last week of the previous year', () => {
-    const date = new Date('2025-01-01T07:00:00.000Z');
-    expect(getWeek(date)).toEqual({ year: 2024, week: 52 });
-  });
-});
+//   it('when the first day of the year starts on the last week of the previous year', () => {
+//     const date = new Date('2025-01-01T07:00:00.000Z');
+//     expect(getWeek(date)).toEqual({ year: 2024, week: 52 });
+//   });
+// });
 
 describe('getFormattedWeek should return a correctly formatted week', () => {
   it('when the first day of the year is a Sunday', () => {
@@ -46,9 +46,9 @@ describe('getWeekStartEnd', () => {
 });
 
 describe('getSeasonWeekNumberFromWeek', () => {
-  it('should return the season week number from the week when the season is ahead of the current week', () => {
+  it('should return the season week number when the season is ahead of the current week', () => {
     const currentSeasonWeek = 3;
-    const currentSeasonStartDate = DateTime.fromObject({ year: 2024, month: 9, day: 1 }, { zone: timezone });
+    const currentSeasonStartDate = DateTime.fromObject({ year: 2024, month: 9, day: 1 }, { zone: 'utc' });
     const currentSeasonDate = currentSeasonStartDate.plus({
       weeks: currentSeasonWeek
     });
@@ -58,9 +58,9 @@ describe('getSeasonWeekNumberFromWeek', () => {
     );
   });
 
-  it('should return the season week number from the week when the season is behind the current week', () => {
+  it('should return the season week number when the season is behind the current week', () => {
     const currentSeasonWeek = 10;
-    const currentSeasonStartDate = DateTime.fromObject({ year: 2024, month: 11, day: 1 }, { zone: timezone });
+    const currentSeasonStartDate = DateTime.fromObject({ year: 2024, month: 11, day: 1 }, { zone: 'utc' });
     const currentSeasonDate = currentSeasonStartDate.plus({
       weeks: currentSeasonWeek
     });
--- packages/scoutgame/src/utils.ts
+++ packages/scoutgame/src/utils.ts
@@ -1,81 +1,43 @@
 import { DateTime } from 'luxon';
 
-export const timezone = 'America/New_York';
-
 export const currentSeason = 1;
 
 // Season 1 started on 2024-09-22
 // TODO: Make sure to update this before the season starts
-export const currentSeasonStartDate = DateTime.fromObject({ year: 2024, month: 9, day: 22 }, { zone: timezone });
+export const currentSeasonStartDate = DateTime.fromObject({ year: 2024, month: 9, day: 22 }, { zone: 'utc' });
 export const currentSeasonEndDate = currentSeasonStartDate.plus({ weeks: 13 });
 
 export const streakWindow = 7 * 24 * 60 * 60 * 1000;
 
 // Return the format of week
 export function getCurrentWeek() {
-  return getFormattedWeek(new Date());
+  return formatWeek(DateTime.utc());
 }
 
 export function getLastWeek() {
-  return getFormattedWeek(DateTime.now().minus({ week: 1 }).toJSDate());
+  return formatWeek(DateTime.utc().minus({ week: 1 }));
 }
 
 // get the number of the current week, with Sunday being the first day of the week in New York time
 export function getFormattedWeek(date: Date): string {
-  return formatWeek(getWeek(date));
+  return formatWeek(DateTime.fromJSDate(date));
 }
 
 export function getWeekStartEnd(date: Date) {
-  const newYorkDate = DateTime.fromJSDate(date, { zone: timezone });
-
-  // Shift the weekday to make Sunday the first day of the week
-  const dayOfWeek = newYorkDate.weekday % 7; // Sunday = 0, Monday = 1, ..., Saturday = 6
-
-  // Find the start of the week (Sunday)
-  const startOfWeek = newYorkDate.minus({ days: dayOfWeek }).startOf('day');
-  const endOfWeek = startOfWeek.plus({ days: 7 });
+  const utcDate = DateTime.fromJSDate(date).toUTC();
+  const startOfWeek = utcDate.startOf('week');
+  const endOfWeek = utcDate.endOf('week');
   return { start: startOfWeek, end: endOfWeek };
 }
 
-function formatWeek({ year, week }: { year: number; week: number }) {
-  return `${year}-W${week.toString().padStart(2, '0')}`;
-}
-
-// weeks start on Sunday. The year depends on the Sunday of the current week.
-// So if Jan 1 is on a Wednesday, the first week of the year starts on the Sunday of the next week.
-export function getWeek(date: Date): { week: number; year: number } {
-  // Get the current date in the specified timezone
-  const newYorkDate = DateTime.fromJSDate(date, { zone: timezone });
-
-  // Shift the weekday to make Sunday the first day of the week
-  const dayOfWeek = newYorkDate.weekday % 7; // Sunday = 0, Monday = 1, ..., Saturday = 6
-
-  // Find the start of the week (Sunday)
-  const startOfWeek = newYorkDate.minus({ days: dayOfWeek });
-
-  // Calculate the week number by comparing to the start of the year
-  const startOfYear = DateTime.fromObject({ year: newYorkDate.year, month: 1, day: 1 }, { zone: timezone });
-  const startOfYearDayOfWeek = startOfYear.weekday % 7;
-  const diffInDays = Math.floor(
-    startOfWeek
-      .diff(startOfYear, 'days')
-      // Adjust for the start of the year being in the middle of the week
-      .minus({ days: startOfYearDayOfWeek }).days
-  );
-
-  // Calculate the week number, rounding down to the nearest week
-  const weekNumber = Math.floor(diffInDays / 7) + 1;
-
-  // if the week number is 53, it means the last day of the year falls on the first week of the next year
-  if (weekNumber === 0) {
-    return { week: 52, year: newYorkDate.year - 1 };
-  }
-  return { week: weekNumber, year: newYorkDate.year };
+function formatWeek(date: DateTime) {
+  // token reference: https://moment.github.io/luxon/#/formatting?id=table-of-tokens
+  return date.toFormat(`kkkk-'W'WW`);
 }
 
 export function isToday(date: Date) {
-  const dateDay = DateTime.fromJSDate(date, { zone: timezone }).startOf('day');
-  return dateDay.equals(DateTime.now().setZone(timezone).startOf('day'));
+  const dateDay = DateTime.fromJSDate(date, { zone: 'utc' }).startOf('day');
+  return dateDay.equals(DateTime.utc().startOf('day'));
 }
 
 export function getCurrentWeekPoints() {
@@ -84,14 +46,14 @@ export function getCurrentWeekPoints() {
 }
 
 export function getCurrentWeekNumber() {
-  const currentDate = DateTime.now().setZone(timezone);
+  const currentDate = DateTime.utc();
   const weeksDiff = currentDate.diff(currentSeasonStartDate, 'weeks').weeks;
   return Math.floor(weeksDiff) + 1;
 }
 
 export function getSeasonWeekNumberFromWeek({ seasonStartDate, week }: { week: string; seasonStartDate: DateTime }) {
   const [year, weekNumber] = week.split('-W');
-  const date = DateTime.fromObject({ year: parseInt(year), day: 1, month: 1 }, { zone: timezone }).plus({
+  const date = DateTime.fromObject({ year: parseInt(year), day: 1, month: 1 }, { zone: 'utc' }).plus({
     weeks: parseInt(weekNumber)
   });
   const weeksDiff = date.diff(seasonStartDate, 'weeks').weeks;
