11bf47fb09db88db682e0d0df48be810e0265517
Safwan Shaheer <devorein00@gmail.com>
2024-09-27 20:27:46 +0600

    Added scout card link

--- apps/scoutgame/components/common/Gallery/ScoutsGallery.tsx
+++ apps/scoutgame/components/common/Gallery/ScoutsGallery.tsx
@@ -1,4 +1,5 @@
 import { Box, Grid2 as Grid } from '@mui/material';
+import Link from 'next/link';
 
 import type { ScoutInfo } from '../Card/ScoutCard';
 import { ScoutCard } from '../Card/ScoutCard';
@@ -9,7 +10,9 @@ export function ScoutsGallery({ scouts }: { scouts: ScoutInfo[] }) {
       <Grid container spacing={{ xs: 1, sm: 2 }} columns={{ xs: 3, sm: 6, md: 3 }}>
         {scouts.map((scout) => (
           <Grid key={scout.username} size={{ xs: 1 }}>
-            <ScoutCard scout={scout} />
+            <Link href={`/u/${scout.username}`}>
+              <ScoutCard scout={scout} />
+            </Link>
           </Grid>
         ))}
       </Grid>
