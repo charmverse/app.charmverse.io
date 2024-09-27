b1297abda22317ed3a60294b7c73ef4a51189bfc
Safwan Shaheer <devorein00@gmail.com>
2024-09-27 17:41:48 +0600

    Fixed issues with font size

--- packages/scoutgame/src/builderNfts/generateNftImage.tsx
+++ packages/scoutgame/src/builderNfts/generateNftImage.tsx
@@ -3,17 +3,41 @@ import path from 'path';
 
 import { ImageResponse } from 'next/og';
 import React from 'react';
+import type { Font } from 'satori';
 import sharp from 'sharp';
 
-const OVERLAY_FOLDER = path.join(path.resolve(__dirname, '../'), 'public', 'overlays');
+const OVERLAY_FOLDER = path.join(path.resolve(__dirname, '../'), 'assets', 'overlays');
 const overlayFiles = fs.readdirSync(OVERLAY_FOLDER);
 const overlaysBase64 = overlayFiles.map((file) => {
   const filePath = path.join(OVERLAY_FOLDER, file);
   const data = fs.readFileSync(filePath);
   return `data:image/png;base64,${data.toString('base64')}`;
 });
-const noPfpAvatarFile = path.join(path.resolve(__dirname, '../'), 'public', 'no_pfp_avatar.png');
+const noPfpAvatarFile = path.join(path.resolve(__dirname, '../'), 'assets', 'no_pfp_avatar.png');
 const noPfpAvatarBase64 = `data:image/png;base64,${fs.readFileSync(noPfpAvatarFile).toString('base64')}`;
+const fontPath = path.join(path.resolve(__dirname, '../'), 'assets', 'fonts', 'K2D-Medium.ttf');
+const fontBuffer = fs.readFileSync(fontPath);
+const font: Font = {
+  name: 'K2D',
+  data: fontBuffer,
+  style: 'normal',
+  weight: 400
+};
+
+// Function to determine font size
+function calculateFontSize(text: string, maxWidth: number, initialFontSize: number): number {
+  const minFontSize = 10;
+  let fontSize = initialFontSize;
+
+  while (fontSize > minFontSize) {
+    if (text.length * fontSize * 0.6 < maxWidth) {
+      return fontSize;
+    }
+    fontSize -= 1;
+  }
+
+  return minFontSize;
+}
 
 export async function generateNftImage({
   avatar,
@@ -23,6 +47,13 @@ export async function generateNftImage({
   username: string;
 }): Promise<Buffer> {
   const randomOverlay = overlaysBase64[Math.floor(Math.random() * overlaysBase64.length)];
+  let avatarBuffer: Buffer | null = null;
+  if (avatar) {
+    const response = await fetch(avatar);
+    const arrayBuffer = await response.arrayBuffer();
+    avatarBuffer = await sharp(Buffer.from(arrayBuffer)).resize({ height: 400, fit: 'contain' }).toBuffer();
+  }
+
   const baseImage = new ImageResponse(
     (
       <div
@@ -36,7 +67,13 @@ export async function generateNftImage({
           position: 'relative'
         }}
       >
-        <img src={noPfpAvatarBase64} width={300} height={400} />
+        <img
+          src={avatarBuffer ? `data:image/png;base64,${avatarBuffer.toString('base64')}` : noPfpAvatarBase64}
+          style={{
+            height: 400,
+            objectFit: 'contain'
+          }}
+        />
         <img src={randomOverlay} width={300} height={400} style={{ position: 'absolute', top: 0, left: 0 }} />
         <div
           style={{
@@ -44,18 +81,20 @@ export async function generateNftImage({
             display: 'flex',
             justifyContent: 'center',
             flexDirection: 'row',
-            top: 300,
+            bottom: 20,
             position: 'absolute',
-            flexWrap: 'wrap',
             paddingLeft: 10,
             paddingRight: 10
           }}
         >
           <p
             style={{
-              whiteSpace: 'wrap',
               color: 'white',
-              textAlign: 'center'
+              textAlign: 'center',
+              fontSize: `${calculateFontSize(username, 280, 24)}px`,
+              whiteSpace: 'nowrap',
+              maxWidth: `${280}px`,
+              fontFamily: 'K2D'
             }}
           >
             {username}
@@ -65,25 +104,13 @@ export async function generateNftImage({
     ),
     {
       width: 300,
-      height: 400
+      height: 400,
+      fonts: [font]
     }
   );
 
-  // Step 3: Convert ImageResponse to Buffer
   const baseImageBuffer = await baseImage.arrayBuffer();
+  const imageBuffer = sharp(Buffer.from(baseImageBuffer)).png().toBuffer();
 
-  // Step 4: Use sharp to overlay images
-  const image = sharp(Buffer.from(baseImageBuffer)).png();
-
-  // Step 5: Generate final image buffer
-  const finalImageBuffer = await image.toBuffer();
-
-  return finalImageBuffer;
+  return imageBuffer;
 }
-
-generateNftImage({
-  avatar: null,
-  username: 'test'
-}).then((buffer) => {
-  fs.writeFileSync('test.png', new Uint8Array(buffer));
-});
