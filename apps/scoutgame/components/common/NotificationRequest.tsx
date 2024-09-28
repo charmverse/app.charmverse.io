33a36473f1fbfd5f66c73fa24061ae818780cc15
Matt Casey <mattwad@gmail.com>
2024-09-28 16:02:19 -0600

    fix error in Safari (#4671)

--- apps/scoutgame/components/common/NotificationRequest.tsx
+++ apps/scoutgame/components/common/NotificationRequest.tsx
@@ -7,9 +7,11 @@ import { useLocalStorage } from 'usehooks-ts';
 import { savePushNotificationSubscriptionAction } from 'lib/pwa/savePushNotificationSubscriptionAction';
 
 export function NotificationRequest({ vapidPublicKey }: { vapidPublicKey?: string }) {
+  const WindowNotification = window.Notification as typeof Notification | undefined;
+  const notificationsSupported = 'Notification' in window && 'serviceWorker' in navigator && 'PushManager' in window;
   const [permission, setPermission] = useLocalStorage<NotificationPermission>(
     'notificationPermission',
-    typeof window !== 'undefined' ? window?.Notification?.permission : 'default'
+    WindowNotification?.permission || 'default'
   );
 
   const [snackbarState, setSnackbarState] = useState<boolean>(false);
@@ -24,12 +26,15 @@ export function NotificationRequest({ vapidPublicKey }: { vapidPublicKey?: strin
   };
 
   const requestPermission = useCallback(async () => {
-    const notificationsSupported = 'Notification' in window && 'serviceWorker' in navigator && 'PushManager' in window;
-    const notificationPermission = await Notification.requestPermission();
+    if (!WindowNotification || !notificationsSupported) {
+      return;
+    }
+
+    const notificationPermission = await WindowNotification.requestPermission();
     setPermission(notificationPermission);
 
     if (notificationPermission === 'granted') {
-      if (!notificationsSupported || Notification.permission !== 'granted') {
+      if (WindowNotification.permission !== 'granted') {
         return;
       }
 
@@ -54,7 +59,7 @@ export function NotificationRequest({ vapidPublicKey }: { vapidPublicKey?: strin
 
       await savePushNotificationSubscriptionAction({ subscription: json });
     }
-  }, [vapidPublicKey, setPermission]);
+  }, [vapidPublicKey, setPermission, notificationsSupported]);
 
   // Update permission state if the user changes it manually
   useEffect(() => {
