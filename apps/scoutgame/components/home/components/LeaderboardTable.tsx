2d56d86b41aef7f32e9b98a22e6b506809efe853
Matt Casey <mattwad@gmail.com>
2024-09-28 18:50:14 -0600

    fix scout buttons on home page (#4672)

--- apps/scoutgame/components/home/components/LeaderboardTable.tsx
+++ apps/scoutgame/components/home/components/LeaderboardTable.tsx
@@ -1,7 +1,4 @@
-'use client';
-
-import type { Theme } from '@mui/material';
-import { Box, Button, Stack, TableHead, Typography, useMediaQuery } from '@mui/material';
+import { Box, Button, Stack, TableHead, Typography } from '@mui/material';
 import Paper from '@mui/material/Paper';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
@@ -11,14 +8,14 @@ import TableRow from '@mui/material/TableRow';
 import { currentSeasonNumber, getCurrentSeasonWeekNumber } from '@packages/scoutgame/dates';
 import { DateTime } from 'luxon';
 import Image from 'next/image';
-import Link from 'next/link';
 
+import { Hidden } from 'components/common/Hidden';
+import { ScoutButton } from 'components/common/ScoutButton/ScoutButton';
 import type { LeaderBoardRow } from 'lib/builders/getLeaderboard';
 
 import { Avatar } from '../../common/Avatar';
 
 export function LeaderboardTable({ data }: { data: LeaderBoardRow[] }) {
-  const isMobile = useMediaQuery((theme: Theme) => theme.breakpoints.down('sm'));
   const sorted = data.sort((a, b) => b.progress - a.progress);
 
   return (
@@ -36,11 +33,12 @@ export function LeaderboardTable({ data }: { data: LeaderBoardRow[] }) {
               }
             }}
           >
-            {isMobile ? (
+            <Hidden mdUp>
               <TableCell colSpan={4} sx={{ textAlign: 'center' }}>
                 SEASON {currentSeasonNumber} WEEK {getCurrentSeasonWeekNumber()} DAY {(DateTime.now().weekday % 7) + 1}
               </TableCell>
-            ) : (
+            </Hidden>
+            <Hidden mdDown>
               <>
                 <TableCell align='center'>RANK</TableCell>
                 <TableCell>BUILDER</TableCell>
@@ -53,7 +51,7 @@ export function LeaderboardTable({ data }: { data: LeaderBoardRow[] }) {
                 </TableCell>
                 <TableCell />
               </>
-            )}
+            </Hidden>
           </TableRow>
         </TableHead>
         <TableBody>
@@ -94,13 +92,11 @@ export function LeaderboardTable({ data }: { data: LeaderBoardRow[] }) {
                   <Image width={15} height={15} src='/images/profile/icons/hex-gem-icon.svg' alt='Gem' />
                 </Stack>
               </TableCell>
-              {!isMobile && (
+              <Hidden mdDown>
                 <TableCell>
-                  <Button fullWidth variant='buy' LinkComponent={Link} href={`/u/${row.username}/checkout`}>
-                    ${row.price || 0}
-                  </Button>
+                  <ScoutButton price={row.price} builderId={row.builderId} />
                 </TableCell>
-              )}
+              </Hidden>
             </TableRow>
           ))}
         </TableBody>
