7a6a92f88bcb559bb92206a3cc096a1f4dc25aa6
Safwan Shaheer <devorein00@gmail.com>
2024-09-30 12:21:14 +0600

    Return pre-selected builders for sg homepage

--- apps/scoutgame/components/home/components/BuildersCarousel/TodaysHotBuildersCarousel.tsx
+++ apps/scoutgame/components/home/components/BuildersCarousel/TodaysHotBuildersCarousel.tsx
@@ -5,7 +5,7 @@ import { getTodaysHotBuilders } from 'lib/builders/getTodaysHotBuilders';
 import { BuildersCarousel } from './BuildersCarousel';
 
 export async function TodaysHotBuildersCarousel() {
-  const builders = await getTodaysHotBuilders({ limit: 10 });
+  const builders = await getTodaysHotBuilders();
 
   return <BuildersCarousel builders={builders} />;
 }
