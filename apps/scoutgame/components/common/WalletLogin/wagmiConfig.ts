837aaa251f4b63755b652d20fe23b6b58fff2b7a
mattcasey <mattwad@gmail.com>
2024-09-30 12:30:42 -0600

    remove list

--- apps/scoutgame/components/common/WalletLogin/wagmiConfig.ts
+++ apps/scoutgame/components/common/WalletLogin/wagmiConfig.ts
@@ -1,21 +1,12 @@
 import env from '@beam-australia/react-env';
 import { getDefaultConfig } from '@rainbow-me/rainbowkit';
-import { coinbaseWallet, rainbowWallet, metaMaskWallet, walletConnectWallet } from '@rainbow-me/rainbowkit/wallets';
 import type { Chain, Transport } from 'viem';
 import * as chains from 'viem/chains';
-import { http, createConfig, cookieStorage, createStorage } from 'wagmi';
-import { injected, walletConnect } from 'wagmi/connectors';
+import { http, cookieStorage, createStorage } from 'wagmi';
 
 export function getConfig() {
   const walletConnectProjectId = env('WALLETCONNECT_PROJECTID');
   const viemChains = Object.values(chains) as unknown as [Chain, ...Chain[]];
-
-  // const connectors = [
-  //   injected({
-  //     shimDisconnect: true
-  //   }),
-  //   ...(projectId ? [walletConnect({ projectId })] : [])
-  // ];
 
   const transports = viemChains.reduce<Record<string, Transport>>((acc, chain) => {
     acc[chain.id] = http();
@@ -26,13 +17,6 @@ export function getConfig() {
     appName: 'Scout Game',
     projectId: walletConnectProjectId,
     chains: viemChains,
-    // connectors,
-    wallets: [
-      {
-        groupName: 'SG List',
-        wallets: [coinbaseWallet, metaMaskWallet, rainbowWallet, walletConnectWallet]
-      }
-    ],
     ssr: true,
     storage: createStorage({ storage: cookieStorage }),
     transports
