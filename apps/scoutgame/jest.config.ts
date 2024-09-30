1a129519128e5a38ebb00c3856bb5287da81ac69
mattcasey <mattwad@gmail.com>
2024-09-29 19:10:13 -0600

    fix flaky scoutgame tests

--- apps/scoutgame/jest.config.ts
+++ apps/scoutgame/jest.config.ts
@@ -1,6 +1,9 @@
 import { compilerOptions } from './tsconfig.json';
 
 export default {
+  // Teardown function after all tests run
+  globalTeardown: '<rootDir>/jest.teardown-init.js',
+
   transform: {
     '^.+\\.(t|j)sx?$': [
       '@swc/jest',
