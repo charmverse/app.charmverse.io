007361668d29f43594f46372480c713e19808cce
mattcasey <mattwad@gmail.com>
2024-10-01 10:58:59 -0600

    dont use state to capture Wagmi config

--- apps/scoutgame/components/common/WalletLogin/WagmiProvider.tsx
+++ apps/scoutgame/components/common/WalletLogin/WagmiProvider.tsx
@@ -8,10 +8,9 @@ import { WagmiProvider as OriginalWagmiProvider } from 'wagmi';
 
 import { getConfig } from './wagmiConfig';
 
+const config = getConfig();
+const queryClient = new QueryClient();
 export function WagmiProvider({ children, initialState }: { children: React.ReactNode; initialState?: State }) {
-  const [config] = useState(() => getConfig());
-  const [queryClient] = useState(() => new QueryClient());
-
   return (
     <OriginalWagmiProvider initialState={initialState} config={config}>
       <QueryClientProvider client={queryClient}>
