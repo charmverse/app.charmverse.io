68fe4abbbe7ce566bd665d79c4558bfadd037646
Matt Casey <mattwad@gmail.com>
2024-10-01 13:14:49 -0600

    Add tests for home page + put wagmi at top of layout (#4710)
    * add e2e for home page and wagmi
    
    * bring back wagmi to top of page

--- apps/scoutgame/__e2e__/homePage.spec.ts
+++ apps/scoutgame/__e2e__/homePage.spec.ts
@@ -1,4 +1,13 @@
+import { getCurrentWeek, currentSeason } from '@packages/scoutgame/dates';
+import { mockBuilder, mockUserWeeklyStats } from '@packages/scoutgame/testing/database';
+
 import { expect, test } from './test';
+
+async function mockLeaderboard() {
+  const builder = await mockBuilder();
+  const userWeeklyStats = await mockUserWeeklyStats({ userId: builder.id });
+  return { builder, userWeeklyStats };
+}
 
 test.describe('Home page', () => {
   test('Open the app and go to profile page as a public user', async ({ page, homePage }) => {
@@ -7,5 +16,19 @@ test.describe('Home page', () => {
     await page.waitForURL('**/home');
 
     await expect(homePage.container).toBeVisible();
+  });
+  test('Can navigate to each tab', async ({ page, homePage }) => {
+    // add some mock data
+    await mockLeaderboard();
+
+    await page.goto('/home');
+    await expect(homePage.container).toBeVisible();
+
+    for (const tab of homePage.tabs) {
+      await homePage.selectTab(tab);
+      await expect(homePage.tabView(tab)).toBeVisible();
+    }
+    await homePage.selectTab('leaderboard');
+    await expect(homePage.tabView('leaderboard')).toBeVisible();
   });
 });
