405904abf7e7cf40ce856b3fc735042e71e74cc8
Safwan Shaheer <devorein00@gmail.com>
2024-10-04 03:26:51 +0600

    Fixed home page crash with back button

--- apps/scoutgame/components/home/components/BuildersCarousel/BuildersCarousel.tsx
+++ apps/scoutgame/components/home/components/BuildersCarousel/BuildersCarousel.tsx
@@ -1,7 +1,10 @@
 'use client';
 
+import { useEffect, useState } from 'react';
+
 import { Carousel } from 'components/common/Carousel/Carousel';
-import { useLgScreen, useMdScreen, useXsmScreen } from 'hooks/useMediaScreens';
+import { LoadingCards } from 'components/common/Loading/LoadingCards';
+import { useLgScreen, useMdScreen } from 'hooks/useMediaScreens';
 import type { BuilderInfo } from 'lib/builders/interfaces';
 
 import { BuilderCard } from '../../../common/Card/BuilderCard/BuilderCard';
@@ -9,8 +12,16 @@ import { BuilderCard } from '../../../common/Card/BuilderCard/BuilderCard';
 export function BuildersCarousel({ builders, userId }: { builders: BuilderInfo[]; userId?: string }) {
   const isMdScreen = useMdScreen();
   const isLgScreen = useLgScreen();
-
   const size = isLgScreen ? 'large' : isMdScreen ? 'small' : 'x-small';
+  const [isClientSide, setIsClientSide] = useState(false);
+
+  useEffect(() => {
+    setIsClientSide(true);
+  }, []);
+
+  if (!isClientSide) {
+    return <LoadingCards />;
+  }
 
   return (
     <Carousel>
