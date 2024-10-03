e4e341a082413ffe9c5b28fe0a05c18a5c80e4a1
Matt Casey <mattwad@gmail.com>
2024-10-02 21:12:00 -0600

    add scripts to import scouts and repos into Scout Game (#4729)

--- packages/farcaster/src/getFarcasterUserById.ts
+++ packages/farcaster/src/getFarcasterUserById.ts
@@ -5,8 +5,13 @@ import type { FarcasterUser } from './interfaces';
 const userApiUrl = 'https://api.neynar.com/v2/farcaster/user/bulk';
 
 export async function getFarcasterUserById(fid: number) {
+  const users = await getFarcasterUserByIds([fid]);
+  return users[0] || null;
+}
+
+export async function getFarcasterUserByIds(fids: number[]) {
   const { users } = await http.GET<{ users: FarcasterUser[] }>(
-    `${userApiUrl}?fids=${fid}`,
+    `${userApiUrl}?fids=${fids}`,
     {},
     {
       credentials: 'omit',
@@ -15,5 +20,5 @@ export async function getFarcasterUserById(fid: number) {
       }
     }
   );
-  return users[0] || null;
+  return users;
 }
