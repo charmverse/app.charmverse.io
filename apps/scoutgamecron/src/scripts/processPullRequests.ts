c62fd60ea7f03153ede623a9c1b468e92869ee41
mattcasey <mattwad@gmail.com>
2024-10-02 22:56:16 -0600

    show field

--- apps/scoutgamecron/src/scripts/processPullRequests.ts
+++ apps/scoutgamecron/src/scripts/processPullRequests.ts
@@ -1,10 +1,7 @@
 import { processPullRequests } from '../tasks/processPullRequests';
 import { DateTime } from 'luxon';
-import { decodeGithubUserId } from '../tasks/processPullRequests/getPullRequests';
 (async () => {
-  console.log(Buffer.from('U_kgDOB0sJ8Q', 'base64').toString('utf-8'));
-  console.log(Buffer.from('U_kgDOB-4rVA', 'base64').toString('utf-8'));
-  // await processPullRequests({
-  //   createdAfter: DateTime.fromISO('2024-09-29', { zone: 'utc' }).toJSDate()
-  // });
+  await processPullRequests({
+    createdAfter: DateTime.fromISO('2024-09-29', { zone: 'utc' }).toJSDate()
+  });
 })();
