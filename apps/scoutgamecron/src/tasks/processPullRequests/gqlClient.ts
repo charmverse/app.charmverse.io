ff1e545afdb7254bb3c5d876e6dac1b9e5461baa
mattcasey <mattwad@gmail.com>
2024-10-04 00:38:25 -0600

    fix retries and wait longer for cron

--- apps/scoutgamecron/src/tasks/processPullRequests/gqlClient.ts
+++ apps/scoutgamecron/src/tasks/processPullRequests/gqlClient.ts
@@ -1,24 +1,31 @@
+import { log } from '@charmverse/core/log';
 import { Octokit } from '@octokit/core';
 import { throttling } from '@octokit/plugin-throttling';
 
 // we need to use octokit core to use the throttling plugin
+// ref: https://octokit.github.io/rest.js/v19/#throttling
 Octokit.plugin(throttling);
 
 const octokit = new Octokit({
   auth: process.env.GITHUB_ACCESS_TOKEN,
   throttle: {
     onRateLimit: (retryAfter, options, _octokit, retryCount) => {
-      octokit.log.warn(`Request quota exhausted for request ${options.method} ${options.url}`);
+      log.warn(`[Octokit] Request quota exhausted for request ${options.method} ${options.url}`);
 
-      if (retryCount < 1) {
-        // only retries once
-        octokit.log.info(`Retrying after ${retryAfter} seconds!`);
-        return true;
-      }
+      log.info(`[Octokit] Retrying after ${retryAfter} seconds!`);
+      return true;
+      // if (retryCount < 2) {
+      //   // only retries twice
+      //   return true;
+      // }
     },
     onSecondaryRateLimit: (retryAfter, options, _octokit) => {
       // does not retry, only logs a warning
-      octokit.log.warn(`SecondaryRateLimit detected for request ${options.method} ${options.url}`);
+      log.warn(
+        `[Octokit] SecondaryRateLimit detected for request ${options.method} ${options.url}. Retrying after ${retryAfter} seconds!`
+      );
+      // try again
+      return true;
     }
   }
 });
