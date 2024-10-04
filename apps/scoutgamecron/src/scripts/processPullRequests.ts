1d0d6e31ddb8a6e3560cb7c4ff254f1f491af4e2
Matt Casey <mattwad@gmail.com>
2024-10-03 21:44:20 -0600

    optimize saving github events (#4741)
    * optimize saving github events
    
    * use throttling
    
    * update id
    
    * .
    
    * .

--- apps/scoutgamecron/src/scripts/processPullRequests.ts
+++ apps/scoutgamecron/src/scripts/processPullRequests.ts
@@ -1,7 +1,9 @@
 import { processPullRequests } from '../tasks/processPullRequests';
 import { DateTime } from 'luxon';
-(async () => {
+import { getCurrentWeek, currentSeason } from '@packages/scoutgame/dates';
+
   await processPullRequests({
-    createdAfter: DateTime.fromISO('2024-09-29', { zone: 'utc' }).toJSDate()
+    createdAfter: DateTime.fromISO('2024-09-29', { zone: 'utc' }).toJSDate(),
+    onlyProcessNewRepos: true
   });
 })();
