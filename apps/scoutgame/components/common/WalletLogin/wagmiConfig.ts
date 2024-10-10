e81a253dd0f05deeb27488cd4c757e84b6388214
Valentin L <ludu.valentin@gmail.com>
2024-10-10 10:23:37 -0500

    Alchemy RPC for SG FE

--- apps/scoutgame/components/common/WalletLogin/wagmiConfig.ts
+++ apps/scoutgame/components/common/WalletLogin/wagmiConfig.ts
@@ -1,21 +1,35 @@
 import env from '@beam-australia/react-env';
 import { getDefaultConfig } from '@rainbow-me/rainbowkit';
+import { getAlchemyBaseUrl } from '@root/lib/blockchain/provider/alchemy/client';
 import type { Chain, Transport } from 'viem';
-import * as chains from 'viem/chains';
-import { http, cookieStorage, createStorage } from 'wagmi';
+import { http, cookieStorage, createStorage, fallback } from 'wagmi';
+import {
+  arbitrum,
+  arbitrumSepolia,
+  mainnet,
+  optimism,
+  optimismSepolia,
+  sepolia,
+  zora,
+  zoraSepolia
+} from 'wagmi/chains';
 
 export function getConfig() {
   const walletConnectProjectId = env('WALLETCONNECT_PROJECTID');
-  const viemChains = Object.values(chains) as unknown as [Chain, ...Chain[]];
-  const transports = viemChains.reduce<Record<string, Transport>>((acc, chain) => {
-    acc[chain.id] = http();
+  const wagmiChains = [mainnet, sepolia, optimism, optimismSepolia, arbitrum, arbitrumSepolia, zora, zoraSepolia] as [
+    Chain,
+    ...Chain[]
+  ];
+  const transports = wagmiChains.reduce<Record<string, Transport>>((acc, chain) => {
+    const rpcUrl = getAlchemyBaseUrl(chain.id);
+    acc[chain.id] = fallback([http(rpcUrl), http()]);
     return acc;
   }, {});
 
   const config = getDefaultConfig({
     appName: 'Scout Game',
     projectId: walletConnectProjectId,
-    chains: viemChains,
+    chains: wagmiChains,
     ssr: true,
     storage: createStorage({ storage: cookieStorage }),
     transports
