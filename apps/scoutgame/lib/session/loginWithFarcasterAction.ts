7d31bcb5b7e2739262af9359a041f768bce276aa
Safwan Shaheer <devorein00@gmail.com>
2024-10-16 15:56:12 +0600

    Added invite code login

--- apps/scoutgame/lib/session/loginWithFarcasterAction.ts
+++ apps/scoutgame/lib/session/loginWithFarcasterAction.ts
@@ -1,5 +1,8 @@
 'use server';
 
+import env from '@beam-australia/react-env';
+import { log } from '@charmverse/core/log';
+
 import { actionClient } from 'lib/actions/actionClient';
 import { findOrCreateFarcasterUser } from 'lib/farcaster/findOrCreateFarcasterUser';
 import { verifyFarcasterUser } from 'lib/farcaster/verifyFarcasterUser';
@@ -9,12 +12,18 @@ import { authSchema } from '../farcaster/config';
 import { authorizeUserByLaunchDate } from './authorizeUserByLaunchDate';
 import { saveSession } from './saveSession';
 
+const inviteCode = env('SCOUTGAME_INVITE_CODE') ?? process.env.REACT_APP_SCOUTGAME_INVITE_CODE;
+
 export const loginWithFarcasterAction = actionClient
   .metadata({ actionName: 'login_with_farcaster' })
   .schema(authSchema)
   .action(async ({ ctx, parsedInput }) => {
     const { fid } = await verifyFarcasterUser(parsedInput);
-    await authorizeUserByLaunchDate({ fid });
+    if (parsedInput.inviteCode !== inviteCode) {
+      await authorizeUserByLaunchDate({ fid });
+    } else {
+      log.info('User joined with invite code', { fid });
+    }
     const user = await findOrCreateFarcasterUser({ fid });
 
     await saveSession(ctx, { scoutId: user.id });
