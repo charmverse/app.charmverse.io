8dd9e99c891dd63bd9b3db7cf511db0a6a14e743
Safwan Shaheer <devorein00@gmail.com>
2024-10-17 20:04:58 +0600

    Alert on discord for low sg wallet gas balance

---
+++ apps/scoutgamecron/src/tasks/alertLowWalletGasBalance/getWalletGasBalanceInUSD.ts
@@ -0,0 +1,30 @@
+import env from '@beam-australia/react-env';
+import { GET, POST } from '@packages/utils/http';
+
+export async function getWalletGasBalanceInUSD(walletAddress: string) {
+  const apiKey = process.env.ALCHEMY_API_KEY || env('ALCHEMY_API_KEY');
+  const alchemyApiUrl = `https://opt-mainnet.g.alchemy.com/v2/${apiKey}`;
+  const response = await POST<{ result: string }>(alchemyApiUrl, {
+    jsonrpc: '2.0',
+    method: 'eth_getBalance',
+    params: [walletAddress, 'latest'],
+    id: 1
+  });
+
+  if (!response.result) {
+    throw new Error('Unable to fetch the balance');
+  }
+
+  const balanceWei = response.result;
+  const balanceEth = parseInt(balanceWei, 16) / 10 ** 18;
+
+  const priceResponse = await GET<{ ethereum: { usd: number } }>('https://api.coingecko.com/api/v3/simple/price', {
+    ids: 'ethereum',
+    vs_currencies: 'usd'
+  });
+  const ethPriceInUSD = priceResponse.ethereum.usd;
+
+  const balanceInUSD = balanceEth * ethPriceInUSD;
+
+  return balanceInUSD;
+}
