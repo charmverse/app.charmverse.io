da6512bee371167defc6dd7c42456602abf19371
Valentin L <ludu.valentin@gmail.com>
2024-10-17 17:35:58 +0300

    Revert DocumentContainer

--- apps/scoutgame/components/common/DocumentPageContainer/DocumentPageContainer.tsx
+++ apps/scoutgame/components/common/DocumentPageContainer/DocumentPageContainer.tsx
@@ -12,17 +12,12 @@ export function DocumentPageContainer({
   'data-test': dataTest
 }: PropsWithChildren<{ 'data-test'?: string }>) {
   return (
-    <Container data-test={dataTest} maxWidth='lg'>
-      <Stack py={8} gap={8} maxWidth='100%' flexDirection='row'>
-        <Stack sx={{ display: { xs: 'none', md: 'flex' } }}>
-          <SidebarInfo />
-        </Stack>
-        <Stack maxWidth='854px' width='100%' mx='auto' gap={8}>
-          {children}
-          <ScrollButton scrollType='up' sx={{ textAlign: 'center', width: '100%' }}>
-            back to top <ArrowDropUpIcon fontSize='small' />
-          </ScrollButton>
-        </Stack>
+    <Container data-test={dataTest} maxWidth='md'>
+      <Stack py={8} gap={8} mx='auto' width='854px' maxWidth='100%'>
+        {children}
+        <ScrollButton scrollType='up' sx={{ textAlign: 'center', width: '100%' }}>
+          back to top <ArrowDropUpIcon fontSize='small' />
+        </ScrollButton>
       </Stack>
     </Container>
   );
