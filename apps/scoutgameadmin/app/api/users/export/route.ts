6c31037d903125503f94932e9e62b26fb4e784b8
mattcasey <mattwad@gmail.com>
2024-10-17 23:39:17 -0600

    .

--- apps/scoutgameadmin/app/api/users/export/route.ts
+++ apps/scoutgameadmin/app/api/users/export/route.ts
@@ -1,4 +1,5 @@
 import { prisma } from '@charmverse/core/prisma-client';
+import { currentSeason } from '@packages/scoutgame/dates';
 import { stringify } from 'csv-stringify/sync';
 import type { NextRequest } from 'next/server';
 
@@ -8,7 +9,9 @@ type ScoutWithGithubUser = {
   id: string;
   username: string;
   createdAt: string;
-  // avatar: string;
+  email?: string;
+  tokenId?: number;
+  optedInToMarketing?: string;
   builderStatus?: string;
   githubLogin?: string;
   fid?: number;
@@ -26,23 +29,33 @@ export async function GET(req: NextRequest) {
     select: {
       id: true,
       username: true,
+      sendMarketing: true,
       createdAt: true,
       avatar: true,
+      email: true,
       builderStatus: true,
       farcasterId: true,
       farcasterName: true,
       currentBalance: true,
       githubUser: true,
       userSeasonStats: true,
-      userWeeklyStats: true
+      userWeeklyStats: true,
+      builderNfts: {
+        where: {
+          season: currentSeason
+        }
+      }
     }
   });
   const rows: ScoutWithGithubUser[] = users.map((user) => ({
     id: user.id,
     username: user.username,
     createdAt: user.createdAt.toDateString(),
+    email: user.email || undefined,
+    optedInToMarketing: user.sendMarketing ? 'Yes' : '',
     // avatar: user.avatar || '',
     builderStatus: user.builderStatus || undefined,
+    tokenId: user.builderNfts[0]?.tokenId || undefined,
     fid: user.farcasterId || undefined,
     farcasterName: user.farcasterName || undefined,
     githubLogin: user.githubUser[0]?.login,
