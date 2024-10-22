d2d940eac96e497416d81b599ba3b2802a99362c
Matt Casey <mattwad@gmail.com>
2024-10-22 16:19:10 -0600

    Show 'how it works' to anon users (#4872)
    * show welcome modal for the first time a user visits the site
    
    * check the path
    
    * only show in prod env
    
    * add only for anon users
    
    * .
    
    * .

--- packages/utils/src/browser.ts
+++ packages/utils/src/browser.ts
@@ -9,3 +9,38 @@ export function isMac() {
     navigator.platform.toUpperCase().indexOf('IPAD') >= 0
   );
 }
+
+export function getCookie(name: string): string | undefined {
+  if (typeof document === 'undefined') {
+    return undefined;
+  }
+  const cookieMap = document.cookie.split(';').reduce<{ [key: string]: string }>((cookies, cookie) => {
+    const _name = cookie.trim().split('=')[0];
+    const value = cookie.trim().split('=')[1];
+    cookies[_name] = decodeURIComponent(value);
+    return cookies;
+  }, {});
+  return cookieMap[name];
+}
+
+// cookies reference: https://developer.mozilla.org/en-US/docs/Web/API/document/cookie
+export function setCookie({
+  name,
+  value,
+  expiresInDays = 10 * 365,
+  expiresAfterSession = false,
+  secure = false
+}: {
+  name: string;
+  value: string;
+  expiresInDays?: number;
+  expiresAfterSession?: boolean;
+  secure?: boolean;
+}) {
+  const expires = new Date();
+
+  expires.setDate(expires.getDate() + expiresInDays);
+  document.cookie = `${name}=${encodeURIComponent(value)};${
+    expiresAfterSession ? '' : ` expires=${expires.toUTCString()};`
+  } path=/; ${secure ? 'secure;' : ''}}`;
+}
