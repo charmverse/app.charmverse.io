570b051b384059b295cb671f2d471c287a5d5b2b
Safwan Shaheer <devorein00@gmail.com>
2024-10-22 20:08:09 +0600

    Updated points table ui

--- apps/scoutgame/components/claim/components/PointsTable.tsx
+++ apps/scoutgame/components/claim/components/PointsTable.tsx
@@ -5,15 +5,36 @@ import type { ReactNode } from 'react';
 
 import type { WeeklyReward } from 'lib/points/getPointsWithEvents';
 
+function getOrdinal(n: number): string {
+  const ordinal = new Intl.PluralRules('en', { type: 'ordinal' }).select(n);
+  const suffix = { zero: '', one: 'st', two: 'nd', few: 'rd', many: 'th', other: 'th' }[ordinal];
+  return `${n}${suffix}`;
+}
+
+function PointsCell({ points }: { points: number }) {
+  return (
+    <Stack direction='row' alignItems='center' justifyContent='flex-end' gap={0.5}>
+      <Typography>{points}</Typography>
+      <Image alt='scout game icon' src='/images/profile/scout-game-icon.svg' width={20} height={20} />
+    </Stack>
+  );
+}
+
 function WeeklyPointsRows({ weeklyReward }: { weeklyReward: WeeklyReward }) {
   const rows: ReactNode[] = [];
 
   if (weeklyReward.rank) {
     rows.push(
       <TableRow key={`${weeklyReward.weekNumber}rank`} sx={{ mb: 1 }}>
-        <TableCell align='left'>Finished {weeklyReward.rank}</TableCell>
-        <TableCell align='center'>{weeklyReward.weekNumber}</TableCell>
-        <TableCell align='right'>{weeklyReward.githubContributionReward.points}</TableCell>
+        <TableCell align='left' sx={{ minWidth: 150 }}>
+          <Typography>Finished {getOrdinal(weeklyReward.rank)}</Typography>
+        </TableCell>
+        <TableCell align='center'>
+          <Typography>{weeklyReward.weekNumber}</Typography>
+        </TableCell>
+        <TableCell align='right'>
+          <PointsCell points={weeklyReward.githubContributionReward?.points || 0} />
+        </TableCell>
       </TableRow>
     );
   }
@@ -21,9 +42,15 @@ function WeeklyPointsRows({ weeklyReward }: { weeklyReward: WeeklyReward }) {
   if (weeklyReward.builderReward) {
     rows.push(
       <TableRow key={`${weeklyReward.weekNumber}builder-rewards`} sx={{ mb: 1 }}>
-        <TableCell align='left'>Builder rewards</TableCell>
-        <TableCell align='center'>{weeklyReward.weekNumber}</TableCell>
-        <TableCell align='right'>{weeklyReward.builderReward.points}</TableCell>
+        <TableCell align='left' sx={{ minWidth: 150 }}>
+          <Typography>Builder rewards</Typography>
+        </TableCell>
+        <TableCell align='center'>
+          <Typography>{weeklyReward.weekNumber}</Typography>
+        </TableCell>
+        <TableCell align='right'>
+          <PointsCell points={weeklyReward.builderReward.points} />
+        </TableCell>
       </TableRow>
     );
   }
@@ -31,7 +58,7 @@ function WeeklyPointsRows({ weeklyReward }: { weeklyReward: WeeklyReward }) {
   if (weeklyReward.soldNftReward) {
     rows.push(
       <TableRow key={`${weeklyReward.weekNumber}sold-nft`} sx={{ mb: 1 }}>
-        <TableCell align='left'>
+        <TableCell align='left' sx={{ minWidth: 150 }}>
           <Stack direction='row' alignItems='center' justifyContent='flex-start' gap={0.5}>
             <Typography>Sold {weeklyReward.soldNftReward.quantity}</Typography>
             <Image alt='card' src='/images/profile/icons/card.svg' width={18} height={18} />
@@ -40,11 +67,8 @@ function WeeklyPointsRows({ weeklyReward }: { weeklyReward: WeeklyReward }) {
         <TableCell align='center'>
           <Typography>{weeklyReward.weekNumber}</Typography>
         </TableCell>
-        <TableCell>
-          <Stack direction='row' alignItems='center' justifyContent='flex-end' gap={0.5}>
-            <Typography>{weeklyReward.soldNftReward.points}</Typography>
-            <Image alt='scout game icon' src='/images/profile/scout-game-icon.svg' width={20} height={20} />
-          </Stack>
+        <TableCell align='right'>
+          <PointsCell points={weeklyReward.soldNftReward.points} />
         </TableCell>
       </TableRow>
     );
