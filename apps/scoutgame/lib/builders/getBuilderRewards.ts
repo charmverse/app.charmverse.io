5536aad2b4eb9cb2d70d55b1af2707bcd8333a8b
Safwan Shaheer <devorein00@gmail.com>
2024-10-23 18:08:07 +0600

    Refactored get builder rewards

--- apps/scoutgame/lib/builders/getBuilderRewards.ts
+++ apps/scoutgame/lib/builders/getBuilderRewards.ts
@@ -11,86 +11,76 @@ type BuilderReward = {
 };
 
 export async function getSeasonBuilderRewards({ userId }: { userId: string }): Promise<BuilderReward[]> {
-  const pointsReceipts = await prisma.pointsReceipt.findMany({
+  const scout = await prisma.scout.findUniqueOrThrow({
     where: {
-      recipientId: userId,
-      event: {
-        type: 'gems_payout'
-      }
+      id: userId
     },
     select: {
-      value: true,
-      event: {
+      nftPurchaseEvents: {
+        where: {
+          builderNFT: {
+            season: currentSeason
+          }
+        },
         select: {
-          builderId: true
-        }
-      }
-    }
-  });
-
-  const uniqueBuilderIds = Array.from(new Set(pointsReceipts.map((receipt) => receipt.event.builderId)));
-
-  const builders = await prisma.scout.findMany({
-    where: {
-      id: { in: uniqueBuilderIds }
-    },
-    select: {
-      id: true,
-      username: true,
-      avatar: true
-    }
-  });
-
-  const nftPurchaseEvents = await prisma.nFTPurchaseEvent.findMany({
-    where: {
-      scoutId: userId
-    },
-    select: {
-      builderNFT: {
-        select: {
-          builderId: true
+          builderNFT: {
+            select: {
+              builderId: true
+            }
+          },
+          tokensPurchased: true
         }
       },
-      tokensPurchased: true
+      pointsReceived: {
+        where: {
+          value: {
+            gt: 0
+          },
+          event: {
+            type: 'gems_payout'
+          }
+        },
+        select: {
+          value: true,
+          event: {
+            select: {
+              builder: {
+                select: {
+                  id: true,
+                  username: true,
+                  avatar: true
+                }
+              }
+            }
+          }
+        }
+      }
     }
   });
 
   const builderTokensRecord: Record<string, number> = {};
 
-  nftPurchaseEvents.forEach((event) => {
+  scout.nftPurchaseEvents.forEach((event) => {
     const builderId = event.builderNFT.builderId;
     builderTokensRecord[builderId] = (builderTokensRecord[builderId] || 0) + event.tokensPurchased;
   });
 
-  const buildersRecord: Record<string, Omit<BuilderReward, 'points' | 'rank'>> = {};
-  builders.forEach((builder) => {
-    const cardsHeld = builderTokensRecord[builder.id] || 0;
-    if (cardsHeld) {
-      buildersRecord[builder.id] = {
-        username: builder.username,
-        avatar: builder.avatar,
-        cardsHeld
-      };
-    }
-  });
-
   const builderRewardsRecord: Record<string, BuilderReward> = {};
 
-  pointsReceipts.forEach((receipt) => {
-    const builderId = receipt.event.builderId;
-    const builder = buildersRecord[builderId];
-    if (builder) {
-      if (!builderRewardsRecord[builderId]) {
-        builderRewardsRecord[builderId] = {
-          username: builder.username,
-          avatar: builder.avatar,
-          cardsHeld: builder.cardsHeld,
-          points: 0,
-          rank: null
-        };
-      }
-      builderRewardsRecord[builderId].points += receipt.value;
+  scout.pointsReceived.forEach((receipt) => {
+    const builder = receipt.event.builder;
+    const builderId = builder.id;
+    const cardsHeld = builderTokensRecord[builderId];
+    if (!builderRewardsRecord[builderId]) {
+      builderRewardsRecord[builderId] = {
+        username: builder.username,
+        avatar: builder.avatar,
+        cardsHeld,
+        points: 0,
+        rank: null
+      };
     }
+    builderRewardsRecord[builderId].points += receipt.value;
   });
 
   return Object.values(builderRewardsRecord).sort((a, b) => b.points - a.points);
@@ -103,32 +93,58 @@ export async function getWeeklyBuilderRewards({
   userId: string;
   week: string;
 }): Promise<BuilderReward[]> {
-  const pointsReceipts = await prisma.pointsReceipt.findMany({
+  const scout = await prisma.scout.findUniqueOrThrow({
     where: {
-      recipientId: userId,
-      value: {
-        gt: 0
-      },
-      event: {
-        week,
-        type: 'gems_payout'
-      }
+      id: userId
     },
     select: {
-      value: true,
-      event: {
+      nftPurchaseEvents: {
+        where: {
+          builderEvent: {
+            week: {
+              lte: week
+            }
+          },
+          builderNFT: {
+            season: currentSeason
+          }
+        },
         select: {
-          builder: {
+          builderNFT: {
             select: {
-              id: true,
-              username: true,
-              avatar: true,
-              userWeeklyStats: {
-                where: {
-                  week
-                },
+              builderId: true
+            }
+          },
+          tokensPurchased: true
+        }
+      },
+      pointsReceived: {
+        where: {
+          value: {
+            gt: 0
+          },
+          event: {
+            week,
+            type: 'gems_payout'
+          }
+        },
+        select: {
+          value: true,
+          event: {
+            select: {
+              builder: {
                 select: {
-                  rank: true
+                  id: true,
+                  username: true,
+                  avatar: true,
+                  userWeeklyStats: {
+                    where: {
+                      week
+                    },
+                    select: {
+                      rank: true
+                    }
+                  }
                 }
               }
             }
@@ -138,44 +154,19 @@ export async function getWeeklyBuilderRewards({
     }
   });
 
-  const uniqueBuilderIds = Array.from(new Set(pointsReceipts.map((receipt) => receipt.event.builder.id)));
-
-  const nftPurchaseEvents = await prisma.nFTPurchaseEvent.findMany({
-    where: {
-      scoutId: userId,
-      builderEvent: {
-        week: {
-          lte: week
-        }
-      },
-      builderNFT: {
-        builderId: { in: uniqueBuilderIds },
-        season: currentSeason
-      }
-    },
-    select: {
-      builderNFT: {
-        select: {
-          builderId: true
-        }
-      },
-      tokensPurchased: true
-    }
-  });
-
   const builderTokensRecord: Record<string, number> = {};
 
-  nftPurchaseEvents.forEach((event) => {
+  scout.nftPurchaseEvents.forEach((event) => {
     const builderId = event.builderNFT.builderId;
     builderTokensRecord[builderId] = (builderTokensRecord[builderId] || 0) + event.tokensPurchased;
   });
 
-  return pointsReceipts
+  return scout.pointsReceived
     .map((receipt) => {
       const builder = receipt.event.builder;
-      const cardsHeld = builderTokensRecord[builder.id] || 0;
+      const cardsHeld = builderTokensRecord[builder.id];
       const rank = builder.userWeeklyStats[0]?.rank || null;
-      if (rank === null || cardsHeld === 0) {
+      if (rank === null) {
         return null;
       }
       return {
