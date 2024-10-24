1e8ac0f1aba80804ec410e34c7bd5075557d36f8
mattcasey <mattwad@gmail.com>
2024-10-24 09:46:46 -0600

    add db query to health check

--- apps/scoutgame/app/api/health/route.ts
+++ apps/scoutgame/app/api/health/route.ts
@@ -1,3 +1,7 @@
-export function GET() {
+import { prisma } from '@charmverse/core/prisma-client';
+
+export async function GET() {
+  // test that the DB is configured properly
+  await prisma.scout.findFirst();
   return new Response('ok');
 }
