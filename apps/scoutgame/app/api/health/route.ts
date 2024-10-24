1f2dd16c8a3d89b8f80c205ed0ab4fe7626e2ea7
mattcasey <mattwad@gmail.com>
2024-10-24 10:04:31 -0600

    force dynamic

--- apps/scoutgame/app/api/health/route.ts
+++ apps/scoutgame/app/api/health/route.ts
@@ -1,5 +1,7 @@
 import { prisma } from '@charmverse/core/prisma-client';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET() {
   // test that the DB is configured properly
   await prisma.scout.findFirst();
