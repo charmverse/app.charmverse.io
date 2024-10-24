18e88dc456aeba3a58d8271a5262ac610a9c294e
motechFR <mo@anyahq.co>
2024-10-24 23:47:32 +0300

    Cleanup the admin dashboard

--- apps/scoutgameadmin/components/contract/ContractDashboard.tsx
+++ apps/scoutgameadmin/components/contract/ContractDashboard.tsx
@@ -1,39 +1,118 @@
-import { Grid2 } from '@mui/material';
+import { Box, Divider, Grid2, IconButton, Typography } from '@mui/material';
 import Link from 'next/link';
+import { MdLaunch } from 'react-icons/md';
 
 import type { BuilderNFTContractData } from 'lib/contract/getContractData';
 
-export function ContractDashboard(data: BuilderNFTContractData) {
+function ContractLink({
+  address,
+  linkType = 'address',
+  title,
+  subtitle
+}: {
+  address: string;
+  linkType?: 'address' | 'token' | 'contract';
+  title: string;
+  subtitle?: string;
+}) {
   return (
-    <Grid2 container gap={2}>
-      <Grid2 size={{ xs: 12 }}>
-        <h2>Proxy address</h2>
-        <Link href={`https://optimism.blockscout.com/token/${data.contractAddress}`} target='_blank'>
-          {data.contractAddress}
-        </Link>
-      </Grid2>
-      <Grid2 size={{ xs: 12 }}>
-        <h2>Current Admin</h2>
-        <Link href={`https://optimism.blockscout.com/address/${data.currentAdmin}`} target='_blank'>
-          {data.currentAdmin}
-        </Link>
-      </Grid2>
-      <Grid2 size={{ xs: 12 }}>
-        <h2>Current Implementation</h2>
-        <Link href={`https://optimism.blockscout.com/address/${data.currentImplementation}`} target='_blank'>
-          {data.currentImplementation}
-        </Link>
-      </Grid2>
-      <Grid2 size={{ xs: 12 }}>
-        <h2>Proceeds Receiver</h2>
-        <Link href={`https://optimism.blockscout.com/address/${data.proceedsReceiver}`} target='_blank'>
-          {data.proceedsReceiver}
-        </Link>
-      </Grid2>
+    <Box gap={1} display='flex' flexDirection='column'>
+      <Typography variant='h6'>{title}</Typography>
+      <Box sx={{ minHeight: '40px' }}>{subtitle && <Typography variant='body2'>{subtitle}</Typography>}</Box>
+      <Link href={`https://optimism.blockscout.com/${linkType}/${address}`} target='_blank'>
+        {address}
+        <IconButton size='small' color='primary'>
+          <MdLaunch size='16px' />
+        </IconButton>
+      </Link>
+    </Box>
+  );
+}
 
-      <Grid2 size={{ xs: 12 }}>
-        <h2>Registered builders</h2>
-        <p>{data.totalSupply.toString()}</p>
+function SectionTitle({ title }: { title: string }) {
+  return (
+    <Typography variant='h5' fontWeight='bold'>
+      {title}
+    </Typography>
+  );
+}
+
+function GridDivider() {
+  return (
+    <Grid2 size={12}>
+      <Divider />
+    </Grid2>
+  );
+}
+
+export function ContractDashboard(data: BuilderNFTContractData) {
+  const itemSize = { xs: 12, md: 6 };
+
+  return (
+    <Grid2 container spacing={2} px={6}>
+      <Grid2 size={12}>
+        <SectionTitle title='Contract Addresses' />
+      </Grid2>
+      <Grid2 size={itemSize}>
+        <ContractLink
+          address={data.contractAddress}
+          title='Proxy address'
+          linkType='token'
+          subtitle='Season-long contract holding the data about the minted NFTs, which delegates minting to an implementation contract.'
+        />
+      </Grid2>
+      <Grid2 size={itemSize}>
+        <ContractLink
+          address={data.currentImplementation}
+          title='Current Implementation'
+          subtitle='This contract is called by the proxy and handles the minting logic. We upgrade to a new implementation multiple times over the season.'
+        />
+      </Grid2>
+      <GridDivider />
+      <Grid2 size={12}>
+        <SectionTitle title='Data' />
+      </Grid2>
+      <Grid2 size={{ xs: 12, md: 4 }}>
+        <Typography variant='h6' fontWeight='bold'>
+          Registered builder NFTs
+        </Typography>
+        <Typography variant='body1' fontWeight='bold'>
+          {data.totalSupply.toString()}
+        </Typography>
+      </Grid2>
+      <Grid2 size={{ xs: 12, md: 4 }}>
+        {/* Currently, this is the balance of the proceeds receiver wallet. Once we start moving funds, we should look at logs instead */}
+        <Typography variant='h6' fontWeight='bold'>
+          Sales
+        </Typography>
+        <Typography variant='body1' fontWeight='bold'>
+          {Number(data.receiverUsdcBalance).toLocaleString('en-US')} USD
+        </Typography>
+      </Grid2>
+      <GridDivider />
+      <Grid2 size={12}>
+        <SectionTitle title='Roles & Permissions' />
+      </Grid2>
+      <Grid2 size={itemSize}>
+        <ContractLink
+          address={data.currentAdmin}
+          title='Admin'
+          subtitle='Admin wallet can upgrade the contract, update the wallet that receives proceeds from NFT sales, modify pricing, register builders and mint tokens.'
+        />
+      </Grid2>
+      <Grid2 size={itemSize}>
+        <ContractLink
+          address={data.currentMinter}
+          title='Minter'
+          subtitle='Minter wallet can register new builder nfts and mint tokens to any address.'
+        />
+      </Grid2>
+      <Grid2 size={itemSize}>
+        <ContractLink
+          address={data.proceedsReceiver}
+          title='Proceeds Receiver'
+          subtitle='This is the wallet address that receives funds paid to mint builder NFTs.'
+        />
       </Grid2>
     </Grid2>
   );
