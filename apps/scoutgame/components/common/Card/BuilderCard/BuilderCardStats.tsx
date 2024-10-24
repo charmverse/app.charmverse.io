4c87c3f06802e04ff443fefe75bc80b24a589bf2
Safwan Shaheer <devorein00@gmail.com>
2024-10-24 20:35:52 +0600

    Show builder rank in card stats (#4884)
    * Show builder rank in card stats
    
    * Added tooltip for builder nft card stats
    
    * Fixed tooltip copy

--- apps/scoutgame/components/common/Card/BuilderCard/BuilderCardStats.tsx
+++ apps/scoutgame/components/common/Card/BuilderCard/BuilderCardStats.tsx
@@ -1,42 +1,47 @@
-import { Stack, Typography } from '@mui/material';
+import { Stack, Tooltip, Typography } from '@mui/material';
 import Image from 'next/image';
 
-import { GemsIcon, PointsIcon } from 'components/common/Icons';
+import { PointsIcon } from 'components/common/Icons';
 
 export function BuilderCardStats({
-  gemsCollected,
   builderPoints,
-  nftsSold
+  nftsSold,
+  rank
 }: {
-  gemsCollected?: number;
   builderPoints?: number;
   nftsSold?: number;
+  rank?: number;
 }) {
   return (
     <Stack flexDirection='row' alignItems='center' justifyContent='space-between' gap={1}>
       {typeof builderPoints === 'number' && (
-        <Stack flexDirection='row' gap={0.2} alignItems='center'>
-          <Typography variant='body2' component='span' color='green.main'>
-            {builderPoints}
-          </Typography>
-          <PointsIcon size={15} color='green' />
-        </Stack>
+        <Tooltip title='Total # of Scout Points earned this season to date'>
+          <Stack flexDirection='row' gap={0.2} alignItems='center'>
+            <Typography variant='body2' component='span' color='green.main'>
+              {builderPoints}
+            </Typography>
+            <PointsIcon size={15} color='green' />
+          </Stack>
+        </Tooltip>
       )}
-      {typeof gemsCollected === 'number' && (
-        <Stack flexDirection='row' gap={0.2} alignItems='center'>
-          <Typography variant='body2' component='span' color='text.secondary'>
-            {gemsCollected}
-          </Typography>
-          <GemsIcon size={15} />
-        </Stack>
+      {typeof rank === 'number' && (
+        <Tooltip title='Current week’s rank'>
+          <Stack flexDirection='row' gap={0.2} alignItems='center'>
+            <Typography variant='body2' component='span' color='text.secondary'>
+              #{rank}
+            </Typography>
+          </Stack>
+        </Tooltip>
       )}
       {typeof nftsSold === 'number' && (
-        <Stack flexDirection='row' gap={0.2} alignItems='center'>
-          <Typography variant='body2' component='span' color='orange.main'>
-            {nftsSold}
-          </Typography>
-          <Image width={12} height={12} src='/images/profile/icons/nft-orange-icon.svg' alt='Nfts' />
-        </Stack>
+        <Tooltip title='Total # of cards sold this season to date'>
+          <Stack flexDirection='row' gap={0.2} alignItems='center'>
+            <Typography variant='body2' component='span' color='orange.main'>
+              {nftsSold}
+            </Typography>
+            <Image width={12} height={12} src='/images/profile/icons/nft-orange-icon.svg' alt='Nfts' />
+          </Stack>
+        </Tooltip>
       )}
     </Stack>
   );
