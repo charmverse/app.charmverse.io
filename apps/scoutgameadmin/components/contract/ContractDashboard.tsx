965c9369d84d6e98ae2e056605de1cc766206b46
motechFR <mo@anyahq.co>
2024-10-25 19:27:54 +0300

    Add additional sales data

--- apps/scoutgameadmin/components/contract/ContractDashboard.tsx
+++ apps/scoutgameadmin/components/contract/ContractDashboard.tsx
@@ -46,14 +46,15 @@ function GridDivider() {
 }
 
 export function ContractDashboard(data: BuilderNFTContractData) {
-  const itemSize = { xs: 12, md: 6 };
+  const itemSizeTwoColumnMd = { xs: 12, md: 6 };
+  const itemSizeThreeColumnMd = { xs: 12, md: 4 };
 
   return (
     <Grid2 container spacing={2} px={6}>
       <Grid2 size={12}>
         <SectionTitle title='Contract Addresses' />
       </Grid2>
-      <Grid2 size={itemSize}>
+      <Grid2 size={itemSizeTwoColumnMd}>
         <ContractLink
           address={data.contractAddress}
           title='Proxy address'
@@ -61,7 +62,7 @@ export function ContractDashboard(data: BuilderNFTContractData) {
           subtitle='Season-long contract holding the data about the minted NFTs, which delegates minting to an implementation contract.'
         />
       </Grid2>
-      <Grid2 size={itemSize}>
+      <Grid2 size={itemSizeTwoColumnMd}>
         <ContractLink
           address={data.currentImplementation}
           title='Current Implementation'
@@ -72,38 +73,66 @@ export function ContractDashboard(data: BuilderNFTContractData) {
       <Grid2 size={12}>
         <SectionTitle title='Data' />
       </Grid2>
-      <Grid2 size={{ xs: 12, md: 4 }}>
+      <Grid2 size={itemSizeThreeColumnMd}>
         <Typography variant='h6'>Registered builder NFTs</Typography>
         <Typography variant='body1' fontWeight='bold'>
           {data.totalSupply.toString()}
         </Typography>
       </Grid2>
-      <Grid2 size={{ xs: 12, md: 4 }}>
+      <Grid2 size={itemSizeThreeColumnMd}>
         {/* Currently, this is the balance of the proceeds receiver wallet. Once we start moving funds, we should look at logs instead */}
         <Typography variant='h6'>Sales</Typography>
         <Typography variant='body1' fontWeight='bold'>
           {Number(data.receiverUsdcBalance).toLocaleString('en-US')} USD
         </Typography>
       </Grid2>
+      <Grid2 size={itemSizeThreeColumnMd}>
+        {/* Currently, this is the balance of the proceeds receiver wallet. Once we start moving funds, we should look at logs instead */}
+        <Typography variant='h6'>Unique NFT holders</Typography>
+        <Typography variant='body1' fontWeight='bold'>
+          {Number(data.nftSalesData.uniqueHolders).toLocaleString('en-US')}
+        </Typography>
+      </Grid2>
+      <Grid2 size={itemSizeThreeColumnMd}>
+        {/* Currently, this is the balance of the proceeds receiver wallet. Once we start moving funds, we should look at logs instead */}
+        <Typography variant='h6'>Total NFTs minted</Typography>
+        <Typography variant='body1' fontWeight='bold'>
+          {Number(data.nftSalesData.totalNftsSold).toLocaleString('en-US')}
+        </Typography>
+      </Grid2>
+      <Grid2 size={itemSizeThreeColumnMd}>
+        {/* Currently, this is the balance of the proceeds receiver wallet. Once we start moving funds, we should look at logs instead */}
+        <Typography variant='h6'>NFTs paid with points</Typography>
+        <Typography variant='body1' fontWeight='bold'>
+          {Number(data.nftSalesData.nftsPaidWithPoints).toLocaleString('en-US')}
+        </Typography>
+      </Grid2>
+      <Grid2 size={itemSizeThreeColumnMd}>
+        {/* Currently, this is the balance of the proceeds receiver wallet. Once we start moving funds, we should look at logs instead */}
+        <Typography variant='h6'>NFTs paid with crypto</Typography>
+        <Typography variant='body1' fontWeight='bold'>
+          {Number(data.nftSalesData.nftsPaidWithCrypto).toLocaleString('en-US')}
+        </Typography>
+      </Grid2>
       <GridDivider />
       <Grid2 size={12}>
         <SectionTitle title='Roles & Permissions' />
       </Grid2>
-      <Grid2 size={itemSize}>
+      <Grid2 size={itemSizeTwoColumnMd}>
         <ContractLink
           address={data.currentAdmin}
           title='Admin'
           subtitle='Admin wallet can upgrade the contract, update the wallet that receives proceeds from NFT sales, modify pricing, register builders and mint tokens.'
         />
       </Grid2>
-      <Grid2 size={itemSize}>
+      <Grid2 size={itemSizeTwoColumnMd}>
         <ContractLink
           address={data.currentMinter}
           title='Minter'
           subtitle='Minter wallet can register new builder nfts and mint tokens to any address.'
         />
       </Grid2>
-      <Grid2 size={itemSize}>
+      <Grid2 size={itemSizeTwoColumnMd}>
         <ContractLink
           address={data.proceedsReceiver}
           title='Proceeds Receiver'
