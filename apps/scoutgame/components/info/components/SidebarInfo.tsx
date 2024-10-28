7af4708d09b2f6e0c78a569293dc19ab79511290
mattcasey <mattwad@gmail.com>
2024-10-28 11:52:40 -0600

    add how it works page for lit protocol

--- apps/scoutgame/components/info/components/SidebarInfo.tsx
+++ apps/scoutgame/components/info/components/SidebarInfo.tsx
@@ -23,7 +23,8 @@ const partners = [
   { href: '/info/partner-rewards/celo', text: 'Celo' },
   { href: '/info/partner-rewards/game7', text: 'Game7' },
   { href: '/info/partner-rewards/moxie', text: 'Moxie' },
-  { href: '/info/partner-rewards/bountycaster', text: 'BountyCaster' }
+  { href: '/info/partner-rewards/bountycaster', text: 'BountyCaster' },
+  { href: '/info/partner-rewards/lit', text: 'Lit Protocol' }
 ];
 
 export function SidebarInfo({ handleClose }: Readonly<{ handleClose?: () => void }>) {
