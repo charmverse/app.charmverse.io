5965f77fd4dff797c107433d90a8c051ab6aa8d1
mattcasey <mattwad@gmail.com>
2024-10-31 15:19:14 -0600

    add approve builder in the callback

--- apps/scoutgameadmin/lib/users/createBuilder.ts
+++ apps/scoutgameadmin/lib/users/createBuilder.ts
@@ -1,4 +1,6 @@
 import { prisma } from '@charmverse/core/prisma-client';
+import { octokit } from '@packages/github/client';
+import { approveBuilder } from '@packages/scoutgame/builders/approveBuilder';
 
 export type CreateBuilderParams = {
   userId: string;
@@ -17,4 +19,32 @@ export async function createBuilder(params: CreateBuilderParams) {
   if (!scout.githubUser && !params.githubLogin) {
     throw new Error('Github login is required');
   }
+  if (params.githubLogin) {
+    const githubUser = await octokit.rest.users.getByUsername({ username: params.githubLogin });
+    const githubUserDB = await prisma.githubUser.findUnique({
+      where: {
+        id: githubUser.data.id
+      }
+    });
+    if (githubUserDB) {
+      throw new Error('Github user already exists');
+    }
+
+    await prisma.githubUser.upsert({
+      where: {
+        id: githubUser.data.id
+      },
+      update: {
+        builderId: scout.id
+      },
+      create: {
+        id: githubUser.data.id,
+        login: params.githubLogin,
+        displayName: githubUser.data.name,
+        email: githubUser.data.email,
+        builderId: scout.id
+      }
+    });
+  }
+  await approveBuilder({ builderId: scout.id });
 }
