c350ff10775e6dc539cc85715525e29c0e29bfca
mattcasey <mattwad@gmail.com>
2024-11-01 10:29:10 -0600

    remove refresh logic

--- apps/scoutgame/hooks/api/session.ts
+++ apps/scoutgame/hooks/api/session.ts
@@ -5,10 +5,6 @@ import type { SessionUser } from 'lib/session/getUserFromSession';
 
 import { useGETImmutable, useGETtrigger } from './helpers';
 
-export function useRefreshUserProfiles() {
-  return useGETImmutable<[]>('/api/session/refresh');
-}
-
 export function useGetUser() {
   return useGETImmutable<SessionUser | null>('/api/session/user');
 }
