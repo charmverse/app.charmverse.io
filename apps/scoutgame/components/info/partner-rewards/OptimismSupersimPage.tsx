744a9a1856c57fe980b5dda55bb0ee0a48674aba
mattcasey <mattwad@gmail.com>
2024-11-04 14:50:11 -0700

    changes to supersim copy

--- apps/scoutgame/components/info/partner-rewards/OptimismSupersimPage.tsx
+++ apps/scoutgame/components/info/partner-rewards/OptimismSupersimPage.tsx
@@ -1,4 +1,5 @@
 import {
+  Divider,
   Link,
   Paper,
   Table,
@@ -39,12 +40,15 @@ function Document() {
         </Link>{' '}
         repo!
       </Typography>
+      <Typography>Supersim is an open source tool: The Local Multi-L2 Development Environment.</Typography>
       <Typography>
-        Supersim is a lightweight tool, being built in the open, to simulate the Superchain locally. Supersim allows
-        developers to spin up multiple local nodes with one command as well as to simulate op-stack functionality. The
-        multichain development workflow brings complexity and new challenges, but Supersim helps developers build out
-        their ideas faster.
+        Experiment with Superchain Interop using Supersim, the lightweight tool to simulate the Superchain locally.
       </Typography>
+      <Typography>
+        With Supersim, you can simulate entire cross-chain application flows, intricate message passing infrastructure,
+        and OP Stack specific logic with one command.
+      </Typography>
+      <Typography>Locally observe cross-chain interactions and iterate on multi-chain apps.</Typography>
       <Typography variant='h6' color='secondary' mt={2}>
         How it works
       </Typography>
@@ -68,57 +72,59 @@ function Document() {
       </Typography>
       <Typography>
         For those more adventurous, earn 200 to 500 OP for merging a PR addressing a new issue. Create an issue in the
-        repository detailing your proposed contribution before starting work. OP will respond to new issues and let the
-        you know if a PR would be accepted! Here are high priority additional desired features:
+        repository detailing your proposed contribution before starting work. The contribution must be substantial, more
+        than typos/syntax fixes. OP will respond to new issues and then let you know if a PR would be accepted! Here are
+        high priority additional desired features:
       </Typography>
       <List>
         <ListItem>Additional examples (see /examples/tic-tac-toe) showcasing projects to build with Supersim</ListItem>
         <ListItem>Improvements and additions to documentation</ListItem>
         <ListItem>
-          Additional integrations (see /integrations/hyperland) showcasing services that can be run locally alongside
+          Additional integrations (see /integrations/hyperlane) showcasing services that can be run locally alongside
           Supersim to provide an even better local development experience
         </ListItem>
       </List>
-      <TableContainer component={Paper}>
-        <Table sx={{ '& th, & td': { px: 0 } }} aria-label='Celo tiers table'>
-          <TableHead>
-            <TableRow>
-              <TableCell>Issue</TableCell>
-              <TableCell align='right'>Reward</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody sx={{ '& td, & th': { border: 0 } }}>
-            <TableRow>
-              <TableCell>
-                <Link
-                  href='https://github.com/ethereum-optimism/supersim/issues?q=is%3Aopen+is%3Aissue+label%3A%22good+first+issue%22'
-                  target='_blank'
-                >
-                  good first issue
-                </Link>
-              </TableCell>
-              <TableCell align='right'>200 OP</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>
-                <Link
-                  href='https://github.com/ethereum-optimism/supersim/issues?q=is%3Aopen+is%3Aissue+label%3A%22help+wanted%22+'
-                  target='_blank'
-                >
-                  help wanted
-                </Link>
-              </TableCell>
-              <TableCell align='right'>200 OP</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>Create your own*</TableCell>
-              <TableCell align='right'>200 to 500 OP**</TableCell>
-            </TableRow>
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <Typography>*See list of high priority additional features above for inspiration.</Typography>
-      <Typography>**Optimism will determine the reward based on the complexity and priority of the issue.</Typography>
+      <Table sx={{ '& th, & td': { px: 0 } }} aria-label='Celo tiers table'>
+        <TableHead>
+          <TableRow>
+            <TableCell>Issue</TableCell>
+            <TableCell align='right'>Reward</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody sx={{ '& td, & th': { border: 0 } }}>
+          <TableRow>
+            <TableCell>
+              <Link
+                href='https://github.com/ethereum-optimism/supersim/issues?q=is%3Aopen+is%3Aissue+label%3A%22good+first+issue%22'
+                target='_blank'
+              >
+                good first issue
+              </Link>
+            </TableCell>
+            <TableCell align='right'>200 OP</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>
+              <Link
+                href='https://github.com/ethereum-optimism/supersim/issues?q=is%3Aopen+is%3Aissue+label%3A%22help+wanted%22+'
+                target='_blank'
+              >
+                help wanted
+              </Link>
+            </TableCell>
+            <TableCell align='right'>200 OP</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>Create your own*</TableCell>
+            <TableCell align='right'>200 to 500 OP**</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+      <Divider />
+      <Typography variant='caption'>* See list of high priority additional features above for inspiration.</Typography>
+      <Typography variant='caption'>
+        ** Optimism will determine the reward based on the complexity and priority of the issue.
+      </Typography>
     </InfoCard>
   );
 }
