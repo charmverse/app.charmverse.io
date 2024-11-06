5f5ccb06363a40d11de55a289d28a0b52d069119
Safwan Shaheer <devorein00@gmail.com>
2024-11-06 20:38:09 +0600

    Improved daily claim styling

--- apps/scoutgame/components/quests/components/DailyClaimGallery/DailyClaimCard.tsx
+++ apps/scoutgame/components/quests/components/DailyClaimGallery/DailyClaimCard.tsx
@@ -1,28 +1,60 @@
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 import { Stack, Typography } from '@mui/material';
+import { DateTime } from 'luxon';
 import Image from 'next/image';
 
 import type { DailyClaim } from 'lib/users/getDailyClaims';
 
+import { DailyClaimGift } from './DailyClaimGift';
+
 export function DailyClaimCard({ dailyClaim }: { dailyClaim: DailyClaim }) {
+  const todayDate = DateTime.now().startOf('day');
+  const claimDateDay = DateTime.fromJSDate(dailyClaim.date.toJSDate(), { zone: 'utc' }).startOf('day');
+  const isClaimToday = claimDateDay.equals(todayDate);
+  const isPastDate = todayDate > claimDateDay;
+  const buttonLabel = isClaimToday ? 'Claim' : `Day ${dailyClaim.day}`;
+  const isClaimed = dailyClaim.claimed;
+
   return (
     <Stack
       sx={{
-        backgroundColor: 'background.paper',
-        width: '100%',
+        backgroundColor: isClaimed
+          ? 'background.paper'
+          : isPastDate
+            ? 'background.dark'
+            : isClaimToday
+              ? 'secondary.main'
+              : 'primary.dark',
         height: 100,
+        paddingBottom: 0.25,
         borderRadius: 1,
         alignItems: 'center',
         position: 'relative'
       }}
     >
-      <Stack direction='row' justifyContent='center' alignItems='center' gap={0.5} flex={1}>
-        <Typography variant='h5' fontWeight={600}>
-          +1
-        </Typography>
-        <Image src='/images/profile/scout-game-profile-icon.png' alt='Scout game icon' width={22.5} height={13.5} />
+      <Stack flex={1} position='relative' alignItems='center' justifyContent='center' width='100%'>
+        {!isClaimed ? (
+          <Stack sx={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}>
+            <DailyClaimGift variant={isPastDate ? 'disabled' : isClaimToday ? 'secondary' : 'primary'} size={68} />
+          </Stack>
+        ) : (
+          <CheckCircleIcon
+            fontSize='small'
+            color='secondary'
+            sx={{
+              position: 'absolute',
+              top: 5,
+              right: 5
+            }}
+          />
+        )}
+        <Stack direction='row' gap={0.5} alignItems='center' zIndex={1} top={7.5} position='relative'>
+          <Typography fontWeight={600}>+1</Typography>
+          <Image src='/images/profile/scout-game-profile-icon.png' alt='Scout game icon' width={18} height={10} />
+        </Stack>
       </Stack>
-      <Typography variant='body2' position='absolute' bottom={10}>
-        Day {dailyClaim.day}
+      <Typography variant='body2' color={isClaimToday ? 'secondary.dark' : 'initial'} fontWeight={600}>
+        {buttonLabel}
       </Typography>
     </Stack>
   );
