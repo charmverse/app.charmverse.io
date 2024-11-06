38faabe24e49b9c7e461a53907ee10506dcebc2f
Safwan Shaheer <devorein00@gmail.com>
2024-11-06 18:11:54 +0600

    Autoplay hot builder carousel and updated hot builders query (#4958)
    * Autoplay hot builder carousel
    
    * Updated todays hot builder retrieval
    
    * Added tests for todays hot builders fn

--- apps/scoutgame/lib/builders/getTodaysHotBuilders.ts
+++ apps/scoutgame/lib/builders/getTodaysHotBuilders.ts
@@ -1,6 +1,5 @@
 import { prisma } from '@charmverse/core/prisma-client';
-import { currentSeason, getCurrentWeek } from '@packages/scoutgame/dates';
-import { isProdEnv } from '@root/config/constants';
+import { currentSeason, getCurrentWeek, getLastWeek } from '@packages/scoutgame/dates';
 
 import { BasicUserInfoSelect } from 'lib/users/queries';
 
@@ -8,170 +7,111 @@ import type { BuilderInfo } from './interfaces';
 
 export type Last7DaysGems = { date: string; gemsCount: number }[];
 
-const preselectedBuilderUsernames = [
-  'samkuhlmann',
-  'earth2travis',
-  'dan13ram.eth',
-  'andreitr.eth',
-  'stephancill',
-  'piesrtasty',
-  'thecreative.eth',
-  'gregfromstl',
-  'millzdoteth',
-  'icemonkey'
-];
+const userSelect = {
+  ...BasicUserInfoSelect,
+  userSeasonStats: {
+    where: {
+      season: currentSeason
+    },
+    select: {
+      pointsEarnedAsBuilder: true,
+      nftsSold: true
+    }
+  },
+  builderCardActivities: {
+    select: {
+      last7Days: true
+    }
+  },
+  userWeeklyStats: {
+    where: {
+      week: getCurrentWeek()
+    },
+    select: {
+      gemsCollected: true,
+      rank: true
+    }
+  },
+  builderNfts: {
+    where: {
+      season: currentSeason
+    },
+    select: {
+      currentPrice: true,
+      imageUrl: true
+    }
+  }
+};
 
 export async function getTodaysHotBuilders(): Promise<BuilderInfo[]> {
-  if (isProdEnv) {
-    const builders = await prisma.scout.findMany({
-      where: {
-        builderStatus: 'approved',
-        path: {
-          in: preselectedBuilderUsernames
-        }
-      },
-      select: {
-        ...BasicUserInfoSelect,
-        userSeasonStats: {
-          where: {
-            season: currentSeason
-          },
-          select: {
-            pointsEarnedAsBuilder: true,
-            nftsSold: true
-          }
-        },
-        builderCardActivities: {
-          select: {
-            last7Days: true
-          }
-        },
-        userWeeklyStats: {
-          where: {
-            week: getCurrentWeek()
-          },
-          select: {
-            rank: true
-          }
-        },
-        builderNfts: {
-          where: {
-            season: currentSeason
-          },
-          select: {
-            currentPrice: true,
-            imageUrl: true
-          }
-        }
-      }
-    });
-
-    return builders
-      .map((builder) => {
-        return {
-          id: builder.id,
-          path: builder.path,
-          displayName: builder.displayName,
-          builderPoints: builder.userSeasonStats[0]?.pointsEarnedAsBuilder || 0,
-          price: builder.builderNfts[0]?.currentPrice ?? 0,
-          nftImageUrl: builder.builderNfts[0]?.imageUrl,
-          nftsSold: builder.userSeasonStats[0]?.nftsSold || 0,
-          builderStatus: builder.builderStatus!,
-          rank: builder.userWeeklyStats[0]?.rank || -1,
-          last7DaysGems: ((builder.builderCardActivities[0]?.last7Days as unknown as Last7DaysGems) || [])
-            .map((gem) => gem.gemsCount)
-            .slice(-7)
-        };
-      })
-      .sort((a, b) => preselectedBuilderUsernames.indexOf(a.id) - preselectedBuilderUsernames.indexOf(b.id));
-  }
-
-  const builders = await prisma.userWeeklyStats.findMany({
+  const currentWeekBuilders = await prisma.userWeeklyStats.findMany({
     where: {
-      week: getCurrentWeek(),
       user: {
         builderStatus: 'approved',
-        builderNfts: {
+        userWeeklyStats: {
           some: {
-            season: currentSeason
+            week: getCurrentWeek(),
+            gemsCollected: {
+              gt: 0
+            }
           }
         }
-      }
+      },
+      week: getCurrentWeek()
     },
+    take: 3,
     orderBy: {
       rank: 'asc'
     },
-    take: 10,
     select: {
-      gemsCollected: true,
       user: {
-        select: {
-          ...BasicUserInfoSelect,
-          builderCardActivities: {
-            select: {
-              last7Days: true
-            }
-          },
-          userSeasonStats: {
-            where: {
-              season: currentSeason
-            },
-            select: {
-              pointsEarnedAsBuilder: true,
-              nftsSold: true
-            }
-          },
-          userWeeklyStats: {
-            where: {
-              week: getCurrentWeek()
-            },
-            select: {
-              rank: true
-            }
-          },
-          nftPurchaseEvents: {
-            where: {
-              builderNFT: {
-                season: currentSeason
-              }
-            },
-            distinct: ['scoutId'],
-            select: {
-              scoutId: true
-            }
-          },
-          builderNfts: {
-            where: {
-              season: currentSeason
-            },
-            select: {
-              currentPrice: true,
-              imageUrl: true
-            }
-          }
-        }
+        select: userSelect
       }
     }
   });
 
-  const mappedBuilders = builders.map((builder) => {
-    const user = builder.user;
+  const totalCurrentWeekBuilders = currentWeekBuilders.length;
+
+  const previousWeekBuilders = await prisma.userWeeklyStats.findMany({
+    where: {
+      user: {
+        id: {
+          notIn: currentWeekBuilders.map((builder) => builder.user.id)
+        },
+        builderStatus: 'approved'
+      },
+      week: getLastWeek()
+    },
+    orderBy: {
+      rank: 'asc'
+    },
+    take: 10 - totalCurrentWeekBuilders,
+    select: {
+      user: {
+        select: userSelect
+      }
+    }
+  });
+
+  const builders = [
+    ...currentWeekBuilders.map((builder) => builder.user),
+    ...previousWeekBuilders.map((builder) => builder.user)
+  ];
+
+  return builders.map((builder) => {
     return {
-      id: user.id,
-      path: user.path,
-      displayName: user.displayName,
-      builderPoints: user.userSeasonStats[0]?.pointsEarnedAsBuilder || 0,
-      price: user.builderNfts[0]?.currentPrice ?? 0,
-      nftImageUrl: user.builderNfts[0]?.imageUrl,
-      nftsSold: user.userSeasonStats[0]?.nftsSold || 0,
-      rank: user.userWeeklyStats[0]?.rank || -1,
-      scoutedBy: user.nftPurchaseEvents.length,
-      builderStatus: user.builderStatus!,
-      last7DaysGems: ((user.builderCardActivities[0]?.last7Days as unknown as Last7DaysGems) || [])
+      id: builder.id,
+      path: builder.path,
+      displayName: builder.displayName,
+      builderPoints: builder.userSeasonStats[0]?.pointsEarnedAsBuilder || 0,
+      price: builder.builderNfts[0]?.currentPrice ?? 0,
+      nftImageUrl: builder.builderNfts[0]?.imageUrl,
+      nftsSold: builder.userSeasonStats[0]?.nftsSold || 0,
+      builderStatus: builder.builderStatus!,
+      rank: builder.userWeeklyStats[0]?.rank || -1,
+      last7DaysGems: ((builder.builderCardActivities[0]?.last7Days as unknown as Last7DaysGems) || [])
         .map((gem) => gem.gemsCount)
         .slice(-7)
     };
   });
-
-  return mappedBuilders;
 }
