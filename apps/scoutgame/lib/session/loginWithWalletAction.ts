01c162549adc14f433a198b085fc30e1277bb729
Safwan Shaheer <devorein00@gmail.com>
2024-11-07 22:09:52 +0600

    Added invite code for wallet login (#4965)

--- apps/scoutgame/lib/session/loginWithWalletAction.ts
+++ apps/scoutgame/lib/session/loginWithWalletAction.ts
@@ -1,6 +1,10 @@
 'use server';
 
+import { log } from '@charmverse/core/log';
 import { trackUserAction } from '@packages/mixpanel/trackUserAction';
+import { authSecret } from '@root/config/constants';
+import { sealData } from 'iron-session';
+import { cookies } from 'next/headers';
 
 import { actionClient } from 'lib/actions/actionClient';
 import { findOrCreateWalletUser } from 'lib/blockchain/findOrCreateWalletUser';
@@ -16,8 +20,16 @@ export const loginWithWalletAction = actionClient
   .schema(loginWithWalletSchema)
   .action(async ({ ctx, parsedInput }) => {
     const newUserId = ctx.session.anonymousUserId;
-
     const { walletAddress } = await verifyWalletSignature(parsedInput);
+
+    if (parsedInput.inviteCode) {
+      cookies().set(
+        'invite-code',
+        await sealData({ inviteCode: parsedInput.inviteCode }, { password: authSecret as string })
+      );
+      log.info(`Builder logged in with invite code: ${parsedInput.inviteCode}`, { walletAddress });
+    }
+
     const user = await findOrCreateWalletUser({ wallet: walletAddress, newUserId });
     await saveSession(ctx, { scoutId: user.id });
     const sessionUser = (await getUserFromSession()) as SessionUser;
