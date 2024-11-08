fb85335830c4053b3056c71b8fc60e0bebe54246
Valentin L <ludu.valentin@gmail.com>
2024-11-08 20:03:24 +0200

    Update

--- apps/scoutgametelegram/app/api/session/user/route.ts
+++ apps/scoutgametelegram/app/api/session/user/route.ts
@@ -3,24 +3,8 @@ import { NextResponse } from 'next/server';
 
 import { getUserFromSession } from 'lib/session/getUserFromSession';
 
-const baseUrl = 'https://403f-85-204-75-2.ngrok-free.app';
-
 // This API Route is non-blocking and called on every page load. Use it to refresh things about the current user
-export async function GET(req: Request, res: Response) {
-  const cookieName = process.env.AUTH_COOKIE || '';
-  const authSecret = process.env.AUTH_SECRET || '';
-
-  const ironOptions: SessionOptions = {
-    cookieName,
-    password: authSecret,
-    cookieOptions: {
-      sameSite: 'none',
-      domain: new URL(baseUrl).hostname,
-      // secure: true should be used in production (HTTPS) but can't be used in development (HTTP)
-      secure: true
-    }
-  };
-
+export async function GET() {
   const user = await getUserFromSession();
 
   return NextResponse.json(user);
--- apps/scoutgametelegram/lib/session/getSession.ts
+++ apps/scoutgametelegram/lib/session/getSession.ts
@@ -5,10 +5,8 @@ import { cookies } from 'next/headers';
 
 import { getIronOptions } from './getIronOptions';
 
-const baseUrl = 'https://403f-85-204-75-2.ngrok-free.app';
-
 export async function getSession<T extends object = SessionData>(o?: SessionOptions) {
-  const options = getIronOptions({ domain: new URL(baseUrl).hostname, secure: true });
+  const options = getIronOptions();
 
   const session = await getIronSession<T>(cookies(), options);
 
