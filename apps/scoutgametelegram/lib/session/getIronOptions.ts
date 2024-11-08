eff44c037f09396ae3c096368cd9c31eb99b3fbc
Valentin L <ludu.valentin@gmail.com>
2024-11-08 23:21:44 +0200

    Revert unnecessary change

--- apps/scoutgametelegram/lib/session/getIronOptions.ts
+++ apps/scoutgametelegram/lib/session/getIronOptions.ts
@@ -1,12 +1,10 @@
-import { baseUrl, cookieName } from '@packages/utils/constants';
+import { baseUrl, cookieName, authSecret } from '@packages/utils/constants';
 import type { SessionOptions } from 'iron-session';
 
 export function getIronOptions({
   sameSite = 'lax',
   ...restOptions
 }: SessionOptions['cookieOptions'] = {}): SessionOptions {
-  const authSecret = process.env.AUTH_SECRET;
-
   if (!authSecret) {
     throw new Error('AUTH_SECRET is not defined');
   }
