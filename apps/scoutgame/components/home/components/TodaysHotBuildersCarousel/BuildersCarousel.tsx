365f3170619c0cce21ded18a0c1e192cdb3697b2
Matt Casey <mattwad@gmail.com>
2024-11-07 23:29:33 -0700

    Add info cards to home carousel (#4970)
    * add cards
    
    * add cards to carousel
    
    * .
    
    * add test

---
+++ apps/scoutgame/components/home/components/TodaysHotBuildersCarousel/BuildersCarousel.tsx
@@ -0,0 +1,44 @@
+'use client';
+
+import { BuilderCard } from 'components/common/Card/BuilderCard/BuilderCard';
+import { Carousel } from 'components/common/Carousel/Carousel';
+import { useIsMounted } from 'hooks/useIsMounted';
+import { useLgScreen, useMdScreen } from 'hooks/useMediaScreens';
+import type { BuilderInfo } from 'lib/builders/interfaces';
+
+import { AdCard } from './AdCard';
+
+export function BuildersCarousel({ builders }: { builders: BuilderInfo[] }) {
+  const isMdScreen = useMdScreen();
+  const isLgScreen = useLgScreen();
+  const size = isLgScreen ? 'large' : isMdScreen ? 'small' : 'x-small';
+  const isMounted = useIsMounted();
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
+    <Carousel>
+      {[
+        <AdCard
+          data-test='ad-card-optimism'
+          key='op-new-scout-ad'
+          size={size}
+          path='/info/partner-rewards/optimism'
+          src='/images/home/op-new-scout-ad.png'
+        />,
+        <AdCard
+          data-test='ad-card-moxie'
+          key='moxie-fan-reward-ad'
+          size={size}
+          path='/info/partner-rewards/moxie'
+          src='/images/home/moxie-fan-reward-ad.png'
+        />,
+        ...builders.map((builder) => (
+          <BuilderCard size={size} key={builder.id} builder={builder} showPurchaseButton showHotIcon />
+        ))
+      ]}
+    </Carousel>
+  );
+}
