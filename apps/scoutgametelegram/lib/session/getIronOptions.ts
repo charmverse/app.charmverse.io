d88fd97f5ab160582920adec1fa660b7b1e20884
Valentin L <ludu.valentin@gmail.com>
2024-11-11 13:28:29 +0200

    Try to fix imports

--- apps/scoutgametelegram/lib/session/getIronOptions.ts
+++ apps/scoutgametelegram/lib/session/getIronOptions.ts
@@ -1,16 +1,15 @@
-import { baseUrl, cookieName, authSecret } from '@packages/utils/constants';
 import type { SessionOptions } from 'iron-session';
 
 export function getIronOptions({
   sameSite = 'lax',
   ...restOptions
 }: SessionOptions['cookieOptions'] = {}): SessionOptions {
+  const baseUrl = process.env.DOMAIN;
+  const authSecret = process.env.AUTH_SECRET;
+  const cookieName = process.env.AUTH_COOKIE || 'scoutgame-tg-session';
+
   if (!authSecret) {
     throw new Error('AUTH_SECRET is not defined');
-  }
-
-  if (!cookieName) {
-    throw new Error('AUTH_COOKIE is not defined');
   }
 
   const ironOptions: SessionOptions = {
