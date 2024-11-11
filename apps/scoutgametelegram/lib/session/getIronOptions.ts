f045e17c2d749a9deba7beadc3c6546180427afa
Valentin L <ludu.valentin@gmail.com>
2024-11-11 21:45:52 +0200

    dont throw error for iron session options

--- apps/scoutgametelegram/lib/session/getIronOptions.ts
+++ apps/scoutgametelegram/lib/session/getIronOptions.ts
@@ -5,17 +5,9 @@ export function getIronOptions({
   sameSite = 'lax',
   ...restOptions
 }: SessionOptions['cookieOptions'] = {}): SessionOptions {
-  if (!authSecret) {
-    throw new Error('AUTH_SECRET is not defined');
-  }
-
-  if (!cookieName) {
-    throw new Error('AUTH_COOKIE is not defined');
-  }
-
   const ironOptions: SessionOptions = {
     cookieName,
-    password: authSecret,
+    password: authSecret || '',
     cookieOptions: {
       sameSite,
       // secure: true should be used in production (HTTPS) but can't be used in development (HTTP)
