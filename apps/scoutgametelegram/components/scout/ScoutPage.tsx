2783473f80f5d57145b317f183f03fede56c35f7
Safwan Shaheer <devorein00@gmail.com>
2024-11-14 21:06:06 +0600

    Added info modal

--- apps/scoutgametelegram/components/scout/ScoutPage.tsx
+++ apps/scoutgametelegram/components/scout/ScoutPage.tsx
@@ -1,10 +1,11 @@
-import { Box, Stack, Typography } from '@mui/material';
+import { Box, IconButton, Stack, Typography } from '@mui/material';
 import { LoadingTable } from '@packages/scoutgame-ui/components/claim/components/common/LoadingTable';
 import { HeaderMessage } from '@packages/scoutgame-ui/components/common/Header/HeaderMessage';
 import { LoadingCards } from '@packages/scoutgame-ui/components/common/Loading/LoadingCards';
 import { TodaysHotBuildersCarousel } from '@packages/scoutgame-ui/components/home/TodaysHotBuildersCarousel/TodaysHotBuildersCarousel';
 import { Suspense } from 'react';
 
+import { InfoModal } from './InfoModal';
 import { scoutTabs, ScoutTabsMenu } from './ScoutPageTable/components/ScoutTabsMenu';
 import { ScoutPageTable } from './ScoutPageTable/ScoutPageTable';
 
@@ -24,10 +25,13 @@ export function ScoutPage({ tab, order, sort }: { tab: string; order: string; so
         sx={{
           position: 'sticky',
           top: 0,
-          zIndex: 1000
+          zIndex: 1000,
+          display: 'relative',
+          width: '100%'
         }}
       >
         <ScoutTabsMenu tab={currentTab} />
+        <InfoModal builder={currentTab === 'builders'} />
       </Box>
       <Suspense key={currentTab} fallback={<LoadingTable />}>
         <ScoutPageTable tab={currentTab} order={order} sort={sort} />
