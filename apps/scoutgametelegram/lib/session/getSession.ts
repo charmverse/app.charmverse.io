d2ff320e839f66b2631a5546cccf8266ab3b1e0b
Valentin L <ludu.valentin@gmail.com>
2024-11-15 18:00:04 +0200

    Update exports and session

--- apps/scoutgametelegram/lib/session/getSession.ts
+++ apps/scoutgametelegram/lib/session/getSession.ts
@@ -1,7 +1,5 @@
 import { getSession as getInitialSession } from '@packages/scoutgame/session/getSession';
 
-const baseUrl = 'https://5afa-2a02-2f04-8212-300-693c-507e-16d1-6b98.ngrok-free.app';
-
 export async function getSession() {
-  return getInitialSession({ domain: new URL(baseUrl).hostname, secure: true, sameSite: 'none' });
+  return getInitialSession({ sameSite: 'none' });
 }
