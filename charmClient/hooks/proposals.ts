fa39ab401b0a2314ae51023a3bca2ea5d8d580a4
mattcasey <mattwad@gmail.com>
2024-11-16 00:40:05 -0700

    fix it

--- charmClient/hooks/proposals.ts
+++ charmClient/hooks/proposals.ts
@@ -129,12 +129,16 @@ export function useUpdateProposalFormFields({ proposalId }: { proposalId: string
 }
 
 export function useGetProposalFormFieldAnswers({ proposalId }: { proposalId: MaybeString }) {
-  return useGET<FieldAnswerInput[]>(proposalId ? `/api/proposals/${proposalId}/form/answers` : null, {
-    // disable refreshing answers while user is working on the form
-    revalidateOnFocus: false,
-    revalidateOnReconnect: false,
-    revalidateIfStale: true // refresh on mount, eg. navigating around
-  });
+  return useGET<FieldAnswerInput[]>(
+    proposalId ? `/api/proposals/${proposalId}/form/answers` : null,
+    {},
+    {
+      // disable refreshing answers while user is working on the form
+      revalidateOnFocus: false,
+      revalidateOnReconnect: false,
+      revalidateIfStale: true // refresh on mount, eg. navigating around
+    }
+  );
 }
 
 export function useUpdateProposalFormFieldAnswers({ proposalId }: { proposalId: MaybeString }) {
