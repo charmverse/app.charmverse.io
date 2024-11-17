464af5bd724d5729a424e0bbe0dece6af63164ad
Safwan Shaheer <devorein00@gmail.com>
2024-11-17 09:46:30 +0600

    Added profile dropdown menu

--- apps/scoutgametelegram/components/common/Header.tsx
+++ apps/scoutgametelegram/components/common/Header.tsx
@@ -1,13 +1,24 @@
 'use client';
 
 import InfoIcon from '@mui/icons-material/Info';
-import { Avatar, IconButton, Stack, Typography } from '@mui/material';
+import { Avatar, Box, Button, Menu, IconButton, MenuItem, Stack, Typography } from '@mui/material';
 import { useUser } from '@packages/scoutgame-ui/providers/UserProvider';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useState } from 'react';
+import type { MouseEvent } from 'react';
 
 export function Header() {
   const { user } = useUser();
+  const [anchorElUser, setAnchorElUser] = useState<null | HTMLElement>(null);
+
+  const handleOpenUserMenu = (event: MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
 
   return (
     <Stack position='static' flexDirection='row' justifyContent='space-between' alignItems='center' width='100%' p={1}>
@@ -28,31 +39,67 @@ export function Header() {
           </IconButton>
         </Link>
         {user ? (
-          <Stack
+          <Box
+            borderColor='secondary.main'
+            borderRadius='30px'
             sx={{
-              flexDirection: 'row',
-              alignItems: 'center',
-              gap: 1,
-              borderRadius: '30px',
               position: 'relative',
-              borderColor: 'secondary.main',
-              borderWidth: '2px',
-              borderStyle: 'solid',
-              pointerEvents: 'none'
+              '&::before': {
+                content: '""',
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                right: 0,
+                bottom: 0,
+                borderColor: 'secondary.main',
+                borderRadius: '28px',
+                borderWidth: '2px',
+                borderStyle: 'solid',
+                pointerEvents: 'none'
+              }
             }}
           >
-            <Typography fontSize='16px' sx={{ pl: 2 }} color='text.primary' data-test='user-points-balance'>
-              {user.currentBalance}
-            </Typography>
-            <Image
-              src='/images/scout-game-profile-icon.png'
-              width={18}
-              height={12}
-              alt='Scout Game points icon'
-              priority={true}
-            />
-            <Avatar src={user?.avatar || undefined} style={{ width: 30, height: 30 }} />
-          </Stack>
+            <Button
+              variant='text'
+              onClick={handleOpenUserMenu}
+              sx={{ p: 0, display: 'flex', alignItems: 'center', gap: 1 }}
+            >
+              <Typography fontSize='16px' sx={{ pl: 2 }} color='text.primary' data-test='user-points-balance'>
+                {user.currentBalance}
+              </Typography>
+              <Image
+                src='/images/profile/scout-game-icon.svg'
+                width={20}
+                height={20}
+                alt='Scout Game points icon'
+                priority={true}
+              />
+              <Avatar src={user?.avatar || undefined} />
+            </Button>
+            <Menu
+              sx={{ mt: 5 }}
+              slotProps={{
+                paper: { sx: { '.MuiList-root': { pb: 0 }, maxWidth: '250px' } }
+              }}
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right'
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right'
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+              onClick={handleCloseUserMenu}
+            >
+              <MenuItem>
+                <Link href='/profile'>{user.displayName}</Link>
+              </MenuItem>
+            </Menu>
+          </Box>
         ) : null}
       </Stack>
     </Stack>
