e35cf7dd17a549138186a6dc063846f61cc0886f
Safwan Shaheer <devorein00@gmail.com>
2024-11-17 09:11:41 +0600

    Added info page

--- apps/scoutgametelegram/app/(general)/layout.tsx
+++ apps/scoutgametelegram/app/(general)/layout.tsx
@@ -12,7 +12,9 @@ export default function Layout({
   return (
     <>
       <Box sx={{ minHeight: 'calc(100vh + 120px)', position: 'relative', overflow: 'auto', px: 1 }}>
-        <Header />
+        <Box sx={{ position: 'relative', zIndex: 1 }}>
+          <Header />
+        </Box>
         <InfoBackgroundImage />
         <Box sx={{ position: 'relative', zIndex: 1 }}>{children}</Box>
       </Box>
--- apps/scoutgametelegram/components/common/Header.tsx
+++ apps/scoutgametelegram/components/common/Header.tsx
@@ -1,8 +1,10 @@
 'use client';
 
-import { Avatar, Stack, Typography } from '@mui/material';
+import InfoIcon from '@mui/icons-material/Info';
+import { Avatar, IconButton, Stack, Typography } from '@mui/material';
 import { useUser } from '@packages/scoutgame-ui/providers/UserProvider';
 import Image from 'next/image';
+import Link from 'next/link';
 
 export function Header() {
   const { user } = useUser();
@@ -15,36 +17,42 @@ export function Header() {
         height={35}
         alt='Scout Game logo'
         priority={true}
-        style={{ verticalAlign: 'middle', zIndex: 2 }}
+        style={{ verticalAlign: 'middle' }}
       />
-      {user ? (
-        <Stack
-          sx={{
-            flexDirection: 'row',
-            alignItems: 'center',
-            gap: 1,
-            borderRadius: '30px',
-            zIndex: 2,
-            position: 'relative',
-            borderColor: 'secondary.main',
-            borderWidth: '2px',
-            borderStyle: 'solid',
-            pointerEvents: 'none'
-          }}
-        >
-          <Typography fontSize='16px' sx={{ pl: 2 }} color='text.primary' data-test='user-points-balance'>
-            {user.currentBalance}
-          </Typography>
-          <Image
-            src='/images/scout-game-profile-icon.png'
-            width={18}
-            height={12}
-            alt='Scout Game points icon'
-            priority={true}
-          />
-          <Avatar src={user?.avatar || undefined} style={{ width: 30, height: 30 }} />
-        </Stack>
-      ) : null}
+      <Stack flexDirection='row' alignItems='center'>
+        <Link href='/info'>
+          <IconButton>
+            <InfoIcon color='secondary' />
+          </IconButton>
+        </Link>
+        {user ? (
+          <Stack
+            sx={{
+              flexDirection: 'row',
+              alignItems: 'center',
+              gap: 1,
+              borderRadius: '30px',
+              position: 'relative',
+              borderColor: 'secondary.main',
+              borderWidth: '2px',
+              borderStyle: 'solid',
+              pointerEvents: 'none'
+            }}
+          >
+            <Typography fontSize='16px' sx={{ pl: 2 }} color='text.primary' data-test='user-points-balance'>
+              {user.currentBalance}
+            </Typography>
+            <Image
+              src='/images/scout-game-profile-icon.png'
+              width={18}
+              height={12}
+              alt='Scout Game points icon'
+              priority={true}
+            />
+            <Avatar src={user?.avatar || undefined} style={{ width: 30, height: 30 }} />
+          </Stack>
+        ) : null}
+      </Stack>
     </Stack>
   );
 }
