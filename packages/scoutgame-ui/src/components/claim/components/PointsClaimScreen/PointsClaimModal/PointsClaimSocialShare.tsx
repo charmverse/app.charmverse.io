93b1e002205fdba9cb0897fe573f2b72ee3d8fca
Safwan Shaheer <devorein00@gmail.com>
2024-11-18 08:31:51 +0600

    Fixed x claim screen preview card (#5029)
    * Fixed mobile screen issues
    
    * Added support for x card preview
    
    * use scoutgame url
    
    * use user path

--- packages/scoutgame-ui/src/components/claim/components/PointsClaimScreen/PointsClaimModal/PointsClaimSocialShare.tsx
+++ packages/scoutgame-ui/src/components/claim/components/PointsClaimScreen/PointsClaimModal/PointsClaimSocialShare.tsx
@@ -1,6 +1,7 @@
 import { Typography, Stack, IconButton } from '@mui/material';
-import { getLastWeek } from '@packages/scoutgame/dates';
 import Image from 'next/image';
+
+import { useMdScreen } from '../../../../../hooks/useMediaScreens';
 
 export function PointsClaimSocialShare({
   builderPoints,
@@ -15,12 +16,13 @@ export function PointsClaimSocialShare({
   scoutPoints: number;
   builders: string[];
 }) {
-  const imageUrl = `https://cdn.charmverse.io/points-claim/${userId}/${getLastWeek()}.png`;
+  const isMd = useMdScreen();
+  const imageUrl = `https://scoutgame.xyz/points-claim/${userPath}`;
   const shareMessage = builderPoints
-    ? `I scored ${builderPoints} Scout Points this week as a Top Builder! Discover my work and scout me to see what I'm building next!\nMy profile: https://soutgame.xyz/u/${userPath}\n\n`
+    ? `I scored ${builderPoints} Scout Points this week as a Top Builder! Discover my work and scout me to see what I'm building next!\nMy profile: https://scoutgame.xyz/u/${userPath}\n\n`
     : `I scored ${scoutPoints} Scout Points this week as a Top Scout! Big shoutout to my top Builders: ${builders.join(
         ', '
-      )}. Who will be next?\nMy profile: https://soutgame.xyz/u/${userPath}\n\n`;
+      )}. Who will be next?\nMy profile: https://scoutgame.xyz/u/${userPath}\n\n`;
 
   const handleShare = (platform: 'x' | 'telegram' | 'warpcast') => {
     const urls = {
@@ -31,27 +33,32 @@ export function PointsClaimSocialShare({
     window.open(urls[platform], '_blank');
   };
 
+  const size = !isMd ? 30 : 42.5;
+
   return (
     <Stack
       sx={{
         justifyContent: 'center',
-        p: 2,
+        p: {
+          xs: 1,
+          md: 2
+        },
         alignItems: 'center',
         backgroundColor: '#D8E1FF'
       }}
     >
-      <Typography variant='h6' color='#000' fontWeight='bold'>
+      <Typography variant={isMd ? 'h6' : 'subtitle1'} color='#000' fontWeight='bold'>
         Share your win!
       </Typography>
       <Stack flexDirection='row' justifyContent='center'>
         <IconButton onClick={() => handleShare('x')}>
-          <Image src='/images/logos/x.png' alt='X' width={42.5} height={42.5} />
+          <Image src='/images/logos/x.png' alt='X' width={size} height={size} />
         </IconButton>
         <IconButton onClick={() => handleShare('telegram')}>
-          <Image src='/images/logos/telegram.png' alt='Telegram' width={42.5} height={42.5} />
+          <Image src='/images/logos/telegram.png' alt='Telegram' width={size} height={size} />
         </IconButton>
         <IconButton onClick={() => handleShare('warpcast')}>
-          <Image src='/images/logos/warpcast.png' alt='Warpcast' width={42.5} height={42.5} />
+          <Image src='/images/logos/warpcast.png' alt='Warpcast' width={size} height={size} />
         </IconButton>
       </Stack>
     </Stack>
