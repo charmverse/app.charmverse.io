cfe964f6e49e227f5dd3ab29374d86179185ce5c
Safwan Shaheer <devorein00@gmail.com>
2024-11-18 08:18:18 +0600

    Added support for x card preview

--- apps/scoutgame/app/(general)/points-claim/[path]/page.tsx
+++ apps/scoutgame/app/(general)/points-claim/[path]/page.tsx
@@ -1,5 +1,6 @@
 import { getLastWeek } from '@packages/scoutgame/dates';
 import { getUserByPath } from '@packages/scoutgame/users/getUserByPath';
+import { baseUrl } from '@packages/utils/constants';
 import { notFound } from 'next/navigation';
 
 export default async function Claim({ params }: { params: { path: string } }) {
@@ -23,6 +24,19 @@ export default async function Claim({ params }: { params: { path: string } }) {
       <meta name='fc:frame:button:2' content='Play now' />
       <meta name='fc:frame:button:2:action' content='link' />
       <meta name='fc:frame:button:2:target' content={process.env.DOMAIN} />
+
+      <meta name='og:image' content={claimScreenUrl} />
+      <meta name='og:image:width' content='600' />
+      <meta name='og:image:height' content='600' />
+      <meta name='og:url' content={`${baseUrl}/points-claim/${params.path}`} />
+      <meta name='og:title' content={`Scout Game - ${user.displayName}`} />
+      <meta name='og:description' content={`Points claim screen for ${user.displayName}`} />
+
+      <meta name='twitter:card' content='summary_large_image' />
+      <meta name='twitter:title' content={`Scout Game - ${user.displayName}`} />
+      <meta name='twitter:description' content={`Points claim screen for ${user.displayName}`} />
+      <meta name='twitter:image' content={claimScreenUrl} />
+      <meta name='twitter:url' content={`${baseUrl}/points-claim/${params.path}`} />
     </>
   );
 }
