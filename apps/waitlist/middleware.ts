4cc40536798706930755e301119270987d4a3980
motechFR <mo@anyahq.co>
2024-11-19 14:37:38 +0200

    Add middleware to redirect frames

--- apps/waitlist/middleware.ts
+++ apps/waitlist/middleware.ts
@@ -2,9 +2,16 @@ import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
 
 export async function middleware(request: NextRequest) {
+  if (request.nextUrl.pathname.startsWith('/api/frame')) {
+    return NextResponse.redirect(
+      new URL(`https://scoutgame.xyz/${request.nextUrl.pathname}${request.nextUrl.search}`),
+      302
+    );
+  }
+
   return NextResponse.redirect(new URL('https://scoutgame.xyz'));
 }
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|images|favicon.ico|robots.txt|__ENV.js|manifest.webmanifest).*)']
+  matcher: ['/((?!_next/static|_next/image|images|favicon.ico|robots.txt|__ENV.js|manifest.webmanifest).*)']
 };
