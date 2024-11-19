fdf41f5c277f37147dd8a432febc2a215f4c49b6
mattcasey <mattwad@gmail.com>
2024-11-19 13:51:39 -0700

    add select statements to places where we query the scoutg

--- apps/scoutgameadmin/lib/session/getAdminUser.ts
+++ apps/scoutgameadmin/lib/session/getAdminUser.ts
@@ -4,7 +4,7 @@ import { isProdEnv } from '@root/config/constants';
 const whitelistedIds: number[] = [472, 4339, 4356, 1212, 318061, 10921, 828888];
 
 export async function getAdminUser({ fid }: { fid: number }) {
-  const user = await prisma.scout.findFirstOrThrow({ where: { farcasterId: fid } });
+  const user = await prisma.scout.findFirstOrThrow({ where: { farcasterId: fid }, select: { id: true } });
   if (!isProdEnv) {
     return user;
   }
