cee3a2a47935654fa2ac18ee3da83b154ed34763
mattcasey <mattwad@gmail.com>
2024-11-19 12:34:53 -0700

    updae script to pull top scouts and builders

--- apps/scoutgame/scripts/query.ts
+++ apps/scoutgame/scripts/query.ts
@@ -2,15 +2,20 @@ import { prisma } from '@charmverse/core/prisma-client';
 
 import { getLeaderboard } from 'lib/builders/getLeaderboard';
 import { getTopScoutsByWeek } from 'lib/scouts/getTopScouts';
+import { getWeekStartEndFormatted, getDateFromISOWeek } from '@packages/scoutgame/dates';
 async function query() {
   // const existingAccounts = await getUserByPath('thescoho');
   // console.log(existingAccounts);
-  const builders = await getLeaderboard({ week: '2024-W43' });
+  const week = '2024-W46';
+
+  console.log('Retrieving leaderboard for week:', getWeekStartEndFormatted(getDateFromISOWeek(week).toJSDate()));
+
+  const builders = await getLeaderboard({ week: week });
   console.log('Top Builders');
   console.log(
     builders.map((b, index) => `${index + 1}. https://scoutgame.xyz/u/${b.path} (${b.gemsCollected} gems)`).join('\n')
   );
-  const scouts = await getTopScoutsByWeek({ week: '2024-W45' });
+  const scouts = await getTopScoutsByWeek({ week: week });
   console.log('Top Scouts');
   console.log(
     scouts.map((s, index) => `${index + 1}. https://scoutgame.xyz/u/${s.path}  (${s.pointsEarned} points)`).join('\n')
