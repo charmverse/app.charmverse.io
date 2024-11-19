0ea011aca49e0390c1066fe20ca725fe53b3c589
motechFR <mo@anyahq.co>
2024-11-19 16:03:40 +0200

    Fix button

--- apps/waitlist/components/common/JoinWaitlistButton.tsx
+++ apps/waitlist/components/common/JoinWaitlistButton.tsx
@@ -5,23 +5,12 @@ import { Button } from '@mui/material';
 import { useRouter } from 'next/navigation';
 import { useAction } from 'next-safe-action/hooks';
 
-import { joinWaitlistAction } from 'lib/waitlistSlots/joinWaitlistAction';
-
 export default function JoinWaitlistButton() {
   const router = useRouter();
 
-  const { execute: joinWaitlist } = useAction(joinWaitlistAction, {
-    onSuccess: async () => {
-      router.push('/score');
-    },
-    onError(err) {
-      log.error('Error on join waitlist', { error: err.error.serverError });
-    }
-  });
-
   return (
-    <Button color='primary' onClick={() => joinWaitlist()}>
-      Join the waitlist
+    <Button color='primary' disabled>
+      Waitlist Closed
     </Button>
   );
 }
