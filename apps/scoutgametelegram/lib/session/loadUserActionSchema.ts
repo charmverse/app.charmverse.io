e8fd8ff3e541264bec4ae179921bd55f6cac727f
Valentin L <ludu.valentin@gmail.com>
2024-11-20 13:15:08 +0200

    Create route for telegram init data

--- apps/scoutgametelegram/lib/session/loadUserActionSchema.ts
+++
@@ -1,7 +0,0 @@
-import * as yup from 'yup';
-
-export const userActionSchema = yup.object({
-  initData: yup.string().required()
-});
-
-export type UserActionSchema = yup.InferType<typeof userActionSchema>;
