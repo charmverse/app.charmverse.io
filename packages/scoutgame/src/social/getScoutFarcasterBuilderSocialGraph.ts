3a2c264e5473ed4acd9cbd3a029a4aeb3b435a4d
motechFR <mo@anyahq.co>
2024-11-21 15:58:18 +0200

    Add both methods

--- packages/scoutgame/src/social/getScoutFarcasterBuilderSocialGraph.ts
+++ packages/scoutgame/src/social/getScoutFarcasterBuilderSocialGraph.ts
@@ -1,5 +1,5 @@
 import { fetchQueryWithPagination, init } from '@airstack/node';
-import { gql } from '@apollo/client';
+import { InvalidInputError } from '@charmverse/core/errors';
 import { prisma } from '@charmverse/core/prisma-client';
 import { prettyPrint } from '@packages/utils/strings';
 
@@ -31,12 +31,6 @@ type SocialFollowersResponse = {
   SocialFollowers: {
     Follower: {
       followerProfileId: string;
-      followerAddress: {
-        socials: {
-          dappName: string;
-          profileName: string;
-        }[];
-      };
     }[];
     pageInfo: {
       nextCursor: string;
@@ -44,7 +38,7 @@ type SocialFollowersResponse = {
   };
 };
 
-async function getBuildersFollowingUser({ fid }: { fid: number }) {
+async function getBuildersFollowingUser({ fid }: { fid: number }): Promise<number[]> {
   const uniqueBuilderFids = await getBuildersWithFarcasterIds();
   // For debugging, replace by this line and reimport gql from '@apollo/client'
   // const query = gql`
@@ -65,12 +59,6 @@ async function getBuildersFollowingUser({ fid }: { fid: number }) {
       ) {
         Follower {
           followerProfileId
-          followerAddress {
-            socials(input: { filter: { dappName: { _eq: farcaster } } }) {
-              dappName
-              profileName
-            }
-          }
         }
         pageInfo {
         nextCursor
@@ -103,5 +91,81 @@ async function getBuildersFollowingUser({ fid }: { fid: number }) {
 
   await fetchData();
 
-  return records.map((record) => record.followerProfileId);
+  return records.map((record) => Number(record.followerProfileId));
+}
+
+type SocialFollowingResponse = {
+  SocialFollowings: {
+    Following: {
+      followingProfileId: string;
+    }[];
+    pageInfo: {
+      nextCursor: string;
+    };
+  };
+};
+
+async function getBuildersFollowedByUser({ fid }: { fid: number }): Promise<number[]> {
+  const uniqueBuilderFids = await getBuildersWithFarcasterIds();
+  // For debugging, replace by this line and reimport gql from '@apollo/client'
+  // const query = gql`
+  const query = `  
+    query GetUserFarcasterFollowings($fid: String!, $selectedFids: [String!], $cursor: String) {
+  SocialFollowings(
+    input: {blockchain: ALL, filter: {dappName: {_eq: farcaster}, followerProfileId: {_eq: $fid}, followingProfileId: {_in: $selectedFids}}, limit: 10, cursor: $cursor, order: {blockNumber: ASC}}
+  ) {
+    Following {
+      followingProfileId
+    }
+    pageInfo {
+      nextCursor
+    }
+  }
+}
+  `;
+
+  const records: SocialFollowingResponse['SocialFollowings']['Following'] = [];
+
+  async function fetchData(cursor?: string) {
+    const { data, hasNextPage } = await fetchQueryWithPagination(query, {
+      fid: fid.toString(),
+      selectedFids: uniqueBuilderFids.map(String),
+      cursor
+    });
+
+    const typedData = data as SocialFollowingResponse;
+
+    prettyPrint({ typedData });
+
+    if (typedData.SocialFollowings.Following) {
+      records.push(...typedData.SocialFollowings.Following);
+
+      if (hasNextPage && typedData.SocialFollowings.pageInfo.nextCursor) {
+        return fetchData(typedData.SocialFollowings.pageInfo.nextCursor);
+      }
+    }
+  }
+
+  await fetchData();
+
+  return records.map((record) => Number(record.followingProfileId));
+}
+
+export async function getScoutFarcasterBuilderSocialGraph({ fid }: { fid: number }): Promise<{
+  following: number[];
+  followers: number[];
+}> {
+  if (!fid) {
+    throw new InvalidInputError('fid is required');
+  }
+
+  const [following, followers] = await Promise.all([
+    getBuildersFollowedByUser({ fid }),
+    getBuildersFollowingUser({ fid })
+  ]);
+
+  return {
+    following,
+    followers
+  };
 }
