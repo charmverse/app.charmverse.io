fed88ad9b58d867338efe0d63d5d9daedbbe86ac
mattcasey <mattwad@gmail.com>
2024-11-22 00:03:19 -0700

    add script

--- scripts/migrations/2024_11_21_saveWallets.ts
+++ scripts/migrations/2024_11_21_saveWallets.ts
@@ -1,9 +1,42 @@
-// @ts-nocheck
 import { prisma } from '@charmverse/core/prisma-client';
 import { getFarcasterUserById } from '@packages/farcaster/getFarcasterUserById';
+import { getOnchainEvents } from '@packages/scoutgame/builderNfts/getOnchainPurchaseEvents';
+import { uniq } from 'lodash';
 
-// Migrate walletAddress from the scout table to the scoutWallet table
-async function query() {
+// Save user wallets based on Farcaster profile and NFT purchases
+async function syncNFTWallets() {
+  const users = await prisma.scout.findMany({
+    orderBy: {
+      createdAt: 'desc'
+    },
+    where: {
+      nftPurchaseEvents: {
+        some: {}
+      }
+    },
+    include: { scoutWallet: true }
+  });
+  console.log(`Found ${users.length} users with NFT purchases`);
+
+  const events = await getOnchainEvents();
+  console.log(`Found ${events.length} onchain events`);
+  return;
+  for (const user of users) {
+    const walletsFromOnchain = uniq(
+      events
+        .filter((event) => event.scoutId === user.id)
+        .map((event) => event.transferEvent.to)
+        .filter(Boolean)
+    );
+    const newWallets = walletsFromOnchain.filter(
+      (address) => !user.scoutWallet.some((w) => w.address === address.toLowerCase())
+    );
+
+    await saveUserWallets(user.id, newWallets);
+  }
+}
+
+async function syncFarcasterWallets() {
   const users = await prisma.scout.findMany({
     orderBy: {
       createdAt: 'desc'
@@ -15,31 +48,16 @@ async function query() {
     },
     include: { scoutWallet: true }
   });
+  console.log(`Found ${users.length} users with Farcaster profiles`);
 
   for (const user of users) {
-    const farcasterProfile = await getFarcasterUserById(user.farcasterId);
-    // console.log('farcasterProfile', farcasterProfile?.verifications);
+    const farcasterProfile = await getFarcasterUserById(user.farcasterId!);
     const newWallets = farcasterProfile?.verifications.filter(
-      (address) => !user.scoutWallet.some((w) => w.address === address)
+      (address) => !user.scoutWallet.some((w) => w.address === address.toLowerCase())
     );
-    if (newWallets.length > 0) {
-      const existing = await prisma.scoutWallet.findMany({
-        where: {
-          address: { in: newWallets.map((address) => address.toLowerCase()) }
-        }
-      });
-      if (existing.length > 0) {
-        // safe to ignore, but interesting to know if this happens
-        console.log('Wallet already exists', { user, existing, newWallets });
-      }
-      const newWalletsToCreate = newWallets.filter((address) => !existing.some((w) => w.address === address));
-      if (newWalletsToCreate.length > 0) {
-        // await prisma.scoutWallet.createMany({
-        //   data: newWalletsToCreate.map((address) => ({ address: address.toLowerCase(), scoutId: user.id }))
-        // });
-        console.log('Created wallets', { userId: user.id, newWalletsToCreate });
-      }
-    }
+
+    await saveUserWallets(user.id, newWallets);
+
     if (users.indexOf(user) % 10 === 0) {
       console.log(`Updated ${user.id} users. Latest createdAt: ${user.createdAt}`);
     }
@@ -48,4 +66,26 @@ async function query() {
   console.log(`Migrated ${users.length} users`);
 }
 
-query();
+async function saveUserWallets(userId: string, newWallets: string[]) {
+  if (newWallets.length > 0) {
+    const existing = await prisma.scoutWallet.findMany({
+      where: {
+        address: { in: newWallets.map((address) => address.toLowerCase()) }
+      }
+    });
+    if (existing.length > 0) {
+      // safe to ignore, but interesting to know if this happens
+      console.log('Wallet already exists', { userId, existing, newWallets });
+    }
+    const newWalletsToCreate = newWallets.filter((address) => !existing.some((w) => w.address === address));
+    if (newWalletsToCreate.length > 0) {
+      await prisma.scoutWallet.createMany({
+        data: newWalletsToCreate.map((address) => ({ address: address.toLowerCase(), scoutId: userId }))
+      });
+      console.log('Created wallets', { userId: userId, newWalletsToCreate });
+    }
+  }
+}
+
+//syncFarcasterWallets();
+syncNFTWallets();
