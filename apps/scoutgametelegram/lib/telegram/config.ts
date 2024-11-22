3edf9a18b493af7c1ee03bb9684e4b572d7dcbf6
Valentin <ludu.valentin@gmail.com>
2024-11-22 18:52:54 +0200

    Telegram redirects and startapp param improvement (#5062)

--- apps/scoutgametelegram/lib/telegram/config.ts
+++ apps/scoutgametelegram/lib/telegram/config.ts
@@ -1,1 +1,2 @@
 export const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
+export const telegramBotName = 'ScoutGameXYZBot';
--- packages/scoutgame/src/session/getUserFromSession.ts
+++ packages/scoutgame/src/session/getUserFromSession.ts
@@ -1,14 +1,18 @@
+import type { SessionOptions } from 'iron-session';
+
 import { getSession } from './getSession';
 import { cacheGetUser, getUser } from './getUser';
 import type { SessionUser } from './interfaces';
 
-export async function getUserFromSession(): Promise<SessionUser | null> {
-  const session = await getSession();
+export async function getUserFromSession(cookieOptions?: SessionOptions['cookieOptions']): Promise<SessionUser | null> {
+  const session = await getSession(cookieOptions);
   return getUser(session.scoutId);
 }
 
-export async function getCachedUserFromSession(): Promise<SessionUser | null> {
-  const session = await getSession();
+export async function getCachedUserFromSession(
+  cookieOptions?: SessionOptions['cookieOptions']
+): Promise<SessionUser | null> {
+  const session = await getSession(cookieOptions);
   const cached = cacheGetUser(session.scoutId);
 
   return cached;
