c54e8b3968df99f6fdc28b44060c6adca0599a46
mattcasey <mattwad@gmail.com>
2024-11-26 00:03:22 -0700

    add padding on mobile footer for ios

--- apps/scoutgametelegram/components/layout/StickyFooter.tsx
+++ apps/scoutgametelegram/components/layout/StickyFooter.tsx
@@ -1,11 +1,28 @@
 import { Box } from '@mui/material';
+import { styled } from '@mui/material/styles';
 
 import { SiteNavigation } from 'components/common/SiteNavigation';
 
+const StickyContainer = styled('div')`
+  position: sticky;
+  bottom: 0;
+  z-index: 1000;
+  width: 100%;
+  // add some padding for the bottom nav on mobile ios
+  // https://stackoverflow.com/questions/31541998/ios-mobile-safari-the-bottom-bar-covers-my-footer
+  @media screen and (max-width: 767px) {
+    _::-webkit-full-page-media,
+    _:future,
+    :root .safari_only {
+      padding-bottom: 20px;
+    }
+  }
+`;
+
 export function StickyFooter() {
   return (
-    <Box component='footer' position='sticky' bottom={0} zIndex={1000} width='100%'>
+    <StickyContainer>
       <SiteNavigation />
-    </Box>
+    </StickyContainer>
   );
 }
