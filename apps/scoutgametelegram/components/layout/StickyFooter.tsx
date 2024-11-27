f00f5af31fcfb9aef4c949c061c3690c510c57c1
mattcasey <mattwad@gmail.com>
2024-11-27 11:02:51 -0700

    ..

--- apps/scoutgametelegram/components/layout/StickyFooter.tsx
+++ apps/scoutgametelegram/components/layout/StickyFooter.tsx
@@ -1,44 +1,12 @@
-'use client';
-
-import { styled } from '@mui/material/styles';
-import { useState, useCallback, useEffect } from 'react';
+import { Box } from '@mui/material';
+import { getPlatform } from '@packages/mixpanel/utils';
 
 import { SiteNavigation } from 'components/common/SiteNavigation';
 
-const StickyContainer = styled('footer')`
-  position: sticky;
-  bottom: 0;
-  z-index: 1000;
-  width: 100%;
-`;
-
 export function StickyFooter() {
-  const [paddingBottom, setPaddingBottom] = useState(0);
-  const handleSafeAreaChanged = useCallback(() => {
-    // console.log('safeAreaChanged', this.safeAreaInset);
-    // console.log('safeAreaChanged', e);
-    // @ts-ignore
-    // eslint-disable-next-line react/no-this-in-sfc
-    setPaddingBottom(this.safeAreaInset.bottom);
-  }, [setPaddingBottom]);
-
-  // add some padding for the bottom nav on mobile ios
-  useEffect(() => {
-    // console.log('adding safeAreaChanged listener');
-    // @ts-ignore
-    if (window.Telegram?.WebApp) {
-      // @ts-ignore - https://core.telegram.org/bots/webapps#events-available-for-mini-apps
-      window.Telegram.WebApp.onEvent('safeAreaChanged', handleSafeAreaChanged);
-    }
-    return () => {
-      // @ts-ignore - https://core.telegram.org/bots/webapps#events-available-for-mini-apps
-      window.Telegram.WebApp.offEvent('safeAreaChanged', handleSafeAreaChanged);
-    };
-  }, [handleSafeAreaChanged]);
-
   return (
-    <StickyContainer style={{ paddingBottom }}>
-      <SiteNavigation />
-    </StickyContainer>
+    <Box component='footer' position='sticky' bottom={0} zIndex={1000} width='100%'>
+      <SiteNavigation isTelegram={getPlatform() === 'telegram'} />
+    </Box>
   );
 }
