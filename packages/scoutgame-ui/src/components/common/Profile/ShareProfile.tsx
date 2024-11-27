cfe26dd1232467cb86ac2e9da69cc308033f14f3
Safwan Shaheer <devorein00@gmail.com>
2024-11-27 19:09:46 +0600

    Use link component for share links

--- packages/scoutgame-ui/src/components/common/Profile/ShareProfile.tsx
+++ packages/scoutgame-ui/src/components/common/Profile/ShareProfile.tsx
@@ -5,6 +5,7 @@ import ShareIcon from '@mui/icons-material/Share';
 import { IconButton, Menu, MenuItem, Tooltip, Typography } from '@mui/material';
 import { baseUrl } from '@packages/utils/constants';
 import Image from 'next/image';
+import Link from 'next/link';
 import { useState } from 'react';
 
 export function ShareProfile({ userPath }: { userPath: string }) {
@@ -30,24 +31,6 @@ export function ShareProfile({ userPath }: { userPath: string }) {
     }, 500);
   };
 
-  const handleShareToWarpcast = () => {
-    handleCloseShareMenu();
-    const url = `https://warpcast.com/~/compose?text=${encodeURIComponent(shareMessage)}`;
-    window.open(url, '_blank');
-  };
-
-  const handleShareToX = () => {
-    handleCloseShareMenu();
-    const url = `https://x.com/intent/tweet?text=${encodeURIComponent(shareMessage)}`;
-    window.open(url, '_blank');
-  };
-
-  const handleShareToTelegram = () => {
-    handleCloseShareMenu();
-    const url = `https://t.me/share/url?url=${encodeURIComponent(shareMessage)}`;
-    window.open(url, '_blank');
-  };
-
   return (
     <>
       <Tooltip title='Share profile'>
@@ -61,30 +44,36 @@ export function ShareProfile({ userPath }: { userPath: string }) {
         onClose={handleCloseShareMenu}
         onClick={handleCloseShareMenu}
       >
-        <MenuItem onClick={handleShareToWarpcast}>
-          <Image
-            src='/images/profile/icons/warpcast-circle-icon.svg'
-            width={20}
-            height={20}
-            alt='warpcast icon'
-            style={{ marginRight: 8 }}
-          />
-          <Typography variant='body2'>Warpcast</Typography>
-        </MenuItem>
-        <MenuItem onClick={handleShareToX}>
-          <Image src='/images/logos/x.png' width={20} height={20} alt='x logo' style={{ marginRight: 8 }} />
-          <Typography variant='body2'>X</Typography>
-        </MenuItem>
-        <MenuItem onClick={handleShareToTelegram}>
-          <Image
-            src='/images/logos/telegram.png'
-            width={20}
-            height={20}
-            alt='telegram logo'
-            style={{ marginRight: 8 }}
-          />
-          <Typography variant='body2'>Telegram</Typography>
-        </MenuItem>
+        <Link href={`https://warpcast.com/~/compose?text=${encodeURIComponent(shareMessage)}`} target='_blank'>
+          <MenuItem onClick={handleCloseShareMenu}>
+            <Image
+              src='/images/profile/icons/warpcast-circle-icon.svg'
+              width={20}
+              height={20}
+              alt='warpcast icon'
+              style={{ marginRight: 8 }}
+            />
+            <Typography variant='body2'>Warpcast</Typography>
+          </MenuItem>
+        </Link>
+        <Link href={`https://x.com/intent/tweet?text=${encodeURIComponent(shareMessage)}`} target='_blank'>
+          <MenuItem onClick={handleCloseShareMenu}>
+            <Image src='/images/logos/x.png' width={20} height={20} alt='x logo' style={{ marginRight: 8 }} />
+            <Typography variant='body2'>X</Typography>
+          </MenuItem>
+        </Link>
+        <Link href={`https://t.me/share/url?url=${encodeURIComponent(shareMessage)}`} target='_blank'>
+          <MenuItem onClick={handleCloseShareMenu}>
+            <Image
+              src='/images/logos/telegram.png'
+              width={20}
+              height={20}
+              alt='telegram logo'
+              style={{ marginRight: 8 }}
+            />
+            <Typography variant='body2'>Telegram</Typography>
+          </MenuItem>
+        </Link>
         <MenuItem onClick={handleCopyLink} disabled={isCopied}>
           <FileCopyOutlinedIcon color='primary' fontSize='small' sx={{ mr: 1 }} />
           <Typography variant='body2'>{isCopied ? 'Copied!' : 'Copy link'}</Typography>
