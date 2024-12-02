6699b41675cbc62255957831024d36264bd53406
Safwan Shaheer <devorein00@gmail.com>
2024-12-02 18:24:44 +0600

    fixed signout e2e tests

--- apps/scoutgame/__e2e__/signOut.spec.ts
+++ apps/scoutgame/__e2e__/signOut.spec.ts
@@ -3,12 +3,12 @@ import { mockBuilder } from '@packages/scoutgame/testing/database';
 import { expect, test } from './test';
 
 test.describe('Sign Out', () => {
-  test('Should be able to sign out', async ({ homePage, utils, page }) => {
+  test('Should be able to sign out', async ({ utils, page }) => {
     const builder = await mockBuilder({});
 
     await utils.loginAsUserId(builder.id);
-    await page.goto('/home');
-    await page.waitForURL('**/home');
+    await page.goto('/scout');
+    await page.waitForURL('**/scout');
 
     const userPill = page.locator('data-test=user-menu-pill');
     await expect(userPill).toBeVisible();
