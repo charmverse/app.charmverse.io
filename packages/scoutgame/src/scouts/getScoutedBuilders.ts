8154c18609507e1bdee1d6723d8f8346a723d4eb
motechFR <mo@anyahq.co>
2024-12-06 01:33:34 +0100

    Dont return NFT with 0 purchases

--- packages/scoutgame/src/scouts/getScoutedBuilders.ts
+++ packages/scoutgame/src/scouts/getScoutedBuilders.ts
@@ -74,36 +74,43 @@ export async function getScoutedBuilders({ scoutId }: { scoutId: string }): Prom
   });
 
   return builders.flatMap((builder) => {
-    return builder.builderNfts.map((nft) => {
-      const nftsSoldData = nft.nftSoldEvents.reduce(
-        (acc, event) => {
-          acc.total += event.tokensPurchased;
-          if (event.scoutId === scoutId) {
-            acc.toScout += event.tokensPurchased;
-          }
-          return acc;
-        },
-        { total: 0, toScout: 0 }
-      );
-      const nftData = {
-        id: builder.id,
-        nftImageUrl: nft.imageUrl,
-        path: builder.path,
-        displayName: builder.displayName,
-        builderStatus: builder.builderStatus!,
-        builderPoints: builder.userSeasonStats[0]?.pointsEarnedAsBuilder ?? 0,
-        nftsSold: nftsSoldData.total,
-        nftsSoldToScout: nftsSoldData.toScout,
-        rank: builder.userWeeklyStats[0]?.rank ?? -1,
-        price: nft.currentPrice ?? 0,
-        last7DaysGems: ((builder.builderCardActivities[0]?.last7Days as unknown as Last7DaysGems) || [])
-          .map((gem) => gem.gemsCount)
-          .slice(-7),
-        nftType: nft.nftType || 'default',
-        congratsImageUrl: nft.congratsImageUrl
-      };
+    return builder.builderNfts
+      .map((nft) => {
+        const nftsSoldData = nft.nftSoldEvents.reduce(
+          (acc, event) => {
+            acc.total += event.tokensPurchased;
+            if (event.scoutId === scoutId) {
+              acc.toScout += event.tokensPurchased;
+            }
+            return acc;
+          },
+          { total: 0, toScout: 0 }
+        );
 
-      return nftData;
-    });
+        if (nftsSoldData.toScout === 0) {
+          return null;
+        }
+
+        const nftData: BuilderInfo = {
+          id: builder.id,
+          nftImageUrl: nft.imageUrl,
+          path: builder.path,
+          displayName: builder.displayName,
+          builderStatus: builder.builderStatus!,
+          builderPoints: builder.userSeasonStats[0]?.pointsEarnedAsBuilder ?? 0,
+          nftsSold: nftsSoldData.total,
+          nftsSoldToScout: nftsSoldData.toScout,
+          rank: builder.userWeeklyStats[0]?.rank ?? -1,
+          price: nft.currentPrice ?? 0,
+          last7DaysGems: ((builder.builderCardActivities[0]?.last7Days as unknown as Last7DaysGems) || [])
+            .map((gem) => gem.gemsCount)
+            .slice(-7),
+          nftType: nft.nftType || 'default',
+          congratsImageUrl: nft.congratsImageUrl
+        };
+
+        return nftData;
+      })
+      .filter((nft) => nft !== null) as BuilderInfo[];
   });
 }
