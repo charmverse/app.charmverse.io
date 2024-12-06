d20330e00c213019a7bf2635f346d48ad7aa194f
Valentin L <ludu.valentin@gmail.com>
2024-12-06 15:58:17 +0200

    Revert dev data

--- apps/scoutgametelegram/lib/session/getSession.ts
+++ apps/scoutgametelegram/lib/session/getSession.ts
@@ -1,5 +1,5 @@
 import { getSession as getSessionBase } from '@packages/scoutgame/session/getSession';
 
 export const getSession = () => {
-  return getSessionBase({ sameSite: 'none', secure: true, domain: 'ngrok.app' });
+  return getSessionBase({ sameSite: 'none' });
 };
