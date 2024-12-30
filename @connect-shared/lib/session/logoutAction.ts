cb0a9dc95b742dacc5f48432121ed838bc21831f
mattcasey <mattwad@gmail.com>
2024-12-30 13:46:32 -0700

    fix types

--- @connect-shared/lib/session/logoutAction.ts
+++ @connect-shared/lib/session/logoutAction.ts
@@ -5,7 +5,7 @@ import { log } from '@charmverse/core/log';
 import { actionClient } from '../actions/actionClient';
 
 export const logoutAction = actionClient.metadata({ actionName: 'logout' }).action(async ({ ctx }) => {
-  const userId = ctx.session.user?.id || ctx.session.scoutId;
+  const userId = ctx.session.user?.id;
   ctx.session.destroy();
   await ctx.session.save();
 
