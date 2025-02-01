2328c7de7d35ec34238551a6e57dc21d67836ee5
mattcasey <mattwad@gmail.com>
2025-01-31 18:28:34 -0700

    remove unused code that is broken types

--- connectors/config.ts
+++ connectors/config.ts
@@ -41,45 +41,45 @@ export const wagmiConfig = createConfig({
 // get wagmi config based on env - TODO: do we need a method? or just override the wagmiConfig export?
 export const getWagmiConfig = (): typeof wagmiConfig => {
   return wagmiConfig;
-  if (!isTestEnv) {
-    return wagmiConfig;
-  }
+  // if (!isTestEnv) {
+  //   return wagmiConfig;
+  // }
 
-  // use custom window.ethereum object in tests to be able to mock it
-  // https://github.com/DePayFi/web3-mock#viem--wagmi
-  // NOTE: we need window object first, to be able to use window.ethereum
-  if (typeof window !== 'undefined') {
-    // get the test wallet address set by the test runner or use a default one
-    const storedAccount = window.localStorage.getItem('charm.v1.testWalletAddress') as Address;
-    const account = storedAccount || '0x80c2AE072212ab96B7fa2fEE0efba986DC46C4e5';
+  // // use custom window.ethereum object in tests to be able to mock it
+  // // https://github.com/DePayFi/web3-mock#viem--wagmi
+  // // NOTE: we need window object first, to be able to use window.ethereum
+  // if (typeof window !== 'undefined') {
+  //   // get the test wallet address set by the test runner or use a default one
+  //   const storedAccount = window.localStorage.getItem('charm.v1.testWalletAddress') as Address;
+  //   const account = storedAccount || '0x80c2AE072212ab96B7fa2fEE0efba986DC46C4e5';
 
-    // use mocked window.ethereum when available or default to http provider
-    const transport =
-      typeof window.ethereum !== 'undefined' ? custom(window.ethereum) : http(mainnet.rpcUrls.default.http[0]);
+  //   // use mocked window.ethereum when available or default to http provider
+  //   const transport =
+  //     typeof window.ethereum !== 'undefined' ? custom(window.ethereum) : http(mainnet.rpcUrls.default.http[0]);
 
-    return createConfig({
-      // autoConnect: !!storedAccount,
-      chains: [mainnet],
-      connectors: [
-        mock({
-          // walletClient: createWalletClient({
-          //   chain: mainnet,
-          //   transport,
-          //   account
-          // }),
-          accounts: [account]
-          // flags: {
-          //   isAuthorized: true
-          // }
-        }),
-        injected({
-          shimDisconnect: true
-        })
-      ],
-      transports: {
-        [mainnet.id]: transport
-      }
-    });
-  }
-  return wagmiConfig;
+  //   return createConfig({
+  //     // autoConnect: !!storedAccount,
+  //     chains: [mainnet],
+  //     connectors: [
+  //       mock({
+  //         // walletClient: createWalletClient({
+  //         //   chain: mainnet,
+  //         //   transport,
+  //         //   account
+  //         // }),
+  //         accounts: [account]
+  //         // flags: {
+  //         //   isAuthorized: true
+  //         // }
+  //       }),
+  //       injected({
+  //         shimDisconnect: true
+  //       })
+  //     ],
+  //     transports: {
+  //       [mainnet.id]: transport
+  //     }
+  //   });
+  // }
+  // return wagmiConfig;
 };
