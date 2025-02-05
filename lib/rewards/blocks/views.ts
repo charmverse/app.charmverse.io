94af6d2c0259f5a0e99476615743a6a42fa23d46
Valentin <ludu.valentin@gmail.com>
2025-02-05 18:31:10 +0200

    Filter deleted fields in the milestone table (#5175)

--- lib/rewards/blocks/views.ts
+++ lib/rewards/blocks/views.ts
@@ -134,7 +134,8 @@ export function getProposalRewardsView({
   const view = getDefaultView({ spaceId });
 
   // all custom properties are visible by default
-  view.fields.visiblePropertyIds = (board.fields.cardProperties as { id: string }[])
+  view.fields.visiblePropertyIds = (board.fields.cardProperties as { id: string; deletedAt?: string }[])
+    .filter((p) => !p.deletedAt)
     .map((p) => p.id)
     .filter((id: string) => {
       if (rewardTypes.includes('custom') && (id === REWARD_CUSTOM_VALUE || id === DUE_DATE_ID)) {
