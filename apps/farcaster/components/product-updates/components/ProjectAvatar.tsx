5784aa05b9dc45d3fc970671b72b8adc441d7d4c
mattcasey <mattwad@gmail.com>
2025-05-07 14:03:02 -0600

    remove farcaster and sunnyawards

--- apps/farcaster/components/product-updates/components/ProjectAvatar.tsx
+++
@@ -1,5 +0,0 @@
-import { Avatar } from '@packages/connect-shared/components/common/Avatar';
-
-export function ProjectAvatar({ src }: { src?: string | null }) {
-  return <Avatar avatar={src || `/images/default-project-avatar.png`} size='small' />;
-}
