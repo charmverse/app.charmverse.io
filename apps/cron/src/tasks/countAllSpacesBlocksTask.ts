d4d3282a2e37eef1a36a6b078eefb3fe0dd12bbe
mattcasey <mattwad@gmail.com>
2025-05-08 22:28:38 -0600

    bring back cron

--- apps/cron/src/tasks/countAllSpacesBlocksTask.ts
+++ apps/cron/src/tasks/countAllSpacesBlocksTask.ts
@@ -4,33 +4,32 @@ import { deleteArchivalBlockCounts } from '@packages/spaces/deleteArchivalBlockC
 
 const perBatch = 1000;
 
-// TODO: bring back count space blocks
-// export async function countAllSpacesBlocks({ offset = 0 }: { offset?: number } = {}): Promise<void> {
-//   // Load limited number of spaces at a time
-//   const spaces = await prisma.space.findMany({
-//     select: {
-//       id: true
-//     },
-//     skip: offset,
-//     take: perBatch,
-//     orderBy: {
-//       id: 'asc'
-//     }
-//   });
+export async function countAllSpacesBlocks({ offset = 0 }: { offset?: number } = {}): Promise<void> {
+  // Load limited number of spaces at a time
+  const spaces = await prisma.space.findMany({
+    select: {
+      id: true
+    },
+    skip: offset,
+    take: perBatch,
+    orderBy: {
+      id: 'asc'
+    }
+  });
 
-//   for (const space of spaces) {
-//     await countSpaceBlocksAndSave({ spaceId: space.id });
-//   }
+  for (const space of spaces) {
+    await countSpaceBlocksAndSave({ spaceId: space.id });
+  }
 
-//   if (spaces.length > 0) {
-//     return countAllSpacesBlocks({ offset: offset + perBatch });
-//   }
-// }
+  if (spaces.length > 0) {
+    return countAllSpacesBlocks({ offset: offset + perBatch });
+  }
+}
 
 export async function countAllSpacesBlocksTask(): Promise<void> {
   // Delete old block count entries
   await deleteArchivalBlockCounts();
 
   // Wrapped function since Cron will call the method with the date
-  // await countAllSpacesBlocks();
+  await countAllSpacesBlocks();
 }
