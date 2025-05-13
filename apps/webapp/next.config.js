9a23e9f7a2c0a6a7dba3ac8fc05142e92cfd9257
mattcasey <mattwad@gmail.com>
2025-05-13 11:06:25 -0600

    fix config

--- apps/webapp/next.config.js
+++ apps/webapp/next.config.js
@@ -5,7 +5,65 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const BundleAnalyzer = require('next-bundle-analyzer');
 
-const esmModules = require('./next.base').esmModules;
+const esmModules = [
+  '@bangle.dev/base-components',
+  '@bangle.dev/utils',
+  'prosemirror-utils-bangle',
+  '@popperjs/core',
+  '@fullcalendar/common',
+  '@fullcalendar/core',
+  '@fullcalendar/daygrid',
+  '@fullcalendar/interaction',
+  '@fullcalendar/react',
+  'dnd-core',
+  'react-dnd',
+  '@react-dnd/invariant',
+  '@react-dnd/asap',
+  '@react-dnd/shallowequal',
+  'react-pdf',
+  'react-dnd-html5-backend',
+  'react-dnd-multi-backend',
+  'react-dnd-touch-backend',
+  'dnd-multi-backend',
+  'dnd-core',
+  '@react-dnd/invariant',
+  '@react-dnd/asap',
+  '@react-dnd/shallowequal',
+  'react-dnd-preview',
+  'redux',
+  '@hookform/resolvers',
+  // 'uuid',
+  'data-uri-to-buffer',
+  'fetch-blob',
+  'lodash-es',
+  // '@babel/runtime',
+  'formdata-polyfill',
+  'jose',
+  'nanoid',
+  '@lens-protocol',
+  'wagmi' // compile wagmi to avoid error: QueryClientProvider must be used as a child of ReactQueryClientProvider when running app
+];
+
+// this breaks the dev environment with an error when importing MUI icons: Cannot use 'import.meta' outside a module
+if (process.env.NODE_ENV === 'test') {
+  esmModules.push('@babel/runtime');
+  esmModules.push('isows');
+  // related modules
+  esmModules.push(
+    ...[
+      'wagmi',
+      '@wagmi/core',
+      '@wagmi/connectors',
+      '@walletconnect/core',
+      '@walletconnect/utils',
+      '@walletconnect/ethereum-provider',
+      '@walletconnect/relay-auth',
+      'preact',
+      'uint8arrays',
+      'multiformats'
+    ]
+  );
+}
 
 const config = {
   poweredByHeader: false,
