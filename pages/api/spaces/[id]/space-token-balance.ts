e8a6a328b6dd8a4610ed65a59bbce79b51c5cf53
Safwan Shaheer <devorein00@gmail.com>
2025-05-13 18:47:30 +0600

    space token balance

--- pages/api/spaces/[id]/space-token-balance.ts
+++ pages/api/spaces/[id]/space-token-balance.ts
@@ -1,8 +1,7 @@
-import { prisma } from '@charmverse/core/prisma-client';
 import { withSessionRoute } from '@root/lib/session/withSession';
+import { getSpaceTokenBalance } from '@root/lib/spaces/getSpaceTokenBalance';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import nc from 'next-connect';
-import { formatUnits } from 'viem';
 
 import { onError, onNoMatch, requireSpaceMembership, requireUser } from 'lib/middleware';
 
@@ -16,35 +15,9 @@ handler
 async function getSpaceTokenBalanceController(req: NextApiRequest, res: NextApiResponse<number>) {
   const { id: spaceId } = req.query as { id: string };
 
-  const space = await prisma.space.findUniqueOrThrow({
-    where: {
-      id: spaceId
-    },
-    select: {
-      subscriptionPayments: {
-        select: {
-          paidTokenAmount: true
-        }
-      },
-      spaceSubscriptionContributions: {
-        select: {
-          paidTokenAmount: true
-        }
-      }
-    }
-  });
+  const spaceTokenBalance = await getSpaceTokenBalance({ spaceId });
 
-  const subscriptionPaymentsAmount = space?.subscriptionPayments.reduce(
-    (acc, curr) => acc + BigInt(curr.paidTokenAmount),
-    BigInt(0)
-  );
-  const spaceSubscriptionContributionsAmount = space?.spaceSubscriptionContributions.reduce(
-    (acc, curr) => acc + BigInt(curr.paidTokenAmount),
-    BigInt(0)
-  );
-  const remainingSpaceTokenBalance = spaceSubscriptionContributionsAmount - subscriptionPaymentsAmount;
-
-  res.status(200).json(Number(formatUnits(remainingSpaceTokenBalance, 18)));
+  res.status(200).json(spaceTokenBalance);
 }
 
 export default withSessionRoute(handler);
