e05df87389f3f23954c105c2c0a66f57f3f965f8
mattcasey <mattwad@gmail.com>
2025-05-27 14:24:49 -0600

    more fixes

--- apps/webapp/pages/_document.tsx
+++ apps/webapp/pages/_document.tsx
@@ -3,29 +3,25 @@ import { DocumentHeadTags, documentGetInitialProps } from '@mui/material-nextjs/
 import { charmBlue as blueColor } from '@packages/config/colors';
 import Document, { Head, Html, Main, NextScript } from 'next/document';
 
-class MyDocument extends Document {
-  render() {
-    return (
-      <Html lang='en'>
-        <Head>
-          <DocumentHeadTags {...props} />
-          <meta name='theme-color' content={blueColor} />
-          <link rel='icon' href='/favicon.png' />
-          <script src='/__ENV.js' />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+export default function MyDocument(props: any) {
+  return (
+    <Html lang='en'>
+      <Head>
+        <DocumentHeadTags {...props} />
+        <meta name='theme-color' content={blueColor} />
+        <link rel='icon' href='/favicon.png' />
+        <script src='/__ENV.js' />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
 
 // ref: https://mui.com/material-ui/integrations/nextjs/#pages-router
-MyDocument.getInitialProps = async (ctx) => {
+MyDocument.getInitialProps = async (ctx: any) => {
   const finalProps = await documentGetInitialProps(ctx);
   return finalProps;
 };
-
-export default MyDocument;
