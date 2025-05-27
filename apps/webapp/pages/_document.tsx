3a4d2d06f13c32b457b422047f1b51c7036bccc1
mattcasey <mattwad@gmail.com>
2025-05-27 13:09:31 -0600

    add updates

--- apps/webapp/pages/_document.tsx
+++ apps/webapp/pages/_document.tsx
@@ -1,14 +1,14 @@
 /* eslint-disable @next/next/no-sync-scripts */
+import { DocumentHeadTags, documentGetInitialProps } from '@mui/material-nextjs/v15-pagesRouter';
 import { charmBlue as blueColor } from '@packages/config/colors';
 import Document, { Head, Html, Main, NextScript } from 'next/document';
-
-// Source for Emotion SSR: https://github.com/mui/material-ui/tree/332081eb5e5e107d915e3c70f92e430dc364048f/examples/nextjs-with-typescript
 
 class MyDocument extends Document {
   render() {
     return (
       <Html lang='en'>
         <Head>
+          <DocumentHeadTags {...props} />
           <meta name='theme-color' content={blueColor} />
           <link rel='icon' href='/favicon.png' />
           <script src='/__ENV.js' />
@@ -21,4 +21,10 @@ class MyDocument extends Document {
     );
   }
 }
+
+MyDocument.getInitialProps = async (ctx) => {
+  const finalProps = await documentGetInitialProps(ctx);
+  return finalProps;
+};
+
 export default MyDocument;
