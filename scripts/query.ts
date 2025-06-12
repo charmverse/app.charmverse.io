2951347b0e1f426839d6a6ef7623438fda9b4786
Matt Casey <mattwad@gmail.com>
2025-06-12 12:38:13 -0600

    add script to delete inactive spaces (#5238)

--- scripts/query.ts
+++ scripts/query.ts
@@ -6,35 +6,9 @@ import { task as exportSpaceDataTask } from '../apps/cron/src/tasks/exportSpaceD
 import { getSpaceTokenBalance } from '@packages/spaces/getSpaceTokenBalance';
 import { parseUnits } from 'viem';
 import { permissionsApiClient } from '@packages/lib/permissions/api/client';
-const goldTierPrice = 10_000;
-const silverTierPrice = 2_500;
-
-const newTier = 'gold' as const;
-const discount = 0.9;
-const tierPrice = newTier === 'gold' ? goldTierPrice : silverTierPrice;
+import fs from 'fs';
 
 async function query() {
-  prettyPrint(
-    await prisma.space.findFirst({
-      where: {
-        domain: 'holonlab'
-      },
-      include: {
-        spaceRoles: {
-          select: {
-            user: {
-              select: {
-                id: true,
-                email: true,
-                username: true
-              }
-            }
-          }
-        }
-      }
-    })
-  );
-  console.log(await getSpaceTokenBalance({ spaceId: '07c4bb6c-4be0-4927-8005-8820abadd783' }));
+  console.log(await prisma.cardNotification.count({}));
 }
-
 query();
