7c5cadad6e1e262b454d823d02cf997ea04ea826
Safwan Shaheer <devorein00@gmail.com>
2025-07-04 07:39:47 -0600

    updated admin users list

--- apps/webapp/lib/admin/users.ts
+++ apps/webapp/lib/admin/users.ts
@@ -1,5 +1,6 @@
 export const whitelist = [
   'd5b4e5db-868d-47b0-bc78-ebe9b5b2c835', // chris
   '4e1d4522-6437-4393-8ed1-9c56e53235f4', // matt
-  'dc521ceb-495e-40cc-940e-3b1cafc7a2e1' // alex
+  'dc521ceb-495e-40cc-940e-3b1cafc7a2e1', // alex
+  '5906c806-9497-43c7-9ffc-2eecd3c3a3ec' // safwan
 ];
